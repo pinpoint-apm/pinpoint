ca0fb89a4354d880cd3d496004c9e49464bbb424
Chisu Yu <puseori9th@gmail.com>
2012-08-06 02:53:52 +0000

    [유치수] [NOBTS] format the codes.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-tomcat-profiler/trunk@500 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/profiler/util/DefaultTBaseLocator.java
+++ src/main/java/com/profiler/util/DefaultTBaseLocator.java
@@ -3,18 +3,18 @@ package com.profiler.util;
 import com.profiler.dto.*;
 import org.apache.thrift.TBase;
 
-public class DefaultTBaseLocator implements TBaseLocator{
-    @Override
-    public TBase lookup(Header header) {
-        short type = header.getType();
-        switch (type) {
-            case 10:
-                return new JVMInfoThriftDTO();
-            case 20:
-                return new RequestDataListThriftDTO();
-            case 30:
-                return new RequestThriftDTO();
-        }
-        throw new IllegalArgumentException("Unsupported type:"  + type + " " + header);
-    }
+public class DefaultTBaseLocator implements TBaseLocator {
+	@Override
+	public TBase<?, ?> lookup(Header header) {
+		short type = header.getType();
+		switch (type) {
+			case 10:
+				return new JVMInfoThriftDTO();
+			case 20:
+				return new RequestDataListThriftDTO();
+			case 30:
+				return new RequestThriftDTO();
+		}
+		throw new IllegalArgumentException("Unsupported type:" + type + " " + header);
+	}
 }
