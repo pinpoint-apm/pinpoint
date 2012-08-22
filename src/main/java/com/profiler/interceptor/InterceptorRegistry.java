2a06c2a3486fa40fae25e129df25ff29d3b15317
Woonduk Kang <wd.kang@navercorp.com>
2012-08-22 06:47:12 +0000

    [강운덕] [LUCYSUS-1744] interceptor api 변경.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-tomcat-profiler/trunk@529 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/profiler/interceptor/InterceptorRegistry.java
+++ src/main/java/com/profiler/interceptor/InterceptorRegistry.java
@@ -7,8 +7,8 @@ public class InterceptorRegistry {
 
     private static final ConcurrentMap<String, Interceptor> INTERCEPTOR_MAP = new ConcurrentHashMap<String, Interceptor>(256);
 
-    public static void addInterceptor(String className, AroundInterceptor aroundInterceptor) {
-        INTERCEPTOR_MAP.put(className, aroundInterceptor);
+    public static void addInterceptor(String className, Interceptor interceptor) {
+        INTERCEPTOR_MAP.put(className, interceptor);
     }
 
     public static Interceptor getInterceptor(String className) {
