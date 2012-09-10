4138a1c975abc14eb8bc5effd40bd968a8c33920
Chisu Yu <puseori9th@gmail.com>
2012-09-10 07:41:44 +0000

    [유치수] [NOBTS] remove unnecessary files.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-server/trunk@613 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/profiler/data/reader/JVMDataReader.java
+++ src/main/java/com/profiler/data/reader/JVMDataReader.java
@@ -5,45 +5,24 @@ import java.net.DatagramPacket;
 import org.apache.log4j.Logger;
 import org.apache.thrift.TBase;
 
-import com.profiler.config.TomcatProfilerReceiverConfig;
-import com.profiler.config.TomcatProfilerReceiverConstant;
 import com.profiler.dto.JVMInfoThriftDTO;
 
 public class JVMDataReader implements Reader {
 	private static final Logger logger = Logger.getLogger(JVMDataReader.class.getName());
-	
+
 	public JVMDataReader() {
 	}
 
 	public void handler(TBase<?, ?> tbase, DatagramPacket datagramPacket) {
-        if (logger.isDebugEnabled()) {
-            logger.debug("handle " + tbase);
-        }
+		if (logger.isDebugEnabled()) {
+			logger.debug("handle " + tbase);
+		}
 		try {
-            JVMInfoThriftDTO dto = (JVMInfoThriftDTO) tbase;
-			int agentHashCode=dto.getAgentHashCode();
-			
-//			RequestTransactionDataManager manager=new RequestTransactionDataManager();
-//			manager.addAgentHashCode(agentHashCode);
-			
-			checkAgentHashCodeIsExist(agentHashCode);
+			JVMInfoThriftDTO dto = (JVMInfoThriftDTO) tbase;
 
-			if(TomcatProfilerReceiverConfig.USING_HBASE) {
-				String tableName=TomcatProfilerReceiverConstant.HBASE_JVM_TABLE+"_"+agentHashCode;
-//				PutJVMData put=new PutJVMData(tableName,dto);
-//				DataPutThreadManager.execute(put);
-			}
-			
+			System.out.println("Got JVM DTO. " + dto);
 		} catch (Exception e) {
 			logger.warn("ReadJVMData handle error " + e.getMessage(), e);
 		}
-	}
-	private void checkAgentHashCodeIsExist(int agentHashCode) {
-//		RequestTransactionDataManager manager=new RequestTransactionDataManager();
-//		if(manager.containsAgentInfo(agentHashCode)) {
-////			System.out.println(ReceivedDataManager.getAgentVO(agentHashCode));
-//		} else {
-////			System.out.println("No host data");
-//		}
 	}
 }
