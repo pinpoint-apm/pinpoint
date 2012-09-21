33e41ccd5b64ad012d2c53e35bec63794caecc94
Chisu Yu <puseori9th@gmail.com>
2012-09-21 02:33:48 +0000

    [유치수] [NOBTS] add build script.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-tomcat-profiler/trunk@677 84d0f5b1-2673-498c-a247-62c4ff18d310

---
+++ build.sh
@@ -0,0 +1,15 @@
+# agent
+mvn clean eclipse:eclipse package -Dmaven.test.skip
+
+cd ./target/classes
+jar cvfM HippoAgent.jar ./src/META-INF/MANIFEST.MF ./*
+
+rm -fr ../../../hippo-testbed/agent
+mkdir ../../../hippo-testbed/agent
+cp ./HippoAgent.jar ../../../hippo-testbed/agent
+
+cd ../../../hippo-tomcat-profiler
+cp ./target/dependency/javassist-3.16.1-GA.jar ../hippo-testbed/agent
+cp ./target/dependency/hippo-commons-0.0.1.jar ../hippo-testbed/agent
+cp ./lib/libthrift-0.8.0.wolog.jar ../hippo-testbed/agent
+cp ./runscript/* ../hippo-testbed/agent
