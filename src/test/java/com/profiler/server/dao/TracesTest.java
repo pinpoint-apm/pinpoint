2fe6b65d25670017ff3a079bf99352ed778edc42
Chisu Yu <puseori9th@gmail.com>
2012-09-25 09:51:05 +0000

    [유치수] [NOBTS] add serviceName to thrift.Span
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-server/trunk@703 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/test/java/com/profiler/server/dao/TracesTest.java
+++ src/test/java/com/profiler/server/dao/TracesTest.java
@@ -96,7 +96,7 @@ public class TracesTest {
         UUID uuid = UUID.randomUUID();
         List<Annotation> ano = Collections.emptyList();
         List<BinaryAnnotation> bano = Collections.emptyList();
-        Span span = new Span("UnitTest", System.currentTimeMillis(), uuid.getMostSignificantBits(), uuid.getLeastSignificantBits(), "test", 1, ano, bano);
+        Span span = new Span("UnitTest", System.currentTimeMillis(), uuid.getMostSignificantBits(), uuid.getLeastSignificantBits(), "test", "rpc", 1, ano, bano);
         return span;
     }
 }
