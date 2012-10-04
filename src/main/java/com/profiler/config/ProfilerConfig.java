a8d701c7b476a323774a09b6b917e0474d9c2bd8
Woonduk Kang <wd.kang@navercorp.com>
2012-10-04 04:57:40 +0000

    [강운덕] [LUCYSUS-1744] 설정파일의 위치를 찾는 부분 임시 수정.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-tomcat-profiler/trunk@723 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/profiler/config/ProfilerConfig.java
+++ src/main/java/com/profiler/config/ProfilerConfig.java
@@ -36,12 +36,12 @@ public class ProfilerConfig {
     @Deprecated
     public static boolean QUERY_COUNT_OVER_10000 = false;
 
-    private boolean jdbcProfile = false;
-    private boolean jdbcProfileMySql = false;
-    private boolean jdbcProfileMsSql = false;
-    private boolean jdbcProfileOracle = false;
-    private boolean jdbcProfileCubrid = false;
-    private boolean jdbcProfileDbcp = false;
+    private boolean jdbcProfile = true;
+    private boolean jdbcProfileMySql = true;
+    private boolean jdbcProfileMsSql = true;
+    private boolean jdbcProfileOracle = true;
+    private boolean jdbcProfileCubrid = true;
+    private boolean jdbcProfileDbcp = true;
 
 
     public ProfilerConfig() {
@@ -50,16 +50,18 @@ public class ProfilerConfig {
     public void readConfigFile() throws IOException{
         String hippoConfigFileName = System.getProperty("hippo.config");
         if (hippoConfigFileName == null) {
-            logger.info("hippo.config property is not set. Using default property file(\"hippo.config\")");
-            hippoConfigFileName = "hippo.config";
+            logger.info("hippo.config property is not set. Using default value:" + this);
+            return;
         }
 
         try {
+            // TODO file path를 찾는 부분을 수정해야됨 현재 제대로 안찾아짐. 설정파일이 classpath 에 걸려 있지않으므로 파일위치를 못찾음.
+            hippoConfigFileName = "hippo.config";
             Properties properties = PropertyUtils.readProperties(hippoConfigFileName);
             readPropertyValues(properties);
         } catch (FileNotFoundException fe) {
             if (logger.isLoggable(Level.WARNING)) {
-                logger.log(Level.WARNING, hippoConfigFileName + " file is not exists. Please check configuration. Cause:" + fe.getMessage(), fe);
+                logger.log(Level.WARNING, hippoConfigFileName + " file is not exists. Please check configuration.");
             }
             throw fe;
         } catch (IOException e) {
@@ -173,5 +175,18 @@ public class ProfilerConfig {
 
     public boolean isJdbcProfileDbcp() {
         return jdbcProfileDbcp;
+    }
+
+    @Override
+    public String toString() {
+        return "ProfilerConfig{" +
+                "profileEnable=" + profileEnable +
+                ", jdbcProfile=" + jdbcProfile +
+                ", jdbcProfileMySql=" + jdbcProfileMySql +
+                ", jdbcProfileMsSql=" + jdbcProfileMsSql +
+                ", jdbcProfileOracle=" + jdbcProfileOracle +
+                ", jdbcProfileCubrid=" + jdbcProfileCubrid +
+                ", jdbcProfileDbcp=" + jdbcProfileDbcp +
+                '}';
     }
 }
