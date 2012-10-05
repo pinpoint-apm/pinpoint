d8f8a964c972fe3bd9fec64c4677b6f8dc1d7139
Chisu Yu <puseori9th@gmail.com>
2012-10-05 07:46:24 +0000

    [유치수] [NOBTS] add hbase table reset script
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-commons/trunk@726 84d0f5b1-2673-498c-a247-62c4ff18d310

---
+++ hippo-hbase-reset.sh
@@ -0,0 +1,18 @@
+exec ./hbase shell <<EOF
+disable 'Servers'
+disable 'TraceIndex'
+disable 'Traces'
+disable 'SystemInfo'
+
+drop 'Servers'
+drop 'TraceIndex'
+drop 'Traces'
+drop 'SystemInfo'
+
+create 'Servers', { NAME => 'Agents' }
+create 'TraceIndex', { NAME => 'Trace' }
+create 'Traces', { NAME => 'Span' }
+create 'SystemInfo', { NAME => 'JVM' }
+
+list
+EOF
