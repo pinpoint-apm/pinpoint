63937ac00da4c406a15c8c2ffccbc56a3f076a76
Chisu Yu <puseori9th@gmail.com>
2012-10-08 02:28:07 +0000

    [유치수] [NOBTS] change testweb
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-testweb/trunk@729 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/nhn/hippo/testweb/controller/HelloWorldController.java
+++ src/main/java/com/nhn/hippo/testweb/controller/HelloWorldController.java
@@ -2,9 +2,9 @@ package com.nhn.hippo.testweb.controller;
 
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Random;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 import net.spy.memcached.ArcusClient;
 import net.spy.memcached.ConnectionFactoryBuilder;
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.nhn.hippo.testweb.domain.Member;
 import com.nhn.hippo.testweb.service.MemberService;
@@ -29,37 +28,36 @@ public class HelloWorldController {
 	@Autowired
 	private MemberService service;
 
+	/**
+	 * DO NOTHING
+	 * 
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value = "/donothing", method = RequestMethod.GET)
+	public String donothing(Model model) {
+		System.out.println("do nothing.");
+		return "donothing";
+	}
+
+	/**
+	 * CALL ARCUS
+	 * 
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping(value = "/arcus", method = RequestMethod.POST)
-	public String arcus(Model model, @RequestParam("id") int id) {
-		// delete
-		System.out.println("before service.delete");
-		service.delete(id);
-		System.out.println("after service.delete");
-
-		arcusSet(model);
-		arcusSet(model);
-
-		return "arcus";
-	}
-
-	@RequestMapping(value = "/arcustest", method = RequestMethod.GET)
-	public String arcusSet(Model model) {
-		setDataIntoArcus();
-		return "arcus";
-	}
-
-	private void setDataIntoArcus() {
+	public String arcus(Model model) {
 		try {
-			System.out.println("before arcus.set");
 			Future<Boolean> future = arcus.set("hippo:testkey", 10, "Hello, Hippo.");
-			// future.get(1000L, TimeUnit.MILLISECONDS);
-			System.out.println("after arcus.set");
+			future.get(1000L, TimeUnit.MILLISECONDS);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		return "arcus";
 	}
 
-	@RequestMapping(value = "/helloworld", method = RequestMethod.GET)
+	@RequestMapping(value = "/mysql", method = RequestMethod.GET)
 	public String mysql(Model model) {
 		int id = (new Random()).nextInt();
 
@@ -69,24 +67,33 @@ public class HelloWorldController {
 		member.setJoined(new Date());
 
 		// add
-		System.out.println("before service.add");
 		service.add(member);
-		System.out.println("after service.add");
 
 		// list
-		System.out.println("before service.list");
-		List<Member> list = service.list();
-		System.out.println("after service.list");
+		service.list();
 
-		// invoke http
-		System.out.println("before invoke.http");
-		HttpInvoker client = new HttpInvoker(new HttpConnectorOptions());
-		String executeToBloc = client.executeToBloc("http://localhost:9080/arcus.hippo?id=" + id, new HashMap<String, Object>());
-		System.out.println("after invoke.http");
+		// del
+		service.delete(id);
 
-		// setDataIntoArcus();
-		// setDataIntoArcus();
-
-		return "helloworld";
+		return "mysql";
 	}
+
+	@RequestMapping(value = "/invoke_arcus_http", method = RequestMethod.GET)
+	public String invoke_arcus_http(Model model) {
+		HttpInvoker client = new HttpInvoker(new HttpConnectorOptions());
+		client.executeToBloc("http://localhost:9080/arcus.hippo", new HashMap<String, Object>());
+
+		return "http";
+	}
+
+	@RequestMapping(value = "/combination", method = RequestMethod.GET)
+	public String combination(Model model) {
+		donothing(model);
+		arcus(model);
+		mysql(model);
+		invoke_arcus_http(model);
+
+		return "http";
+	}
+
 }
