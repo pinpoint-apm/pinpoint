a146020686550e99e2ad319a5edd3a8b038da7ef
Woonduk Kang <wd.kang@navercorp.com>
2012-10-11 01:57:38 +0000

    [강운덕] [LUCYSUS-1744] span조회 로직 추가.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-server/trunk@753 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/profiler/server/dao/Traces.java
+++ src/main/java/com/profiler/server/dao/Traces.java
@@ -1,16 +1,14 @@
 package com.profiler.server.dao;
 
+import com.profiler.common.dto.thrift.Span;
+import com.profiler.common.hbase.HBaseTables;
 import com.profiler.common.hbase.HbaseOperations2;
+import com.profiler.common.util.SpanUtils;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import com.profiler.common.dto.thrift.Span;
-import com.profiler.common.hbase.HBaseClient;
-import com.profiler.common.hbase.HBaseTables;
-import com.profiler.common.util.SpanUtils;
 
 public class Traces {
 
@@ -19,21 +17,19 @@ public class Traces {
 	private final byte[] COLFAM_SPAN = Bytes.toBytes("Span");
 
     @Autowired
-	private HBaseClient client;
-
-    @Autowired
     private HbaseOperations2 hbaseTemplate;
 
 	public boolean insert(final Span span, final byte[] spanBytes) {
-		try {
+        try {
             Put put = new Put(SpanUtils.getTracesRowkey(span), span.getTimestamp());
             put.add(COLFAM_SPAN, Bytes.toBytes(span.getSpanID()), spanBytes);
             hbaseTemplate.put(HBaseTables.TRACES, put);
 
-			return true;
-		} catch (Exception e) {
-			logger.error(e.getMessage(), e);
-		}
-		return false;
-	}
+            return true;
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
+        return false;
+    }
+
 }
