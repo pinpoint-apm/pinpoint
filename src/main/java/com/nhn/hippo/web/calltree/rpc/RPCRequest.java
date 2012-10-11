39e2dddc5108670bcc856e8245e65611a9c806f3
Woonduk Kang <wd.kang@navercorp.com>
2012-10-11 10:16:33 +0000

    [강운덕] [LUCYSUS-1744] rpc call tree 객체 이동.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-web/trunk@772 84d0f5b1-2673-498c-a247-62c4ff18d310

---
+++ src/main/java/com/nhn/hippo/web/calltree/rpc/RPCRequest.java
@@ -0,0 +1,44 @@
+package com.nhn.hippo.web.calltree.rpc;
+
+/**
+ * @author netspider
+ */
+public class RPCRequest {
+    private final String id;
+    private final RPC from;
+    private final RPC to;
+    private int callCount = 1;
+
+    public RPCRequest(RPC from, RPC to) {
+        this.from = from;
+        this.to = to;
+        this.id = from.getId() + to.getId();
+    }
+
+    public void increaseCallCount() {
+        callCount++;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public RPC getFrom() {
+        return from;
+    }
+
+    public RPC getTo() {
+        return to;
+    }
+
+    public int getCallCount() {
+        return callCount;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{from=").append(from).append(", to=").append(to).append(", cc=").append(callCount).append("}");
+        return sb.toString();
+    }
+}
