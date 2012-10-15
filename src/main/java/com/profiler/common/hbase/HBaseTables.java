6d24017f35a46a10526928ced6e21640b2feb2fd
Woonduk Kang <wd.kang@navercorp.com>
2012-10-15 10:01:55 +0000

    [강운덕] [LUCYSUS-1744] htable 이름을 상수로 사용도록 추가함.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-commons/trunk@787 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/profiler/common/hbase/HBaseTables.java
+++ src/main/java/com/profiler/common/hbase/HBaseTables.java
@@ -1,10 +1,15 @@
 package com.profiler.common.hbase;
 
+import org.apache.hadoop.hbase.util.Bytes;
+
 public class HBaseTables {
 
-	public static final String TRACE_INDEX = "TraceIndex";
+    public static final String TRACE_INDEX = "TraceIndex";
+    public static final byte[] TRACE_INDEX_CF_TRACE = Bytes.toBytes("Trace");
+    public static final byte[] TRACE_INDEX_CN_ID = Bytes.toBytes("ID");
 
-	public static final String TRACES = "Traces";
+    public static final String TRACES = "Traces";
+    public static final byte[] TRACES_CF_SPAN = Bytes.toBytes("Span");
 
-	public static final String SERVERS = "Servers";
+    public static final String SERVERS = "Servers";
 }
