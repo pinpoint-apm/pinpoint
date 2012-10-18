0e0b3b56cd5d98993616f34b8a9ac6d7abf0267f
Woonduk Kang <wd.kang@navercorp.com>
2012-10-18 05:42:20 +0000

    [강운덕] [LUCYSUS-1744] hbase 저장시의 데이터를 컴팩트하게 함.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-tomcat-profiler/trunk@808 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/profiler/context/Trace.java
+++ src/main/java/com/profiler/context/Trace.java
@@ -1,316 +1,322 @@
 package com.profiler.context;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import com.profiler.common.util.AnnotationTranscoder;
 import com.profiler.common.util.AnnotationTranscoder.Encoded;
 import com.profiler.sender.DataSender;
 import com.profiler.util.NamedThreadLocal;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
- * 
  * @author netspider
- * 
  */
 public final class Trace {
 
-	private static final Logger logger = Logger.getLogger(Trace.class.getName());
+    private static final Logger logger = Logger.getLogger(Trace.class.getName());
 
-	private static final DeadlineSpanMap spanMap = new DeadlineSpanMap();
-	private static final ThreadLocal<TraceIDStack> traceIdLocal = new NamedThreadLocal<TraceIDStack>("TraceId");
-	private static volatile boolean tracingEnabled = true;
+    private static final DeadlineSpanMap spanMap = new DeadlineSpanMap();
+    private static final ThreadLocal<TraceIDStack> traceIdLocal = new NamedThreadLocal<TraceIDStack>("TraceId");
+    private static volatile boolean tracingEnabled = true;
 
-	private static final AnnotationTranscoder transcoder = new AnnotationTranscoder();
-	
-	private Trace() {
-	}
+    private static final AnnotationTranscoder transcoder = new AnnotationTranscoder();
 
-	public static void handle(TraceHandler handler) {
-		TraceIDStack traceIDStack = traceIdLocal.get();
-		if (traceIDStack == null) {
-			traceIDStack = new TraceIDStack();
-			traceIdLocal.set(traceIDStack);
-		}
+    private Trace() {
+    }
 
-		try {
-			TraceID nextId = getNextTraceId();
-			traceIDStack.incr();
+    public static void handle(TraceHandler handler) {
+        TraceIDStack traceIDStack = traceIdLocal.get();
+        if (traceIDStack == null) {
+            traceIDStack = new TraceIDStack();
+            traceIdLocal.set(traceIDStack);
+        }
 
-			if (traceIDStack.getTraceId() == null) {
-				System.out.println(getCurrentTraceId());
-				traceIDStack.setTraceId(nextId);
-			}
+        try {
+            TraceID nextId = getNextTraceId();
+            traceIDStack.incr();
 
-			handler.handle();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			traceIDStack.decr();
-		}
-	}
+            if (traceIDStack.getTraceId() == null) {
+                if (logger.isLoggable(Level.FINE)) {
+                    logger.fine(getCurrentTraceId().toString());
+                }
+                traceIDStack.setTraceId(nextId);
+            }
 
-	public static void traceBlockBegin() {
-		TraceIDStack traceIDStack = traceIdLocal.get();
-		if (traceIDStack == null) {
-			traceIDStack = new TraceIDStack();
-			traceIdLocal.set(traceIDStack);
-		}
+            handler.handle();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            traceIDStack.decr();
+        }
+    }
 
-		try {
-			TraceID nextId = getNextTraceId();
-			traceIDStack.incr();
+    public static void traceBlockBegin() {
+        TraceIDStack traceIDStack = traceIdLocal.get();
+        if (traceIDStack == null) {
+            traceIDStack = new TraceIDStack();
+            traceIdLocal.set(traceIDStack);
+        }
 
-			if (traceIDStack.getTraceId() == null) {
-				traceIDStack.setTraceId(nextId);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+        try {
+            TraceID nextId = getNextTraceId();
+            traceIDStack.incr();
 
-	public static void traceBlockEnd() {
-		TraceIDStack traceIDStack = traceIdLocal.get();
-		traceIDStack.decr();
-	}
+            if (traceIDStack.getTraceId() == null) {
+                traceIDStack.setTraceId(nextId);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	/**
-	 * Get current TraceID or if's not exists create new one and return it.
-	 * 
-	 * @return
-	 */
-	public static TraceID getTraceIdOrCreateNew() {
-		// TraceID id = traceIdLocal.get();
-		TraceIDStack stack = traceIdLocal.get();
-		TraceID id = null;
-		if (stack != null) {
-			id = stack.getTraceId();
-		}
+    public static void traceBlockEnd() {
+        TraceIDStack traceIDStack = traceIdLocal.get();
+        traceIDStack.decr();
+    }
 
-		if (id == null) {
-			System.out.println("create new traceid");
+    /**
+     * Get current TraceID or if's not exists create new one and return it.
+     *
+     * @return
+     */
+    public static TraceID getTraceIdOrCreateNew() {
+        // TraceID id = traceIdLocal.get();
+        TraceIDStack stack = traceIdLocal.get();
+        TraceID id = null;
+        if (stack != null) {
+            id = stack.getTraceId();
+        }
 
-			id = TraceID.newTraceId();
-			// traceIdLocal.set(id);
+        if (id == null) {
+            System.out.println("create new traceid");
 
-			if (stack == null) {
-				traceIdLocal.set(new TraceIDStack());
-			}
+            id = TraceID.newTraceId();
+            // traceIdLocal.set(id);
 
-			traceIdLocal.get().setTraceId(id);
-			return id;
-		}
+            if (stack == null) {
+                traceIdLocal.set(new TraceIDStack());
+            }
 
-		return id;
-	}
+            traceIdLocal.get().setTraceId(id);
+            return id;
+        }
 
-	public static boolean removeCurrentTraceIdFromStack() {
-		TraceIDStack stack = traceIdLocal.get();
-		TraceID traceId = null;
+        return id;
+    }
 
-		if (stack != null) {
-			traceId = stack.getTraceId();
-		} else {
-			// TODO : remove this log.
-			System.out.println("#############################################################");
-			System.out.println("# Something's going wrong. Stack is not exists.             #");
-			System.out.println("#############################################################");
+    public static boolean removeCurrentTraceIdFromStack() {
+        TraceIDStack stack = traceIdLocal.get();
+        TraceID traceId = null;
 
-			stack = new TraceIDStack();
-			traceIdLocal.set(stack);
-		}
+        if (stack != null) {
+            traceId = stack.getTraceId();
+        } else {
+            // TODO : remove this log.
+            if (logger.isLoggable(Level.FINE)) {
+                logger.log(Level.FINE,
+                        "#############################################################" +
+                                "\n# Something's going wrong. Stack is not exists.             #" +
+                                "\n#############################################################");
+            }
 
-		if (traceId != null) {
-			stack.clear();
-			spanMap.remove(traceId);
-			return true;
-		}
-		return false;
-	}
+            stack = new TraceIDStack();
+            traceIdLocal.set(stack);
+        }
 
-	/**
-	 * Get current TraceID. If it was not set this will return null.
-	 * 
-	 * @return
-	 */
-	public static TraceID getCurrentTraceId() {
-		// return traceIdLocal.get();
-		TraceIDStack stack = traceIdLocal.get();
+        if (traceId != null) {
+            stack.clear();
+            spanMap.remove(traceId);
+            return true;
+        }
+        return false;
+    }
 
-		if (stack == null) {
-			return null;
-		}
+    /**
+     * Get current TraceID. If it was not set this will return null.
+     *
+     * @return
+     */
+    public static TraceID getCurrentTraceId() {
+        // return traceIdLocal.get();
+        TraceIDStack stack = traceIdLocal.get();
 
-		return stack.getTraceId();
-	}
+        if (stack == null) {
+            return null;
+        }
 
-	public static void enable() {
-		tracingEnabled = true;
-	}
+        return stack.getTraceId();
+    }
 
-	public static void disable() {
-		tracingEnabled = false;
-	}
+    public static void enable() {
+        tracingEnabled = true;
+    }
 
-	public static TraceID getNextTraceId() {
-		TraceID current = getTraceIdOrCreateNew();
-		return current.getNextTraceId();
-	}
+    public static void disable() {
+        tracingEnabled = false;
+    }
 
-	public static void setTraceId(TraceID traceId) {
-		// TODO: remove this, just for debugging.
-		if (getCurrentTraceId() != null) {
-			System.out.println("###############################################################################################################");
-			System.out.println("# [DEBUG MSG] TraceID is overwritten.");
-			System.out.println("#   Before : " + getCurrentTraceId());
-			System.out.println("#   After  : " + traceId);
-			System.out.println("###############################################################################################################");
-			new RuntimeException("TraceID overwritten.").printStackTrace();
-		}
+    public static TraceID getNextTraceId() {
+        TraceID current = getTraceIdOrCreateNew();
+        return current.getNextTraceId();
+    }
 
-		TraceIDStack stack = traceIdLocal.get();
+    public static void setTraceId(TraceID traceId) {
+        // TODO: remove this, just for debugging.
+        if (getCurrentTraceId() != null) {
+            if (logger.isLoggable(Level.FINE)) {
+                logger.log(Level.FINE,
+                        "###############################################################################################################" +
+                                "\n# [DEBUG MSG] TraceID is overwritten." +
+                                "\n#   Before : " + getCurrentTraceId() +
+                                "\n#   After  : " + traceId +
+                                "\n###############################################################################################################");
+                new RuntimeException("TraceID overwritten.").printStackTrace();
+            }
+        }
 
-		if (stack == null)
-			traceIdLocal.set(new TraceIDStack());
+        TraceIDStack stack = traceIdLocal.get();
 
-		Trace.traceIdLocal.get().setTraceId(traceId);
-		// Trace.traceIdLocal.set(traceId);
-	}
+        if (stack == null)
+            traceIdLocal.set(new TraceIDStack());
 
-	private static void mutate(TraceID traceId, SpanUpdater spanUpdater) {
-		Span span = spanMap.update(traceId, spanUpdater);
+        Trace.traceIdLocal.get().setTraceId(traceId);
+        // Trace.traceIdLocal.set(traceId);
+    }
 
-		if (span.isExistsAnnotationKey(Annotation.ClientRecv.getCode()) || span.isExistsAnnotationKey(Annotation.ServerSend.getCode())) {
-			// remove current context threadId from stack
-			removeCurrentTraceIdFromStack();
-			logSpan(span);
-		}
-	}
+    private static void mutate(TraceID traceId, SpanUpdater spanUpdater) {
+        Span span = spanMap.update(traceId, spanUpdater);
 
-	static void logSpan(Span span) {
-		try {
-			// TODO: send span to the server.
-			System.out.println("\n\n[WRITE SPAN] hashCode=" + span.hashCode() + ",\n\t " + span + ",\n\t SpanMap.size=" + spanMap.size() + ",\n\t CurrentThreadID=" + Thread.currentThread().getId() + ",\n\t CurrentThreadName=" + Thread.currentThread().getName() + "\n\n");
+        if (span.isExistsAnnotationKey(Annotation.ClientRecv.getCode()) || span.isExistsAnnotationKey(Annotation.ServerSend.getCode())) {
+            // remove current context threadId from stack
+            removeCurrentTraceIdFromStack();
+            logSpan(span);
+        }
+    }
 
-			// TODO: remove this, just for debugging
-			// if (spanMap.size() > 0) {
-			// System.out.println("##################################################################");
-			// System.out.println("# [DEBUG MSG] WARNING SpanMap size > 0 check spanMap.            #");
-			// System.out.println("##################################################################");
-			// System.out.println("current spamMap=" + spanMap);
-			// }
+    static void logSpan(Span span) {
+        try {
+            // TODO: send span to the server.
+            System.out.println("\n\n[WRITE SPAN] hashCode=" + span.hashCode() + ",\n\t " + span + ",\n\t SpanMap.size=" + spanMap.size() + ",\n\t CurrentThreadID=" + Thread.currentThread().getId() + ",\n\t CurrentThreadName=" + Thread.currentThread().getName() + "\n\n");
 
-			DataSender.getInstance().addDataToSend(span.toThrift());
+            // TODO: remove this, just for debugging
+            // if (spanMap.size() > 0) {
+            // System.out.println("##################################################################");
+            // System.out.println("# [DEBUG MSG] WARNING SpanMap size > 0 check spanMap.            #");
+            // System.out.println("##################################################################");
+            // System.out.println("current spamMap=" + spanMap);
+            // }
 
-			span.cancelTimer();
-		} catch (Exception e) {
-			logger.log(Level.SEVERE, e.getMessage(), e);
-		}
-	}
+            DataSender.getInstance().addDataToSend(span.toThrift());
 
-	public static void record(Annotation annotation) {
-		if (!tracingEnabled)
-			return;
+            span.cancelTimer();
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, e.getMessage(), e);
+        }
+    }
 
-		annotate(annotation.getCode(), null);
-	}
+    public static void record(Annotation annotation) {
+        if (!tracingEnabled)
+            return;
 
-	public static void record(Annotation annotation, long duration) {
-		if (!tracingEnabled)
-			return;
+        annotate(annotation.getCode(), null);
+    }
 
-		annotate(annotation.getCode(), duration);
-	}
+    public static void record(Annotation annotation, long duration) {
+        if (!tracingEnabled)
+            return;
 
-	public static void recordAttribute(final String key, final String value) {
-		recordAttibute(key, (Object) value);
-	}
+        annotate(annotation.getCode(), duration);
+    }
 
-	public static void recordAttibute(final String key, final Object value) {
-		if (!tracingEnabled)
-			return;
+    public static void recordAttribute(final String key, final String value) {
+        recordAttibute(key, (Object) value);
+    }
 
-		try {
-			mutate(getTraceIdOrCreateNew(), new SpanUpdater() {
-				@Override
-				public Span updateSpan(Span span) {
-					Encoded enc = transcoder.encode(value);
-					span.addAnnotation(new HippoAnnotation(System.currentTimeMillis(), key, enc.getValueType(), enc.getBytes(), null));
-					return span;
-				}
-			});
-		} catch (Exception e) {
-			logger.log(Level.SEVERE, e.getMessage(), e);
-		}
-	}
+    public static void recordAttibute(final String key, final Object value) {
+        if (!tracingEnabled)
+            return;
 
-	public static void recordMessage(String message) {
-		if (!tracingEnabled)
-			return;
+        try {
+            mutate(getTraceIdOrCreateNew(), new SpanUpdater() {
+                @Override
+                public Span updateSpan(Span span) {
+                    Encoded enc = transcoder.encode(value);
+                    span.addAnnotation(new HippoAnnotation(System.currentTimeMillis(), key, enc.getValueType(), enc.getBytes(), null));
+                    return span;
+                }
+            });
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, e.getMessage(), e);
+        }
+    }
 
-		annotate(message, null);
-	}
+    public static void recordMessage(String message) {
+        if (!tracingEnabled)
+            return;
 
-	public static void recordRpcName(final String service, final String rpc) {
-		if (!tracingEnabled)
-			return;
+        annotate(message, null);
+    }
 
-		try {
-			mutate(getTraceIdOrCreateNew(), new SpanUpdater() {
-				@Override
-				public Span updateSpan(Span span) {
-					span.setServiceName(service);
-					span.setName(rpc);
-					return span;
-				}
-			});
-		} catch (Exception e) {
-			logger.log(Level.SEVERE, e.getMessage(), e);
-		}
-	}
-	
-	public static void recordTerminalEndPoint(final String endPoint) {
-		recordEndPoint(endPoint, true);
-	}
-	
-	public static void recordEndPoint(final String endPoint) {
-		recordEndPoint(endPoint, false);
-	}
+    public static void recordRpcName(final String service, final String rpc) {
+        if (!tracingEnabled)
+            return;
 
-	// TODO: final String... endPoint로 받으면 합치는데 비용이 들어가 그냥 한번에 받는게 나을것 같음.
-	private static void recordEndPoint(final String endPoint, final boolean isTerminal) {
-		if (!tracingEnabled)
-			return;
+        try {
+            mutate(getTraceIdOrCreateNew(), new SpanUpdater() {
+                @Override
+                public Span updateSpan(Span span) {
+                    span.setServiceName(service);
+                    span.setName(rpc);
+                    return span;
+                }
+            });
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, e.getMessage(), e);
+        }
+    }
 
-		try {
-			mutate(getTraceIdOrCreateNew(), new SpanUpdater() {
-				@Override
-				public Span updateSpan(Span span) {
-					// set endpoint to both span and annotations
-					span.setEndPoint(endPoint);
-					span.setTerminal(isTerminal);
-					return span;
-				}
-			});
-		} catch (Exception e) {
-			logger.log(Level.SEVERE, e.getMessage(), e);
-		}
-	}
+    public static void recordTerminalEndPoint(final String endPoint) {
+        recordEndPoint(endPoint, true);
+    }
 
-	private static void annotate(final String key, final Long duration) {
-		if (!tracingEnabled)
-			return;
+    public static void recordEndPoint(final String endPoint) {
+        recordEndPoint(endPoint, false);
+    }
 
-		try {
-			mutate(getTraceIdOrCreateNew(), new SpanUpdater() {
-				@Override
-				public Span updateSpan(Span span) {
-					span.addAnnotation(new HippoAnnotation(System.currentTimeMillis(), key, duration));
-					return span;
-				}
-			});
-		} catch (Exception e) {
-			logger.log(Level.SEVERE, e.getMessage(), e);
-		}
-	}
+    // TODO: final String... endPoint로 받으면 합치는데 비용이 들어가 그냥 한번에 받는게 나을것 같음.
+    private static void recordEndPoint(final String endPoint, final boolean isTerminal) {
+        if (!tracingEnabled)
+            return;
+
+        try {
+            mutate(getTraceIdOrCreateNew(), new SpanUpdater() {
+                @Override
+                public Span updateSpan(Span span) {
+                    // set endpoint to both span and annotations
+                    span.setEndPoint(endPoint);
+                    span.setTerminal(isTerminal);
+                    return span;
+                }
+            });
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, e.getMessage(), e);
+        }
+    }
+
+    private static void annotate(final String key, final Long duration) {
+        if (!tracingEnabled)
+            return;
+
+        try {
+            mutate(getTraceIdOrCreateNew(), new SpanUpdater() {
+                @Override
+                public Span updateSpan(Span span) {
+                    span.addAnnotation(new HippoAnnotation(System.currentTimeMillis(), key, duration));
+                    return span;
+                }
+            });
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, e.getMessage(), e);
+        }
+    }
 }
