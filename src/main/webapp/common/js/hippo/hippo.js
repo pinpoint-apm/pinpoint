dfe8f1a3a80527dce0e4cecb0d5f2693ec1cdfea
Chisu Yu <puseori9th@gmail.com>
2012-10-26 00:46:51 +0000

    [유치수] [NOBTS] multi-selectable scatter chart.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-web/trunk@824 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/webapp/common/js/hippo/hippo.js
+++ src/main/webapp/common/js/hippo/hippo.js
@@ -88,29 +88,55 @@ function drawSankeyChart(graphdata, targetId) {
 	}
 }
 
+var selectedTraceIdSet = {};
+
 function drawScatter(data, targetId) {
+	//Brush.
+	var brush = d3.svg.brush()
+	    .on("brushstart", brushstart)
+	    .on("brush", brush)
+	    .on("brushend", brushend);
+	    
+	function brushstart(p) {
+	}
+
+	// find selected dot
+	function brush(p) {
+	  var e = brush.extent();
+	  svg.selectAll(".dot").each(function(d) {
+	    if( new Date(e[0][0]).getTime() <= d.timestamp && d.timestamp <= new Date(e[1][0]).getTime()
+	        && e[0][1] <= d.executionTime && d.executionTime <= e[1][1]) {
+	    	selectedTraceIdSet[d.traceId] = d;
+	    }
+	  });
+	}
+
+	function brushend() {
+		displaySelectedTraceIdList(selectedTraceIdSet);
+		selectedTraceIdSet = {};
+	}
+
+	var w = 960;
+	var h = 500;
+	
 	var margin = {top: 20, right: 20, bottom: 30, left: 40},
-	    width = 960 - margin.left - margin.right,
-	    height = 500 - margin.top - margin.bottom;
+	    width = w - margin.left - margin.right,
+	    height = h - margin.top - margin.bottom;
 
 	var x = d3.time.scale().range([0, width]);
 	var y = d3.scale.linear().range([height, 0]);
 	var color = d3.scale.category10();
 
-	var xAxis = d3.svg.axis()
-	    .scale(x)
-	    .orient("bottom");
-
-	var yAxis = d3.svg.axis()
-	    .scale(y)
-	    .orient("left");
+	var xAxis = d3.svg.axis().scale(x).orient("bottom");
+	var yAxis = d3.svg.axis().scale(y).orient("left");
 	
-	var svg = d3.select(targetId).append("svg")
+	 svg = d3.select(targetId).append("svg")
 	    .attr("width", width + margin.left + margin.right)
 	    .attr("height", height + margin.top + margin.bottom)
 	  .append("g")
-	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
+	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+	 	.call(brush.x(x).y(y));
+	
 	  //data.forEach(function(d) {
 	  //  d.timestamp = +d.timestamp;
 	  //  d.executionTime = +d.executionTime;
@@ -145,7 +171,7 @@ function drawScatter(data, targetId) {
 	      .data(data)
 	    .enter().append("circle")
 	      .attr("class", "dot")
-	      .attr("r", 3.5)
+	      .attr("r", 3)
 	      .attr("cx", function(d) { return x(d.timestamp); })
 	      .attr("cy", function(d) { return y(d.executionTime); })
 	      .style("fill", function(d) { return color(d.name); })
@@ -173,4 +199,46 @@ function drawScatter(data, targetId) {
 
 function openTrace(uuid) {
     window.open("/selectTransaction.hippo?traceId=" + uuid);
+}
+
+function displaySelectedTraceIdList(traces) {
+	var keys = Object.keys(traces);
+	
+	if (keys.length == 0) {
+		return;
+	}
+	
+	
+	if (keys.length == 1) {
+		openTrace(keys[0]);
+		return;
+	}
+	
+	$("#selectedBusinessTransactionsDetail TBODY").empty();
+	
+    var html = [];
+    for (var i = 0; i < keys.length; i++) {
+    	console.log(traces[keys[i]]);
+    	
+        html.push("<tr>");
+
+        html.push("<td>");
+        html.push(i + 1);
+        html.push("</td>");
+
+        html.push("<td><a href='#' onclick='openTrace(\"");
+        html.push(traces[keys[i]].traceId);
+        html.push("\");'>");
+        html.push(traces[keys[i]].traceId);
+        html.push("</a></td>");
+
+        html.push("<td>");
+        html.push(traces[keys[i]].executionTime);
+        html.push("</td>");
+
+        html.push("</tr>");
+    }
+    $("#selectedBusinessTransactionsDetail TBODY").append(html.join(''));
+	
+	$('#traceIdSelectModal').modal({});
 }
