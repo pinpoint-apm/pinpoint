6001aa54cc70182d928a1c574e547922ba1bada6
Chisu Yu <puseori9th@gmail.com>
2012-10-26 00:48:35 +0000

    [유치수] [NOBTS] change hbase schema creation script.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-commons/trunk@825 84d0f5b1-2673-498c-a247-62c4ff18d310

--- hippo-hbase-reset.sh
+++ hippo-hbase-reset.sh
@@ -4,18 +4,21 @@ disable 'RootTraceIndex'
 disable 'TraceIndex'
 disable 'Traces'
 disable 'SystemInfo'
+disable 'RootTraceIndex'
 
 drop 'Servers'
 drop 'RootTraceIndex'
 drop 'TraceIndex'
 drop 'Traces'
 drop 'SystemInfo'
+drop 'RootTraceIndex'
 
 create 'Servers', { NAME => 'Agents' }
 create 'RootTraceIndex', { NAME => 'Trace' }
 create 'TraceIndex', { NAME => 'Trace' }
 create 'Traces', { NAME => 'Span' }, { NAME => 'Annotation' }
 create 'SystemInfo', { NAME => 'JVM' }
+create 'RootTraceIndex', { NAME => 'Trace' }
 
 list
 EOF
