4c6d63e80f66c9b200543ff96244d0da2ca63d14
Woonduk Kang <wd.kang@navercorp.com>
2012-11-06 06:38:57 +0000

    [강운덕] [LUCYSUS-1744] 리팩토링
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-web/trunk@843 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/nhn/hippo/web/dao/HbaseApplicationIndexDao.java
+++ src/main/java/com/nhn/hippo/web/dao/HbaseApplicationIndexDao.java
@@ -16,38 +16,37 @@ import com.profiler.common.hbase.HBaseTables;
 import com.profiler.common.hbase.HbaseOperations2;
 
 /**
- * 
  * @author netspider
- * 
  */
 @Repository
 public class HbaseApplicationIndexDao implements ApplicationIndexDao {
-	private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
+    private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
 
-	@Autowired
-	private HbaseOperations2 hbaseOperations2;
+    @Autowired
+    private HbaseOperations2 hbaseOperations2;
 
-	@Autowired
-	@Qualifier("applicationNameMapper")
-	private RowMapper<String> applicationNameMapper;
+    @Autowired
+    @Qualifier("applicationNameMapper")
+    private RowMapper<String> applicationNameMapper;
 
-	@Autowired
-	@Qualifier("agentIdMapper")
-	private RowMapper<String[]> agentIdMapper;
+    @Autowired
+    @Qualifier("agentIdMapper")
+    private RowMapper<String[]> agentIdMapper;
 
-	@Override
-	public List<String> selectAllApplicationNames() {
-		Scan scan = new Scan();
-		return hbaseOperations2.find(HBaseTables.APPLICATION_INDEX, scan, applicationNameMapper);
-	}
+    @Override
+    public List<String> selectAllApplicationNames() {
+        Scan scan = new Scan();
+        scan.setCaching(30);
+        return hbaseOperations2.find(HBaseTables.APPLICATION_INDEX, scan, applicationNameMapper);
+    }
 
-	@Override
-	public String[] selectAgentIds(String applicationName) {
-		byte[] rowKey = Bytes.toBytes(applicationName);
+    @Override
+    public String[] selectAgentIds(String applicationName) {
+        byte[] rowKey = Bytes.toBytes(applicationName);
 
-		Get get = new Get(rowKey);
-		get.addFamily(HBaseTables.APPLICATION_CF_AGENTS);
+        Get get = new Get(rowKey);
+        get.addFamily(HBaseTables.APPLICATION_CF_AGENTS);
 
-		return hbaseOperations2.get(HBaseTables.APPLICATION_INDEX, get, agentIdMapper);
-	}
+        return hbaseOperations2.get(HBaseTables.APPLICATION_INDEX, get, agentIdMapper);
+    }
 }
