4f340ed20c121267cb12faff97517dfdb04ec58d
Woonduk Kang <wd.kang@navercorp.com>
2012-11-21 02:54:07 +0000

    [강운덕] [LUCYSUS-1744] annotation을 datatype 을 자동 인코딩 하도록 변경.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-web/trunk@886 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/nhn/hippo/web/utils/AnnotationUtils.java
+++ src/main/java/com/nhn/hippo/web/utils/AnnotationUtils.java
@@ -8,22 +8,24 @@ import com.profiler.common.util.AnnotationTranscoder;
 
 public class AnnotationUtils {
 
-	private static final AnnotationTranscoder transcoder = new AnnotationTranscoder();
-	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss SSS");
+    private static final AnnotationTranscoder transcoder = new AnnotationTranscoder();
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss SSS");
 
-	public static String bytesToString(int dataType, byte[] data) {
-		Object decoded = transcoder.decode(dataType, data);
-		if (decoded != null)
-			return decoded.toString();
-		else
-			return null;
-	}
+    public static String bytesToString(int dataType, byte[] data) {
+        Object decoded = transcoder.decode(dataType, data);
+        if (decoded != null)
+            return decoded.toString();
+        else
+            return null;
+    }
 
-	public static String longToDateStr(long date) {
-		return dateFormat.format(new Date(date));
-	}
+    @Deprecated
+    public static String longToDateStr(long date) {
+        // thread safe하지 않음
+        return dateFormat.format(new Date(date));
+    }
 
-	public static String longLongToUUID(long mostTraceId, long leastTraceId) {
-		return new UUID(mostTraceId, leastTraceId).toString();
-	}
+    public static String longLongToUUID(long mostTraceId, long leastTraceId) {
+        return new UUID(mostTraceId, leastTraceId).toString();
+    }
 }
