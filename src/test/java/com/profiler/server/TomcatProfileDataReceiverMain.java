7c5d5d809d2ef19f49d5bb24af3ad1d0e1f6075d
Woonduk Kang <wd.kang@navercorp.com>
2012-11-23 02:38:33 +0000

    [강운덕] [LUCYSUS-1744] maven test가 정상적으로 돌아가도록 수정.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-server/trunk@904 84d0f5b1-2673-498c-a247-62c4ff18d310

---
+++ src/test/java/com/profiler/server/TomcatProfileDataReceiverMain.java
@@ -0,0 +1,8 @@
+package com.profiler.server;
+
+public class TomcatProfileDataReceiverMain {
+
+    public static void main(String[] args) {
+        Server.main(args);
+    }
+}
--- src/test/java/com/profiler/server/TomcatProfileDataReceiverTest.java
+++
@@ -1,8 +0,0 @@
-package com.profiler.server;
-
-public class TomcatProfileDataReceiverTest {
-
-	public static void main(String[] args) {
-		Server.main(args);
-	}
-}
