57588d1ab5e74f779cfa9c4f16db251a20de6aa3
Chisu Yu <puseori9th@gmail.com>
2012-11-27 02:10:47 +0000

    [유치수] [NOBTS] add terminal info, change dbtype to servicetype
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-commons/trunk@918 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/profiler/common/ServiceType.java
+++ src/main/java/com/profiler/common/ServiceType.java
@@ -1,14 +1,23 @@
 package com.profiler.common;
 
 public enum ServiceType {
-
-	TOMCAT((short) 0, "TOMCAT", false), 
-	BLOC((short) 1, "BLOC", true), 
-	MEMCACHED((short) 2, "MEMCACHED", true), 
-	ARCUS((short) 3, "ARCUS", true),
-	MYSQL((short) 4, "MYSQL", true), 
-	UNKNOWN((short) -1, "UNKNOWN", false);
-
+	
+	UNKNOWN(		(short) 0,		"UNKNOWN",		false),
+	UNKNOWN_CLOUD(	(short) 1,		"UNKNOWN_EXT",	false),
+	
+	TOMCAT(			(short) 1001,	"TOMCAT",		false), 
+	BLOC(			(short) 1002,	"BLOC",			true),
+	
+	MEMCACHED(		(short) 2001,	"MEMCACHED",	true), 
+	ARCUS(			(short) 2002,	"ARCUS",		true),
+	
+	MYSQL(			(short) 3001,	"MYSQL",		true),
+	MSSQL(			(short) 3002,	"MSSQL",		true),
+	ORACLE(			(short) 3003,	"ORACLE",		true),
+	CUBRID(			(short) 3004,	"CUBRID",		true),
+	
+	HTTP_CLIENT(	(short) 9001,	"HTTP_CLIENT",	false);
+	
 	private short code;
 	private String desc;
 	private boolean terminal;
@@ -20,23 +29,31 @@ public enum ServiceType {
 	}
 
 	public static ServiceType parse(short code) {
-		if (TOMCAT.code == code) {
-			return TOMCAT;
-		} else if (BLOC.code == code) {
-			return BLOC;
-		} else if (MEMCACHED.code == code) {
-			return MEMCACHED;
-		} else if (ARCUS.code == code) {
-			return ARCUS;
-		} else if (MYSQL.code == code) {
-			return MYSQL;
-		} else {
-			return UNKNOWN;
+		ServiceType[] values = ServiceType.values();
+
+		for (ServiceType type : values) {
+			if (type.code == code) {
+				return type;
+			}
 		}
+
+		return UNKNOWN;
 	}
 
+	public boolean isRpcClient() {
+		return code >= 9000 && code < 10000;
+	}
+	
 	public short getCode() {
 		return code;
+	}
+	
+	public String getDesc() {
+		return desc;
+	}
+
+	public boolean isTerminal() {
+		return terminal;
 	}
 	
 	@Override
