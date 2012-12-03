242ada9dfa1c1befc8cbaaa4ec5540a865a7b79c
Chisu Yu <puseori9th@gmail.com>
2012-12-03 07:12:39 +0000

    [유치수] [NOBTS] terminal node request count incr.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-server/trunk@958 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/profiler/server/dao/hbase/HbaseTerminalStatisticsDao.java
+++ src/main/java/com/profiler/server/dao/hbase/HbaseTerminalStatisticsDao.java
@@ -3,11 +3,15 @@ package com.profiler.server.dao.hbase;
 import static com.profiler.common.hbase.HBaseTables.TERMINAL_STATISTICS;
 import static com.profiler.common.hbase.HBaseTables.TERMINAL_STATISTICS_CF_COUNTER;
 
+import org.apache.hadoop.hbase.KeyValue;
+import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.hadoop.hbase.RowMapper;
 
 import com.profiler.common.ServiceType;
 import com.profiler.common.hbase.HbaseOperations2;
@@ -39,10 +43,47 @@ public class HbaseTerminalStatisticsDao implements TerminalStatisticsDao {
 		long timeSlot = TimeSlot.getSlot(System.currentTimeMillis());
 		final byte[] rowKey = TerminalSpanUtils.makeRowKey(sourceApplicationName, timeSlot);
 
-		final Put put = new Put(rowKey);
-		put.add(TERMINAL_STATISTICS_CF_COUNTER, columnName, Bytes.toBytes(1));
-
-		hbaseTemplate.put(TERMINAL_STATISTICS, put);
+//		final Put put = new Put(rowKey);
+//		put.add(TERMINAL_STATISTICS_CF_COUNTER, columnName, Bytes.toBytes(1));
+		
+//		Get get = new Get(rowKey);
+//		Long count = hbaseTemplate.get(TERMINAL_STATISTICS, get, new RowMapper<Long>() {
+//			@Override
+//			public Long mapRow(Result result, int rowNum) throws Exception {
+//				if (result != null) {
+//					KeyValue[] kv = result.raw();
+//					if (kv.length > 0) {
+//						return Bytes.toLong(kv[0].getValue());
+//					}
+//				}
+//				return null;
+//			}
+//		});
+//		
+//		System.out.println("before = " + count);
+		
+		hbaseTemplate.incrementColumnValue(TERMINAL_STATISTICS, rowKey, TERMINAL_STATISTICS_CF_COUNTER, columnName, 1L);
+		
+//		System.out.println("increment value," + sourceApplicationName + " => " + destApplicationName);
+		
+//		Get get2 = new Get(rowKey);
+//		Long count2 = hbaseTemplate.get(TERMINAL_STATISTICS, get2, new RowMapper<Long>() {
+//			@Override
+//			public Long mapRow(Result result, int rowNum) throws Exception {
+//				if (result != null) {
+//					KeyValue[] kv = result.raw();
+//					if (kv.length > 0) {
+//						return Bytes.toLong(kv[0].getValue());
+//					}
+//				}
+//				return null;
+//			}
+//		});
+//		
+//		System.out.println("after = " + count2);		
+		
+		
+//		hbaseTemplate.put(TERMINAL_STATISTICS, put);
 
 		// TODO respose time histogram, agentId list가 추가되어야 함.
 	}
