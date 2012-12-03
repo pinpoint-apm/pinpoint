262f7880c2587f0570bee725115530ae231e7158
Woonduk Kang <wd.kang@navercorp.com>
2012-12-03 08:36:25 +0000

    [강운덕] [LUCYSUS-1744] 비지니스 트랜잭션 다시 보이게 수정.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-web/trunk@978 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/nhn/hippo/web/calltree/span/SpanAlign.java
+++ src/main/java/com/nhn/hippo/web/calltree/span/SpanAlign.java
@@ -1,6 +1,7 @@
 package com.nhn.hippo.web.calltree.span;
 
 import com.profiler.common.bo.SpanBo;
+import com.profiler.common.bo.SubSpanBo;
 import com.profiler.common.dto.thrift.Span;
 import org.apache.commons.lang.StringUtils;
 
@@ -10,10 +11,26 @@ import org.apache.commons.lang.StringUtils;
 public class SpanAlign {
     private int depth;
     private SpanBo span;
+    private SubSpanBo subSpanBo;
+    private boolean root = true;
 
     public SpanAlign(int depth, SpanBo span) {
         this.depth = depth;
         this.span = span;
+    }
+
+    public SpanAlign(int depth, SpanBo root, SubSpanBo subSpanBo) {
+        this.depth = depth;
+        this.span = root;
+        this.subSpanBo = subSpanBo;
+    }
+
+    public void setRoot(boolean root) {
+        this.root = root;
+    }
+
+    public boolean isRoot() {
+        return root;
     }
 
     public int getDepth() {
@@ -30,5 +47,9 @@ public class SpanAlign {
 
     public SpanBo getSpan() {
         return span;
+    }
+
+    public SubSpanBo getSubSpanBo() {
+        return subSpanBo;
     }
 }
--- src/main/java/com/nhn/hippo/web/service/SpanServiceImpl.java
+++ src/main/java/com/nhn/hippo/web/service/SpanServiceImpl.java
@@ -1,9 +1,8 @@
 package com.nhn.hippo.web.service;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
+import com.profiler.common.bo.SubSpanBo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,61 +23,116 @@ import com.profiler.common.mapping.MethodMapping;
 @Service
 public class SpanServiceImpl implements SpanService {
 
-	private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	@Autowired
-	private TraceDao traceDao;
+    @Autowired
+    private TraceDao traceDao;
 
-	@Override
-	public List<SpanAlign> selectSpan(String uuid) {
-		UUID id = UUID.fromString(uuid);
-		List<SpanBo> spans = traceDao.selectSpanAndAnnotation(id);
-		if (spans == null) {
-			return Collections.emptyList();
-		}
-		transitionApiId(spans);
-		List<SpanAlign> order = order(spans);
-		// TODO root span not found시 row data라도 보여줘야 됨.
-		if (order.size() != spans.size()) {
-			// TODO 중간 노드 데이터 분실 ? 혹은 잘못된 데이터 생성?
-			logger.info("span node not complete! ");
-		}
-		return order;
+    @Override
+    public List<SpanAlign> selectSpan(String uuid) {
+        UUID id = UUID.fromString(uuid);
+        List<SpanBo> spans = traceDao.selectSpanAndAnnotation(id);
+        if (spans == null) {
+            return Collections.emptyList();
+        }
+        transitionApiId(spans);
+        List<SpanAlign> order = order(spans);
+        List<SpanAlign> list = populateSubSpan(order);
+        // TODO root span not found시 row data라도 보여줘야 됨.
+        if (order.size() != spans.size()) {
+            // TODO 중간 노드 데이터 분실 ? 혹은 잘못된 데이터 생성?
+            logger.info("span node not complete! ");
+        }
+        return list;
 
-	}
+    }
 
-	private void transitionApiId(List<SpanBo> spans) {
-		for (SpanBo spanBo : spans) {
-			List<AnnotationBo> annotationBoList = spanBo.getAnnotationBoList();
-			for (AnnotationBo annotationBo : annotationBoList) {
-				// TODO API-ID 일단 날코딩 나중에 뭔가 key를 따자
-				if ("API-ID".equals(annotationBo.getKey())) {
-					MethodMapping methodMapping = null;
-					try {
-						methodMapping = ApiMappingTable.findMethodMapping((Integer) annotationBo.getValue());
-					} catch (Exception e) {
-						e.printStackTrace(); // To change body of catch
-												// statement use File | Settings
-												// | File Templates.
-					}
-					String className = methodMapping.getClassMapping().getClassName();
-					String methodName = methodMapping.getMethodName();
-					String[] parameterType = methodMapping.getParameterType();
-					String[] parameterName = methodMapping.getParameterName();
-					String args = ApiUtils.mergeParameterVariableNameDescription(parameterType, parameterName);
-					AnnotationBo api = new AnnotationBo();
-					api.setKey("API");
-					api.setValue(className + "." + methodName + args);
-					annotationBoList.add(api);
-					break;
-				}
-			}
-		}
-	}
+    private List populateSubSpan(List<SpanAlign> order) {
+        List<SpanAlign> populatedList = new ArrayList<SpanAlign>();
 
-	private List<SpanAlign> order(List<SpanBo> spans) {
+        for (int i = 0; i < order.size(); i++) {
+            populatedSpan(populatedList, order, i);
+        }
+        return populatedList;
+    }
 
-		SpanAligner spanAligner = new SpanAligner(spans);
-		return spanAligner.sort();
-	}
+    private void populatedSpan(List<SpanAlign> populatedList, List<SpanAlign> order, int i) {
+        SpanAlign spanAlign = order.get(i);
+        SpanBo span = spanAlign.getSpan();
+        populatedList.add(spanAlign);
+        long startTime = span.getStartTime();
+        List<SubSpanBo> subSpanBos = sortSubSpan(span);
+        for (SubSpanBo subSpanBo : subSpanBos) {
+            long subStartTime = startTime + subSpanBo.getStartElapsed();
+            long nextSpanStartTime = getNextSpanStartTime(order, i);
+            if (subStartTime >= nextSpanStartTime) {
+                SpanAlign subSpanAlign = new SpanAlign(spanAlign.getDepth(), span, subSpanBo);
+                subSpanAlign.setRoot(false);
+                populatedList.add(subSpanAlign);
+            } else {
+                populatedSpan(populatedList, order, i);
+            }
+        }
+    }
+
+    public long getNextSpanStartTime(List<SpanAlign> order, int i) {
+        if (i < order.size()) {
+            return 0;
+        }
+        return order.get(i + 1).getSpan().getStartTime();
+    }
+
+    private List<SubSpanBo> sortSubSpan(SpanBo span) {
+        List<SubSpanBo> subSpanList = span.getSubSpanList();
+        Collections.sort(subSpanList, new Comparator<SubSpanBo>() {
+            @Override
+            public int compare(SubSpanBo o1, SubSpanBo o2) {
+                long o1Timestamp = o1.getSequence();
+                long o2Timestamp = o2.getSequence();
+                if (o1Timestamp > o2Timestamp) {
+                    return 1;
+                }
+                if (o1Timestamp == o2Timestamp) {
+                    return 0;
+                }
+                return -1;
+            }
+        });
+        return subSpanList;
+    }
+
+    private void transitionApiId(List<SpanBo> spans) {
+        for (SpanBo spanBo : spans) {
+            List<AnnotationBo> annotationBoList = spanBo.getAnnotationBoList();
+            for (AnnotationBo annotationBo : annotationBoList) {
+                // TODO API-ID 일단 날코딩 나중에 뭔가 key를 따자
+                if ("API-ID".equals(annotationBo.getKey())) {
+                    MethodMapping methodMapping = null;
+                    try {
+                        methodMapping = ApiMappingTable.findMethodMapping((Integer) annotationBo.getValue());
+                    } catch (Exception e) {
+                        e.printStackTrace(); // To change body of catch
+                        // statement use File | Settings
+                        // | File Templates.
+                    }
+                    String className = methodMapping.getClassMapping().getClassName();
+                    String methodName = methodMapping.getMethodName();
+                    String[] parameterType = methodMapping.getParameterType();
+                    String[] parameterName = methodMapping.getParameterName();
+                    String args = ApiUtils.mergeParameterVariableNameDescription(parameterType, parameterName);
+                    AnnotationBo api = new AnnotationBo();
+                    api.setKey("API");
+                    api.setValue(className + "." + methodName + args);
+                    annotationBoList.add(api);
+                    break;
+                }
+            }
+        }
+    }
+
+    private List<SpanAlign> order(List<SpanBo> spans) {
+
+        SpanAligner spanAligner = new SpanAligner(spans);
+        return spanAligner.sort();
+    }
 }
