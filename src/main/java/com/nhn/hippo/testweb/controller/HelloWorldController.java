185dc416122aa3754a2797e6862a37abaa86ffbb
Woonduk Kang <wd.kang@navercorp.com>
2012-12-03 06:56:34 +0000

    [강운덕] [LUCYSUS-1744] span 데이터 구조 변경.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-testweb/trunk@955 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/nhn/hippo/testweb/controller/HelloWorldController.java
+++ src/main/java/com/nhn/hippo/testweb/controller/HelloWorldController.java
@@ -12,6 +12,7 @@ import net.spy.memcached.ArcusClient;
 import net.spy.memcached.ConnectionFactoryBuilder;
 import net.spy.memcached.MemcachedClient;
 
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,117 +24,123 @@ import com.nhn.hippo.testweb.util.HttpConnectorOptions;
 import com.nhn.hippo.testweb.util.HttpInvoker;
 
 @Controller
-public class HelloWorldController {
+public class HelloWorldController implements DisposableBean {
 
-	private final ArcusClient arcus;
-	private final MemcachedClient memcached;
+    private final ArcusClient arcus;
+    private final MemcachedClient memcached;
 
-	public HelloWorldController() throws IOException {
-		arcus = ArcusClient.createArcusClient("dev.arcuscloud.nhncorp.com:17288", "dev", new ConnectionFactoryBuilder());
-		memcached = new MemcachedClient(AddrUtil.getAddresses("10.25.149.80:11211"));
-	}
+    public HelloWorldController() throws IOException {
+        arcus = ArcusClient.createArcusClient("dev.arcuscloud.nhncorp.com:17288", "dev", new ConnectionFactoryBuilder());
+        memcached = new MemcachedClient(AddrUtil.getAddresses("10.25.149.80:11211"));
+    }
 
-	@Autowired
-	private MemberService service;
+    @Autowired
+    private MemberService service;
 
-	@RequestMapping(value = "/donothing")
-	public String donothing(Model model) {
-		System.out.println("do nothing.");
-		return "donothing";
-	}
+    @RequestMapping(value = "/donothing")
+    public String donothing(Model model) {
+        System.out.println("do nothing.");
+        return "donothing";
+    }
 
-	@RequestMapping(value = "/arcus")
-	public String arcus(Model model) {
-		Future<Boolean> future = null;
-		try {
-			future = arcus.set("hippo:testkey", 10, "Hello, Hippo.");
-			future.get(1000L, TimeUnit.MILLISECONDS);
-		} catch (Exception e) {
-			if (future != null)
-				future.cancel(true);
-		}
-		return "arcus";
-	}
+    @RequestMapping(value = "/arcus")
+    public String arcus(Model model) {
+        Future<Boolean> future = null;
+        try {
+            future = arcus.set("hippo:testkey", 10, "Hello, Hippo.");
+            future.get(1000L, TimeUnit.MILLISECONDS);
+        } catch (Exception e) {
+            if (future != null)
+                future.cancel(true);
+        }
+        return "arcus";
+    }
 
-	@RequestMapping(value = "/memcached")
-	public String memcached(Model model) {
-		Future<Boolean> future = null;
-		try {
-			future = memcached.set("hippo:testkey", 10, "Hello, Hippo.");
-			future.get(1000L, TimeUnit.MILLISECONDS);
-		} catch (Exception e) {
-			if (future != null)
-				future.cancel(true);
-		}
-		return "memcached";
-	}
+    @RequestMapping(value = "/memcached")
+    public String memcached(Model model) {
+        Future<Boolean> future = null;
+        try {
+            future = memcached.set("hippo:testkey", 10, "Hello, Hippo.");
+            future.get(1000L, TimeUnit.MILLISECONDS);
+        } catch (Exception e) {
+            if (future != null)
+                future.cancel(true);
+        }
+        return "memcached";
+    }
 
-	@RequestMapping(value = "/mysql")
-	public String mysql(Model model) {
-		int id = (new Random()).nextInt();
+    @RequestMapping(value = "/mysql")
+    public String mysql(Model model) {
+        int id = (new Random()).nextInt();
 
-		Member member = new Member();
-		member.setId(id);
-		member.setName("chisu");
-		member.setJoined(new Date());
+        Member member = new Member();
+        member.setId(id);
+        member.setName("chisu");
+        member.setJoined(new Date());
 
-		// add
-		service.add(member);
+        // add
+        service.add(member);
 
-		// list
-		service.list();
+        // list
+        service.list();
 
-		// del
-		service.delete(id);
+        // del
+        service.delete(id);
 
-		return "mysql";
-	}
+        return "mysql";
+    }
 
-	@RequestMapping(value = "/remotecombination")
-	public String remotecombination(Model model) {
-		HttpInvoker client = new HttpInvoker(new HttpConnectorOptions());
-		client.executeToBloc("http://localhost:8080/combination.hippo", new HashMap<String, Object>());
+    @RequestMapping(value = "/remotecombination")
+    public String remotecombination(Model model) {
+        HttpInvoker client = new HttpInvoker(new HttpConnectorOptions());
+        client.executeToBloc("http://localhost:8080/combination.hippo", new HashMap<String, Object>());
 
-		return "remotecombination";
-	}
+        return "remotecombination";
+    }
 
-	@RequestMapping(value = "/combination")
-	public String combination(Model model) {
-		mysql(model);
-		arcus(model);
-		memcached(model);
+    @RequestMapping(value = "/combination")
+    public String combination(Model model) {
+        mysql(model);
+        arcus(model);
+        memcached(model);
 
-		HttpInvoker client = new HttpInvoker(new HttpConnectorOptions());
-		client.executeToBloc("http://www.naver.com/", new HashMap<String, Object>());
-		client.executeToBloc("http://www.naver.com/", new HashMap<String, Object>());
+        HttpInvoker client = new HttpInvoker(new HttpConnectorOptions());
+        client.executeToBloc("http://www.naver.com/", new HashMap<String, Object>());
+        client.executeToBloc("http://www.naver.com/", new HashMap<String, Object>());
 
-		client.executeToBloc("http://section.cafe.naver.com/", new HashMap<String, Object>());
-		client.executeToBloc("http://section.cafe.naver.com/", new HashMap<String, Object>());
+        client.executeToBloc("http://section.cafe.naver.com/", new HashMap<String, Object>());
+        client.executeToBloc("http://section.cafe.naver.com/", new HashMap<String, Object>());
 
-		return "combination";
-	}
+        return "combination";
+    }
 
-	@RequestMapping(value = "/error500")
-	public String error500(Model model) {
-		int i = 1 / 0;
-		return "error";
-	}
+    @RequestMapping(value = "/error500")
+    public String error500(Model model) {
+        int i = 1 / 0;
+        return "error";
+    }
 
-	@RequestMapping(value = "/throwexception")
-	public String exception(Model model) {
-		throw new RuntimeException("Exception test");
-	}
+    @RequestMapping(value = "/throwexception")
+    public String exception(Model model) {
+        throw new RuntimeException("Exception test");
+    }
 
-	@RequestMapping(value = "/arcustimeout")
-	public String arcustimeout(Model model) {
-		Future<Boolean> future = null;
-		try {
-			future = arcus.set("hippo:expect-timeout", 10, "Hello, Timeout.");
-			future.get(100L, TimeUnit.MICROSECONDS);
-		} catch (Exception e) {
-			if (future != null)
-				future.cancel(true);
-		}
-		return "timeout";
-	}
+    @RequestMapping(value = "/arcustimeout")
+    public String arcustimeout(Model model) {
+        Future<Boolean> future = null;
+        try {
+            future = arcus.set("hippo:expect-timeout", 10, "Hello, Timeout.");
+            future.get(100L, TimeUnit.MICROSECONDS);
+        } catch (Exception e) {
+            if (future != null)
+                future.cancel(true);
+        }
+        return "timeout";
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        arcus.shutdown();
+        memcached.shutdown();
+    }
 }
