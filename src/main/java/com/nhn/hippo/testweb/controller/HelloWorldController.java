49e0fd95ed15776d995a51b748af64d77beaf8b7
Chisu Yu <puseori9th@gmail.com>
2012-12-10 08:02:50 +0000

    [유치수] [NOBTS] add encoding testcase
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-testweb/trunk@1007 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/nhn/hippo/testweb/controller/HelloWorldController.java
+++ src/main/java/com/nhn/hippo/testweb/controller/HelloWorldController.java
@@ -17,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.nhn.hippo.testweb.domain.Member;
 import com.nhn.hippo.testweb.service.MemberService;
@@ -37,6 +38,15 @@ public class HelloWorldController implements DisposableBean {
     @Autowired
     private MemberService service;
 
+	@RequestMapping(value = "/encoding")
+	public String encoding(Model model, @RequestParam("name") String name) {
+		System.out.println("name=" + name);
+		
+		new RuntimeException().printStackTrace();
+		
+		return "donothing";
+	}
+    
     @RequestMapping(value = "/donothing")
     public String donothing(Model model) {
         System.out.println("do nothing.");
