569f40c275fad37f451b10c4fdd8e2dc4d0f1a1c
Woonduk Kang <wd.kang@navercorp.com>
2012-12-20 05:23:51 +0000

    [강운덕] [LUCYSUS-1744] 데이터 사이즈 축소를 위해 annotaion의 time을 제거함.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-commons/trunk@1045 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/test/java/com/profiler/common/bo/AnnotationBoTest.java
+++ src/test/java/com/profiler/common/bo/AnnotationBoTest.java
@@ -31,7 +31,6 @@ public class AnnotationBoTest {
         AnnotationBo bo2 = new AnnotationBo();
         bo2.readValue(buffer.getBuffer(), 0);
         Assert.assertEquals(bo.getKey(), bo2.getKey());
-        Assert.assertEquals(bo.getTimestamp(), bo2.getTimestamp());
         Assert.assertEquals(bo.getValueType(), bo2.getValueType());
         Assert.assertArrayEquals(bo.getByteValue(), bo2.getByteValue());
     }
