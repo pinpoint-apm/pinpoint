c1578af83238704d7a08a0eba068264c355293b8
Woonduk Kang <wd.kang@navercorp.com>
2012-12-20 06:02:30 +0000

    [강운덕] [LUCYSUS-1744] BinarySearch부분의 오류를 발견하여 마크
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-web/trunk@1048 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/nhn/hippo/web/utils/AnnotationUtils.java
+++ src/main/java/com/nhn/hippo/web/utils/AnnotationUtils.java
@@ -13,69 +13,69 @@ import com.profiler.common.bo.Span;
 
 public class AnnotationUtils {
 
-	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss SSS");
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss SSS");
 
-	private static final String API = "API";
-	private static final String ARCUS_COMMAND = "arcus.command";
-	private static final String HTTP_URL = "http.url";
+    private static final String API = "API";
+    private static final String ARCUS_COMMAND = "arcus.command";
+    private static final String HTTP_URL = "http.url";
 
-	@Deprecated
-	public static String longToDateStr(long date) {
-		// thread safe하지 않음
-		return dateFormat.format(new Date(date));
-	}
+    @Deprecated
+    public static String longToDateStr(long date) {
+        // thread safe하지 않음
+        return dateFormat.format(new Date(date));
+    }
 
-	public static String longLongToUUID(long mostTraceId, long leastTraceId) {
-		return new UUID(mostTraceId, leastTraceId).toString();
-	}
+    public static String longLongToUUID(long mostTraceId, long leastTraceId) {
+        return new UUID(mostTraceId, leastTraceId).toString();
+    }
 
-	private static Comparator<AnnotationBo> annotationKeyComparator = new Comparator<AnnotationBo>() {
-		@Override
-		public int compare(AnnotationBo a1, AnnotationBo a2) {
-			return a1.getKey().compareTo(a2.getKey());
-		}
-	};
+    private static Comparator<AnnotationBo> annotationKeyComparator = new Comparator<AnnotationBo>() {
+        @Override
+        public int compare(AnnotationBo a1, AnnotationBo a2) {
+            return a1.getKey().compareTo(a2.getKey());
+        }
+    };
 
-	public static void sortAnnotationListByKey(Span span) {
-		List<AnnotationBo> list = span.getAnnotationBoList();
-		Collections.sort(list, annotationKeyComparator);
-	}
+    public static void sortAnnotationListByKey(Span span) {
+        List<AnnotationBo> list = span.getAnnotationBoList();
+        Collections.sort(list, annotationKeyComparator);
+    }
 
-	public static Object getDisplayMethod(Span span) {
-		List<AnnotationBo> list = span.getAnnotationBoList();
-		int index = Collections.binarySearch(list, API);
+    public static Object getDisplayMethod(Span span) {
+        List<AnnotationBo> list = span.getAnnotationBoList();
+        int index = Collections.binarySearch(list, API);
 
-		if (index > -1) {
-			return list.get(index).getValue();
-		}
+        if (index > -1) {
+            return list.get(index).getValue();
+        }
 
-		if (span.getServiceType() == ServiceType.ARCUS || span.getServiceType() == ServiceType.MEMCACHED) {
-			return span.getRpc(); // return OperationImpl
-		}
+        if (span.getServiceType() == ServiceType.ARCUS || span.getServiceType() == ServiceType.MEMCACHED) {
+            return span.getRpc(); // return OperationImpl
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	public static Object getDisplayArgument(Span span) {
-		List<AnnotationBo> list = span.getAnnotationBoList();
-		int index = -1;
+    public static Object getDisplayArgument(Span span) {
+        List<AnnotationBo> list = span.getAnnotationBoList();
+        int index = -1;
+        // TODO BinarySearch는 collection이 정렬되어 있어야 올바른 값을 찾을수 있음. 오동작할것으로 보임.
+        if (span.getServiceType() == ServiceType.ARCUS || span.getServiceType() == ServiceType.MEMCACHED) {
+            index = Collections.binarySearch(list, ARCUS_COMMAND);
+        }
 
-		if (span.getServiceType() == ServiceType.ARCUS || span.getServiceType() == ServiceType.MEMCACHED) {
-			index = Collections.binarySearch(list, ARCUS_COMMAND);
-		}
+        if (span.getServiceType() == ServiceType.HTTP_CLIENT) {
+            index = Collections.binarySearch(list, HTTP_URL);
+        }
 
-		if (span.getServiceType() == ServiceType.HTTP_CLIENT) {
-			index = Collections.binarySearch(list, HTTP_URL);
-		}
-		
-		if (span.getServiceType() == ServiceType.TOMCAT) {
-			index = Collections.binarySearch(list, HTTP_URL);
-		}
+        if (span.getServiceType() == ServiceType.TOMCAT) {
+            index = Collections.binarySearch(list, HTTP_URL);
+        }
 
-		if (index > -1) {
-			return list.get(index).getValue();
-		}
+        if (index > -1) {
+            return list.get(index).getValue();
+        }
 
-		return null;
-	}
+        return null;
+    }
 }
