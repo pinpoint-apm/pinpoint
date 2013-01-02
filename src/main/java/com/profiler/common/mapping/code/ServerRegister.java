fb89f17f148afea47545f0aebf4117def3ed1fbe
Chisu Yu <puseori9th@gmail.com>
2013-01-02 05:15:48 +0000

    [유치수] [NOBTS] fix BLOC Http acceptor api description
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-commons/trunk@1074 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/profiler/common/mapping/code/ServerRegister.java
+++ src/main/java/com/profiler/common/mapping/code/ServerRegister.java
@@ -12,16 +12,16 @@ public class ServerRegister implements Register {
 
     public static final int TomcatStandardHostValveCode = 5000;
     private static final ClassMapping TomcatStandardHostValve = new ClassMapping(TomcatStandardHostValveCode, "org.apache.catalina.core.StandardHostValve",
-            new MethodMapping("invoke", new String[]{"org.apache.catalina.connector.Request", "org.apache.catalina.connector.Response"}, new String[]{"request", "response"})
+    		new MethodMapping("invoke", new String[] { "org.apache.catalina.connector.Request", "org.apache.catalina.connector.Response" }, new String[] { "request", "response" })
     );
 
     public static final int BlocHttpHandlerCode = 5010;
     private static final ClassMapping BlocHttpHandler = new ClassMapping(BlocHttpHandlerCode, "com.nhncorp.lucy.bloc.handler.HTTPHandler$BlocAdapter",
-            new MethodMapping("invoke", new String[]{"external.org.apache.coyote.Request", "external.org.apache.coyote.Response"}, new String[]{"request", "response"})
+    		new MethodMapping("execute", new String[] { "external.org.apache.coyote.Request", "external.org.apache.coyote.Response" }, new String[] { "request", "response" })
     );
     
     public static final int SpringServletHandlerCode = 5020;
-    private static final ClassMapping frameworkServlet = new ClassMapping(SpringServletHandlerCode, "org.springframework.web.servlet.FrameworkServlet",
+    private static final ClassMapping FrameworkServlet = new ClassMapping(SpringServletHandlerCode, "org.springframework.web.servlet.FrameworkServlet",
 			new MethodMapping("doDelete", new String[] { "javax.servlet.http.HttpServletRequest", "javax.servlet.http.HttpServletResponse" }, new String[]{"request", "response"}),
 			new MethodMapping("doGet", new String[] { "javax.servlet.http.HttpServletRequest", "javax.servlet.http.HttpServletResponse" }, new String[] { "request", "response" }),
 			new MethodMapping("doPost", new String[] { "javax.servlet.http.HttpServletRequest", "javax.servlet.http.HttpServletResponse" }, new String[] { "request", "response" }),
@@ -29,7 +29,7 @@ public class ServerRegister implements Register {
     		);
     
     public static final int HttpServletHandlerCode = 5030;
-    private static final ClassMapping httpServlet = new ClassMapping(HttpServletHandlerCode, "javax.servlet.http.HttpServlet",
+    private static final ClassMapping HttpServlet = new ClassMapping(HttpServletHandlerCode, "javax.servlet.http.HttpServlet",
 			new MethodMapping("doDelete", new String[] { "javax.servlet.http.HttpServletRequest", "javax.servlet.http.HttpServletResponse" }, new String[] { "request", "response" }),
 			new MethodMapping("doGet", new String[] { "javax.servlet.http.HttpServletRequest", "javax.servlet.http.HttpServletResponse" }, new String[] { "request", "response" }),
 			new MethodMapping("doHead", new String[] { "javax.servlet.http.HttpServletRequest", "javax.servlet.http.HttpServletResponse" }, new String[] { "request", "response" }),
@@ -43,7 +43,7 @@ public class ServerRegister implements Register {
     public void register(ApiMappingTable apiMappingTable, int startRange, int endRange) {
         apiMappingTable.put(TomcatStandardHostValve);
         apiMappingTable.put(BlocHttpHandler);
-        apiMappingTable.put(frameworkServlet);
-        apiMappingTable.put(httpServlet);
+        apiMappingTable.put(FrameworkServlet);
+        apiMappingTable.put(HttpServlet);
     }
 }
