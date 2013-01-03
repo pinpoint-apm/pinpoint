138f9fe23df786682003295063a4497374a6597d
Chisu Yu <puseori9th@gmail.com>
2013-01-03 09:30:51 +0000

    [유치수] [NOBTS] change call stack table.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-commons/trunk@1080 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/profiler/common/AnnotationNames.java
+++ src/main/java/com/profiler/common/AnnotationNames.java
@@ -1,9 +1,22 @@
 package com.profiler.common;
 
+/**
+ * 
+ * @author netspider
+ * 
+ */
 public class AnnotationNames {
-
 	public static final String API = "API";
 	public static final String API_ID = "API-ID";
+
 	public static final String EXCEPTION = "Exception";
 
+	public static final String BINDVALUE = "BindValue";
+	public static final String PREPAREDSTATEMENT = "PreparedStatement";
+	public static final String ARGS0 = "args[0]";
+
+	public static final String HTTP_URL = "http.url";
+	public static final String HTTP_PARAM = "http.params";
+
+	public static final String ARCUS_COMMAND = "arcus.command";
 }
