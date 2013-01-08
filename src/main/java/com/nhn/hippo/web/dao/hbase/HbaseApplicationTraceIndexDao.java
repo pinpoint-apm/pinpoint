41c769fe2d2ebf9d223bd0e76c7999c29e867530
Woonduk Kang <wd.kang@navercorp.com>
2013-01-08 01:48:30 +0000

    [강운덕] [LUCYSUS-1744] sql의 meta data를 따기 위한 일부 util및 agent수정 코드 부분 커밋.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-web/trunk@1081 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/nhn/hippo/web/dao/hbase/HbaseApplicationTraceIndexDao.java
+++ src/main/java/com/nhn/hippo/web/dao/hbase/HbaseApplicationTraceIndexDao.java
@@ -23,58 +23,58 @@ import com.profiler.common.util.SpanUtils;
 @Repository
 public class HbaseApplicationTraceIndexDao implements ApplicationTraceIndexDao {
 
-	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	private final byte[] COLFAM_TRACE = HBaseTables.APPLICATION_TRACE_INDEX_CF_TRACE;
+    private final byte[] COLFAM_TRACE = HBaseTables.APPLICATION_TRACE_INDEX_CF_TRACE;
 
-	@Autowired
-	private HbaseOperations2 hbaseOperations2;
+    @Autowired
+    private HbaseOperations2 hbaseOperations2;
 
-	@Autowired
-	@Qualifier("traceIndexMapper")
-	private RowMapper<List<byte[]>> traceIndexMapper;
+    @Autowired
+    @Qualifier("traceIndexMapper")
+    private RowMapper<List<byte[]>> traceIndexMapper;
 
-	private int scanCacheSize = 40;
+    private int scanCacheSize = 200;
 
-	public void setScanCacheSize(int scanCacheSize) {
-		this.scanCacheSize = scanCacheSize;
-	}
+    public void setScanCacheSize(int scanCacheSize) {
+        this.scanCacheSize = scanCacheSize;
+    }
 
-	@Override
-	public List<List<byte[]>> scanTraceIndex(String applicationName, long start, long end) {
-		Scan scan = createScan(applicationName, start, end);
-		return hbaseOperations2.find(HBaseTables.APPLICATION_TRACE_INDEX, scan, traceIndexMapper);
-	}
+    @Override
+    public List<List<byte[]>> scanTraceIndex(String applicationName, long start, long end) {
+        Scan scan = createScan(applicationName, start, end);
+        return hbaseOperations2.find(HBaseTables.APPLICATION_TRACE_INDEX, scan, traceIndexMapper);
+    }
 
-	@Override
-	public List<List<List<byte[]>>> multiScanTraceIndex(String[] applicationNames, long start, long end) {
-		final List<Scan> multiScan = new ArrayList<Scan>(applicationNames.length);
-		for (String agent : applicationNames) {
-			Scan scan = createScan(agent, start, end);
-			multiScan.add(scan);
-		}
-		return hbaseOperations2.find(HBaseTables.APPLICATION_TRACE_INDEX, multiScan, traceIndexMapper);
-	}
+    @Override
+    public List<List<List<byte[]>>> multiScanTraceIndex(String[] applicationNames, long start, long end) {
+        final List<Scan> multiScan = new ArrayList<Scan>(applicationNames.length);
+        for (String agent : applicationNames) {
+            Scan scan = createScan(agent, start, end);
+            multiScan.add(scan);
+        }
+        return hbaseOperations2.find(HBaseTables.APPLICATION_TRACE_INDEX, multiScan, traceIndexMapper);
+    }
 
-	private Scan createScan(String agent, long start, long end) {
-		Scan scan = new Scan();
-		// cache size를 지정해야 되는거 같음.??
-		scan.setCaching(this.scanCacheSize);
+    private Scan createScan(String agent, long start, long end) {
+        Scan scan = new Scan();
+        // cache size를 지정해야 되는거 같음.??
+        scan.setCaching(this.scanCacheSize);
 
-		byte[] bAgent = Bytes.toBytes(agent);
-		byte[] traceIndexStartKey = SpanUtils.getTraceIndexRowKey(bAgent, start);
-		scan.setStartRow(traceIndexStartKey);
-		// TODO 추가 filter를 구현하여 scan시 중복된 값을 제가 할수 있음. 단 server에도 Filter 클래스가
-		// 배포되어야 한다.
-		// scan.setFilter(new ValueFilter());
+        byte[] bAgent = Bytes.toBytes(agent);
+        byte[] traceIndexStartKey = SpanUtils.getTraceIndexRowKey(bAgent, start);
+        scan.setStartRow(traceIndexStartKey);
+        // TODO 추가 filter를 구현하여 scan시 중복된 값을 제가 할수 있음. 단 server에도 Filter 클래스가
+        // 배포되어야 한다.
+        // scan.setFilter(new ValueFilter());
 
-		byte[] traceIndexEndKey = SpanUtils.getTraceIndexRowKey(bAgent, end);
-		scan.setStopRow(traceIndexEndKey);
-		scan.addFamily(COLFAM_TRACE);
-		scan.setId("traceIndexScan");
+        byte[] traceIndexEndKey = SpanUtils.getTraceIndexRowKey(bAgent, end);
+        scan.setStopRow(traceIndexEndKey);
+        scan.addFamily(COLFAM_TRACE);
+        scan.setId("traceIndexScan");
 
-		// json으로 변화해서 로그를 찍어서. 최초 변환 속도가 느림.
-		logger.debug("create scan:{}", scan);
-		return scan;
-	}
+        // json으로 변화해서 로그를 찍어서. 최초 변환 속도가 느림.
+        logger.debug("create scan:{}", scan);
+        return scan;
+    }
 }
