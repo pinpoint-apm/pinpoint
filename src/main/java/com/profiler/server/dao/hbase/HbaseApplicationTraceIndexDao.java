4770d29c9b700605fe6f7d43dde724c8d6d5b822
Chisu Yu <puseori9th@gmail.com>
2013-01-10 09:44:52 +0000

    [유치수] [NOBTS] add elapsedTime+resultCode to application index.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-server/trunk@1091 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/profiler/server/dao/hbase/HbaseApplicationTraceIndexDao.java
+++ src/main/java/com/profiler/server/dao/hbase/HbaseApplicationTraceIndexDao.java
@@ -3,11 +3,16 @@ package com.profiler.server.dao.hbase;
 import static com.profiler.common.hbase.HBaseTables.APPLICATION_TRACE_INDEX;
 import static com.profiler.common.hbase.HBaseTables.APPLICATION_TRACE_INDEX_CF_TRACE;
 
+import java.util.List;
+
 import org.apache.hadoop.hbase.client.Put;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.profiler.common.AnnotationNames;
+import com.profiler.common.dto.thrift.Annotation;
 import com.profiler.common.dto.thrift.Span;
 import com.profiler.common.hbase.HbaseOperations2;
+import com.profiler.common.util.BytesUtils;
 import com.profiler.common.util.SpanUtils;
 import com.profiler.server.dao.ApplicationTraceIndexDao;
 
@@ -23,9 +28,24 @@ public class HbaseApplicationTraceIndexDao implements ApplicationTraceIndexDao {
 
 	@Override
 	public void insert(String applicationName, final Span span) {
+		int elapsedTime = span.getElapsed();
+		int resultCode = 0;
+
+		List<Annotation> anns = span.getAnnotations();
+		for (Annotation ann : anns) {
+			if (AnnotationNames.EXCEPTION.equals(ann.getKey())) {
+				resultCode = 1;
+				break;
+			}
+		}
+
+		byte[] value = new byte[8];
+		BytesUtils.writeInt(elapsedTime, value, 0);
+		BytesUtils.writeInt(resultCode, value, 4);
+
 		// TODO 서버가 받은 시간으로 변경해야 될듯.
 		Put put = new Put(SpanUtils.getApplicationTraceIndexRowKey(applicationName, span), span.getStartTime());
-		put.add(APPLICATION_TRACE_INDEX_CF_TRACE, SpanUtils.getTraceId(span), null);
+		put.add(APPLICATION_TRACE_INDEX_CF_TRACE, SpanUtils.getTraceId(span), value);
 
 		hbaseTemplate.put(APPLICATION_TRACE_INDEX, put);
 	}
