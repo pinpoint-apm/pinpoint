35d32010e63bf99afe29d2a0449742653125293e
Woonduk Kang <wd.kang@navercorp.com>
2013-02-01 07:58:11 +0000

    [강운덕] [LUCYSUS-1744] TBaseLocator에서 header를 정적으로 처리하도록 함. queue경합을 줄이기 위해 datasender에서 drain queue를 추가적으로 하도록함.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-commons/trunk@1140 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/profiler/common/util/TBaseLocator.java
+++ src/main/java/com/profiler/common/util/TBaseLocator.java
@@ -1,10 +1,13 @@
 package com.profiler.common.util;
 
+import com.profiler.common.dto.Header;
 import org.apache.thrift.TBase;
 import org.apache.thrift.TException;
 
 public interface TBaseLocator {
     TBase<?, ?> tBaseLookup(short type) throws TException;
 
-    short typeLookup(TBase<?, ?> tbase) throws TException;
+//    short typeLookup(TBase<?, ?> tbase) throws TException;
+
+    Header headerLookup(TBase<?, ?> dto) throws TException;
 }
