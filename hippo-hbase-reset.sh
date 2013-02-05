2acd3db6efd46ff53e36fb3f5dc96fd9ff4ce302
Woonduk Kang <wd.kang@navercorp.com>
2013-02-05 09:21:37 +0000

    [강운덕] [LUCYSUS-1744] TTL 추가. 3일치
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-commons/trunk@1150 84d0f5b1-2673-498c-a247-62c4ff18d310

--- hippo-hbase-reset.sh
+++ hippo-hbase-reset.sh
@@ -23,17 +23,17 @@ drop 'TerminalStatistics'
 drop 'SqlMetaData'
 drop 'ApiMetaData'
 
-create 'AgentInfo', { NAME => 'Info' }
-create 'AgentIdApplicationIndex', { NAME => 'Application' }
-create 'ApplicationIndex', { NAME => 'Agents' }
-create 'RootTraceIndex', { NAME => 'Trace' }
-create 'TraceIndex', { NAME => 'Trace' }
-create 'ApplicationTraceIndex', { NAME => 'Trace' }
-create 'Traces', { NAME => 'Span' }, { NAME => 'Annotation' }, { NAME => 'TerminalSpan' }
-create 'SystemInfo', { NAME => 'JVM' }
-create 'TerminalStatistics', { NAME => 'Cnt' }, { NAME => 'ErrCnt' }
-create 'SqlMetaData', { NAME => 'Sql' }
-create 'ApiMetaData', { NAME => 'Api' }
+create 'AgentInfo', { NAME => 'Info' , TTL => 259200 }
+create 'AgentIdApplicationIndex', { NAME => 'Application' , TTL => 259200 }
+create 'ApplicationIndex', { NAME => 'Agents' , TTL => 259200 }
+create 'RootTraceIndex', { NAME => 'Trace' , TTL => 259200  }
+create 'TraceIndex', { NAME => 'Trace' , TTL => 259200  }
+create 'ApplicationTraceIndex', { NAME => 'Trace' , TTL => 259200  }
+create 'Traces', { NAME => 'Span' , TTL => 259200  }, { NAME => 'Annotation' , TTL => 259200  }, { NAME => 'TerminalSpan' , TTL => 259200  }
+create 'SystemInfo', { NAME => 'JVM' , TTL => 259200  }
+create 'TerminalStatistics', { NAME => 'Cnt' , TTL => 259200  }, { NAME => 'ErrCnt' , TTL => 259200  }
+create 'SqlMetaData', { NAME => 'Sql' , TTL => 259200  }
+create 'ApiMetaData', { NAME => 'Api' , TTL => 259200  }
 
 list
 EOF
