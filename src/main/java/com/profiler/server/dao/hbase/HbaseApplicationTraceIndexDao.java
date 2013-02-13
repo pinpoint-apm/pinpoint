f0b57c16b0901166716c1b702e67986479e242e8
Woonduk Kang <wd.kang@navercorp.com>
2013-02-13 10:20:28 +0000

    [강운덕] [LUCYSUS-1744] AnnotationNames -> AnnotationKey로 변경.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-server/trunk@1192 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/profiler/server/dao/hbase/HbaseApplicationTraceIndexDao.java
+++ src/main/java/com/profiler/server/dao/hbase/HbaseApplicationTraceIndexDao.java
@@ -3,14 +3,10 @@ package com.profiler.server.dao.hbase;
 import static com.profiler.common.hbase.HBaseTables.APPLICATION_TRACE_INDEX;
 import static com.profiler.common.hbase.HBaseTables.APPLICATION_TRACE_INDEX_CF_TRACE;
 
-import java.util.List;
-
 import com.profiler.server.util.AcceptedTime;
 import org.apache.hadoop.hbase.client.Put;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.profiler.common.AnnotationNames;
-import com.profiler.common.dto.thrift.Annotation;
 import com.profiler.common.dto.thrift.Span;
 import com.profiler.common.hbase.HbaseOperations2;
 import com.profiler.common.util.BytesUtils;
