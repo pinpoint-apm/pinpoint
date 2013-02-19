1046297c9b1d5fc6d8ac2e81c037fb5a8d19a250
Woonduk Kang <wd.kang@navercorp.com>
2013-02-19 10:11:16 +0000

    [강운덕] [LUCYSUS-1744] trace객체의 데이터 레코딩시 중복 데이터의 통합을 위해서 api를 개별로 분리함.    통계 테이블의 데이터가 실시간으로 갱신되지 않는 문제가 있어 수정함.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-web/trunk@1231 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/nhn/hippo/web/dao/hbase/HbaseTerminalStatisticsDao.java
+++ src/main/java/com/nhn/hippo/web/dao/hbase/HbaseTerminalStatisticsDao.java
@@ -1,9 +1,13 @@
 package com.nhn.hippo.web.dao.hbase;
 
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.hadoop.hbase.client.Scan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.hadoop.hbase.RowMapper;
@@ -24,6 +28,7 @@ import com.profiler.common.util.TimeSlot;
 @Repository
 public class HbaseTerminalStatisticsDao implements TerminalStatisticsDao {
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 	private int scanCacheSize = 40;
 
 	@Autowired
@@ -36,12 +41,19 @@ public class HbaseTerminalStatisticsDao implements TerminalStatisticsDao {
 	@Override
 	public List<Map<String, TerminalStatistics>> selectTerminal(String applicationName, long from, long to) {
 		Scan scan = createScan(applicationName, from, to);
-		return hbaseOperations2.find(HBaseTables.TERMINAL_STATISTICS, scan, terminalRequestCountMapper);
+        return hbaseOperations2.find(HBaseTables.TERMINAL_STATISTICS, scan, terminalRequestCountMapper);
 	}
 
 	private Scan createScan(String applicationName, long from, long to) {
-		byte[] startKey = TerminalSpanUtils.makeRowKey(applicationName, TimeSlot.getStatisticsRowSlot(from));
-		byte[] endKey = TerminalSpanUtils.makeRowKey(applicationName, TimeSlot.getStatisticsRowSlot(to));
+        long startTime = TimeSlot.getStatisticsRowSlot(from);
+        // hbase의 scanner를 사용하여 검색시 endTime은 검색 대상에 포함되지 않기 때문에, +1을 해줘야 된다.
+        long endTime = TimeSlot.getStatisticsRowSlot(to) + 1;
+        if (logger.isDebugEnabled()) {
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss,SSS");
+            logger.debug("scan startTime:{} endTime:{}", simpleDateFormat.format(new Date(startTime)), simpleDateFormat.format(new Date(endTime)));
+        }
+        byte[] startKey = TerminalSpanUtils.makeRowKey(applicationName, startTime);
+		byte[] endKey = TerminalSpanUtils.makeRowKey(applicationName, endTime);
 
 		Scan scan = new Scan();
 		scan.setCaching(this.scanCacheSize);
