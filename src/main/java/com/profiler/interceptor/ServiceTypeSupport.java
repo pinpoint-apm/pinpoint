1808f92a68fc1e04cf1ef0d56a7d2e711bf2b169
Woonduk Kang <wd.kang@navercorp.com>
2013-02-20 10:55:37 +0000

    [강운덕] [LUCYSUS-1744] span, subspan의 데이터 정형화를 위해 일부 수정분. commit
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-tomcat-profiler/trunk@1237 84d0f5b1-2673-498c-a247-62c4ff18d310

---
+++ src/main/java/com/profiler/interceptor/ServiceTypeSupport.java
@@ -0,0 +1,12 @@
+package com.profiler.interceptor;
+
+import com.profiler.common.ServiceType;
+
+/**
+ *
+ */
+public interface ServiceTypeSupport {
+
+    void setServiceType(ServiceType serviceType);
+
+}
