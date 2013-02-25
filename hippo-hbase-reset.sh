93f96d100032d20683bd3bbd69da0dfb150965bd
Chisu Yu <puseori9th@gmail.com>
2013-02-25 05:51:22 +0000

    [유치수] [NOBTS] metadata는 TTL을 지정하면 안됨.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-commons/trunk@1251 84d0f5b1-2673-498c-a247-62c4ff18d310

--- hippo-hbase-reset.sh
+++ hippo-hbase-reset.sh
@@ -2,40 +2,40 @@ exec ./hbase shell <<EOF
 disable 'AgentInfo'
 disable 'AgentIdApplicationIndex'
 disable 'ApplicationIndex'
+disable 'SqlMetaData'
+disable 'ApiMetaData'
 disable 'RootTraceIndex'
 disable 'TraceIndex'
 disable 'ApplicationTraceIndex'
 disable 'Traces'
 disable 'SystemInfo'
 disable 'TerminalStatistics'
-disable 'SqlMetaData'
-disable 'ApiMetaData'
 disable 'BusinessTransactionStatistics'
 
 drop 'AgentInfo'
 drop 'AgentIdApplicationIndex'
 drop 'ApplicationIndex'
+drop 'SqlMetaData'
+drop 'ApiMetaData'
 drop 'RootTraceIndex'
 drop 'TraceIndex'
 drop 'ApplicationTraceIndex'
 drop 'Traces'
 drop 'SystemInfo'
 drop 'TerminalStatistics'
-drop 'SqlMetaData'
-drop 'ApiMetaData'
 drop 'BusinessTransactionStatistics'
 
 create 'AgentInfo', { NAME => 'Info' }
 create 'AgentIdApplicationIndex', { NAME => 'Application' }
 create 'ApplicationIndex', { NAME => 'Agents' }
+create 'SqlMetaData', { NAME => 'Sql' }
+create 'ApiMetaData', { NAME => 'Api' }
 create 'RootTraceIndex', { NAME => 'Trace' , TTL => 259200  }
 create 'TraceIndex', { NAME => 'Trace' , TTL => 259200  }
 create 'ApplicationTraceIndex', { NAME => 'Trace' , TTL => 259200  }
 create 'Traces', { NAME => 'Span' , TTL => 259200  }, { NAME => 'Annotation' , TTL => 259200  }, { NAME => 'TerminalSpan' , TTL => 259200  }
 create 'SystemInfo', { NAME => 'JVM' , TTL => 259200  }
 create 'TerminalStatistics', { NAME => 'Counter' , TTL => 259200  }, { NAME => 'ErrorCount' , TTL => 259200  }
-create 'SqlMetaData', { NAME => 'Sql' , TTL => 259200  }
-create 'ApiMetaData', { NAME => 'Api' , TTL => 259200  }
 create 'BusinessTransactionStatistics', { NAME => 'Normal' , TTL => 259200  }, { NAME => 'Slow' , TTL => 259200  }, { NAME => 'Error' , TTL => 259200  }
 
 list
