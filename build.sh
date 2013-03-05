b588918c72847d560e67a5222cc387c714674ccc
Chisu Yu <puseori9th@gmail.com>
2013-03-05 01:57:02 +0000

    [유치수] [NOBTS] fix build script
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-commons/trunk@1282 84d0f5b1-2673-498c-a247-62c4ff18d310

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 # common lib
 ./makethrift.sh
 mvn clean eclipse:eclipse package install -Dmaven.test.skip
-cp ./target/hippo-commons-0.0.1.jar ../hippo-testbed/hippo-server
+cp ./target/hippo-commons-0.0.2.jar ../hippo-testbed/hippo-server
