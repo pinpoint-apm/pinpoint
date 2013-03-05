7465d59af14f31069b1b3a8c421bf8b0b4513c91
Chisu Yu <puseori9th@gmail.com>
2013-03-05 01:57:02 +0000

    [유치수] [NOBTS] fix build script
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-tomcat-profiler/trunk@1282 84d0f5b1-2673-498c-a247-62c4ff18d310

--- build.sh
+++ build.sh
@@ -10,6 +10,6 @@ cp ./HippoAgent.jar ../../../hippo-testbed/agent
 
 cd ../../../hippo-tomcat-profiler
 cp ./target/dependency/javassist-3.16.1-GA.jar ../hippo-testbed/agent
-cp ./target/dependency/hippo-commons-0.0.1.jar ../hippo-testbed/agent
+cp ./target/dependency/hippo-commons-0.0.2.jar ../hippo-testbed/agent
 cp ./lib/libthrift-0.8.0.wolog.jar ../hippo-testbed/agent
 cp ./runscript/* ../hippo-testbed/agent
