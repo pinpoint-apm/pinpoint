ef4ecab52b7383f7bee780e6a7ab1f4e6341565b
Chisu Yu <puseori9th@gmail.com>
2013-03-11 01:53:38 +0000

    [유치수] [NOBTS] server 시작, 중지 스크립트 수정.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-server/trunk@1307 84d0f5b1-2673-498c-a247-62c4ff18d310

--- runscript/start.sh
+++ runscript/start.sh
@@ -1,1 +1,3 @@
-java -Xms1g -Xmx1g -classpath `find . -name '*.jar' | tr "\n" :` com.profiler.server.Server
+java -Xms1g -Xmx1g -classpath `find . -name '*.jar' | tr "\n" :` com.profiler.server.Server >> server.log &
+sleep 0.3
+tail -f server.log
---
+++ runscript/stop.sh
@@ -0,0 +1,1 @@
+kill -9 `ps -ef | grep hippo-server | grep -v grep | awk '{ print $2 }'`
