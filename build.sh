746573a85a1b93a58ab4eadacb8455d4f1a5ddae
Chisu Yu <puseori9th@gmail.com>
2013-03-28 05:49:39 +0000

    [유치수] [NOBTS] 빌드 스크립트 변경.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-tomcat-profiler/trunk@1365 84d0f5b1-2673-498c-a247-62c4ff18d310

--- build.sh
+++ build.sh
@@ -1,14 +1,10 @@
 # agent
 mvn clean install eclipse:eclipse package dependency:copy-dependencies -Dmaven.test.skip
 
-cd ./target/classes
-jar cvfM HippoAgent.jar ./src/META-INF/MANIFEST.MF ./*
+rm -fr ../hippo-testbed/agent
+mkdir ../hippo-testbed/agent
+cp ./target/hippo-tomcat-profiler-0.0.2.jar ../hippo-testbed/agent
 
-rm -fr ../../../hippo-testbed/agent
-mkdir ../../../hippo-testbed/agent
-cp ./HippoAgent.jar ../../../hippo-testbed/agent
-
-cd ../../../hippo-tomcat-profiler
 cp ./target/dependency/javassist-3.16.1-GA.jar ../hippo-testbed/agent
 cp ./target/dependency/hippo-commons-0.0.2.jar ../hippo-testbed/agent
 cp ./lib/libthrift-0.8.0.wolog.jar ../hippo-testbed/agent
