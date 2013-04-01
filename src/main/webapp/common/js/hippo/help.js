13a0db8baacbf443d725ce5d9229464c5976ab1b
Chisu Yu <puseori9th@gmail.com>
2013-04-01 02:08:49 +0000

    [유치수] [NOBTS] 도움말 아이콘 추가.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-web/trunk@1372 84d0f5b1-2673-498c-a247-62c4ff18d310

---
+++ src/main/webapp/common/js/hippo/help.js
@@ -0,0 +1,17 @@
+var helptext = {
+		// Focus on passing transactions
+		1001 : "선택한 구간이나 리소스를 사용하는 요청으로 서버 지도를 다시 그립니다.",
+		// Response scatter chart
+		1002 : "응답시간에 대한 scatter chart를 조회합니다.",
+		// Show Requests
+		1003 : "선택한 구간이나 리소스를 통과하는 요청들의 목록을 조회합니다.",
+		
+		// scatter chart란?
+		2001 : "",
+		// request list
+		2002 : ""
+}
+
+function help(id) {
+	alert(helptext[id]);
+}
