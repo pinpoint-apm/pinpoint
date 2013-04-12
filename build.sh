f58967a4723dcf561e23472fc40fe9b94265b12b
Chisu Yu <puseori9th@gmail.com>
2013-04-12 06:38:42 +0000

    [유치수] [PROBE-168] thrift 버전 0.9.0으로 변경.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-commons/trunk@1446 84d0f5b1-2673-498c-a247-62c4ff18d310

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
+# common은 install만 하면 된다.
+# bootstrap이 빌드될 때 common의 class를 bootstrap.jar에 포함시킴.
+
 # common lib
 ./makethrift.sh
 mvn clean eclipse:eclipse package install -Dmaven.test.skip
-cp ./target/hippo-commons-0.0.2.jar ../hippo-testbed/hippo-server
+#cp ./target/hippo-commons-0.0.2.jar ../hippo-testbed/hippo-server
--- makethrift.sh
+++ makethrift.sh
@@ -1,1 +1,1 @@
-mvn clean eclipse:eclipse -P withThrift -Dthrift.executable.property=/Users/netspider/DEV-TOOLS/thrift-0.8.0/bin/thrift
+mvn clean eclipse:eclipse -P withThrift -Dthrift.executable.property=/Users/netspider/DEV-TOOLS/thrift-0.9.0/bin/thrift
