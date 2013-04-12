6b62af8d4cf774eb0d52fe59644103a080d44521
Chisu Yu <puseori9th@gmail.com>
2013-04-12 06:38:42 +0000

    [유치수] [PROBE-168] thrift 버전 0.9.0으로 변경.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-server/trunk@1446 84d0f5b1-2673-498c-a247-62c4ff18d310

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #server
 mvn clean eclipse:eclipse install package dependency:copy-dependencies -Dmaven.test.skip
+
 rm -fr ../hippo-testbed/hippo-server
 mkdir -p ../hippo-testbed/hippo-server/lib
+
 cp ./target/hippo-server-0.0.2.jar ../hippo-testbed/hippo-server
 cp ./target/dependency/*.jar ../hippo-testbed/hippo-server/lib
 cp ./runscript/* ../hippo-testbed/hippo-server
