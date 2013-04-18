97e5d557d32d4427533950dadc26788f5046406b
Woonduk Kang <wd.kang@navercorp.com>
2013-04-18 06:27:21 +0000

    [강운덕] [LUCYSUS-1744] 일부 테스트 케이스 이동.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-profiler-bootstrap/trunk@1499 84d0f5b1-2673-498c-a247-62c4ff18d310

---
+++ src/test/java/com/profiler/util/Test.java
@@ -0,0 +1,30 @@
+package com.profiler.util;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+
+/**
+ *
+ */
+public class Test {
+    public static void main(String[] args) throws UnknownHostException, SocketException {
+        System.out.println("Canonical:" + InetAddress.getLocalHost().getCanonicalHostName());
+        System.out.println("normal:" + InetAddress.getLocalHost().getHostName());
+
+        System.out.println("NetworkInterface");
+        Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+        while(networkInterfaces.hasMoreElements()) {
+            NetworkInterface networkInterface = networkInterfaces.nextElement();
+            System.out.println("Nic:" + networkInterface);
+            Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
+            while(inetAddresses.hasMoreElements()) {
+                InetAddress inetAddress = inetAddresses.nextElement();
+                System.out.println(inetAddress.getCanonicalHostName());
+                System.out.println(inetAddress.getHostName());
+            }
+        }
+    }
+}
