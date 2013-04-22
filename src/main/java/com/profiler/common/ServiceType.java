0470fea12c4dcbb5a721ba79ff45f260a6aae577
Chisu Yu <puseori9th@gmail.com>
2013-04-22 07:19:02 +0000

    [유치수] [NOBTS] UNKNOWN_GROUP 추가.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-commons/trunk@1522 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/profiler/common/ServiceType.java
+++ src/main/java/com/profiler/common/ServiceType.java
@@ -10,6 +10,7 @@ public enum ServiceType {
     UNKNOWN((short) 0, "UNKNOWN", false, true, false, Histogram.NORMAL),
     UNKNOWN_CLOUD((short) 1, "UNKNOWN_CLOUD", false, true, false, Histogram.NORMAL),
     CLIENT((short) 2, "CLIENT", false, false, false, Histogram.NORMAL),
+    UNKNOWN_GROUP((short) 3, "UNKNOWN_GROUP", false, true, false, Histogram.NORMAL),
 
     // WAS류 1000번 부터 시작
     TOMCAT((short) 1010, "TOMCAT", false, true, false, Histogram.NORMAL),
