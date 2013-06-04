22bda2aba98fbb01064b27c032bf3a8298846438
Chisu Yu <puseori9th@gmail.com>
2013-06-04 06:20:55 +0000

    [유치수] [NOBTS] 빌드 스크립트 변경.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-profiler-bootstrap/trunk@1758 84d0f5b1-2673-498c-a247-62c4ff18d310

--- build.sh
+++ build.sh
@@ -1,25 +1,25 @@
 # profiler
 pushd .
-cd ../hippo-tomcat-profiler
+cd ../pinpoint-tomcat-profiler
 mvn clean install eclipse:eclipse package dependency:copy-dependencies -Dmaven.test.skip -P withThrift -Dthrift.executable.property=/Users/netspider/DEV-TOOLS/thrift-0.9.0/bin/thrift
 popd
 
 # bootstrap
 mvn clean install eclipse:eclipse package dependency:copy-dependencies -Dmaven.test.skip
 
-rm -fr ../hippo-testbed/agent
-mkdir -p ../hippo-testbed/agent/lib
+rm -fr ../pinpoint-testbed/agent
+mkdir -p ../pinpoint-testbed/agent/lib
 
-cp ./target/hippo-profiler-bootstrap-0.0.2-jar-with-dependencies.jar ../hippo-testbed/agent/hippo-profiler-bootstrap-0.0.2.jar
+cp ./target/pinpoint-profiler-bootstrap-0.0.2-jar-with-dependencies.jar ../pinpoint-testbed/agent/pinpoint-profiler-bootstrap-0.0.2.jar
 
-cp ../hippo-tomcat-profiler/target/hippo-tomcat-profiler-0.0.2.jar ../hippo-testbed/agent/lib
-cp ../hippo-tomcat-profiler/target/dependency/javassist-3.16.1-GA.jar ../hippo-testbed/agent/lib
-cp ../hippo-tomcat-profiler/target/dependency/libthrift-0.9.0.jar ../hippo-testbed/agent/lib
+cp ../pinpoint-tomcat-profiler/target/pinpoint-profiler-0.0.3-SNAPSHOT.jar ../pinpoint-testbed/agent/lib
+cp ../pinpoint-tomcat-profiler/target/dependency/javassist-3.16.1-GA.jar ../pinpoint-testbed/agent/lib
+cp ../pinpoint-tomcat-profiler/target/dependency/libthrift-0.9.0.jar ../pinpoint-testbed/agent/lib
 
-cp ../hippo-tomcat-profiler/target/dependency/log4j-1.2.16.jar  ../hippo-testbed/agent/lib
-cp ../hippo-tomcat-profiler/target/dependency/slf4j-log4j12-1.6.6.jar  ../hippo-testbed/agent/lib
-cp ../hippo-tomcat-profiler/target/dependency/slf4j-api-1.6.6.jar  ../hippo-testbed/agent/lib
+cp ../pinpoint-tomcat-profiler/target/dependency/log4j-1.2.16.jar  ../pinpoint-testbed/agent/lib
+cp ../pinpoint-tomcat-profiler/target/dependency/slf4j-log4j12-1.6.6.jar  ../pinpoint-testbed/agent/lib
+cp ../pinpoint-tomcat-profiler/target/dependency/slf4j-api-1.6.6.jar  ../pinpoint-testbed/agent/lib
 
-cp ../hippo-tomcat-profiler/runscript/help.txt ../hippo-testbed/agent
-cp ../hippo-tomcat-profiler/runscript/hippo.config ../hippo-testbed/agent
-cp ../hippo-tomcat-profiler/runscript/log4j.xml ../hippo-testbed/agent/lib
+cp ../pinpoint-tomcat-profiler/runscript/help.txt ../pinpoint-testbed/agent
+cp ../pinpoint-tomcat-profiler/runscript/pinpoint.config ../pinpoint-testbed/agent
+cp ../pinpoint-tomcat-profiler/runscript/log4j.xml ../pinpoint-testbed/agent/lib
