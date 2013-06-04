64f3fbf6c03f42262ed58c283a828538dd02c6ff
Woonduk Kang <wd.kang@navercorp.com>
2013-06-04 06:25:08 +0000

    [강운덕] [LUCYSUS-1744] profiler의 기본 패키지를 pinpoint/profiler로 변경. testcase 도 변경함.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-profiler-bootstrap/trunk@1762 84d0f5b1-2673-498c-a247-62c4ff18d310

---
+++ src/test/java/com/nhn/pinpoint/profiler/config/ProfilerConfigTest.java
@@ -0,0 +1,45 @@
+package com.nhn.pinpoint.profiler.config;
+
+import java.io.IOException;
+
+import com.nhn.pinpoint.profiler.config.ProfilerConfig;
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class ProfilerConfigTest {
+
+	@Test
+	public void testIsProfilableClassWithNoConfiguration() throws IOException {
+		ProfilerConfig profilerConfig = new ProfilerConfig();
+		profilerConfig.setProfilableClass("com.nhn.pinpoint.testweb.controller.*,com.nhn.pinpoint.testweb.MyClass");
+
+		Assert.assertFalse(profilerConfig.isProfilableClass("com/nhn/pinpoint/testweb/controllers/MyController"));
+		Assert.assertFalse(profilerConfig.isProfilableClass("net/spider/king/wang/Jjang"));
+		Assert.assertFalse(profilerConfig.isProfilableClass("com/nhn/pinpoint/testweb2/controller/MyController"));
+		Assert.assertFalse(profilerConfig.isProfilableClass("com/nhn/pinpoint/testweb2/MyClass"));
+	}
+	
+	/**
+	 * <pre>
+	 * configuration is 
+	 * profile.package.include=com.nhn.pinpoint.testweb.controller.*,com.nhn.pinpoint.testweb.MyClass
+	 * </pre>
+	 * 
+	 * @throws IOException
+	 */
+	@Test
+	public void testIsProfilableClass() throws IOException {
+
+		ProfilerConfig profilerConfig = new ProfilerConfig();
+        profilerConfig.setProfilableClass("com.nhn.pinpoint.testweb.controller.*,com.nhn.pinpoint.testweb.MyClass");
+
+		Assert.assertTrue(profilerConfig.isProfilableClass("com/nhn/pinpoint/testweb/MyClass"));
+		Assert.assertTrue(profilerConfig.isProfilableClass("com/nhn/pinpoint/testweb/controller/MyController"));
+		Assert.assertTrue(profilerConfig.isProfilableClass("com/nhn/pinpoint/testweb/controller/customcontroller/MyCustomController"));
+
+		Assert.assertFalse(profilerConfig.isProfilableClass("com/nhn/pinpoint/testweb/MyUnknownClass"));
+		Assert.assertFalse(profilerConfig.isProfilableClass("com/nhn/pinpoint/testweb/controller2/MyController"));
+	}
+
+}
