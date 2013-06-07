5d826a853183e84825461fd866999a25e20a50ab
Chisu Yu <puseori9th@gmail.com>
2013-06-07 08:00:03 +0000

    [유치수] [PINPOINT-212] obfuscate script  추가.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-profiler-bootstrap/trunk@1820 84d0f5b1-2673-498c-a247-62c4ff18d310

---
+++ obfuscate.sh
@@ -0,0 +1,23 @@
+HOME="/Users/netspider/Documents/workspace_pinpoint"
+VERSION="0.0.3-SNAPSHOT"
+
+echo "Remove previous"
+rm -fr $HOME/pinpoint-testbed/agent-obfuscated
+rm -fr $HOME/pinpoint-testbed/agent-org
+mkdir $HOME/pinpoint-testbed/agent-obfuscated
+mkdir $HOME/pinpoint-testbed/agent-org
+
+echo "Backup original"
+cp $HOME/pinpoint-testbed/agent/pinpoint-bootstrap-$VERSION.jar $HOME/pinpoint-testbed/agent-org/
+cp $HOME/pinpoint-testbed/agent/lib/pinpoint-profiler-$VERSION.jar $HOME/pinpoint-testbed/agent-org/
+
+echo "Obfuscating"
+proguard.sh @proguard.conf
+
+echo "Replace original"
+cp $HOME/pinpoint-testbed/agent-obfuscated/pinpoint-bootstrap-$VERSION.jar $HOME/pinpoint-testbed/agent/
+cp $HOME/pinpoint-testbed/agent-obfuscated/pinpoint-profiler-$VERSION.jar $HOME/pinpoint-testbed/agent/lib
+
+echo "Result is..."
+ls -al $HOME/pinpoint-testbed/agent-obfuscated
+ls -al $HOME/pinpoint-testbed/agent-org
