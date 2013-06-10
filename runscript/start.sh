aa67b75c80eb56a0e5f16941647d492650e9b90b
Chisu Yu <puseori9th@gmail.com>
2013-06-10 02:33:49 +0000

    [유치수] [NOBTS] 시작 스크립트 수정.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-server/trunk@1825 84d0f5b1-2673-498c-a247-62c4ff18d310

--- runscript/start.sh
+++ runscript/start.sh
@@ -1,3 +1,3 @@
-java -Xms1g -Xmx1g -classpath `find . -name '*.jar' | tr "\n" :` Server >> collector.log &
+java -Xms1g -Xmx1g -classpath `find . -name '*.jar' | tr "\n" :` com.nhn.pinpoint.collector.Server >> collector.log &
 sleep 0.3
 tail -f collector.log
