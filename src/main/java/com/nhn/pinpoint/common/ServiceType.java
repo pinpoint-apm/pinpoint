5e0a661f1dfd9323f63839b84d433076441e104f
Hoonmin Kim <harebox@gmail.com>
2013-06-10 08:04:22 +0000

    [김훈민] [NOBTS] Arcus interceptor : stacktrace에서 destination id 출력되도록 수정
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-commons/trunk@1839 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/nhn/pinpoint/common/ServiceType.java
+++ src/main/java/com/nhn/pinpoint/common/ServiceType.java
@@ -44,8 +44,8 @@ public enum ServiceType {
     SPRING_MVC((short) 5051, "SPRING", false, false, false, Histogram.NORMAL),
 
     // memory cache  8000
-    MEMCACHED((short) 8050, "MEMCACHED", true, true, true, Histogram.FAST),
-    MEMCACHED_FUTURE_GET((short) 8051, "MEMCACHED", true, false, true, Histogram.FAST),
+    MEMCACHED((short) 8050, "MEMCACHED", true, true, false, Histogram.FAST),
+    MEMCACHED_FUTURE_GET((short) 8051, "MEMCACHED", true, false, false, Histogram.FAST),
     ARCUS((short) 8100, "ARCUS", true, true, false, Histogram.FAST),
     ARCUS_FUTURE_GET((short) 8101, "ARCUS", true, false, true, Histogram.FAST),
 
