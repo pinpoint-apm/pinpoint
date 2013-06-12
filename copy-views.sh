f35ec2def88b2d328765e3d4c65250939c39ed46
Chisu Yu <puseori9th@gmail.com>
2013-06-12 23:28:42 +0000

    [유치수] [NOBTS] 버전 0.0.4-SNAPSHOT으로 변경.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-web/trunk@1874 84d0f5b1-2673-498c-a247-62c4ff18d310

--- copy-views.sh
+++ copy-views.sh
@@ -1,4 +1,4 @@
-VERSION="0.0.3-SNAPSHOT"
+VERSION="0.0.4-SNAPSHOT"
 cp ./src/main/webapp/WEB-INF/views/*.jsp ./target/pinpoint-web-$VERSION/WEB-INF/views
 cp ./src/main/webapp/WEB-INF/web.xml ./target/pinpoint-web-$VERSION/WEB-INF
 cp ./src/main/webapp/*.html ./target/pinpoint-web-$VERSION
