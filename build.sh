adb812e6f6014dd60017f1bc07503044dc176606
Chisu Yu <puseori9th@gmail.com>
2013-06-14 02:45:03 +0000

    [유치수] [NOBTS] 버전 0.0.4-SNAPSHOT으로 변경.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-server/trunk@1883 84d0f5b1-2673-498c-a247-62c4ff18d310

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
+VERSION="0.0.4-SNAPSHOT"
+
 #server
 mvn clean eclipse:eclipse install package dependency:copy-dependencies -Dmaven.test.skip
 
 rm -fr ../pinpoint-testbed/pinpoint-collector
 mkdir -p ../pinpoint-testbed/pinpoint-collector/lib
 
-cp ./target/pinpoint-collector-0.0.3-SNAPSHOT.jar ../pinpoint-testbed/pinpoint-collector
+cp ./target/pinpoint-collector-$VERSION.jar ../pinpoint-testbed/pinpoint-collector
 cp ./target/dependency/*.jar ../pinpoint-testbed/pinpoint-collector/lib
 cp ./runscript/* ../pinpoint-testbed/pinpoint-collector
