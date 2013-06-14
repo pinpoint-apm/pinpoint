91a68071d0fbda847c61b4a643ca43edfb6142dc
Chisu Yu <puseori9th@gmail.com>
2013-06-14 02:45:36 +0000

    [유치수] [NOBTS] 버전 0.0.4-SNAPSHOT으로 변경.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-commons/trunk@1884 84d0f5b1-2673-498c-a247-62c4ff18d310

--- build.sh
+++ build.sh
@@ -4,4 +4,3 @@
 # common lib
 ./makethrift.sh
 mvn clean eclipse:eclipse package dependency:copy-dependencies install -Dmaven.test.skip
-#cp ./target/pinpoint-commons-0.0.2.jar ../pinpoint-testbed/pinpoint-server
