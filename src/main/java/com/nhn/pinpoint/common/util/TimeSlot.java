70c596429d175c613f06089f364a44d5e22d7fe0
Chisu Yu <puseori9th@gmail.com>
2013-07-26 07:43:54 +0000

    [유치수] [NOBTS] timeslot을 다시 1분으로 변경.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-commons/trunk@2027 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/nhn/pinpoint/common/util/TimeSlot.java
+++ src/main/java/com/nhn/pinpoint/common/util/TimeSlot.java
@@ -6,7 +6,7 @@ package com.nhn.pinpoint.common.util;
  * 
  */
 public class TimeSlot {
-	private static final int RESOLUTION = 5 * 60000; // 5min
+	private static final int RESOLUTION =  60000; // 1min
 
 	public static long getStatisticsRowSlot(long time) {
         // 과거 time을 기준으로 얻어오나, 모두 동일하게 과거 시간의 슬롯을 얻어오게 되므로 + RESOLUTION을 하지 않아도 된다.
