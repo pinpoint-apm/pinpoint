39820b1573f213ed488d7c2e9ad093ef6a824c51
Woonduk Kang <wd.kang@navercorp.com>
2013-08-01 02:03:23 +0000

    [강운덕] [LUCYSUS-1744] lock free 자료 구조로 변경.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-server/trunk@2058 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/nhn/pinpoint/collector/util/FixedPool.java
+++ src/main/java/com/nhn/pinpoint/collector/util/FixedPool.java
@@ -1,13 +1,14 @@
 package com.nhn.pinpoint.collector.util;
 
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  *
  */
 public class FixedPool<T> {
-
-    private final LinkedBlockingQueue<T> queue = new LinkedBlockingQueue<T>();
+    // 구지 blocking 할 필요가 없음. queue안에 충분한 양이 무조껀 있어야 됨. 없으면 뭔가 릭임.
+    private final Queue<T> queue = new ConcurrentLinkedQueue<T>();
 
     private final FixedPoolFactory<T> factory;
 
