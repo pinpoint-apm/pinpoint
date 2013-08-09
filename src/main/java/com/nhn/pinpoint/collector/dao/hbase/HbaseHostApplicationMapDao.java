12d5b3b619ae0106d945a97e9476f856540556a9
Chisu Yu <puseori9th@gmail.com>
2013-08-09 01:41:56 +0000

    [유치수] [NOBTS] host application map을 매번 저장하지 않도록 함.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-server/trunk@2116 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/nhn/pinpoint/collector/dao/hbase/HbaseHostApplicationMapDao.java
+++ src/main/java/com/nhn/pinpoint/collector/dao/hbase/HbaseHostApplicationMapDao.java
@@ -3,6 +3,9 @@ package com.nhn.pinpoint.collector.dao.hbase;
 import static com.nhn.pinpoint.common.hbase.HBaseTables.HOST_APPLICATION_MAP;
 import static com.nhn.pinpoint.common.hbase.HBaseTables.HOST_APPLICATION_MAP_CF_MAP;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
 import com.nhn.pinpoint.collector.dao.HostApplicationMapDao;
 import com.nhn.pinpoint.collector.util.AcceptedTimeService;
 import org.apache.hadoop.hbase.util.Bytes;
@@ -30,8 +33,19 @@ public class HbaseHostApplicationMapDao implements HostApplicationMapDao {
 	@Autowired
 	private AcceptedTimeService acceptedTimeService;
 
+	private final ConcurrentMap<String, Object> cache = new ConcurrentHashMap<String, Object>(1024);
+	private long lastUpdated = System.currentTimeMillis();
+
 	@Override
 	public void insert(String host, String applicationName, short serviceType) {
+		String cacheKey = host + applicationName + serviceType;
+
+		// 매 번 넣을 필요 없음.
+		if (cache.containsKey(cacheKey)) {
+			logger.debug("Skip insert host-application map. host={}, applicationName={}, serviceType={}", new Object[] { host, applicationName, serviceType });
+			return;
+		}
+
 		logger.debug("Insert host-application map. host={}, applicationName={}, serviceType={}", new Object[] { host, applicationName, serviceType });
 
 		byte[] rowKey = Bytes.toBytes(TimeUtils.reverseCurrentTimeMillis(TimeSlot.getStatisticsRowSlot(acceptedTimeService.getAcceptedTime())));
@@ -43,5 +57,12 @@ public class HbaseHostApplicationMapDao implements HostApplicationMapDao {
 		byte[] value = BytesUtils.concat(applicationNameBytes, offsetBytes, serviceTypeBytes);
 
 		hbaseTemplate.put(HOST_APPLICATION_MAP, rowKey, HOST_APPLICATION_MAP_CF_MAP, columnName, value);
+
+		if (System.currentTimeMillis() - lastUpdated > 5000) {
+			cache.clear();
+		} else {
+			cache.put(cacheKey, 1);
+		}
+		lastUpdated = System.currentTimeMillis();
 	}
 }
