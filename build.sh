e3349bcfbb80c9710d4ca339f255a3373528149e
Chisu Yu <puseori9th@gmail.com>
2013-08-13 06:57:24 +0000

    [유치수] [NOBTS] 빌드 스크립트 변경.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-server/trunk@2139 84d0f5b1-2673-498c-a247-62c4ff18d310

--- build.sh
+++ build.sh
@@ -1,11 +1,16 @@
 VERSION="0.0.4-SNAPSHOT"
+DEPLOY_DIR="../pinpoint-testbed/pinpoint-collector"
 
 #server
 mvn clean eclipse:eclipse install package dependency:copy-dependencies -Dmaven.test.skip
 
-rm -fr ../pinpoint-testbed/pinpoint-collector
-mkdir -p ../pinpoint-testbed/pinpoint-collector/lib
+rm -fr $DEPLOY_DIR
+mkdir -p $DEPLOY_DIR/lib
 
-cp ./target/pinpoint-collector-$VERSION.jar ../pinpoint-testbed/pinpoint-collector
-cp ./target/dependency/*.jar ../pinpoint-testbed/pinpoint-collector/lib
-cp ./runscript/* ../pinpoint-testbed/pinpoint-collector
+cp ./target/pinpoint-collector-$VERSION.jar $DEPLOY_DIR
+cp ./target/dependency/*.jar $DEPLOY_DIR/lib
+cp ./runscript/* $DEPLOY_DIR
+cp ./target/classes/applicationContext*.xml $DEPLOY_DIR
+cp ./target/classes/hbase.properties $DEPLOY_DIR
+cp ./target/classes/log4j.xml $DEPLOY_DIR
+cp ./target/classes/pinpoint-collector.properties $DEPLOY_DIR
