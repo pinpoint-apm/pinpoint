53ee0d3392f9f5116ee6b42224ad618f89ff1729
Chisu Yu <puseori9th@gmail.com>
2013-08-13 07:22:28 +0000

    [유치수] [NOBTS] 빌드 스크립트에 프로파일을 설정할 수 있도록 함.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-web/trunk@2142 84d0f5b1-2673-498c-a247-62c4ff18d310

--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
+#profile
+PROFILE=""
+if [ "$1" != "" ] ; then
+	$PROFILE="-P$1"
+fi
+
 # web ui
-mvn clean eclipse:eclipse package war:exploded -Dmaven.test.skip
+mvn clean eclipse:eclipse package war:exploded -Dmaven.test.skip $PROFILE
