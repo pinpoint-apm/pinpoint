8e458b685ff6afe3eb3bbf6fdcb96bbb6afdf11f
Chisu Yu <puseori9th@gmail.com>
2013-08-13 07:06:32 +0000

    [유치수] [NOBTS] 빌드 스크립트 수정.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-profiler-bootstrap/trunk@2140 84d0f5b1-2673-498c-a247-62c4ff18d310

--- build.sh
+++ build.sh
@@ -1,29 +1,30 @@
 VERSION="0.0.4-SNAPSHOT"
+DEPLOY_DIR="../pinpoint-testbed/agent"
+
 # profiler
 pushd .
 cd ../pinpoint-profiler
-mvn clean install eclipse:eclipse package dependency:copy-dependencies -Dmaven.test.skip -P withThrift -Dthrift.executable.property=/Users/netspider/DEV-TOOLS/thrift-0.9.0/bin/thrift
+mvn clean install eclipse:eclipse package dependency:copy-dependencies -Dmaven.test.skip -Dthrift.executable.property=/Users/netspider/DEV-TOOLS/thrift-0.9.0/bin/thrift
 popd
 
 # bootstrap
 mvn clean install eclipse:eclipse package dependency:copy-dependencies -Dmaven.test.skip
 
-rm -fr ../pinpoint-testbed/agent
-mkdir -p ../pinpoint-testbed/agent/lib
+rm -fr $DEPLOY_DIR
+mkdir -p $DEPLOY_DIR/lib
 
-cp ./target/pinpoint-bootstrap-$VERSION-jar-with-dependencies.jar ../pinpoint-testbed/agent/pinpoint-bootstrap-$VERSION.jar
+cp ./target/pinpoint-bootstrap-$VERSION-jar-with-dependencies.jar $DEPLOY_DIR/pinpoint-bootstrap-$VERSION.jar
 
-cp ../pinpoint-rpc/target/pinpoint-rpc-$VERSION.jar ../pinpoint-testbed/agent/lib
-cp ../pinpoint-rpc/target/dependency/netty-3.6.6.Final.jar ../pinpoint-testbed/agent/lib
+cp ../pinpoint-rpc/target/pinpoint-rpc-$VERSION.jar $DEPLOY_DIR/lib
+cp ../pinpoint-rpc/target/dependency/netty-3.6.6.Final.jar $DEPLOY_DIR/lib
 
-cp ../pinpoint-profiler/target/pinpoint-profiler-$VERSION.jar ../pinpoint-testbed/agent/lib
-cp ../pinpoint-profiler/target/dependency/javassist-3.16.1-GA.jar ../pinpoint-testbed/agent/lib
-cp ../pinpoint-profiler/target/dependency/libthrift-0.9.0.jar ../pinpoint-testbed/agent/lib
+cp ../pinpoint-profiler/target/pinpoint-profiler-$VERSION.jar $DEPLOY_DIR/lib
+cp ../pinpoint-profiler/target/dependency/javassist-3.16.1-GA.jar $DEPLOY_DIR/lib
+cp ../pinpoint-profiler/target/dependency/libthrift-0.9.0.jar $DEPLOY_DIR/lib
+cp ../pinpoint-profiler/target/dependency/log4j-1.2.16.jar $DEPLOY_DIR/lib
+cp ../pinpoint-profiler/target/dependency/slf4j-log4j12-1.6.6.jar $DEPLOY_DIR/lib
+cp ../pinpoint-profiler/target/dependency/slf4j-api-1.6.6.jar $DEPLOY_DIR/lib
 
-cp ../pinpoint-profiler/target/dependency/log4j-1.2.16.jar  ../pinpoint-testbed/agent/lib
-cp ../pinpoint-profiler/target/dependency/slf4j-log4j12-1.6.6.jar  ../pinpoint-testbed/agent/lib
-cp ../pinpoint-profiler/target/dependency/slf4j-api-1.6.6.jar  ../pinpoint-testbed/agent/lib
-
-cp ../pinpoint-profiler/runscript/help.txt ../pinpoint-testbed/agent
-cp ../pinpoint-profiler/runscript/pinpoint.config ../pinpoint-testbed/agent
-cp ../pinpoint-profiler/runscript/log4j.xml ../pinpoint-testbed/agent/lib
+cp ../pinpoint-profiler/runscript/help.txt $DEPLOY_DIR
+cp ../pinpoint-profiler/target/classes/pinpoint.config $DEPLOY_DIR
+cp ../pinpoint-profiler/target/classes/log4j.xml $DEPLOY_DIR
