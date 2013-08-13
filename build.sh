4f9870cfc7748a56c47bcb384f25848a56591adf
Chisu Yu <puseori9th@gmail.com>
2013-08-13 07:26:08 +0000

    [유치수] [NOBTS] 빌드 스크립트 수정.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-web/trunk@2143 84d0f5b1-2673-498c-a247-62c4ff18d310

--- build.sh
+++ build.sh
@@ -2,6 +2,13 @@
 PROFILE=""
 if [ "$1" != "" ] ; then
 	$PROFILE="-P$1"
+	echo "*********************"
+	echo "USING PROFILE $1"
+	echo "*********************"
+else
+	echo "*********************"
+	echo "USING DEFAULT PROFILE"
+	echo "*********************"
 fi
 
 # web ui
