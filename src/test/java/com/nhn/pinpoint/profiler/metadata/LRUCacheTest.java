faa201d9b76060a051e2d004e97ea17cef0b61b0
Woonduk Kang <wd.kang@navercorp.com>
2013-08-16 08:46:46 +0000

    [강운덕] [LUCYSUS-1744] lrucache의 구현체를 guava로 변경함. 14.0.1
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-tomcat-profiler/trunk@2156 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/test/java/com/nhn/pinpoint/profiler/metadata/LRUCacheTest.java
+++ src/test/java/com/nhn/pinpoint/profiler/metadata/LRUCacheTest.java
@@ -1,7 +1,5 @@
 package com.nhn.pinpoint.profiler.metadata;
 
-import com.nhn.pinpoint.profiler.metadata.LRUCache;
-import com.nhn.pinpoint.profiler.metadata.SqlObject;
 import junit.framework.Assert;
 import org.junit.Test;
 
@@ -13,39 +11,39 @@ import java.util.Random;
 public class LRUCacheTest {
     @Test
     public void testPut() throws Exception {
-        int cacheSize = 100;
-        LRUCache LRUCache = new LRUCache(cacheSize);
+        long cacheSize = 100;
+        LRUCache LRUCache = new LRUCache((int) cacheSize);
         Random random = new Random();
         for (int i = 0; i < 1000; i++) {
             LRUCache.put(new SqlObject(String.valueOf(random.nextInt(100000))));
         }
 
-        int size = LRUCache.getSize();
+        long size = LRUCache.getSize();
         Assert.assertEquals(size, cacheSize);
 
     }
 
     @Test
     public void testGetSize() throws Exception {
-        LRUCache LRUCache = new LRUCache(2);
-        Assert.assertEquals(LRUCache.getSize(), 0);
+        LRUCache cache = new LRUCache(2);
+        Assert.assertEquals(cache.getSize(), 0);
 
         SqlObject sqlObject = new SqlObject("test");
 
-        boolean hit = LRUCache.put(sqlObject);
+        boolean hit = cache.put(sqlObject);
         Assert.assertTrue(hit);
-        Assert.assertEquals(LRUCache.getSize(), 1);
+        Assert.assertEquals(cache.getSize(), 1);
 
-        boolean hit2 = LRUCache.put(sqlObject);
+        boolean hit2 = cache.put(sqlObject);
         Assert.assertFalse(hit2);
-        Assert.assertEquals(LRUCache.getSize(), 1);
+        Assert.assertEquals(cache.getSize(), 1);
 //        "23 123";
 //        "DCArMlhwQO 7"
-        LRUCache.put(new SqlObject("23 123"));
-        LRUCache.put(new SqlObject("DCArMlhwQO 7"));
-        LRUCache.put(new SqlObject("3"));
-        LRUCache.put(new SqlObject("4"));
-        Assert.assertEquals(LRUCache.getSize(), 2);
+        cache.put(new SqlObject("23 123"));
+        cache.put(new SqlObject("DCArMlhwQO 7"));
+        cache.put(new SqlObject("3"));
+        cache.put(new SqlObject("4"));
+        Assert.assertEquals(cache.getSize(), 2);
 
 
     }
