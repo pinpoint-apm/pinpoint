4da3471e47284e6071615a198eb14aa9a4737e8b
Woonduk Kang <wd.kang@navercorp.com>
2013-10-04 08:20:05 +0000

    [강운덕] [LUCYSUS-1744] JVMInfo 관련 클래스가 TAgentStat과 내용이 겹쳐 삭제함.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-server/trunk@2424 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/nhn/pinpoint/collector/dao/JvmInfoDao.java
+++
@@ -1,7 +0,0 @@
-package com.nhn.pinpoint.collector.dao;
-
-import com.nhn.pinpoint.thrift.dto.TJVMInfoThriftDTO;
-
-public interface JvmInfoDao {
-    void insert(TJVMInfoThriftDTO jvmInfoThriftDTO, byte[] jvmInfoBytes);
-}
