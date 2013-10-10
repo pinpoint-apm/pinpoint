a51cff62e05167548d470572fc6798907124e14c
Woonduk Kang <wd.kang@navercorp.com>
2013-10-10 11:14:18 +0000

    [강운덕] [LUCYSUS-1744] class Transform시 contextClassLoader에 agentClassLoader를 세팅하고 들어가도록 함.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-profiler-bootstrap/trunk@2490 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/nhn/pinpoint/bootstrap/PinpointURLClassLoader.java
+++ src/main/java/com/nhn/pinpoint/bootstrap/PinpointURLClassLoader.java
@@ -3,6 +3,8 @@ package com.nhn.pinpoint.bootstrap;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.net.URLStreamHandlerFactory;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * mark class loader
@@ -19,4 +21,12 @@ public class PinpointURLClassLoader extends URLClassLoader {
     public PinpointURLClassLoader(URL[] urls, ClassLoader parent, URLStreamHandlerFactory factory) {
         super(urls, parent, factory);
     }
+
+
+    @Override
+    public Class<?> loadClass(String name) throws ClassNotFoundException {
+        // classLoading문제시에 좀더 쉽게 찾을수 있도록 override
+        return super.loadClass(name);
+    }
+
 }
