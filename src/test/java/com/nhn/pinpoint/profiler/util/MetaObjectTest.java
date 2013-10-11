62f5e927cfc31fc9d82654e14e7f605af45300b3
Woonduk Kang <wd.kang@navercorp.com>
2013-10-11 05:07:40 +0000

    [강운덕] [LUCYSUS-1744] testcase추가.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-profiler-bootstrap/trunk@2496 84d0f5b1-2673-498c-a247-62c4ff18d310

---
+++ src/test/java/com/nhn/pinpoint/profiler/util/MetaObjectTest.java
@@ -0,0 +1,39 @@
+package com.nhn.pinpoint.profiler.util;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+/**
+ *
+ */
+public class MetaObjectTest {
+    private String test;
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+
+    @Test
+    public void testSetInvoke() throws Exception {
+        MetaObjectTest test = new MetaObjectTest();
+
+        MetaObject metaObject = new MetaObject("setTest", String.class);
+        Object result = metaObject.invoke(test, "set");
+        Assert.assertEquals(test.getTest(), "set");
+        Assert.assertNull(result);
+    }
+
+    @Test
+    public void testGetInvoke() throws Exception {
+        MetaObjectTest test = new MetaObjectTest();
+        test.setTest("get");
+
+        MetaObject metaObject = new MetaObject("getTest");
+        Object result = metaObject.invoke(test);
+        Assert.assertEquals(test.getTest(), result);
+    }
+}
