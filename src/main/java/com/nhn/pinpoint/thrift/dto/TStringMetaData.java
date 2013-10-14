699fb7282e39b872ef158ad6950b8ceee1098505
Woonduk Kang <wd.kang@navercorp.com>
2013-10-14 11:11:34 +0000

    [강운덕] [LUCYSUS-1744] string cache api추가. jdbc connect시 string cache api 를 사용하는 것으로 변경. 최초 connect시 30개 connection이 생성되는 testcase에서 4k에서 2k로 데이터 사이즈 축소.
    git-svn-id: http://svn.bds.nhncorp.com/pe/pinpoint-thrift/trunk@2520 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/nhn/pinpoint/thrift/dto/TStringMetaData.java
+++ src/main/java/com/nhn/pinpoint/thrift/dto/TStringMetaData.java
@@ -12,13 +12,25 @@ import org.apache.thrift.scheme.StandardScheme;
 
 import org.apache.thrift.scheme.TupleScheme;
 import org.apache.thrift.protocol.TTupleProtocol;
+import org.apache.thrift.protocol.TProtocolException;
 import org.apache.thrift.EncodingUtils;
+import org.apache.thrift.TException;
+import org.apache.thrift.async.AsyncMethodCallback;
+import org.apache.thrift.server.AbstractNonblockingServer.*;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.EnumMap;
+import java.util.Set;
+import java.util.HashSet;
 import java.util.EnumSet;
 import java.util.Collections;
 import java.util.BitSet;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TStringMetaData implements org.apache.thrift.TBase<TStringMetaData, TStringMetaData._Fields>, java.io.Serializable, Cloneable, Comparable<TStringMetaData> {
   private static final org.apache.thrift.protocol.TStruct STRUCT_DESC = new org.apache.thrift.protocol.TStruct("TStringMetaData");
