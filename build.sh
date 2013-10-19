e68012096d48a9933cee5d50439c412438cce399
Chisu Yu <puseori9th@gmail.com>
2013-10-19 00:33:39 +0000

    [유치수] [NOBTS] collector를 tomcat에 얹는 작업으로 인한 빌드 스크립트 및 테스트베드 수정.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-server/trunk@2556 84d0f5b1-2673-498c-a247-62c4ff18d310

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 VERSION="0.0.4-SNAPSHOT"
-DEPLOY_DIR="../pinpoint-testbed/pinpoint-collector"
+DEPLOY_DIR="../pinpoint-testbed/pinpoint-collector/webapps"
+FILENAME="pinpoint-collector-$VERSION.war"
+
+rm $DEPLOY_DIR/$FILENAME 
 
 #profile
 PROFILE=""
@@ -22,52 +25,7 @@ if [[ $rc != 0 ]] ; then
         exit $rc
 fi
 
-rm -fr $DEPLOY_DIR
-mkdir -p $DEPLOY_DIR/lib
-
-cp ./target/pinpoint-collector-$VERSION.jar $DEPLOY_DIR
-rc=$?
-if [[ $rc != 0 ]] ; then
-        echo "BUILD FAILED $rc"
-        exit $rc
-fi
-
-cp ./target/dependency/*.jar $DEPLOY_DIR/lib
-rc=$?
-if [[ $rc != 0 ]] ; then
-        echo "BUILD FAILED $rc"
-        exit $rc
-fi
-
-cp ./runscript/* $DEPLOY_DIR
-rc=$?
-if [[ $rc != 0 ]] ; then
-        echo "BUILD FAILED $rc"
-        exit $rc
-fi
-
-cp ./target/classes/applicationContext*.xml $DEPLOY_DIR
-rc=$?
-if [[ $rc != 0 ]] ; then
-        echo "BUILD FAILED $rc"
-        exit $rc
-fi
-
-cp ./target/classes/hbase.properties $DEPLOY_DIR
-rc=$?
-if [[ $rc != 0 ]] ; then
-        echo "BUILD FAILED $rc"
-        exit $rc
-fi
-
-cp ./target/classes/log4j.xml $DEPLOY_DIR
-rc=$?
-if [[ $rc != 0 ]] ; then
-        echo "BUILD FAILED $rc"
-        exit $rc
-fi
-
-cp ./target/classes/pinpoint-collector.properties $DEPLOY_DIR
+cp ./target/pinpoint-collector-$VERSION.war $DEPLOY_DIR
 rc=$?
 if [[ $rc != 0 ]] ; then
         echo "BUILD FAILED $rc"
