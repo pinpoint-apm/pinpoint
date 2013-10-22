fe25ab85cb937d23e685fd01461043ccb3f41f9d
Chisu Yu <puseori9th@gmail.com>
2013-10-22 02:10:00 +0000

    [유치수] [NOBTS]  서버맵에서 link statistics보이지 않는 문제 수정.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-web/trunk@2581 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/nhn/pinpoint/web/controller/ApplicationMapController.java
+++ src/main/java/com/nhn/pinpoint/web/controller/ApplicationMapController.java
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.nhn.pinpoint.common.ServiceType;
 import com.nhn.pinpoint.web.applicationmap.ApplicationMap;
-import com.nhn.pinpoint.web.applicationmap.rawdata.ApplicationStatistics;
-import com.nhn.pinpoint.web.calltree.server.ServerCallTree;
 import com.nhn.pinpoint.web.filter.Filter;
 import com.nhn.pinpoint.web.filter.FilterBuilder;
 import com.nhn.pinpoint.web.service.ApplicationMapService;
@@ -89,26 +87,26 @@ public class ApplicationMapController {
 		return "applicationmap.filtered.view";
 	}
 
-	@Deprecated
-	@RequestMapping(value = "/getFilteredServerMapData", method = RequestMethod.GET)
-	public String getFilteredServerMapData(Model model,
-											HttpServletResponse response,
-											@RequestParam("application") String applicationName, 
-											@RequestParam("serviceType") short serviceType,
-											@RequestParam("from") long from,
-											@RequestParam("to") long to,
-											@RequestParam(value = "filter", required = false) String filterText,
-											@RequestParam(value = "limit", required = false, defaultValue = "1000000") int limit) {
-		ResultWithMark<Set<TransactionId>, Long> traceIdSet = flow.selectTraceIdsFromApplicationTraceIndex(applicationName, from, to, limit);
-		Filter filter = FilterBuilder.build(filterText);
-		ServerCallTree map = flow.selectServerCallTree(traceIdSet.getValue(), filter);
-		
-		model.addAttribute("nodes", map.getNodes());
-		model.addAttribute("links", map.getLinks());
-		model.addAttribute("filter", filter);
-
-		return "applicationmap.filtered";
-	}
+//	@Deprecated
+//	@RequestMapping(value = "/getFilteredServerMapData", method = RequestMethod.GET)
+//	public String getFilteredServerMapData(Model model,
+//											HttpServletResponse response,
+//											@RequestParam("application") String applicationName, 
+//											@RequestParam("serviceType") short serviceType,
+//											@RequestParam("from") long from,
+//											@RequestParam("to") long to,
+//											@RequestParam(value = "filter", required = false) String filterText,
+//											@RequestParam(value = "limit", required = false, defaultValue = "1000000") int limit) {
+//		ResultWithMark<Set<TransactionId>, Long> traceIdSet = flow.selectTraceIdsFromApplicationTraceIndex(applicationName, from, to, limit);
+//		Filter filter = FilterBuilder.build(filterText);
+//		ServerCallTree map = flow.selectServerCallTree(traceIdSet.getValue(), filter);
+//		
+//		model.addAttribute("nodes", map.getNodes());
+//		model.addAttribute("links", map.getLinks());
+//		model.addAttribute("filter", filter);
+//
+//		return "applicationmap.filtered";
+//	}
 	
 	@RequestMapping(value = "/getFilteredServerMapData2", method = RequestMethod.GET)
 	public String getFilteredServerMapData2(Model model,
@@ -141,13 +139,13 @@ public class ApplicationMapController {
 	// 필터 사용 안함.
 	@RequestMapping(value = "/linkStatistics", method = RequestMethod.GET)
 	public String getLinkStatistics(Model model,
-			HttpServletResponse response, 
-			@RequestParam("from") long from,
-			@RequestParam("to") long to,
-			@RequestParam("srcApplicationName") String srcApplicationName,
-			@RequestParam("srcServiceType") short srcServiceType,
-			@RequestParam("destApplicationName") String destApplicationName,
-			@RequestParam("destServiceType") short destServiceType) {
+									HttpServletResponse response, 
+									@RequestParam("from") long from,
+									@RequestParam("to") long to,
+									@RequestParam("srcApplicationName") String srcApplicationName,
+									@RequestParam("srcServiceType") short srcServiceType,
+									@RequestParam("destApplicationName") String destApplicationName,
+									@RequestParam("destServiceType") short destServiceType) {
 		
 		LinkStatistics linkStatistics = flow.linkStatistics(from, to, srcApplicationName, srcServiceType, destApplicationName, destServiceType);
 
@@ -165,13 +163,16 @@ public class ApplicationMapController {
 		model.addAttribute("timeseriesSlotIndex", linkStatistics.getTimeseriesSlotIndex().entrySet().iterator());
 		model.addAttribute("timeseriesValue", linkStatistics.getTimeseriesValue());
 
+		// FIXME lastFetchedTimestamp는 filtered에서만 사용되는 값으로 여기에서는 필요 없음. 일단 임시방편으로 -1로 세팅.
+		model.addAttribute("lastFetchedTimestamp", -1);
+		
 		return "linkStatisticsDetail";
 	}
 	
 	// 선택한 연결선을 통과하는 요청의 통계 정보 조회.
 	// 필터 사용.
 	@RequestMapping(value = "/filteredLinkStatistics", method = RequestMethod.GET)
-	public String getLinkStatisticsDetail(Model model,
+	public String getFilteredLinkStatistics(Model model,
 									HttpServletResponse response, 
 									@RequestParam("application") String applicationName,
 									@RequestParam("serviceType") short serviceType,
@@ -194,21 +195,21 @@ public class ApplicationMapController {
 		return "linkStatisticsDetail";
 	}
 	
-	@Deprecated
-	@RequestMapping(value = "/applicationStatistics", method = RequestMethod.GET)
-	public String getLinkStatistics(Model model,
-			HttpServletResponse response, 
-			@RequestParam("from") long from,
-			@RequestParam("to") long to,
-			@RequestParam("applicationName") String applicationName,
-			@RequestParam("serviceType") short serviceType) {
-		
-		ApplicationStatistics stat = applicationMapService.selectApplicationStatistics(applicationName, serviceType, from, to);
-		
-		model.addAttribute("from", from);
-		model.addAttribute("to", to);
-		model.addAttribute("applicationStatistics", stat);
-
-		return "applicationStatisticsDetail";
-	}
+//	@Deprecated
+//	@RequestMapping(value = "/applicationStatistics", method = RequestMethod.GET)
+//	public String getLinkStatistics(Model model,
+//			HttpServletResponse response, 
+//			@RequestParam("from") long from,
+//			@RequestParam("to") long to,
+//			@RequestParam("applicationName") String applicationName,
+//			@RequestParam("serviceType") short serviceType) {
+//		
+//		ApplicationStatistics stat = applicationMapService.selectApplicationStatistics(applicationName, serviceType, from, to);
+//		
+//		model.addAttribute("from", from);
+//		model.addAttribute("to", to);
+//		model.addAttribute("applicationStatistics", stat);
+//
+//		return "applicationStatisticsDetail";
+//	}
 }
