4a8eaf0167e8763c4c8c9ed57c6e900f24aa8ef7
Woonduk Kang <wd.kang@navercorp.com>
2013-10-29 10:28:29 +0000

    [강운덕] [LUCYSUS-1744] spanId 를 중복으로 가지고 있을수 있도록 수정함. 추가적으로 spanId충돌시 최대한 정확한 매칭을 찾을수 있도록 추가 개발이 있어야 함.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-web/trunk@2649 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/nhn/pinpoint/web/calltree/span/SpanAligner2.java
+++ src/main/java/com/nhn/pinpoint/web/calltree/span/SpanAligner2.java
@@ -24,16 +24,16 @@ public class SpanAligner2 {
 
 
     private static final Integer ROOT = -1;
-	private final Map<Integer, SpanBo> spanMap;
+	private final Map<Integer, List<SpanBo>> spanMap;
 	private Integer rootSpanId = null;
     private int matchType = FAIL_MATCH;
 
-	public SpanAligner2(List<SpanBo> spans, long selectedSpanStartTime) {
+	public SpanAligner2(List<SpanBo> spans, long collectorAcceptTime) {
         this.spanMap = buildSpanMap(spans);
-        this.rootSpanId = findRootSpanId(spans, selectedSpanStartTime);
+        this.rootSpanId = findRootSpanId(spans, collectorAcceptTime);
     }
 
-    private int findRootSpanId(List<SpanBo> spans, long selectedSpanStartTime) {
+    private int findRootSpanId(List<SpanBo> spans, long collectorAcceptTime) {
         final List<SpanBo> root = new ArrayList<SpanBo>();
         for (SpanBo span : spans) {
             if (span.getParentSpanId() == ROOT) {
@@ -57,11 +57,11 @@ public class SpanAligner2 {
         // root 분실. 혹은 아직 도착하지 않아 root가 완성 되지 않음. 즉 진행중인 process일 수 있음.
         // 차선책으로 자신이 조회한 span의 시작 시간을 기준으로 span을 조회한다.
         // span에서 데이터를 추출하는 것이기 때문에, 왠간하면 데이터는 존재함. hbase insert시 data insert를 실패할 경우 없을수 있음.
-        final List<SpanBo> startTimeMatcher = new ArrayList<SpanBo>();
+        final List<SpanBo> collectorAcceptTimeMatcher = new ArrayList<SpanBo>();
         for(SpanBo span : spans) {
             // collectorTime이 힌트로 들어온다.
-            if (span.getCollectorAcceptTime() == selectedSpanStartTime) {
-                startTimeMatcher.add(span);
+            if (span.getCollectorAcceptTime() == collectorAcceptTime) {
+                collectorAcceptTimeMatcher.add(span);
             }
         }
         // startTime 기반 match. 아래 추가 정보가 제공 되면 더 정확하게 매치가 가능하다.
@@ -72,38 +72,48 @@ public class SpanAligner2 {
         // "applicationId" : "emeroad-app",
         // "callStackStart" : 1383024213315,
         // "callStackEnd" : 2010,
-        final int startMatchSize = startTimeMatcher.size();
+        final int startMatchSize = collectorAcceptTimeMatcher.size();
         if (startMatchSize == 1) {
-            final SpanBo spanBo = startTimeMatcher.get(0);
-            logger.info("startTime span found startTime match:{}", spanBo);
+            final SpanBo spanBo = collectorAcceptTimeMatcher.get(0);
+            logger.info("collectorAcceptTime span found startTime match:{}", spanBo);
             matchType = START_TIME_MATCH;
             return spanBo.getSpanId();
         }
         if (startMatchSize > 1) {
-            logger.warn("startTime match collision. size:{} selectedSpanStartTime:{} span:{} allSpan:{}", startMatchSize, selectedSpanStartTime, startMatchSize, spans);
-            throw new IllegalStateException("startTime match collision size:" + startMatchSize + " selectedSpanStartTime:" + selectedSpanStartTime);
+            logger.warn("collectorAcceptTime match collision. size:{} collectorAcceptTime:{} allSpan:{}", startMatchSize, collectorAcceptTime, spans);
+            throw new IllegalStateException("startTime match collision size:" + startMatchSize + " collectorAcceptTime:" + collectorAcceptTime);
         }
         // 여기서 다음상황으로 더 정확하게 매치가 가능한가? 마땅히 call stack을 랜더링 할수 있는 방법 없음
-        logger.warn("startTime match not found. size:{} selectedSpanStartTime:{} span:{} allSpan:{}", startMatchSize, selectedSpanStartTime, startMatchSize, spans);
-        throw new IllegalStateException("startTime match not found startTime size:" + startMatchSize + " selectedSpanStartTime:" + selectedSpanStartTime);
+        logger.warn("collectorAcceptTime match not found. size:{} collectorAcceptTime:{} allSpan:{}", startMatchSize, collectorAcceptTime, spans);
+        throw new IllegalStateException("startTime match not found startTime size:" + startMatchSize + " collectorAcceptTime:" + collectorAcceptTime);
     }
 
-    private Map<Integer, SpanBo> buildSpanMap(List<SpanBo> spans) {
-        Map<Integer, SpanBo> spanMap = new HashMap<Integer, SpanBo>();
+    private Map<Integer, List<SpanBo>> buildSpanMap(List<SpanBo> spans) {
+        final Map<Integer, List<SpanBo>> spanMap = new HashMap<Integer, List<SpanBo>>();
         for (SpanBo span : spans) {
-            spanMap.put(span.getSpanId(), span);
+            List<SpanBo> spanBoList = spanMap.get(span.getSpanId());
+            if (spanBoList == null) {
+                spanBoList = new ArrayList<SpanBo>();
+                spanBoList.add(span);
+                spanMap.put(span.getSpanId(), spanBoList);
+            } else {
+                spanBoList.add(span);
+            }
         }
         return spanMap;
     }
 
     public List<SpanAlign> sort() {
 		List<SpanAlign> list = new ArrayList<SpanAlign>();
-		final SpanBo root = spanMap.get(rootSpanId);
-		if (root == null) {
-			throw new IllegalStateException("root span not found. rootSpanId=" + rootSpanId + ", map=" + spanMap.keySet());
+		final List<SpanBo> rootList = spanMap.get(rootSpanId);
+		if (rootList == null || rootList.size() == 0) {
+			throw new IllegalStateException("rootList span not found. rootSpanId=" + rootSpanId + ", map=" + spanMap.keySet());
 		}
-
-		populate(root, 0, list);
+        if (rootList.size() > 1) {
+            throw new IllegalStateException("duplicate rootList span found. rootSpanId=" + rootSpanId + ", map=" + spanMap.keySet());
+        }
+        SpanBo rootSpanBo = rootList.get(0);
+        populate(rootSpanBo, 0, list);
 
 		return list;
 	}
@@ -144,7 +154,9 @@ public class SpanAligner2 {
 			int nextSpanId = spanEventBo.getNextSpanId();
 			if (nextSpanId != ROOT && spanMap.containsKey(nextSpanId)) {
                 int childDepth = currentDepth + 1;
-                SpanBo spanBo = spanMap.get(nextSpanId);
+                // TODO 중복 처리?
+                SpanBo spanBo = spanMap.get(nextSpanId).get(0);
+                logger.info("remote spanEvent:{} spanBo:{}", spanEventBo, spanBo);
                 if (spanBo != null) {
                     populate(spanBo, childDepth, container);
                 } else {
