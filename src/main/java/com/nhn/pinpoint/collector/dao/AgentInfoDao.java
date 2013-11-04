b55a930fbaf8ceed3bf289442d2d53f8985a2679
Woonduk Kang <wd.kang@navercorp.com>
2013-11-04 08:47:09 +0000

    [강운덕] [LUCYSUS-1744] spanId를 long으로 변경안된 부분이 있어 수정.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-server/trunk@2747 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/nhn/pinpoint/collector/dao/AgentInfoDao.java
+++ src/main/java/com/nhn/pinpoint/collector/dao/AgentInfoDao.java
@@ -3,7 +3,7 @@ package com.nhn.pinpoint.collector.dao;
 import com.nhn.pinpoint.thrift.dto.TAgentInfo;
 
 /**
- *
+ * @author emeroad
  */
 public interface AgentInfoDao {
     void insert(TAgentInfo agentInfo);
