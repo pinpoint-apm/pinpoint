55c7a7751e8195ddb8dce7108b4854a7e2f32eb3
Hoonmin Kim <harebox@gmail.com>
2013-11-04 02:10:09 +0000

    [김훈민] [PINPOINT-213] 수집하는 데이터 줄임. GC 통계를 일반화. AgentStat에 다른 종류의 통계가 들어갈 수 있도록 수정.
    git-svn-id: http://svn.bds.nhncorp.com/pe/pinpoint-thrift/trunk@2724 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/thrift/Pinpoint.thrift
+++ src/main/thrift/Pinpoint.thrift
@@ -17,94 +17,28 @@ struct TAgentInfo {
 
 }
 
-struct TStatWithSerialCollector {
-	1: string	agentId
-	2: i64		timestamp
-	3: i64		jvmMemoryTotalUsed
-	4: i64		jvmMemoryTotalMax
-	5: i64		jvmMemoryHeapUsed
-	6: i64		jvmMemoryHeapMax
-	7: i64		jvmMemoryNonHeapUsed
-	8: i64		jvmMemoryNonHeapMax
-	9: i64		jvmMemoryPoolsCodeCacheUsage
-	10: i64		jvmMemoryPoolsEdenSpaceUsage
-	11: i64		jvmMemoryPoolsSurvivorSpaceUsage
-	12: i64		jvmMemoryPoolsPermGenUsage
-	13: i64		jvmMemoryPoolsTenuredGenUsage
-	14: i64		jvmGcCopyCount
-	15: i64		jvmGcCopyTime
-	16: i64		jvmGcMarkSweepCompactCount
-	17: i64		jvmGcMarkSweepCompactTime
-	200: optional string metadata
+enum TJvmGcType {
+    UNKNOWN,
+    SERIAL,
+    PARALLEL,
+    CMS,
+    G1    
 }
 
-struct TStatWithParallelCollector {
-	1: string	agentId
-	2: i64		timestamp
-	3: i64		jvmMemoryTotalUsed
-	4: i64		jvmMemoryTotalMax
-	5: i64		jvmMemoryHeapUsed
-	6: i64		jvmMemoryHeapMax
-	7: i64		jvmMemoryNonHeapUsed
-	8: i64		jvmMemoryNonHeapMax
-	9: i64		jvmMemoryPoolsCodeCacheUsage
-	10: i64		jvmMemoryPoolsPSEdenSpaceUsage
-	11: i64		jvmMemoryPoolsPSSurvivorSpaceUsage
-	12: i64		jvmMemoryPoolsPSPermGenUsage
-	13: i64		jvmMemoryPoolsPSOldGenUsage
-	14: i64		jvmGcPSMarkSweepCount
-	15: i64		jvmGcPSMarkSweepTime
-	16: i64		jvmGcPSScavengeCount
-	17: i64		jvmGcPSScavengeTime
-	200: optional string metadata
+struct TJvmGc {
+    1: TJvmGcType   type = TJvmGcType.UNKNOWN
+    2: i64          jvmMemoryHeapUsed
+    3: i64          jvmMemoryHeapMax
+    4: i64          jvmMemoryNonHeapUsed
+    5: i64          jvmMemoryNonHeapMax
+    6: i64          jvmGcOldCount
+    7: i64          jvmGcOldTime
 }
 
-struct TStatWithCmsCollector {
-	1: string	agentId
-	2: i64		timestamp
-	3: i64		jvmMemoryTotalUsed
-	4: i64		jvmMemoryTotalMax
-	5: i64		jvmMemoryHeapUsed
-	6: i64		jvmMemoryHeapMax
-	7: i64		jvmMemoryNonHeapUsed
-	8: i64		jvmMemoryNonHeapMax
-	9: i64		jvmMemoryPoolsCodeCacheUsage
-	10: i64		jvmMemoryPoolsParEdenSpaceUsage
-	11: i64		jvmMemoryPoolsParSurvivorSpaceUsage
-	12: i64		jvmMemoryPoolsCMSOldGenUsage
-	13: i64		jvmMemoryPoolsCMSPermGenUsage
-	14: i64		jvmGcParNewCount
-	15: i64		jvmGcParNewTime
-	16: i64		jvmGcCmsCount
-	17: i64		jvmGcCmsTime
-	200: optional string metadata
+struct TAgentStat {
+    1: string               agentId
+    2: i64                  startTimestamp
+    3: i64                  timestamp
+    10: optional TJvmGc     gc
+    200: optional string    metadata    
 }
-
-struct TStatWithG1Collector {
-	1: string	agentId
-	2: i64		timestamp
-	3: i64		jvmMemoryTotalUsed
-	4: i64		jvmMemoryTotalMax
-	5: i64		jvmMemoryHeapUsed
-	6: i64		jvmMemoryHeapMax
-	7: i64		jvmMemoryNonHeapUsed
-	8: i64		jvmMemoryNonHeapMax
-	9: i64		jvmMemoryPoolsCodeCacheUsage
-	10: i64		jvmMemoryPoolsG1EdenSpaceUsage
-	11: i64		jvmMemoryPoolsG1OldGenUsage
-	12: i64		jvmMemoryPoolsG1PermGenUsage
-	13: i64		jvmMemoryPoolsG1SurvivorSpaceUsage
-	14: i64		jvmGcG1OldGenerationCount
-	15: i64		jvmGcG1OldGenerationTime
-	16: i64		jvmGcG1YoungGenerationCount
-	17: i64		jvmGcG1YoungGenerationTime
-	200: optional string metadata
-}
-
-union TAgentStat {
-	1: TStatWithSerialCollector		serial
-	2: TStatWithParallelCollector	parallel
-	3: TStatWithCmsCollector		cms
-	4: TStatWithG1Collector			g1
-}
-
