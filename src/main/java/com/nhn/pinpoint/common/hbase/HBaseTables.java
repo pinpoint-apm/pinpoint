d6308072b59c03706716c1173c0a01ca1d3ddc9c
Woonduk Kang <wd.kang@navercorp.com>
2013-11-05 05:30:34 +0000

    [강운덕] [LUCYSUS-1744] 사용하지 않는 class삭제.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-commons/trunk@2769 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/nhn/pinpoint/common/hbase/HBaseTables.java
+++ src/main/java/com/nhn/pinpoint/common/hbase/HBaseTables.java
@@ -11,12 +11,6 @@ public class HBaseTables {
 	public static final int APPLICATION_NAME_MAX_LEN = PinpointConstants.APPLICATION_NAME_MAX_LEN;
     public static final int AGENT_NAME_MAX_LEN = PinpointConstants.AGENT_NAME_MAX_LEN;
 	
-    @Deprecated
-    public static final String TRACE_INDEX = "TraceIndex";
-    @Deprecated
-    public static final byte[] TRACE_INDEX_CF_TRACE = Bytes.toBytes("I"); // Trace
-    @Deprecated
-    public static final int TRACE_INDEX_ROW_DISTRIBUTE_SIZE = 1; // Trace_Index hash size
 
     public static final String APPLICATION_TRACE_INDEX = "ApplicationTraceIndex";
     public static final byte[] APPLICATION_TRACE_INDEX_CF_TRACE = Bytes.toBytes("I"); // applicationIndex
@@ -32,11 +26,6 @@ public class HBaseTables {
     public static final byte[] TRACES_CF_ANNOTATION = Bytes.toBytes("A");  //Annotation
     public static final byte[] TRACES_CF_TERMINALSPAN = Bytes.toBytes("T"); //TerminalSpan
 
-    @Deprecated
-    public static final String SERVERS_INDEX = "ServersIndex";
-    @Deprecated
-    public static final byte[] SERVERS_INDEX_CF_AGENTS = Bytes.toBytes("Agents");
-
     public static final String APPLICATION_INDEX = "ApplicationIndex";
     public static final byte[] APPLICATION_INDEX_CF_AGENTS = Bytes.toBytes("Agents");
 
@@ -47,28 +36,6 @@ public class HBaseTables {
     public static final String AGENTID_APPLICATION_INDEX = "AgentIdApplicationIndex";
     public static final byte[] AGENTID_APPLICATION_INDEX_CF_APPLICATION = Bytes.toBytes("Application");
 
-    @Deprecated
-    public static final String TERMINAL_STATISTICS = "TerminalStatistics";
-    @Deprecated
-    public static final byte[] TERMINAL_STATISTICS_CF_COUNTER = Bytes.toBytes("Counter");
-    @Deprecated
-	public static final byte[] TERMINAL_STATISTICS_CQ_ERROR_SLOT = Bytes.toBytes((short) -1);
-
-	@Deprecated
-	public static final String CLIENT_STATISTICS = "ClientStatistics";
-	@Deprecated
-	public static final byte[] CLIENT_STATISTICS_CF_COUNTER = Bytes.toBytes("Counter");
-	@Deprecated
-	public static final byte[] CLIENT_STATISTICS_CQ_ERROR_SLOT = Bytes.toBytes((short) -1);
-
-	@Deprecated
-	public static final String BUSINESS_TRANSACTION_STATISTICS = "BusinessTransactionStatistics";
-	@Deprecated
-	public static final byte[] BUSINESS_TRANSACTION_STATISTICS_CF_NORMAL = Bytes.toBytes("Normal");
-	@Deprecated
-	public static final byte[] BUSINESS_TRANSACTION_STATISTICS_CF_SLOW = Bytes.toBytes("Slow");
-	@Deprecated
-	public static final byte[] BUSINESS_TRANSACTION_STATISTICS_CF_ERROR = Bytes.toBytes("Error");
 
     public static final String SQL_METADATA = "SqlMetaData";
     public static final byte[] SQL_METADATA_CF_SQL = Bytes.toBytes("Sql");
