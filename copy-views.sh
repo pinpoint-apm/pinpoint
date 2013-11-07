b5b35d04cecc07bf4039eb676bfc81b5dcb9d69a
Chisu Yu <puseori9th@gmail.com>
2013-11-07 02:07:07 +0000

    [유치수] [NOBTS] 0.0.4로 버전 변경.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-web/trunk@2823 84d0f5b1-2673-498c-a247-62c4ff18d310

--- copy-views.sh
+++ copy-views.sh
@@ -1,4 +1,4 @@
-VERSION="0.0.4-SNAPSHOT"
+VERSION="0.0.4"
 cp ./src/main/webapp/WEB-INF/views/*.jsp ./target/pinpoint-web-$VERSION/WEB-INF/views
 cp ./src/main/webapp/WEB-INF/web.xml ./target/pinpoint-web-$VERSION/WEB-INF
 cp ./src/main/webapp/*.html ./target/pinpoint-web-$VERSION
