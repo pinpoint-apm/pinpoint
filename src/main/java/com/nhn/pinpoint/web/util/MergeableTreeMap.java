5a8a9d3e3ff4ee76aa164139fec06779250ef093
Woonduk Kang <wd.kang@navercorp.com>
2013-11-13 09:52:56 +0000

    [강운덕] [LUCYSUS-1744] 자료구조 튜닝.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-web/trunk@2904 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/nhn/pinpoint/web/util/MergeableTreeMap.java
+++ src/main/java/com/nhn/pinpoint/web/util/MergeableTreeMap.java
@@ -14,12 +14,12 @@ public class MergeableTreeMap<K, V extends Mergeable<K, V>> extends TreeMap<K, V
 	private static final long serialVersionUID = 8751127723531630287L;
 
 	public V putOrMerge(K key, V value) {
-		V v = get(key);
-		if (v == null) {
-			put(key, v = value);
+		final V find = get(key);
+		if (find == null) {
+			put(key, value);
 		} else {
-			put(key, v.mergeWith(value));
+            find.mergeWith(value);
 		}
-		return v;
+		return find;
 	}
 }
