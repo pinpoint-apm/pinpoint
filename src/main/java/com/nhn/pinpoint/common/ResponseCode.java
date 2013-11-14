a7768d1424ae50b81d911e9acc55afc10bd3fb11
Woonduk Kang <wd.kang@navercorp.com>
2013-11-14 10:10:26 +0000

    [강운덕] [LUCYSUS-1744] applicationMap 구성요성 리팩토링
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-commons/trunk@2918 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/nhn/pinpoint/common/ResponseCode.java
+++ src/main/java/com/nhn/pinpoint/common/ResponseCode.java
@@ -1,15 +1,5 @@
 package com.nhn.pinpoint.common;
 
 public enum ResponseCode {
-	NORMAL(0), WARN(1), SLOW(2), ERROR(3);
-
-	int code;
-
-	ResponseCode(int code) {
-		this.code = code;
-	}
-
-	public int getCode() {
-		return this.code;
-	}
+	NORMAL(), WARN(), SLOW(), ERROR();
 }
