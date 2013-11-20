4d50118b9b5c90c4236f99618f25b6a4fbeb1dc4
Chisu Yu <puseori9th@gmail.com>
2013-11-20 01:52:14 +0000

    [유치수] [NOBTS] demo controller 추가.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-testweb/trunk@2942 84d0f5b1-2673-498c-a247-62c4ff18d310

---
+++ src/main/java/com/nhn/pinpoint/testweb/controller/DemoController.java
@@ -0,0 +1,130 @@
+package com.nhn.pinpoint.testweb.controller;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Random;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+import net.spy.memcached.AddrUtil;
+import net.spy.memcached.ArcusClient;
+import net.spy.memcached.ConnectionFactoryBuilder;
+import net.spy.memcached.MemcachedClient;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.nhn.pinpoint.testweb.service.CubridService;
+import com.nhn.pinpoint.testweb.service.MemberService;
+import com.nhn.pinpoint.testweb.util.HttpConnectorOptions;
+import com.nhn.pinpoint.testweb.util.HttpInvoker;
+
+@Controller
+public class DemoController implements DisposableBean {
+
+	/**
+	 * dev-pinpoint-demo002.ncl
+	 */
+	private static final String HTTP_URL_BACKEND_WEB1 = "http://dev-pinpoint-workload002.ncl:8080/backend1.pinpoint";
+
+	/**
+	 * dev-pinpoint-demo003.ncl
+	 */
+	private static final String HTTP_URL_BACKEND_WEB2 = "http://dev-pinpoint-workload003.ncl:8080/backend2.pinpoint";
+
+	private final ArcusClient arcus;
+	private final MemcachedClient memcached;
+
+	public DemoController() throws IOException {
+		arcus = ArcusClient.createArcusClient("dev.arcuscloud.nhncorp.com:17288", "dev", new ConnectionFactoryBuilder());
+		memcached = new MemcachedClient(AddrUtil.getAddresses("10.25.149.80:11244,10.25.149.80:11211,10.25.149.79:11211"));
+	}
+
+	@Autowired
+	private MemberService service;
+
+	@Autowired
+	private CubridService cubridService;
+
+	@RequestMapping(value = "/demo")
+	public String demo() {
+		callBackend1();
+		callBackend2();
+		memcached();
+		naver();
+		return "demo";
+	}
+
+	@RequestMapping(value = "/backend1")
+	public String backend1() {
+		arcus();
+		mysql();
+		return "demo";
+	}
+
+	@RequestMapping(value = "/backend2")
+	public String backend2() {
+		mysql();
+		cubrid();
+		return "demo";
+	}
+
+	private void callBackend1() {
+		HttpInvoker client = new HttpInvoker(new HttpConnectorOptions());
+		client.execute(HTTP_URL_BACKEND_WEB1, new HashMap<String, Object>());
+	}
+
+	private void callBackend2() {
+		HttpInvoker client = new HttpInvoker(new HttpConnectorOptions());
+		client.execute(HTTP_URL_BACKEND_WEB2, new HashMap<String, Object>());
+	}
+
+	private void arcus() {
+		int rand = new Random().nextInt(100);
+		String key = "pinpoint:demo-" + rand;
+		Future<Object> getFuture = null;
+		try {
+			getFuture = arcus.asyncGet(key);
+			getFuture.get(1000L, TimeUnit.MILLISECONDS);
+		} catch (Exception e) {
+			if (getFuture != null)
+				getFuture.cancel(true);
+			throw new RuntimeException(e);
+		}
+	}
+
+	private void memcached() {
+		int rand = new Random().nextInt(100);
+		String key = "pinpoint:demo-" + rand;
+		Future<Object> getFuture = null;
+		try {
+			getFuture = memcached.asyncGet(key);
+			getFuture.get(1000L, TimeUnit.MILLISECONDS);
+		} catch (Exception e) {
+			if (getFuture != null)
+				getFuture.cancel(true);
+			throw new RuntimeException(e);
+		}
+	}
+
+	private void mysql() {
+		service.list();
+	}
+
+	private void cubrid() {
+		cubridService.createStatement();
+	}
+
+	private void naver() {
+		HttpInvoker client = new HttpInvoker(new HttpConnectorOptions());
+		client.execute("http://www.naver.com/", new HashMap<String, Object>());
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		arcus.shutdown();
+		memcached.shutdown();
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/demo.jsp
@@ -0,0 +1,2 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR" pageEncoding="EUC-KR" %>
+DEMO
