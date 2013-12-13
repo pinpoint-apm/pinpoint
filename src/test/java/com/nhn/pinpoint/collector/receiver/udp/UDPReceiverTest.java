04664072bead0ae05ad5fab5c9985dfd1c4457d6
Woonduk Kang <wd.kang@navercorp.com>
2013-12-13 12:05:42 +0000

    [강운덕] [PINPOINT-244] udp 패킷 전송시 data가 0인것을 보낼수 있는지 확인.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-server/trunk@3022 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/test/java/com/nhn/pinpoint/collector/receiver/udp/UDPReceiverTest.java
+++ src/test/java/com/nhn/pinpoint/collector/receiver/udp/UDPReceiverTest.java
@@ -1,15 +1,14 @@
 package com.nhn.pinpoint.collector.receiver.udp;
 
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.SocketException;
+import java.io.IOException;
+import java.net.*;
 
 import com.nhn.pinpoint.collector.receiver.DataReceiver;
 import com.nhn.pinpoint.collector.receiver.DispatchHandler;
 import junit.framework.Assert;
 
 import org.apache.thrift.TBase;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,6 +21,7 @@ public class UDPReceiverTest {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Test
+    @Ignore
 	public void startStop() {
 		try {
 			DataReceiver receiver = new UDPReceiver("test", new DispatchHandler() {
@@ -54,7 +54,21 @@ public class UDPReceiverTest {
         DatagramSocket datagramSocket = new DatagramSocket();
         int receiveBufferSize = datagramSocket.getReceiveBufferSize();
         System.out.println(receiveBufferSize);
+
         datagramSocket.setReceiveBufferSize(64*1024*10);
         System.out.println(datagramSocket.getReceiveBufferSize());
+
+        datagramSocket.close();
+    }
+
+    @Test
+    public void sendSocketBufferSize() throws IOException {
+        DatagramPacket datagramPacket = new DatagramPacket(new byte[0], 0, 0);
+
+        DatagramSocket datagramSocket = new DatagramSocket();
+        datagramSocket.connect(new InetSocketAddress("127.0.0.1", 9995));
+
+        datagramSocket.send(datagramPacket);
+        datagramSocket.close();
     }
 }
