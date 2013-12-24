8fe4d974846ef7e945df431cb68c8570005b02b0
Woonduk Kang <wd.kang@navercorp.com>
2013-12-24 07:32:45 +0000

    [강운덕] [PINPOINT-254] value 가 음수 일 경우 mod처리가 정상적으로 되지 않는 문제 수정.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-commons/trunk@3046 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/test/java/com/nhn/pinpoint/common/util/MathUtilsTest.java
+++ src/test/java/com/nhn/pinpoint/common/util/MathUtilsTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * @author emeroad
  */
@@ -17,4 +19,29 @@ public class MathUtilsTest {
         Assert.assertTrue(MathUtils.fastAbs(0) == 0);
         Assert.assertTrue(MathUtils.fastAbs(1) > 0);
     }
+
+
+    @Test
+    public void overflow() {
+
+
+        logger.debug("abs:{}", Math.abs(Integer.MIN_VALUE));
+        logger.debug("fastabs:{}", MathUtils.fastAbs(Integer.MIN_VALUE));
+
+        int index = Integer.MIN_VALUE -2;
+        for(int i =0; i<5; i++) {
+            logger.debug("{}------------", i);
+            logger.debug("{}", index);
+            logger.debug("mod:{}", index % 3);
+            logger.debug("abs:{}", Math.abs(index));
+            logger.debug("fastabs:{}", MathUtils.fastAbs(index));
+
+            index++;
+        }
+
+
+
+    }
+
+
 }
