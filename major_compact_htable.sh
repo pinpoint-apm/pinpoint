451cbc101a10f58d195ce8013a7b2f10294c8e27
Woonduk Kang <wd.kang@navercorp.com>
2014-01-07 07:47:23 +0000

    [강운덕] [PINPOINT-241] spring-data-hadoop 버전 업 1.0.1->1.0.2
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-commons/trunk@3080 84d0f5b1-2673-498c-a247-62c4ff18d310

---
+++ major_compact_htable.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+exec hbase shell <<EOF
+
+major_compact 'AgentInfo'
+major_compact 'AgentStat'
+major_compact 'ApplicationIndex'
+
+major_compact 'StringMetaData'
+major_compact 'SqlMetaData'
+major_compact 'ApiMetaData'
+
+major_compact 'ApplicationTraceIndex'
+major_compact 'Traces'
+
+major_compact 'ApplicationMapStatisticsCaller'
+major_compact 'ApplicationMapStatisticsCallee'
+major_compact 'ApplicationStatistics'
+major_compact 'HostApplicationMap'
+
+
+major_compact 'AgentInfo'
+major_compact 'AgentStat'
+major_compact 'ApplicationIndex'
+
+major_compact 'StringMetaData'
+major_compact 'SqlMetaData'
+major_compact 'ApiMetaData'
+
+major_compact 'ApplicationTraceIndex'
+major_compact 'Traces'
+
+major_compact 'ApplicationMapStatisticsCaller'
+major_compact 'ApplicationMapStatisticsCallee'
+major_compact 'ApplicationStatistics'
+major_compact 'HostApplicationMap'
+
+EOF
