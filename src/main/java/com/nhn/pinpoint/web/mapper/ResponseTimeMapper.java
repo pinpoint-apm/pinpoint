4cba0f18bd47e86ef2fe55f953cf7a2dc03d176c
Woonduk Kang <wd.kang@navercorp.com>
2014-01-29 02:32:14 +0000

    [강운덕] [PINPOINT-267] testcase가 깨지는 부분이 있어 임시 commit
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-web/trunk@3230 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/nhn/pinpoint/web/mapper/ResponseTimeMapper.java
+++ src/main/java/com/nhn/pinpoint/web/mapper/ResponseTimeMapper.java
@@ -2,7 +2,11 @@ package com.nhn.pinpoint.web.mapper;
 
 import com.nhn.pinpoint.common.buffer.Buffer;
 import com.nhn.pinpoint.common.buffer.FixedBuffer;
+import com.nhn.pinpoint.common.hbase.HBaseTables;
+import com.nhn.pinpoint.common.util.BytesUtils;
+import com.nhn.pinpoint.web.util.TimeUtils;
 import com.nhn.pinpoint.web.vo.RawResponseTime;
+import org.apache.hadoop.hbase.KeyValue;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.springframework.data.hadoop.hbase.RowMapper;
@@ -16,9 +20,34 @@ public class ResponseTimeMapper implements RowMapper<RawResponseTime> {
         if (result.isEmpty()) {
             return null;
         }
-        final byte[] row = result.getRow();
-        final Buffer rowBuffer = new FixedBuffer(row);
-        rowBuffer.readPrefixedString();
+        RawResponseTime rawResponseTime = createRawResponseTime(result);
 
+        for (KeyValue keyValue : result.raw()) {
+            if (!Bytes.equals(keyValue.getFamily(), HBaseTables.APPLICATION_MAP_STATISTICS_SELF_CF_COUNTER)) {
+                return rawResponseTime;
+            }
+            final byte[] row = keyValue.getRow();
+            final byte[] value = keyValue.getValue();
+            recordColumn(rawResponseTime, row, value);
+        }
+        return rawResponseTime;
+    }
+
+    void recordColumn(RawResponseTime rawResponseTime, byte[] row, byte[] value) {
+        short slotNumber = Bytes.toShort(row);
+        String agentId = Bytes.toString(row, 2, row.length - 2);
+        long count = Bytes.toLong(value);
+        rawResponseTime.getHistogram().addSample(slotNumber, count);
+    }
+
+    private RawResponseTime createRawResponseTime(Result result) {
+        final byte[] row = result.getRow();
+
+        final Buffer rowBuffer = new FixedBuffer(row);
+        String applicationName = rowBuffer.readPrefixedString();
+        short serviceType = rowBuffer.readShort();
+        long time = com.nhn.pinpoint.common.util.TimeUtils.recoveryCurrentTimeMillis(rowBuffer.readLong());
+
+        return new RawResponseTime(applicationName, serviceType, time);
     }
 }
