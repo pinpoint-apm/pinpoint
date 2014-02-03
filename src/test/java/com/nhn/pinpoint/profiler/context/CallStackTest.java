3bac9961ecd66883eec2ac9dbd005a31423c98ae
Woonduk Kang <wd.kang@navercorp.com>
2014-02-03 07:10:50 +0000

    [강운덕] [PINPOINT-267] stack Testcase추가
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-tomcat-profiler/trunk@3238 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/test/java/com/nhn/pinpoint/profiler/context/CallStackTest.java
+++ src/test/java/com/nhn/pinpoint/profiler/context/CallStackTest.java
@@ -1,5 +1,6 @@
 package com.nhn.pinpoint.profiler.context;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,7 +27,37 @@ public class CallStackTest {
     }
 
     @Test
-    public void testPop() throws Exception {
+    public void testPushPop1() throws Exception {
+        CallStack callStack = new CallStack(new Span());
 
+        callStack.push();
+        callStack.popRoot();
+
+    }
+
+    @Test
+    public void testPushPop2() throws Exception {
+        CallStack callStack = new CallStack(new Span());
+
+        callStack.push();
+        callStack.push();
+
+        callStack.pop();
+        callStack.popRoot();
+
+    }
+
+    @Ignore
+    @Test
+    public void testPop_Fail() throws Exception {
+        CallStack callStack = new CallStack(new Span());
+
+        callStack.push();
+        callStack.push();
+
+        callStack.pop();
+
+        callStack.pop();
+        // pop일때의 인덱스 조정을 다시 생각해 보는게 좋을듯하다.
     }
 }
