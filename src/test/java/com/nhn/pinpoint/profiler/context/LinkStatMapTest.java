12cebdf068f9fb8195a49d452e464c9cf17f3914
Woonduk Kang <wd.kang@navercorp.com>
2014-02-06 10:02:49 +0000

    [강운덕] [PINPOINT-267] 아직 commit안되어야 되는 test가 commit되어 임시 수정.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-tomcat-profiler/trunk@3273 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/test/java/com/nhn/pinpoint/profiler/context/LinkStatMapTest.java
+++ src/test/java/com/nhn/pinpoint/profiler/context/LinkStatMapTest.java
@@ -2,8 +2,6 @@ package com.nhn.pinpoint.profiler.context;
 
 import junit.framework.Assert;
 import org.junit.Test;
-import sun.util.logging.resources.logging_it;
-
 import java.util.List;
 
 /**
@@ -13,10 +11,10 @@ public class LinkStatMapTest {
 
     @Test
     public void testRecord() throws Exception {
-        LinkStatMap map = new LinkStatMap();
-
-        map.recordLink("aa", (short) 0, "test", 0, true);
-        map.recordLink("aa", (short) 0, "test", 0, true);
+//        LinkStatMap map = new LinkStatMap();
+//
+//        map.recordLink("aa", (short) 0, "test", 0, true);
+//        map.recordLink("aa", (short) 0, "test", 0, true);
 //        List<LinkData> all = map.getAll();
 //
 //        Assert.assertEquals(all.size(), 1);
