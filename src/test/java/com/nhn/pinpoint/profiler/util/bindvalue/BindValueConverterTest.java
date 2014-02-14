240e769b415442bb646e97f25913301b5de305ae
Woonduk Kang <wd.kang@navercorp.com>
2014-02-14 04:05:37 +0000

    [강운덕] [PROFILER-1] testcase 추가.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-tomcat-profiler/trunk@3336 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/test/java/com/nhn/pinpoint/profiler/util/bindvalue/BindValueConverterTest.java
+++ src/test/java/com/nhn/pinpoint/profiler/util/bindvalue/BindValueConverterTest.java
@@ -1,5 +1,6 @@
 package com.nhn.pinpoint.profiler.util.bindvalue;
 
+import junit.framework.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -14,5 +15,18 @@ public class BindValueConverterTest {
         byte[] bytes = new byte[] {1, 2, 4};
         String s = Arrays.toString(bytes);
         System.out.println(s);
+    }
+
+    @Test
+    public void testBindValueBoolean() throws Exception {
+        String setBoolean = BindValueConverter.convert("setBoolean", new Object[]{null, Boolean.TRUE});
+        Assert.assertEquals(setBoolean, Boolean.TRUE.toString());
+    }
+
+    @Test
+    public void testBindValueNotSupport() throws Exception {
+        // 지원되지 않는 api 라도 Exception이 발생하면 안됨.
+        String setBoolean = BindValueConverter.convert("setXxxx", new Object[]{null, "XXX"});
+        Assert.assertEquals(setBoolean, "");
     }
 }
