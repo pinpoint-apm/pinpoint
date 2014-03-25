467149f8c0eb6b02bb285a8eaa46dab9eeb171d3
Chisu Yu <puseori9th@gmail.com>
2014-03-25 07:00:08 +0000

    [유치수] [NOBTS] 난독화 스크립트 추가.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-profiler-bootstrap/trunk@3530 84d0f5b1-2673-498c-a247-62c4ff18d310

--- obfuscate.sh
+++ obfuscate.sh
@@ -1,27 +1,58 @@
 HOME="/Users/netspider/Documents/workspace_pinpoint"
 VERSION="1.0.2-SNAPSHOT"
 
-echo "Remove previous"
-rm -fr $HOME/pinpoint-testbed/agent-obfuscated
+echo "Remove previous version"
+#rm -fr $HOME/pinpoint-testbed/agent-obfuscated
 rm -fr $HOME/pinpoint-testbed/agent-org
-mkdir $HOME/pinpoint-testbed/agent-obfuscated
+#mkdir $HOME/pinpoint-testbed/agent-obfuscated
 mkdir $HOME/pinpoint-testbed/agent-org
+echo "...OK"
 
-echo "Backup original"
+echo "Backup original(not obfuscated) profiler"
 cp $HOME/pinpoint-testbed/agent/pinpoint-bootstrap-$VERSION.jar $HOME/pinpoint-testbed/agent-org/
 cp $HOME/pinpoint-testbed/agent/lib/pinpoint-profiler-$VERSION.jar $HOME/pinpoint-testbed/agent-org/
 cp $HOME/pinpoint-testbed/agent/lib/pinpoint-rpc-$VERSION.jar $HOME/pinpoint-testbed/agent-org/
+echo "...OK"
 
 echo "Obfuscating"
 # osx : rt.jar -> /System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Classes/classes.jar
 RT_JAR="/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home/lib/rt.jar"
 proguard.sh -libraryjars $RT_JAR @proguard-$VERSION.conf
+echo "...OK"
 
-echo "Replace original"
+echo "Replace original bootstrap"
 cp $HOME/pinpoint-bootstrap/target/pinpoint-bootstrap-$VERSION-jar-with-dependencies-obfuscated.jar $HOME/pinpoint-testbed/agent/pinpoint-bootstrap-$VERSION.jar
-cp $HOME/pinpoint-profiler/target/pinpoint-profiler-$VERSION-obfuscated.jar $HOME/pinpoint-testbed/agent/lib/pinpoint-profiler-$VERSION.jar
-cp $HOME/pinpoint-rpc/target/pinpoint-rpc-$VERSION-obfuscated.jar $HOME/pinpoint-testbed/agent/lib/pinpoint-rpc-$VERSION.jar
+rc=$?
+if [[ $rc != 0 ]] ; then
+        echo "OBFUSCATION FAILED $rc"
+        exit $rc
+fi
+echo "...OK"
 
-echo "Result is..."
-ls -al $HOME/pinpoint-testbed/agent-obfuscated
+echo "Replace original profiler"
+cp $HOME/pinpoint-profiler/target/pinpoint-profiler-$VERSION-obfuscated.jar $HOME/pinpoint-testbed/agent/lib/pinpoint-profiler-$VERSION.jar
+rc=$?
+if [[ $rc != 0 ]] ; then
+        echo "OBFUSCATION FAILED $rc"
+        exit $rc
+fi
+echo "...OK"
+
+echo "Replace original rpc lib"
+cp $HOME/pinpoint-rpc/target/pinpoint-rpc-$VERSION-obfuscated.jar $HOME/pinpoint-testbed/agent/lib/pinpoint-rpc-$VERSION.jar
+rc=$?
+if [[ $rc != 0 ]] ; then
+        echo "OBFUSCATION FAILED $rc"
+        exit $rc
+fi
+echo "...OK"
+
+echo ""
+echo "Backup agent is ..."
 ls -al $HOME/pinpoint-testbed/agent-org
+
+echo ""
+echo "Obfuscated agent is ..."
+ls -al $HOME/pinpoint-testbed/agent
+
+echo "Everything is OK"
