21329f61930afaf69150f43039a28e223d505b34
Woonduk Kang <wd.kang@navercorp.com>
2014-03-31 09:02:01 +0000

    [강운덕] [WEB-95] DateUtils 튜닝.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-commons/trunk@3583 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/nhn/pinpoint/common/util/DateUtils.java
+++ src/main/java/com/nhn/pinpoint/common/util/DateUtils.java
@@ -1,5 +1,8 @@
 package com.nhn.pinpoint.common.util;
 
+import org.springframework.core.NamedThreadLocal;
+
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -8,17 +11,26 @@ import java.util.Date;
  */
 public final class DateUtils {
 
+    private static final NamedThreadLocal<DateFormat> CACHE = new NamedThreadLocal<DateFormat>(DateUtils.class.getName()) {
+        @Override
+        protected DateFormat initialValue() {
+            return new SimpleDateFormat(FORMAT);
+        }
+    };
+
     private static final String FORMAT = "yyyy-MM-dd HH:mm:ss SSS";
 
     private DateUtils() {
     }
 
     public static String longToDateStr(long date) {
-        return longToDateStr(date, FORMAT);
+        final DateFormat dateFormat = CACHE.get();
+        return dateFormat.format(date);
     }
 
     public static String longToDateStr(long date, String fmt) {
-        final SimpleDateFormat format = new SimpleDateFormat((fmt == null) ? FORMAT : fmt);
+        String pattern = (fmt == null) ? FORMAT : fmt;
+        final SimpleDateFormat format = new SimpleDateFormat(pattern);
         return format.format(new Date(date));
     }
 }
