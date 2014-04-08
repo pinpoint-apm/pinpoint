081cf47da7c9ca7318c73dad31954ddb1787e144
Woonduk Kang <wd.kang@navercorp.com>
2014-04-08 08:49:30 +0000

    [강운덕] [PROFILER-12] Pinpoint Class에서 자기 자신의 jar는 delegation 체크를 하지 않고 스스로 먼저 로드하도록 함.   - 이를 위해 bootstrap 로더에 있는 profiler패키지는 bootstrap로 이동시킴.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-testweb/trunk@3618 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/nhn/pinpointtest/SimpleApp.java
+++ src/main/java/com/nhn/pinpointtest/SimpleApp.java
@@ -5,6 +5,8 @@ public class SimpleApp {
 		HttpCustomServerHandler handler = new HttpCustomServerHandler();
 		System.out.println(handler);
 		handler.messageReceived(null,  null);
-		System.out.println("Hello PinPoint!");
+        System.out.println("Hello PinPoint!---------");
+        Thread.sleep(1000*10);
+		System.out.println("Hello PinPoint!=============");
 	}
 }
