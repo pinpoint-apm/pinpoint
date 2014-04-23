638ffcb18dee880f03adf6c0d93c9adda2d28ce1
Hyun Jeong <hyungil.jeong@navercorp.com>
2014-04-23 10:59:17 +0000

    [정현길] [PROFILER-20] TestClassLoader을 사용하는 JUnit ClassRunner 수정
    CI 빌드 fail 방지 - Pinpoint JUnit ClassRunner 완료 후 다시 add 예정
    
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-tomcat-profiler/trunk@3730 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/test/java/com/nhn/pinpoint/profiler/modifier/orm/ibatis/SqlMapClientImplModifierTest.java
+++ src/test/java/com/nhn/pinpoint/profiler/modifier/orm/ibatis/SqlMapClientImplModifierTest.java
@@ -1,45 +1,12 @@
 package com.nhn.pinpoint.profiler.modifier.orm.ibatis;
 
-import static org.junit.Assert.*;
 
-import java.lang.reflect.Constructor;
-
-import org.junit.Test;
-import org.mockito.Mock;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.ibatis.sqlmap.client.SqlMapClient;
-import com.ibatis.sqlmap.engine.impl.SqlMapClientImpl;
-import com.ibatis.sqlmap.engine.impl.SqlMapExecutorDelegate;
 
 /**
- *
  * @author Hyun Jeong
  */
 public class SqlMapClientImplModifierTest {
 
-	private final Logger logger = LoggerFactory.getLogger(SqlMapClientImplModifierTest.class);
 	
-	@Mock
-	private SqlMapExecutorDelegate mockSqlMapExecutorDelegate;
-
-
-	
-	@Test
-	public void testModify() throws Exception {
-		final String className = SqlMapClientImplModifier.TARGET_CLASS_NAME.replace('/', '.');
-
-
-//		Class<SqlMapClientImpl> sqlMapClientClazz = (Class<SqlMapClientImpl>)loader.loadClass(className);
-//        Class<SqlMapClientImpl> sqlMapClientClazz = (Class<SqlMapClientImpl>)loader.loadClass(className);
-//
-//        Constructor<?>[] constructors = sqlMapClientClazz.getConstructors();
-//		SqlMapClient sqlMapClient = (SqlMapClient)constructors[0].newInstance(mockSqlMapExecutorDelegate);
-
-        SqlMapClient sqlMapClient = new SqlMapClientImpl(mockSqlMapExecutorDelegate);
-//		SqlMapClient sqlMapClient = sqlMapClientClazz.getDeclaredConstructor(SqlMapExecutorDelegate.class).newInstance(mockSqlMapExecutorDelegate);
-		sqlMapClient.queryForList("abc");
-	}
 
 }
