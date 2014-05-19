4b48185d193d65f6ae7f52e5f5f5294e5753d270
Chisu Yu <puseori9th@gmail.com>
2014-05-19 08:06:19 +0000

    [유치수] [web-108] server instance count 추가.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-web/trunk@3884 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/nhn/pinpoint/web/applicationmap/ServerInstanceList.java
+++ src/main/java/com/nhn/pinpoint/web/applicationmap/ServerInstanceList.java
@@ -1,56 +1,68 @@
 package com.nhn.pinpoint.web.applicationmap;
 
-import com.nhn.pinpoint.web.view.ServerInstanceListSerializer;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.nhn.pinpoint.web.view.ServerInstanceListSerializer;
 
 /**
  * @author emeroad
+ * @author netspider
  */
-@JsonSerialize(using=ServerInstanceListSerializer.class)
+@JsonSerialize(using = ServerInstanceListSerializer.class)
 public class ServerInstanceList {
 
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    private final Map<String, List<ServerInstance>> serverInstanceList = new TreeMap<String, List<ServerInstance>>();
+	private final Map<String, List<ServerInstance>> serverInstanceList = new TreeMap<String, List<ServerInstance>>();
 
-    public ServerInstanceList() {
-    }
+	public ServerInstanceList() {
+	}
 
-    public Map<String, List<ServerInstance>> getServerInstanceList() {
-        // list의 소트가 안되 있는 문제가 있음.
-        return serverInstanceList;
-    }
+	public Map<String, List<ServerInstance>> getServerInstanceList() {
+		// list의 소트가 안되 있는 문제가 있음.
+		return serverInstanceList;
+	}
 
+	public int getInstanceCount() {
+		int count = 0;
+		for (Entry<String, List<ServerInstance>> entry : serverInstanceList.entrySet()) {
+			List<ServerInstance> value = entry.getValue();
+			if (value != null) {
+				count += value.size();
+			}
+		}
+		return count;
+	}
 
-    private void addServerInstance(List<ServerInstance> nodeList, ServerInstance serverInstance) {
-        for (ServerInstance  node : nodeList) {
-            boolean equalsNode = node.equals(serverInstance);
-            if (equalsNode) {
-                return;
-            }
-        }
-        nodeList.add(serverInstance);
-    }
+	private void addServerInstance(List<ServerInstance> nodeList, ServerInstance serverInstance) {
+		for (ServerInstance node : nodeList) {
+			boolean equalsNode = node.equals(serverInstance);
+			if (equalsNode) {
+				return;
+			}
+		}
+		nodeList.add(serverInstance);
+	}
 
+	private List<ServerInstance> getServerInstanceList(String hostName) {
+		List<ServerInstance> find = serverInstanceList.get(hostName);
+		if (find == null) {
+			find = new ArrayList<ServerInstance>();
+			serverInstanceList.put(hostName, find);
+		}
+		return find;
+	}
 
-
-    private List<ServerInstance> getServerInstanceList(String hostName) {
-        List<ServerInstance> find = serverInstanceList.get(hostName);
-        if (find == null) {
-            find = new ArrayList<ServerInstance>();
-            serverInstanceList.put(hostName, find);
-        }
-        return find;
-    }
-
-    void addServerInstance(ServerInstance serverInstance) {
-        List<ServerInstance> find = getServerInstanceList(serverInstance.getHostName());
-        addServerInstance(find, serverInstance);
-    }
-
-
+	void addServerInstance(ServerInstance serverInstance) {
+		List<ServerInstance> find = getServerInstanceList(serverInstance.getHostName());
+		addServerInstance(find, serverInstance);
+	}
 }
