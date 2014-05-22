67e4c149fb6346e25612dbdfc5692b6654205e93
Woonduk Kang <wd.kang@navercorp.com>
2014-05-22 01:59:44 +0000

    [강운덕] [PROFILER-31] spanId 의 radom 획득을 쉽게 변경가능하도록 리팩토링.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-tomcat-profiler/trunk@3928 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/nhn/pinpoint/profiler/context/SpanId.java
+++ src/main/java/com/nhn/pinpoint/profiler/context/SpanId.java
@@ -2,6 +2,8 @@ package com.nhn.pinpoint.profiler.context;
 
 import com.nhn.pinpoint.profiler.util.jdk.ThreadLocalRandom;
 
+import java.util.Random;
+
 /**
  * @author emeroad
  */
@@ -13,12 +15,17 @@ public class SpanId {
 
     public static long newSpanId() {
         // thread 마다 가능한 겹치지 않는 값이 생성되면 문제 없으므로 ThreadLocalRandom으로 변경함.
-        final ThreadLocalRandom seed = ThreadLocalRandom.current();
+        final Random random = getRandom();
 
-        return createSpanId(seed);
+        return createSpanId(random);
     }
 
-    private static long createSpanId(ThreadLocalRandom seed) {
+    // 이거 구현 바꾸면 다른 random 사용이 가능함.
+    private static Random getRandom() {
+        return ThreadLocalRandom.current();
+    }
+
+    private static long createSpanId(Random seed) {
         long id = seed.nextLong();
         while (id == NULL) {
             id = seed.nextLong();
@@ -27,7 +34,7 @@ public class SpanId {
     }
 
     public static long nextSpanID(long spanId, long parentSpanId) {
-        final ThreadLocalRandom seed = ThreadLocalRandom.current();
+        final Random seed = getRandom();
 
         long newId = createSpanId(seed);
         while (newId == spanId || newId == parentSpanId) {
