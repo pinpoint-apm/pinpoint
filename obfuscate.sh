c3ac4dd7c0a67fa672de4d8760a6ecdbaf0e7271
Chisu Yu <puseori9th@gmail.com>
2014-06-11 07:06:15 +0000

    [유치수] [profiler-42] 패키지, 난독화 스크립트 수정
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-profiler-bootstrap/trunk@4011 84d0f5b1-2673-498c-a247-62c4ff18d310

--- obfuscate.sh
+++ obfuscate.sh
@@ -1,67 +1,95 @@
+#! /bin/bash
 HOME="/Users/netspider/Documents/workspace_pinpoint"
 VERSION="1.0.2-SNAPSHOT"
 
-echo "Remove previous version"
+echo "** Remove previous version"
 #rm -fr $HOME/pinpoint-testbed/agent-obfuscated
 rm -fr $HOME/pinpoint-testbed/agent-org
 #mkdir $HOME/pinpoint-testbed/agent-obfuscated
 mkdir $HOME/pinpoint-testbed/agent-org
-echo "...OK"
+echo "** ...OK"
 
-echo "Backup original(not obfuscated) profiler"
+echo "** Backup original(not obfuscated) bootstrap"
 cp $HOME/pinpoint-testbed/agent/pinpoint-bootstrap-$VERSION.jar $HOME/pinpoint-testbed/agent-org/
+rc=$?
+if [[ $rc != 0 ]] ; then
+        echo "** OBFUSCATION FAILED $rc"
+        exit $rc
+fi
+echo "** ...OK"
+echo "** Backup original(not obfuscated) profiler"
 cp $HOME/pinpoint-testbed/agent/lib/pinpoint-profiler-$VERSION.jar $HOME/pinpoint-testbed/agent-org/
+rc=$?
+if [[ $rc != 0 ]] ; then
+        echo "** OBFUSCATION FAILED $rc"
+        exit $rc
+fi
+echo "** ...OK"
+echo "** Backup original(not obfuscated) rpc"
 cp $HOME/pinpoint-testbed/agent/lib/pinpoint-rpc-$VERSION.jar $HOME/pinpoint-testbed/agent-org/
-echo "...OK"
+rc=$?
+if [[ $rc != 0 ]] ; then
+        echo "** OBFUSCATION FAILED $rc"
+        exit $rc
+fi
+echo "** ...OK"
 
-echo "Obfuscating"
+echo ""
+echo "** Run Proguard."
 # osx : rt.jar -> /System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Classes/classes.jar
 RT_JAR="/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home/lib/rt.jar"
 proguard.sh -libraryjars $RT_JAR @proguard-$VERSION.conf
-echo "...OK"
-
-echo "Replace original bootstrap"
-cp $HOME/pinpoint-bootstrap/target/pinpoint-bootstrap-$VERSION-jar-with-dependencies-obfuscated.jar $HOME/pinpoint-testbed/agent/pinpoint-bootstrap-$VERSION.jar
 rc=$?
 if [[ $rc != 0 ]] ; then
-        echo "OBFUSCATION FAILED $rc"
+        echo "** OBFUSCATION FAILED $rc"
         exit $rc
 fi
-echo "...OK"
+echo "** ...OK"
+echo ""
 
-echo "Replace original profiler"
-cp $HOME/pinpoint-profiler/target/pinpoint-profiler-$VERSION-obfuscated.jar $HOME/pinpoint-testbed/agent/lib/pinpoint-profiler-$VERSION.jar
+echo "** Replace original bootstrap"
+cp $HOME/pinpoint-bootstrap/target/pinpoint-bootstrap-$VERSION-obfuscated.jar $HOME/pinpoint-testbed/agent/pinpoint-bootstrap-$VERSION.jar
 rc=$?
 if [[ $rc != 0 ]] ; then
-        echo "OBFUSCATION FAILED $rc"
+        echo "** OBFUSCATION FAILED $rc"
         exit $rc
 fi
-echo "...OK"
+echo "** ...OK"
 
-echo "Replace original rpc lib"
+echo "** Replace original profiler"
+cp $HOME/pinpoint-profiler/target/pinpoint-agent/lib/pinpoint-profiler-$VERSION-obfuscated.jar $HOME/pinpoint-testbed/agent/lib/pinpoint-profiler-$VERSION.jar
+rc=$?
+if [[ $rc != 0 ]] ; then
+        echo "** OBFUSCATION FAILED $rc"
+        exit $rc
+fi
+echo "** ...OK"
+
+echo "** Replace original rpc lib"
 cp $HOME/pinpoint-rpc/target/pinpoint-rpc-$VERSION-obfuscated.jar $HOME/pinpoint-testbed/agent/lib/pinpoint-rpc-$VERSION.jar
 rc=$?
 if [[ $rc != 0 ]] ; then
-        echo "OBFUSCATION FAILED $rc"
+        echo "** OBFUSCATION FAILED $rc"
         exit $rc
 fi
-echo "...OK"
+echo "** ...OK"
 
-echo "Copy mapping file"
+echo "** Copy mapping file"
 cp $HOME/pinpoint-bootstrap/target/agent.map $HOME/pinpoint-testbed/agent/agent.map
 rc=$?
 if [[ $rc != 0 ]] ; then
-        echo "OBFUSCATION FAILED $rc"
+        echo "** OBFUSCATION FAILED $rc"
         exit $rc
 fi
-echo "...OK"
+echo "** ...OK"
 
 echo ""
-echo "Backup agent is ..."
+echo "** Original backup files"
 ls -lR $HOME/pinpoint-testbed/agent-org
 
 echo ""
-echo "Obfuscated agent is ..."
+echo "** Obfuscated agent files"
 ls -lR $HOME/pinpoint-testbed/agent
 
-echo "Everything is OK"
+echo ""
+echo "** Everything is OK"
