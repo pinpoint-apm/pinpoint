8dd29eea9af434ac6b28f853d029c66ed5e90cb8
Woonduk Kang <wd.kang@navercorp.com>
2014-06-16 08:07:14 +0000

    [강운덕] [PROFILER-39] TargetClassLoader를 명시하지 않은 부분이 있어 수정함.
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-tomcat-profiler/trunk@4024 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/nhn/pinpoint/profiler/modifier/connector/npc/interceptor/CreateConnectorInterceptor.java
+++ src/main/java/com/nhn/pinpoint/profiler/modifier/connector/npc/interceptor/CreateConnectorInterceptor.java
@@ -2,20 +2,17 @@ package com.nhn.pinpoint.profiler.modifier.connector.npc.interceptor;
 
 import java.net.InetSocketAddress;
 
+import com.nhn.pinpoint.bootstrap.interceptor.*;
 import com.nhn.pinpoint.common.AnnotationKey;
 import com.nhn.pinpoint.common.ServiceType;
 import com.nhn.pinpoint.bootstrap.context.Trace;
 import com.nhn.pinpoint.bootstrap.context.TraceContext;
 import com.nhn.pinpoint.bootstrap.context.TraceId;
-import com.nhn.pinpoint.bootstrap.interceptor.ByteCodeMethodDescriptorSupport;
-import com.nhn.pinpoint.bootstrap.interceptor.MethodDescriptor;
-import com.nhn.pinpoint.bootstrap.interceptor.SimpleAroundInterceptor;
-import com.nhn.pinpoint.bootstrap.interceptor.TraceContextSupport;
 import com.nhn.pinpoint.bootstrap.logging.PLogger;
 import com.nhn.pinpoint.bootstrap.logging.PLoggerFactory;
 import com.nhncorp.lucy.npc.connector.NpcConnectorOption;
 
-public class CreateConnectorInterceptor implements SimpleAroundInterceptor, ByteCodeMethodDescriptorSupport, TraceContextSupport {
+public class CreateConnectorInterceptor implements SimpleAroundInterceptor, ByteCodeMethodDescriptorSupport, TraceContextSupport, TargetClassLoader {
 
 	private final PLogger logger = PLoggerFactory.getLogger(this.getClass());
 	private final boolean isDebug = logger.isDebugEnabled();
