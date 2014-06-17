8178204645ad1f6b3f1792a0ccb023f5c2e5a451
Denny Lim <hi.iamdenny@gmail.com>
2014-06-17 06:16:11 +0000

    [임대현] #166 [transactionView] 3축 차트, amChart로 변경
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-web/trunk@4033 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/webapp/scripts/directives/agentChartGroup.js
+++ src/main/webapp/scripts/directives/agentChartGroup.js
@@ -60,55 +60,6 @@ pinpointApp.directive('agentChartGroup', [ 'agentChartGroupConfig', '$timeout', 
                 element.tabs('paging');
             };
 
-            /**
-             * parse agent stat
-             * @param agentData
-             * @param agentStat
-             * @returns {*}
-             */
-            parseAgentStat = function (agentData, agentStat) {
-                agentData.line.forEach(function (line) {
-                    if (line.bar) {
-                        // bar chart
-                        var pointsTime = agentStat.charts.jvmGcOldTime.points;
-                        var pointsCount = agentStat.charts.jvmGcOldCount.points;
-
-                        if (pointsTime.length !== pointsCount.length) {
-                            console.log('assertion error', 'time.length != count.length');
-                            return;
-                        }
-
-                        for (var i = pointsCount.length - 1; i >= 0; --i) {
-                            var timestamp = pointsTime[i][cfg.POINTS_TIMESTAMP];
-                            var currTime = pointsTime[i][cfg.POINTS_MAX];
-                            var currCount = pointsCount[i][cfg.POINTS_MAX];
-                            var prevTime = line.prevTime;
-                            var prevCount = line.prevCount;
-
-                            if (!line.prevTime || !line.prevCount) {
-                                line.values.push({x: timestamp, y: 0});
-                                line.prevTime = currTime;
-                                line.prevCount = currCount;
-                            } else {
-                                if ((currCount - prevCount > 0) && (currTime - prevTime > 0)) {
-                                    line.values.push({x: timestamp, y: currTime - prevTime});
-                                    line.prevTime = currTime;
-                                    line.prevCount = currCount;
-                                } else {
-                                    line.values.push({x: timestamp, y: 0});
-                                }
-                            }
-                        }
-                    } else {
-                        // line chart
-                        var points = agentStat.charts[line.id].points;
-                        for (var j = points.length - 1; j >= 0; --j) {
-                            line.values.push({x: points[j][cfg.POINTS_TIMESTAMP], y: points[j][cfg.POINTS_MAX]});
-                        }
-                    }
-                });
-                return agentData;
-            };
 
             /**
              * show heap chart
@@ -117,20 +68,12 @@ pinpointApp.directive('agentChartGroup', [ 'agentChartGroupConfig', '$timeout', 
             showHeapChart = function (agentStat) {
                 htChartCache.Heap = true;
                 var heap = { id: 'heap', title: 'Heap', span: 'span12', line: [
-                    { id: 'jvmMemoryHeapUsed', key: 'used', values: [] },
-                    { id: 'jvmMemoryHeapMax', key: 'max', values: [] },
-                    { id: 'gc', key: 'GC', values: [], bar: true }
+                    { id: 'jvmMemoryHeapUsed', key: 'Used', values: [], isFgc: false },
+                    { id: 'jvmMemoryHeapMax', key: 'Max', values: [], isFgc: false },
+                    { id: 'fgc', key: 'FGC', values: [], isFgc: true }
                 ]};
 
-                heap = parseAgentStat(heap, agentStat);
-
-                var oLinePlusBarChartVo = {
-                    height: '150px',
-                    y1AxisLabel: 'GC elapsed time (ms)',
-                    margin: {top: 0, right: 60, bottom: 20, left: 70},
-                    datum: heap.line
-                };
-                scope.$broadcast('linePlusBarChart.initialize.Heap_' + scope.namespace, oLinePlusBarChartVo);
+                scope.$broadcast('jvmMemoryChart.initAndRenderWithData.forHeap_' + scope.namespace, AgentDao.parseChartDataForAmcharts(heap, agentStat), '100%', '100%');
             };
 
             /**
@@ -140,20 +83,12 @@ pinpointApp.directive('agentChartGroup', [ 'agentChartGroupConfig', '$timeout', 
             showPermGenChart = function (agentStat) {
                 htChartCache.PermGen = true;
                 var nonheap = { id: 'nonheap', title: 'PermGen', span: 'span12', line: [
-                    { id: 'jvmMemoryNonHeapUsed', key: 'used', values: [] },
-                    { id: 'jvmMemoryNonHeapMax', key: 'max', values: [] },
-                    { id: 'gc', key: 'GC', values: [], bar: true }
+                    { id: 'jvmMemoryNonHeapUsed', key: 'Used', values: [], isFgc: false },
+                    { id: 'jvmMemoryNonHeapMax', key: 'Max', values: [], isFgc: false },
+                    { id: 'fgc', key: 'FGC', values: [], isFgc: true }
                 ]};
 
-                nonheap = parseAgentStat(nonheap, agentStat);
-
-                var oLinePlusBarChartVo = {
-                    height: '150px',
-                    y1AxisLabel: 'GC elapsed time (ms)',
-                    margin: {top: 0, right: 60, bottom: 20, left: 70},
-                    datum: nonheap.line
-                };
-                scope.$broadcast('linePlusBarChart.initialize.PermGen_' + scope.namespace, oLinePlusBarChartVo);
+                scope.$broadcast('jvmMemoryChart.initAndRenderWithData.forNonHeap_' + scope.namespace, AgentDao.parseChartDataForAmcharts(nonheap, agentStat), '100%', '100%');
             };
 
             /**
