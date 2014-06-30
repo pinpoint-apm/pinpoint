c968b7fece37fa6352a0afdf5af52b9b4b4fa2dc
Chisu Yu <puseori9th@gmail.com>
2014-06-30 07:15:30 +0000

    [유치수] [NOBTS] 중복된 로그 제거
    git-svn-id: http://svn.bds.nhncorp.com/pe/hippo-tomcat-profiler/trunk@4100 84d0f5b1-2673-498c-a247-62c4ff18d310

--- src/main/java/com/nhn/pinpoint/profiler/modifier/bloc/handler/interceptor/ExecuteMethodInterceptor.java
+++ src/main/java/com/nhn/pinpoint/profiler/modifier/bloc/handler/interceptor/ExecuteMethodInterceptor.java
@@ -57,27 +57,23 @@ public class ExecuteMethodInterceptor implements SimpleAroundInterceptor, ByteCo
             TraceId traceId = populateTraceIdFromRequest(request);
             Trace trace;
             if (traceId != null) {
-                if (isDebug) {
-                    logger.debug("TraceID exist. continue trace. {} requestUrl:{}, remoteAddr:{}", new Object[]{traceId, requestURL, remoteAddr});
-                }
                 trace = traceContext.continueTraceObject(traceId);
                 if (!trace.canSampled()) {
                     if (isDebug) {
                         logger.debug("TraceID exist. camSampled is false. skip trace. traceId:{}, requestUrl:{}, remoteAddr:{}", new Object[]{traceId, requestURL, remoteAddr});
-                        return;
                     }
+                    return;
                 } else {
                     if (isDebug) {
                         logger.debug("TraceID exist. continue trace. traceId:{}, requestUrl:{}, remoteAddr:{}", new Object[]{traceId, requestURL, remoteAddr});
                     }
                 }
             } else {
-                if (isDebug) {
-                    logger.debug("TraceID not exist. start new trace. {} requestUrl:{}, remoteAddr:{}", new Object[]{traceId, requestURL, remoteAddr});
-                }
                 trace = traceContext.newTraceObject();
                 if (!trace.canSampled()){
-                    logger.debug("TraceID not exist. camSampled is false. skip trace. requestUrl:{}, remoteAddr:{}", new Object[]{requestURL, remoteAddr});
+                	if (isDebug) {
+                		logger.debug("TraceID not exist. camSampled is false. skip trace. requestUrl:{}, remoteAddr:{}", new Object[]{requestURL, remoteAddr});
+                	}
                     return;
                 } else {
                     if (isDebug) {
