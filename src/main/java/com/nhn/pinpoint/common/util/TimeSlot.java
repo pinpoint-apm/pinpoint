a92719d1a09b3ef531fc3a220c23b24950243507
Woonduk Kang <wd.kang@navercorp.com>
2014-07-30 13:44:20 +0900

    [COMMON-3] timeSlot의 static api를 제거함.

--- src/main/java/com/nhn/pinpoint/common/util/TimeSlot.java
+++ src/main/java/com/nhn/pinpoint/common/util/TimeSlot.java
@@ -1,15 +1,8 @@
 package com.nhn.pinpoint.common.util;
 
 /**
- * 
- * @author netspider
  * @author emeroad
  */
-public class TimeSlot {
-	private static final int RESOLUTION =  60000; // 1min
-
-	public static long getStatisticsRowSlot(long time) {
-        // 과거 time을 기준으로 얻어오나, 모두 동일하게 과거 시간의 슬롯을 얻어오게 되므로 + RESOLUTION을 하지 않아도 된다.
-		return (time / RESOLUTION) * RESOLUTION;
-	}
+public interface TimeSlot {
+    public long getTimeSlot(long time);
 }
--- src/test/java/com/nhn/pinpoint/common/util/TimeSlotTest.java
+++ src/test/java/com/nhn/pinpoint/common/util/TimeSlotTest.java
@@ -12,15 +12,42 @@ public class TimeSlotTest {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    private final TimeSlot timeSlot = new DefaultTimeSlot();
+
     @Test
-    public void testGetStatisticsRowSlot() throws Exception {
+    public void testGetTimeSlot() throws Exception {
         long currentTime = System.currentTimeMillis();
         // 슬롯 넘버를 알아온다.
-        long statisticsRowSlot = TimeSlot.getStatisticsRowSlot(currentTime);
+        long timeSlot = this.timeSlot.getTimeSlot(currentTime);
 
         logger.info("{} currentTime ", currentTime);
-        logger.info("{} statisticsRowSlot", statisticsRowSlot);
-        Assert.assertTrue(currentTime> statisticsRowSlot);
-
+        logger.info("{} timeSlot", timeSlot);
+        Assert.assertTrue(currentTime >= timeSlot);
      }
+
+    @Test
+    public void testSlotTime1() throws Exception {
+        int slotTest = 60 * 1000;
+
+        // 슬롯 넘버를 알아온다.
+        long timeSlot = this.timeSlot.getTimeSlot(slotTest);
+
+        logger.info("{} slotTest ", slotTest);
+        logger.info("{} timeSlot", timeSlot);
+        Assert.assertEquals(slotTest, timeSlot);
+    }
+
+    @Test
+    public void testSlotTime2() throws Exception {
+        int sourceTest = 60 * 1000;
+        int slotTest = sourceTest + 1;
+
+
+        // 슬롯 넘버를 알아온다.
+        long timeSlot = this.timeSlot.getTimeSlot(slotTest);
+
+        logger.info("{} slotTest ", slotTest);
+        logger.info("{} timeSlot", timeSlot);
+        Assert.assertEquals(sourceTest, timeSlot);
+    }
 }
