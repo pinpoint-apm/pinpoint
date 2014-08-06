b6c0275a7c2dd1a30a7f099c249c397ed493e442
Hyun Jeong <hyungil.jeong@navercorp.com>
2014-08-06 17:30:28 +0900

    [#73] AgentStat 모아 보내기 구현

--- src/main/java/com/nhn/pinpoint/profiler/monitor/AgentStatMonitor.java
+++ src/main/java/com/nhn/pinpoint/profiler/monitor/AgentStatMonitor.java
@@ -1,5 +1,7 @@
 package com.nhn.pinpoint.profiler.monitor;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -7,7 +9,8 @@ import java.util.concurrent.TimeUnit;
 import com.nhn.pinpoint.profiler.monitor.codahale.AgentStatCollectorFactory;
 import com.nhn.pinpoint.profiler.monitor.codahale.cpu.CpuLoadCollector;
 import com.nhn.pinpoint.profiler.monitor.codahale.gc.GarbageCollector;
-import com.nhn.pinpoint.thrift.dto.TAgentStat;
+import com.nhn.pinpoint.thrift.dto.TAgentStatBatch;
+import com.nhn.pinpoint.thrift.dto.TAgentStatSingle;
 import com.nhn.pinpoint.thrift.dto.TCpuLoad;
 import com.nhn.pinpoint.thrift.dto.TJvmGc;
 
@@ -25,11 +28,13 @@ import com.nhn.pinpoint.profiler.sender.DataSender;
  */
 public class AgentStatMonitor {
 
-    private static final long DEFAULT_INTERVAL = 1000 * 5;
+    private static final long DEFAULT_COLLECTION_INTERVAL_MS = 1000 * 5;
+    private static final int DEFAULT_NUM_COLLECTIONS_PER_SEND = 6;
 
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private final boolean isTrace = logger.isTraceEnabled();
-
+    private final long collectionIntervalMs;
+    private final int numCollectionsPerBatch;
 
 	private final ScheduledExecutorService executor = new ScheduledThreadPoolExecutor(1, new PinpointThreadFactory("Pinpoint-stat-monitor", true));
 
@@ -41,6 +46,10 @@ public class AgentStatMonitor {
     private final long agentStartTime;
 
     public AgentStatMonitor(DataSender dataSender, String agentId, long startTime) {
+    	this(dataSender, agentId, startTime, DEFAULT_COLLECTION_INTERVAL_MS, DEFAULT_NUM_COLLECTIONS_PER_SEND);
+    }
+
+    public AgentStatMonitor(DataSender dataSender, String agentId, long startTime, long collectionInterval, int numCollectionsPerBatch) {
         if (dataSender == null) {
             throw new NullPointerException("dataSender must not be null");
         }
@@ -50,6 +59,8 @@ public class AgentStatMonitor {
         this.dataSender = dataSender;
         this.agentId = agentId;
         this.agentStartTime = startTime;
+        this.collectionIntervalMs = collectionInterval;
+        this.numCollectionsPerBatch = numCollectionsPerBatch;
         this.agentStatCollectorFactory = createAgentStatCollectorFactory();
         this.garbageCollector = agentStatCollectorFactory.getGarbageCollector();
         this.cpuLoadCollector = agentStatCollectorFactory.getCpuLoadCollector();
@@ -63,12 +74,9 @@ public class AgentStatMonitor {
     }
 
 	public void start() {
-		CollectJob job = new CollectJob();
-		// FIXME 설정에서 수집 주기를 가져올 수 있어야 한다.
-		long interval = DEFAULT_INTERVAL;
 		long wait = 0;
-		
-		executor.scheduleAtFixedRate(job, wait, interval, TimeUnit.MILLISECONDS);
+		CollectJob job = new CollectJob(this.numCollectionsPerBatch);
+		executor.scheduleAtFixedRate(job, wait, this.collectionIntervalMs, TimeUnit.MILLISECONDS);
 		logger.info("AgentStat monitor started");
 	}
 
@@ -83,27 +91,63 @@ public class AgentStatMonitor {
 	}
 
     private class CollectJob implements Runnable {
+    	
+    	// 어차피 한 개의 쓰레드에서 한 개의 객체만 사용.
+    	// 멀티쓰레드로 돌릴 일이 생긴다면 바꿔야 함. (그럴 일은 없을 것 같음)
+    	private final int numStatsPerBatch;
+    	private int collectCount = 0;
+    	private List<TAgentStatSingle> agentStats;
+    	
+    	private CollectJob(int numStatsPerBatch) {
+    		this.numStatsPerBatch = numStatsPerBatch;
+    		this.agentStats = new ArrayList<TAgentStatSingle>(this.numStatsPerBatch);
+    	}
 
         public void run() {
 			try {
-                // TAgentStat 객체를 준비한다.
-                // TODO TAgentStat을 재활용시 datasender가 별도의 thread이기 때문에. multithread문제가 생길수 있음.
-                final TAgentStat agentStat = new TAgentStat();
-                agentStat.setAgentId(agentId);
-                agentStat.setStartTimestamp(agentStartTime);
-				agentStat.setTimestamp(System.currentTimeMillis());
-                final TJvmGc gc = garbageCollector.collect();
-                agentStat.setGc(gc);
-                final TCpuLoad cpuLoad = cpuLoadCollector.collectCpuLoad();
-                agentStat.setCpuLoad(cpuLoad);
-                if (isTrace) {
-                    logger.trace("collect agentStat:{}", agentStat);
-                }
-                dataSender.send(agentStat);
+				final TAgentStatSingle agentStatSingle = collectAgentStat();
+				this.agentStats.add(agentStatSingle);
+				if (++this.collectCount >= this.numStatsPerBatch) {
+					sendAgentStats();
+					resetBatchCount();
+				}
 			} catch (Exception ex) {
 				logger.warn("AgentStat collect failed. Caused:{}", ex.getMessage(), ex);
 			}
 		}
+    	
+    	private TAgentStatSingle collectAgentStat() {
+			final TAgentStatSingle agentStatSingle = new TAgentStatSingle();
+			agentStatSingle.setTimeStamp(System.currentTimeMillis());
+			final TJvmGc gc = garbageCollector.collect();
+			agentStatSingle.setGc(gc);
+			final TCpuLoad cpuLoad = cpuLoadCollector.collectCpuLoad();
+			agentStatSingle.setCpuLoad(cpuLoad);
+			if (isTrace) {
+				logger.trace("collect agentStat:{}", agentStatSingle);
+			}
+			return agentStatSingle;
+    	}
+    	
+    	private void sendAgentStats() {
+            // TAgentStat 객체를 준비한다.
+            // TODO TAgentStat을 재활용시 datasender가 별도의 thread이기 때문에. multithread문제가 생길수 있음.
+			final TAgentStatBatch agentStatBatch = new TAgentStatBatch();
+			agentStatBatch.setAgentId(agentId);
+			agentStatBatch.setStartTimestamp(agentStartTime);
+			agentStatBatch.setAgentStats(this.agentStats);
+            if (isTrace) {
+	            logger.trace("collect agentStat:{}", agentStatBatch);
+	        }
+            dataSender.send(agentStatBatch);
+    	}
+    	
+    	private void resetBatchCount() {
+            this.collectCount = 0;
+            // 위와 마찬가지로 agentStats 리스트 재활용시, datasender가 별도의 thread이기 때문에, 
+            // send하기 전에 리스트가 변경될 수 있음. 따라서 새로운 리스트를 만들어준다.
+            this.agentStats = new ArrayList<TAgentStatSingle>(this.numStatsPerBatch);
+    	}
 	}
 
 }
--- src/test/java/com/nhn/pinpoint/profiler/monitor/AgentStatMonitorTest.java
+++ src/test/java/com/nhn/pinpoint/profiler/monitor/AgentStatMonitorTest.java
@@ -1,23 +1,51 @@
 package com.nhn.pinpoint.profiler.monitor;
 
-import com.nhn.pinpoint.profiler.sender.LoggingDataSender;
-import org.junit.Test;
+import static org.junit.Assert.*;
 
+import com.nhn.pinpoint.profiler.sender.PeekableDataSender;
+import com.nhn.pinpoint.thrift.dto.TAgentStatBatch;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.MockitoAnnotations;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author hyungil.jeong
+ */
 public class AgentStatMonitorTest {
 
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	private PeekableDataSender<TAgentStatBatch> peekableDataSender;
+
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+		this.peekableDataSender = new PeekableDataSender<TAgentStatBatch>();
+	}
 
 	@Test
-	public void test() throws InterruptedException {
-
+	public void testAgentStatMonitor() throws InterruptedException {
+		// Given
+		final long collectionIntervalMs = 1000 * 1;
+		final int numCollectionsPerBatch = 2;
+		final int minNumBatchToTest = 2;
+		final long totalTestDurationMs = collectionIntervalMs * numCollectionsPerBatch * minNumBatchToTest;
+		// When
 		System.setProperty("pinpoint.log", "test.");
-		AgentStatMonitor monitor = new AgentStatMonitor(new LoggingDataSender(), "agentId", System.currentTimeMillis());
-
+		AgentStatMonitor monitor = new AgentStatMonitor(this.peekableDataSender, "agentId", System.currentTimeMillis(), collectionIntervalMs,
+				numCollectionsPerBatch);
 		monitor.start();
-
-        Thread.sleep(100);
-
-        monitor.stop();
-
+		Thread.sleep(totalTestDurationMs);
+		monitor.stop();
+		// Then
+		assertTrue(peekableDataSender.size() >= minNumBatchToTest);
+		for (TAgentStatBatch agentStatBatch : peekableDataSender) {
+	        logger.debug("agentStatBatch:{}", agentStatBatch);
+			assertTrue(agentStatBatch.getAgentStats().size() <= numCollectionsPerBatch);
+		}
 	}
 
 }
