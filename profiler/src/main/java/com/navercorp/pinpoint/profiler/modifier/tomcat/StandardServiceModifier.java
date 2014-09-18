127165ff794ea9c4dd3ef9b8fc275cd5aeb67393
Hyun Jeong <hyungil.jeong@navercorp.com>
2014-09-18 13:07:06 +0900

    [#14] Changed Tomcat start event hook to when StandardService starts.

---
+++ profiler/src/main/java/com/navercorp/pinpoint/profiler/modifier/tomcat/StandardServiceModifier.java
@@ -0,0 +1,72 @@
+package com.nhn.pinpoint.profiler.modifier.tomcat;
+
+import java.security.ProtectionDomain;
+
+import com.nhn.pinpoint.bootstrap.Agent;
+import com.nhn.pinpoint.bootstrap.interceptor.Interceptor;
+import com.nhn.pinpoint.profiler.LifeCycleEventListener;
+import com.nhn.pinpoint.profiler.interceptor.bci.ByteCodeInstrumentor;
+import com.nhn.pinpoint.profiler.interceptor.bci.InstrumentClass;
+import com.nhn.pinpoint.profiler.modifier.AbstractModifier;
+import com.nhn.pinpoint.profiler.modifier.tomcat.interceptor.StandardServiceStartInterceptor;
+import com.nhn.pinpoint.profiler.modifier.tomcat.interceptor.StandardServiceStopInterceptor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author cowboy93, netspider
+ * @author hyungil.jeong
+ */
+public class StandardServiceModifier extends AbstractModifier {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public StandardServiceModifier(ByteCodeInstrumentor byteCodeInstrumentor, Agent agent) {
+        super(byteCodeInstrumentor, agent);
+    }
+
+    public String getTargetClass() {
+        return "org/apache/catalina/core/StandardService";
+    }
+
+    @Override
+    public byte[] modify(ClassLoader classLoader, String javassistClassName, ProtectionDomain protectedDomain, byte[] classFileBuffer) {
+        if (logger.isInfoEnabled()) {
+            logger.info("Modifying. {}", javassistClassName);
+        }
+//        byteCodeInstrumentor.checkLibrary(classLoader, javassistClassName);
+
+        try {
+            InstrumentClass standardService = byteCodeInstrumentor.getClass(javassistClassName);
+
+            LifeCycleEventListener lifeCycleEventListener = new LifeCycleEventListener(agent);
+            Interceptor standardServiceStartInterceptor = new StandardServiceStartInterceptor(lifeCycleEventListener);
+            Interceptor standardServiceStopInterceptor = new StandardServiceStopInterceptor(lifeCycleEventListener);
+
+            boolean isHooked = false;
+            // Tomcat 6 - org.apache.catalina.core.StandardService.start(), stop()
+            if (isHooked = (standardService.hasDeclaredMethod("start", null) && standardService.hasDeclaredMethod("stop", null))) {
+                standardService.addInterceptor("start", null, standardServiceStartInterceptor);
+                standardService.addInterceptor("stop", null, standardServiceStopInterceptor);
+            }
+            // Tomcat 7, 8 - org.apache.catalina.core.StandardService.startInternal(), stopInternal()
+            else if (isHooked = (standardService.hasDeclaredMethod("startInternal", null) && standardService.hasDeclaredMethod("stopInternal", null))) {
+                standardService.addInterceptor("startInternal", null, standardServiceStartInterceptor);
+                standardService.addInterceptor("stopInternal", null, standardServiceStopInterceptor);
+            }
+            
+            if (isHooked) {
+                logger.info("{} class is converted.", javassistClassName);
+            } else {
+                logger.warn("{} class not converted - start() or startInternal() method not found.", javassistClassName);
+            }
+            return standardService.toBytecode();
+        } catch (Exception e) {
+            if (logger.isWarnEnabled()) {
+                logger.warn("modify fail. Cause:" + e.getMessage(), e);
+            }
+        }
+        return null;
+    }
+}
