38da1174342ec5132dfb2f11e525160e9bf35696
Woonduk Kang <wd.kang@navercorp.com>
2014-09-24 13:29:04 +0900

    [#10] add mssqlserver test, testcase refactoring

---
+++ testweb/src/main/java/com/nhn/pinpoint/testweb/repository/MsSqlServerDaoIbatis.java
@@ -0,0 +1,58 @@
+package com.nhn.pinpoint.testweb.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.orm.ibatis.SqlMapClientTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ */
+@Repository
+public class MsSqlServerDaoIbatis implements MsSqlServerDao {
+
+    @Autowired
+    @Qualifier("mysqlSqlMapClientTemplate")
+    private SqlMapClientTemplate sqlMapClientTemplate;
+
+    @Autowired
+    @Qualifier("jtdsDataSource")
+    private DataSource datasource;
+
+    @Override
+    public int selectOne() {
+        return (Integer) sqlMapClientTemplate.queryForObject("selectOne");
+    }
+
+    @Override
+    public boolean createStatement() {
+        Connection connection = null;
+        Statement statement = null;
+        try {
+            connection = datasource.getConnection();
+            statement = connection.createStatement();
+            return statement.execute("select 1");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                }
+            }
+
+        }
+    }
+}
