b5b7d8920d7d20c2a99e1b7712a293b6cd696517
Woonduk Kang <wd.kang@navercorp.com>
2014-09-25 11:51:27 +0900

    remove unused script

--- thrift/build.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-mvn clean eclipse:eclipse package dependency:copy-dependencies install -Dmaven.test.skip
-
-rc=$?
-if [[ $rc != 0 ]] ; then
-        echo "BUILD FAILED $rc"
-        exit $rc
-fi
