20e2803f923e0a97b5575a28febb5f430b4b6b86
Woonduk Kang <wd.kang@navercorp.com>
2014-09-25 15:51:23 +0900

    code clean up, remove unused code, script

--- collector/runscript/start.sh
+++
@@ -1,4 +0,0 @@
-JMX_OPT="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9999 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
-java -Xms2g -Xmx2g -classpath `find . -name '*.jar' | tr "\n" :` $JMX_OPT com.nhn.pinpoint.collector.Server >> collector.log &
-sleep 0.3
-tail -f collector.log
