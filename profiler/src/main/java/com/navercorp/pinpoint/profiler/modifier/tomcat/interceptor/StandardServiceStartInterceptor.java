6c8d14d2a4277efd8e30d4211def8f5ee860013b
Hyun Jeong <hyungil.jeong@navercorp.com>
2014-09-25 15:26:58 +0900

    [#15] Added Tomcat server info collection

--- profiler/src/main/java/com/navercorp/pinpoint/profiler/modifier/tomcat/interceptor/StandardServiceStartInterceptor.java
+++ profiler/src/main/java/com/navercorp/pinpoint/profiler/modifier/tomcat/interceptor/StandardServiceStartInterceptor.java
@@ -2,9 +2,11 @@ package com.nhn.pinpoint.profiler.modifier.tomcat.interceptor;
 
 import org.apache.catalina.util.ServerInfo;
 
+import com.nhn.pinpoint.bootstrap.context.TraceContext;
 import com.nhn.pinpoint.bootstrap.interceptor.LifeCycleEventListener;
 import com.nhn.pinpoint.bootstrap.interceptor.SimpleAroundInterceptor;
 import com.nhn.pinpoint.bootstrap.interceptor.TargetClassLoader;
+import com.nhn.pinpoint.bootstrap.interceptor.TraceContextSupport;
 import com.nhn.pinpoint.bootstrap.logging.PLogger;
 import com.nhn.pinpoint.bootstrap.logging.PLoggerFactory;
 
@@ -12,10 +14,12 @@ import com.nhn.pinpoint.bootstrap.logging.PLoggerFactory;
  * @author emeroad
  * @author hyungil.jeong
  */
-public class StandardServiceStartInterceptor implements SimpleAroundInterceptor, TargetClassLoader {
+public class StandardServiceStartInterceptor implements SimpleAroundInterceptor, TraceContextSupport, TargetClassLoader {
 
     private final PLogger logger = PLoggerFactory.getLogger(this.getClass());
     private final boolean isDebug = logger.isDebugEnabled();
+
+    private TraceContext traceContext;
 
     private LifeCycleEventListener lifeCycleEventListener;
 
@@ -33,11 +37,17 @@ public class StandardServiceStartInterceptor implements SimpleAroundInterceptor,
         if (isDebug) {
             logger.afterInterceptor(target, args, result, throwable);
         }
-        String tomcatInfo = ServerInfo.getServerInfo();
-        logger.info("Tomcat Version : {}", tomcatInfo);
-        // if (!InterceptorUtils.isSuccess(result)) {
-        // return;
-        // }
+        populateMetaData();
         lifeCycleEventListener.start();
+    }
+
+    private void populateMetaData() {
+        String serverInfo = ServerInfo.getServerInfo();
+        this.traceContext.getServerMetaDataHolder().setServerName(serverInfo);
+    }
+
+    @Override
+    public void setTraceContext(TraceContext traceContext) {
+        this.traceContext = traceContext;
     }
 }
