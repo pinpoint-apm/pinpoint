bb93b958ff2577569ae50fb11fc007158c6e37d3
koo-taejin <koo.taejin@navercorp.com>
2014-12-01 16:49:07 +0900

    #11 added ClassUtils license.

--- rpc/src/main/java/com/navercorp/pinpoint/rpc/util/ClassUtils.java
+++ rpc/src/main/java/com/navercorp/pinpoint/rpc/util/ClassUtils.java
@@ -1,10 +1,42 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.nhn.pinpoint.rpc.util;
 
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * @author koo.taejin
+ * <p>Operates on classes without using reflection.</p>
+ *
+ * <p>This class handles invalid <code>null</code> inputs as best it can.
+ * Each method documents its behaviour in more detail.</p>
+ *
+ * <p>The notion of a <code>canonical name</code> includes the human
+ * readable name for the type, for example <code>int[]</code>. The
+ * non-canonical method variants work with the JVM names, such as
+ * <code>[I</code>. </p>
+ *
+ * @author Apache Software Foundation
+ * @author Gary Gregory
+ * @author Norm Deane
+ * @author Alban Peignier
+ * @author Tomasz Blachowicz
+ * @since 2.0
+ * @version $Id: ClassUtils.java 907121 2010-02-05 22:53:21Z mbenson $
  */
 public class ClassUtils {
 
@@ -32,10 +64,76 @@ public class ClassUtils {
         }
     }
 
+    // Is assignable
+    // ----------------------------------------------------------------------
+    /**
+     * <p>Checks if an array of Classes can be assigned to another array of Classes.</p>
+     *
+     * <p>This method calls {@link #isAssignable(Class, Class) isAssignable} for each
+     * Class pair in the input arrays. It can be used to check if a set of arguments
+     * (the first parameter) are suitably compatible with a set of method parameter types
+     * (the second parameter).</p>
+     *
+     * <p>Unlike the {@link Class#isAssignableFrom(java.lang.Class)} method, this
+     * method takes into account widenings of primitive classes and
+     * <code>null</code>s.</p>
+     *
+     * <p>Primitive widenings allow an int to be assigned to a <code>long</code>,
+     * <code>float</code> or <code>double</code>. This method returns the correct
+     * result for these cases.</p>
+     *
+     * <p><code>Null</code> may be assigned to any reference type. This method will
+     * return <code>true</code> if <code>null</code> is passed in and the toClass is
+     * non-primitive.</p>
+     *
+     * <p>Specifically, this method tests whether the type represented by the
+     * specified <code>Class</code> parameter can be converted to the type
+     * represented by this <code>Class</code> object via an identity conversion
+     * widening primitive or widening reference conversion. See
+     * <em><a href="http://java.sun.com/docs/books/jls/">The Java Language Specification</a></em>,
+     * sections 5.1.1, 5.1.2 and 5.1.4 for details.</p>
+     *
+     * @param classArray  the array of Classes to check, may be <code>null</code>
+     * @param toClassArray  the array of Classes to try to assign into, may be <code>null</code>
+     * @return <code>true</code> if assignment possible
+     */
 	public static boolean isAssignable(Class cls, Class toClass) {
 		return isAssignable(cls, toClass, true);
 	}
-		
+	
+    /**
+     * <p>Checks if an array of Classes can be assigned to another array of Classes.</p>
+     *
+     * <p>This method calls {@link #isAssignable(Class, Class) isAssignable} for each
+     * Class pair in the input arrays. It can be used to check if a set of arguments
+     * (the first parameter) are suitably compatible with a set of method parameter types
+     * (the second parameter).</p>
+     *
+     * <p>Unlike the {@link Class#isAssignableFrom(java.lang.Class)} method, this
+     * method takes into account widenings of primitive classes and
+     * <code>null</code>s.</p>
+     *
+     * <p>Primitive widenings allow an int to be assigned to a <code>long</code>,
+     * <code>float</code> or <code>double</code>. This method returns the correct
+     * result for these cases.</p>
+     *
+     * <p><code>Null</code> may be assigned to any reference type. This method will
+     * return <code>true</code> if <code>null</code> is passed in and the toClass is
+     * non-primitive.</p>
+     *
+     * <p>Specifically, this method tests whether the type represented by the
+     * specified <code>Class</code> parameter can be converted to the type
+     * represented by this <code>Class</code> object via an identity conversion
+     * widening primitive or widening reference conversion. See
+     * <em><a href="http://java.sun.com/docs/books/jls/">The Java Language Specification</a></em>,
+     * sections 5.1.1, 5.1.2 and 5.1.4 for details.</p>
+     *
+     * @param classArray  the array of Classes to check, may be <code>null</code>
+     * @param toClassArray  the array of Classes to try to assign into, may be <code>null</code>
+     * @param autoboxing  whether to use implicit autoboxing/unboxing between primitives and wrappers
+     * @return <code>true</code> if assignment possible
+     * @since 2.5
+     */
 	public static boolean isAssignable(Class<?> cls, Class<?> toClass, boolean autoboxing) {
 		if (toClass == null) {
 			return false;
@@ -97,6 +195,18 @@ public class ClassUtils {
 		return toClass.isAssignableFrom(cls);
 	}
 
+    /**
+     * <p>Converts the specified primitive Class object to its corresponding
+     * wrapper Class object.</p>
+     *
+     * <p>NOTE: From v2.2, this method handles <code>Void.TYPE</code>,
+     * returning <code>Void.TYPE</code>.</p>
+     *
+     * @param cls  the class to convert, may be null
+     * @return the wrapper class for <code>cls</code> or <code>cls</code> if
+     * <code>cls</code> is not a primitive. <code>null</code> if null input.
+     * @since 2.1
+     */
 	public static Class primitiveToWrapper(Class cls) {
 		Class convertedClass = cls;
 		if (cls != null && cls.isPrimitive()) {
@@ -105,6 +215,16 @@ public class ClassUtils {
 		return convertedClass;
 	}
 
+    /**
+     * <p>Converts the specified array of primitive Class objects to an array of
+     * its corresponding wrapper Class objects.</p>
+     *
+     * @param classes  the class array to convert, may be null or empty
+     * @return an array which contains for each given class, the wrapper class or
+     * the original class if class is not a primitive. <code>null</code> if null input.
+     * Empty array if an empty array passed in.
+     * @since 2.1
+     */
 	public static Class wrapperToPrimitive(Class cls) {
 		return wrapperPrimitiveMap.get(cls);
 	}
