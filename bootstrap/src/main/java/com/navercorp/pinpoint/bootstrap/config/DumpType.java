04e28b165f60d9ea5bc4f5b436677601360c549c
Sungkwan Kim <sungkwan.kim@navercorp.com>
2014-12-08 19:34:27 +0900

    #110 convert Korean comments to English

--- bootstrap/src/main/java/com/navercorp/pinpoint/bootstrap/config/DumpType.java
+++ bootstrap/src/main/java/com/navercorp/pinpoint/bootstrap/config/DumpType.java
@@ -4,7 +4,7 @@ package com.nhn.pinpoint.bootstrap.config;
  * @author emeroad
  */
 public enum DumpType {
-//    NONE(-1), 의 경우 이미 더 앞단에서 할지 말지를 결정하므로 중복설정이라 주석처리함.
+//  NONE(-1),  comment out becasue of duplicated configuration. 
     ALWAYS(0), EXCEPTION(1);
 
     private int code;
