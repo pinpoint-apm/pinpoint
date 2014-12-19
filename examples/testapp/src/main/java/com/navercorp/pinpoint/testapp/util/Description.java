cd91c554008a3a69bee613c18cb2845b7d32826f
Hyun Jeong <hyungil.jeong@navercorp.com>
2014-12-19 17:54:00 +0900

    [#123] added default value (empty string) for Description

--- examples/testapp/src/main/java/com/navercorp/pinpoint/testapp/util/Description.java
+++ examples/testapp/src/main/java/com/navercorp/pinpoint/testapp/util/Description.java
@@ -8,5 +8,5 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Description {
-    String value();
+    String value() default "";
 }
