1e5902788044f5fbcad06735cdcaf917afea053e
Woonduk Kang <wd.kang@navercorp.com>
2014-12-19 15:36:14 +0900

    #119 code clean-up - classLoader.close() after jdk1.7

--- bootstrap/src/main/java/com/navercorp/pinpoint/bootstrap/plugin/PluginClassLoaderFactory.java
+++ bootstrap/src/main/java/com/navercorp/pinpoint/bootstrap/plugin/PluginClassLoaderFactory.java
@@ -1,10 +1,18 @@
 package com.navercorp.pinpoint.bootstrap.plugin;
 
+import java.io.Closeable;
+import java.io.IOException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class PluginClassLoaderFactory {
+
+    private final Logger logger = Logger.getLogger(PluginClassLoaderFactory.class.getName());
+
+
     private final URL[] pluginJars;
     private final ConcurrentHashMap<ClassLoader, ClassLoader> cache = new ConcurrentHashMap<ClassLoader, ClassLoader>();
     
@@ -13,16 +21,34 @@ public class PluginClassLoaderFactory {
     }
     
     public ClassLoader get(ClassLoader loader) { 
-        ClassLoader forPlugin = cache.get(loader);
-        
+        final ClassLoader forPlugin = cache.get(loader);
         if (forPlugin != null) {
             return forPlugin;
         }
         
-        ClassLoader newInstance = new URLClassLoader(pluginJars, loader);
-        ClassLoader inCache = cache.putIfAbsent(loader, newInstance);
-        
-        return inCache == null ? newInstance : inCache;
+        final ClassLoader newInstance = new URLClassLoader(pluginJars, loader);
+        final ClassLoader before = cache.putIfAbsent(loader, newInstance);
+        if (before == null) {
+            return newInstance;
+        }
+        else {
+            close (newInstance);
+            return before;
+        }
+    }
+
+    private void close(ClassLoader classLoader) {
+        if (classLoader == null) {
+            return;
+        }
+        // after jdk 1.7
+        if (classLoader instanceof Closeable) {
+            try {
+                ((Closeable)classLoader).close();
+            } catch (IOException e) {
+                logger.log(Level.WARNING, "PluginClassLoader.close() fail. Caused:" + e.getMessage(), e);
+            }
+        }
     }
 
 }
