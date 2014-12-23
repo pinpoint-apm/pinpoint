74257cda5087038225474731fc7e27c81c121df8
Woonduk Kang <wd.kang@navercorp.com>
2014-12-23 11:44:26 +0900

    #140 remove webstorage code(websql, indexeddb)

--- web/src/main/webapp/scripts/services/TransactionDao.js
+++ web/src/main/webapp/scripts/services/TransactionDao.js
@@ -4,35 +4,15 @@ pinpointApp.constant('TransactionDaoConfig', {
     transactionInfoUrl: '/transactionInfo.pinpoint'
 });
 
-pinpointApp.service('TransactionDao', [ 'TransactionDaoConfig','$timeout', 'WebSql', 'IndexedDb', '$window',
-    function Transactiondao(cfg, $timeout, oWebSql, oIndexedDb, $window) {
+pinpointApp.service('TransactionDao', ['TransactionDaoConfig', '$timeout', '$window',
+    function Transactiondao(cfg, $timeout, $window) {
 
-        // define private variables
-        var sDaoType;
 
         /**
          * initialize, especially remove old transaction data
          */
         $timeout(function () {
-            if (oIndexedDb.isAvailable()) {
-                sDaoType = 'IndexedDb';
-                oIndexedDb.deleteOldData('transactionData', 'add_date', Date.now() - 60 * 60 * 12);  // 12시간 전
-            } else if (oWebSql.isAvailable()) {
-                sDaoType = 'WebSql';
-                oWebSql.getDb().transaction(function (oTx) {
-//            tx.executeSql('DROP TABLE IF EXISTS transactionData', [], function () {
-//                console.log('DROP ', arguments);
-//            });
-//            oTx.executeSql('CREATE TABLE IF NOT EXISTS transactionData (ID INTEGER PRIMARY KEY ASC, name TEXT, data TEXT, add_date DATETIME)', [], function () {
-//            });
-                    oTx.executeSql('DELETE TABLE FROM transaction WHERE add_date < datetime("now", "-12 hours")', [], function () {
-                        console.log('The data of transaction before 12 hours has been deleted.');
-                    });
-                });
-            } else {
-                sDaoType = 'window';
-                $window.transactionData = {};
-            }
+            $window.transactionData = {};
         });
 
         /**
@@ -42,17 +22,7 @@ pinpointApp.service('TransactionDao', [ 'TransactionDaoConfig','$timeout', 'WebS
          * @param cb
          */
         this.addData = function (name, data, cb) {
-            if (sDaoType === 'IndexedDb') {
-                oIndexedDb.addData('transactionData', {
-                    name: name,
-                    data: data,
-                    add_date: Date.now()
-                });
-            } else if (sDaoType === 'WebSql') {
-                oWebSql.executeSql('INSERT INTO transactionData (name, data, add_date) VALUES (?, ?, datetime("now", "localtime"))', [name, JSON.stringify(data)], cb);
-            } else if (sDaoType === 'window') {
-                $window.transactionData[name] = data;
-            }
+            $window.transactionData[name] = data;
         };
 
         /**
@@ -61,32 +31,8 @@ pinpointApp.service('TransactionDao', [ 'TransactionDaoConfig','$timeout', 'WebS
          * @param cb
          */
         this.getDataByName = function (name, cb) {
-            if (sDaoType === 'IndexedDb') {
-                oIndexedDb.getData('transactionData', 'name', name, function (err, results) {
-                    if (!err) {
-                        if (angular.isFunction(cb)) {
-                            if (angular.isDefined(results.data)) {
-                                cb(results.data);
-                            } else {
-                                cb({});
-                            }
-                        }
-                    }
-                });
-            } else if (sDaoType === 'WebSql') {
-                oWebSql.executeSql('SELECT data FROM transactionData WHERE name = ?', [name], function (results) {
-                    if (angular.isFunction(cb)) {
-                        if (results.rows) {
-                            cb(JSON.parse(results.rows.item(0).data));
-                        } else {
-                            cb();
-                        }
-                    }
-                });
-            } else if (sDaoType === 'window') {
-                if (angular.isFunction(cb)) {
-                    cb(opener.transactionData[name] || {});
-                }
+            if (angular.isFunction(cb)) {
+                cb(opener.transactionData[name] || {});
             }
         };
 
