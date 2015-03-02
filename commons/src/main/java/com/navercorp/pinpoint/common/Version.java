9b87d87c9c45059e1cf02c115343b7a1369d22fc
kr14910 <koo.taejin@navercorp.com>
2015-03-02 14:18:02 +0900

    1.0.4 release #154
    changed version. (1.0.4-SNAPSHOT -> 1.0.4)

--- commons/src/main/java/com/navercorp/pinpoint/common/Version.java
+++ commons/src/main/java/com/navercorp/pinpoint/common/Version.java
@@ -16,5 +16,5 @@
 
 package com.navercorp.pinpoint.common;
 public final class Version {
-    public static final String VERSION = "1.0.4-SNAPSHOT";
+    public static final String VERSION = "1.0.4";
 }
