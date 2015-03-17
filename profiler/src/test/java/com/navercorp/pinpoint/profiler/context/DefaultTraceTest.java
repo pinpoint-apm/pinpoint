35f6d9538a8eb888f0c759893d63efa6349467f4
HyunGil Jeong <hyungil.jeong@navercorp.com>
2015-03-17 10:56:08 +0900

    clean up redundant service type fields.

--- profiler/src/test/java/com/navercorp/pinpoint/profiler/context/DefaultTraceTest.java
+++ profiler/src/test/java/com/navercorp/pinpoint/profiler/context/DefaultTraceTest.java
@@ -16,25 +16,19 @@
 
 package com.navercorp.pinpoint.profiler.context;
 
-import com.navercorp.pinpoint.common.ServiceType;
-import com.navercorp.pinpoint.common.Version;
-import com.navercorp.pinpoint.profiler.AgentInformation;
 import com.navercorp.pinpoint.profiler.context.DefaultTrace;
 import com.navercorp.pinpoint.profiler.context.DefaultTraceContext;
 import com.navercorp.pinpoint.profiler.context.storage.SpanStorage;
 import com.navercorp.pinpoint.profiler.logging.Slf4jLoggerBinderInitializer;
 import com.navercorp.pinpoint.profiler.sender.LoggingDataSender;
+import com.navercorp.pinpoint.test.TestAgentInformation;
 
 import org.junit.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * @author emeroad
  */
 public class DefaultTraceTest {
-
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @BeforeClass
     public static void before() throws Exception {
@@ -49,8 +43,7 @@ public class DefaultTraceTest {
 
     @Test
     public void testPushPop() {
-        DefaultTraceContext defaultTraceContext = new DefaultTraceContext();
-        defaultTraceContext.setAgentInformation(new AgentInformation("agentId", "applicationName", System.currentTimeMillis(), 10, "test", "127.0.0.1", ServiceType.STAND_ALONE, Version.VERSION));
+        DefaultTraceContext defaultTraceContext = new DefaultTraceContext(new TestAgentInformation());
         DefaultTrace trace = new DefaultTrace(defaultTraceContext, 1);
 
         trace.setStorage(new SpanStorage(LoggingDataSender.DEFAULT_LOGGING_DATA_SENDER));
