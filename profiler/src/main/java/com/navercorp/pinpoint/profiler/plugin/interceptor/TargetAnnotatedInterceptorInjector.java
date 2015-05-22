ece25051195c064779b73c6971faaa6f3835a051
Jongho Moon <jongho.moon@navercorp.com>
2015-05-22 14:27:44 +0900

    Implemented ProfilerPluginClassLoader which inject plugin classes into the target classloader

--- profiler/src/main/java/com/navercorp/pinpoint/profiler/plugin/interceptor/TargetAnnotatedInterceptorInjector.java
+++ profiler/src/main/java/com/navercorp/pinpoint/profiler/plugin/interceptor/TargetAnnotatedInterceptorInjector.java
@@ -29,7 +29,6 @@ import com.navercorp.pinpoint.bootstrap.plugin.annotation.TargetMethod;
 import com.navercorp.pinpoint.bootstrap.plugin.annotation.Targets;
 import com.navercorp.pinpoint.exception.PinpointException;
 import com.navercorp.pinpoint.profiler.plugin.DefaultProfilerPluginContext;
-import com.navercorp.pinpoint.profiler.plugin.TypeUtils;
 import com.navercorp.pinpoint.profiler.plugin.objectfactory.AutoBindingObjectFactory;
 import com.navercorp.pinpoint.profiler.plugin.transformer.ClassCookBook;
 import com.navercorp.pinpoint.profiler.plugin.transformer.ClassRecipe;
@@ -46,16 +45,16 @@ import com.navercorp.pinpoint.profiler.plugin.transformer.MethodTransformer;
 
 public class TargetAnnotatedInterceptorInjector implements ClassRecipe {
     private final DefaultProfilerPluginContext pluginContext;
-    private final String interceptorName;
+    private final String interceptorClassName;
     private final Object[] providedArguments;
     
     private final String groupName;
     private final ExecutionPolicy executionPoint;
 
 
-    public TargetAnnotatedInterceptorInjector(DefaultProfilerPluginContext pluginContext, String interceptorName, Object[] providedArguments, String groupName, ExecutionPolicy executionPoint) {
+    public TargetAnnotatedInterceptorInjector(DefaultProfilerPluginContext pluginContext, String interceptorClassName, Object[] providedArguments, String groupName, ExecutionPolicy executionPoint) {
         this.pluginContext = pluginContext;
-        this.interceptorName = interceptorName;
+        this.interceptorClassName = interceptorClassName;
         this.providedArguments = providedArguments;
         this.groupName = groupName;
         this.executionPoint = executionPoint;
@@ -63,9 +62,9 @@ public class TargetAnnotatedInterceptorInjector implements ClassRecipe {
 
     @Override
     public void edit(ClassLoader classLoader, InstrumentClass target) throws Throwable {
-        Class<? extends Interceptor> interceptorType = TypeUtils.loadClass(classLoader, interceptorName);
+        Class<? extends Interceptor> interceptorType = pluginContext.getClassInjector().loadClass(classLoader, interceptorClassName);
         
-        AnnotatedInterceptorInjector injector = new AnnotatedInterceptorInjector(pluginContext, interceptorName, providedArguments, groupName, executionPoint);
+        AnnotatedInterceptorInjector injector = new AnnotatedInterceptorInjector(pluginContext, interceptorClassName, providedArguments, groupName, executionPoint);
         ClassRecipe recipe = createMethodEditor(classLoader, interceptorType, target,  injector);
         
         recipe.edit(classLoader, target);
@@ -106,7 +105,7 @@ public class TargetAnnotatedInterceptorInjector implements ClassRecipe {
         }
         
         if (editors.isEmpty()) {
-            throw new PinpointException("No target is specified. At least one of @Targets, @TargetMethod, @TargetConstructor, @TargetFilter must present. interceptor: " + interceptorName);
+            throw new PinpointException("No target is specified. At least one of @Targets, @TargetMethod, @TargetConstructor, @TargetFilter must present. interceptor: " + interceptorClassName);
         }
         
         return editors.size() == 1 ? editors.get(0) : new ClassCookBook(editors);
@@ -116,7 +115,7 @@ public class TargetAnnotatedInterceptorInjector implements ClassRecipe {
         String methodName = annotation.name();
         
         if (methodName == null) {
-            throw new PinpointException("name() of @TargetMethod cannot be null: " + interceptorName);
+            throw new PinpointException("name() of @TargetMethod cannot be null: " + interceptorClassName);
         }
         
         String[] parameterTypeNames = annotation.paramTypes();
@@ -133,7 +132,7 @@ public class TargetAnnotatedInterceptorInjector implements ClassRecipe {
         String type = annotation.type();
         
         if (type == null) {
-            throw new PinpointException("type of @TargetFilter is null: " + interceptorName);
+            throw new PinpointException("type of @TargetFilter is null: " + interceptorClassName);
         }
         
         AutoBindingObjectFactory filterFactory = new AutoBindingObjectFactory(pluginContext, targetClass, classLoader);
@@ -147,7 +146,7 @@ public class TargetAnnotatedInterceptorInjector implements ClassRecipe {
     public String toString() {
         StringBuilder builder = new StringBuilder();
         builder.append("TargetAnnotatedInterceptorInjector [interceptorClass=");
-        builder.append(interceptorName);
+        builder.append(interceptorClassName);
         
         if (providedArguments != null) {
             builder.append(", constructorArguments=");
