af5a4ad09a91effd637437de7797dca57e40fb71
leesangyoon <pinetree408@gmail.com>
2015-05-28 23:13:17 +0900

    fix annotationkey 9002 json-lib to json-lib.json.length

--- plugins/json-lib/src/main/java/com/navercorp/pinpoint/plugin/json/lib/JsonLibConstants.java
+++ plugins/json-lib/src/main/java/com/navercorp/pinpoint/plugin/json/lib/JsonLibConstants.java
@@ -26,5 +26,5 @@ import com.navercorp.pinpoint.common.trace.ServiceType;
  */
 public interface JsonLibConstants {
     public static final ServiceType SERVICE_TYPE = ServiceType.of(5012, "JSON-LIB", NORMAL_SCHEMA);
-    public static final AnnotationKey JSON_LIB_ANNOTATION_KEY_JSON_LENGTH = new AnnotationKey(9002, "JSON_LENGTH");
+    public static final AnnotationKey JSON_LIB_ANNOTATION_KEY_JSON_LENGTH = new AnnotationKey(9002, "json-lib.json.length");
 }
