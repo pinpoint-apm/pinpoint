dc0220cbbcea2be4279b4322a8ff6b4e444de77e
Woonduk Kang <wd.kang@navercorp.com>
2015-05-28 16:58:17 +0900

    #320_backport_change_htable_api  - added hbase 1.0 timeout option  - added thread pool option

--- commons/src/main/java/com/navercorp/pinpoint/common/hbase/PooledHTableFactory.java
+++ commons/src/main/java/com/navercorp/pinpoint/common/hbase/PooledHTableFactory.java
@@ -16,13 +16,19 @@
 
 package com.navercorp.pinpoint.common.hbase;
 
+import com.navercorp.pinpoint.common.util.ExecutorFactory;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.client.HTableInterface;
-import org.apache.hadoop.hbase.client.HTableInterfaceFactory;
-import org.apache.hadoop.hbase.client.HTablePool;
+import org.apache.hadoop.hbase.client.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.DisposableBean;
+import org.springframework.data.hadoop.hbase.HbaseSystemException;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * HTableInterfaceFactory based on HTablePool.
@@ -30,21 +36,50 @@ import java.io.IOException;
  */
 public class PooledHTableFactory implements HTableInterfaceFactory, DisposableBean {
 
-    private HTablePool hTablePool;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     public static final int DEFAULT_POOL_SIZE = 256;
+    public static final int DEFAULT_WORKER_QUEUE_SIZE = 1024*5;
+    public static final boolean DEFAULT_PRESTART_THREAD_POOL = false;
+
+    private final ExecutorService executor;
+    private final HConnection connection;
+
 
     public PooledHTableFactory(Configuration config) {
-        this.hTablePool = new HTablePool(config, DEFAULT_POOL_SIZE);
+        this(config, DEFAULT_POOL_SIZE, DEFAULT_WORKER_QUEUE_SIZE, DEFAULT_PRESTART_THREAD_POOL);
     }
 
-    public PooledHTableFactory(Configuration config, int poolSize) {
-        this.hTablePool = new HTablePool(config, poolSize);
+    public PooledHTableFactory(Configuration config, int poolSize, int workerQueueSize, boolean prestartThreadPool) {
+        this.executor = createExecutorService(poolSize, workerQueueSize, prestartThreadPool);
+        try {
+            this.connection = (HConnection)ConnectionFactory.createConnection(config, executor);
+        } catch (IOException e) {
+            throw new HbaseSystemException(e);
+        }
+    }
+
+    private ExecutorService createExecutorService(int poolSize, int workQueueMaxSize, boolean prestartThreadPool) {
+
+        logger.info("create HConnectionThreadPoolExecutor poolSize:{}, workerQueueMaxSize:{}", poolSize, workQueueMaxSize);
+
+        ThreadPoolExecutor threadPoolExecutor = ExecutorFactory.newFixedThreadPool(poolSize, workQueueMaxSize, "Pinpoint-HConnectionExecutor", true);
+        if (prestartThreadPool) {
+            logger.info("prestartAllCoreThreads");
+            threadPoolExecutor.prestartAllCoreThreads();
+        }
+
+        return threadPoolExecutor;
     }
 
 
     @Override
     public HTableInterface createHTableInterface(Configuration config, byte[] tableName) {
-        return hTablePool.getTable(tableName);
+        try {
+            return connection.getTable(tableName, executor);
+        } catch (IOException e) {
+            throw new HbaseSystemException(e);
+        }
     }
 
     @Override
@@ -57,8 +92,26 @@ public class PooledHTableFactory implements HTableInterfaceFactory, DisposableBe
 
     @Override
     public void destroy() throws Exception {
-        if (hTablePool != null) {
-            this.hTablePool.close();
+        logger.info("PooledHTableFactory.destroy()");
+        if (connection != null) {
+            try {
+                this.connection.close();
+            } catch (IOException ex) {
+                logger.warn("Connection.close() error:" + ex.getMessage(), ex);
+            }
+        }
+
+        if (this.executor != null) {
+            this.executor.shutdown();
+            try {
+                final boolean shutdown = executor.awaitTermination(1000 * 5, TimeUnit.MILLISECONDS);
+                if (!shutdown) {
+                    final List<Runnable> discardTask = this.executor.shutdownNow();
+                    logger.warn("discard task size:{}", discardTask.size());
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
         }
     }
 }
