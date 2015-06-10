ddbf180377d3119a9e78fc89725f86e02a3ca8c7
koo.taejin <koo.taejin@navercorp.com>
2015-06-10 14:49:00 +0900

    [collector] Managing server's resources with JMX. #582
    Managing server's resources with JMX.

---
+++ collector/src/main/java/com/navercorp/pinpoint/collector/manage/PinpointCollectorManager.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2014 NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.navercorp.pinpoint.collector.manage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Taejin Koo
+ */
+public class PinpointCollectorManager {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private final PinpointMBeanServer pinpointMBeanServer;
+    private final List<PinpointCollectorMBean> pinpointMBeanList = new ArrayList<PinpointCollectorMBean>();
+    
+    public PinpointCollectorManager() {
+        this.pinpointMBeanServer = new PinpointMBeanServer();
+    }
+
+    @PostConstruct
+    public void initilize() {
+        logger.info("PinpointCollectorManager initialization started.");
+        
+        for (PinpointCollectorMBean pinpontMBean : pinpointMBeanList) {
+            try {
+                pinpointMBeanServer.registerMBean(pinpontMBean);
+            } catch (Exception e) {
+                logger.warn("Failed to register {} MBean.", pinpontMBean);
+            }
+        }
+
+        logger.info("PinpointCollectorManager initialization completed.");
+    }
+
+    @PreDestroy
+    public void finalize() {
+        logger.info("PinpointCollectorManager finalization started.");
+
+        for (PinpointCollectorMBean pinpontMBean : pinpointMBeanList) {
+            try {
+                pinpointMBeanServer.unregisterMBean(pinpontMBean);
+            } catch (Exception e) {
+                logger.warn("Failed to unregister {} MBean.", pinpontMBean);
+            }
+        }
+
+        logger.info("PinpointCollectorManager finalization completed.");
+    }
+    
+    public PinpointCollectorMBean getMBean(String name) {
+        return pinpointMBeanServer.getPinpointMBean(name);
+    }
+    
+
+}
