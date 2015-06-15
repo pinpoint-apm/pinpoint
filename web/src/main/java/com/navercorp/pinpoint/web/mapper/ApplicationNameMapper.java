4fa9ef9ddb1cddcc4e78dd31a2ae0fafb19a41c7
HyunGil Jeong <hyungil.jeong@navercorp.com>
2015-06-15 18:19:40 +0900

    #611 fix issues with applications having same name but different types

--- web/src/main/java/com/navercorp/pinpoint/web/mapper/ApplicationNameMapper.java
+++ web/src/main/java/com/navercorp/pinpoint/web/mapper/ApplicationNameMapper.java
@@ -16,25 +16,50 @@
 
 package com.navercorp.pinpoint.web.mapper;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.CellUtil;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.springframework.data.hadoop.hbase.RowMapper;
 import org.springframework.stereotype.Component;
 
+import com.navercorp.pinpoint.common.ServiceType;
 import com.navercorp.pinpoint.web.vo.Application;
 
 /**
  *
  */
 @Component
-public class ApplicationNameMapper implements RowMapper<Application> {
+public class ApplicationNameMapper implements RowMapper<List<Application>> {
+    
     @Override
-    public Application mapRow(Result result, int rowNum) throws Exception {
+    public List<Application> mapRow(Result result, int rowNum) throws Exception {
         if (result.isEmpty()) {
-            return null;
+            return Collections.emptyList();
         }
+        Set<Short> uniqueTypeCodes = new HashSet<Short>();
         String applicationName = Bytes.toString(result.getRow());
-        short serviceType = Bytes.toShort(result.value());
-        return new Application(applicationName, serviceType);
+        
+        List<Cell> list = result.listCells();
+        if (list == null) {
+            return Collections.emptyList();
+        }
+        
+        for (Cell cell : list) {
+            short serviceTypeCode = Bytes.toShort(CellUtil.cloneValue(cell));
+            uniqueTypeCodes.add(serviceTypeCode);
+        }
+        List<Application> applications = new ArrayList<Application>();
+        for (short serviceTypeCode : uniqueTypeCodes) {
+            applications.add(new Application(applicationName, ServiceType.findServiceType(serviceTypeCode)));
+        }      
+
+        return applications;
     }
 }
