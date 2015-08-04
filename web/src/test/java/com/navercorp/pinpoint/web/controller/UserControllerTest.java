504360df758eb656d0bfa6d5d7a46240bba0ee9a
Minwoo Jung <minwoo.jung@navercorp.com>
2015-08-04 16:40:55 +0900

    [#762] add CRUD logic for user, member of userGroup

--- web/src/test/java/com/navercorp/pinpoint/web/controller/UserControllerTest.java
+++ web/src/test/java/com/navercorp/pinpoint/web/controller/UserControllerTest.java
@@ -23,6 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
