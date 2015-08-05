90f95705443c5a5e7c0f278d6a697169a3b031d4
Minwoo Jung <minwoo.jung@navercorp.com>
2015-08-05 18:52:50 +0900

    [#762] add crud logic for alarm rule

--- web/src/test/java/com/navercorp/pinpoint/web/controller/AlarmControllerTest.java
+++ web/src/test/java/com/navercorp/pinpoint/web/controller/AlarmControllerTest.java
@@ -22,6 +22,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -32,11 +37,14 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import com.navercorp.pinpoint.web.dao.UserDao;
-import com.navercorp.pinpoint.web.vo.User;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.navercorp.pinpoint.web.dao.AlarmDao;
 
 /**
  * @author minwoo.jung
@@ -45,53 +53,59 @@ import com.navercorp.pinpoint.web.vo.User;
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
 @ContextConfiguration(locations = {"classpath:servlet-context.xml", "classpath:applicationContext-web.xml"})
-public class UserControllerTest {
-    
-    private final static String USER_ID = "naver00";
-    private final static String USER_NAME = "minwoo";
-    private final static String USER_DEPARTMENT = "Web platfrom development team";
-    private final static String USER_PHONENUMBER = "01012347890";
-    private final static String USER_EMAIL = "min@naver.com";
+public class AlarmControllerTest {
+    private final static String APPLICATION_ID = "test-application";
+    private final static String USER_GROUP_ID = "test-pinpoint_group";
+    private final static String CHECKER_NAME = "ERROR_COUNT";
+    private final static int THRESHOLD = 100;
+    private final static boolean  SMS_SEND = false;
+    private final static boolean  EMAIL_SEND = true;
+    private final static String NOTES = "for unit test";
     
     @Autowired
     private WebApplicationContext wac;
- 
     
     @Autowired
-    private UserDao userDao;
-    
+    private AlarmDao alarmDao;
+
     private MockMvc mockMvc;
     
     @Before
     public void setup() {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
-        User user = new User();
-        user.setUserId(USER_ID);
-        userDao.deleteUser(user);
+        this.alarmDao.deleteRule(USER_GROUP_ID);
     }
     
+    @SuppressWarnings("unchecked")
     @Test
-    public void insertAndDeleteUser() throws Exception {
+    public void insertRule() throws Exception {
         String jsonParm = "{" +
-                            "\"userId\" : \"" + USER_ID + "\"," + 
-                            "\"name\" : \"" + USER_NAME + "\"," + 
-                            "\"department\" : \"" + USER_DEPARTMENT + "\"," + 
-                            "\"phoneNumber\" : \"" + USER_PHONENUMBER + "\"," + 
-                            "\"email\" : \"" + USER_EMAIL + "\"" + 
+                            "\"applicationId\" : \"" + APPLICATION_ID + "\"," + 
+                            "\"userGroupId\" : \"" + USER_GROUP_ID + "\"," + 
+                            "\"checkerName\" : \"" + CHECKER_NAME + "\"," + 
+                            "\"threshold\" : " + THRESHOLD + "," + 
+                            "\"smsSend\" : " + SMS_SEND + "," + 
+                            "\"emailSend\" : \"" + EMAIL_SEND  + "\"," + 
+                            "\"notes\" : \"" + NOTES + "\"" + 
                           "}"; 
                            
-        this.mockMvc.perform(post("/user.pinpoint").contentType(MediaType.APPLICATION_JSON).content(jsonParm))
-                        .andExpect(status().isOk())
-                        .andExpect(content().contentType("application/json;charset=UTF-8"))
-                        .andExpect(jsonPath("$", hasKey("result")))
-                        .andExpect(jsonPath("$.result").value("SUCCESS"))
-                        .andReturn();
+        MvcResult result = this.mockMvc.perform(post("/alarmRule.pinpoint").contentType(MediaType.APPLICATION_JSON).content(jsonParm))
+                                            .andExpect(status().isOk())
+                                            .andExpect(content().contentType("application/json;charset=UTF-8"))
+                                            .andReturn();
+        String content = result.getResponse().getContentAsString();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, String> resultMap = objectMapper.readValue(content, HashMap.class);
+        Assert.assertEquals(resultMap.get("result"), "SUCCESS");
+        Assert.assertNotNull(resultMap.get("ruleId"));
         
-        this.mockMvc.perform(delete("/user.pinpoint").contentType(MediaType.APPLICATION_JSON).content("{\"userId\" : \"" + USER_ID + "\"}"))
+        this.mockMvc.perform(delete("/alarmRule.pinpoint").contentType(MediaType.APPLICATION_JSON).content("{\"ruleId\" : \"" + resultMap.get("ruleId") + "\"}"))
                         .andExpect(status().isOk())
                         .andExpect(content().contentType("application/json;charset=UTF-8"))
                         .andExpect(jsonPath("$", hasKey("result")))
                         .andExpect(jsonPath("$.result").value("SUCCESS"))
                         .andReturn();
     }
+    
 }
