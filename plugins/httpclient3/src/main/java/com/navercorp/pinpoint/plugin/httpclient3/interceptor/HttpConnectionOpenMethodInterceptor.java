507acd567f3c663b7652ea384f09d53eb7105496
Jongho Moon <jongho.moon@navercorp.com>
2015-08-11 17:58:59 +0900

    httpclient3 plugin now uses new plugin api

--- plugins/httpclient3/src/main/java/com/navercorp/pinpoint/plugin/httpclient3/interceptor/HttpConnectionOpenMethodInterceptor.java
+++ plugins/httpclient3/src/main/java/com/navercorp/pinpoint/plugin/httpclient3/interceptor/HttpConnectionOpenMethodInterceptor.java
@@ -16,33 +16,25 @@
 
 package com.navercorp.pinpoint.plugin.httpclient3.interceptor;
 
-import com.navercorp.pinpoint.bootstrap.FieldAccessor;
 import com.navercorp.pinpoint.bootstrap.context.SpanEventRecorder;
 import com.navercorp.pinpoint.bootstrap.context.TraceContext;
 import com.navercorp.pinpoint.bootstrap.interceptor.MethodDescriptor;
 import com.navercorp.pinpoint.bootstrap.interceptor.SpanEventSimpleAroundInterceptorForPlugin;
-import com.navercorp.pinpoint.bootstrap.plugin.annotation.Name;
 import com.navercorp.pinpoint.common.trace.AnnotationKey;
 import com.navercorp.pinpoint.common.trace.ServiceType;
+import com.navercorp.pinpoint.plugin.httpclient3.HostNameGetter;
 import com.navercorp.pinpoint.plugin.httpclient3.HttpClient3Constants;
+import com.navercorp.pinpoint.plugin.httpclient3.PortNumberGetter;
+import com.navercorp.pinpoint.plugin.httpclient3.ProxyHostNameGetter;
+import com.navercorp.pinpoint.plugin.httpclient3.ProxyPortNumberGetter;
 
 /**
  * @author jaehong.kim
  */
 public class HttpConnectionOpenMethodInterceptor extends SpanEventSimpleAroundInterceptorForPlugin implements HttpClient3Constants {
 
-    private FieldAccessor hostNameAccessor;
-    private FieldAccessor portNumberAccessor;
-    private FieldAccessor proxyHostNameAccessor;
-    private FieldAccessor proxyPortNumberAccessor;
-
-    public HttpConnectionOpenMethodInterceptor(TraceContext traceContext, MethodDescriptor methodDescriptor, @Name(FIELD_HOST_NAME) FieldAccessor hostNameAccessor, @Name(FIELD_PORT_NUMBER) FieldAccessor portNumberAccessor,
-            @Name(FIELD_PROXY_HOST_NAME) FieldAccessor proxyHostNameAccessor, @Name(FIELD_PROXY_PORT_NUMBER) FieldAccessor proxyPortNumberAccessor) {
+    public HttpConnectionOpenMethodInterceptor(TraceContext traceContext, MethodDescriptor methodDescriptor) {
         super(traceContext, methodDescriptor);
-        this.hostNameAccessor = hostNameAccessor;
-        this.portNumberAccessor = portNumberAccessor;
-        this.proxyHostNameAccessor = proxyHostNameAccessor;
-        this.proxyPortNumberAccessor = proxyPortNumberAccessor;
     }
 
     @Override
@@ -50,14 +42,14 @@ public class HttpConnectionOpenMethodInterceptor extends SpanEventSimpleAroundIn
         recorder.recordApi(methodDescriptor);
         recorder.recordServiceType(ServiceType.HTTP_CLIENT_INTERNAL);
 
-        if (hostNameAccessor.isApplicable(target) && portNumberAccessor.isApplicable(target) && proxyHostNameAccessor.isApplicable(target) && proxyPortNumberAccessor.isApplicable(target)) {
+        if (target instanceof HostNameGetter && target instanceof PortNumberGetter && target instanceof ProxyHostNameGetter && target instanceof ProxyPortNumberGetter) {
             final StringBuilder sb = new StringBuilder();
-            if (proxyHostNameAccessor.get(target) != null) {
-                sb.append(proxyHostNameAccessor.get(target));
-                sb.append(":").append(proxyPortNumberAccessor.get(target));
+            if (((ProxyHostNameGetter)target)._$PINPOINT$_getProxyHostName() != null) {
+                sb.append(((ProxyHostNameGetter)target)._$PINPOINT$_getProxyHostName());
+                sb.append(":").append(((ProxyPortNumberGetter)target)._$PINPOINT$_getProxyPortNumber());
             } else {
-                sb.append(hostNameAccessor.get(target));
-                sb.append(":").append(proxyPortNumberAccessor.get(target));
+                sb.append(((HostNameGetter)target)._$PINPOINT$_getHostName());
+                sb.append(":").append(((PortNumberGetter)target)._$PINPOINT$_getPortNumber());
             }
             recorder.recordAttribute(AnnotationKey.HTTP_INTERNAL_DISPLAY, sb.toString());
         }
