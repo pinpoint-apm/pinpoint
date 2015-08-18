debe709e188772ff6ab39ee0a59a661c223df86e
Minwoo Jung <minwoo.jung@navercorp.com>
2015-08-18 16:27:00 +0900

    [#762] edit class name

--- web/src/test/java/com/navercorp/pinpoint/web/alarm/checker/SlowRateToCalleCheckerTest.java
+++ web/src/test/java/com/navercorp/pinpoint/web/alarm/checker/SlowRateToCalleCheckerTest.java
@@ -85,7 +85,7 @@ public class SlowRateToCalleCheckerTest {
     public void checkTest() {
         Application application = new Application(FROM_SERVICE_NAME, ServiceType.STAND_ALONE);
         MapStatisticsCallerDataCollector dataCollector = new MapStatisticsCallerDataCollector(DataCollectorCategory.CALLER_STAT, application, dao, System.currentTimeMillis(), 300000);
-        Rule rule = new Rule(FROM_SERVICE_NAME, CheckerCategory.SLOW_RATE_TO_CALLE.getName(), 70, "testGroup", false, false, TO_SERVICE_NAME + 1);
+        Rule rule = new Rule(FROM_SERVICE_NAME, CheckerCategory.SLOW_RATE_TO_CALLEE.getName(), 70, "testGroup", false, false, TO_SERVICE_NAME + 1);
         SlowRateToCalleeChecker checker = new SlowRateToCalleeChecker(dataCollector, rule);
         
         checker.check();
@@ -96,7 +96,7 @@ public class SlowRateToCalleCheckerTest {
     public void checkTest2() {
         Application application = new Application(FROM_SERVICE_NAME, ServiceType.STAND_ALONE);
         MapStatisticsCallerDataCollector dataCollector = new MapStatisticsCallerDataCollector(DataCollectorCategory.CALLER_STAT, application, dao, System.currentTimeMillis(), 300000);
-        Rule rule = new Rule(FROM_SERVICE_NAME, CheckerCategory.SLOW_RATE_TO_CALLE.getName(), 71, "testGroup", false, false, TO_SERVICE_NAME + 1);
+        Rule rule = new Rule(FROM_SERVICE_NAME, CheckerCategory.SLOW_RATE_TO_CALLEE.getName(), 71, "testGroup", false, false, TO_SERVICE_NAME + 1);
         SlowRateToCalleeChecker checker = new SlowRateToCalleeChecker(dataCollector, rule);
         
         checker.check();
@@ -107,7 +107,7 @@ public class SlowRateToCalleCheckerTest {
     public void checkTest3() {
         Application application = new Application(FROM_SERVICE_NAME, ServiceType.STAND_ALONE);
         MapStatisticsCallerDataCollector dataCollector = new MapStatisticsCallerDataCollector(DataCollectorCategory.CALLER_STAT, application, dao, System.currentTimeMillis(), 300000);
-        Rule rule = new Rule(FROM_SERVICE_NAME, CheckerCategory.SLOW_RATE_TO_CALLE.getName(), 90, "testGroup", false, false, TO_SERVICE_NAME + 2);
+        Rule rule = new Rule(FROM_SERVICE_NAME, CheckerCategory.SLOW_RATE_TO_CALLEE.getName(), 90, "testGroup", false, false, TO_SERVICE_NAME + 2);
         SlowRateToCalleeChecker checker = new SlowRateToCalleeChecker(dataCollector, rule);
         
         checker.check();
--- web/src/test/java/com/navercorp/pinpoint/web/alarm/checker/TotalCountToCalleCheckerTest.java
+++ web/src/test/java/com/navercorp/pinpoint/web/alarm/checker/TotalCountToCalleCheckerTest.java
@@ -84,7 +84,7 @@ public class TotalCountToCalleCheckerTest {
     public void checkTest() {
         Application application = new Application(FROM_SERVICE_NAME, ServiceType.STAND_ALONE);
         MapStatisticsCallerDataCollector dataCollector = new MapStatisticsCallerDataCollector(DataCollectorCategory.CALLER_STAT, application, dao, System.currentTimeMillis(), 300000);
-        Rule rule = new Rule(FROM_SERVICE_NAME, CheckerCategory.TOTAL_COUNT_TO_CALLE.getName(), 10, "testGroup", false, false, TO_SERVICE_NAME + 1);
+        Rule rule = new Rule(FROM_SERVICE_NAME, CheckerCategory.TOTAL_COUNT_TO_CALLEE.getName(), 10, "testGroup", false, false, TO_SERVICE_NAME + 1);
         TotalCountToCalleeChecker checker = new TotalCountToCalleeChecker(dataCollector, rule);
         
         checker.check();
@@ -95,7 +95,7 @@ public class TotalCountToCalleCheckerTest {
     public void checkTest2() {
         Application application = new Application(FROM_SERVICE_NAME, ServiceType.STAND_ALONE);
         MapStatisticsCallerDataCollector dataCollector = new MapStatisticsCallerDataCollector(DataCollectorCategory.CALLER_STAT, application, dao, System.currentTimeMillis(), 300000);
-        Rule rule = new Rule(FROM_SERVICE_NAME, CheckerCategory.TOTAL_COUNT_TO_CALLE.getName(), 11, "testGroup", false, false, TO_SERVICE_NAME + 1);
+        Rule rule = new Rule(FROM_SERVICE_NAME, CheckerCategory.TOTAL_COUNT_TO_CALLEE.getName(), 11, "testGroup", false, false, TO_SERVICE_NAME + 1);
         TotalCountToCalleeChecker checker = new TotalCountToCalleeChecker(dataCollector, rule);
         
         checker.check();
@@ -106,7 +106,7 @@ public class TotalCountToCalleCheckerTest {
     public void checkTest3() {
         Application application = new Application(FROM_SERVICE_NAME, ServiceType.STAND_ALONE);
         MapStatisticsCallerDataCollector dataCollector = new MapStatisticsCallerDataCollector(DataCollectorCategory.CALLER_STAT, application, dao, System.currentTimeMillis(), 300000);
-        Rule rule = new Rule(FROM_SERVICE_NAME, CheckerCategory.TOTAL_COUNT_TO_CALLE.getName(), 10, "testGroup", false, false, TO_SERVICE_NAME + 2);
+        Rule rule = new Rule(FROM_SERVICE_NAME, CheckerCategory.TOTAL_COUNT_TO_CALLEE.getName(), 10, "testGroup", false, false, TO_SERVICE_NAME + 2);
         TotalCountToCalleeChecker checker = new TotalCountToCalleeChecker(dataCollector, rule);
         
         checker.check();
