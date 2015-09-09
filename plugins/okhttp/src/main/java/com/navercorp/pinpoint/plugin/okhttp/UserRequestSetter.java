de7ec854ffded217f656436ef18528877c6eb3a0
Jaehong Kim <jaehong.kim@navercorp.com>
2015-09-09 14:33:58 +0900

    add OkHttp plugin.

---
+++ plugins/okhttp/src/main/java/com/navercorp/pinpoint/plugin/okhttp/UserRequestSetter.java
@@ -0,0 +1,10 @@
+package com.navercorp.pinpoint.plugin.okhttp;
+
+import com.squareup.okhttp.Request;
+
+/**
+ * Created by nbp on 2015-09-08.
+ */
+public interface UserRequestSetter {
+    public Request _$PINPOINT$_getUserRequest();
+}
---
+++ plugins/okhttp/src/main/java/com/navercorp/pinpoint/plugin/okhttp/interceptor/HttpEngineSendRequestMethodInterceptor.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright 2014 NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.navercorp.pinpoint.plugin.okhttp.interceptor;
+
+import com.navercorp.pinpoint.bootstrap.context.*;
+import com.navercorp.pinpoint.bootstrap.instrument.AttachmentFactory;
+import com.navercorp.pinpoint.bootstrap.interceptor.MethodDescriptor;
+import com.navercorp.pinpoint.bootstrap.interceptor.SimpleAroundInterceptor;
+import com.navercorp.pinpoint.bootstrap.interceptor.group.InterceptorGroup;
+import com.navercorp.pinpoint.bootstrap.interceptor.group.InterceptorGroupInvocation;
+import com.navercorp.pinpoint.bootstrap.logging.PLogger;
+import com.navercorp.pinpoint.bootstrap.logging.PLoggerFactory;
+import com.navercorp.pinpoint.bootstrap.plugin.annotation.Group;
+import com.navercorp.pinpoint.bootstrap.sampler.SamplingFlagUtils;
+import com.navercorp.pinpoint.common.trace.AnnotationKey;
+import com.navercorp.pinpoint.plugin.okhttp.ConnectionGetter;
+import com.navercorp.pinpoint.plugin.okhttp.OkHttpConstants;
+import com.navercorp.pinpoint.plugin.okhttp.UserRequestGetter;
+import com.navercorp.pinpoint.plugin.okhttp.UserResponseGetter;
+import com.squareup.okhttp.HttpUrl;
+import com.squareup.okhttp.Request;
+
+/**
+ * @author jaehong.kim
+ */
+@Group(OkHttpConstants.SEND_REQUEST_SCOPE)
+public class HttpEngineSendRequestMethodInterceptor implements SimpleAroundInterceptor, OkHttpConstants {
+    private final PLogger logger = PLoggerFactory.getLogger(this.getClass());
+    private final boolean isDebug = logger.isDebugEnabled();
+
+    private TraceContext traceContext;
+    private MethodDescriptor methodDescriptor;
+    private InterceptorGroup interceptorGroup;
+
+    public HttpEngineSendRequestMethodInterceptor(TraceContext traceContext, MethodDescriptor methodDescriptor, InterceptorGroup interceptorGroup) {
+        this.traceContext = traceContext;
+        this.methodDescriptor = methodDescriptor;
+        this.interceptorGroup = interceptorGroup;
+    }
+
+    @Override
+    public void before(Object target, Object[] args) {
+        if (isDebug) {
+            logger.beforeInterceptor(target, args);
+        }
+
+        final Trace trace = traceContext.currentRawTraceObject();
+        if (trace == null) {
+            return;
+        }
+
+        if (!validate(target)) {
+            return;
+        }
+
+        SpanEventRecorder recorder = trace.traceBlockBegin();
+        try {
+            final TraceId nextId = trace.getTraceId().getNextTraceId();
+            recorder.recordNextSpanId(nextId.getSpanId());
+            recorder.recordServiceType(OK_HTTP_CLIENT);
+
+            InterceptorGroupInvocation invocation = interceptorGroup.getCurrentInvocation();
+            if (invocation != null) {
+                invocation.getOrCreateAttachment(new AttachmentFactory() {
+                    @Override
+                    public Object createAttachment() {
+                        return nextId;
+                    }
+                });
+            }
+        } catch (Throwable t) {
+            logger.warn("Failed to BEFORE process. {}", t.getMessage(), t);
+        }
+    }
+
+    private boolean validate(Object target) {
+        if (!(target instanceof UserRequestGetter)) {
+            logger.debug("Invalid target object. Need field accessor({}).", FIELD_USER_REQUEST);
+            return false;
+        }
+
+        if (!(target instanceof UserResponseGetter)) {
+            logger.debug("Invalid target object. Need field accessor({}).", FIELD_USER_RESPONSE);
+            return false;
+        }
+
+        if (!(target instanceof ConnectionGetter)) {
+            logger.debug("Invalid target object. Need field accessor({}).", FIELD_CONNECTION);
+            return false;
+        }
+
+        return true;
+    }
+
+    private String getHost(HttpUrl url) {
+        return url.port() != HttpUrl.defaultPort(url.scheme())
+                ? url.host() + ":" + url.port()
+                : url.host();
+    }
+
+    @Override
+    public void after(Object target, Object[] args, Object result, Throwable throwable) {
+        if (isDebug) {
+            logger.afterInterceptor(target, args);
+        }
+
+        final Trace trace = traceContext.currentTraceObject();
+        if (trace == null) {
+            return;
+        }
+
+        if (!validate(target)) {
+            return;
+        }
+
+        try {
+            SpanEventRecorder recorder = trace.currentSpanEventRecorder();
+            recorder.recordApi(methodDescriptor);
+            recorder.recordException(throwable);
+
+            Request request = ((UserRequestGetter) target)._$PINPOINT$_getUserRequest();
+            if (request != null) {
+                recorder.recordAttribute(AnnotationKey.HTTP_URL, request.httpUrl().toString());
+                recorder.recordDestinationId(request.httpUrl().host() + ":" + request.httpUrl().port());
+            }
+
+            // clear attachment.
+            InterceptorGroupInvocation invocation = interceptorGroup.getCurrentInvocation();
+            if(invocation != null && invocation.getAttachment() != null) {
+                invocation.removeAttachment();
+            }
+        } finally {
+            trace.traceBlockEnd();
+        }
+    }
+}
