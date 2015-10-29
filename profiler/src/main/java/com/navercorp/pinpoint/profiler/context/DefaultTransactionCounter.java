f24d04f2eae8db78a240cfd7d1202f5c9fb671bd
HyunGil Jeong <hyungil.jeong@navercorp.com>
2015-10-29 17:57:06 +0900

    #1103 Change how internal trace ids are generated

--- profiler/src/main/java/com/navercorp/pinpoint/profiler/context/DefaultTransactionCounter.java
+++ profiler/src/main/java/com/navercorp/pinpoint/profiler/context/DefaultTransactionCounter.java
@@ -29,7 +29,7 @@ public class DefaultTransactionCounter implements TransactionCounter {
         }
         this.idGenerator = idGenerator;
     }
-
+    
     @Override
     public long getTransactionCount(SamplingType samplingType) {
         // overflow improbable
@@ -37,11 +37,11 @@ public class DefaultTransactionCounter implements TransactionCounter {
         case SAMPLED_NEW:
             return idGenerator.currentTransactionId() - IdGenerator.INITIAL_TRANSACTION_ID;
         case SAMPLED_CONTINUATION:
-            return Math.abs(idGenerator.currentContinuedTransactionId() - IdGenerator.INITIAL_CONTINUED_TRANSACTION_ID);
+            return Math.abs(idGenerator.currentContinuedTransactionId() - IdGenerator.INITIAL_CONTINUED_TRANSACTION_ID) / IdGenerator.DECREMENT_CYCLE;
         case UNSAMPLED_NEW:
-            return Math.abs(idGenerator.currentDisabledId() - IdGenerator.INITIAL_DISABLED_ID);
+            return Math.abs(idGenerator.currentDisabledId() - IdGenerator.INITIAL_DISABLED_ID) / IdGenerator.DECREMENT_CYCLE;
         case UNSAMPLED_CONTINUATION:
-            return idGenerator.currentContinuedDisabledId() - IdGenerator.INITIAL_CONTINUED_DISABLED_ID;
+            return Math.abs(idGenerator.currentContinuedDisabledId() - IdGenerator.INITIAL_CONTINUED_DISABLED_ID) / IdGenerator.DECREMENT_CYCLE;
         default:
             return 0L;
         }
