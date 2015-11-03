522d7a22c7bd69ae54d12be43ff3d97da567c2a5
Jaehong Kim <jaehong.kim@navercorp.com>
2015-11-03 17:53:00 +0900

    refactoring.

--- bootstrap-core/src/main/java/com/navercorp/pinpoint/bootstrap/plugin/jdbc/interceptor/PreparedStatementBindVariableInterceptor.java
+++ bootstrap-core/src/main/java/com/navercorp/pinpoint/bootstrap/plugin/jdbc/interceptor/PreparedStatementBindVariableInterceptor.java
@@ -49,6 +49,7 @@ public class PreparedStatementBindVariableInterceptor implements StaticAroundInt
     public void before(Object target, String className, String methodName, String parameterDescription, Object[] args) {
     }
 
+
     @Override
     public void after(Object target, String className, String methodName, String parameterDescription, Object[] args, Object result, Throwable throwable) {
         if (isDebug) {
