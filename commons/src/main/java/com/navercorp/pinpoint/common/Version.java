0541fd4a4463e4eb0d9851856b1fe6520e0a59f7
koo.taejin <koo.taejin@navercorp.com>
2015-12-11 16:00:47 +0900

    1.5.1 release #1340
    changed version (1.5.1 -> 1.5.2-SNAPSHOT)

--- commons/src/main/java/com/navercorp/pinpoint/common/Version.java
+++ commons/src/main/java/com/navercorp/pinpoint/common/Version.java
@@ -1,4 +1,4 @@
 package com.navercorp.pinpoint.common;
 public final class Version {
-    public static final String VERSION = "1.5.1";
+    public static final String VERSION = "1.5.2-SNAPSHOT";
 }
