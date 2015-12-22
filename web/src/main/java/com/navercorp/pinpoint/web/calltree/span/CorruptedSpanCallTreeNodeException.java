241c852a7aaf94634adbef5fc2aeb852abec6f10
Jaehong Kim <jaehong.kim@navercorp.com>
2015-12-22 14:32:55 +0900

    Add corrupted case for sequence overflow.

--- web/src/main/java/com/navercorp/pinpoint/web/calltree/span/CorruptedSpanCallTreeNodeException.java
+++ web/src/main/java/com/navercorp/pinpoint/web/calltree/span/CorruptedSpanCallTreeNodeException.java
@@ -25,7 +25,14 @@ public class CorruptedSpanCallTreeNodeException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
 
-    public CorruptedSpanCallTreeNodeException(String message) {
+    private String title;
+
+    public CorruptedSpanCallTreeNodeException(String title, String message) {
         super(message);
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
     }
 }
