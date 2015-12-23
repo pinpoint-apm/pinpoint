f8a4b7edd9f8501f034df543905648a05e9d58dc
Jaehong Kim <jaehong.kim@navercorp.com>
2015-12-23 16:58:13 +0900

    refactoring.

--- web/src/main/java/com/navercorp/pinpoint/web/view/TransactionInfoCallStackSerializer.java
+++ web/src/main/java/com/navercorp/pinpoint/web/view/TransactionInfoCallStackSerializer.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015 NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.navercorp.pinpoint.web.view;
 
 import com.fasterxml.jackson.core.JsonGenerator;
@@ -8,7 +23,7 @@ import com.fasterxml.jackson.databind.SerializerProvider;
 import java.io.IOException;
 
 /**
- * Created by Naver on 2015-10-22.
+ * @author jaehong.kim
  */
 public class TransactionInfoCallStackSerializer extends JsonSerializer<TransactionInfoViewModel.CallStack> {
 
--- web/src/main/java/com/navercorp/pinpoint/web/view/TransactionInfoViewModel.java
+++ web/src/main/java/com/navercorp/pinpoint/web/view/TransactionInfoViewModel.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015 NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.navercorp.pinpoint.web.view;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -16,7 +31,7 @@ import java.net.URL;
 import java.util.*;
 
 /**
- * Created by Naver on 2015-10-21.
+ * @author jaehong.kim
  */
 public class TransactionInfoViewModel {
 
@@ -135,14 +150,15 @@ public class TransactionInfoViewModel {
         long barRatio = 0;
         for(Record record : recordSet.getRecordList()) {
             if(first) {
-                long begin = record.getBegin();
-                long end = record.getBegin() + record.getElapsed();
-                if(end  - begin > 0) {
-                    barRatio = 100 / (end - begin);
+                if(record.isMethod()) {
+                    long begin = record.getBegin();
+                    long end = record.getBegin() + record.getElapsed();
+                    if(end  - begin > 0) {
+                        barRatio = 100 / (end - begin);
+                    }
                 }
                 first = false;
             }
-            // TODO
             list.add(new CallStack(record, barRatio));
         }
 
