a7433b75af8ed335936922d3b1de36def6cdad11
Jinkai.Ma <majinkai@handu.com>
2016-01-08 17:36:22 +0800

    Add support for dubbo #1416

---
+++ plugins/dubbo/src/main/java/com/navercorp/pinpoint/plugin/dubbo/interceptor/DubboConsumerInterceptor.java
@@ -0,0 +1,100 @@
+package com.navercorp.pinpoint.plugin.dubbo.interceptor;
+
+import com.alibaba.dubbo.rpc.Invoker;
+import com.alibaba.dubbo.rpc.RpcContext;
+import com.alibaba.dubbo.rpc.RpcInvocation;
+import com.navercorp.pinpoint.bootstrap.context.*;
+import com.navercorp.pinpoint.bootstrap.interceptor.AroundInterceptor1;
+import com.navercorp.pinpoint.plugin.dubbo.DubboConstants;
+
+/**
+ * @author Jinkai.Ma
+ */
+public class DubboConsumerInterceptor implements AroundInterceptor1 {
+
+    private final MethodDescriptor descriptor;
+    private final TraceContext traceContext;
+
+    public DubboConsumerInterceptor(TraceContext traceContext, MethodDescriptor descriptor) {
+        this.descriptor = descriptor;
+        this.traceContext = traceContext;
+    }
+
+    @Override
+    public void before(Object target, Object arg0) {
+        Trace trace = this.getTrace(target);
+        if (trace == null) {
+            return;
+        }
+
+        RpcInvocation invocation = (RpcInvocation) arg0;
+
+        if (trace.canSampled()) {
+            SpanEventRecorder recorder = trace.traceBlockBegin();
+
+            // RPC call trace have to be recorded with a service code in RPC client code range.
+            recorder.recordServiceType(DubboConstants.DUBBO_CONSUMER_SERVICE_TYPE);
+
+            // You have to issue a TraceId the receiver of this request will use.
+            TraceId nextId = trace.getTraceId().getNextTraceId();
+
+            // Then record it as next span id.
+            recorder.recordNextSpanId(nextId.getSpanId());
+
+            // Finally, pass some tracing data to the server.
+            // How to put them in a message is protocol specific.
+            // This example assumes that the target protocol message can include any metadata (like HTTP headers).
+            invocation.setAttachment(DubboConstants.META_TRANSACTION_ID, nextId.getTransactionId());
+            invocation.setAttachment(DubboConstants.META_SPAN_ID, Long.toString(nextId.getSpanId()));
+            invocation.setAttachment(DubboConstants.META_PARENT_SPAN_ID, Long.toString(nextId.getParentSpanId()));
+            invocation.setAttachment(DubboConstants.META_PARENT_APPLICATION_TYPE, Short.toString(traceContext.getServerTypeCode()));
+            invocation.setAttachment(DubboConstants.META_PARENT_APPLICATION_NAME, traceContext.getApplicationName());
+            invocation.setAttachment(DubboConstants.META_FLAGS, Short.toString(nextId.getFlags()));
+        } else {
+            // If sampling this transaction is disabled, pass only that infomation to the server.
+            invocation.setAttachment(DubboConstants.META_DO_NOT_TRACE, "1");
+        }
+    }
+
+    @Override
+    public void after(Object target, Object arg0, Object result, Throwable throwable) {
+        Trace trace = this.getTrace(target);
+        if (trace == null) {
+            return;
+        }
+
+        RpcInvocation invocation = (RpcInvocation) arg0;
+
+        try {
+            SpanEventRecorder recorder = trace.currentSpanEventRecorder();
+
+            recorder.recordApi(descriptor);
+
+            if (throwable == null) {
+                String endPoint = RpcContext.getContext().getRemoteAddressString();
+                // RPC client have to record end point (server address)
+                recorder.recordEndPoint(endPoint);
+
+                // Optionally, record the destination id (logical name of server. e.g. DB name)
+                recorder.recordDestinationId(endPoint);
+                recorder.recordAttribute(DubboConstants.DUBBO_ARGS_ANNOTATION_KEY, invocation.getArguments());
+                recorder.recordAttribute(DubboConstants.DUBBO_RESULT_ANNOTATION_KEY, result);
+            } else {
+                recorder.recordException(throwable);
+            }
+        } finally {
+            trace.traceBlockEnd();
+        }
+    }
+
+    private Trace getTrace(Object target) {
+        Invoker invoker = (Invoker) target;
+        // Ignore monitor service.
+        if (DubboConstants.MONITOR_SERVICE_FQCN.equals(invoker.getInterface().getName())) {
+            return null;
+        }
+
+        return traceContext.currentTraceObject();
+    }
+
+}
---
+++ plugins/dubbo/src/main/java/com/navercorp/pinpoint/plugin/dubbo/interceptor/DubboProviderInterceptor.java
@@ -0,0 +1,95 @@
+package com.navercorp.pinpoint.plugin.dubbo.interceptor;
+
+import com.alibaba.dubbo.rpc.Invoker;
+import com.alibaba.dubbo.rpc.RpcContext;
+import com.alibaba.dubbo.rpc.RpcInvocation;
+import com.navercorp.pinpoint.bootstrap.context.*;
+import com.navercorp.pinpoint.bootstrap.interceptor.SpanSimpleAroundInterceptor;
+import com.navercorp.pinpoint.bootstrap.util.NumberUtils;
+import com.navercorp.pinpoint.common.trace.ServiceType;
+import com.navercorp.pinpoint.plugin.dubbo.DubboConstants;
+
+/**
+ * @author Jinkai.Ma
+ */
+public class DubboProviderInterceptor extends SpanSimpleAroundInterceptor {
+
+    public DubboProviderInterceptor(TraceContext traceContext, MethodDescriptor descriptor) {
+        super(traceContext, descriptor, DubboProviderInterceptor.class);
+    }
+
+    @Override
+    protected Trace createTrace(Object target, Object[] args) {
+        Invoker invoker = (Invoker) target;
+
+        // Ignore monitor service.
+        if (DubboConstants.MONITOR_SERVICE_FQCN.equals(invoker.getInterface().getName())) {
+            return traceContext.disableSampling();
+        }
+
+        RpcInvocation invocation = (RpcInvocation) args[0];
+
+        // If this transaction is not traceable, mark as disabled.
+        if (invocation.getAttachment(DubboConstants.META_DO_NOT_TRACE) != null) {
+            return traceContext.disableSampling();
+        }
+
+        String transactionId = invocation.getAttachment(DubboConstants.META_TRANSACTION_ID);
+
+        // If there's no trasanction id, a new trasaction begins here.
+        if (transactionId == null) {
+            return traceContext.newTraceObject();
+        }
+
+        // otherwise, continue tracing with given data.
+        long parentSpanID = NumberUtils.parseLong(invocation.getAttachment(DubboConstants.META_PARENT_SPAN_ID), SpanId.NULL);
+        long spanID = NumberUtils.parseLong(invocation.getAttachment(DubboConstants.META_SPAN_ID), SpanId.NULL);
+        short flags = NumberUtils.parseShort(invocation.getAttachment(DubboConstants.META_FLAGS), (short) 0);
+        TraceId traceId = traceContext.createTraceId(transactionId, parentSpanID, spanID, flags);
+
+        return traceContext.continueTraceObject(traceId);
+    }
+
+
+    @Override
+    protected void doInBeforeTrace(SpanRecorder recorder, Object target, Object[] args) {
+        RpcInvocation invocation = (RpcInvocation) args[0];
+        RpcContext rpcContext = RpcContext.getContext();
+
+        // You have to record a service type within Server range.
+        recorder.recordServiceType(DubboConstants.DUBBO_PROVIDER_SERVICE_TYPE);
+
+        // Record rpc name, client address, server address.
+        recorder.recordRpcName(invocation.getInvoker().getInterface().getSimpleName() + ":" + invocation.getMethodName());
+        recorder.recordEndPoint(rpcContext.getLocalAddressString());
+        recorder.recordRemoteAddress(rpcContext.getRemoteAddressString());
+
+        // If this transaction did not begin here, record parent(client who sent this request) information
+        if (!recorder.isRoot()) {
+            String parentApplicationName = invocation.getAttachment(DubboConstants.META_PARENT_APPLICATION_NAME);
+
+            if (parentApplicationName != null) {
+                short parentApplicationType = NumberUtils.parseShort(invocation.getAttachment(DubboConstants.META_PARENT_APPLICATION_TYPE), ServiceType.UNDEFINED.getCode());
+                recorder.recordParentApplication(parentApplicationName, parentApplicationType);
+
+                // Pinpoint finds caller - callee relation by matching caller's end point and callee's acceptor host.
+                // https://github.com/naver/pinpoint/issues/1395
+                recorder.recordAcceptorHost(rpcContext.getLocalAddressString());
+            }
+        }
+    }
+
+    @Override
+    protected void doInAfterTrace(SpanRecorder recorder, Object target, Object[] args, Object result, Throwable throwable) {
+        RpcInvocation invocation = (RpcInvocation) args[0];
+
+        recorder.recordApi(methodDescriptor);
+        recorder.recordAttribute(DubboConstants.DUBBO_ARGS_ANNOTATION_KEY, invocation.getArguments());
+
+        if (throwable == null) {
+            recorder.recordAttribute(DubboConstants.DUBBO_RESULT_ANNOTATION_KEY, result);
+        } else {
+            recorder.recordException(throwable);
+        }
+    }
+}
