de23563d118bf2b7f73c72e7f7361d222b50bf60
Jaehong Kim <jaehong.kim@navercorp.com>
2016-01-27 16:00:01 +0900

    fix gap of async&sibling node.

--- web/src/main/java/com/navercorp/pinpoint/web/calltree/span/CallTreeIterator.java
+++ web/src/main/java/com/navercorp/pinpoint/web/calltree/span/CallTreeIterator.java
@@ -79,56 +79,49 @@ public class CallTreeIterator implements Iterator<CallTreeNode> {
 
     public long getGap() {
         final CallTreeNode current = getCurrent();
-        final long lastExecuteTime = getLastExecuteTime();
-        final SpanAlign currentAlign = current.getValue();
-        if (currentAlign.isSpan()) {
-            return currentAlign.getStartTime() - lastExecuteTime;
+        if (current.isRoot()) {
+            return 0;
         }
 
-        if (currentAlign.isAsyncFirst()) {
+        if (current.getValue().isAsyncFirst()) {
             final CallTreeNode parent = getAsyncParent(current);
             if (parent == null) {
                 return 0;
             }
-            final SpanAlign parentAlign = parent.getValue();
-            return currentAlign.getStartTime() - parentAlign.getStartTime();
+            // skip sibling.
+            return current.getValue().getStartTime() - parent.getValue().getStartTime();
         }
 
-        return currentAlign.getStartTime() - lastExecuteTime;
-    }
-
-    public long getLastExecuteTime() {
-        final CallTreeNode current = getCurrent();
-        final SpanAlign currentAlign = current.getValue();
-        if (current.isRoot()) {
-            return currentAlign.getStartTime();
-        }
-
-        CallTreeNode prev = getPrev();
+        final CallTreeNode prev = getPrev();
         if (prev == null) {
             throw new IllegalStateException("A non-root CallTreeNode must have a previous node");
         }
-        final SpanAlign prevAlign = prev.getValue();
-        if (!currentAlign.isAsync() && !prevAlign.isSpan() && prevAlign.isAsync()) {
-            // skip sub(async) call tree.
-            prev = current.getParent();
+
+        return current.getValue().getStartTime() - getLastExecuteTime(current, prev);
+    }
+
+
+    public long getLastExecuteTime(final CallTreeNode current, final CallTreeNode prev) {
+        if (prev.getDepth() < current.getDepth()) {
+            // push and not closed.
+            return prev.getValue().getStartTime();
         }
 
-        if (prev.getDepth() == current.getDepth()) {
-            // equal
-            final SpanAlign align = prev.getValue();
-            return align.getLastTime();
-        }
-
+        CallTreeNode node = prev;
         if (prev.getDepth() > current.getDepth()) {
-            // less, back step
-            CallTreeNode sibling = getPrevSibling(current);
-            final SpanAlign align = sibling.getValue();
-            return align.getStartTime();
-        } else {
-            // bigger
-            final SpanAlign align = prev.getValue();
-            return align.getStartTime();
+            // pop prev sibling.
+            node = getPrevSibling(current);
+        }
+        while (true) {
+            if (!node.getValue().isAsyncFirst()) {
+                // not async first.
+                return node.getValue().getLastTime();
+            } else if (isFirstChild(node)) {
+                // first child
+                return node.getParent().getValue().getStartTime();
+            }
+            // pop prev sibling.
+            node = getPrevSibling(node);
         }
     }
 
@@ -136,15 +129,22 @@ public class CallTreeIterator implements Iterator<CallTreeNode> {
         CallTreeNode sibling = node.getParent().getChild();
         while (node != sibling.getSibling()) {
             sibling = sibling.getSibling();
+            if(sibling == null) {
+                throw new IllegalStateException("Not found prev sibling " + node);
+            }
         }
 
         return sibling;
     }
 
+    boolean isFirstChild(final CallTreeNode node) {
+        return node.getParent().getChild() == node;
+    }
+
     CallTreeNode getAsyncParent(final CallTreeNode node) {
         final int asyncId = node.getValue().getSpanEventBo().getAsyncId();
         CallTreeNode parent = node.getParent();
-        while (!parent.isRoot()) {
+        while (parent != null && !parent.isRoot()) {
             if (!parent.getValue().isSpan() && asyncId == parent.getValue().getSpanEventBo().getNextAsyncId()) {
                 return parent;
             }
---
+++ web/src/test/java/com/navercorp/pinpoint/web/calltree/span/CallStackMock.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright 2016 NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.navercorp.pinpoint.web.calltree.span;
+
+import com.navercorp.pinpoint.common.bo.SpanBo;
+import com.navercorp.pinpoint.common.bo.SpanEventBo;
+
+/**
+ * @author jaehong.kim
+ */
+public class CallStackMock {
+    private static final int STACK_SIZE = 8;
+    private static final int DEFAULT_INDEX = 0;
+    private static long currentTime = 1;
+
+    private SpanEventBo[] stack = new SpanEventBo[STACK_SIZE];
+
+    private final SpanBo spanBo;
+    private int index = DEFAULT_INDEX;
+    private short sequence;
+    private int latestStackIndex = 0;
+
+    private boolean async = false;
+    private int asyncId = 0;
+    private CallTree callTree;
+
+    public CallStackMock() {
+        this(false, -1);
+    }
+
+    public CallStackMock(boolean async, int asyncId) {
+        this.spanBo = new SpanBo();
+        this.spanBo.setStartTime(currentTime);
+        currentTime++;
+
+        this.async = async;
+        if (this.async) {
+            callTree = new SpanAsyncCallTree(new SpanAlign(spanBo));
+        } else {
+            callTree = new SpanCallTree(new SpanAlign(spanBo));
+        }
+        this.asyncId = asyncId;
+    }
+
+    public void push() {
+        final SpanEventBo spanEvent = new SpanEventBo();
+        final int startElapsed = (int) (currentTime - spanBo.getStartTime());
+        currentTime++;
+        spanEvent.setStartElapsed(startElapsed);
+        spanEvent.setSequence(sequence++);
+
+        if (this.async) {
+            spanEvent.setAsyncId(asyncId);
+        }
+
+        checkExtend(index + 1);
+        stack[index++] = spanEvent;
+        if (this.latestStackIndex != this.index) {
+            this.latestStackIndex = this.index;
+            spanEvent.setDepth(this.latestStackIndex);
+        }
+
+        callTree.add(spanEvent.getDepth(), new SpanAlign(spanBo, spanEvent));
+    }
+
+    private void checkExtend(final int size) {
+        final SpanEventBo[] originalStack = this.stack;
+        if (size >= originalStack.length) {
+            final int copyStackSize = originalStack.length << 1;
+            final SpanEventBo[] copyStack = new SpanEventBo[copyStackSize];
+            System.arraycopy(originalStack, 0, copyStack, 0, originalStack.length);
+            this.stack = copyStack;
+        }
+    }
+
+    public SpanEventBo pop() {
+        final SpanEventBo spanEvent = peek();
+        if (spanEvent != null) {
+            stack[index - 1] = null;
+            index--;
+            final int endElapsed = (int) (currentTime - (spanBo.getStartTime() + spanEvent.getStartElapsed()));
+            spanEvent.setEndElapsed(endElapsed);
+        }
+
+        return spanEvent;
+    }
+
+    public SpanEventBo peek() {
+        if (index == DEFAULT_INDEX) {
+            return null;
+        }
+
+        return stack[index - 1];
+    }
+
+    public boolean empty() {
+        if (this.async) {
+            return index == DEFAULT_INDEX + 1;
+        }
+
+        return index == DEFAULT_INDEX;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void append(CallTree subCallTree) {
+        if (peek() == null) {
+            callTree.add(0, subCallTree);
+        } else {
+            callTree.add(peek().getDepth(), subCallTree);
+        }
+    }
+
+    public CallTree close() {
+        if (this.async) {
+            pop();
+        }
+        final int after = (int) (currentTime - spanBo.getStartTime());
+        spanBo.setElapsed(after);
+        return callTree;
+    }
+}
