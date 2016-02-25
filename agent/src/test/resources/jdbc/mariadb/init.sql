471a798b2393c1366e75361fd1af2047a3b219e5
HyunGil Jeong <hyungil.jeong@navercorp.com>
2016-02-25 17:57:21 +0900

    #1541 Add tests for MariaDB stored procedure calls

--- agent/src/test/resources/jdbc/mariadb/init.sql
+++ agent/src/test/resources/jdbc/mariadb/init.sql
@@ -3,3 +3,12 @@ CREATE TABLE IF NOT EXISTS playground (id int(5) NOT NULL, name varchar(50) DEFA
 INSERT INTO playground VALUES (1, 'ONE');
 INSERT INTO playground VALUES (2, 'TWO');
 INSERT INTO playground VALUES (3, 'THREE');
+
+DELIMITER //
+CREATE PROCEDURE getPlaygroundByName (IN inputParamName VARCHAR(50), OUT outputParamCount INT)
+    BEGIN
+        SELECT count(*) INTO outputParamCount FROM playground;
+        SELECT id, name FROM playground WHERE name = inputParamName ORDER BY id ASC;
+    END;
+//
+DELIMITER ;
