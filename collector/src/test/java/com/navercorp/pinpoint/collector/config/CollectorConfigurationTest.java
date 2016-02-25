634c8d75aa0ebe11b0e5cab289964958f975963e
koo.taejin <koo.taejin@navercorp.com>
2016-02-25 15:35:38 +0900

    allow whitespace for collector.l4.ip configuration in pinpoint-collector.properties #1553

--- collector/src/test/java/com/navercorp/pinpoint/collector/config/CollectorConfigurationTest.java
+++ collector/src/test/java/com/navercorp/pinpoint/collector/config/CollectorConfigurationTest.java
@@ -16,9 +16,11 @@
 
 package com.navercorp.pinpoint.collector.config;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.InputStream;
+import java.util.Properties;
 
 /**
  * @author emeroad
@@ -30,4 +32,20 @@ public class CollectorConfigurationTest {
 
 
     }
+
+    @Test
+    public void l4IpTest() throws Exception {
+        Properties properties = new Properties();
+        properties.put("collector.l4.ip", "127.0.0.1 , 192.168.0.1, 255.255.255.255");
+
+        CollectorConfiguration collectorConfiguration = new CollectorConfiguration();
+        collectorConfiguration.setProperties(properties);
+
+        collectorConfiguration.afterPropertiesSet();
+
+        Assert.assertTrue(collectorConfiguration.getL4IpList().contains("127.0.0.1"));
+        Assert.assertTrue(collectorConfiguration.getL4IpList().contains("192.168.0.1"));
+        Assert.assertTrue(collectorConfiguration.getL4IpList().contains("255.255.255.255"));
+    }
+
 }
