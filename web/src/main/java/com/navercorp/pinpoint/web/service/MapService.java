de231360c128f9444daf23edc3af5b06e0c2625d
koo.taejin <koo.taejin@navercorp.com>
2016-03-10 14:41:09 +0900

    add getResponseTimeHistogramData restful api. #1589

--- web/src/main/java/com/navercorp/pinpoint/web/service/MapService.java
+++ web/src/main/java/com/navercorp/pinpoint/web/service/MapService.java
@@ -17,6 +17,7 @@
 package com.navercorp.pinpoint.web.service;
 
 import com.navercorp.pinpoint.web.applicationmap.ApplicationMap;
+import com.navercorp.pinpoint.web.view.ApplicationTimeHistogramViewModel;
 import com.navercorp.pinpoint.web.vo.Application;
 import com.navercorp.pinpoint.web.vo.Range;
 import com.navercorp.pinpoint.web.vo.SearchOption;
@@ -33,5 +34,7 @@ public interface MapService {
      * @return
      */
     ApplicationMap selectApplicationMap(Application sourceApplication, Range range, SearchOption searchOption);
+
+    ApplicationTimeHistogramViewModel selectResponseTimeHistogramData(Application application, Range range);
 
 }
