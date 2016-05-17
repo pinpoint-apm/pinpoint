f58f92e676c84d8d3825c2baca53630402e44c6c
koo.taejin <koo.taejin@navercorp.com>
2016-05-17 14:18:29 +0900

    extract tools module #1774 extract tools mudule.

--- tools/src/main/java/com/navercorp/pinpoint/tools/NetworkAvailabilityChecker.java
+++ tools/src/main/java/com/navercorp/pinpoint/tools/NetworkAvailabilityChecker.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.navercorp.pinpoint.profiler.tools;
+package com.navercorp.pinpoint.tools;
 
 import com.navercorp.pinpoint.bootstrap.config.DefaultProfilerConfig;
 import com.navercorp.pinpoint.bootstrap.config.ProfilerConfig;
--- tools/src/main/java/com/navercorp/pinpoint/tools/PinpointTools.java
+++ tools/src/main/java/com/navercorp/pinpoint/tools/PinpointTools.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.navercorp.pinpoint.profiler.tools;
+package com.navercorp.pinpoint.tools;
 
 /**
  * Marker interface to prevent obfuscation
