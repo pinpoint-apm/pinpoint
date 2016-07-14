2fe23d827b353f16f70efe7f74cd57e52b9e20a9
koo.taejin <koo.taejin@navercorp.com>
2016-07-14 18:09:55 +0900

    [no issue] fix test case.

--- commons-server/src/test/java/com/navercorp/pinpoint/common/server/util/DisruptorExecutorTest.java
+++ commons-server/src/test/java/com/navercorp/pinpoint/common/server/util/DisruptorExecutorTest.java
@@ -25,7 +25,6 @@ public class DisruptorExecutorTest {
         ExecutorService executor = DisruptorExecutors.newSingleProducerExecutor(2, bufferSize, threadFactory);
     }
 
-
     @Test
     public void simpleTest() throws InterruptedException {
         int bufferSize = 10;
@@ -33,11 +32,13 @@ public class DisruptorExecutorTest {
 
         ExecutorService executor = DisruptorExecutors.newSingleProducerExecutor(1, newBufferSize, threadFactory, new TimeoutBlockingWaitStrategy(10000, TimeUnit.MILLISECONDS));
 
-        CountDownLatch latch = new CountDownLatch(1);
-        execute(newBufferSize, executor, latch);
+        CountDownLatch startLatch = new CountDownLatch(1);
+        CountDownLatch endLatch = new CountDownLatch(1);
+        execute(newBufferSize, executor, startLatch, endLatch);
+        startLatch.await();
 
         List<Runnable> runnableList = executor.shutdownNow();
-        latch.countDown();
+        endLatch.countDown();
 
         Assert.assertEquals(newBufferSize - 1, runnableList.size());
         Assert.assertTrue(executor.isShutdown());
@@ -81,23 +82,33 @@ public class DisruptorExecutorTest {
     }
 
     private void execute(int newBufferSize, ExecutorService executor, CountDownLatch latch) {
+        execute(newBufferSize, executor, null, latch);
+    }
+
+    private void execute(int newBufferSize, ExecutorService executor, CountDownLatch startLatch, CountDownLatch endLatch) {
         for (int i = 0; i < newBufferSize; i++) {
-            executor.execute(new LatchAwaitRunnable(latch));
+            executor.execute(new LatchAwaitRunnable(startLatch, endLatch));
         }
     }
 
     class LatchAwaitRunnable implements Runnable {
 
-        private final CountDownLatch latch;
+        private final CountDownLatch startLatch;
+        private final CountDownLatch endLatch;
 
-        public LatchAwaitRunnable(CountDownLatch latch) {
-            this.latch = latch;
+        public LatchAwaitRunnable(CountDownLatch startLatch, CountDownLatch endLatch) {
+            this.startLatch = startLatch;
+            this.endLatch = endLatch;
         }
 
         @Override
         public void run() {
+            if (startLatch != null) {
+                startLatch.countDown();
+            }
+
             try {
-                latch.await();
+                endLatch.await();
             } catch (InterruptedException e) {
             }
         }
