72595a44d29bcdebebf021da71e08d6b4a9f06c0
emeroad <wd.kang@navercorp.com>
2016-09-12 14:59:29 +0900

    remove duplicate code

--- bootstrap-core/src/main/java/com/navercorp/pinpoint/bootstrap/plugin/jdbc/bindvalue/Types.java
+++ bootstrap-core/src/main/java/com/navercorp/pinpoint/bootstrap/plugin/jdbc/bindvalue/Types.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 NAVER Corp.
+ * Copyright 2016 NAVER Corp.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,9 +12,10 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
 
-package com.navercorp.pinpoint.profiler.util.bindvalue;
+package com.navercorp.pinpoint.bootstrap.plugin.jdbc.bindvalue;
 
 import java.lang.reflect.Field;
 import java.util.HashMap;
