ca3707ff1f66ecd75497fd96e68b0518ee2e1403
emeroad <wd.kang@navercorp.com>
2016-10-18 18:15:48 +0900

    Polish Jboss Plugin  - cleanup config

--- bootstrap-core/src/main/java/com/navercorp/pinpoint/bootstrap/config/ProfilerConfig.java
+++ bootstrap-core/src/main/java/com/navercorp/pinpoint/bootstrap/config/ProfilerConfig.java
@@ -113,18 +113,5 @@ public interface ProfilerConfig {
     boolean readBoolean(String propertyName, boolean defaultValue);
 
     Map<String, String> readPattern(String propertyNamePatternRegex);
-    
-    boolean isJbossTraceEjb();
 
-    String getJbossRealIpHeader();
-
-    String getJbossRealIpEmptyValue();
-
-    boolean isJbossTraceRequestParam();
-
-    Filter<String> getJbossExcludeProfileMethodFilter();
-
-    boolean isJbossHidePinpointHeader();
-
-    Filter<String> getJbossExcludeUrlFilter();
 }
