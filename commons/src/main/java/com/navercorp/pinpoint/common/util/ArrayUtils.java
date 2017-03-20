5450e1633af83f3f89f5758eae09c7b944585aef
emeroad <wd.kang@navercorp.com>
2017-03-20 17:30:18 +0900

    [#2800] Add isEmpty() API to ArrayUtils  - copy & modify apache commons lang3

--- commons/src/main/java/com/navercorp/pinpoint/common/util/ArrayUtils.java
+++ commons/src/main/java/com/navercorp/pinpoint/common/util/ArrayUtils.java
@@ -72,4 +72,456 @@ public final class ArrayUtils {
             sb.append(", ");
         }
     }
+
+
+    // copy apache commons lang3 ----------------------------------------------------------------------
+    /**
+     * <p>Checks if an array of Objects is empty or {@code null}.
+     *
+     * @param array  the array to test
+     * @return {@code true} if the array is empty or {@code null}
+     * @since 2.1
+     */
+    public static <T> boolean isEmpty(final T[] array) {
+        return getLength(array) == 0;
+    }
+
+    /**
+     * <p>Checks if an array of primitive longs is empty or {@code null}.
+     *
+     * @param array  the array to test
+     * @return {@code true} if the array is empty or {@code null}
+     * @since 2.1
+     */
+    public static boolean isEmpty(final long[] array) {
+        return getLength(array) == 0;
+    }
+
+    /**
+     * <p>Checks if an array of primitive ints is empty or {@code null}.
+     *
+     * @param array  the array to test
+     * @return {@code true} if the array is empty or {@code null}
+     * @since 2.1
+     */
+    public static boolean isEmpty(final int[] array) {
+        return getLength(array) == 0;
+    }
+
+    /**
+     * <p>Checks if an array of primitive shorts is empty or {@code null}.
+     *
+     * @param array  the array to test
+     * @return {@code true} if the array is empty or {@code null}
+     * @since 2.1
+     */
+    public static boolean isEmpty(final short[] array) {
+        return getLength(array) == 0;
+    }
+
+    /**
+     * <p>Checks if an array of primitive chars is empty or {@code null}.
+     *
+     * @param array  the array to test
+     * @return {@code true} if the array is empty or {@code null}
+     * @since 2.1
+     */
+    public static boolean isEmpty(final char[] array) {
+        return getLength(array) == 0;
+    }
+
+    /**
+     * <p>Checks if an array of primitive bytes is empty or {@code null}.
+     *
+     * @param array  the array to test
+     * @return {@code true} if the array is empty or {@code null}
+     * @since 2.1
+     */
+    public static boolean isEmpty(final byte[] array) {
+        return getLength(array) == 0;
+    }
+
+    /**
+     * <p>Checks if an array of primitive doubles is empty or {@code null}.
+     *
+     * @param array  the array to test
+     * @return {@code true} if the array is empty or {@code null}
+     * @since 2.1
+     */
+    public static boolean isEmpty(final double[] array) {
+        return getLength(array) == 0;
+    }
+
+    /**
+     * <p>Checks if an array of primitive floats is empty or {@code null}.
+     *
+     * @param array  the array to test
+     * @return {@code true} if the array is empty or {@code null}
+     * @since 2.1
+     */
+    public static boolean isEmpty(final float[] array) {
+        return getLength(array) == 0;
+    }
+
+    /**
+     * <p>Checks if an array of primitive booleans is empty or {@code null}.
+     *
+     * @param array  the array to test
+     * @return {@code true} if the array is empty or {@code null}
+     * @since 2.1
+     */
+    public static boolean isEmpty(final boolean[] array) {
+        return getLength(array) == 0;
+    }
+
+
+    // copy apache commons lang3 ----------------------------------------------------------------------
+    /**
+     * <p>Checks if an array of Objects is not empty and not {@code null}.
+     *
+     * @param <T> the component type of the array
+     * @param array  the array to test
+     * @return {@code true} if the array is not empty and not {@code null}
+     * @since 2.5
+     */
+    public static <T> boolean isNotEmpty(final T[] array) {
+        return !isEmpty(array);
+    }
+
+    /**
+     * <p>Checks if an array of primitive longs is not empty and not {@code null}.
+     *
+     * @param array  the array to test
+     * @return {@code true} if the array is not empty and not {@code null}
+     * @since 2.5
+     */
+    public static boolean isNotEmpty(final long[] array) {
+        return !isEmpty(array);
+    }
+
+    /**
+     * <p>Checks if an array of primitive ints is not empty and not {@code null}.
+     *
+     * @param array  the array to test
+     * @return {@code true} if the array is not empty and not {@code null}
+     * @since 2.5
+     */
+    public static boolean isNotEmpty(final int[] array) {
+        return !isEmpty(array);
+    }
+
+    /**
+     * <p>Checks if an array of primitive shorts is not empty and not {@code null}.
+     *
+     * @param array  the array to test
+     * @return {@code true} if the array is not empty and not {@code null}
+     * @since 2.5
+     */
+    public static boolean isNotEmpty(final short[] array) {
+        return !isEmpty(array);
+    }
+
+    /**
+     * <p>Checks if an array of primitive chars is not empty and not {@code null}.
+     *
+     * @param array  the array to test
+     * @return {@code true} if the array is not empty and not {@code null}
+     * @since 2.5
+     */
+    public static boolean isNotEmpty(final char[] array) {
+        return !isEmpty(array);
+    }
+
+    /**
+     * <p>Checks if an array of primitive bytes is not empty and not {@code null}.
+     *
+     * @param array  the array to test
+     * @return {@code true} if the array is not empty and not {@code null}
+     * @since 2.5
+     */
+    public static boolean isNotEmpty(final byte[] array) {
+        return !isEmpty(array);
+    }
+
+    /**
+     * <p>Checks if an array of primitive doubles is not empty and not {@code null}.
+     *
+     * @param array  the array to test
+     * @return {@code true} if the array is not empty and not {@code null}
+     * @since 2.5
+     */
+    public static boolean isNotEmpty(final double[] array) {
+        return !isEmpty(array);
+    }
+
+    /**
+     * <p>Checks if an array of primitive floats is not empty and not {@code null}.
+     *
+     * @param array  the array to test
+     * @return {@code true} if the array is not empty and not {@code null}
+     * @since 2.5
+     */
+    public static boolean isNotEmpty(final float[] array) {
+        return !isEmpty(array);
+    }
+
+    /**
+     * <p>Checks if an array of primitive booleans is not empty and not {@code null}.
+     *
+     * @param array  the array to test
+     * @return {@code true} if the array is not empty and not {@code null}
+     * @since 2.5
+     */
+    public static boolean isNotEmpty(final boolean[] array) {
+        return !isEmpty(array);
+    }
+
+    //---------------------------------------------------------------------------
+
+
+    // Enhancement type safety & Remove native call ---------------------------------------------------------------------------
+    /**
+     * <p>Returns the length of the specified array.
+     * This method can deal with {@code Object} arrays and with primitive arrays.
+     *
+     * <p>If the input array is {@code null}, {@code 0} is returned.
+     *
+     * <pre>
+     *
+     * ArrayUtils.getLength(null)            = 0
+     * ArrayUtils.getLength([])              = 0
+     * ArrayUtils.getLength([null])          = 1
+     * ArrayUtils.getLength([true, false])   = 2
+     * ArrayUtils.getLength([1, 2, 3])       = 3
+     * ArrayUtils.getLength(["a", "b", "c"]) = 3
+     * </pre>
+     *
+     * @param array  the array to retrieve the length from, may be null
+     * @return The length of the array, or {@code 0} if the array is {@code null}
+     * @throws IllegalArgumentException if the object argument is not an array.
+     * @since 2.1
+     */
+    public static int getLength(final long[] array) {
+        if (array == null) {
+            return 0;
+        }
+        return array.length;
+    }
+
+    /**
+     * <p>Returns the length of the specified array.
+     * This method can deal with {@code Object} arrays and with primitive arrays.
+     *
+     * <p>If the input array is {@code null}, {@code 0} is returned.
+     *
+     * <pre>
+     * ArrayUtils.getLength(null)            = 0
+     * ArrayUtils.getLength([])              = 0
+     * ArrayUtils.getLength([null])          = 1
+     * ArrayUtils.getLength([true, false])   = 2
+     * ArrayUtils.getLength([1, 2, 3])       = 3
+     * ArrayUtils.getLength(["a", "b", "c"]) = 3
+     * </pre>
+     *
+     * @param array  the array to retrieve the length from, may be null
+     * @return The length of the array, or {@code 0} if the array is {@code null}
+     * @throws IllegalArgumentException if the object argument is not an array.
+     * @since 2.1
+     */
+    public static int getLength(final int[] array) {
+        if (array == null) {
+            return 0;
+        }
+        return array.length;
+    }
+
+    /**
+     * <p>Returns the length of the specified array.
+     * This method can deal with {@code Object} arrays and with primitive arrays.
+     *
+     * <p>If the input array is {@code null}, {@code 0} is returned.
+     *
+     * <pre>
+     * ArrayUtils.getLength(null)            = 0
+     * ArrayUtils.getLength([])              = 0
+     * ArrayUtils.getLength([null])          = 1
+     * ArrayUtils.getLength([true, false])   = 2
+     * ArrayUtils.getLength([1, 2, 3])       = 3
+     * ArrayUtils.getLength(["a", "b", "c"]) = 3
+     * </pre>
+     *
+     * @param array  the array to retrieve the length from, may be null
+     * @return The length of the array, or {@code 0} if the array is {@code null}
+     * @throws IllegalArgumentException if the object argument is not an array.
+     * @since 2.1
+     */
+    public static int getLength(final short[] array) {
+        if (array == null) {
+            return 0;
+        }
+        return array.length;
+    }
+
+    /**
+     * <p>Returns the length of the specified array.
+     * This method can deal with {@code Object} arrays and with primitive arrays.
+     *
+     * <p>If the input array is {@code null}, {@code 0} is returned.
+     *
+     * <pre>
+     * ArrayUtils.getLength(null)            = 0
+     * ArrayUtils.getLength([])              = 0
+     * ArrayUtils.getLength([null])          = 1
+     * ArrayUtils.getLength([true, false])   = 2
+     * ArrayUtils.getLength([1, 2, 3])       = 3
+     * ArrayUtils.getLength(["a", "b", "c"]) = 3
+     * </pre>
+     *
+     * @param array  the array to retrieve the length from, may be null
+     * @return The length of the array, or {@code 0} if the array is {@code null}
+     * @throws IllegalArgumentException if the object argument is not an array.
+     * @since 2.1
+     */
+    public static int getLength(final char[] array) {
+        if (array == null) {
+            return 0;
+        }
+        return array.length;
+    }
+
+    /**
+     * <p>Returns the length of the specified array.
+     * This method can deal with {@code Object} arrays and with primitive arrays.
+     *
+     * <p>If the input array is {@code null}, {@code 0} is returned.
+     *
+     * <pre>
+     * ArrayUtils.getLength(null)            = 0
+     * ArrayUtils.getLength([])              = 0
+     * ArrayUtils.getLength([null])          = 1
+     * ArrayUtils.getLength([true, false])   = 2
+     * ArrayUtils.getLength([1, 2, 3])       = 3
+     * ArrayUtils.getLength(["a", "b", "c"]) = 3
+     * </pre>
+     *
+     * @param array  the array to retrieve the length from, may be null
+     * @return The length of the array, or {@code 0} if the array is {@code null}
+     * @throws IllegalArgumentException if the object argument is not an array.
+     * @since 2.1
+     */
+    public static int getLength(final byte[] array) {
+        if (array == null) {
+            return 0;
+        }
+        return array.length;
+    }
+
+    /**
+     * <p>Returns the length of the specified array.
+     * This method can deal with {@code Object} arrays and with primitive arrays.
+     *
+     * <p>If the input array is {@code null}, {@code 0} is returned.
+     *
+     * <pre>
+     * ArrayUtils.getLength(null)            = 0
+     * ArrayUtils.getLength([])              = 0
+     * ArrayUtils.getLength([null])          = 1
+     * ArrayUtils.getLength([true, false])   = 2
+     * ArrayUtils.getLength([1, 2, 3])       = 3
+     * ArrayUtils.getLength(["a", "b", "c"]) = 3
+     * </pre>
+     *
+     * @param array  the array to retrieve the length from, may be null
+     * @return The length of the array, or {@code 0} if the array is {@code null}
+     * @throws IllegalArgumentException if the object argument is not an array.
+     * @since 2.1
+     */
+    public static int getLength(final double[] array) {
+        if (array == null) {
+            return 0;
+        }
+        return array.length;
+    }
+
+    /**
+     * <p>Returns the length of the specified array.
+     * This method can deal with {@code Object} arrays and with primitive arrays.
+     *
+     * <p>If the input array is {@code null}, {@code 0} is returned.
+     *
+     * <pre>
+     * ArrayUtils.getLength(null)            = 0
+     * ArrayUtils.getLength([])              = 0
+     * ArrayUtils.getLength([null])          = 1
+     * ArrayUtils.getLength([true, false])   = 2
+     * ArrayUtils.getLength([1, 2, 3])       = 3
+     * ArrayUtils.getLength(["a", "b", "c"]) = 3
+     * </pre>
+     *
+     * @param array  the array to retrieve the length from, may be null
+     * @return The length of the array, or {@code 0} if the array is {@code null}
+     * @throws IllegalArgumentException if the object argument is not an array.
+     * @since 2.1
+     */
+    public static int getLength(final float[] array) {
+        if (array == null) {
+            return 0;
+        }
+        return array.length;
+    }
+
+    /**
+     * <p>Returns the length of the specified array.
+     * This method can deal with {@code Object} arrays and with primitive arrays.
+     *
+     * <p>If the input array is {@code null}, {@code 0} is returned.
+     *
+     * <pre>
+     * ArrayUtils.getLength(null)            = 0
+     * ArrayUtils.getLength([])              = 0
+     * ArrayUtils.getLength([null])          = 1
+     * ArrayUtils.getLength([true, false])   = 2
+     * ArrayUtils.getLength([1, 2, 3])       = 3
+     * ArrayUtils.getLength(["a", "b", "c"]) = 3
+     * </pre>
+     *
+     * @param array  the array to retrieve the length from, may be null
+     * @return The length of the array, or {@code 0} if the array is {@code null}
+     * @throws IllegalArgumentException if the object argument is not an array.
+     * @since 2.1
+     */
+    public static int getLength(final boolean[] array) {
+        if (array == null) {
+            return 0;
+        }
+        return array.length;
+    }
+
+    /**
+     * <p>Returns the length of the specified array.
+     * This method can deal with {@code Object} arrays and with primitive arrays.
+     *
+     * <p>If the input array is {@code null}, {@code 0} is returned.
+     *
+     * <pre>
+     * ArrayUtils.getLength(null)            = 0
+     * ArrayUtils.getLength([])              = 0
+     * ArrayUtils.getLength([null])          = 1
+     * ArrayUtils.getLength([true, false])   = 2
+     * ArrayUtils.getLength([1, 2, 3])       = 3
+     * ArrayUtils.getLength(["a", "b", "c"]) = 3
+     * </pre>
+     *
+     * @param array  the array to retrieve the length from, may be null
+     * @return The length of the array, or {@code 0} if the array is {@code null}
+     * @throws IllegalArgumentException if the object argument is not an array.
+     * @since 2.1
+     */
+    public static <T> int getLength(final T[] array) {
+        if (array == null) {
+            return 0;
+        }
+        return array.length;
+    }
+
 }
