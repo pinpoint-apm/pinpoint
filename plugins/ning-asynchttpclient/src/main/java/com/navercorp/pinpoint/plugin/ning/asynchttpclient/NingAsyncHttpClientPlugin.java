97cae2e9d8ed478b6104450d139608222fe0e6fd
jaehong-kim <jaehong.kim@navercorp.com>
2017-06-14 15:21:46 +0900

    [#3036] Add support for ning asynchttpclient 1.9.x and 2.x versions.

--- plugins/ning-asynchttpclient/src/main/java/com/navercorp/pinpoint/plugin/ning/asynchttpclient/NingAsyncHttpClientPlugin.java
+++ plugins/ning-asynchttpclient/src/main/java/com/navercorp/pinpoint/plugin/ning/asynchttpclient/NingAsyncHttpClientPlugin.java
@@ -26,6 +26,8 @@ import com.navercorp.pinpoint.bootstrap.instrument.Instrumentor;
 import com.navercorp.pinpoint.bootstrap.instrument.transformer.TransformCallback;
 import com.navercorp.pinpoint.bootstrap.instrument.transformer.TransformTemplate;
 import com.navercorp.pinpoint.bootstrap.instrument.transformer.TransformTemplateAware;
+import com.navercorp.pinpoint.bootstrap.logging.PLogger;
+import com.navercorp.pinpoint.bootstrap.logging.PLoggerFactory;
 import com.navercorp.pinpoint.bootstrap.plugin.ProfilerPlugin;
 import com.navercorp.pinpoint.bootstrap.plugin.ProfilerPluginSetupContext;
 import com.navercorp.pinpoint.bootstrap.plugin.util.InstrumentUtils;
@@ -39,6 +41,7 @@ import com.navercorp.pinpoint.common.trace.ServiceTypeFactory;
  * @author jaehong.kim
  */
 public class NingAsyncHttpClientPlugin implements ProfilerPlugin, TransformTemplateAware {
+    private final PLogger logger = PLoggerFactory.getLogger(this.getClass());
     public static final ServiceType ASYNC_HTTP_CLIENT = ServiceTypeFactory.of(9056, "ASYNC_HTTP_CLIENT", RECORD_STATISTICS);
     public static final ServiceType ASYNC_HTTP_CLIENT_INTERNAL = ServiceTypeFactory.of(9057, "ASYNC_HTTP_CLIENT_INTERNAL", "ASYNC_HTTP_CLIENT");
 
@@ -46,6 +49,27 @@ public class NingAsyncHttpClientPlugin implements ProfilerPlugin, TransformTempl
 
     @Override
     public void setup(ProfilerPluginSetupContext context) {
+        final NingAsyncHttpClientPluginConfig config = new NingAsyncHttpClientPluginConfig(context.getConfig());
+        if (!config.isEnable()) {
+            if (logger.isInfoEnabled()) {
+                logger.info("NingAsyncHttpClientPlugin disabled");
+            }
+            return;
+        }
+        if (logger.isInfoEnabled()) {
+            logger.info("Add NingAsyncHttpClientPlugin {}", config);
+        }
+
+        // 1.8.x ~ 1.9.x
+        logger.debug("Add AsyncHttpClient(1.8.x ~ 1.9.x)");
+        addAsyncHttpClientTransformer();
+
+        // 2.x
+        logger.debug("Add DefaultAsyncHttpClient(2.x ~");
+        addDefaultAsyncHttpClientTransformer();
+    }
+
+    private void addAsyncHttpClientTransformer() {
         transformTemplate.transform("com.ning.http.client.AsyncHttpClient", new TransformCallback() {
 
             @Override
@@ -54,6 +78,20 @@ public class NingAsyncHttpClientPlugin implements ProfilerPlugin, TransformTempl
 
                 final InstrumentMethod executeRequestMethod = InstrumentUtils.findMethod(target, "executeRequest", "com.ning.http.client.Request", "com.ning.http.client.AsyncHandler");
                 executeRequestMethod.addInterceptor("com.navercorp.pinpoint.plugin.ning.asynchttpclient.interceptor.ExecuteRequestInterceptor");
+                return target.toBytecode();
+            }
+        });
+    }
+
+    private void addDefaultAsyncHttpClientTransformer() {
+        transformTemplate.transform("org.asynchttpclient.DefaultAsyncHttpClient", new TransformCallback() {
+
+            @Override
+            public byte[] doInTransform(Instrumentor instrumentor, ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws InstrumentException {
+                InstrumentClass target = instrumentor.getInstrumentClass(loader, className, classfileBuffer);
+
+                final InstrumentMethod executeRequestMethod = InstrumentUtils.findMethod(target, "execute", "org.asynchttpclient.Request", "org.asynchttpclient.AsyncHandler");
+                executeRequestMethod.addInterceptor("com.navercorp.pinpoint.plugin.ning.asynchttpclient.interceptor.ExecuteInterceptor");
                 return target.toBytecode();
             }
         });
