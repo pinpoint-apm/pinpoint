89cf11be862620aefafea061a6d50802255618b6
denz <hyojong.song2015@navercorp.com>
2017-06-26 14:53:45 +0900

    [#noissue] remove not used data

--- web/src/main/java/com/navercorp/pinpoint/web/applicationmap/ApplicationMapWithScatterData.java
+++ web/src/main/java/com/navercorp/pinpoint/web/applicationmap/ApplicationMapWithScatterData.java
@@ -16,7 +16,7 @@
 package com.navercorp.pinpoint.web.applicationmap;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
 import com.navercorp.pinpoint.web.scatter.ScatterData;
 import com.navercorp.pinpoint.web.vo.Application;
 
@@ -36,18 +36,17 @@ public class ApplicationMapWithScatterData implements ApplicationMap {
         this.applicationScatterDataMap = applicationScatterDataMap;
     }
 
-    @JsonProperty("nodeDataArray")
     @Override
     public Collection<Node> getNodes() {
         return applicationMap.getNodes();
     }
 
-    @JsonProperty("linkDataArray")
     @Override
     public Collection<Link> getLinks() {
         return applicationMap.getLinks();
     }
-    
+
+    @JsonValue
     public ApplicationMap getApplicationMap() {
         return applicationMap;
     }
