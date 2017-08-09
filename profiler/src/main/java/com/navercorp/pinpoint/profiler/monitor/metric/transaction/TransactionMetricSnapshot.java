4beaecc6b81f86d74cd7357c5a9f40008860369b
HyunGil Jeong <hyungil.jeong@navercorp.com>
2017-08-09 19:26:58 +0900

    [#3219] Remove gauges when collecting transaction counts

---
+++ profiler/src/main/java/com/navercorp/pinpoint/profiler/monitor/metric/transaction/TransactionMetricSnapshot.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2017 NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.navercorp.pinpoint.profiler.monitor.metric.transaction;
+
+/**
+ * @author HyunGil Jeong
+ */
+public class TransactionMetricSnapshot {
+
+    private final long sampledNewCount;
+    private final long sampledContinuationCount;
+    private final long unsampledNewCount;
+    private final long unsampledContinuationCount;
+
+    public TransactionMetricSnapshot(long sampledNewCount,
+                                     long sampledContinuationCount,
+                                     long unsampledNewCount,
+                                     long unsampledContinuationCount) {
+        this.sampledNewCount = sampledNewCount;
+        this.sampledContinuationCount = sampledContinuationCount;
+        this.unsampledNewCount = unsampledNewCount;
+        this.unsampledContinuationCount = unsampledContinuationCount;
+    }
+
+    public long getSampledNewCount() {
+        return sampledNewCount;
+    }
+
+    public long getSampledContinuationCount() {
+        return sampledContinuationCount;
+    }
+
+    public long getUnsampledNewCount() {
+        return unsampledNewCount;
+    }
+
+    public long getUnsampledContinuationCount() {
+        return unsampledContinuationCount;
+    }
+}
