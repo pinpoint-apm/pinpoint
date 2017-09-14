6bcc304e29dc9bd1052eecb504711182ed78d39a
koo.taejin <koo.taejin@navercorp.com>
2017-09-14 17:20:06 +0900

    [#3211] fix bug

--- plugins/netty/src/main/java/com/navercorp/pinpoint/plugin/netty/NettyPlugin.java
+++ plugins/netty/src/main/java/com/navercorp/pinpoint/plugin/netty/NettyPlugin.java
@@ -114,7 +114,7 @@ public class NettyPlugin implements ProfilerPlugin, TransformTemplateAware {
 
             final InstrumentMethod writeMethod2 = InstrumentUtils.findMethod(target, "write", "java.lang.Object", "io.netty.channel.ChannelPromise");
             if (writeMethod2 != null) {
-                writeMethod1.addScopedInterceptor(NettyConstants.INTERCEPTOR_CHANNEL_PIPELINE_WRITE, NettyConstants.SCOPE_WRITE, ExecutionPolicy.BOUNDARY);
+                writeMethod2.addScopedInterceptor(NettyConstants.INTERCEPTOR_CHANNEL_PIPELINE_WRITE, NettyConstants.SCOPE_WRITE, ExecutionPolicy.BOUNDARY);
             }
 
             final InstrumentMethod writeAndFlushMethod1 = InstrumentUtils.findMethod(target, "writeAndFlush", "java.lang.Object");
@@ -168,7 +168,7 @@ public class NettyPlugin implements ProfilerPlugin, TransformTemplateAware {
 
             final InstrumentMethod addListenerMethod2 = InstrumentUtils.findMethod(target, "addListeners", "io.netty.util.concurrent.GenericFutureListener[]");
             if (addListenerMethod2 != null) {
-                addListenerMethod1.addScopedInterceptor(NettyConstants.INTERCEPTOR_CHANNEL_PROMISE_ADD_LISTENER, NettyConstants.SCOPE, ExecutionPolicy.BOUNDARY);
+                addListenerMethod2.addScopedInterceptor(NettyConstants.INTERCEPTOR_CHANNEL_PROMISE_ADD_LISTENER, NettyConstants.SCOPE, ExecutionPolicy.BOUNDARY);
             }
 
             return target.toBytecode();
