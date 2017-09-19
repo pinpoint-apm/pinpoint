637c8abec949bf4fbdcdb4c75fbe4c5d46c4d24d
koo.taejin <koo.taejin@navercorp.com>
2017-09-19 09:48:59 +0900

    [#cleanup]  remove duplicated code in stat codec

--- commons-server/src/main/java/com/navercorp/pinpoint/common/server/bo/codec/stat/v2/DeadlockCodecV2.java
+++ commons-server/src/main/java/com/navercorp/pinpoint/common/server/bo/codec/stat/v2/DeadlockCodecV2.java
@@ -21,109 +21,93 @@ import com.navercorp.pinpoint.common.server.bo.codec.stat.AgentStatCodec;
 import com.navercorp.pinpoint.common.server.bo.codec.stat.AgentStatDataPointCodec;
 import com.navercorp.pinpoint.common.server.bo.codec.stat.header.AgentStatHeaderDecoder;
 import com.navercorp.pinpoint.common.server.bo.codec.stat.header.AgentStatHeaderEncoder;
-import com.navercorp.pinpoint.common.server.bo.codec.stat.header.BitCountingHeaderDecoder;
 import com.navercorp.pinpoint.common.server.bo.codec.stat.header.BitCountingHeaderEncoder;
 import com.navercorp.pinpoint.common.server.bo.codec.stat.strategy.StrategyAnalyzer;
 import com.navercorp.pinpoint.common.server.bo.codec.stat.strategy.UnsignedIntegerEncodingStrategy;
-import com.navercorp.pinpoint.common.server.bo.codec.stat.strategy.UnsignedLongEncodingStrategy;
 import com.navercorp.pinpoint.common.server.bo.codec.strategy.EncodingStrategy;
-import com.navercorp.pinpoint.common.server.bo.serializer.stat.AgentStatDecodingContext;
 import com.navercorp.pinpoint.common.server.bo.stat.DeadlockBo;
-import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.util.Assert;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * @author Taejin Koo
  */
 @Component("deadlockCodecV2")
-public class DeadlockCodecV2 implements AgentStatCodec<DeadlockBo> {
-
-    private static final byte VERSION = 2;
-
-    private final AgentStatDataPointCodec codec;
+public class DeadlockCodecV2 extends AbstractAgentStatCodecV2<DeadlockBo> {
 
     @Autowired
     public DeadlockCodecV2(AgentStatDataPointCodec codec) {
-        Assert.notNull(codec, "agentStatDataPointCodec must not be null");
-        this.codec = codec;
+        super(codec);
     }
 
     @Override
-    public byte getVersion() {
-        return VERSION;
+    protected CodecEncoder createCodecEncoder() {
+        return new DeadlockCodecEncoder(codec);
     }
 
     @Override
-    public void encodeValues(Buffer valueBuffer, List<DeadlockBo> deadlockBos) {
-        if (CollectionUtils.isEmpty(deadlockBos)) {
-            throw new IllegalArgumentException("deadlockBos must not be empty");
-        }
-        final int numValues = deadlockBos.size();
-        valueBuffer.putVInt(numValues);
+    protected CodecDecoder createCodecDecoder() {
+        return new DeadlockCodecDecoder(codec);
+    }
 
-        List<Long> startTimestamps = new ArrayList<Long>(numValues);
-        List<Long> timestamps = new ArrayList<Long>(numValues);
-        UnsignedIntegerEncodingStrategy.Analyzer.Builder deadlockedThreadCountAnalyzerBuilder = new UnsignedIntegerEncodingStrategy.Analyzer.Builder();
-        for (DeadlockBo deadlockBo : deadlockBos) {
-            startTimestamps.add(deadlockBo.getStartTimestamp());
-            timestamps.add(deadlockBo.getTimestamp());
+    private static class DeadlockCodecEncoder implements AgentStatCodec.CodecEncoder<DeadlockBo> {
 
-            int deadlockedThreadCount = deadlockBo.getDeadlockedThreadCount();
-            deadlockedThreadCountAnalyzerBuilder.addValue(deadlockedThreadCount);
+        private final AgentStatDataPointCodec codec;
+        private final UnsignedIntegerEncodingStrategy.Analyzer.Builder deadlockedThreadCountAnalyzerBuilder = new UnsignedIntegerEncodingStrategy.Analyzer.Builder();
+
+        public DeadlockCodecEncoder(AgentStatDataPointCodec codec) {
+            this.codec = codec;
         }
 
-        this.codec.encodeValues(valueBuffer, UnsignedLongEncodingStrategy.REPEAT_COUNT, startTimestamps);
-        this.codec.encodeTimestamps(valueBuffer, timestamps);
-        this.encodeDataPoints(valueBuffer, deadlockedThreadCountAnalyzerBuilder.build());
+        @Override
+        public void addValue(DeadlockBo deadlockBo) {
+            deadlockedThreadCountAnalyzerBuilder.addValue(deadlockBo.getDeadlockedThreadCount());
+        }
+
+        @Override
+        public void encode(AgentStatDataPointCodec codec, Buffer valueBuffer) {
+            StrategyAnalyzer<Integer> deadlockedThreadIdAnalyzer = deadlockedThreadCountAnalyzerBuilder.build();
+
+            // encode header
+            AgentStatHeaderEncoder headerEncoder = new BitCountingHeaderEncoder();
+            headerEncoder.addCode(deadlockedThreadIdAnalyzer.getBestStrategy().getCode());
+
+            final byte[] header = headerEncoder.getHeader();
+            valueBuffer.putPrefixedBytes(header);
+
+            // encode values
+            this.codec.encodeValues(valueBuffer, deadlockedThreadIdAnalyzer.getBestStrategy(), deadlockedThreadIdAnalyzer.getValues());
+        }
+
     }
 
-    private void encodeDataPoints(Buffer valueBuffer, StrategyAnalyzer<Integer> deadlockedThreadIdAnalyzer) {
-        // encode header
-        AgentStatHeaderEncoder headerEncoder = new BitCountingHeaderEncoder();
-        headerEncoder.addCode(deadlockedThreadIdAnalyzer.getBestStrategy().getCode());
+    private static class DeadlockCodecDecoder implements AgentStatCodec.CodecDecoder<DeadlockBo> {
 
-        final byte[] header = headerEncoder.getHeader();
-        valueBuffer.putPrefixedBytes(header);
+        private final AgentStatDataPointCodec codec;
 
-        // encode values
-        this.codec.encodeValues(valueBuffer, deadlockedThreadIdAnalyzer.getBestStrategy(), deadlockedThreadIdAnalyzer.getValues());
-    }
+        private List<Integer> deadlockedThreadCountList;
 
-    @Override
-    public List<DeadlockBo> decodeValues(Buffer valueBuffer, AgentStatDecodingContext decodingContext) {
-        final String agentId = decodingContext.getAgentId();
-        final long baseTimestamp = decodingContext.getBaseTimestamp();
-        final long timestampDelta = decodingContext.getTimestampDelta();
-        final long initialTimestamp = baseTimestamp + timestampDelta;
+        public DeadlockCodecDecoder(AgentStatDataPointCodec codec) {
+            this.codec = codec;
+        }
 
-        int numValues = valueBuffer.readVInt();
-        List<Long> startTimestamps = this.codec.decodeValues(valueBuffer, UnsignedLongEncodingStrategy.REPEAT_COUNT, numValues);
-        List<Long> timestamps = this.codec.decodeTimestamps(initialTimestamp, valueBuffer, numValues);
+        @Override
+        public void decode(Buffer valueBuffer, AgentStatHeaderDecoder headerDecoder, int valueSize) {
+            EncodingStrategy<Integer> deadlockedThreadCountEncodingStrategy = UnsignedIntegerEncodingStrategy.getFromCode(headerDecoder.getCode());
 
-        // decode headers
-        final byte[] header = valueBuffer.readPrefixedBytes();
-        AgentStatHeaderDecoder headerDecoder = new BitCountingHeaderDecoder(header);
-        EncodingStrategy<Integer> deadlockedThreadCountEncodingStrategy = UnsignedIntegerEncodingStrategy.getFromCode(headerDecoder.getCode());
+            // decode values
+            this.deadlockedThreadCountList = codec.decodeValues(valueBuffer, deadlockedThreadCountEncodingStrategy, valueSize);
+        }
 
-        // decode values
-        List<Integer> deadlockedThreadCountList = codec.decodeValues(valueBuffer, deadlockedThreadCountEncodingStrategy, numValues);
-
-        List<DeadlockBo> deadlockBos = new ArrayList<DeadlockBo>(numValues);
-        for (int i = 0; i < numValues; ++i) {
+        @Override
+        public DeadlockBo getValue(int index) {
             DeadlockBo deadlockBo = new DeadlockBo();
-            deadlockBo.setAgentId(agentId);
-            deadlockBo.setStartTimestamp(startTimestamps.get(i));
-            deadlockBo.setTimestamp(timestamps.get(i));
-            deadlockBo.setDeadlockedThreadCount(deadlockedThreadCountList.get(i));
-
-            deadlockBos.add(deadlockBo);
+            deadlockBo.setDeadlockedThreadCount(deadlockedThreadCountList.get(index));
+            return deadlockBo;
         }
-        return deadlockBos;
+
     }
 
 }
