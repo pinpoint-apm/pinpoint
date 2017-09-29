6e1bd23ae9d703b6bede570039cc1728cb347cac
Mamidala, Sateesh (CONT) <sateesh.mamidala@capitalone.com>
2017-09-29 13:51:19 -0400

    Revert "Create navbar.directive.js"
    This reverts commit b2ecba9a186d331ab19b09555816aa8da963c007.

--- web/src/main/webapp/features/navbar/navbar.directive.js
+++ web/src/main/webapp/features/navbar/navbar.directive.js
@@ -309,7 +309,6 @@
 								oNavbarVoService.setReadablePeriod( PreferenceService.getRealtimeScatterXRangeStr() );
 								oNavbarVoService.setQueryEndDateTime( CommonUtilService.formatDate( currentServerTime ) );
 								oNavbarVoService.autoCalculateByQueryEndDateTimeAndReadablePeriod();
-								UrlVoService.setReadablePeriod(cfg.periodType.REALTIME);
 								UrlVoService.setQueryEndDateTime( CommonUtilService.formatDate( currentServerTime ) );
 								UrlVoService.autoCalculateByQueryEndDateTimeAndReadablePeriod();
 								emitAsChanged();
