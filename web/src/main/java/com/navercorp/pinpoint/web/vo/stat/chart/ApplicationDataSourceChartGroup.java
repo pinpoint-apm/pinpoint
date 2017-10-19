f84abaa39883f453f362dc8229bbb30ee2c4d753
minwoo.jung <minwoo.jung@navercorp.com>
2017-10-19 17:36:54 +0900

    [#3378] aggregate agent's dataSource to show application's dataSource

--- web/src/main/java/com/navercorp/pinpoint/web/vo/stat/chart/ApplicationDataSourceChartGroup.java
+++ web/src/main/java/com/navercorp/pinpoint/web/vo/stat/chart/ApplicationDataSourceChartGroup.java
@@ -32,16 +32,16 @@ public class ApplicationDataSourceChartGroup implements ApplicationStatChartGrou
     private static final UncollectedDataSourcePointCreater UNCOLLECTED_DATASOURCE_POINT = new UncollectedDataSourcePointCreater();
 
     private final String url;
-    private final String serviceTypeCodeName;
+    private final String serviceType;
     private final Map<ChartType, Chart> dataSourceChartMap;
 
     public enum DataSourceChartType implements ChartType {
         ACTIVE_CONNECTION_SIZE
     }
 
-    public ApplicationDataSourceChartGroup(TimeWindow timeWindow, String url, String serviceTypeCodeName, List<AggreJoinDataSourceBo> aggreJoinDataSourceBoList) {
+    public ApplicationDataSourceChartGroup(TimeWindow timeWindow, String url, String serviceType, List<AggreJoinDataSourceBo> aggreJoinDataSourceBoList) {
         this.url = url;
-        this.serviceTypeCodeName = serviceTypeCodeName;
+        this.serviceType = serviceType;
         this.dataSourceChartMap = new HashMap<>();
         List<Point> activeConnectionCountList = new ArrayList<>(aggreJoinDataSourceBoList.size());
 
@@ -49,6 +49,7 @@ public class ApplicationDataSourceChartGroup implements ApplicationStatChartGrou
             activeConnectionCountList.add(new DataSourcePoint(aggreJoinDataSourceBo.getTimestamp(), aggreJoinDataSourceBo.getMinActiveConnectionSize(), aggreJoinDataSourceBo.getMinActiveConnectionAgentId(), aggreJoinDataSourceBo.getMaxActiveConnectionSize(), aggreJoinDataSourceBo.getMaxActiveConnectionAgentId(), aggreJoinDataSourceBo.getAvgActiveConnectionSize()));
         }
 
+//        여기서 빈 부분을 알아서 채워줘야 될것같은데 왜 그럴까 이건 디버깅을 해보는게 좋을듯함.
         dataSourceChartMap.put(DataSourceChartType.ACTIVE_CONNECTION_SIZE, new TimeSeriesChartBuilder(timeWindow, UNCOLLECTED_DATASOURCE_POINT).build(activeConnectionCountList));
     }
 
@@ -56,8 +57,8 @@ public class ApplicationDataSourceChartGroup implements ApplicationStatChartGrou
         return url;
     }
 
-    public String getServiceTypeCodeName() {
-        return serviceTypeCodeName;
+    public String getServiceType() {
+        return serviceType;
     }
 
     @Override
