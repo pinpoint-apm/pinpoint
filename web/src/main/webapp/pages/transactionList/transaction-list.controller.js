8124c9654d698b8c44c44dd914145c682b693b01
denz <hyojong.song2015@navercorp.com>
2017-11-15 16:10:48 +0900

    [#3548] After request transaction metadata, recalculate the total value.

--- web/src/main/webapp/pages/transactionList/transaction-list.controller.js
+++ web/src/main/webapp/pages/transactionList/transaction-list.controller.js
@@ -1,40 +1,36 @@
 (function( $ ) {
 	'use strict';
 	/**
-	 * (en)TransactionListCtrl 
+	 * (en)TransactionListCtrl
 	 * @ko TransactionListCtrl
 	 * @group Controller
 	 * @name TransactionListCtrl
 	 * @class
 	 */
 	pinpointApp.constant("TransactionListConfig", {
-	    applicationUrl: "transactionmetadata.pinpoint",
+		applicationUrl: "transactionmetadata.pinpoint",
 		MIN_TRANSACTION_LIST_HEIGHT: 75,
-	    MAX_FETCH_BLOCK_SIZE: 100,
+		MAX_FETCH_BLOCK_SIZE: 100,
 		TRANSACTION_LIST_HANDLE_POSITION: "transactionList.resizer"
 	});
-	
+
 	pinpointApp.controller("TransactionListCtrl", ["TransactionListConfig", "$scope", "$location", "locationService", "$routeParams", "$rootScope", "$timeout", "$window", "$http", "webStorage", "TimeSliderVoService", "TransactionDaoService", "AnalyticsService", "helpContentService",
-	    function (cfg, $scope, $location, locationService, $routeParams, $rootScope, $timeout, $window, $http, webStorage, TimeSliderVoService, oTransactionDaoService, analyticsService, helpContentService) {
+		function (cfg, $scope, $location, locationService, $routeParams, $rootScope, $timeout, $window, $http, webStorage, TimeSliderVoService, oTransactionDaoService, analyticsService, helpContentService) {
 			analyticsService.send(analyticsService.CONST.TRANSACTION_LIST_PAGE);
-	        // define private variables
-	        var nFetchCount, nLastFetchedIndex, htTransactionInfo, htTransactionData, oTimeSliderVoService;
+			// define private variables
+			var nFetchCount, nLastFetchedIndex, nRequestCount, htTransactionInfo, htTransactionData, oTimeSliderVoService;
 			var aParamTransactionInfo, beforeTransactionDetailUrl = "";
-	
-	        // define private variables of methods
-	        var fetchStart, fetchNext, fetchAll, emitTransactionListToTable, getQuery, getTransactionList, changeTransactionDetail,
-				getTransactionInfoFromWindow, hasScatterByApplicationName, getDataByTransactionInfo, getTransactionInfoFromURL, hasParent, hasValidParam, initAndLoad, alertAndMove;
-	
-	        /**
-	         * initialization
-	         */
-	        $timeout(function () {
-	
-	            // initialize private variables;
-	            nFetchCount = 1;
-	            nLastFetchedIndex = 0;
-	            $scope.transactionDetailUrl = 'index.html?vs=' + Date.now() + '#/transactionDetail';
-	            $scope.sidebarLoading = true;
+
+			/**
+			 * initialization
+			 */
+			$timeout(function () {
+
+				// initialize private variables;
+				//nFetchCount = 1;
+				nLastFetchedIndex = 0;
+				$scope.transactionDetailUrl = 'index.html?vs=' + Date.now() + '#/transactionDetail';
+				$scope.sidebarLoading = true;
 
 				var bHasParent = hasParent();
 				var bHasValidParam = hasValidParam();
@@ -63,10 +59,10 @@
 					}
 				}
 
-	            $timeout(function () {
+				$timeout(function () {
 					var resizerY = webStorage.get( cfg.TRANSACTION_LIST_HANDLE_POSITION ) === null ? (window.innerHeight - 40) / 2 : parseInt( webStorage.get( cfg.TRANSACTION_LIST_HANDLE_POSITION ) );
 					resizerY = Math.max( cfg.MIN_TRANSACTION_LIST_HEIGHT, resizerY );
-	                if( $("#main-container").length !== 0 ) {
+					if( $("#main-container").length !== 0 ) {
 						$("#main-container").layout({
 							north__minSize: 30,
 							north__size: resizerY,
@@ -81,25 +77,23 @@
 							}
 						});
 					}
-	            }, 100);
-	
-	        }, 100);
-			alertAndMove = function( msg ) {
+				}, 100);
+
+			}, 100);
+			function alertAndMove( msg ) {
 				alert( msg );
 				locationService.path( "/main/" + $routeParams.application + "/" + $routeParams.readablePeriod + "/" + $routeParams.queryEndDateTime ).replace();
-			};
-
-			initAndLoad = function(bHasTransactionInfo) {
+			}
+			function initAndLoad(bHasTransactionInfo) {
 				oTimeSliderVoService = new TimeSliderVoService();
 				oTimeSliderVoService.setTotal(htTransactionData.length);
 
-				fetchStart( bHasTransactionInfo );
-			};
-
-			hasParent = function() {
+				fetchStart(bHasTransactionInfo);
+			}
+			function hasParent() {
 				return angular.isDefined( $window.opener );
-			};
-			hasValidParam = function() {
+			}
+			function hasValidParam() {
 				if ( angular.isUndefined( $window.opener ) || $window.opener === null ) return false;
 				var $parentParams = $window.opener.$routeParams;
 				if ( angular.isDefined($routeParams) && angular.isDefined($parentParams) ) {
@@ -116,15 +110,9 @@
 					}
 				}
 				return false;
-			};
-
-	        /**
-	         * parse window name
-	         * @param windowName
-	         * @returns {{applicationName: *, nXFrom: *, nXTo: *, nYFrom: *, nYTo: *}}
-	         */
-			getTransactionInfoFromWindow = function (windowName) {
-	            var t = windowName.split('|');
+			}
+			function getTransactionInfoFromWindow(windowName) {
+				var t = windowName.split('|');
 				if (t.length === 6 ) {
 					return {
 						applicationName: t[0],
@@ -145,8 +133,8 @@
 						include: t[6]
 					};
 				}
-	        };
-			getTransactionInfoFromURL = function() {
+			}
+			function getTransactionInfoFromURL() {
 				return {
 					applicationName: $routeParams.application.split("@")[0],
 					nXFrom: parseInt(aParamTransactionInfo[1]) - 1000,
@@ -154,104 +142,75 @@
 					nYFrom: 0,
 					nYTo: 0
 				};
-			};
-	
-	        /**
-	         * has scatter by application name
-	         * @param applicationName
-	         * @returns {*}
-	         */
-	        hasScatterByApplicationName = function (applicationName) {
-	            return angular.isDefined($window.opener.htoScatter[applicationName]);
-	        };
-	
-	        /**
-	         * get data by transaction list info
-	         * @param t
-	         * @returns {*}
-	         */
-	        getDataByTransactionInfo = function (t) {
-	            var oScatter = $window.opener.htoScatter[t.applicationName];
+			}
+			function hasScatterByApplicationName(applicationName) {
+				return angular.isDefined($window.opener.htoScatter[applicationName]);
+			}
+			function getDataByTransactionInfo(t) {
+				var oScatter = $window.opener.htoScatter[t.applicationName];
 				if ( t.type ) {
 					return oScatter.getDataByRange( t.type, t.min, t.max, t.agent, t.include );
 				} else {
 					return oScatter.getDataByXY( t.nXFrom, t.nXTo, t.nYFrom, t.nYTo, t.agent, t.include );
 				}
 
-	        };
-	
-	        /**
-	         * emit transaction list to table
-	         * @param data
-	         */
-	        emitTransactionListToTable = function (data) {
-	            $scope.$emit('transactionTableDirective.appendTransactionList', data.metadata);
-	        };
-	
-	        /**
-	         * get query
-	         * @returns {Array | Boolean}
-	         */
-	        getQuery = function () {
-	            if (!htTransactionData) {
-	                $window.alert("Query failed - Query parameter cache deleted.\n\nPossibly due to scatter chart being refreshed.");
-	                return false;
-	            }
-	            var query = [];
-	            for (var i = nLastFetchedIndex, j = 0; i < cfg.MAX_FETCH_BLOCK_SIZE * nFetchCount && i < htTransactionData.length; i++, j++) {
-	                if (i > 0) {
-	                    query.push("&");
-	                }
-	                query = query.concat(["I", j, "=", htTransactionData[i][0]]);
-	                query = query.concat(["&T", j, "=", htTransactionData[i][1]]);
-	                query = query.concat(["&R", j, "=", htTransactionData[i][2]]);
-	                nLastFetchedIndex++;
-	            }
-	            nFetchCount++;
-	            return query;
-	        };
-	
-	        /**
-	         * fetch next
-	         */
-	        fetchNext = function () {
-	            getTransactionList(getQuery(), function (data) {
-	                if (data.metadata.length === 0 ) {
-	                    $scope.$emit('timeSliderDirective.disableMore');
-	                    $scope.$emit('timeSliderDirective.changeMoreToDone');
-	                    return false;
-	                } else if (data.metadata.length < cfg.MAX_FETCH_BLOCK_SIZE || oTimeSliderVoService.getTotal() === data.metadata.length + oTimeSliderVoService.getCount()) {
-	                    $scope.$emit('timeSliderDirective.disableMore');
-	                    $scope.$emit('timeSliderDirective.changeMoreToDone');
-	                    oTimeSliderVoService.setInnerFrom(htTransactionInfo.nXFrom);
-	                } else {
-	                    $scope.$emit('timeSliderDirective.enableMore');
-	                    oTimeSliderVoService.setInnerFrom(_.last(data.metadata).collectorAcceptTime);
-	                }
-	                emitTransactionListToTable(data);
-	
-	                oTimeSliderVoService.addCount(data.metadata.length);
-	                $scope.$emit('timeSliderDirective.setInnerFromTo', oTimeSliderVoService);
-	                $scope.sidebarLoading = false;
-	            });
-	        };
-	
-	        /**
-	         * fetch all
-	         */
-	        fetchAll = function () {
-	            cfg.MAX_FETCH_BLOCK_SIZE = 100000000;
-	            fetchNext();
-	        };
-	
-	        /**
-	         * fetch start
-	         */
-	        var fetchStartLoadTryCount = 0;
-	        var fetchStartLoadTryMaxCount = 3;
-	        fetchStart = function ( bHasTransactionInfo ) {
+			}
+			function emitTransactionListToTable(data) {
+				$scope.$emit('transactionTableDirective.appendTransactionList', data.metadata);
+			}
+			function getQuery() {
+				if (!htTransactionData) {
+					$window.alert("Query failed - Query parameter cache deleted.\n\nPossibly due to scatter chart being refreshed.");
+					return false;
+				}
+				nRequestCount = 0;
+				var query = [];
+				var maxLen = nLastFetchedIndex + cfg.MAX_FETCH_BLOCK_SIZE;
+				for (var i = nLastFetchedIndex, j = 0; i < maxLen && i < htTransactionData.length; i++, j++) {
+					if (i > 0) {
+						query.push("&");
+					}
+					query = query.concat(
+						["I", j, "=", htTransactionData[i][0]],
+						["&T", j, "=", htTransactionData[i][1]],
+						["&R", j, "=", htTransactionData[i][2]]
+					);
+					nRequestCount++;
+					nLastFetchedIndex++;
+				}
+				return query;
+			}
+			function fetchNext() {
+				getTransactionList(getQuery(), function (data) {
+					if (data.metadata.length === 0 ) {
+						$scope.$emit('timeSliderDirective.disableMore');
+						$scope.$emit('timeSliderDirective.changeMoreToDone');
+						return false;
+					} else if (data.metadata.length < cfg.MAX_FETCH_BLOCK_SIZE || data.metadata.length + oTimeSliderVoService.getCount() >= oTimeSliderVoService.getTotal() ) {
+						$scope.$emit('timeSliderDirective.disableMore');
+						$scope.$emit('timeSliderDirective.changeMoreToDone');
+						oTimeSliderVoService.setInnerFrom(htTransactionInfo.nXFrom);
+					} else {
+						$scope.$emit('timeSliderDirective.enableMore');
+						oTimeSliderVoService.setInnerFrom(_.last(data.metadata).collectorAcceptTime);
+					}
+					emitTransactionListToTable(data);
+					updateTotal(data.metadata.length);
+					oTimeSliderVoService.addCount(data.metadata.length);
+					$scope.$emit('timeSliderDirective.setInnerFromTo', oTimeSliderVoService);
+					$scope.sidebarLoading = false;
+				});
+			}
+			var fetchStartLoadTryCount = 0;
+			var fetchStartLoadTryMaxCount = 3;
+			function updateTotal( responseCount ) {
+				if ( responseCount > nRequestCount ) {
+					oTimeSliderVoService.setTotal( oTimeSliderVoService.getTotal() + responseCount - nRequestCount );
+				}
+			}
+			function fetchStart( bHasTransactionInfo ) {
 				var query = getQuery();
-	            getTransactionList(query, function (data) {
+				getTransactionList(query, function (data) {
 					if (data.metadata.length === 0) {
 						$scope.$emit('timeSliderDirective.disableMore');
 						$scope.$emit('timeSliderDirective.changeMoreToDone');
@@ -265,7 +224,7 @@
 							$window.close();
 						}
 						return false;
-					} else if (data.metadata.length < cfg.MAX_FETCH_BLOCK_SIZE || oTimeSliderVoService.getTotal() === data.metadata.length) {
+					} else if (data.metadata.length < cfg.MAX_FETCH_BLOCK_SIZE || data.metadata.length >= oTimeSliderVoService.getTotal() ) {
 						$scope.$emit('timeSliderDirective.disableMore');
 						$scope.$emit('timeSliderDirective.changeMoreToDone');
 						oTimeSliderVoService.setInnerFrom(htTransactionInfo.nXFrom);
@@ -275,6 +234,7 @@
 					}
 					emitTransactionListToTable(data);
 
+					updateTotal(data.metadata.length);
 					oTimeSliderVoService.setFrom(htTransactionInfo.nXFrom);
 					oTimeSliderVoService.setTo(htTransactionInfo.nXTo);
 					oTimeSliderVoService.setInnerTo(htTransactionInfo.nXTo);
@@ -292,16 +252,10 @@
 							elapsed: aParamTransactionInfo[2]
 						});
 					}
-	            });
-	        };
-	
-	        /**
-	         * get transaction list
-	         * @param query
-	         * @param cb
-	         */
-	        getTransactionList = function (query, cb) {
-	            $http.post(cfg.applicationUrl, query.join(""), {
+				});
+			}
+			function getTransactionList(query, cb) {
+				$http.post(cfg.applicationUrl, query.join(""), {
 					headers: {'Content-Type': 'application/x-www-form-urlencoded'}
 				}).success(function (data, status) {
 					if (angular.isFunction(cb)) {
@@ -310,13 +264,8 @@
 				}).error(function (data, status) {
 					$window.alert("Failed to fetching the request information.");
 				});
-	        };
-	
-	        /**
-	         * change transaction detail
-	         * @param transaction
-	         */
-	        changeTransactionDetail = function (transaction) {
+			}
+			function changeTransactionDetail(transaction) {
 				var transactionDetailUrl = 'index.html?vs=' + Date.now() + '#/transactionDetail';
 				if (transaction.traceId && transaction.collectorAcceptTime) {
 					transactionDetailUrl += '/' + $window.encodeURIComponent(transaction.traceId) + '/' + transaction.collectorAcceptTime + '/' + transaction.agentId + '/' + transaction.spanId;
@@ -330,28 +279,20 @@
 						$scope.transactionDetailUrl = transactionDetailUrl;
 					});
 				}
-	        };
-	
-	        /**
-	         * scope event on transactionTable.applicationSelected
-	         */
-	        $scope.$on('transactionTableDirective.applicationSelected', function (event, transaction) {
-	            changeTransactionDetail(transaction);
-	        });
-	
-	        /**
-	         * scope event on timeSliderDirective.moreClicked
-	         */
-	        $scope.$on('timeSliderDirective.moreClicked', function (event) {
-	            $scope.sidebarLoading = true;
-	            $scope.$emit('timeSliderDirective.disableMore');
-	            $timeout(function () {
-	                fetchNext();
-	            }, 1000);
-	        });
+			}
+			$scope.$on('transactionTableDirective.applicationSelected', function (event, transaction) {
+				changeTransactionDetail(transaction);
+			});
+			$scope.$on('timeSliderDirective.moreClicked', function (event) {
+				$scope.sidebarLoading = true;
+				$scope.$emit('timeSliderDirective.disableMore');
+				$timeout(function () {
+					fetchNext();
+				}, 1000);
+			});
 			$scope.completedDetailPageLoad = function() {
 				$scope.$emit( "transactionTableDirective.completedDetailPageLoad" );
 			};
-	    }
+		}
 	]);
 })( jQuery );
