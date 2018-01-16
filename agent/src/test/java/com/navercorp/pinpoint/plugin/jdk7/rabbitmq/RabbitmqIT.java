224b6b0bed9ea1d8ba26a1c929fcc58b111e8c17
HyunGil Jeong <hyungil.jeong@navercorp.com>
2018-01-16 11:49:17 +0900

    [#1412] Clean up IT test

--- agent/src/test/java/com/navercorp/pinpoint/plugin/jdk7/rabbitmq/RabbitmqIT.java
+++ agent/src/test/java/com/navercorp/pinpoint/plugin/jdk7/rabbitmq/RabbitmqIT.java
@@ -3,9 +3,6 @@ package com.navercorp.pinpoint.plugin.jdk7.rabbitmq;
 import com.navercorp.pinpoint.bootstrap.plugin.test.Expectations;
 import com.navercorp.pinpoint.bootstrap.plugin.test.PluginTestVerifier;
 import com.navercorp.pinpoint.bootstrap.plugin.test.PluginTestVerifierHolder;
-import com.navercorp.pinpoint.test.plugin.Dependency;
-import com.navercorp.pinpoint.test.plugin.JvmArgument;
-import com.navercorp.pinpoint.test.plugin.PinpointPluginTestSuite;
 import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
@@ -22,7 +19,6 @@ import org.apache.qpid.server.BrokerOptions;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
@@ -35,7 +31,7 @@ import static org.junit.Assert.assertTrue;
  * @author Jiaqi Feng
  */
 
-public class RabbitmqIT {
+public abstract class RabbitmqIT {
     public static String publishClassSignature="com.rabbitmq.client.impl.ChannelN.basicPublish(java.lang.String, java.lang.String, boolean, boolean, com.rabbitmq.client.AMQP$BasicProperties, byte[])";
 
     public static String RUNNABLE_NAME="ConsumerDispatcher$5";
@@ -80,6 +76,7 @@ public class RabbitmqIT {
 
         String exchange="test-pp";
         String queuename="queue-pp";
+        String routingKey = "test";
         String message="hello rabbit mq";
 
         SaslConfig saslConfig = new SaslConfig() {
@@ -98,67 +95,64 @@ public class RabbitmqIT {
         // producer side
         //System.out.println("RabbitmqSender.doGet() ---- start");
 
-        try {
-            ConnectionFactory factory = new ConnectionFactory();
-            factory.setHost("localhost");
-            factory.setPort(20179);
-            factory.setSaslConfig(saslConfig);
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setPort(20179);
+        factory.setSaslConfig(saslConfig);
 
-            // enable below to test AutorecoveringChannel
-            //factory.setAutomaticRecoveryEnabled(true);
+        // enable below to test AutorecoveringChannel
+        //factory.setAutomaticRecoveryEnabled(true);
 
-            Connection connection = factory.newConnection();
-            Channel channel = connection.createChannel();
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
 
-            channel.exchangeDeclare(exchange, "direct", false);
-            channel.queueDeclare(queuename, false, false, false, null);
-            channel.queueBind(queuename, exchange, "test");
+        channel.exchangeDeclare(exchange, "direct", false);
+        channel.queueDeclare(queuename, false, false, false, null);
+        channel.queueBind(queuename, exchange, routingKey);
 
-            AMQP.BasicProperties.Builder builder = new AMQP.BasicProperties.Builder();
-            channel.basicPublish(exchange, "test", false, false, builder.appId("test").build(), message.getBytes());
-            //System.out.println("RabbitmqSender ---- Sent message:" + message);
+        AMQP.BasicProperties.Builder builder = new AMQP.BasicProperties.Builder();
+        channel.basicPublish(exchange, routingKey, false, false, builder.appId("test").build(), message.getBytes());
+        //System.out.println("RabbitmqSender ---- Sent message:" + message);
 
-            channel.close();
-            connection.close();
+        channel.close();
+        connection.close();
 
-            //comsumer
-            factory = new ConnectionFactory();
-            factory.setHost("localhost");
-            factory.setPort(20179);
-            factory.setSaslConfig(saslConfig);
-            connection = factory.newConnection();
-            channel = connection.createChannel();
+        //comsumer
+        factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setPort(20179);
+        factory.setSaslConfig(saslConfig);
+        connection = factory.newConnection();
+        channel = connection.createChannel();
 
-            channel.queueDeclare(queuename, false, false, false, null);
-            //System.out.println("Receiver ---- Waiting for messages");
+        channel.queueDeclare(queuename, false, false, false, null);
+        //System.out.println("Receiver ---- Waiting for messages");
 
-            final CountDownLatch latch = new CountDownLatch(1);
+        final CountDownLatch latch = new CountDownLatch(1);
 
-            Consumer consumer = new DefaultConsumer(channel) {
-                @Override
-                public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties,
-                                           byte[] body) throws IOException {
-                    super.handleDelivery(consumerTag, envelope, properties, body);
-                    String message = new String(body, "UTF-8");
-                    //System.out.println("Receiver ---- Received exchange=" + envelope.getExchange() + ", routingkey=" + envelope.getRoutingKey() + ", message=" + message);
-                    latch.countDown();
-                }
-            };
-            channel.basicConsume(queuename, true, consumer);
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties,
+                                       byte[] body) throws IOException {
+                super.handleDelivery(consumerTag, envelope, properties, body);
+                String message = new String(body, "UTF-8");
+                //System.out.println("Receiver ---- Received exchange=" + envelope.getExchange() + ", routingkey=" + envelope.getRoutingKey() + ", message=" + message);
+                latch.countDown();
+            }
+        };
+        channel.basicConsume(queuename, true, consumer);
 
-            // wait consumer
-            assertTrue(latch.await(10, TimeUnit.SECONDS));
+        // wait consumer
+        assertTrue(latch.await(10, TimeUnit.SECONDS));
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        final String remoteAddress = connection.getAddress().getHostAddress() + ":" + connection.getPort();
 
         verifier.printCache();
         verifier.verifyTrace(Expectations.event("RABBITMQ", publishClassSignature,
-                        annotation("rabbitmq.exchange", "test-pp"),
-                        annotation("rabbitmq.routingkey", "test")),
-                Expectations.root("RABBITMQ", "com.rabbitmq.client.DefaultConsumer.handleDelivery(java.lang.String, com.rabbitmq.client.Envelope, com.rabbitmq.client.AMQP$BasicProperties, byte[])", null, "exchange:test-pp", "127.0.0.1:20179",
-                        annotation("rabbitmq.routingkey", "test")));
+                annotation("rabbitmq.exchange", exchange),
+                annotation("rabbitmq.routingkey", routingKey)),
+                Expectations.root("RABBITMQ", "com.rabbitmq.client.DefaultConsumer.handleDelivery(java.lang.String, com.rabbitmq.client.Envelope, com.rabbitmq.client.AMQP$BasicProperties, byte[])", null, "exchange:" + exchange, remoteAddress,
+                        annotation("rabbitmq.routingkey", routingKey)));
 
         verifier.verifyTraceCount(0);
     }
--- agent/src/test/java/com/navercorp/pinpoint/plugin/jdk7/rabbitmq/Rabbitmq_3_2_IT.java
+++ agent/src/test/java/com/navercorp/pinpoint/plugin/jdk7/rabbitmq/Rabbitmq_3_2_IT.java
@@ -10,7 +10,6 @@ import org.junit.runner.RunWith;
  */
 @RunWith(PinpointPluginTestSuite.class)
 @Dependency({"com.rabbitmq:amqp-client:[3.2.0,4.0.0)", "org.apache.qpid:qpid-broker:6.1.1"})
-//@Dependency({"com.rabbitmq:amqp-client:3.5.5", "org.apache.qpid:qpid-broker:6.1.1"})
 @JvmArgument({"-Dpinpoint.configcenter=false"})
 public class Rabbitmq_3_2_IT extends RabbitmqIT {
     public Rabbitmq_3_2_IT() {
