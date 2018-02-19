5af83384d50eaab0d0267164266651b225124ac0
emeroad <wd.kang@navercorp.com>
2018-02-19 18:48:30 +0900

    [#3827] DNS update for UDP datasender

--- profiler/src/main/java/com/navercorp/pinpoint/profiler/sender/UdpDataSender.java
+++ profiler/src/main/java/com/navercorp/pinpoint/profiler/sender/UdpDataSender.java
@@ -16,6 +16,11 @@
 
 package com.navercorp.pinpoint.profiler.sender;
 
+import com.navercorp.pinpoint.common.annotations.VisibleForTesting;
+import com.navercorp.pinpoint.common.plugin.util.HostAndPort;
+import com.navercorp.pinpoint.common.util.Assert;
+import com.navercorp.pinpoint.rpc.client.DnsSocketAddressProvider;
+import com.navercorp.pinpoint.rpc.client.SocketAddressProvider;
 import com.navercorp.pinpoint.thrift.io.HeaderTBaseSerializer;
 import com.navercorp.pinpoint.thrift.io.HeaderTBaseSerializerFactory;
 import org.apache.thrift.TBase;
@@ -26,6 +31,7 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
+import java.net.PortUnreachableException;
 import java.net.SocketException;
 
 /**
@@ -52,17 +58,18 @@ public class UdpDataSender extends AbstractDataSender implements DataSender {
 
     private final AsyncQueueingExecutor<Object> executor;
 
+    private final UdpSocketAddressProvider socketAddressProvider;
+
     public UdpDataSender(String host, int port, String threadName, int queueSize) {
         this(host, port, threadName, queueSize, SOCKET_TIMEOUT, SEND_BUFFER_SIZE);
     }
 
     public UdpDataSender(String host, int port, String threadName, int queueSize, int timeout, int sendBufferSize) {
-        if (host == null ) {
-            throw new NullPointerException("host must not be null");
+        Assert.requireNonNull(host, "host must not be null");
+        if (!HostAndPort.isValidPort(port)) {
+            throw new IllegalArgumentException("port out of range:" + port);
         }
-        if (threadName == null) {
-            throw new NullPointerException("threadName must not be null");
-        }
+        Assert.requireNonNull(host, "host must not be null");
         if (queueSize <= 0) {
             throw new IllegalArgumentException("queueSize");
         }
@@ -73,9 +80,12 @@ public class UdpDataSender extends AbstractDataSender implements DataSender {
             throw new IllegalArgumentException("sendBufferSize");
         }
 
+        final SocketAddressProvider socketAddressProvider = new DnsSocketAddressProvider(host, port);
+        this.socketAddressProvider = new RefreshStrategy(socketAddressProvider);
+        final InetSocketAddress currentAddress = this.socketAddressProvider.resolve();
+        logger.info("UdpDataSender initialized. host={}", currentAddress);
         // TODO If fail to create socket, stop agent start
-        logger.info("UdpDataSender initialized. host={}, port={}", host, port);
-        this.udpSocket = createSocket(host, port, timeout, sendBufferSize);
+        this.udpSocket = createSocket(timeout, sendBufferSize);
 
         this.executor = createAsyncQueueingExecutor(queueSize, threadName);
     }
@@ -90,7 +100,7 @@ public class UdpDataSender extends AbstractDataSender implements DataSender {
         executor.stop();
     }
 
-    private DatagramSocket createSocket(String host, int port, int timeout, int sendBufferSize) {
+    private DatagramSocket createSocket(int timeout, int sendBufferSize) {
         try {
             final DatagramSocket datagramSocket = new DatagramSocket();
 
@@ -103,8 +113,6 @@ public class UdpDataSender extends AbstractDataSender implements DataSender {
                 }
             }
 
-            final InetSocketAddress serverAddress = new InetSocketAddress(host, port);
-            datagramSocket.connect(serverAddress);
             return datagramSocket;
         } catch (SocketException e) {
             throw new IllegalStateException("DatagramSocket create fail. Cause" + e.getMessage(), e);
@@ -112,39 +120,49 @@ public class UdpDataSender extends AbstractDataSender implements DataSender {
     }
 
     protected void sendPacket(Object message) {
-        if (message instanceof TBase) {
-            final TBase dto = (TBase) message;
-            // do not copy bytes because it's single threaded
-            final byte[] internalBufferData = serialize(this.serializer, dto);
-            if (internalBufferData == null) {
-                logger.warn("interBufferData is null");
-                return;
-            }
-
-            final int internalBufferSize = this.serializer.getInterBufferSize();
-            if (isLimit(internalBufferSize)) {
-                // When packet size is greater than UDP packet size limit, it's better to discard packet than let the socket API fails.
-                logger.warn("discard packet. Caused:too large message. size:{}, {}", internalBufferSize, dto);
-                return;
-            }
-            // it's safe to reuse because it's single threaded
-            reusePacket.setData(internalBufferData, 0, internalBufferSize);
-
-            try {
-                udpSocket.send(reusePacket);
-                if (isDebug) {
-                    logger.debug("Data sent. size:{}, {}", internalBufferSize, dto);
-                }
-            } catch (IOException e) {
-                logger.info("packet send error. size:{}, {}", internalBufferSize, dto, e);
-            }
-        } else {
+        if (!(message instanceof TBase)) {
             logger.warn("sendPacket fail. invalid type:{}", message != null ? message.getClass() : null);
             return;
         }
+        final InetSocketAddress inetSocketAddress = socketAddressProvider.resolve();
+        if (inetSocketAddress.getAddress() == null) {
+            logger.info("dns lookup fail host:{}", inetSocketAddress);
+            return;
+        }
+
+        final TBase dto = (TBase) message;
+        // do not copy bytes because it's single threaded
+        final byte[] internalBufferData = serialize(this.serializer, dto);
+        if (internalBufferData == null) {
+            logger.warn("interBufferData is null");
+            return;
+        }
+
+        final int internalBufferSize = this.serializer.getInterBufferSize();
+        if (isLimit(internalBufferSize)) {
+            // When packet size is greater than UDP packet size limit, it's better to discard packet than let the socket API fails.
+            logger.warn("discard packet. Caused:too large message. size:{}, {}", internalBufferSize, dto);
+            return;
+        }
+        // it's safe to reuse because it's single threaded
+        reusePacket.setData(internalBufferData, 0, internalBufferSize);
+        reusePacket.setAddress(inetSocketAddress.getAddress());
+        reusePacket.setPort(inetSocketAddress.getPort());
+        try {
+            udpSocket.send(reusePacket);
+            if (isDebug) {
+                logger.debug("Data sent. size:{}, {}", internalBufferSize, dto);
+            }
+        } catch (PortUnreachableException pe) {
+            this.socketAddressProvider.handlePortUnreachable();
+            logger.info("packet send error. size:{}, {}", internalBufferSize, dto, pe);
+        } catch (IOException e) {
+            logger.info("packet send error. size:{}, {}", internalBufferSize, dto, e);
+        }
+
     }
 
-    // for test
+    @VisibleForTesting
     protected boolean isLimit(int interBufferSize) {
         if (interBufferSize > UDP_MAX_PACKET_LENGTH) {
             return true;
