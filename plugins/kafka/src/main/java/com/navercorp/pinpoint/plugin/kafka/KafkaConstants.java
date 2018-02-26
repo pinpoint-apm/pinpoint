a20d3b7f38e2ff12a6b712c8e23525b6c46dd184
kiseok.jang <kiseok.jang@nhn.com>
2018-02-26 11:37:45 +0900

    change service type code

--- plugins/kafka/src/main/java/com/navercorp/pinpoint/plugin/kafka/KafkaConstants.java
+++ plugins/kafka/src/main/java/com/navercorp/pinpoint/plugin/kafka/KafkaConstants.java
@@ -22,7 +22,7 @@ import com.navercorp.pinpoint.common.trace.ServiceTypeFactory;
 import static com.navercorp.pinpoint.common.trace.ServiceTypeProperty.RECORD_STATISTICS;
 
 public class KafkaConstants {
-    public static final ServiceType KAFKA = ServiceTypeFactory.of(9995, "KAFKA", "KAFKA", RECORD_STATISTICS);
+    public static final ServiceType KAFKA = ServiceTypeFactory.of(9160, "KAFKA_CLIENT", "KAFKA_CLIENT", RECORD_STATISTICS);
     public static final String PINPOINT_HEADER_DELIMITIER = "@";
     public static final String PINPOINT_HEADER_PREFIX = PINPOINT_HEADER_DELIMITIER + "pinpoint_start" + PINPOINT_HEADER_DELIMITIER;
     public static final String PINPOINT_HEADER_POSTFIX = PINPOINT_HEADER_DELIMITIER + "pinpoint_end";
