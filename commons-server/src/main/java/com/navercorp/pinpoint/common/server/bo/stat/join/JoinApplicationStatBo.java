2d38b9428c40e84dbb4632fe382e6f1896fb6f35
Roy Kim <sungwook0115.kim@gmail.com>
2018-03-08 13:47:54 +0900

    [#3839] common - filedescriptor of applicaton

--- commons-server/src/main/java/com/navercorp/pinpoint/common/server/bo/stat/join/JoinApplicationStatBo.java
+++ commons-server/src/main/java/com/navercorp/pinpoint/common/server/bo/stat/join/JoinApplicationStatBo.java
@@ -32,6 +32,7 @@ public class JoinApplicationStatBo implements JoinStatBo {
     private List<JoinActiveTraceBo> joinActiveTraceBoList = Collections.emptyList();
     private List<JoinResponseTimeBo> joinResponseTimeBoList = Collections.emptyList();
     private List<JoinDataSourceListBo> joinDataSourceListBoList = Collections.emptyList();
+    private List<JoinFileDescriptorBo> joinFileDescriptorBoList = Collections.emptyList();
 
     private long timestamp = Long.MIN_VALUE;
     private StatType statType = StatType.APP_STST;
@@ -49,6 +50,7 @@ public class JoinApplicationStatBo implements JoinStatBo {
         newJoinApplicationStatBo.setJoinActiveTraceBoList(joinActiveTraceBoByTimeSlice(joinApplicationStatBoList));
         newJoinApplicationStatBo.setJoinResponseTimeBoList(joinResponseTimeBoByTimeSlice(joinApplicationStatBoList));
         newJoinApplicationStatBo.setJoinDataSourceListBoList(JoinDataSourceListBoBytTimeSlice(joinApplicationStatBoList));
+        newJoinApplicationStatBo.setJoinFileDescriptorBoList(joinFileDescriptorBoByTimeSlice(joinApplicationStatBoList));
         newJoinApplicationStatBo.setTimestamp(extractMinTimestamp(newJoinApplicationStatBo));
         return newJoinApplicationStatBo;
     }
@@ -89,6 +91,12 @@ public class JoinApplicationStatBo implements JoinStatBo {
         for (JoinDataSourceListBo joinDataSourceListBo : joinApplicationStatBo.getJoinDataSourceListBoList()) {
             if (joinDataSourceListBo.getTimestamp() < minTimestamp) {
                 minTimestamp = joinDataSourceListBo.getTimestamp();
+            }
+        }
+
+        for (JoinFileDescriptorBo joinFileDescriptorBo : joinApplicationStatBo.getJoinFileDescriptorBoList()) {
+            if (joinFileDescriptorBo.getTimestamp() < minTimestamp) {
+                minTimestamp = joinFileDescriptorBo.getTimestamp();
             }
         }
 
@@ -264,6 +272,33 @@ public class JoinApplicationStatBo implements JoinStatBo {
         return newJoinCpuLoadBoList;
     }
 
+    private static List<JoinFileDescriptorBo> joinFileDescriptorBoByTimeSlice(List<JoinApplicationStatBo> joinApplicationStatBoList) {
+        Map<Long, List<JoinFileDescriptorBo>> joinFileDescriptorBoMap = new HashMap<Long, List<JoinFileDescriptorBo>>();
+
+        for (JoinApplicationStatBo joinApplicationStatBo : joinApplicationStatBoList) {
+            for (JoinFileDescriptorBo joinFileDescriptorBo : joinApplicationStatBo.getJoinFileDescriptorBoList()) {
+                long shiftTimestamp = shiftTimestamp(joinFileDescriptorBo.getTimestamp());
+                List<JoinFileDescriptorBo> joinFileDescriptorBoList = joinFileDescriptorBoMap.get(shiftTimestamp);
+
+                if (joinFileDescriptorBoList == null) {
+                    joinFileDescriptorBoList = new ArrayList<JoinFileDescriptorBo>();
+                    joinFileDescriptorBoMap.put(shiftTimestamp, joinFileDescriptorBoList);
+                }
+
+                joinFileDescriptorBoList.add(joinFileDescriptorBo);
+            }
+        }
+
+        List<JoinFileDescriptorBo> newJoinFileDescriptorBoList = new ArrayList<JoinFileDescriptorBo>();
+
+        for (Map.Entry<Long, List<JoinFileDescriptorBo>> entry : joinFileDescriptorBoMap.entrySet()) {
+            List<JoinFileDescriptorBo> joinFileDescriptorBoList = entry.getValue();
+            JoinFileDescriptorBo joinFileDescriptorBo = JoinFileDescriptorBo.joinFileDescriptorBoList(joinFileDescriptorBoList, entry.getKey());
+            newJoinFileDescriptorBoList.add(joinFileDescriptorBo);
+        }
+        return newJoinFileDescriptorBoList;
+    }
+
     public static JoinApplicationStatBo joinApplicationStatBo(List<JoinApplicationStatBo> joinApplicationStatBoList) {
         JoinApplicationStatBo newJoinApplicationStatBo = new JoinApplicationStatBo();
 
@@ -364,6 +399,14 @@ public class JoinApplicationStatBo implements JoinStatBo {
         return joinDataSourceListBoList;
     }
 
+    public List<JoinFileDescriptorBo> getJoinFileDescriptorBoList() {
+        return joinFileDescriptorBoList;
+    }
+
+    public void setJoinFileDescriptorBoList(List<JoinFileDescriptorBo> joinFileDescriptorBoList) {
+        this.joinFileDescriptorBoList = joinFileDescriptorBoList;
+    }
+
     public static List<JoinApplicationStatBo> createJoinApplicationStatBo(String applicationId, JoinAgentStatBo joinAgentStatBo, long rangeTime) {
         List<JoinApplicationStatBo> joinApplicationStatBoList = new ArrayList<JoinApplicationStatBo>();
         List<JoinAgentStatBo> joinAgentStatBoList = splitJoinAgentStatBo(applicationId, joinAgentStatBo, rangeTime);
@@ -378,6 +421,7 @@ public class JoinApplicationStatBo implements JoinStatBo {
             joinApplicationStatBo.setJoinActiveTraceBoList(sliceJoinAgentStatBo.getJoinActiveTraceBoList());
             joinApplicationStatBo.setJoinResponseTimeBoList(sliceJoinAgentStatBo.getJoinResponseTimeBoList());
             joinApplicationStatBo.setJoinDataSourceListBoList(sliceJoinAgentStatBo.getJoinDataSourceListBoList());
+            joinApplicationStatBo.setJoinFileDescriptorBoList(sliceJoinAgentStatBo.getJoinFileDescriptorBoList());
             joinApplicationStatBoList.add(joinApplicationStatBo);
         }
 
@@ -392,6 +436,7 @@ public class JoinApplicationStatBo implements JoinStatBo {
         sliceJoinActiveTraceBo(applicationId, joinAgentStatBo, rangeTime, joinAgentStatBoMap);
         sliceJoinResponseTimeBo(applicationId, joinAgentStatBo, rangeTime, joinAgentStatBoMap);
         sliceJoinDataSourceListBo(applicationId, joinAgentStatBo, rangeTime, joinAgentStatBoMap);
+        sliceJoinFileDescriptorBo(applicationId, joinAgentStatBo, rangeTime, joinAgentStatBoMap);
         return new ArrayList<JoinAgentStatBo>(joinAgentStatBoMap.values());
     }
 
@@ -528,6 +573,28 @@ public class JoinApplicationStatBo implements JoinStatBo {
         }
     }
 
+    private static void sliceJoinFileDescriptorBo(String applicationId, JoinAgentStatBo joinAgentStatBo, long rangeTime, Map<Long, JoinAgentStatBo> joinAgentStatBoMap) {
+        Map<Long, List<JoinFileDescriptorBo>> joinFileDescriptorBoMap = new HashMap<Long, List<JoinFileDescriptorBo>>();
+
+        for (JoinFileDescriptorBo joinFileDescriptorBo : joinAgentStatBo.getJoinFileDescriptorBoList()) {
+            long timestamp = joinFileDescriptorBo.getTimestamp();
+            long time = timestamp - (timestamp % rangeTime);
+            List<JoinFileDescriptorBo> joinFileDescriptorBoList = joinFileDescriptorBoMap.get(time);
+
+            if (joinFileDescriptorBoList == null) {
+                joinFileDescriptorBoList = new ArrayList<JoinFileDescriptorBo>();
+                joinFileDescriptorBoMap.put(time, joinFileDescriptorBoList);
+            }
+
+            joinFileDescriptorBoList.add(joinFileDescriptorBo);
+        }
+        for (Map.Entry<Long, List<JoinFileDescriptorBo>> entry : joinFileDescriptorBoMap.entrySet()) {
+            long time = entry.getKey();
+            JoinAgentStatBo sliceJoinAgentStatBo = getORCreateJoinAgentStatBo(applicationId, joinAgentStatBoMap, time);
+            sliceJoinAgentStatBo.setJoinFileDescriptorBoList(entry.getValue());
+        }
+    }
+
     private static JoinAgentStatBo getORCreateJoinAgentStatBo(String applicationId, Map<Long, JoinAgentStatBo> joinAgentStatBoMap, long time) {
         JoinAgentStatBo joinAgentStatBo = joinAgentStatBoMap.get(time);
 
@@ -552,6 +619,7 @@ public class JoinApplicationStatBo implements JoinStatBo {
             ", joinActiveTraceBoList=" + joinActiveTraceBoList +
             ", joinResponseTimeBoList=" + joinResponseTimeBoList +
             ", joinDataSourceListBoList=" + joinDataSourceListBoList +
+            ", joinFileDescriptorBoList=" + joinFileDescriptorBoList +
             ", statType=" + statType +
             '}';
     }
