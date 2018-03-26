25bae2f16a94ca0287857421cf63c5fc9f95e922
Roy Kim <sungwook0115.kim@gmail.com>
2018-03-26 19:19:50 +0900

    [#3813]  web - directbuffer of agent

--- web/src/main/java/com/navercorp/pinpoint/web/controller/AgentStatController.java
+++ web/src/main/java/com/navercorp/pinpoint/web/controller/AgentStatController.java
@@ -16,16 +16,19 @@
 
 package com.navercorp.pinpoint.web.controller;
 
+
+
 import com.navercorp.pinpoint.common.server.bo.stat.ActiveTraceBo;
 import com.navercorp.pinpoint.common.server.bo.stat.AgentStatDataPoint;
 import com.navercorp.pinpoint.common.server.bo.stat.CpuLoadBo;
 import com.navercorp.pinpoint.common.server.bo.stat.DataSourceListBo;
 import com.navercorp.pinpoint.common.server.bo.stat.DeadlockBo;
+import com.navercorp.pinpoint.common.server.bo.stat.DirectBufferBo;
+import com.navercorp.pinpoint.common.server.bo.stat.FileDescriptorBo;
 import com.navercorp.pinpoint.common.server.bo.stat.JvmGcBo;
 import com.navercorp.pinpoint.common.server.bo.stat.JvmGcDetailedBo;
 import com.navercorp.pinpoint.common.server.bo.stat.ResponseTimeBo;
 import com.navercorp.pinpoint.common.server.bo.stat.TransactionBo;
-import com.navercorp.pinpoint.common.server.bo.stat.FileDescriptorBo;
 import com.navercorp.pinpoint.web.service.stat.ActiveTraceChartService;
 import com.navercorp.pinpoint.web.service.stat.ActiveTraceService;
 import com.navercorp.pinpoint.web.service.stat.AgentStatChartService;
@@ -36,6 +39,10 @@ import com.navercorp.pinpoint.web.service.stat.DataSourceChartService;
 import com.navercorp.pinpoint.web.service.stat.DataSourceService;
 import com.navercorp.pinpoint.web.service.stat.DeadlockChartService;
 import com.navercorp.pinpoint.web.service.stat.DeadlockService;
+import com.navercorp.pinpoint.web.service.stat.DirectBufferChartService;
+import com.navercorp.pinpoint.web.service.stat.DirectBufferService;
+import com.navercorp.pinpoint.web.service.stat.FileDescriptorChartService;
+import com.navercorp.pinpoint.web.service.stat.FileDescriptorService;
 import com.navercorp.pinpoint.web.service.stat.JvmGcChartService;
 import com.navercorp.pinpoint.web.service.stat.JvmGcDetailedChartService;
 import com.navercorp.pinpoint.web.service.stat.JvmGcDetailedService;
@@ -44,9 +51,6 @@ import com.navercorp.pinpoint.web.service.stat.ResponseTimeChartService;
 import com.navercorp.pinpoint.web.service.stat.ResponseTimeService;
 import com.navercorp.pinpoint.web.service.stat.TransactionChartService;
 import com.navercorp.pinpoint.web.service.stat.TransactionService;
-import com.navercorp.pinpoint.web.service.stat.FileDescriptorChartService;
-import com.navercorp.pinpoint.web.service.stat.FileDescriptorService;
-
 import com.navercorp.pinpoint.web.util.TimeWindow;
 import com.navercorp.pinpoint.web.util.TimeWindowSampler;
 import com.navercorp.pinpoint.web.util.TimeWindowSlotCentricSampler;
@@ -231,6 +235,15 @@ public abstract class AgentStatController<T extends AgentStatDataPoint> {
         @Autowired
         public FileDescriptorController(FileDescriptorService fileDescriptorService, FileDescriptorChartService fileDescriptorChartService) {
             super(fileDescriptorService, fileDescriptorChartService);
+        }
+    }
+
+    @Controller
+    @RequestMapping("/getAgentStat/directBuffer")
+    public static class DirectBufferController extends AgentStatController<DirectBufferBo> {
+        @Autowired
+        public DirectBufferController(DirectBufferService directBufferService, DirectBufferChartService directBufferChartService) {
+            super(directBufferService, directBufferChartService);
         }
     }
 }
