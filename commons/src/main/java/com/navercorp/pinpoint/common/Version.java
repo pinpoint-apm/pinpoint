0de2f841195a222c3da4ebedb95cbeda0a0ee3ce
koo.taejin <koo.taejin@navercorp.com>
2018-03-30 13:58:44 +0900

    [#3950] 1.7.2 release
    version updated (1.7.2 -> 1.7.3-SNAPSHOT)

--- commons/src/main/java/com/navercorp/pinpoint/common/Version.java
+++ commons/src/main/java/com/navercorp/pinpoint/common/Version.java
@@ -1,4 +1,4 @@
 package com.navercorp.pinpoint.common;
 public final class Version {
-    public static final String VERSION = "1.7.2";
+    public static final String VERSION = "1.7.3-SNAPSHOT";
 }
