524ab5b69435c1b0c22f666738b4137ccaae92d5
chaerin16 <chaerin.kim@samsung.com>
2018-05-27 16:08:39 +0900

    [#noissue] Modify Thrift installation script
    - modified to get minimal root authority
    - added exception handling code
    - added link of thrift installation page to show when installation failed

--- thrift/install-thrift-linux.sh
+++ thrift/install-thrift-linux.sh
@@ -1,5 +1,5 @@
 def_path="./src/compiler/linux/"
-def_version="thrift-0.10.0"
+def_bin="thrift-0.10.0"
 
 if [ -z "$1" ]; then
     tpath=${def_path}
@@ -8,35 +8,81 @@ else
 fi
 
 if [ -z "$2" ]; then
-    tversion=${def_version}
+    tbin=${def_bin}
 else
-    tversion=$2
+    tbin=$2
 fi
 
-# Get user's agreement for installation
-echo "INFO: Thrift will be installed. Please enter password."
-sudo test 1
-if [ $? -ne 0 ]; then
-    echo "ERROR: Authentication failed. Exit install script."
-	exit 1
+tversion=`echo $tbin | awk -F'thrift-' '{print $2}'`
+
+# Check if Thrift is installed in system
+echo -n "Check if Thrift is installed in system... "
+installed_path=`which thrift 2>&1`
+if [ $? -eq 0 ]; then
+    echo "yes"
+    # Check Thrift version
+    echo -n "Check if Thrift version is $tversion... "
+	$installed_path --version 2>&1 | grep $tversion >/dev/null
+	if [ $? -eq 0 ]; then
+        echo "yes"
+
+        # Copy Thrift binary to Thrift path
+        echo "INFO: Copy Thrift Binary to Pinpoint Thrift Path"
+        cp ${installed_path} ${tpath}${tbin}
+
+        if [ $? -eq 0 ]; then
+            exit 0
+		else
+		    echo "ERROR: Thrift binary copy failed."
+		    exit 1
+		fi
+		
+	else
+        echo "no"
+	fi
+else
+    echo "no"
+fi
+
+# Check if Thrift is builded but not copied to compiler path
+echo -n "Check if Thrift is builded but not copied to compiler path... "
+if [ -x "thrift-$tversion/compiler/cpp/thrift" ]; then
+    echo "yes"
+
+    # Copy Thrift binary to Thrift path
+    echo "INFO: Copy Thrift Binary to Pinpoint Thrift Path"
+    cp thrift-$tversion/compiler/cpp/thrift ${tpath}${tbin}
+    if [ $? -ne 0 ]; then
+	    echo "ERROR: Thrift binary copy failed."
+	    exit 1
+	fi
+		
+    # cleanup
+    rm -rf thrift-$tversion
+    if [ $? -ne 0 ]; then
+	    echo "WARNING: Thrift build directory cleanup failed. Please remove it manually."
+	fi
+    exit 0
+else
+    echo "no"
 fi
 
 # Check build environment
-echo "INFO: Check Installation Environment"
+echo "INFO: Check Thrift installation environment"
 if [ -f "/etc/os-release" ]; then
     # Get Linux distribution name and version.
     DIST_ID=`cat /etc/os-release | grep "^ID=" | awk -F "=" '{print $2;}' | tr -d '"'`
     DIST_VERSION=`cat /etc/os-release | grep "^VERSION_ID=" | awk -F "=" '{print $2;}' | tr -d '"' | awk -F "." '{print $1;}'`
+
+    echo "Linux distribution: ${DIST_ID}"
+    echo "Version: ${DIST_VERSION}"
 else
     echo "ERROR: This Linux destribution is not supported."
 	exit 1
 fi
 
 if [[ "${DIST_ID}" = "centos" || "${DIST_ID}" = "rhel" ]]; then
-    if [ "${DIST_VERSION}" -ge 7 ]; then
-        echo "Linux distribution: ${DIST_ID}"
-        echo "Version: ${DIST_VERSION}"
-	else
+    if ! [ "${DIST_VERSION}" -ge 7 ]; then
         echo "ERROR: This Linux destribution version is not supported."
 		exit 1
 	fi
@@ -45,11 +91,56 @@ else
 	exit 1
 fi
 
-# Install dependencies and pre-installed packages
-echo "INFO: Install Dependencies and Pre-installed Packages"
+# Check pre-installed packages
+echo "INFO: Check pre-installed packages" 
+pkg_count=0
 if [[ "${DIST_ID}" = "centos" || "${DIST_ID}" = "rhel" ]]; then
-    sudo yum groupinstall -y "Development Tools"
-    sudo yum install -y ant
+    # Development Tools
+	echo -n "Check Development Tools installed... "
+    yum grouplist "Development Tools" installed 2>&1 | grep "Installed Groups:" >/dev/null
+	if [ $? -eq 0 ]; then
+        echo "yes"
+	else
+        echo "no"
+		pkg_arr[${pkg_count}]="Development Tools"
+		let "pkg_count += 1"
+	fi
+
+	# ant
+	echo -n "Check ant installed... "
+	yum list ant installed 2>&1 | grep "Installed Packages" >/dev/null
+	if [ $? -eq 0 ]; then
+        echo "yes"
+	else
+        echo "no"
+		pkg_arr[${pkg_count}]="ant"
+		let "pkg_count += 1"
+	fi
+fi
+
+# Install dependencies and pre-installed packages if needed
+if [ ${pkg_count} -ne 0 ]; then
+    # If installation of pre-installed packages is required, get root privilege.
+    echo "Installation of pre-installed packages is required. Please enter password to get root privilege."
+    sudo test 1
+    if [ $? -ne 0 ]; then
+        echo "ERROR: Authentication failed. Exit install script."
+	    exit 1
+    fi
+
+    # Install dependencies and pre-installed packages
+    echo "INFO: Install Dependencies and Pre-installed Packages"
+    if [[ "${DIST_ID}" = "centos" || "${DIST_ID}" = "rhel" ]]; then
+	    for pkg in "${pkg_arr[@]}"
+		do
+		    echo "INFO: Install $pkg"
+            if [ "$pkg" = "Development Tools" ]; then
+                sudo yum groupinstall -y "Development Tools"
+			elif [ "$pkg" = "ant" ]; then
+                sudo yum install -y ant
+			fi
+		done
+    fi
 fi
 
 # Get Thrift
@@ -58,19 +149,38 @@ fi
 
 # Install Thrift
 echo "INFO: Install Thrift"
-tar zxf ${tpath}${tversion}.tar.gz
-cd ${tversion}
+tar zxf ${tpath}thrift-$tversion.tar.gz
+if [ $? -ne 0 ]; then
+    echo "ERROR: Thrift tarball decompression failed."
+    exit 1
+fi
+
+cd thrift-$tversion
 ./configure
+if [ $? -ne 0 ]; then
+    echo "ERROR: Thrift sourcecode configure failed."
+    exit 1
+fi
+
 make
-sudo make install
+if [ $? -ne 0 ]; then
+    echo "ERROR: Thrift sourcecode build failed."
+    exit 1
+fi
 cd ..
 
 # Copy Thrift binary to Thrift path
 echo "INFO: Copy Thrift Binary to Pinpoint Thrift Path"
-installed_path=`which thrift`
-cp ${installed_path} ${tpath}${tversion}
+cp thrift-$tversion/compiler/cpp/thrift ${tpath}${tbin}
+if [ $? -ne 0 ]; then
+    echo "ERROR: Thrift binary copy failed."
+    exit 1
+fi
 
-# Cleanup
-sudo rm -rf ${tversion}
+# cleanup
+rm -rf thrift-$tversion
+if [ $? -ne 0 ]; then
+    echo "WARNING: Thrift build directory cleanup failed. Please remove it manually."
+fi
 
 echo "INFO: Thrift install completed!"
