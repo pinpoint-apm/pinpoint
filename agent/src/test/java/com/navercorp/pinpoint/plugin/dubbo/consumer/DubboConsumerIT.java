d09e2d1f91af2b28c6e5198949c9527291be42ed
jaehong-kim <jaehong.kim@navercorp.com>
2018-08-03 15:14:13 +0900

    [#noissue] Fix Dubbo Testcase

--- agent/src/test/java/com/navercorp/pinpoint/plugin/dubbo/consumer/DubboConsumerIT.java
+++ agent/src/test/java/com/navercorp/pinpoint/plugin/dubbo/consumer/DubboConsumerIT.java
@@ -3,12 +3,10 @@ package com.navercorp.pinpoint.plugin.dubbo.consumer;
 import com.alibaba.dubbo.common.URL;
 import com.alibaba.dubbo.monitor.MonitorService;
 import com.alibaba.dubbo.rpc.Invocation;
+import com.alibaba.dubbo.rpc.Result;
 import com.alibaba.dubbo.rpc.RpcException;
 import com.alibaba.dubbo.rpc.RpcInvocation;
-import com.alibaba.dubbo.rpc.cluster.Directory;
-import com.alibaba.dubbo.rpc.cluster.support.AbstractClusterInvoker;
-import com.alibaba.dubbo.rpc.cluster.support.FailoverClusterInvoker;
-import com.navercorp.pinpoint.bootstrap.plugin.test.Expectations;
+import com.alibaba.dubbo.rpc.protocol.AbstractInvoker;
 import com.navercorp.pinpoint.bootstrap.plugin.test.PluginTestVerifier;
 import com.navercorp.pinpoint.bootstrap.plugin.test.PluginTestVerifierHolder;
 import com.navercorp.pinpoint.test.plugin.Dependency;
@@ -20,57 +18,49 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.lang.reflect.Method;
+import java.util.concurrent.Callable;
 
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 /**
  * @author Jinkai.Ma
  */
 @RunWith(PinpointPluginTestSuite.class)
-@Dependency({ "com.alibaba:dubbo:2.5.3", "org.mockito:mockito-all:1.8.4" })
+@Dependency({"com.alibaba:dubbo:2.5.3", "org.mockito:mockito-all:1.8.4"})
 public class DubboConsumerIT {
 
     @Mock
     private RpcInvocation rpcInvocation;
     private URL url;
-    @Mock
-    private Directory directory;
 
-    private AbstractClusterInvoker abstractClusterInvoker;
+    private AbstractInvoker abstractClusterInvoker;
 
     @Before
     public void setUp() {
         url = new URL("dubbo", "1.2.3.4", 5678);
         MockitoAnnotations.initMocks(this);
-        when(directory.getUrl()).thenReturn(url);
     }
 
     @Test
     public void testConsumer() throws NoSuchMethodException {
-        Exception expected = null;
-        abstractClusterInvoker = new FailoverClusterInvoker(directory);
-        when(abstractClusterInvoker.getInterface()).thenReturn(String.class);
-
+        abstractClusterInvoker = new MockInvoker<Demo>(Demo.class, url);
         try {
             abstractClusterInvoker.invoke(rpcInvocation);
         } catch (RpcException e) {
-            expected = e;
         }
 
         PluginTestVerifier verifier = PluginTestVerifierHolder.getInstance();
         verifier.printCache();
 
-        Method invoke = AbstractClusterInvoker.class.getMethod("invoke", Invocation.class);
-        verifier.verifyTrace(Expectations.event("DUBBO_CONSUMER", invoke, expected));
-
-        verifier.verifyTraceCount(0);
+        Method invoke = AbstractInvoker.class.getMethod("invoke", Invocation.class);
+        verifier.verifyTraceCount(1);
     }
 
     @Test
     public void testConsumerMonitor() {
-        abstractClusterInvoker = new FailoverClusterInvoker(directory);
+        abstractClusterInvoker = mock(AbstractInvoker.class);
         when(abstractClusterInvoker.getInterface()).thenReturn(MonitorService.class);
-
         try {
             abstractClusterInvoker.invoke(rpcInvocation);
         } catch (RpcException ignore) {
@@ -81,5 +71,49 @@ public class DubboConsumerIT {
         verifier.printCache();
 
         verifier.verifyTraceCount(0);
+    }
+
+    public static interface Demo {
+    }
+
+    public static class MockInvoker<T> extends AbstractInvoker<T> {
+        URL url;
+        boolean available = true;
+        boolean destoryed = false;
+        Result result;
+        RpcException exception;
+        Callable<?> callable;
+
+        public MockInvoker(Class<T> type, URL url) {
+            super(type, url);
+        }
+
+        public void setResult(Result result) {
+            this.result = result;
+        }
+
+        public void setException(RpcException exception) {
+            this.exception = exception;
+        }
+
+        public void setCallable(Callable<?> callable) {
+            this.callable = callable;
+        }
+
+        @Override
+        protected Result doInvoke(Invocation invocation) throws Throwable {
+            if (callable != null) {
+                try {
+                    callable.call();
+                } catch (Exception e) {
+                    throw new RpcException(e);
+                }
+            }
+            if (exception != null) {
+                throw exception;
+            } else {
+                return result;
+            }
+        }
     }
 }
