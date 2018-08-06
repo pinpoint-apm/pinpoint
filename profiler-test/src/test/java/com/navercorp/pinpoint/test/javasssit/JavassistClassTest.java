fae57abeccfc42767a9642bbe3a779ccf311057c
emeroad <wd.kang@navercorp.com>
2018-08-06 15:19:33 +0900

    [#1365] Fix potential stackoverflow of testcase

--- profiler-test/src/test/java/com/navercorp/pinpoint/test/javasssit/JavassistClassTest.java
+++ profiler-test/src/test/java/com/navercorp/pinpoint/test/javasssit/JavassistClassTest.java
@@ -1,18 +1,17 @@
 /*
- * Copyright 2016 NAVER Corp.
+ * Copyright 2018 NAVER Corp.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 package com.navercorp.pinpoint.test.javasssit;
@@ -232,13 +231,13 @@ public class JavassistClassTest {
         return testClassLoader;
     }
 
-    public void assertEqualsIntField(Object target, String fieldName, int value) throws NoSuchFieldException, IllegalAccessException {
+    private void assertEqualsIntField(Object target, String fieldName, int value) throws NoSuchFieldException, IllegalAccessException {
         Field field = target.getClass().getField(fieldName);
         int anInt = field.getInt(target);
         Assert.assertEquals(anInt, value);
     }
 
-    public void assertEqualsObjectField(Object target, String fieldName, Object value) throws NoSuchFieldException, IllegalAccessException {
+    private void assertEqualsObjectField(Object target, String fieldName, Object value) throws NoSuchFieldException, IllegalAccessException {
         Field field = target.getClass().getField(fieldName);
         Object obj = field.get(target);
         Assert.assertEquals(value, obj);
