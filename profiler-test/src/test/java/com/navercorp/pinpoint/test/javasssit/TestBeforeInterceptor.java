5d412e12bc4c8eb9bac504c5e69f9a4c0f861e54
emeroad <wd.kang@navercorp.com>
2018-08-06 15:19:33 +0900

    [#1365] Fix potential stackoverflow of testcase

--- profiler-test/src/test/java/com/navercorp/pinpoint/test/javasssit/TestBeforeInterceptor.java
+++ profiler-test/src/test/java/com/navercorp/pinpoint/test/javasssit/TestBeforeInterceptor.java
@@ -1,18 +1,17 @@
 /*
- * Copyright 2016 NAVER Corp.
+ * Copyright 2018 NAVER Corp.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 package com.navercorp.pinpoint.test.javasssit;
@@ -41,7 +40,7 @@ public class TestBeforeInterceptor implements StaticAroundInterceptor {
 
     @Override
     public void before(Object target, String className, String methodName, String parameterDescription, Object[] args) {
-        logger.info("BEFORE target:" + target + " " + className + "." + methodName + parameterDescription + " args:" + Arrays.toString(args));
+        logger.info("BEFORE target:" + toClassName(target) + " " + className + "." + methodName + parameterDescription + " args:" + Arrays.toString(args));
         this.target = target;
         this.className = className;
         this.methodName = methodName;
@@ -49,6 +48,13 @@ public class TestBeforeInterceptor implements StaticAroundInterceptor {
         call++;
         
         TestInterceptors.add(this);
+    }
+
+    private String toClassName(Object target) {
+        if (target == null) {
+            return "null";
+        }
+        return target.getClass().getName();
     }
 
     @Override
