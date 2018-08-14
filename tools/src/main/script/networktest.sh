2cddbc9be345e430477c6db985915dfcfd6b8acd
Roy Kim <sungwook0115.kim@gmail.com>
2018-08-14 13:00:45 +0900

    [#noissue] refactor tools

--- tools/src/main/script/networktest.sh
+++ tools/src/main/script/networktest.sh
@@ -3,6 +3,7 @@
 WORKING_DIR=$(dirname "$0")
 cd -P "$WORKING_DIR"/.. > /dev/null
 
-CL_PATH=$(find . -name '*.jar' | tr "\n" :)
+CL_PATH=$(find ./tools -name '*.jar' | tr "\n" :)
 echo "CLASSPATH=$CL_PATH"
-java -classpath $CL_PATH com.navercorp.pinpoint.tools.NetworkAvailabilityChecker pinpoint.config
+
+java -cp $CL_PATH com.navercorp.pinpoint.tools.NetworkAvailabilityChecker ./pinpoint.config
