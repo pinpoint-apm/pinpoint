759c356df1eaf464b405762565dc575c2b539b5c
业余布道师 <Victor.Zxy@outlook.com>
2018-09-28 21:25:12 +0800

    add testcase

--- plugins/fastjson/src/test/java/com/navercorp/pinpoint/plugin/fastjson/interceptor/ParseArrayInterceptorTest.java
+++ plugins/fastjson/src/test/java/com/navercorp/pinpoint/plugin/fastjson/interceptor/ParseArrayInterceptorTest.java
@@ -1,11 +1,17 @@
 package com.navercorp.pinpoint.plugin.fastjson.interceptor;
 
 import com.navercorp.pinpoint.bootstrap.context.MethodDescriptor;
+import com.navercorp.pinpoint.bootstrap.context.SpanEventRecorder;
+import com.navercorp.pinpoint.bootstrap.context.Trace;
 import com.navercorp.pinpoint.bootstrap.context.TraceContext;
+import com.navercorp.pinpoint.plugin.fastjson.FastjsonConstants;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ParseArrayInterceptorTest {
@@ -16,8 +22,17 @@ public class ParseArrayInterceptorTest {
     @Mock
     private MethodDescriptor descriptor;
 
+    @Mock
+    private Trace trace;
+
+    @Mock
+    private SpanEventRecorder recorder;
+
     @Test
     public void before() {
+
+        doReturn(trace).when(traceContext).currentTraceObject();
+        doReturn(recorder).when(trace).traceBlockBegin();
 
         ParseArrayInterceptor interceptor = new ParseArrayInterceptor(traceContext, descriptor);
 
@@ -27,8 +42,14 @@ public class ParseArrayInterceptorTest {
     @Test
     public void after() {
 
+        doReturn(trace).when(traceContext).currentTraceObject();
+        doReturn(recorder).when(trace).currentSpanEventRecorder();
+
         ParseArrayInterceptor interceptor = new ParseArrayInterceptor(traceContext, descriptor);
 
-        interceptor.after(null, new Object[]{null}, null, null);
+        interceptor.after(null, new Object[]{"{\"firstName\": \"Json\"}"}, null, null);
+
+        verify(recorder).recordServiceType(FastjsonConstants.SERVICE_TYPE);
+        verify(recorder).recordAttribute(FastjsonConstants.ANNOTATION_KEY_JSON_LENGTH, "{\"firstName\": \"Json\"}".length());
     }
 }
