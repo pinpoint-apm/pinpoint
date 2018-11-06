e52d991a91fd31d55b3f816c697e105251ab4de9
hyojong-song <hyojong.song2015@navercorp.com>
2018-11-06 10:04:08 +0900

    [#noissue] Change code highlight library.
    - highlightjs -> prettiffy

--- web/src/main/webapp/v2/src/app/core/components/syntax-highlight-popup/syntax-highlight-popup.component.ts
+++ web/src/main/webapp/v2/src/app/core/components/syntax-highlight-popup/syntax-highlight-popup.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input, Output, EventEmitter, HostBinding } from '@angular/core';
+declare var PR: any;
+import { Component, OnInit, Input, Output, EventEmitter, HostBinding, AfterViewChecked } from '@angular/core';
 import { ClipboardService } from 'ngx-clipboard';
 
 @Component({
@@ -6,15 +7,16 @@ import { ClipboardService } from 'ngx-clipboard';
     templateUrl: './syntax-highlight-popup.component.html',
     styleUrls: ['./syntax-highlight-popup.component.css']
 })
-export class SyntaxHighlightPopupComponent implements OnInit {
+export class SyntaxHighlightPopupComponent implements OnInit, AfterViewChecked {
     @Input() data: ISyntaxHighlightData;
     @Output() outClosePopup = new EventEmitter<void>();
     @HostBinding('class.font-opensans') fontFamily = true;
 
-    constructor(
-        private clipboardService: ClipboardService
-    ) {}
+    constructor(private clipboardService: ClipboardService) {}
     ngOnInit() {}
+    ngAfterViewChecked() {
+        PR.prettyPrint();
+    }
     onCopyOriginalContents() {
         this.clipboardService.copyFromContent(this.data.originalContents);
     }
@@ -29,5 +31,8 @@ export class SyntaxHighlightPopupComponent implements OnInit {
     }
     hasBind(): boolean {
         return !!this.data.bindValue;
+    }
+    getClassName(): string {
+        return 'language-' + this.data.type.toLowerCase();
     }
 }
