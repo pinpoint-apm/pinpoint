8c02540dca1e8839027dd8a0f41530690e154042
hyojong-song <hyojong.song2015@navercorp.com>
2018-12-05 14:36:39 +0900

    [#noissue] bug fix.
    - Information in the scatter chart is not displayed correctly in real-time mode.

--- web/src/main/webapp/v2/src/app/core/components/response-summary-chart/response-summary-chart-for-side-bar-container.component.ts
+++ web/src/main/webapp/v2/src/app/core/components/response-summary-chart/response-summary-chart-for-side-bar-container.component.ts
@@ -101,18 +101,20 @@ export class ResponseSummaryChartForSideBarContainerComponent implements OnInit,
     }
     private loadResponseSummaryChartData(from?: number, to?: number): void {
         const target = this.getTargetInfo();
-        if (this.isAllAgent() && arguments.length !== 2) {
-            this.passDownChartData(this.agentHistogramDataService.makeChartDataForResponseSummary(target.histogram, this.getChartYMax()));
-        } else {
-            this.agentHistogramDataService.getData(target.key, target.applicationName, target.serviceTypeCode, this.serverMapData, from, to).subscribe((chartData: any) => {
-                const chartDataForAgent = this.isAllAgent() ? chartData['histogram'] : chartData['agentHistogram'][this.selectedAgent];
-                this.passDownChartData(this.agentHistogramDataService.makeChartDataForResponseSummary(chartDataForAgent, this.getChartYMax()));
-            }, (error: IServerErrorFormat) => {
-                this.hasRequestError = true;
-                this.hiddenChart = true;
-                this.setDisable(false);
-                this.changeDetector.detectChanges();
-            });
+        if (target) {
+            if (this.isAllAgent() && arguments.length !== 2) {
+                this.passDownChartData(this.agentHistogramDataService.makeChartDataForResponseSummary(target.histogram, this.getChartYMax()));
+            } else {
+                this.agentHistogramDataService.getData(target.key, target.applicationName, target.serviceTypeCode, this.serverMapData, from, to).subscribe((chartData: any) => {
+                    const chartDataForAgent = this.isAllAgent() ? chartData['histogram'] : chartData['agentHistogram'][this.selectedAgent];
+                    this.passDownChartData(this.agentHistogramDataService.makeChartDataForResponseSummary(chartDataForAgent, this.getChartYMax()));
+                }, (error: IServerErrorFormat) => {
+                    this.hasRequestError = true;
+                    this.hiddenChart = true;
+                    this.setDisable(false);
+                    this.changeDetector.detectChanges();
+                });
+            }
         }
     }
     private passDownChartData(chartData: any): void {
