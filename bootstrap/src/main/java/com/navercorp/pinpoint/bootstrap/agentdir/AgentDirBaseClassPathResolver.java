72aec31c591aa5acc663356f213f9de41338ecf1
emeroad <wd.kang@navercorp.com>
2018-12-14 19:00:31 +0900

    [#5010] Rename bootstrap-core-option module  - bootstrap-core-optional -> bootstrap-java7

--- bootstrap/src/main/java/com/navercorp/pinpoint/bootstrap/agentdir/AgentDirBaseClassPathResolver.java
+++ bootstrap/src/main/java/com/navercorp/pinpoint/bootstrap/agentdir/AgentDirBaseClassPathResolver.java
@@ -44,10 +44,10 @@ public class AgentDirBaseClassPathResolver implements ClassPathResolver {
     private final JarDescription commons = new JarDescription("pinpoint-commons", true);
     private final JarDescription bootstrapCore = new JarDescription("pinpoint-bootstrap-core", true);
     private final JarDescription annotations = new JarDescription("pinpoint-annotations", false);
-    private final JarDescription bootstrapCoreOptional = new JarDescription("pinpoint-bootstrap-core-optional", false);
+    private final JarDescription bootstrapJava7 = new JarDescription("pinpoint-bootstrap-java7", false);
     private final JarDescription bootstrapJava8 = new JarDescription("pinpoint-bootstrap-java8", false);
     private final JarDescription bootstrapJava9 = new JarDescription("pinpoint-bootstrap-java9", false);
-    private final List<JarDescription> bootJarDescriptions = Arrays.asList(commons, bootstrapCore, annotations, bootstrapCoreOptional, bootstrapJava8, bootstrapJava9);
+    private final List<JarDescription> bootJarDescriptions = Arrays.asList(commons, bootstrapCore, annotations, bootstrapJava7, bootstrapJava8, bootstrapJava9);
 
     private final String classPath;
 
