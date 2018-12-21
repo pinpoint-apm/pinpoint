af749585463d9f9b325b07b7a7958aaaf127da80
QooQooDass <bindong.kim@navercorp.com>
2018-12-21 12:34:41 +0900

    [#5001] Refactoring

--- web/src/main/webapp/v2/src/app/core/components/timeline/agent-timeline-data.service.ts
+++ web/src/main/webapp/v2/src/app/core/components/timeline/agent-timeline-data.service.ts
@@ -3,9 +3,6 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { retry } from 'rxjs/operators';
 
-import { NewUrlStateNotificationService } from 'app/shared/services';
-import { UrlPathId } from 'app/shared/models';
-
 export interface IAgentTimeline {
     agentEventTimeline: {
         timelineSegments: any
@@ -19,29 +16,22 @@ export interface IAgentTimeline {
         }[]
     };
 }
-export interface IRetrieveTime {
-    start: number;
-    end: number;
-}
 
 @Injectable()
 export class AgentTimelineDataService {
     private requestURL = 'getAgentStatusTimeline.pinpoint';
 
     constructor(
-        private http: HttpClient,
-        private newUrlStateNotificationService: NewUrlStateNotificationService
+        private http: HttpClient
     ) {}
 
-    getData(retrieveTime: IRetrieveTime): Observable<IAgentTimeline> {
-        const agentId = this.newUrlStateNotificationService.getPathValue(UrlPathId.AGENT_ID);
-
-        return this.http.get<IAgentTimeline>(this.requestURL, this.makeRequestOptionsArgs(agentId, retrieveTime)).pipe(
+    getData(agentId: string, range: number[]): Observable<IAgentTimeline> {
+        return this.http.get<IAgentTimeline>(this.requestURL, this.makeRequestOptionsArgs(agentId, range)).pipe(
             retry(3)
         );
     }
 
-    private makeRequestOptionsArgs(agentId: string, { start: from, end: to }: IRetrieveTime): { 'params': { [key: string]: any } } {
+    private makeRequestOptionsArgs(agentId: string, [from, to]: number[]): { 'params': { [key: string]: any } } {
         return {
             params: new HttpParams()
                 .set('agentId', agentId)
