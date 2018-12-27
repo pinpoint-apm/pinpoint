34acd6462c16a86cfa7b4a95ff84263d481265db
QooQooDass <bindong.kim@navercorp.com>
2018-12-27 11:51:48 +0900

    [#5051] Rewrite the configuration popup interface

--- web/src/main/webapp/v2/src/app/core/components/configuration-popup/index.ts
+++ web/src/main/webapp/v2/src/app/core/components/configuration-popup/index.ts
@@ -1,68 +1,22 @@
 
 import { NgModule } from '@angular/core';
-import { ClipboardModule } from 'ngx-clipboard';
 
 import { SharedModule } from 'app/shared';
-import { AlarmRuleListModule } from 'app/core/components/alarm-rule-list';
-import { ApplicationListModule } from 'app/core/components/application-list';
-import { UserGroupModule } from 'app/core/components/user-group';
-import { GroupMemberModule } from 'app/core/components/group-member';
-import { PinpointUserModule } from 'app/core/components/pinpoint-user';
-import { InboundOutboundRangeSelectorModule } from 'app/core/components/inbound-outbound-range-selector';
-import { SearchPeriodModule } from 'app/core/components/search-period';
-import { TimezoneModule } from 'app/core/components/timezone/index';
-import { DateFormatModule } from 'app/core/components/date-format';
-import { DuplicationCheckModule } from 'app/core/components/duplication-check';
-import { ConfigurationPopupComponent } from './configuration-popup.component';
 import { ConfigurationPopupContainerComponent } from './configuration-popup-container.component';
-import { ConfigurationPopupGeneralContainerComponent } from './configuration-popup-general-container.component';
-import { ConfigurationPopupUsergroupComponent } from './configuration-popup-usergroup.component';
-import { ConfigurationPopupAlarmComponent } from './configuration-popup-alarm.component';
-import { ConfigurationPopupHelpContainerComponent } from './configuration-popup-help-container.component';
-import { ConfigurationPopupInstallationContainerComponent } from './configuration-popup-installation-container.component';
-import { ConfigurationPopupInstallationDownloadLinkComponent } from './configuration-popup-installation-download-link.component';
-import { ConfigurationPopupInstallationJVMArgumentInfoComponent } from './configuration-popup-installation-jvm-argument-info.component';
-
-import { ConfigurationPopupInstallationDataService } from './configuration-popup-installation-data.service';
+import { ConfigurationPopupComponent } from './configuration-popup.component';
 
 @NgModule({
     declarations: [
-        ConfigurationPopupComponent,
         ConfigurationPopupContainerComponent,
-        ConfigurationPopupGeneralContainerComponent,
-        ConfigurationPopupUsergroupComponent,
-        ConfigurationPopupAlarmComponent,
-        ConfigurationPopupHelpContainerComponent,
-        ConfigurationPopupInstallationContainerComponent,
-        ConfigurationPopupInstallationDownloadLinkComponent,
-        ConfigurationPopupInstallationJVMArgumentInfoComponent
+        ConfigurationPopupComponent
     ],
     imports: [
-        SharedModule,
-        ClipboardModule,
-        AlarmRuleListModule,
-        ApplicationListModule,
-        UserGroupModule,
-        GroupMemberModule,
-        PinpointUserModule,
-        InboundOutboundRangeSelectorModule,
-        SearchPeriodModule,
-        ApplicationListModule,
-        TimezoneModule,
-        DateFormatModule,
-        DuplicationCheckModule,
+        SharedModule
     ],
     exports: [],
     entryComponents: [
         ConfigurationPopupContainerComponent,
-        ConfigurationPopupGeneralContainerComponent,
-        ConfigurationPopupUsergroupComponent,
-        ConfigurationPopupAlarmComponent,
-        ConfigurationPopupInstallationContainerComponent,
-        ConfigurationPopupHelpContainerComponent
     ],
-    providers: [
-        ConfigurationPopupInstallationDataService
-    ]
+    providers: []
 })
 export class ConfigurationPopupModule { }
