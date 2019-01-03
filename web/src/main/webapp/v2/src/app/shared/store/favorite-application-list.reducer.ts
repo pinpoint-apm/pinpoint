e9b54696e7d5aa07c5bf49b326b66cb06e673697
QooQooDass <bindong.kim@navercorp.com>
2019-01-03 17:04:10 +0900

    [#5051] Implement temporary bug-preventing code for the fav application list state
    - It will be removed after restructuring the project entirely

--- web/src/main/webapp/v2/src/app/shared/store/favorite-application-list.reducer.ts
+++ web/src/main/webapp/v2/src/app/shared/store/favorite-application-list.reducer.ts
@@ -15,7 +15,12 @@ export class RemoveFavoriteApplication implements Action {
 export function Reducer(state: IApplication[] = [], action: AddFavoriteApplication | RemoveFavoriteApplication): IApplication[] {
     switch (action.type) {
         case ADD_FAVORITE_APPLICATION:
-            return [...state, ...action.payload];
+            // TODO: 프로젝트 구조 수정 후에는 따로 체크 할 필요없이 바로 Add하기.
+            if (isSameFavAppList(state, action.payload)) {
+                return state;
+            } else {
+                return [...state, ...action.payload];
+            }
         case REMOVE_FAVORITE_APPLICATION:
             return state.filter((application: IApplication) => {
                 for (let i = 0 ; i < action.payload.length ; i++) {
@@ -28,4 +33,10 @@ export function Reducer(state: IApplication[] = [], action: AddFavoriteApplicati
         default:
             return state;
     }
+}
+
+function isSameFavAppList(appList1: IApplication[], appList2: IApplication[]): boolean {
+    const makeAppListStrFunc = (appList: IApplication[]) => appList.map((app: IApplication) => app.getApplicationName()).join(',');
+
+    return makeAppListStrFunc(appList1) === makeAppListStrFunc(appList2);
 }
