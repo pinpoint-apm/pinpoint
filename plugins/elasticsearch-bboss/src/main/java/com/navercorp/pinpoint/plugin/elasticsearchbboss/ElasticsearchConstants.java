52bf9176560102b972fe31b93eaf5b3afce30b15
yinbp <yin-bp@163.com>
2019-01-06 11:50:10 +0800

    modify comment

--- plugins/elasticsearch-bboss/src/main/java/com/navercorp/pinpoint/plugin/elasticsearchbboss/ElasticsearchConstants.java
+++ plugins/elasticsearch-bboss/src/main/java/com/navercorp/pinpoint/plugin/elasticsearchbboss/ElasticsearchConstants.java
@@ -1,19 +1,19 @@
-package com.navercorp.pinpoint.plugin.elasticsearchbboss;
-/**
- * Copyright 2008 biaoping.yin
- * <p>
+/*
+ * Copyright 2016 Pinpoint contributors and NAVER Corp.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.navercorp.pinpoint.plugin.elasticsearchbboss;
 
 import com.navercorp.pinpoint.common.trace.*;
 
@@ -21,12 +21,7 @@ import static com.navercorp.pinpoint.common.trace.ServiceTypeProperty.RECORD_STA
 import static com.navercorp.pinpoint.common.trace.ServiceTypeProperty.TERMINAL;
 
 /**
- * <p>Description: </p>
- * <p></p>
- * <p>Copyright (c) 2018</p>
- * @Date 2019/1/5 22:48
- * @author biaoping.yin
- * @version 1.0
+ * @author yinbp[yin-bp@163.com]
  */
 public class ElasticsearchConstants {
 	public static final AnnotationKey ARGS_ANNOTATION_KEY = AnnotationKeyFactory.of(971, "es.args", AnnotationKeyProperty.VIEW_IN_RECORD_SET);
