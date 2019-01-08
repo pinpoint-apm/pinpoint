975764ee9a91747edf1abf94128dfdecb0cc7b29
QooQooDass <bindong.kim@navercorp.com>
2019-01-08 10:47:38 +0900

    [#noissue] Fix: Handle the case the user enters the config page directly
    - Navigate to the main page when clicking the exit button

--- web/src/main/webapp/v2/src/app/shared/store/url-path.reducer.ts
+++ web/src/main/webapp/v2/src/app/shared/store/url-path.reducer.ts
@@ -1,5 +1,7 @@
 import { Action } from '@ngrx/store';
 
+import { UrlPath } from 'app/shared/models';
+
 const UPDATE_URL_PATH = 'UPDATE_URL_PATH';
 
 export class UpdateURLPath implements Action {
@@ -7,7 +9,7 @@ export class UpdateURLPath implements Action {
     constructor(public payload: string) {}
 }
 
-export function Reducer(state: string, action: UpdateURLPath): string {
+export function Reducer(state = `/${UrlPath.MAIN}`, action: UpdateURLPath): string {
     switch (action.type) {
         case UPDATE_URL_PATH:
             return action.payload;
