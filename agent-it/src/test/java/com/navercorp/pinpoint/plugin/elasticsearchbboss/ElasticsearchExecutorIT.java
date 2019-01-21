9e20b47ed3aca2ca1ea427e67e2c2cc0a6c4b883
yinbp <yin-bp@163.com>
2019-01-21 20:44:00 +0800

    change elasticsearch client lib bboss version to 5.3.6

--- agent-it/src/test/java/com/navercorp/pinpoint/plugin/elasticsearchbboss/ElasticsearchExecutorIT.java
+++ agent-it/src/test/java/com/navercorp/pinpoint/plugin/elasticsearchbboss/ElasticsearchExecutorIT.java
@@ -29,7 +29,7 @@ import org.junit.runner.RunWith;
  * @author yinbp[yin-bp@163.com]
  */
 @RunWith(PinpointPluginTestSuite.class)
-@Dependency({"com.bbossgroups.plugins:bboss-elasticsearch-rest-jdbc:5.2.9"})
+@Dependency({"com.bbossgroups.plugins:bboss-elasticsearch-rest-jdbc:5.3.6"})
 public class ElasticsearchExecutorIT {
 	@Test
 	public void test(){
