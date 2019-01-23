a829bb0321b9a0f6b2832ec1ea1d6c024ce5c3ca
koo-taejin <koo.taejin@navercorp.com>
2019-01-23 13:41:37 +0900

    [release] prepare for 1.8.2-RC1

--- commons/src/main/java/com/navercorp/pinpoint/common/Version.java
+++ commons/src/main/java/com/navercorp/pinpoint/common/Version.java
@@ -1,4 +1,4 @@
 package com.navercorp.pinpoint.common;
 public final class Version {
-    public static final String VERSION = "1.8.2-SNAPSHOT";
+    public static final String VERSION = "1.8.2-RC1";
 }
