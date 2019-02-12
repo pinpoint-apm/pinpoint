fa0e48b7dd751c13fb4ebf7e0b39f152aaa55a35
koo-taejin <koo.taejin@navercorp.com>
2019-02-12 15:52:35 +0900

    [release] prepare for 1.8.3-SNAPSHOT
    1.8.2 -> 1.8.3-SNAPSHOT

--- commons/src/main/java/com/navercorp/pinpoint/common/Version.java
+++ commons/src/main/java/com/navercorp/pinpoint/common/Version.java
@@ -1,4 +1,4 @@
 package com.navercorp.pinpoint.common;
 public final class Version {
-    public static final String VERSION = "1.8.2";
+    public static final String VERSION = "1.8.3-SNAPSHOT";
 }
