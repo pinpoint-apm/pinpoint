4a4778605780b762297f1b575d11b50d47d4dc74
Roy Kim <sungwook0115.kim@gmail.com>
2019-03-20 19:42:03 +0900

    [#noissue] enable mongodb IT

--- agent-it/src/test/java/com/navercorp/pinpoint/plugin/mongodb/MongoDBIT_3_7_x_IT.java
+++ agent-it/src/test/java/com/navercorp/pinpoint/plugin/mongodb/MongoDBIT_3_7_x_IT.java
@@ -22,6 +22,7 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.navercorp.pinpoint.plugin.AgentPath;
 import com.navercorp.pinpoint.test.plugin.Dependency;
+import com.navercorp.pinpoint.test.plugin.JvmVersion;
 import com.navercorp.pinpoint.test.plugin.PinpointAgent;
 import com.navercorp.pinpoint.test.plugin.PinpointPluginTestSuite;
 import org.junit.AfterClass;
@@ -33,9 +34,10 @@ import org.junit.runner.RunWith;
  */
 @RunWith(PinpointPluginTestSuite.class)
 @PinpointAgent(AgentPath.PATH)
+@JvmVersion(8)
 @Dependency({
         "org.mongodb:mongodb-driver:[3.7.0,]",
-        "de.flapdoodle.embed:de.flapdoodle.embed.mongo:2.0.0"
+        "de.flapdoodle.embed:de.flapdoodle.embed.mongo:2.1.1"
 })
 public class MongoDBIT_3_7_x_IT extends MongoDBITBase {
 
