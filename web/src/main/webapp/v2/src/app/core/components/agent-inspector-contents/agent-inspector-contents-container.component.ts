afb556dfa01bf51551dd72041fbe4218b1d234ea
QooQooDass <bindong.kim@navercorp.com>
2019-04-03 16:18:28 +0900

    [#5446] Apply the chart layout option to the agent/application inspector page
    - Create SafeStyle pipe to apply the grid style
    - Adjust the dom structure to control the layout
    - Adapt grid display instead of flex

--- web/src/main/webapp/v2/src/app/core/components/agent-inspector-contents/agent-inspector-contents-container.component.ts
+++ web/src/main/webapp/v2/src/app/core/components/agent-inspector-contents/agent-inspector-contents-container.component.ts
@@ -1,11 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subject, Observable, merge, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { MessageQueueService, MESSAGE_TO, WebAppSettingDataService } from 'app/shared/services';
 
 @Component({
     selector: 'pp-agent-inspector-contents-container',
     templateUrl: './agent-inspector-contents-container.component.html',
-    styleUrls: ['./agent-inspector-contents-container.component.css']
+    styleUrls: ['./agent-inspector-contents-container.component.css'],
 })
-export class AgentInspectorContentsContainerComponent implements OnInit {
-    constructor() {}
-    ngOnInit() {}
+export class AgentInspectorContentsContainerComponent implements OnInit, OnDestroy {
+    private unsubscribe = new Subject<void>();
+
+    gridLayout$: Observable<string>;
+
+    constructor(
+        private webAppSettingDataService: WebAppSettingDataService,
+        private messageQueueService: MessageQueueService,
+    ) {}
+
+    ngOnInit() {
+        this.gridLayout$ = merge(
+            of(this.webAppSettingDataService.getChartLayoutOption()),
+            this.messageQueueService.receiveMessage(this.unsubscribe, MESSAGE_TO.INSPECTOR_CHART_SET_LAYOUT).pipe(
+                map(([chartNumbPerRow]: number[]) => chartNumbPerRow)
+            )
+        ).pipe(
+            map((chartNumPerRow: number) => this.getGridLayout(chartNumPerRow))
+        );
+    }
+
+    ngOnDestroy() {
+        this.unsubscribe.next();
+        this.unsubscribe.complete();
+    }
+
+    private getGridLayout(chartNumPerRow: number): string {
+        return `repeat(${chartNumPerRow}, 1fr)`;
+    }
 }
--- web/src/main/webapp/v2/src/app/core/components/application-inspector-contents/application-inspector-contents-container.component.ts
+++ web/src/main/webapp/v2/src/app/core/components/application-inspector-contents/application-inspector-contents-container.component.ts
@@ -1,21 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, of, Subject, merge } from 'rxjs';
+import { map } from 'rxjs/operators';
 
-import { WebAppSettingDataService } from 'app/shared/services';
+import { WebAppSettingDataService, MessageQueueService, MESSAGE_TO } from 'app/shared/services';
 
 @Component({
     selector: 'pp-application-inspector-contents-container',
     templateUrl: './application-inspector-contents-container.component.html',
     styleUrls: ['./application-inspector-contents-container.component.css']
 })
-export class ApplicationInspectorContentsContainerComponent implements OnInit {
+export class ApplicationInspectorContentsContainerComponent implements OnInit, OnDestroy {
+    private unsubscribe = new Subject<void>();
+
     isApplicationInspectorActivated$: Observable<boolean>;
+    gridLayout$: Observable<string>;
 
     constructor(
-        private webAppSettingDataService: WebAppSettingDataService
+        private webAppSettingDataService: WebAppSettingDataService,
+        private messageQueueService: MessageQueueService,
     ) {}
 
     ngOnInit() {
         this.isApplicationInspectorActivated$ = this.webAppSettingDataService.isApplicationInspectorActivated();
+        this.gridLayout$ = merge(
+            of(this.webAppSettingDataService.getChartLayoutOption()),
+            this.messageQueueService.receiveMessage(this.unsubscribe, MESSAGE_TO.INSPECTOR_CHART_SET_LAYOUT).pipe(
+                map(([chartNumbPerRow]: number[]) => chartNumbPerRow)
+            )
+        ).pipe(
+            map((chartNumPerRow: number) => this.getGridLayout(chartNumPerRow))
+        );
+    }
+
+    ngOnDestroy() {
+        this.unsubscribe.next();
+        this.unsubscribe.complete();
+    }
+
+    private getGridLayout(chartNumPerRow: number): string {
+        return `repeat(${chartNumPerRow}, 1fr)`;
     }
 }
