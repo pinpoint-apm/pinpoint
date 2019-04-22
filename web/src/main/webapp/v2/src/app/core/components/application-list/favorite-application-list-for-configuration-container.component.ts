a61b2848eba4129ee5a837d5035bc6e08251f1ce
hyojong-song <hyojong.song2015@navercorp.com>
2019-04-22 14:22:03 +0900

    [#noissue] Add searching in favorite list.

--- web/src/main/webapp/v2/src/app/core/components/application-list/favorite-application-list-for-configuration-container.component.ts
+++ web/src/main/webapp/v2/src/app/core/components/application-list/favorite-application-list-for-configuration-container.component.ts
@@ -1,25 +1,32 @@
-import { Component, OnInit, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { Observable, Subject } from 'rxjs';
+import { Subject, forkJoin } from 'rxjs';
 
-import { WebAppSettingDataService, StoreHelperService, AnalyticsService, TRACKED_EVENT_LIST } from 'app/shared/services';
+import { WebAppSettingDataService, StoreHelperService, TranslateReplaceService, AnalyticsService, TRACKED_EVENT_LIST } from 'app/shared/services';
 
 @Component({
     selector: 'pp-favorite-application-list-for-configuration-container',
-    templateUrl: './application-list-for-configuration-container.component.html',
-    styleUrls: ['./application-list-for-configuration-container.component.css'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+    templateUrl: './favorite-application-list-for-configuration-container.component.html',
+    styleUrls: ['./favorite-application-list-for-configuration-container.component.css']
 })
 export class FavoriteApplicationListForConfigurationContainerComponent implements OnInit, OnDestroy {
     private unsubscribe: Subject<void> = new Subject();
-    applicationList$: Observable<IApplication[]>;
-    emptyText$: Observable<string>;
+    favoriteApplicationList: IApplication[];
+    applicationList: IApplication[];
+    emptyText: string;
     funcImagePath: Function;
     iconBtnClassName = 'far fa-trash-alt';
+    query = '';
+    searchUseEnter = false;
+    SEARCH_MIN_LENGTH = 2;
+    i18nText = {
+        SEARCH_INPUT_GUIDE: ''
+    };
 
     constructor(
         private storeHelperService: StoreHelperService,
         private translateService: TranslateService,
+        private translateReplaceService: TranslateReplaceService,
         private webAppSettingDataService: WebAppSettingDataService,
         private analyticsService: AnalyticsService,
     ) {}
@@ -35,12 +42,43 @@ export class FavoriteApplicationListForConfigurationContainerComponent implement
     }
 
     private initList(): void {
-        this.applicationList$ = this.storeHelperService.getFavoriteApplicationList(this.unsubscribe);
+        this.storeHelperService.getFavoriteApplicationList(this.unsubscribe).subscribe((favoriteApplicationList: IApplication[]) => {
+            this.favoriteApplicationList = favoriteApplicationList;
+            this.filterApplicationList();
+        });
+    }
+    private filterApplicationList(): void {
+        if (this.query !== '') {
+            this.applicationList = this.favoriteApplicationList.filter((app: IApplication) => {
+                return app.getApplicationName().toLowerCase().indexOf(this.query.toLowerCase()) !== -1;
+            });
+        } else {
+            this.applicationList = this.favoriteApplicationList;
+        }
     }
     private initEmptyText(): void {
-        this.emptyText$ = this.translateService.get('CONFIGURATION.GENERAL.EMPTY');
+        forkJoin(
+            this.translateService.get('COMMON.MIN_LENGTH'),
+            this.translateService.get('COMMON.NO_CONTENT_TO_DISPLAY')
+        ).subscribe(([minLengthMessage, emptyText]: string[]) => {
+            this.i18nText.SEARCH_INPUT_GUIDE = this.translateReplaceService.replace(minLengthMessage, this.SEARCH_MIN_LENGTH);
+            this.emptyText = emptyText;
+        });
     }
-
+    onClearSearch(input: HTMLInputElement): void {
+        if (this.query !== '') {
+            this.query = '';
+            input.value = '';
+            this.filterApplicationList();
+            input.focus();
+        }
+    }
+    onSearch(query: string): void {
+        if (this.query !== query) {
+            this.query = query;
+            this.filterApplicationList();
+        }
+    }
     onSelectApp(app: IApplication): void {
         this.webAppSettingDataService.removeFavoriteApplication(app);
         this.analyticsService.trackEvent(TRACKED_EVENT_LIST.REMOVE_FAVORITE_APPLICATION);
