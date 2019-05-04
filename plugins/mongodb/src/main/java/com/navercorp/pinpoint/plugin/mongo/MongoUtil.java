04c31d89dced172bba7cb2cea9a59588ef030cfa
Gowri Sankar <v-gowri.s@turvo.com>
2019-05-04 22:38:11 +0530

    Update MongoUtil to parse the Mongo Query and Bind variable values for MongoDB 2.x Driver

--- plugins/mongodb/src/main/java/com/navercorp/pinpoint/plugin/mongo/MongoUtil.java
+++ plugins/mongodb/src/main/java/com/navercorp/pinpoint/plugin/mongo/MongoUtil.java
@@ -14,13 +14,15 @@
  */
 package com.navercorp.pinpoint.plugin.mongo;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bson.BsonType;
+
+import com.mongodb.DBObject;
 import com.mongodb.WriteConcern;
 import com.navercorp.pinpoint.bootstrap.context.SpanEventRecorder;
 import com.navercorp.pinpoint.common.util.StringStringValue;
-import org.bson.BsonType;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author Roy Kim
@@ -37,6 +39,13 @@ public final class MongoUtil {
 
     //since Mongo Java Driver 3.4
     private static boolean decimal128Enabled() {
+        try {
+            Class.forName("org.bson.BsonType");
+        } catch (ClassNotFoundException e) {
+            // MongoDB Driver < 3.X
+            return false;
+        }
+
         for (BsonType bsonType : BsonType.values()) {
             if (bsonType.name().equalsIgnoreCase("DECIMAL128")) {
                 return true;
@@ -85,6 +94,67 @@ public final class MongoUtil {
         String parsedJsonString = StringJoiner.join(parsedJson, SEPARATOR);
         String jsonParameterString = StringJoiner.join(jsonParameter, SEPARATOR);
         return new NormalizedBson(parsedJsonString, jsonParameterString);
+    }
+
+    public static NormalizedBson parseJson(Object[] args, boolean traceJsonBindValue) {
+        StringBuilder parsedJsonBuilder = new StringBuilder();
+        StringBuilder jsonParamBuilder = new StringBuilder();
+        parsedJsonBuilder.append("{");
+        for (Object arg : args) {
+            if (arg instanceof DBObject[]) {
+                DBObject[] objects = (DBObject[]) arg;
+                for (DBObject object : objects) {
+                    extractFromDbObject(parsedJsonBuilder, jsonParamBuilder, object);
+                }
+                adjustJson(parsedJsonBuilder, jsonParamBuilder);
+            } else if (arg instanceof DBObject) {
+                extractFromDbObject(parsedJsonBuilder, jsonParamBuilder, (DBObject) arg);
+                adjustJson(parsedJsonBuilder, jsonParamBuilder);
+            } else {
+                System.err.println("Not a DBObject; it's a(n) " + arg.getClass());
+            }
+        }
+
+        String parsedJsonString = parsedJsonBuilder.toString();
+        String jsonParameterString = jsonParamBuilder.toString();
+        System.out.println("parsedJsonString - " + parsedJsonString + "; jsonParameterString - " + jsonParameterString);
+        return new NormalizedBson(parsedJsonString, jsonParameterString);
+    }
+
+    /**
+     * @param parsedJsonBuilder
+     * @param jsonParamBuilder
+     * @param object
+     */
+    private static void extractFromDbObject(StringBuilder parsedJsonBuilder, StringBuilder jsonParamBuilder,
+            DBObject object) {
+        for (String key : object.keySet()) {
+            if (key.equals("_id")) {
+                continue;
+            }
+            parsedJsonBuilder.append("\"");
+            jsonParamBuilder.append("\"");
+            parsedJsonBuilder.append(key);
+            parsedJsonBuilder.append("\"");
+            parsedJsonBuilder.append(":");
+            parsedJsonBuilder.append("\"");
+            parsedJsonBuilder.append("?");
+            jsonParamBuilder.append(object.get(key));
+            parsedJsonBuilder.append("\"");
+            jsonParamBuilder.append("\"");
+            parsedJsonBuilder.append(",");
+            jsonParamBuilder.append(",");
+        }
+    }
+
+    /**
+     * @param parsedJsonBuilder
+     * @param jsonParamBuilder
+     */
+    private static void adjustJson(StringBuilder parsedJsonBuilder, StringBuilder jsonParamBuilder) {
+        parsedJsonBuilder.deleteCharAt(parsedJsonBuilder.length() - 1); // Remove the trailing comma
+        jsonParamBuilder.deleteCharAt(jsonParamBuilder.length() - 1); // Remove the trailing comma
+        parsedJsonBuilder.append("}");
     }
 }
 
