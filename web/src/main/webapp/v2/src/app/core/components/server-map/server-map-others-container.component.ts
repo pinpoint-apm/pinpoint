4cac59a8cb73f2359d2d60304764b5cb28600e49
hyojong-song <hyojong.song2015@navercorp.com>
2019-05-13 09:55:05 +0900

    [#noissue] refactoring server-map code. - remove duplicate code

--- web/src/main/webapp/v2/src/app/core/components/server-map/server-map-others-container.component.ts
+++ web/src/main/webapp/v2/src/app/core/components/server-map/server-map-others-container.component.ts
@@ -1,25 +1,27 @@
-import { Component, OnInit, Inject, ChangeDetectionStrategy, ChangeDetectorRef, Injector, ComponentFactoryResolver } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, Inject, ChangeDetectionStrategy, ChangeDetectorRef, Injector, ComponentFactoryResolver } from '@angular/core';
 import { Subject } from 'rxjs';
-import { map, takeUntil, skip } from 'rxjs/operators';
+import { takeUntil, filter, skip } from 'rxjs/operators';
 
-import { WebAppSettingDataService, GutterEventService, StoreHelperService, DynamicPopupService } from 'app/shared/services';
+import { WebAppSettingDataService, GutterEventService, DynamicPopupService } from 'app/shared/services';
 import { ServerMapInteractionService } from './server-map-interaction.service';
 import { ServerMapData } from './class/server-map-data.class';
 import { SERVER_MAP_TYPE, ServerMapType } from './class/server-map-factory';
 import { ServerMapContextPopupContainerComponent } from 'app/core/components/server-map-context-popup/server-map-context-popup-container.component';
+import { ServerMapChangeNotificationService, IServerMapNotificationData } from './server-map-change-notification.service';
 
 @Component({
-    selector: 'pp-server-map-for-transaction-view-container',
-    templateUrl: './server-map-for-transaction-view-container.component.html',
-    styleUrls: ['./server-map-for-transaction-view-container.component.css'],
+    selector: 'pp-server-map-others-container',
+    templateUrl: './server-map-others-container.component.html',
+    styleUrls: ['./server-map-others-container.component.css'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ServerMapForTransactionViewContainerComponent implements OnInit {
-    private unsubscribe: Subject<void> = new Subject<void>();
+export class ServerMapOthersContainerComponent implements OnInit, OnDestroy {
+    @Input() sourceType: string;
+    private unsubscribe: Subject<null> = new Subject();
     baseApplicationKey: string;
     mapData: ServerMapData;
-    funcServerMapImagePath: Function;
     showLoading = true;
+    funcServerMapImagePath: Function;
     constructor(
         private changeDetectorRef: ChangeDetectorRef,
         private injector: Injector,
@@ -28,30 +30,29 @@ export class ServerMapForTransactionViewContainerComponent implements OnInit {
         private dynamicPopupService: DynamicPopupService,
         private gutterEventService: GutterEventService,
         private serverMapInteractionService: ServerMapInteractionService,
-        private storeHelperService: StoreHelperService,
+        private serverMapChangeNotificationService: ServerMapChangeNotificationService,
         @Inject(SERVER_MAP_TYPE) public type: ServerMapType
-    ) { }
-
+    ) {}
     ngOnInit() {
         this.funcServerMapImagePath = this.webAppSettingDataService.getServerMapIconPathMakeFunc();
-        // TODO: ServiceType Empty이슈 체크 #174
-        this.storeHelperService.getTransactionDetailData(this.unsubscribe).pipe(
-            skip(1),
-            map(({ applicationId, applicationMapData: { nodeDataArray, linkDataArray } }: ITransactionDetailData) => {
-                this.baseApplicationKey = (nodeDataArray as INodeInfo[]).find(({ applicationName }: INodeInfo) => {
-                    return applicationId === applicationName;
-                }).key;
-
-                return new ServerMapData(nodeDataArray, linkDataArray);
-            })
-        ).subscribe((mapData: ServerMapData) => {
-            console.log( 'mapData : ', mapData );
-            this.mapData = mapData;
-            this.changeDetectorRef.detectChanges();
-        });
         this.gutterEventService.onGutterResized$.pipe(
+            skip(1),
             takeUntil(this.unsubscribe)
         ).subscribe(() => this.serverMapInteractionService.setRefresh());
+        this.serverMapChangeNotificationService.getObservable(this.sourceType).pipe(
+            takeUntil(this.unsubscribe),
+            filter((_: IServerMapNotificationData) => {
+                return _ !== null;
+            })
+        ).subscribe((data: IServerMapNotificationData) => {
+            this.baseApplicationKey = data.baseApplication;
+            this.mapData = data.serverMapData;
+            this.changeDetectorRef.detectChanges();
+        });
+    }
+    ngOnDestroy() {
+        this.unsubscribe.next();
+        this.unsubscribe.complete();
     }
     onRenderCompleted(msg: string): void {
         this.showLoading = false;
