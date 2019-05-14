345961b8c5e4478568a40702742b24f74a668af5
koo-taejin <koo.taejin@navercorp.com>
2019-05-14 16:12:31 +0900

    [#feature] add lightDumpService command
    add lightDumpService command

--- profiler/src/main/java/com/navercorp/pinpoint/profiler/context/thrift/CommandGrpcToThriftMessageConverter.java
+++ profiler/src/main/java/com/navercorp/pinpoint/profiler/context/thrift/CommandGrpcToThriftMessageConverter.java
@@ -16,10 +16,18 @@
 
 package com.navercorp.pinpoint.profiler.context.thrift;
 
+import com.navercorp.pinpoint.grpc.trace.PActiveThreadLightDump;
 import com.navercorp.pinpoint.grpc.trace.PCmdActiveThreadCountRes;
+import com.navercorp.pinpoint.grpc.trace.PCmdActiveThreadLightDumpRes;
 import com.navercorp.pinpoint.grpc.trace.PCmdEchoResponse;
+import com.navercorp.pinpoint.grpc.trace.PThreadLightDump;
+import com.navercorp.pinpoint.thrift.dto.command.TActiveThreadLightDump;
 import com.navercorp.pinpoint.thrift.dto.command.TCmdActiveThreadCountRes;
+import com.navercorp.pinpoint.thrift.dto.command.TCmdActiveThreadLightDumpRes;
 import com.navercorp.pinpoint.thrift.dto.command.TCommandEcho;
+import com.navercorp.pinpoint.thrift.dto.command.TThreadLightDump;
+import com.navercorp.pinpoint.thrift.dto.command.TThreadState;
+
 import org.apache.thrift.TBase;
 
 import java.util.List;
@@ -35,6 +43,8 @@ public class CommandGrpcToThriftMessageConverter implements MessageConverter<TBa
             return buildTCommandEcho((PCmdEchoResponse) message);
         } else if (message instanceof PCmdActiveThreadCountRes) {
             return buildTCmdActiveThreadCountRes((PCmdActiveThreadCountRes) message);
+        } else if (message instanceof PCmdActiveThreadLightDumpRes) {
+            return buildTCmdActiveThreadLightDumpRes((PCmdActiveThreadLightDumpRes) message);
         }
         return null;
     }
@@ -53,6 +63,42 @@ public class CommandGrpcToThriftMessageConverter implements MessageConverter<TBa
         result.setTimeStamp(timeStamp);
 
         return result;
+    }
+
+    private TCmdActiveThreadLightDumpRes buildTCmdActiveThreadLightDumpRes(PCmdActiveThreadLightDumpRes pCmdActiveThreadLightDumpRes) {
+        TCmdActiveThreadLightDumpRes tCmdActiveThreadLightDumpRes = new TCmdActiveThreadLightDumpRes();
+        tCmdActiveThreadLightDumpRes.setVersion(pCmdActiveThreadLightDumpRes.getVersion());
+        tCmdActiveThreadLightDumpRes.setType(pCmdActiveThreadLightDumpRes.getType());
+        tCmdActiveThreadLightDumpRes.setSubType(pCmdActiveThreadLightDumpRes.getSubType());
+
+        for (PActiveThreadLightDump pActiveThreadLightDump : pCmdActiveThreadLightDumpRes.getThreadDumpList()) {
+            tCmdActiveThreadLightDumpRes.addToThreadDumps(buildTActiveThreadLightDump(pActiveThreadLightDump));
+        }
+
+        return tCmdActiveThreadLightDumpRes;
+    }
+
+    private TActiveThreadLightDump buildTActiveThreadLightDump(PActiveThreadLightDump pActiveThreadLightDump) {
+        TActiveThreadLightDump tActiveThreadLightDump = new TActiveThreadLightDump();
+        tActiveThreadLightDump.setStartTime(pActiveThreadLightDump.getStartTime());
+        tActiveThreadLightDump.setSampled(pActiveThreadLightDump.getSampled());
+
+        if (pActiveThreadLightDump.getSampled()) {
+            tActiveThreadLightDump.setLocalTraceId(pActiveThreadLightDump.getLocalTraceId());
+            tActiveThreadLightDump.setTransactionId(pActiveThreadLightDump.getTransactionId());
+            tActiveThreadLightDump.setEntryPoint(pActiveThreadLightDump.getEntryPoint());
+        }
+
+        tActiveThreadLightDump.setThreadDump(buildTThreadLightDump(pActiveThreadLightDump.getThreadDump()));
+        return tActiveThreadLightDump;
+    }
+
+    private TThreadLightDump buildTThreadLightDump(PThreadLightDump pThreadDump) {
+        TThreadLightDump tThreadLightDump = new TThreadLightDump();
+        tThreadLightDump.setThreadId(pThreadDump.getThreadId());
+        tThreadLightDump.setThreadName(pThreadDump.getThreadName());
+        tThreadLightDump.setThreadState(TThreadState.findByValue(pThreadDump.getThreadStateValue()));
+        return tThreadLightDump;
     }
 
 }
