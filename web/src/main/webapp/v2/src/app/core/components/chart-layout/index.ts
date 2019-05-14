5e790a3ea1833e21e9a6f4aabd067a1fad78b648
hyojong-song <hyojong.song2015@navercorp.com>
2019-05-14 15:14:45 +0900

    [#noissue] Change chart order of inspector by drag and drop.

---
+++ web/src/main/webapp/v2/src/app/core/components/chart-layout/index.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatCardModule, MatGridListModule } from '@angular/material';
+
+import { SharedModule } from 'app/shared';
+import { ChartLayoutContainerComponent } from './chart-layout-container.component';
+import { ChartLayoutComponent } from './chart-layout.component';
+
+@NgModule({
+    imports: [
+        SharedModule,
+        MatCardModule,
+        MatGridListModule,
+        DragDropModule
+    ],
+    exports: [
+        ChartLayoutContainerComponent
+    ],
+    declarations: [
+        ChartLayoutContainerComponent,
+        ChartLayoutComponent
+    ],
+    providers: [],
+})
+export class ChartLayoutModule { }
