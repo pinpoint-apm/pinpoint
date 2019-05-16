abbce1adfa4151b315de8f34d315c973a7b42a00
hyojong-song <hyojong.song2015@navercorp.com>
2019-05-16 15:45:41 +0900

    [#noissue] Add configuration page for inspector chart

---
+++ web/src/main/webapp/v2/src/app/core/components/configuration-inspector-chart-manager/configuration-inspector-chart-manager-container.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'pp-configuration-inspector-chart-manager-container',
+    templateUrl: './configuration-inspector-chart-manager-container.component.html',
+    styleUrls: ['./configuration-inspector-chart-manager-container.component.css']
+})
+export class ConfigurationInspectorChartManagerContainerComponent implements OnInit {
+    constructor() {}
+    ngOnInit() {}
+}
---
+++ web/src/main/webapp/v2/src/app/core/components/configuration-inspector-chart-manager/index.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+
+import { SharedModule } from 'app/shared';
+import { ChartLayoutModule } from 'app/core/components/chart-layout';
+import { InspectorChartListModule } from 'app/core/components/inspector-chart-list';
+import { ConfigurationInspectorChartManagerContainerComponent } from './configuration-inspector-chart-manager-container.component';
+
+@NgModule({
+    declarations: [
+        ConfigurationInspectorChartManagerContainerComponent
+    ],
+    imports: [
+        SharedModule,
+        ChartLayoutModule,
+        InspectorChartListModule
+    ],
+    exports: [
+        ConfigurationInspectorChartManagerContainerComponent
+    ],
+    providers: []
+})
+export class ConfigurationInspectorChartManagerModule { }
