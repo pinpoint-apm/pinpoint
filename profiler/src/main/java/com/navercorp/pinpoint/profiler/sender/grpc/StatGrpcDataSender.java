7a99fa9a02eafdd81cee49f60d612116e21813e1
jaehong-kim <jaehong.kim@navercorp.com>
2019-06-28 16:37:43 +0900

    [#4558] Add protocol buffers message

--- profiler/src/main/java/com/navercorp/pinpoint/profiler/sender/grpc/StatGrpcDataSender.java
+++ profiler/src/main/java/com/navercorp/pinpoint/profiler/sender/grpc/StatGrpcDataSender.java
@@ -23,11 +23,14 @@ import com.google.protobuf.Empty;
 
 import com.navercorp.pinpoint.grpc.trace.PAgentStat;
 import com.navercorp.pinpoint.grpc.trace.PAgentStatBatch;
+import com.navercorp.pinpoint.grpc.trace.PStatMessage;
 import com.navercorp.pinpoint.grpc.trace.StatGrpc;
 import com.navercorp.pinpoint.profiler.context.thrift.MessageConverter;
 
 import com.google.protobuf.GeneratedMessageV3;
 import io.grpc.stub.StreamObserver;
+
+import static com.navercorp.pinpoint.grpc.MessageFormatUtils.debugLog;
 
 /**
  * @author jaehong.kim
@@ -36,12 +39,8 @@ public class StatGrpcDataSender extends GrpcDataSender {
     private final StatGrpc.StatStub statStub;
     private final ExecutorAdaptor reconnectExecutor;
 
-    private volatile StreamObserver<PAgentStat> statStream;
+    private volatile StreamObserver<PStatMessage> statStream;
     private final Reconnector statStreamReconnector;
-
-    private volatile StreamObserver<PAgentStatBatch> statBatchStream;
-    private final Reconnector statBatchStreamReconnector;
-
 
     public StatGrpcDataSender(String host, int port, int senderExecutorQueueSize, MessageConverter<GeneratedMessageV3> messageConverter, ChannelFactoryOption channelFactoryOption) {
         super(host, port, senderExecutorQueueSize, messageConverter, channelFactoryOption);
@@ -59,48 +58,35 @@ public class StatGrpcDataSender extends GrpcDataSender {
             this.statStreamReconnector = new ReconnectAdaptor(reconnectExecutor, statStreamReconnectJob);
             this.statStream = newStatStream();
         }
-        {
-            final Runnable statBatchStreamReconnectJob = new Runnable() {
-                @Override
-                public void run() {
-                    statBatchStream = newStatBatchStream();
-                }
-            };
-            this.statBatchStreamReconnector = new ReconnectAdaptor(reconnectExecutor, statBatchStreamReconnectJob);
-            this.statBatchStream = newStatBatchStream();
-        }
     }
 
     private ExecutorAdaptor newReconnectExecutor() {
         return new ExecutorAdaptor(GrpcDataSender.reconnectScheduler);
     }
 
-    private StreamObserver<PAgentStat> newStatStream() {
+    private StreamObserver<PStatMessage> newStatStream() {
         final StreamId statId = StreamId.newStreamId("stat");
-        final ResponseStreamObserver<PAgentStat, Empty> responseObserver = new ResponseStreamObserver<PAgentStat, Empty>(statId, statStreamReconnector);
+        final ResponseStreamObserver<PStatMessage, Empty> responseObserver = new ResponseStreamObserver<PStatMessage, Empty>(statId, statStreamReconnector);
         return statStub.sendAgentStat(responseObserver);
-    }
-
-    private StreamObserver<PAgentStatBatch> newStatBatchStream() {
-        final StreamId statBatch = StreamId.newStreamId("statBatch");
-        final ResponseStreamObserver<PAgentStatBatch, Empty> responseObserver = new ResponseStreamObserver<PAgentStatBatch, Empty>(statBatch, statBatchStreamReconnector);
-        return statStub.sendAgentStatBatch(responseObserver);
     }
 
     public boolean send0(Object data) {
         final GeneratedMessageV3 message = messageConverter.toMessage(data);
         if (logger.isDebugEnabled()) {
-            logger.debug("Stat send0 data={}", data);
+            logger.debug("Send message={}", debugLog(message));
         }
 
         if (message instanceof PAgentStatBatch) {
             final PAgentStatBatch agentStatBatch = (PAgentStatBatch) message;
-            statBatchStream.onNext(agentStatBatch);
+            final PStatMessage statMessage = PStatMessage.newBuilder().setAgentStatBatch(agentStatBatch).build();
+            statStream.onNext(statMessage);
             return true;
         }
+
         if (message instanceof PAgentStat) {
             final PAgentStat agentStat = (PAgentStat) message;
-            statStream.onNext(agentStat);
+            final PStatMessage statMessage = PStatMessage.newBuilder().setAgentStat(agentStat).build();
+            statStream.onNext(statMessage);
             return true;
         }
         throw new IllegalStateException("unsupported message " + message);
@@ -111,8 +97,6 @@ public class StatGrpcDataSender extends GrpcDataSender {
         if (this.reconnectExecutor != null) {
             this.reconnectExecutor.close();
         }
-        logger.info("statBatchStream.close()");
-        StreamUtils.close(statBatchStream);
         logger.info("statStream.close()");
         StreamUtils.close(statStream);
         super.stop();
