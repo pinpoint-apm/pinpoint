63d014c33bb4c04f0e38c4fa8a8efee7c2e3d642
koo-taejin <koo.taejin@navercorp.com>
2019-07-26 16:09:32 +0900

    [#5807] Shows all the Consumers derived from the request

--- web/src/main/java/com/navercorp/pinpoint/web/calltree/span/LinkedCallTree.java
+++ web/src/main/java/com/navercorp/pinpoint/web/calltree/span/LinkedCallTree.java
@@ -16,6 +16,17 @@
 
 package com.navercorp.pinpoint.web.calltree.span;
 
+import com.navercorp.pinpoint.common.server.bo.AnnotationBo;
+import com.navercorp.pinpoint.common.server.bo.ApiMetaDataBo;
+import com.navercorp.pinpoint.common.server.bo.MethodTypeEnum;
+import com.navercorp.pinpoint.common.server.bo.SpanBo;
+import com.navercorp.pinpoint.common.trace.AnnotationKey;
+import com.navercorp.pinpoint.common.util.Assert;
+import com.navercorp.pinpoint.common.util.TransactionId;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author jaehong.kim
  */
@@ -33,12 +44,83 @@ public class LinkedCallTree implements CallTree {
         this.root.setAlign(updateNode.getAlign());
     }
 
+    public void updateForMultipleChild(final CallTree callTree) {
+        final CallTreeNode newCallTreeNode = changeNodeToVirtualNode(callTree);
+        final SpanBo spanBo = newCallTreeNode.getAlign().getSpanBo();
+
+        // already has child
+        if (root.hasChild()) {
+            CallTreeNode last = getLastChild(root);
+            last.setSibling(newCallTreeNode);
+
+            SpanBo rootSpanBo = root.getAlign().getSpanBo();
+            if (rootSpanBo.getStartTime() > spanBo.getStartTime()) {
+                rootSpanBo.setStartTime(spanBo.getStartTime());
+            }
+        } else {
+            SpanAlign spanAlign = createMultiChildSpanAlign(spanBo.getServiceType(), spanBo.getStartTime());
+
+            this.root.setAlign(spanAlign);
+            this.root.setChild(newCallTreeNode);
+        }
+    }
+
+    private CallTreeNode getLastChild(CallTreeNode rootNode) {
+        Assert.requireNonNull(rootNode, "rootNode must not be null");
+
+        CallTreeNode prevSibling = null;
+        CallTreeNode nextSibling = root.getChild();
+        while (nextSibling != null) {
+            prevSibling = nextSibling;
+            nextSibling = prevSibling.getSibling();
+        }
+
+        return prevSibling;
+    }
+
+    private SpanAlign createMultiChildSpanAlign(short serviceType, long startTime) {
+        SpanBo spanBo = new SpanBo();
+        spanBo.setTransactionId(new TransactionId("UNKNOWN", 0, 0));
+        spanBo.setServiceType(serviceType);
+        spanBo.setStartTime(startTime);
+
+        AnnotationBo multiChildAnnotation = createMultiChildAnnotation();
+
+        List<AnnotationBo> annotationBoList = new ArrayList<>();
+        annotationBoList.add(multiChildAnnotation);
+
+        spanBo.setAnnotationBoList(annotationBoList);
+
+        return new SpanAlign(spanBo, true);
+    }
+
+    private AnnotationBo createMultiChildAnnotation() {
+        ApiMetaDataBo apiMetaDataBo = new ApiMetaDataBo("UNKNOWN", 0, -1);
+        apiMetaDataBo.setApiInfo("Multi Child");
+        apiMetaDataBo.setLineNumber(-1);
+        apiMetaDataBo.setMethodTypeEnum(MethodTypeEnum.INVOCATION);
+
+        return new AnnotationBo(AnnotationKey.API_METADATA.getCode(), apiMetaDataBo);
+    }
+
+    private CallTreeNode changeNodeToVirtualNode(final CallTree originalCallTree) {
+        CallTreeNode originalNode = originalCallTree.getRoot();
+        CallTreeNode newCallTreeNode = new CallTreeNode(this.root, originalNode.getAlign());
+
+        CallTreeNode child = originalNode.getChild();
+        newCallTreeNode.setChild(child);
+
+        child.setParent(newCallTreeNode);
+
+        return newCallTreeNode;
+    }
+
     public void remove() {
         final CallTreeNode parent = this.root.getParent();
         CallTreeNode prev = null;
         CallTreeNode node = parent.getChild();
         while (node != null) {
-            if(node == this.root) {
+            if (node == this.root) {
                 CallTreeNode next = node.getSibling();
                 if (prev == null) {
                     // first sibling
