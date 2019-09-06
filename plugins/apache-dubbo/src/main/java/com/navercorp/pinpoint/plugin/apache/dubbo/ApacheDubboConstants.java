c2bb751c6174cd8950d1aa4edea572a06593e141
emeroad <wd.kang@navercorp.com>
2019-09-06 11:37:53 +0900

    [#noissue] Update copyright of apache-dubbo-plugin

--- plugins/apache-dubbo/src/main/java/com/navercorp/pinpoint/plugin/apache/dubbo/ApacheDubboConstants.java
+++ plugins/apache-dubbo/src/main/java/com/navercorp/pinpoint/plugin/apache/dubbo/ApacheDubboConstants.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 NAVER Corp.
+ * Copyright 2019 NAVER Corp.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,7 +26,6 @@ import static com.navercorp.pinpoint.common.trace.ServiceTypeProperty.RECORD_STA
 
 /**
  * @author K
- * @date 2019-06-14-14:00
  */
 public final class ApacheDubboConstants {
     private ApacheDubboConstants() {
--- plugins/apache-dubbo/src/main/java/com/navercorp/pinpoint/plugin/apache/dubbo/interceptor/ApacheDubboProviderInterceptor.java
+++ plugins/apache-dubbo/src/main/java/com/navercorp/pinpoint/plugin/apache/dubbo/interceptor/ApacheDubboProviderInterceptor.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.navercorp.pinpoint.plugin.apache.dubbo.interceptor;
 
 import com.navercorp.pinpoint.bootstrap.context.MethodDescriptor;
@@ -20,7 +36,6 @@ import org.apache.dubbo.rpc.RpcInvocation;
 
 /**
  * @author K
- * @date 2019-06-14-14:00
  */
 public class ApacheDubboProviderInterceptor extends SpanRecursiveAroundInterceptor {
     private final PLogger logger = PLoggerFactory.getLogger(this.getClass());
