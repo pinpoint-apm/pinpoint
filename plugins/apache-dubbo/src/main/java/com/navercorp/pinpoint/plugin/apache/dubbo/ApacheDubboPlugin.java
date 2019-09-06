1b5898ececb65a1d55b394409f554882687dd812
emeroad <wd.kang@navercorp.com>
2019-09-06 11:37:53 +0900

    [#noissue] Update copyright of apache-dubbo-plugin

--- plugins/apache-dubbo/src/main/java/com/navercorp/pinpoint/plugin/apache/dubbo/ApacheDubboPlugin.java
+++ plugins/apache-dubbo/src/main/java/com/navercorp/pinpoint/plugin/apache/dubbo/ApacheDubboPlugin.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.navercorp.pinpoint.plugin.apache.dubbo;
 
 import com.navercorp.pinpoint.bootstrap.instrument.InstrumentClass;
@@ -19,7 +35,6 @@ import java.security.ProtectionDomain;
 
 /**
  * @author K
- * @date 2019-06-14-14:00
  */
 public class ApacheDubboPlugin implements ProfilerPlugin, TransformTemplateAware {
 
