6e5a72d0b652818dd0597e672218a178a1835ee0
DaYea Yim <dyyim@protonmail.com>
2019-10-04 12:25:02 +0000

    [#noissue] fix useless use of cat

--- thrift/install-thrift-linux.sh
+++ thrift/install-thrift-linux.sh
@@ -74,8 +74,8 @@ fi
 echo "INFO: Check Thrift installation environment"
 if [ -f "/etc/os-release" ]; then
     # Get Linux distribution name and version.
-    DIST_ID=`cat /etc/os-release | grep "^ID=" | awk -F "=" '{print $2;}' | tr -d '"'`
-    DIST_VERSION=`cat /etc/os-release | grep "^VERSION_ID=" | awk -F "=" '{print $2;}' | tr -d '"' | awk -F "." '{print $1;}'`
+    DIST_ID=`grep "^ID=" /etc/os-release | awk -F "=" '{print $2;}' | tr -d '"'`
+    DIST_VERSION=`grep "^VERSION_ID=" /etc/os-release | awk -F "=" '{print $2;}' | tr -d '"' | awk -F "." '{print $1;}'`
 
     echo "Linux distribution: ${DIST_ID}"
     echo "Version: ${DIST_VERSION}"
