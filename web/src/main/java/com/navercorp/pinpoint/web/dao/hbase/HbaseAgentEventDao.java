50d4cf9d816b19a17670ce9f7c942293f8e5a987
emeroad <wd.kang@navercorp.com>
2019-10-15 18:21:40 +0900

    [#5669] Fix wrong TableDescriptor

--- web/src/main/java/com/navercorp/pinpoint/web/dao/hbase/HbaseAgentEventDao.java
+++ web/src/main/java/com/navercorp/pinpoint/web/dao/hbase/HbaseAgentEventDao.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2015 NAVER Corp.
+ * Copyright 2019 NAVER Corp.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -68,7 +68,7 @@ public class HbaseAgentEventDao implements AgentEventDao {
     private ResultsExtractor<List<AgentEventBo>> agentEventResultsExtractor;
 
     @Autowired
-    private TableDescriptor<HbaseColumnFamily.Trace> descriptor;
+    private TableDescriptor<HbaseColumnFamily.AgentEvent> descriptor;
 
     @Override
     public List<AgentEventBo> getAgentEvents(String agentId, Range range, Set<AgentEventType> excludeEventTypes) {
