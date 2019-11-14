2eba96aa75de84c8d95b12d4fc3bf2c39429c5a2
QooQooDass <bindong.kim@navercorp.com>
2019-11-14 11:44:34 +0900

    [#noissue] Fix contextmenu event on servermap
    - Bind timeout instead of the adjust value on Y coordinate because there could be a misposition

--- web/src/main/webapp/v2/src/app/core/components/server-map/class/server-map-diagram-with-gojs.class.ts
+++ web/src/main/webapp/v2/src/app/core/components/server-map/class/server-map-diagram-with-gojs.class.ts
@@ -80,9 +80,12 @@ export class ServerMapDiagramWithGojs extends ServerMapDiagram {
         this.diagram.addDiagramListener('BackgroundContextClicked', (event: go.DiagramEvent) => {
             const {pageX, pageY} = event.diagram.lastInput.event as MouseEvent;
 
-            self.outContextClickBackground.emit({
-                coordX: pageX,
-                coordY: pageY + 1 // os * browser마다 contextmenu 이벤트의 trigger 순서가 달라서 1px의 보정을 줌.
+            // Bind timeout because of the browser contextmenu event occurrence order difference
+            setTimeout(() => {
+                self.outContextClickBackground.emit({
+                    coordX: pageX,
+                    coordY: pageY
+                });
             });
         });
     }
@@ -246,12 +249,15 @@ export class ServerMapDiagramWithGojs extends ServerMapDiagram {
         const {pageX, pageY} = event.event;
 
         if (!Array.isArray(targetInfo)) {
-            this.outContextClickLink.emit({
-                key,
-                coord: {
-                    coordX: pageX,
-                    coordY: pageY + 1
-                }
+            // Bind timeout because of the browser contextmenu event occurrence order difference
+            setTimeout(() => {
+                this.outContextClickLink.emit({
+                    key,
+                    coord: {
+                        coordX: pageX,
+                        coordY: pageY
+                    }
+                });
             });
         }
     }
