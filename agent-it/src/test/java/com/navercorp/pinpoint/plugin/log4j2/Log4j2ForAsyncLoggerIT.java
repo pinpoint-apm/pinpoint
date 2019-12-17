8ea2ee9740dbfdd95e19cad6107a2645629006b6
jaehong-kim <jaehong.kim@navercorp.com>
2019-12-17 17:58:56 +0900

    [#noissue] Patch log4j2 testcase

--- agent-it/src/test/java/com/navercorp/pinpoint/plugin/log4j2/Log4j2ForAsyncLoggerIT.java
+++ agent-it/src/test/java/com/navercorp/pinpoint/plugin/log4j2/Log4j2ForAsyncLoggerIT.java
@@ -29,7 +29,7 @@ import org.junit.runner.RunWith;
 @PinpointConfig("pinpoint-spring-bean-test.config")
 @JvmArgument("-Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector")
 @JvmVersion(7)
-@Dependency({"org.apache.logging.log4j:log4j-core:[2.0,)", "com.lmax:disruptor:[3.4.2]"})
+@Dependency({"org.apache.logging.log4j:log4j-core:[2.0,2.13)", "com.lmax:disruptor:[3.4.2]"})
 public class Log4j2ForAsyncLoggerIT {
 
     @Test
