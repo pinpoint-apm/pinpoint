c6b76abc8dcf593c967af81175c6cbb65ac5b177
emeroad <wd.kang@navercorp.com>
2019-12-26 17:58:30 +0900

    [#6363] Apply RepositoryCache

---
+++ test/src/main/java/com/navercorp/pinpoint/test/plugin/DependencyResolverFactory.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2014 NAVER Corp.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.navercorp.pinpoint.test.plugin;
+
+import org.eclipse.aether.RepositorySystem;
+import org.eclipse.aether.RepositorySystemSession;
+import org.eclipse.aether.repository.RemoteRepository;
+
+import java.util.List;
+
+/**
+ * @author emeroad
+ */
+public class DependencyResolverFactory {
+
+    private final RepositorySystem system;
+    private final RepositorySystemSession session;
+
+    public DependencyResolverFactory() {
+        this(true);
+    }
+
+    public DependencyResolverFactory(boolean supportRemote) {
+        this.system = DependencyResolver.newRepositorySystem(supportRemote);
+        this.session = DependencyResolver.newRepositorySystemSession(this.system);
+    }
+
+    public DependencyResolver get(String... repositoryUrls) {
+        List<RemoteRepository> remoteRepositories = DependencyResolver.newRepositories(repositoryUrls);
+        return new DependencyResolver(system, session, remoteRepositories);
+    }
+}
