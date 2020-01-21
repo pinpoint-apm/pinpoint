74154464810875d43d49714b598a771a76df723b
QooQooDass <bindong.kim@navercorp.com>
2020-01-21 20:55:00 +0900

    [#noissue] Refactor Pinpoint-User Component

--- web/src/main/angular/src/app/core/components/pinpoint-user/pinpoint-user-container.component.ts
+++ web/src/main/angular/src/app/core/components/pinpoint-user/pinpoint-user-container.component.ts
@@ -1,11 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subject, iif, of, forkJoin } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { Subject, forkJoin } from 'rxjs';
 import { TranslateService } from '@ngx-translate/core';
 
 import { TranslateReplaceService, WebAppSettingDataService, MessageQueueService, MESSAGE_TO, AnalyticsService, TRACKED_EVENT_LIST } from 'app/shared/services';
-import { PinpointUser } from './pinpoint-user-create-and-update.component';
-import { PinpointUserDataService, IPinpointUser, IPinpointUserResponse } from './pinpoint-user-data.service';
+import { PinpointUserDataService, IPinpointUserResponse } from './pinpoint-user-data.service';
 import { isThatType } from 'app/core/utils/util';
 
 @Component({
@@ -14,8 +12,9 @@ import { isThatType } from 'app/core/utils/util';
     styleUrls: ['./pinpoint-user-container.component.css']
 })
 export class PinpointUserContainerComponent implements OnInit, OnDestroy {
-    private unsubscribe: Subject<null> = new Subject();
+    private unsubscribe = new Subject<void>();
     private searchQuery = '';
+
     i18nLabel = {
         USER_ID_LABEL: '',
         NAME_LABEL: '',
@@ -34,10 +33,9 @@ export class PinpointUserContainerComponent implements OnInit, OnDestroy {
     };
     allowedUserEdit = false;
     searchUseEnter = true;
-    pinpointUserList: IPinpointUser[] = [];
+    pinpointUserList: IUserProfile[] = [];
     groupMemberList: string[] = [];
-    editPinpointUserIndex: number;
-    editPinpointUser: PinpointUser;
+    editPinpointUser: IUserProfile;
     isUserGroupSelected = false;
     useDisable = true;
     showLoading = true;
@@ -52,6 +50,7 @@ export class PinpointUserContainerComponent implements OnInit, OnDestroy {
         private messageQueueService: MessageQueueService,
         private analyticsService: AnalyticsService,
     ) {}
+
     ngOnInit() {
         this.webAppSettingDataService.useUserEdit().subscribe((allowedUserEdit: boolean) => {
             this.allowedUserEdit = allowedUserEdit;
@@ -66,10 +65,12 @@ export class PinpointUserContainerComponent implements OnInit, OnDestroy {
         this.getI18NText();
         this.getPinpointUserList();
     }
+
     ngOnDestroy() {
         this.unsubscribe.next();
         this.unsubscribe.complete();
     }
+
     private getI18NText(): void {
         forkJoin(
             this.translateService.get('COMMON.MIN_LENGTH'),
@@ -118,14 +119,10 @@ export class PinpointUserContainerComponent implements OnInit, OnDestroy {
             this.i18nLabel.EMAIL_LABEL = emailLabel;
         });
     }
+
     private getPinpointUserList(query?: string): void  {
         this.showProcessing();
-        iif(() => !!query,
-            of(query),
-            this.webAppSettingDataService.getUserDepartment()
-        ).pipe(
-            switchMap((department?: string) => this.pinpointUserDataService.retrieve(department))
-        ).subscribe((result: IPinpointUser[] | IServerErrorShortFormat) => {
+        this.pinpointUserDataService.retrieve(query).subscribe((result: IUserProfile[] | IServerErrorShortFormat) => {
             isThatType<IServerErrorShortFormat>(result, 'errorCode', 'errorMessage')
                 ? this.errorMessage = result.errorMessage
                 : this.pinpointUserList = result;
@@ -135,12 +132,15 @@ export class PinpointUserContainerComponent implements OnInit, OnDestroy {
             this.hideProcessing();
         });
     }
+
     isEnable(): boolean {
         return false;
     }
+
     isChecked(userId: string): boolean {
         return this.groupMemberList.indexOf(userId) !== -1;
     }
+
     onAddUser(pinpointUserId: string): void {
         this.messageQueueService.sendMessage({
             to: MESSAGE_TO.PINPOINT_USER_ADD_USER,
@@ -148,33 +148,34 @@ export class PinpointUserContainerComponent implements OnInit, OnDestroy {
         });
         this.analyticsService.trackEvent(TRACKED_EVENT_LIST.ADD_USER_TO_GROUP);
     }
+
     onCloseErrorMessage(): void {
         this.errorMessage = '';
     }
+
     onSearch(query: string): void {
         this.searchQuery = query;
         this.getPinpointUserList(this.searchQuery);
         this.analyticsService.trackEvent(TRACKED_EVENT_LIST.SEARCH_USER);
     }
+
     onReload(): void {
         this.getPinpointUserList(this.searchQuery);
         this.analyticsService.trackEvent(TRACKED_EVENT_LIST.RELOAD_USER_LIST);
     }
+
     onCloseCreateUserPopup(): void {
         this.showCreate = false;
     }
+
     onShowCreateUserPopup(): void {
         this.showCreate = true;
         this.analyticsService.trackEvent(TRACKED_EVENT_LIST.SHOW_USER_CREATION_POPUP);
     }
-    onCreatePinpointUser(pinpointUser: PinpointUser): void {
-        this.pinpointUserDataService.create({
-            userId: pinpointUser.userId,
-            name: pinpointUser.name,
-            phoneNumber: pinpointUser.phoneNumber,
-            email: pinpointUser.email,
-            department: pinpointUser.department
-        } as IPinpointUser).subscribe((response: IPinpointUserResponse | IServerErrorShortFormat) => {
+
+    // TODO: Refactor - Avoid nested subscribe syntax.
+    onCreatePinpointUser(pinpointUser: IUserProfile): void {
+        this.pinpointUserDataService.create(pinpointUser).subscribe((response: IPinpointUserResponse | IServerErrorShortFormat) => {
             isThatType<IServerErrorShortFormat>(response, 'errorCode', 'errorMessage')
                 ? this.errorMessage = response.errorMessage
                 : (
@@ -187,17 +188,10 @@ export class PinpointUserContainerComponent implements OnInit, OnDestroy {
             this.errorMessage = error;
         });
     }
-    onUpdatePinpointUser(pinpointUser: PinpointUser): void {
+
+    onUpdatePinpointUser(userInfo: IUserProfile): void {
         this.showProcessing();
-        const editPinpointUser = this.pinpointUserList[this.editPinpointUserIndex];
-        this.pinpointUserDataService.update({
-            userId: pinpointUser.userId,
-            name: pinpointUser.name,
-            phoneNumber: pinpointUser.phoneNumber,
-            email: pinpointUser.email,
-            department: pinpointUser.department,
-            number: editPinpointUser.number
-        } as IPinpointUser).subscribe((response: IPinpointUserResponse | IServerErrorShortFormat) => {
+        this.pinpointUserDataService.update(userInfo).subscribe((response: IPinpointUserResponse | IServerErrorShortFormat) => {
             if (isThatType<IServerErrorShortFormat>(response, 'errorCode', 'errorMessage')) {
                 this.errorMessage = response.errorMessage;
             } else {
@@ -206,9 +200,9 @@ export class PinpointUserContainerComponent implements OnInit, OnDestroy {
                     this.messageQueueService.sendMessage({
                         to: MESSAGE_TO.PINPOINT_USER_UPDATE_USER,
                         param: {
-                            userId: pinpointUser.userId,
-                            department: pinpointUser.department,
-                            name: pinpointUser.name
+                            userId: userInfo.userId,
+                            department: userInfo.department,
+                            name: userInfo.name
                         }
                     });
                 }
@@ -221,13 +215,14 @@ export class PinpointUserContainerComponent implements OnInit, OnDestroy {
             this.errorMessage = error.exception.message;
         });
     }
+
     onRemovePinpointUser(userId: string): void {
         this.showProcessing();
         this.pinpointUserDataService.remove(userId).subscribe((response: IPinpointUserResponse | IServerErrorShortFormat) => {
             if (isThatType<IServerErrorShortFormat>(response, 'errorCode', 'errorMessage')) {
                 this.errorMessage = response.errorMessage;
             } else {
-                this.pinpointUserList.splice(this.getPinpointUserIndexByUserId(userId), 1);
+                this.pinpointUserList = this.pinpointUserList.filter(({userId: id}: IUserProfile) => id !== userId);
                 if (this.isUserGroupSelected) {
                     this.messageQueueService.sendMessage({
                         to: MESSAGE_TO.PINPOINT_USER_REMOVE_USER,
@@ -242,33 +237,22 @@ export class PinpointUserContainerComponent implements OnInit, OnDestroy {
             this.errorMessage = error.exception.message;
         });
     }
+
     onEditPinpointUser(userId: string): void {
-        this.editPinpointUserIndex = this.getPinpointUserIndexByUserId(userId);
-        const editPinpointUser = this.pinpointUserList[this.editPinpointUserIndex];
-        this.editPinpointUser = new PinpointUser(
-            editPinpointUser.userId,
-            editPinpointUser.name,
-            editPinpointUser.phoneNumber,
-            editPinpointUser.email,
-            editPinpointUser.department
-        );
+        this.editPinpointUser = this.filterUserById(userId);
         this.onShowCreateUserPopup();
         this.analyticsService.trackEvent(TRACKED_EVENT_LIST.SHOW_USER_UPDATE_POPUP);
     }
-    private getPinpointUserIndexByUserId(userId: string): number {
-        let index = -1;
-        for (let i = 0 ; i < this.pinpointUserList.length ; i++) {
-            if (this.pinpointUserList[i].userId === userId) {
-                index = i;
-                break;
-            }
-        }
-        return index;
+
+    private filterUserById(userId: string): IUserProfile {
+        return this.pinpointUserList.find(({userId: id}: IUserProfile) => id === userId);
     }
+
     private showProcessing(): void {
         this.useDisable = true;
         this.showLoading = true;
     }
+
     private hideProcessing(): void {
         this.useDisable = false;
         this.showLoading = false;
