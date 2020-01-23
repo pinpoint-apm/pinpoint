d101872fa836079db84b105e8fd60f1850c6fbd3
QooQooDass <bindong.kim@navercorp.com>
2020-01-23 16:41:40 +0900

    [#noissue] Change UI Form theme

--- web/src/main/angular/src/app/core/components/user-group/user-group-create-and-update.component.ts
+++ web/src/main/angular/src/app/core/components/user-group/user-group-create-and-update.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewChecked, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { CustomFormValidatorService } from 'app/shared/services/custom-form-validator.service';
@@ -8,35 +8,30 @@ import { CustomFormValidatorService } from 'app/shared/services/custom-form-vali
     templateUrl: './user-group-create-and-update.component.html',
     styleUrls: ['./user-group-create-and-update.component.css']
 })
-export class UserGroupCreateAndUpdateComponent implements OnInit, AfterViewChecked {
+export class UserGroupCreateAndUpdateComponent implements OnInit {
     @Input() showCreate = false;
     @Input() i18nLabel: { [key: string]: string };
     @Input() i18nGuide: { [key: string]: IFormFieldErrorType };
     @Input() minLength: number;
-    @Output() outCreateUserGroup: EventEmitter<string> = new EventEmitter();
-    @Output() outClose: EventEmitter<null> = new EventEmitter();
-    newUserGroupModel = '';
-    userGroupForm: FormGroup;
-    title = 'User Group';
+    @Output() outCreateUserGroup = new EventEmitter<string>();
+    @Output() outClose = new EventEmitter<void>();
+
+    userGroupForm = new FormGroup({
+        userGroupName: new FormControl('', [
+            Validators.required,
+            CustomFormValidatorService.validate(/^[\w\-]{4,30}$/)
+        ])
+    });
+
     constructor() {}
-    ngOnInit() {
-        this.userGroupForm = new FormGroup({
-            'userGroupName': new FormControl(this.newUserGroupModel, [
-                Validators.required,
-                CustomFormValidatorService.validate(/^[\w\-]{4,30}$/)
-            ])
-        });
-    }
-    ngAfterViewChecked() {}
+    ngOnInit() {}
     onCreateOrUpdate(): void {
         this.outCreateUserGroup.emit(this.userGroupForm.get('userGroupName').value);
         this.onClose();
     }
+
     onClose(): void {
         this.outClose.emit();
         this.userGroupForm.reset();
-    }
-    get userGroupName() {
-        return this.userGroupForm.get('userGroupName');
     }
 }
