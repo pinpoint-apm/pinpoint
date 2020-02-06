091cd9149a113ece0b15b868befdc8e78bf5d1f4
Koo Taejin <koo.taejin@navercorp.com>
2020-02-06 14:09:09 +0900

    [release] prepare for 2.0.0-RC3

--- commons/src/main/java/com/navercorp/pinpoint/common/Version.java
+++ commons/src/main/java/com/navercorp/pinpoint/common/Version.java
@@ -1,4 +1,4 @@
 package com.navercorp.pinpoint.common;
 public final class Version {
-    public static final String VERSION = "2.0.0-SNAPSHOT";
+    public static final String VERSION = "2.0.0-RC3";
 }
