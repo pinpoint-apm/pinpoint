9284f64fd44ed5ec841613334ddc3c587bf2781b
minwoo.jung <minwoo.jung@navercorp.com>
2020-03-03 22:37:55 +0900

    [#6558] refactoring hbase Security logic

--- commons-hbase/src/main/java/com/navercorp/pinpoint/common/hbase/ConnectionFactoryBean.java
+++ commons-hbase/src/main/java/com/navercorp/pinpoint/common/hbase/ConnectionFactoryBean.java
@@ -27,6 +27,7 @@ import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.io.IOException;
 import java.util.Objects;
@@ -42,46 +43,36 @@ public class ConnectionFactoryBean implements FactoryBean<Connection>, Initializ
     @Autowired(required = false)
     private TableNameProvider tableNameProvider;
 
+    @Qualifier("hbaseSecurityInterceptor")
+    @Autowired(required = false)
+    private HbaseSecurityInterceptor hbaseSecurityInterceptor = new EmptyHbaseSecurityInterceptor();
+
     private final boolean warmUp;
-    private final Connection connection;
+    private final Configuration configuration;
+    private Connection connection;
 
     public ConnectionFactoryBean(Configuration configuration) {
         Objects.requireNonNull(configuration, "configuration");
-        try {
-            warmUp = configuration.getBoolean("hbase.client.warmup.enable", false);
-            connection = ConnectionFactory.createConnection(configuration);
-        } catch (IOException e) {
-            throw new HbaseSystemException(e);
-        }
+        this.configuration = configuration;
+        warmUp = configuration.getBoolean("hbase.client.warmup.enable", false);
     }
 
     public ConnectionFactoryBean(Configuration configuration, ExecutorService executorService) {
         Objects.requireNonNull(configuration, "configuration");
         Objects.requireNonNull(executorService, "executorService");
-
-        try {
-            warmUp = configuration.getBoolean("hbase.client.warmup.enable", false);
-            connection = ConnectionFactory.createConnection(configuration, executorService);
-        } catch (IOException e) {
-            throw new HbaseSystemException(e);
-        }
-    }
-
-    public ConnectionFactoryBean(Configuration configuration, ExecutorService executorService, HbaseSecurityInterceptor hbaseSecurityInterceptor) {
-        Objects.requireNonNull(configuration, "configuration");
-        Objects.requireNonNull(executorService, "executorService");
-
-        try {
-            warmUp = configuration.getBoolean("hbase.client.warmup.enable", false);
-            hbaseSecurityInterceptor.process(configuration);
-            connection = ConnectionFactory.createConnection(configuration, executorService);
-        } catch (IOException e) {
-            throw new HbaseSystemException(e);
-        }
+        this.configuration = configuration;
+        warmUp = configuration.getBoolean("hbase.client.warmup.enable", false);
     }
 
     @Override
     public void afterPropertiesSet() throws Exception {
+        hbaseSecurityInterceptor.process(configuration);
+        try {
+            connection = ConnectionFactory.createConnection(this.configuration);
+        } catch (IOException e) {
+            throw new HbaseSystemException(e);
+        }
+
         if (warmUp) {
             if (tableNameProvider != null && tableNameProvider.hasDefaultNameSpace()) {
                 logger.info("warmup for hbase connection started");
