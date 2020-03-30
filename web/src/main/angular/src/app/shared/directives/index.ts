1f66961f2eadc9eb4739d85e97bd52ab74b9dcc7
QooQooDass <bindong.kim@navercorp.com>
2020-03-30 12:55:06 +0900

    [#noissue] Clean up unused code

--- web/src/main/angular/src/app/shared/directives/index.ts
+++ web/src/main/angular/src/app/shared/directives/index.ts
@@ -1,7 +1,5 @@
 import { ContextPopupDirective } from './context-popup.directive';
-import { SplitterDirective } from './splitter.directive';
 
 export const DIRECTIVES = [
     ContextPopupDirective,
-    SplitterDirective
 ];
