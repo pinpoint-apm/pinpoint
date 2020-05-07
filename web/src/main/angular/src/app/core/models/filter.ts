ce45f99e2ab549d0cb8eae9785c6547b2f1b8430
King Jin <jinking.this@gmail.com>
2020-05-07 16:12:57 +0800

    fix Filter Transaction Wizard transaction result filter

--- web/src/main/angular/src/app/core/models/filter.ts
+++ web/src/main/angular/src/app/core/models/filter.ts
@@ -118,9 +118,9 @@ export class Filter {
 
     getTransactionResultStr(): string {
         if (this.transactionResult === true) {
-            return 'Success Only';
-        } else if (this.transactionResult === false) {
             return 'Failed Only';
+        } else if (this.transactionResult === false) {
+            return 'Success Only';
         }
         return 'Success + Failed';
     }
