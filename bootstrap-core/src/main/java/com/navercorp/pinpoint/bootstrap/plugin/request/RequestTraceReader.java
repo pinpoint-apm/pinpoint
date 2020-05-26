b84eefb7bea0cf0797d1ebabb82bf5c14d823dfa
King Jin <jinking.this@gmail.com>
2020-05-26 16:26:35 +0800

    add profile config for force-sampling header, default to off

--- bootstrap-core/src/main/java/com/navercorp/pinpoint/bootstrap/plugin/request/RequestTraceReader.java
+++ bootstrap-core/src/main/java/com/navercorp/pinpoint/bootstrap/plugin/request/RequestTraceReader.java
@@ -37,6 +37,7 @@ public class RequestTraceReader<T> {
     private final TraceContext traceContext;
     private final RequestAdaptor<T> requestAdaptor;
     private final boolean async;
+    private final boolean forceSamplingHeaderEnabled;
 
     private final TraceHeaderReader<T> traceHeaderReader;
     private final NameSpaceChecker<T> nameSpaceChecker;
@@ -52,6 +53,7 @@ public class RequestTraceReader<T> {
         this.async = async;
         String applicationNamespace = traceContext.getProfilerConfig().getApplicationNamespace();
         this.nameSpaceChecker = NameSpaceCheckFactory.newNamespace(requestAdaptor, applicationNamespace);
+        this.forceSamplingHeaderEnabled = traceContext.getProfilerConfig().readBoolean("profiler.sampling.force.header", false);
     }
 
     // Read the transaction information from the request.
@@ -85,7 +87,7 @@ public class RequestTraceReader<T> {
     }
 
     private Trace newTrace(T request) {
-        if (requestAdaptor.getHeader(request, Header.HTTP_FORCE_SAMPLE.toString()) != null) {
+        if (forceSamplingHeaderEnabled && requestAdaptor.getHeader(request, Header.HTTP_FORCE_SAMPLE.toString()) != null) {
             SamplingHint.forceSampling();
         }
         final Trace trace = newTrace();
