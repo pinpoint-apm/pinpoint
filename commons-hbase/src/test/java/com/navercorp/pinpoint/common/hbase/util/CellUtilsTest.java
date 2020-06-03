e31769e9aa40b00b1801aacf33f22dcda363563d
Woonduk Kang <wd.kang@navercorp.com>
2020-06-03 13:10:37 +0900

    [#noissue] Add CellUtils - Reduce memory copy

---
+++ commons-hbase/src/test/java/com/navercorp/pinpoint/common/hbase/util/CellUtilsTest.java
@@ -0,0 +1,66 @@
+package com.navercorp.pinpoint.common.hbase.util;
+
+import com.navercorp.pinpoint.common.util.BytesUtils;
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class CellUtilsTest {
+
+    @Test
+    public void rowToString() {
+        Cell cell = mock(Cell.class);
+        String value = "abc";
+        byte[] bytes = Bytes.toBytes(value);
+        when(cell.getRowArray()).thenReturn(bytes);
+        when(cell.getRowOffset()).thenReturn(0);
+        when(cell.getRowLength()).thenReturn((short) bytes.length);
+        Result result = mock(Result.class);
+        Cell[] cells = new Cell[]{cell};
+        when(result.rawCells()).thenReturn(cells);
+
+        Assert.assertEquals(CellUtils.rowToString(result), value);
+    }
+
+    @Test
+    public void qualifierToInt() {
+        Cell cell = mock(Cell.class);
+        int value = 5;
+        byte[] bytes = Bytes.toBytes(value);
+        when(cell.getQualifierArray()).thenReturn(bytes);
+        when(cell.getQualifierOffset()).thenReturn(0);
+        when(cell.getQualifierLength()).thenReturn(bytes.length);
+
+        Assert.assertEquals(CellUtils.qualifierToInt(cell), value);
+    }
+
+    @Test
+    public void qualifierToString() {
+        Cell cell = mock(Cell.class);
+        String value = "abc";
+        byte[] bytes = Bytes.toBytes(value);
+        when(cell.getQualifierArray()).thenReturn(bytes);
+        when(cell.getQualifierOffset()).thenReturn(0);
+        when(cell.getQualifierLength()).thenReturn(bytes.length);
+
+        Assert.assertEquals(CellUtils.qualifierToString(cell), value);
+    }
+
+    @Test
+    public void valueToShort() {
+        Cell cell = mock(Cell.class);
+        short value = 5;
+        byte[] bytes = Bytes.toBytes(value);
+        when(cell.getValueArray()).thenReturn(bytes);
+        when(cell.getValueOffset()).thenReturn(0);
+        when(cell.getValueLength()).thenReturn(bytes.length);
+
+        Assert.assertEquals(CellUtils.valueToShort(cell), value);
+    }
+}
--- web/src/main/java/com/navercorp/pinpoint/web/mapper/AgentIdMapper.java
+++ web/src/main/java/com/navercorp/pinpoint/web/mapper/AgentIdMapper.java
@@ -16,9 +16,9 @@
 
 package com.navercorp.pinpoint.web.mapper;
 
+import com.navercorp.pinpoint.common.hbase.util.CellUtils;
 import org.apache.hadoop.hbase.Cell;
 import org.apache.hadoop.hbase.client.Result;
-import org.apache.hadoop.hbase.util.Bytes;
 import org.springframework.stereotype.Component;
 
 import com.navercorp.pinpoint.common.hbase.RowMapper;
@@ -42,7 +42,7 @@ public class AgentIdMapper implements RowMapper<List<String>> {
         final List<String> agentIdList = new ArrayList<>(rawCells.length);
 
         for (Cell cell : rawCells) {
-            final String agentId = Bytes.toString(cell.getQualifierArray(), cell.getQualifierOffset(), cell.getQualifierLength());
+            final String agentId = CellUtils.qualifierToString(cell);
             agentIdList.add(agentId);
         }
 
