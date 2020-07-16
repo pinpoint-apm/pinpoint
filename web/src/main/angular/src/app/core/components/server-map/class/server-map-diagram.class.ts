84cc7dfc7674190f7f90ec0a813620313c6b593a
QooQooDass <bindong.kim@navercorp.com>
2020-07-16 12:02:46 +0900

    [#noissue] Refactor: Move server-map search logic

--- web/src/main/angular/src/app/core/components/server-map/class/server-map-diagram.class.ts
+++ web/src/main/angular/src/app/core/components/server-map/class/server-map-diagram.class.ts
@@ -21,23 +21,6 @@ export abstract class ServerMapDiagram {
     abstract refresh(): void;
     abstract clear(): void;
 
-    searchNode(query: string): IApplication[] {
-        return this.serverMapData.getNodeList()
-            .reduce((prev: {[key: string]: any}[], curr: {[key: string]: any}) => {
-                const { key, mergedNodes } = curr;
-
-                return NodeGroup.isGroupKey(key) ? [...prev, ...mergedNodes] : [...prev, curr];
-            }, [])
-            .filter(({applicationName}: {applicationName: string}) => {
-                const regCheckQuery = new RegExp(query, 'i');
-
-                return regCheckQuery.test(applicationName);
-            })
-            .map(({key, applicationName, serviceType}: {key: string, applicationName: string, serviceType: string}) => {
-                return new Application(applicationName, serviceType, 0, key);
-            });
-    }
-
     setMergeState(mergeState: IServerMapMergeState): void {
         this.serverMapData.setMergeState(mergeState);
     }
--- web/src/main/angular/src/app/core/components/server-map/server-map-interaction.service.ts
+++ web/src/main/angular/src/app/core/components/server-map/server-map-interaction.service.ts
@@ -3,32 +3,20 @@ import { Subject, Observable } from 'rxjs';
 
 @Injectable()
 export class ServerMapInteractionService {
-    private outSearchWordSource = new Subject<string>();
-    private outSearchResultSource = new Subject<IApplication[]>();
     private outSelectedApplicationSource = new Subject<string>();
     private outRefresh = new Subject<null>();
     private outChangeMergeState = new Subject<IServerMapMergeState>();
 
-    public onSearchWord$: Observable<string>;
-    public onSearchResult$: Observable<IApplication[]>;
     public onSelectedApplication$: Observable<string>;
     public onRefresh$: Observable<null>;
     public onChangeMergeState$: Observable<IServerMapMergeState>;
 
     constructor() {
-        this.onSearchWord$ = this.outSearchWordSource.asObservable();
-        this.onSearchResult$ = this.outSearchResultSource.asObservable();
         this.onSelectedApplication$ = this.outSelectedApplicationSource.asObservable();
         this.onRefresh$ = this.outRefresh.asObservable();
         this.onChangeMergeState$ = this.outChangeMergeState.asObservable();
     }
 
-    setSearchWord(word: string): void {
-        this.outSearchWordSource.next(word);
-    }
-    setSearchResult(result: IApplication[]): void {
-        this.outSearchResultSource.next(result);
-    }
     setSelectedApplication(appKey: string): void {
         this.outSelectedApplicationSource.next(appKey);
     }
