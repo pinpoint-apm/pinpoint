011c1a9bf297634ebf80c895d9665582c319711f
cwJohnPark <pierrotcw@gmail.com>
2020-08-11 07:23:11 +0900

    [#7091] Fix batch auto configuration error

--- web/src/main/java/com/navercorp/pinpoint/web/WebApp.java
+++ web/src/main/java/com/navercorp/pinpoint/web/WebApp.java
@@ -3,12 +3,13 @@ package com.navercorp.pinpoint.web;
 import com.navercorp.pinpoint.common.server.util.ServerBootLogger;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.transaction.TransactionAutoConfiguration;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootConfiguration
-@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class, TransactionAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class, TransactionAutoConfiguration.class, BatchAutoConfiguration.class})
 @ImportResource({ "classpath:applicationContext-web.xml", "classpath:servlet-context-web.xml"})
 public class WebApp {
     private static final ServerBootLogger logger = ServerBootLogger.getLogger(WebApp.class);
