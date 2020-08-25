9c1903f79fa7713e4e2253bc1104ee2a175825a1
Woonduk Kang <wd.kang@navercorp.com>
2020-08-25 18:55:16 +0900

    [#7172] Add Testweb module for Agent

---
+++ agent-testweb/README.md
@@ -0,0 +1,8 @@
+## Agent Testweb
+Pinpoint-agent sample app
+
+## Prerequirements
+[Install pinpoint](../doc/installation.md#quick-overview-of-installation) and start [pinpoint-web](../web/README.md) and [pinpoint-collector](../collector/README.md)
+
+## Sample App
+[ThreadPlugin-Test](thread-plugin-test/README.md)
---
+++ agent-testweb/thread-plugin-test/src/main/java/com/pinpointest/plugin/ThreadPluginTestStarter.java
@@ -0,0 +1,14 @@
+package com.pinpointest.plugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class ThreadPluginTestStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadPluginTestStarter.class, args);
+    }
+}
