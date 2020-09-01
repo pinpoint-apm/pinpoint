6685fcb49f0d49df4b7d1adf6ee1e45232aeb75d
Woonduk Kang <wd.kang@navercorp.com>
2020-09-01 14:17:33 +0900

    [#noissue] Fix plugin version test

--- agent/src/test/java/com/navercorp/pinpoint/plugin/trace/TraceMetadataVerificationTest.java
+++ agent/src/test/java/com/navercorp/pinpoint/plugin/trace/TraceMetadataVerificationTest.java
@@ -16,19 +16,34 @@
 
 package com.navercorp.pinpoint.plugin.trace;
 
+import com.navercorp.pinpoint.common.Version;
 import com.navercorp.pinpoint.common.trace.TraceMetadataProvider;
+import com.navercorp.pinpoint.common.util.FileUtils;
+import com.navercorp.pinpoint.common.util.JvmUtils;
+import com.navercorp.pinpoint.common.util.JvmVersion;
+import com.navercorp.pinpoint.common.util.StringUtils;
 import com.navercorp.pinpoint.loader.plugins.PinpointPluginLoader;
 import com.navercorp.pinpoint.loader.plugins.trace.TraceMetadataProviderLoader;
+import com.navercorp.pinpoint.profiler.plugin.PluginJar;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * @author HyunGil Jeong
  */
 public class TraceMetadataVerificationTest {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     private final PinpointPluginLoader<TraceMetadataProvider> traceMetadataProviderLoader = new TraceMetadataProviderLoader();
 
@@ -36,14 +51,58 @@ public class TraceMetadataVerificationTest {
     public ErrorCollector collector = new ErrorCollector();
 
     @Test
-    public void checkTraceMetadata() {
-        ClassLoader systemClassLoader = ClassLoader.getSystemClassLoader();
-        List<TraceMetadataProvider> traceMetadataProviders = traceMetadataProviderLoader.load(systemClassLoader);
+    public void checkTraceMetadata() throws IOException {
+
+        URL[] libArray = getClassPath();
+        ClassLoader classLoader = new URLClassLoader(libArray);
+
+
+        List<TraceMetadataProvider> traceMetadataProviders = traceMetadataProviderLoader.load(classLoader);
         TraceMetadataVerifier verifier = new TraceMetadataVerifier();
         for (TraceMetadataProvider traceMetadataProvider : traceMetadataProviders) {
             traceMetadataProvider.setup(verifier.getTraceMetadataSetupContext());
         }
         verifier.verifyServiceTypes(collector);
         verifier.verifyAnnotationKeys(collector);
+    }
+
+    private URL[] getClassPath() throws IOException {
+        String classPaths = System.getProperty("java.class.path");
+//        logger.debug("classPath:{}", classPaths);
+
+        List<String> classPathList = StringUtils.tokenizeToStringList(classPaths, ";");
+        List<String> libs = filterJdk6Plugin(classPathList);
+        List<URL> urls = toURls(libs);
+        return urls.toArray(new URL[0]);
+    }
+
+    private List<URL> toURls(List<String> libs) throws IOException {
+        List<URL> list = new ArrayList<URL>();
+        for (String lib : libs) {
+            list.add(FileUtils.toURL(lib));
+        }
+        return list;
+    }
+
+    private List<String> filterJdk6Plugin(List<String> classPathList) {
+        List<String> list = new ArrayList<String>();
+        JvmVersion currentJvmVersion = JvmUtils.getVersion();
+        for (String jarPath : classPathList) {
+            // name filter
+            if (!jarPath.endsWith(String.format("-plugin-%s.jar", Version.VERSION))) {
+                list.add(jarPath);
+                continue;
+            }
+
+            logger.info("plugin:{}", jarPath);
+            PluginJar pluginJar = PluginJar.fromFilePath(jarPath);
+            JvmVersion pluginVersion = JvmVersion.getFromVersion(pluginJar.getPluginCompilerVersion());
+            if (currentJvmVersion.getClassVersion() >= pluginVersion.getClassVersion()) {
+                list.add(jarPath);
+                logger.info("currentJvm:{} pluginVersion:{}", currentJvmVersion, pluginVersion);
+            }
+        }
+
+        return list;
     }
 }
