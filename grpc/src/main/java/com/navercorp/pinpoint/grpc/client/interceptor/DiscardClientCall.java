d501022207f0cbe9c6db66efa35c2adb2dbbc4aa
jaehong-kim <jaehong.kim@navercorp.com>
2020-11-09 10:49:31 +0900

    [#7375] Add DiscardLimiter

--- grpc/src/main/java/com/navercorp/pinpoint/grpc/client/interceptor/DiscardClientCall.java
+++ grpc/src/main/java/com/navercorp/pinpoint/grpc/client/interceptor/DiscardClientCall.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.navercorp.pinpoint.grpc.client.interceptor;
 
 import com.navercorp.pinpoint.common.util.Assert;
@@ -9,17 +25,25 @@ import io.grpc.Metadata;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 
+/**
+ * @author Woonduk Kang(emeroad)
+ * @author jaehong.kim
+ */
 class DiscardClientCall<ReqT, RespT> extends ForwardClientCall<ReqT, RespT> {
-    private static final State OK = new State(true, "OK");
+    private static final State OK = new State(false, "OK");
+    private static final State NOT_READY = new State(false, "stream not ready");
+
     private final AtomicBoolean onReadyState = new AtomicBoolean(false);
     private final AtomicLong pendingCounter = new AtomicLong();
     private final long maxPendingThreshold;
     private final DiscardEventListener listener;
+    private final DiscardLimiter discardLimiter;
 
-    public DiscardClientCall(ClientCall<ReqT, RespT> delegate, DiscardEventListener listener, long maxPendingThreshold) {
+    public DiscardClientCall(ClientCall<ReqT, RespT> delegate, DiscardEventListener listener, long maxPendingThreshold, long discardCountForReconnect, long notReadyTimeoutMillis) {
         super(delegate);
         this.listener = Assert.requireNonNull(listener, "listener");
         this.maxPendingThreshold = maxPendingThreshold;
+        this.discardLimiter = new DiscardLimiter(discardCountForReconnect, notReadyTimeoutMillis);
     }
 
     @Override
@@ -37,6 +61,7 @@ class DiscardClientCall<ReqT, RespT> extends ForwardClientCall<ReqT, RespT> {
     private void reset() {
         this.onReadyState.compareAndSet(false, true);
         this.pendingCounter.set(0);
+        this.discardLimiter.reset();
     }
 
     @Override
@@ -44,24 +69,33 @@ class DiscardClientCall<ReqT, RespT> extends ForwardClientCall<ReqT, RespT> {
         final State state = readyState();
         if (OK == state) {
             super.sendMessage(message);
+        } else if (Boolean.FALSE == state.isCancel()) {
+            discardMessage(message, state.getMessage());
         } else {
-            discardMessage(message, state.getCause());
+            cancel(state.getMessage(), state.getCause());
         }
     }
 
     private State readyState() {
-        // skip pending queue state : DelayedStream
-        if (this.onReadyState.get() == false) {
+        // skip pending queue cancel : DelayedStream
+        if (Boolean.FALSE == this.onReadyState.get()) {
             final long pendingCount = this.pendingCounter.incrementAndGet();
             if (pendingCount > this.maxPendingThreshold) {
-                final String cause = "maximum pending requests " + pendingCount + "/" + this.maxPendingThreshold;
-                return new State(false, cause);
+                final String message = "maximum pending requests " + pendingCount + "/" + this.maxPendingThreshold;
+                return new State(false, message);
             }
             return OK;
         }
 
-        if (isReady() == false) {
-            return new State(false, "stream not ready");
+        final boolean ready = isReady();
+        try {
+            this.discardLimiter.discard(ready);
+        } catch (DiscardLimiter.DiscardLimiterException e) {
+            return new State(true, e.getMessage(), e);
+        }
+
+        if (Boolean.FALSE == ready) {
+            return NOT_READY;
         }
         return OK;
     }
@@ -80,23 +114,35 @@ class DiscardClientCall<ReqT, RespT> extends ForwardClientCall<ReqT, RespT> {
         return onReadyState.get();
     }
 
-
     public long getPendingCount() {
         return pendingCounter.get();
     }
 
     private static class State {
-        private final boolean state;
-        private final String cause;
+        private final boolean cancel;
+        private final String message;
+        private final Throwable cause;
 
-        public State(boolean state, String cause) {
-            this.state = state;
+        public State(boolean cancel, String message) {
+            this(cancel, message, null);
+        }
+
+        public State(boolean cancel, String message, Throwable cause) {
+            this.cancel = cancel;
+            this.message = message;
             this.cause = cause;
         }
 
-        public String getCause() {
+        public boolean isCancel() {
+            return cancel;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public Throwable getCause() {
             return cause;
         }
     }
-
 }
