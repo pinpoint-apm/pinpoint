0cc1517596a6e2545e780e960b31bc6080f13665
Koo Taejin <koo.taejin@navercorp.com>
2021-01-21 18:43:07 +0900

    [#release] prepare for 2.2.2-SNAPSHOT 2.2.1 -> 2.2.2-SNAPSHOT

--- commons/src/main/java/com/navercorp/pinpoint/common/Version.java
+++ commons/src/main/java/com/navercorp/pinpoint/common/Version.java
@@ -1,4 +1,4 @@
 package com.navercorp.pinpoint.common;
 public final class Version {
-    public static final String VERSION = "2.2.1";
+    public static final String VERSION = "2.2.2-SNAPSHOT";
 }
