77f729fb6ddeb22c25d66b2def4d2ece3267f31b
HyunjoonCho <jhj09016@gmail.com>
2021-02-09 19:43:17 +0900

    [#noissue] Port separation

--- collector/src/main/java/com/navercorp/pinpoint/collector/CollectorApp.java
+++ collector/src/main/java/com/navercorp/pinpoint/collector/CollectorApp.java
@@ -1,21 +1,22 @@
 package com.navercorp.pinpoint.collector;
 
+import com.navercorp.pinpoint.common.server.profile.ProfileApplicationListener;
 import com.navercorp.pinpoint.common.server.util.ServerBootLogger;
 import com.navercorp.pinpoint.metric.collector.MetricCollectorApp;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootConfiguration
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
-@ImportResource({ "classpath:applicationContext-collector.xml", "classpath:servlet-context-collector.xml"})
 public class CollectorApp {
     private static final ServerBootLogger logger = ServerBootLogger.getLogger(CollectorApp.class);
 
     public static void main(String[] args) {
         try {
-            CollectorStarter starter = new CollectorStarter(CollectorApp.class, MetricCollectorApp.class);
+            CollectorStarter starter = new CollectorStarter(CollectorApp.class);
+            starter.addModule(TotalCollectorApp.class, new ProfileApplicationListener(), 8081);
+            starter.addModule(MetricCollectorApp.class, 1111);
             starter.start(args);
         } catch (Exception exception) {
             logger.error("[CollectorApp] could not launch app.", exception);
--- collector/src/main/java/com/navercorp/pinpoint/collector/CollectorStarter.java
+++ collector/src/main/java/com/navercorp/pinpoint/collector/CollectorStarter.java
@@ -1,33 +1,54 @@
 package com.navercorp.pinpoint.collector;
 
-import com.navercorp.pinpoint.common.server.profile.ProfileApplicationListener;
 import com.navercorp.pinpoint.common.server.util.ServerBootLogger;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationListener;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class CollectorStarter {
     private static final ServerBootLogger logger = ServerBootLogger.getLogger(CollectorApp.class);
 
-    private final Class<?>[] sources;
+    private final SpringApplicationBuilder builder;
+    private List<SpringApplicationBuilder> builderList = new ArrayList<>();
 
     public CollectorStarter(Class<?>... sources) {
-        this.sources = Objects.requireNonNull(sources, "sources");
+        this.builder = new SpringApplicationBuilder();
+        this.builder.web(WebApplicationType.SERVLET);
+        this.builder.bannerMode(Banner.Mode.OFF);
+
+        builder.sources(sources);
+    }
+
+    public void addModule(Class<?> child, ApplicationListener<?> listener, int port) {
+        builderList.add(builderForSource(child, port).listeners(listener));
+    }
+
+    public void addModule(Class<?> child, int port) {
+        builderList.add(builderForSource(child, port));
+    }
+
+    private SpringApplicationBuilder builderForSource(Class<?> child, int port) {
+        Objects.requireNonNull(child, "child");
+        return this.builder.child(child)
+                .web(WebApplicationType.SERVLET)
+                .bannerMode(Banner.Mode.OFF)
+                .properties(portProperty(port));
+    }
+
+    private String portProperty(int port) {
+        return new StringBuilder("server.port:").append(port).toString();
     }
 
     public void start(String[] args) {
-
-        SpringApplicationBuilder builder = new SpringApplicationBuilder();
-        builder.sources(sources);
-        builder.web(WebApplicationType.SERVLET);
-        builder.bannerMode(Banner.Mode.OFF);
-        builder.listeners(new ProfileApplicationListener());
-
-        SpringApplication springApplication = builder.build();
-        springApplication.run(args);
+        for (SpringApplicationBuilder builder : builderList) {
+            builder.build().run(args);
+        }
     }
 
 }
