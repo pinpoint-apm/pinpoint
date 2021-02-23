65e4680a92e2bd7a5bf42c54639e06701b18f0d0
kkojaeh <kkojaeh@gmail.com>
2021-02-23 12:29:42 +0900

    [#7707] Fix PreparedStatement method parameter name for array type improved bytes display method by Hex

--- bootstraps/bootstrap-core/src/main/java/com/navercorp/pinpoint/bootstrap/plugin/jdbc/bindvalue/BytesConverter.java
+++ bootstraps/bootstrap-core/src/main/java/com/navercorp/pinpoint/bootstrap/plugin/jdbc/bindvalue/BytesConverter.java
@@ -16,7 +16,7 @@
 
 package com.navercorp.pinpoint.bootstrap.plugin.jdbc.bindvalue;
 
-import com.navercorp.pinpoint.common.util.ArrayUtils;
+import java.util.Arrays;
 
 /**
  * @author emeroad
@@ -32,9 +32,35 @@ public class BytesConverter implements Converter {
             if (bytes == null) {
                 return "null";
             } else {
-                return ArrayUtils.abbreviate(bytes);
+                return toHexString(bytes, MAX_BYTES_SIZE);
             }
         }
         return "error";
     }
+
+    // for uuid
+    private static final int MAX_BYTES_SIZE = 16;
+
+    private static final char[] HEX_CHARS = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E',
+        'F' };
+
+    private static String toHexString(byte[] bytes) {
+        char[] result = new char[bytes.length * 2];
+        int idx = 0;
+        for (byte b : bytes) {
+            int temp = (int) b & 0xFF;
+            result[idx++] = HEX_CHARS[temp / 16];
+            result[idx++] = HEX_CHARS[temp % 16];
+        }
+        return new String(result);
+    }
+
+    private static String toHexString(byte[] bytes, int maxSize) {
+        if(bytes.length > maxSize) {
+            return toHexString(Arrays.copyOf(bytes, maxSize)) + "...";
+        }else {
+            return toHexString(bytes);
+        }
+    }
+
 }
