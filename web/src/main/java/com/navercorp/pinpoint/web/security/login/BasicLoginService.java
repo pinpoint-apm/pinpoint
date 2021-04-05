268094e2823bb3ec052502f9ba17efc4f71cc9cc
Koo Taejin <koo.taejin@navercorp.com>
2021-04-05 14:41:05 +0900

    [#7205] Support very simple authentication on Web

---
+++ web/src/main/java/com/navercorp/pinpoint/web/security/login/BasicLoginService.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2021 NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.navercorp.pinpoint.web.security.login;
+
+import com.navercorp.pinpoint.web.config.BasicLoginConfig;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.Cookie;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Taejin Koo
+ */
+@Service
+@Profile("basicLogin")
+public class BasicLoginService {
+
+    private final PinpointMemoryUserDetailsService pinpointMemoryUserDetailsService;
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private final JwtService jwtService;
+
+    @Autowired
+    public BasicLoginService(BasicLoginConfig basicLoginConfig) {
+        PinpointMemoryUserDetailsService pinpointMemoryUserDetailsService = new PinpointMemoryUserDetailsService(basicLoginConfig);
+        this.pinpointMemoryUserDetailsService = pinpointMemoryUserDetailsService;
+
+        this.jwtService = new JwtService(basicLoginConfig);
+    }
+
+    public UserDetails getUserDetails(Cookie[] cookies) {
+        if (cookies == null) {
+            return null;
+        }
+
+        for (Cookie cookie : cookies) {
+            String name = cookie.getName();
+
+            if (BasicLoginConstants.PINPOINT_JWT_COOKIE_NAME.equals(name)) {
+                String pinpointJwtToken = cookie.getValue();
+
+                try {
+                    Date expirationDate = jwtService.getExpirationDate(pinpointJwtToken);
+                    if (expirationDate.getTime() > System.currentTimeMillis()) {
+                        String userId = jwtService.getUserId(pinpointJwtToken);
+
+                        UserDetails userDetails = pinpointMemoryUserDetailsService.loadUserByUsername(String.valueOf(userId));
+                        if (userDetails != null) {
+                            return userDetails;
+                        }
+                    } else {
+                        logger.warn("This token already expired.");
+                    }
+                } catch (ExpiredJwtException e) {
+                    logger.warn("This token already expired. message:{}", e.getMessage(), e);
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public Cookie createNewCookie(String userId) {
+        UserDetails userDetails = pinpointMemoryUserDetailsService.loadUserByUsername(userId);
+        if (userDetails == null) {
+            throw new IllegalArgumentException("Could not load User information.");
+        }
+
+        String token = jwtService.createToken(userDetails);
+        Cookie cookie = new Cookie(BasicLoginConstants.PINPOINT_JWT_COOKIE_NAME, token);
+        cookie.setPath("/");
+
+        long maxAge = TimeUnit.MILLISECONDS.toSeconds(jwtService.getExpirationTimeMillis());
+        cookie.setMaxAge(new Long(maxAge).intValue());
+        return cookie;
+    }
+
+    public UserDetailsService getUserDetailsService() {
+        return pinpointMemoryUserDetailsService;
+    }
+
+}
---
+++ web/src/main/java/com/navercorp/pinpoint/web/security/login/JwtService.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2021 NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.navercorp.pinpoint.web.security.login;
+
+import com.navercorp.pinpoint.common.util.Assert;
+import com.navercorp.pinpoint.common.util.StringUtils;
+import com.navercorp.pinpoint.web.config.BasicLoginConfig;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+/**
+ * @author Taejin Koo
+ */
+public class JwtService {
+
+    private final String KEY_CLAIMS_USER_ID = "userId";
+    private final String KEY_CLAIMS_USER_ROLE = "userRole";
+
+    private final JwtParser jwtParser;
+
+    private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+    private final String secretKey;
+
+    private final long expirationTimeMillis;
+
+    public JwtService(BasicLoginConfig basicLoginConfig) {
+        String secretKey = basicLoginConfig.getJwtSecretKey();
+        if (StringUtils.isEmpty(secretKey)) {
+            throw new IllegalArgumentException("secretKey may not be empty");
+        }
+        this.secretKey = secretKey;
+
+        Assert.isTrue(basicLoginConfig.getExpirationTimeSeconds() > 0, "expirationTimeSeconds must be '>= 0'");
+        this.expirationTimeMillis = TimeUnit.SECONDS.toMillis(basicLoginConfig.getExpirationTimeSeconds());
+
+        JwtParser jwtParser = Jwts.parser();
+        jwtParser.setSigningKey(secretKey);
+        this.jwtParser = jwtParser;
+    }
+
+    public String createToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+
+        Collection<? extends GrantedAuthority> authorities = userDetails.getAuthorities();
+
+        List<String> collect = authorities.stream().map(e -> ((GrantedAuthority) e).getAuthority()).collect(Collectors.toList());
+
+        claims.put(KEY_CLAIMS_USER_ID, userDetails.getUsername());
+        claims.put(KEY_CLAIMS_USER_ROLE, collect);
+
+        return createToken(claims);
+    }
+
+    private String createToken(Map<String, Object> claims) {
+        JwtBuilder jwtBuilder = Jwts.builder();
+
+        jwtBuilder.setClaims(claims);
+        jwtBuilder.setIssuedAt(new Date(System.currentTimeMillis()));
+        jwtBuilder.setExpiration(new Date(System.currentTimeMillis() + expirationTimeMillis));
+        jwtBuilder.signWith(signatureAlgorithm, secretKey);
+
+        return jwtBuilder.compact();
+    }
+
+    public String getUserId(String token) {
+        Jws<Claims> claimsJws = jwtParser.parseClaimsJws(token);
+        Claims body = claimsJws.getBody();
+
+        return body.get(KEY_CLAIMS_USER_ID, String.class);
+    }
+
+    public Date getExpirationDate(String token) {
+        Jws<Claims> claimsJws = jwtParser.parseClaimsJws(token);
+        Claims body = claimsJws.getBody();
+        return body.getExpiration();
+    }
+
+    public long getExpirationTimeMillis() {
+        return expirationTimeMillis;
+    }
+
+}
