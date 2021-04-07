e3d0f511f781ea7ea220c3cec31d5274b7469adf
BillionaireDY <doyoonkim1002@gmail.com>
2021-04-07 16:48:15 +0900

    apply: text color css variable

--- web/src/main/angular/src/app/core/constants/theme.ts
+++ web/src/main/angular/src/app/core/constants/theme.ts
@@ -1,6 +1,17 @@
+const WHITE_COLOR = '#ffffff'
+const BLUE_COLOR = '#469AE4'
+const DARK_BLUE_COLOR = '#4484C3'
+const MINT_COLOR = '#33B692'
+const BLACK_PRIMARY_COLOR = '#333333';
+const BLACK_SECONDARY_COLOR = '#666666';
+const BLACK_TERTIARY_COLOR = '#999999';
+
 export const light = {
-    'background-color': '#fff',
-    'primary-color': '#469ae4',
-    'secondary-color': '#33B692',
-    'dark-primary-color': '#4484c3',
+    'background-color': WHITE_COLOR,
+    'primary-color': BLUE_COLOR,
+    'dark-primary-color': DARK_BLUE_COLOR,
+    'secondary-color': MINT_COLOR,
+    'text-primary-color': BLACK_PRIMARY_COLOR,
+    'text-secondary-color': BLACK_SECONDARY_COLOR,
+    'text-tertiary-color': BLACK_TERTIARY_COLOR,
 };
