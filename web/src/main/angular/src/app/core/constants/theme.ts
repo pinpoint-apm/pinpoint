f530252c8c00538d2662fa1d0c2effce42b09246
BillionaireDY <doyoonkim1002@gmail.com>
2021-04-08 18:40:07 +0900

    change: ccolor code & naming

--- web/src/main/angular/src/app/core/constants/theme.ts
+++ web/src/main/angular/src/app/core/constants/theme.ts
@@ -1,17 +1,17 @@
-const WHITE_COLOR = '#ffffff'
-const BLUE_COLOR = '#469AE4'
-const DARK_BLUE_COLOR = '#4484C3'
-const MINT_COLOR = '#33B692'
-const BLACK_PRIMARY_COLOR = '#333333';
-const BLACK_SECONDARY_COLOR = '#666666';
-const BLACK_TERTIARY_COLOR = '#999999';
+const WHITE_100 = '#ffffff'
+const BLUE_100 = '#469AE4'
+const BLUE_110 = '#4484C3'
+const MINT_100 = '#33B692'
+const BLACK_60 = '#8A939E';
+const BLACK_90 = '#4B535B';
+const BLACK_100 = '#2A2D32';
 
 export const light = {
-    'background-color': WHITE_COLOR,
-    'primary-color': BLUE_COLOR,
-    'dark-primary-color': DARK_BLUE_COLOR,
-    'secondary-color': MINT_COLOR,
-    'text-primary-color': BLACK_PRIMARY_COLOR,
-    'text-secondary-color': BLACK_SECONDARY_COLOR,
-    'text-tertiary-color': BLACK_TERTIARY_COLOR,
+    'background-color': WHITE_100,
+    'primary-color': BLUE_100,
+    'dark-primary-color': BLUE_110,
+    'secondary-color': MINT_100,
+    'text-primary-color': BLACK_100,
+    'text-secondary-color': BLACK_90,
+    'text-tertiary-color': BLACK_60,
 };
