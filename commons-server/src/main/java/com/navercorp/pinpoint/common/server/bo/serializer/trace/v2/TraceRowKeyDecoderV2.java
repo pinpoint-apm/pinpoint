0e74b469626b07ed7ea2332fd8c8e5a102941022
King Jin <jinking.this@gmail.com>
2021-04-12 12:36:29 +0800

    [#7788] refactoring, rename AGENT_NAME_MAX_LEN to AGENT_ID_MAX_LEN in TraceRowKeyEncoderV2/TraceRowKeyDecoderV2 to avoid confusion

--- commons-server/src/main/java/com/navercorp/pinpoint/common/server/bo/serializer/trace/v2/TraceRowKeyDecoderV2.java
+++ commons-server/src/main/java/com/navercorp/pinpoint/common/server/bo/serializer/trace/v2/TraceRowKeyDecoderV2.java
@@ -29,7 +29,7 @@ import java.util.Objects;
 @Component
 public class TraceRowKeyDecoderV2 implements RowKeyDecoder<TransactionId> {
 
-    public static final int AGENT_NAME_MAX_LEN = TraceRowKeyEncoderV2.AGENT_NAME_MAX_LEN;
+    public static final int AGENT_ID_MAX_LEN = TraceRowKeyEncoderV2.AGENT_ID_MAX_LEN;
     public static final int DISTRIBUTE_HASH_SIZE = TraceRowKeyEncoderV2.DISTRIBUTE_HASH_SIZE;
 
     private final int distributeHashSize;
@@ -53,9 +53,9 @@ public class TraceRowKeyDecoderV2 implements RowKeyDecoder<TransactionId> {
 
     private TransactionId readTransactionId(byte[] rowKey, int offset) {
 
-        String agentId = BytesUtils.toStringAndRightTrim(rowKey, offset, AGENT_NAME_MAX_LEN);
-        long agentStartTime = BytesUtils.bytesToLong(rowKey, offset + AGENT_NAME_MAX_LEN);
-        long transactionSequence = BytesUtils.bytesToLong(rowKey, offset + BytesUtils.LONG_BYTE_LENGTH + AGENT_NAME_MAX_LEN);
+        String agentId = BytesUtils.toStringAndRightTrim(rowKey, offset, AGENT_ID_MAX_LEN);
+        long agentStartTime = BytesUtils.bytesToLong(rowKey, offset + AGENT_ID_MAX_LEN);
+        long transactionSequence = BytesUtils.bytesToLong(rowKey, offset + BytesUtils.LONG_BYTE_LENGTH + AGENT_ID_MAX_LEN);
 
         return new TransactionId(agentId, agentStartTime, transactionSequence);
     }
