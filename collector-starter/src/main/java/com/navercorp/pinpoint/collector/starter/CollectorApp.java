8c6613120ecd6ef013e02f80d71e46b8df115ac6
minwoo.jung <minwoo.jung@navercorp.com>
2021-05-24 13:39:24 +0900

    [snapshot] add starter project and refactoring metric project

--- collector-starter/src/main/java/com/navercorp/pinpoint/collector/starter/CollectorApp.java
+++ collector-starter/src/main/java/com/navercorp/pinpoint/collector/starter/CollectorApp.java
@@ -1,12 +1,13 @@
-package com.navercorp.pinpoint.collector;
+package com.navercorp.pinpoint.collector.starter;
 
 import com.navercorp.pinpoint.common.server.util.ServerBootLogger;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootConfiguration
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @ImportResource({ "classpath:applicationContext-collector.xml", "classpath:servlet-context-collector.xml"})
 public class CollectorApp {
     private static final ServerBootLogger logger = ServerBootLogger.getLogger(CollectorApp.class);
@@ -14,7 +15,7 @@ public class CollectorApp {
 
     public static void main(String[] args) {
         try {
-            CollectorStarter starter = new CollectorStarter(CollectorApp.class);
+            ApplicationStarter starter = new ApplicationStarter(CollectorApp.class);
             starter.start(args);
         } catch (Exception exception) {
             logger.error("[CollectorApp] could not launch app.", exception);
---
+++ collector-starter/src/main/java/com/navercorp/pinpoint/collector/starter/multi/application/MultiApplication.java
@@ -0,0 +1,42 @@
+package com.navercorp.pinpoint.collector.starter.multi.application;
+
+import com.navercorp.pinpoint.collector.env.CollectorEnvironmentApplicationListener;
+import com.navercorp.pinpoint.common.server.profile.ProfileApplicationListener;
+import com.navercorp.pinpoint.common.server.util.ServerBootLogger;
+import com.navercorp.pinpoint.metric.collector.MetricCollectorApp;
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootConfiguration
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+public class MultiApplication {
+    private static final ServerBootLogger logger = ServerBootLogger.getLogger(MultiApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplicationBuilder builder = new SpringApplicationBuilder();
+        builder.web(WebApplicationType.SERVLET);
+        builder.bannerMode(Banner.Mode.OFF);
+
+        builder.sources(MultiApplication.class);
+
+        SpringApplicationBuilder collectorAppBuilder = builder.child(BasicCollectorApp.class)
+                .web(WebApplicationType.SERVLET)
+                .bannerMode(Banner.Mode.OFF)
+                .listeners(new CollectorEnvironmentApplicationListener())
+                .properties(new StringBuilder("server.port:").append(1111).toString())
+                .listeners(new ProfileApplicationListener());
+
+        SpringApplicationBuilder metricAppBuilder = builder.child(MetricCollectorApp.class)
+                .web(WebApplicationType.SERVLET)
+                .bannerMode(Banner.Mode.OFF)
+                .listeners(new CollectorEnvironmentApplicationListener())
+                .properties(new StringBuilder("server.port:").append(8081).toString());
+
+        collectorAppBuilder.build().run(args);
+        metricAppBuilder.build().run(args);
+    }
+}
