b0ca32bb14894fc53b6adafa19c01c6c2bce54b4
Woonduk Kang <wd.kang@navercorp.com>
2021-05-28 11:37:48 +0900

    [#6771] Add missing ScatterChartConfig

--- web/src/main/java/com/navercorp/pinpoint/web/config/ScatterChartConfig.java
+++ web/src/main/java/com/navercorp/pinpoint/web/config/ScatterChartConfig.java
@@ -1,7 +1,9 @@
 package com.navercorp.pinpoint.web.config;
 
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.beans.factory.annotation.Value;
 
+@Configurable
 public class ScatterChartConfig {
 
     @Value("${web.scatter.serverside-scan.use-fuzzyrowfilter:false}")
