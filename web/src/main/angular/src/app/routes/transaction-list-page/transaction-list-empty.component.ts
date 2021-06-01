c0d221313419f99ea61651b2ffa3747ea31674a7
BillionaireDY <doyoonkim1002@gmail.com>
2021-06-01 18:13:00 +0900

    apply grey color variables

--- web/src/main/angular/src/app/routes/transaction-list-page/transaction-list-empty.component.ts
+++ web/src/main/angular/src/app/routes/transaction-list-page/transaction-list-empty.component.ts
@@ -8,18 +8,7 @@ import { TranslateService } from '@ngx-translate/core';
             <span>{{message}}</span>
         </div>
     `,
-    styles: [`
-        div {
-            width: 100%;
-            height: 100%;
-            display: flex;
-            font-size: 20px;
-            font-weight: 600;
-            align-items: center;
-            justify-content: center;
-            background-color: rgba(30, 87, 153, 0.3);
-        }
-    `]
+    styleUrls: ['./transaction-list-empty.component.css'],
 })
 export class TransactionListEmptyComponent implements OnInit {
     message: string;
