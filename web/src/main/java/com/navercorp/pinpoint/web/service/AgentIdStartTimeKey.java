a86a4f50595e73915cdc6af41fcf3bf6fe059b83
Woonduk Kang <wd.kang@navercorp.com>
2021-06-09 15:48:25 +0900

    [#6761] Polishing

---
+++ web/src/main/java/com/navercorp/pinpoint/web/service/AgentIdStartTimeKey.java
@@ -0,0 +1,48 @@
+package com.navercorp.pinpoint.web.service;
+
+import com.navercorp.pinpoint.common.server.bo.SpanBo;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class AgentIdStartTimeKey {
+    private final String agentId;
+    private final long agentStartTime;
+
+    public AgentIdStartTimeKey(String agentId, long agentStartTime) {
+        this.agentId = agentId;
+        this.agentStartTime = agentStartTime;
+    }
+
+    public String getAgentId() {
+        return agentId;
+    }
+
+    public long getAgentStartTime() {
+        return agentStartTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AgentIdStartTimeKey that = (AgentIdStartTimeKey) o;
+        return agentStartTime == that.agentStartTime && Objects.equals(agentId, that.agentId);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = agentId != null ? agentId.hashCode() : 0;
+        result = 31 * result + (int) (agentStartTime ^ (agentStartTime >>> 32));
+        return result;
+    }
+
+    public static AgentIdStartTimeKey toAgentStartTimeKey(SpanBo spanBo) {
+        return new AgentIdStartTimeKey(spanBo.getAgentId(), spanBo.getAgentStartTime());
+    }
+
+}
