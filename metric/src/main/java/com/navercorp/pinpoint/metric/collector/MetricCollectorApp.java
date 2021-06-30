469686dfe94f2df18bbd15681c0c3bb13179599e
Woonduk Kang <wd.kang@navercorp.com>
2021-06-30 16:49:51 +0900

    [#noissue] Remove PropertySourcesPlaceholderConfigurer

--- metric/src/main/java/com/navercorp/pinpoint/metric/collector/MetricCollectorApp.java
+++ metric/src/main/java/com/navercorp/pinpoint/metric/collector/MetricCollectorApp.java
@@ -5,10 +5,13 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootConfiguration
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
-@ImportResource({ "classpath:pinot-collector/applicationContext-collector-pinot.xml", "classpath:pinot-collector/servlet-context-collector-pinot.xml"})
+@ImportResource({"classpath:pinot-collector/applicationContext-collector-pinot.xml", "classpath:pinot-collector/servlet-context-collector-pinot.xml"})
+@PropertySource({"classpath:pinot-collector/kafka-producer-factory.properties",
+        "classpath:pinot-collector/kafka-topic.properties"})
 @Profile("metric")
 public class MetricCollectorApp {
 }
