e4d85d30547fb2164fbb29a9fc7d1ae98c55d619
Woonduk Kang <wd.kang@navercorp.com>
2021-07-05 17:35:28 +0900

    [#noissue] Polishing

--- web-starter/src/main/java/com/navercorp/pinpoint/web/starter/WebApp.java
+++ web-starter/src/main/java/com/navercorp/pinpoint/web/starter/WebApp.java
@@ -2,18 +2,14 @@ package com.navercorp.pinpoint.web.starter;
 
 import com.navercorp.pinpoint.common.server.util.ServerBootLogger;
 import com.navercorp.pinpoint.web.WebMvcConfig;
+import com.navercorp.pinpoint.web.WebServerConfig;
 import com.navercorp.pinpoint.web.WebStarter;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.transaction.TransactionAutoConfiguration;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.web.filter.ShallowEtagHeaderFilter;
-
-import java.util.Arrays;
 
 @SpringBootConfiguration
 @EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class, TransactionAutoConfiguration.class, BatchAutoConfiguration.class})
@@ -24,7 +20,7 @@ public class WebApp {
 
     public static void main(String[] args) {
         try {
-            WebStarter starter = new WebStarter(WebApp.class, WebMvcConfig.class);
+            WebStarter starter = new WebStarter(WebApp.class,  WebServerConfig.class, WebMvcConfig.class);
             starter.start(args);
         } catch (Exception exception) {
             logger.error("[WebApp] could not launch app.", exception);
