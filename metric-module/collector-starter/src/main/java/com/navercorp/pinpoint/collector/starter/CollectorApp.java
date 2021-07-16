d4f4463fbbed49d046916f35842e6cf1fb16ce3d
Woonduk Kang <wd.kang@navercorp.com>
2021-07-16 11:30:09 +0900

    [#noissue] Refactor metric-module structure

---
+++ metric-module/collector-starter/src/main/java/com/navercorp/pinpoint/collector/starter/CollectorApp.java
@@ -0,0 +1,26 @@
+package com.navercorp.pinpoint.collector.starter;
+
+import com.navercorp.pinpoint.common.server.util.ServerBootLogger;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.transaction.TransactionAutoConfiguration;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootConfiguration
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, TransactionAutoConfiguration.class})
+@ImportResource({"classpath:applicationContext-collector.xml", "classpath:servlet-context-collector.xml"})
+public class CollectorApp {
+    private static final ServerBootLogger logger = ServerBootLogger.getLogger(CollectorApp.class);
+
+
+    public static void main(String[] args) {
+        try {
+            ApplicationStarter starter = new ApplicationStarter(CollectorApp.class);
+            starter.start(args);
+        } catch (Exception exception) {
+            logger.error("[CollectorApp] could not launch app.", exception);
+        }
+    }
+
+}
