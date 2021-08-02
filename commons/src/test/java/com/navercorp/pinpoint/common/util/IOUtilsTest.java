308ce9fcd61bf8fd745a9fd19da8338a177e9b33
Woonduk Kang <wd.kang@navercorp.com>
2021-08-02 10:07:40 +0900

    [#8088] Improve buffer length check

--- commons/src/test/java/com/navercorp/pinpoint/common/util/IOUtilsTest.java
+++ commons/src/test/java/com/navercorp/pinpoint/common/util/IOUtilsTest.java
@@ -4,15 +4,23 @@ import org.apache.commons.lang3.RandomUtils;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 
 public class IOUtilsTest {
 
     @Test
-    public void toByteArray_small() throws IOException {
+    public void toByteArray_small1() throws IOException {
+        byte[] source = RandomUtils.nextBytes(32);
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(source);
+
+        byte[] bytes = IOUtils.toByteArray(inputStream);
+
+        org.junit.Assert.assertArrayEquals(source, bytes);
+    }
+
+    @Test
+    public void toByteArray_small2() throws IOException {
         byte[] source = RandomUtils.nextBytes(1024);
         ByteArrayInputStream inputStream = new ByteArrayInputStream(source);
 
