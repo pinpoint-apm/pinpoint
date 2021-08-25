e0e97198fa81f0657700e4dd99dbb35cfde2e18e
Woonduk Kang <wd.kang@navercorp.com>
2021-08-25 16:04:44 +0900

    [#noissue] Polishing for Thrift configuration

--- collector/src/test/java/com/navercorp/pinpoint/collector/thrift/config/StatReceiverConfigurationTest.java
+++ collector/src/test/java/com/navercorp/pinpoint/collector/thrift/config/StatReceiverConfigurationTest.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2019 NAVER Corp.
+ * Copyright 2021 NAVER Corp.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,9 +14,8 @@
  * limitations under the License.
  */
 
-package com.navercorp.pinpoint.collector.config;
+package com.navercorp.pinpoint.collector.thrift.config;
 
-import com.navercorp.pinpoint.collector.thrift.config.StatReceiverConfiguration;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
