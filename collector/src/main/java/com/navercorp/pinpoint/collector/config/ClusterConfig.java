d175df796bb31c69965e296ee0273aa8a4dfb5f6
Woonduk Kang <wd.kang@navercorp.com>
2021-08-25 17:37:45 +0900

    [#noissue] Add config dir to component-scan

--- collector/src/main/java/com/navercorp/pinpoint/collector/config/ClusterConfig.java
+++ collector/src/main/java/com/navercorp/pinpoint/collector/config/ClusterConfig.java
@@ -21,9 +21,11 @@ import com.navercorp.pinpoint.common.server.config.LoggingEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
+@Component
 public class ClusterConfig {
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
--- collector/src/main/java/com/navercorp/pinpoint/collector/config/ScatterConfiguration.java
+++ collector/src/main/java/com/navercorp/pinpoint/collector/config/ScatterConfiguration.java
@@ -5,12 +5,12 @@ import com.navercorp.pinpoint.common.server.config.LoggingEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
 // @Experimental
-@Configuration
+@Component
 public class ScatterConfiguration {
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
