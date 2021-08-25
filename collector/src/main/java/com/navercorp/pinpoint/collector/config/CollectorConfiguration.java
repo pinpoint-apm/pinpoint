0a3b756c3d29a4ff0d19bbcb128b6fbd9453a153
Woonduk Kang <wd.kang@navercorp.com>
2021-08-25 17:37:45 +0900

    [#noissue] Add config dir to component-scan

--- collector/src/main/java/com/navercorp/pinpoint/collector/config/CollectorConfiguration.java
+++ collector/src/main/java/com/navercorp/pinpoint/collector/config/CollectorConfiguration.java
@@ -22,6 +22,7 @@ import com.navercorp.pinpoint.common.server.config.LoggingEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.util.Arrays;
@@ -32,6 +33,7 @@ import java.util.Objects;
  * @author emeroad
  * @author jaehong.kim
  */
+@Component
 public class CollectorConfiguration {
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
