5f77c9f9ed243c5c1011a8bc1698525c02b85492
Woonduk Kang <wd.kang@navercorp.com>
2021-09-06 20:25:08 +0900

    [#8200] Remove JDK9 from optional-jdk9

--- profiler-optional/profiler-optional-jdk9/src/main/java11/jdk/internal/access/SharedSecrets.java
+++ profiler-optional/profiler-optional-jdk9/src/main/java11/jdk/internal/access/SharedSecrets.java
@@ -1,7 +1,24 @@
 package jdk.internal.access;
 
+import java.security.ProtectionDomain;
+
 public class SharedSecrets {
     public static JavaLangAccess getJavaLangAccess() {
-        return null;
+        return new JavaLangAccess() {
+            @Override
+            public Class<?> defineClass(ClassLoader cl, String name, byte[] b, ProtectionDomain pd, String source) {
+                return null;
+            }
+
+            @Override
+            public Class<?> defineClass(ClassLoader cl, Class<?> lookup, String name, byte[] b, ProtectionDomain pd, boolean initialize, int flags, Object classData) {
+                return null;
+            }
+
+            @Override
+            public void registerShutdownHook(int slot, boolean registerShutdownInProgress, Runnable hook) {
+
+            }
+        };
     }
 }
