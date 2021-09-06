53249de571dbbcf81fa32f35058e5392c7d53722
Woonduk Kang <wd.kang@navercorp.com>
2021-09-06 20:01:53 +0900

    [#noissue] Add module permission for configuration

--- bootstraps/bootstrap-java9/src/main/java/com/navercorp/pinpoint/bootstrap/java9/module/ModuleSupport.java
+++ bootstraps/bootstrap-java9/src/main/java/com/navercorp/pinpoint/bootstrap/java9/module/ModuleSupport.java
@@ -80,6 +80,10 @@ public class ModuleSupport {
 
     private void addPermissionToValueAnnotation(JavaModule agentModule) {
         JavaModule bootstrapModule = getBootstrapModule();
+
+        agentModule.addOpens("com.navercorp.pinpoint.profiler.instrument.config", bootstrapModule);
+        agentModule.addOpens("com.navercorp.pinpoint.profiler.plugin.config", bootstrapModule);
+
         agentModule.addOpens("com.navercorp.pinpoint.profiler.context.thrift.config", bootstrapModule);
         agentModule.addOpens("com.navercorp.pinpoint.profiler.context.grpc.config", bootstrapModule);
         agentModule.addOpens("com.navercorp.pinpoint.grpc.client.config", bootstrapModule);
