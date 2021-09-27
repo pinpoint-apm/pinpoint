7538715e5716bf48607ed81eb15c2ff1858d952a
Taejin Koo <koo.taejin@navercorp.com>
2021-09-27 19:04:29 +0900

    [#noissue] Fix okhttp IT not working over 3.13 version

---
+++ plugins-it/okhttp-it/src/test/java/com/navercorp/pinpoint/plugin/okhttp/OkHttpClient_3_13_0_to_3_x_IT.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2018 NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.navercorp.pinpoint.plugin.okhttp;
+
+import com.navercorp.pinpoint.pluginit.utils.AgentPath;
+import com.navercorp.pinpoint.pluginit.utils.PluginITConstants;
+import com.navercorp.pinpoint.pluginit.utils.WebServer;
+import com.navercorp.pinpoint.test.plugin.Dependency;
+import com.navercorp.pinpoint.test.plugin.ImportPlugin;
+import com.navercorp.pinpoint.test.plugin.JvmVersion;
+import com.navercorp.pinpoint.test.plugin.PinpointAgent;
+import com.navercorp.pinpoint.test.plugin.PinpointPluginTestSuite;
+
+import org.junit.runner.RunWith;
+
+/**
+ * @author HyunGil Jeong
+ */
+@RunWith(PinpointPluginTestSuite.class)
+@PinpointAgent(AgentPath.PATH)
+@JvmVersion(8)
+@ImportPlugin("com.navercorp.pinpoint:pinpoint-okhttp-plugin")
+@Dependency({"com.squareup.okhttp3:okhttp:[3.13.0,4.0.0-alpha)", WebServer.VERSION, PluginITConstants.VERSION})
+public class OkHttpClient_3_13_0_to_3_x_IT extends OkHttpClient_3_4_0_BaseIT {
+
+}
