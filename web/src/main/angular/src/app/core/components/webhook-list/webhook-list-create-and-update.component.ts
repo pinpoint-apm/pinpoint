d5d95ee6850d94648f61df9b5b4f4d476d17b52a
BillionaireDY <doyoonkim1002@gmail.com>
2021-10-15 18:43:02 +0900

    [#8132] Feat webhook-list-create-and-update

--- web/src/main/angular/src/app/core/components/webhook-list/webhook-list-create-and-update.component.ts
+++ web/src/main/angular/src/app/core/components/webhook-list/webhook-list-create-and-update.component.ts
@@ -1,21 +1,12 @@
 import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-
 import { filterObj } from 'app/core/utils/util';
+import { CustomFormValidatorService } from 'app/shared/services/custom-form-validator.service';
+import { IWebhook } from './webhook-list-data.service';
 
-export const enum NotificationType {
-    ALL = 'all',
-    EMAIL = 'email',
-    SMS = 'sms',
-    WEBHOOK = 'webhook'
-}
-
-export interface IAlarmForm {
-    checkerName: string;
-    userGroupId: string;
-    threshold: number;
-    type: string;
-    notes: string;
+export interface IWebhookForm {
+    alias?: string;
+    url: string;
 }
 
 @Component({
@@ -26,60 +17,54 @@ export interface IAlarmForm {
 export class WebhookListCreateAndUpdateComponent implements OnInit, OnChanges {
     @Input() checkerList: string[];
     @Input() userGroupList: string[];
-    @Input() i18nLabel: {[key: string]: string};
+    @Input() i18nText: {[key: string]: string};
     @Input() i18nFormGuide: {[key: string]: IFormFieldErrorType};
-    @Input() webhookEnable: boolean;
-    @Output() outUpdateAlarm = new EventEmitter<IAlarmForm>();
-    @Output() outCreateAlarm = new EventEmitter<IAlarmForm>();
+    @Input() editWebhook: IWebhook;
+    @Output() outUpdateWebhook = new EventEmitter<IWebhookForm>();
+    @Output() outCreateWebhook = new EventEmitter<IWebhookForm>();
     @Output() outClose = new EventEmitter<void>();
     @Output() outShowHelp = new EventEmitter<{[key: string]: ICoordinate}>();
 
-    alarmForm = new FormGroup({
-        'checkerName': new FormControl('', [Validators.required]),
-        'userGroupId': new FormControl('', [Validators.required]),
-        'threshold': new FormControl(1, [Validators.required, Validators.min(1)]),
-        'type': new FormControl('all'),
-        'notes': new FormControl(''),
+    webhookForm = new FormGroup({
+        'alias': new FormControl('', [
+            Validators.max(100)
+        ]),
+        'url': new FormControl('', [
+            Validators.required, 
+            CustomFormValidatorService.validate(/(?:^|[ \t])((https?:\/\/)?(?:localhost|[\w-]+(?:\.[\w-]+)+)(:\d+)?(\/\S*)?)/gm),
+        ]),
     });
 
     constructor() {}
+    
     ngOnInit() {}
+    
     ngOnChanges(changes: SimpleChanges) {
-        // const alarmChange = changes['editAlarm'];
+        const editWebhook = changes['editWebhook'];
 
-        // if (alarmChange && alarmChange.currentValue) {
-        //     const formattedObj = filterObj((key: string) => Object.keys(this.alarmForm.controls).includes(key), alarmChange.currentValue);
-
-        //     formattedObj.type = this.getTypeStr(this.editAlarm);
-        //     this.alarmForm.reset(formattedObj);
-        // }
+        if (editWebhook && editWebhook.currentValue) {
+            const formattedObj = filterObj((key: string) => Object.keys(this.webhookForm.controls).includes(key), editWebhook.currentValue);
+            this.webhookForm.reset(formattedObj);
+        }
     }
 
     onCreateOrUpdate() {
-        // this.alarmForm.markAllAsTouched();
+        this.webhookForm.markAllAsTouched();
 
-        // if (this.alarmForm.invalid) {
-        //     return;
-        // }
+        if (this.webhookForm.invalid) {
+            return;
+        }
+        const value = this.webhookForm.value;
 
-        // const alarm = this.alarmForm.value;
-
-        // this.editAlarm ? this.outUpdateAlarm.emit(alarm) : this.outCreateAlarm.emit(alarm);
-        // this.onClose();
+        this.editWebhook ? this.outUpdateWebhook.emit(value) : this.outCreateWebhook.emit(value);
+        this.onClose();
     }
 
     onClose() {
         this.outClose.emit();
     }
 
-    onShowHelp(target: HTMLElement): void {
-        // const {left, top, width, height} = target.getBoundingClientRect();
-
-        // this.outShowHelp.emit({
-        //     coord: {
-        //         coordX: left + width / 2,
-        //         coordY: top + height / 2
-        //     }
-        // });
+    get title(): string {
+        return this.editWebhook ? 'Edit Webhook' : 'Add Webhook';
     }
 }
