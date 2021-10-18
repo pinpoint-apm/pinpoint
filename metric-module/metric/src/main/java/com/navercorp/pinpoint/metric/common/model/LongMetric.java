f0fe3d2f7d73be7079d0444cf58c758c9c962027
minwoo.jung <minwoo.jung@navercorp.com>
2021-10-18 15:46:35 +0900

    [#8004] Change timestampInEpoch to eventTime

--- metric-module/metric/src/main/java/com/navercorp/pinpoint/metric/common/model/LongMetric.java
+++ metric-module/metric/src/main/java/com/navercorp/pinpoint/metric/common/model/LongMetric.java
@@ -5,8 +5,8 @@ import java.util.List;
 public class LongMetric extends SystemMetric {
     private final long fieldValue;
 
-    public LongMetric(String metricName, String hostName, String fieldName, long fieldValue, List<Tag> tags, long timestamp) {
-        super(metricName, fieldName, hostName, tags, timestamp);
+    public LongMetric(String metricName, String hostName, String fieldName, long fieldValue, List<Tag> tags, long eventTime) {
+        super(metricName, fieldName, hostName, tags, eventTime);
         this.fieldValue = fieldValue;
     }
 
@@ -22,7 +22,7 @@ public class LongMetric extends SystemMetric {
         sb.append(", field=").append(fieldName);
         sb.append(", value=").append(fieldValue);
         sb.append(", tags=").append(tags);
-        sb.append(", timestamp=").append(timestamp);
+        sb.append(", eventTime=").append(eventTime);
         sb.append('}');
         return sb.toString();
     }
