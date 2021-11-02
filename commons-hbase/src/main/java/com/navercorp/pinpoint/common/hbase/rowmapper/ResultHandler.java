2543009483a03f283a53cc45d6e5c050a91a31e2
emeroad <emeroad@gmail.com>
2021-11-02 13:57:08 +0900

    [#noissue] Remove unnecessary interface

--- commons-hbase/src/main/java/com/navercorp/pinpoint/common/hbase/rowmapper/ResultHandler.java
+++
@@ -1,7 +0,0 @@
-package com.navercorp.pinpoint.common.hbase.rowmapper;
-
-import org.apache.hadoop.hbase.client.Result;
-
-public interface ResultHandler {
-    void mapRow(Result result, int rowNum);
-}
