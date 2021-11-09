a040af480bee338ea8575a450e9a1d003ece3989
Taejin Koo <koo.taejin@navercorp.com>
2021-11-09 18:40:30 +0900

    [#release] prepare for v2.3.1

--- commons/src/main/java/com/navercorp/pinpoint/common/Version.java
+++ commons/src/main/java/com/navercorp/pinpoint/common/Version.java
@@ -1,4 +1,4 @@
 package com.navercorp.pinpoint.common;
 public final class Version {
-    public static final String VERSION = "2.3.1-SNAPSHOT";
+    public static final String VERSION = "2.3.1";
 }
