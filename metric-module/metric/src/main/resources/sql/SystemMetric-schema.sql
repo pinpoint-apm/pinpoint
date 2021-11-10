14cf07e5a6309d5c235b510104cead5e1e524a6b
minwoo.jung <minwoo.jung@navercorp.com>
2021-11-10 15:55:58 +0900

    [#4492] change schema

--- metric-module/metric/src/main/resources/sql/SystemMetric-schema.sql
+++ metric-module/metric/src/main/resources/sql/SystemMetric-schema.sql
@@ -9,11 +9,11 @@ ALTER TABLE system_metric_data_type ADD UNIQUE KEY metric_name_field_name_idx (`
 
 CREATE TABLE `system_metric_tag` (
   `number` int(20) unsigned NOT NULL AUTO_INCREMENT,
-  `host_group_id` varchar(50) NOT NULL,
+  `host_group_name` varchar(50) NOT NULL,
   `host_name` varchar(50) NOT NULL,
   `metric_name` varchar(50) NOT NULL,
   `field_name` varchar(50) NOT NULL,
   `tags` varchar(5000),
   PRIMARY KEY (`number`)
 );
-ALTER TABLE system_metric_tag ADD INDEX application_id_host_name_metric_name_field_name_idx (application_id, host_name, metric_name, field_name);
+ALTER TABLE system_metric_tag ADD INDEX host_group_name_host_name_metric_name_field_name_idx (host_group_name, host_name, metric_name, field_name);
