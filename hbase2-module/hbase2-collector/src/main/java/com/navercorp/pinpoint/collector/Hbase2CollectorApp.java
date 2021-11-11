a887052e6599657ed93fdd8932a5e16056fa82ff
Taejin Koo <koo.taejin@navercorp.com>
2021-11-11 16:57:41 +0900

    [#8394] Fix the collector for Hbase2 does not work

--- hbase2-module/hbase2-collector/src/main/java/com/navercorp/pinpoint/collector/Hbase2CollectorApp.java
+++ hbase2-module/hbase2-collector/src/main/java/com/navercorp/pinpoint/collector/Hbase2CollectorApp.java
@@ -1,26 +1,17 @@
 package com.navercorp.pinpoint.collector;
 
 import com.navercorp.pinpoint.common.server.util.ServerBootLogger;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.transaction.TransactionAutoConfiguration;
-import org.springframework.context.annotation.ImportResource;
 
-@SpringBootConfiguration
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, TransactionAutoConfiguration.class})
-@ImportResource({ "classpath:applicationContext-collector.xml", "classpath:servlet-context-collector.xml"})
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Hbase2CollectorApp {
-    private static final ServerBootLogger logger = ServerBootLogger.getLogger(Hbase2CollectorApp.class);
-
+    private static final ServerBootLogger logger = ServerBootLogger.getLogger(CollectorApp.class);
 
     public static void main(String[] args) {
-        try {
-            CollectorStarter starter = new CollectorStarter(Hbase2CollectorApp.class);
-            starter.start(args);
-        } catch (Exception exception) {
-            logger.error("[CollectorApp] could not launch app.", exception);
-        }
+        logger.info(String.format("%s start", Hbase2CollectorApp.class.getSimpleName()));
+
+        CollectorApp.main(args);
     }
 
 }
