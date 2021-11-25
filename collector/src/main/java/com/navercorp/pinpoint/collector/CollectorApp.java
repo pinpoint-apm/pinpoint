0c777a9f9b104e29d72b1fa49abe06b470ca7450
Taejin Koo <koo.taejin@navercorp.com>
2021-11-25 15:39:38 +0900

    [#8407] Update Spring-boot 2.4.13 - > 2.5.7

--- collector/src/main/java/com/navercorp/pinpoint/collector/CollectorApp.java
+++ collector/src/main/java/com/navercorp/pinpoint/collector/CollectorApp.java
@@ -5,12 +5,13 @@ import com.navercorp.pinpoint.common.server.util.ServerBootLogger;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.sql.init.SqlInitializationAutoConfiguration;
 import org.springframework.boot.autoconfigure.transaction.TransactionAutoConfiguration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootConfiguration
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, TransactionAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, TransactionAutoConfiguration.class, SqlInitializationAutoConfiguration.class})
 @ImportResource({"classpath:applicationContext-collector.xml", "classpath:servlet-context-collector.xml"})
 @Import({CollectorAppPropertySources.class, FlinkContextConfiguration.class})
 public class CollectorApp {
