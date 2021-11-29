54f4744434c9a8ba19e55645b21518b8ec1c68f6
minwoo.jung <minwoo.jung@navercorp.com>
2021-11-29 19:21:29 +0900

    [#4492] Maintain compatibility for changes related to starter and builder logic

--- metric-module/metric/src/main/java/com/navercorp/pinpoint/metric/collector/MetricCollectorApp.java
+++ metric-module/metric/src/main/java/com/navercorp/pinpoint/metric/collector/MetricCollectorApp.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.boot.autoconfigure.transaction.TransactionAutoConfiguration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
@@ -12,7 +13,7 @@ import org.springframework.context.annotation.PropertySource;
 @SpringBootConfiguration
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, TransactionAutoConfiguration.class, KafkaAutoConfiguration.class})
 @ImportResource({"classpath:pinot-collector/applicationContext-collector-pinot.xml", "classpath:pinot-collector/servlet-context-collector-pinot.xml"})
-@PropertySource({"classpath:pinot-collector/kafka-topic.properties"})
+@Import({MetricAppPropertySources.class})
 @Profile("metric")
 public class MetricCollectorApp {
 }
