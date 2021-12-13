0b6956aa50bbdb2eba72eb408970e961977cb5c3
wd-kang <emeroad@gmail.com>
2021-12-13 11:43:18 +0900

    [#8495] Prepare 2.3.3-SNAPSHOT

--- commons/src/main/java/com/navercorp/pinpoint/common/Version.java
+++ commons/src/main/java/com/navercorp/pinpoint/common/Version.java
@@ -1,4 +1,4 @@
 package com.navercorp.pinpoint.common;
 public final class Version {
-    public static final String VERSION = "2.3.2-SNAPSHOT";
+    public static final String VERSION = "2.3.3-SNAPSHOT";
 }
