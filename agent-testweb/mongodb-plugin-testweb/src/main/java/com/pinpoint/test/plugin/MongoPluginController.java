14f454d14f52541aedf4f49ce12037e1f8a48458
jaehong-kim <jaehong.kim@navercorp.com>
2022-03-10 16:23:43 +0900

    init

---
+++ agent-testweb/mongodb-plugin-testweb/src/main/java/com/pinpoint/test/plugin/MongoPluginController.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright 2022 NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pinpoint.test.plugin;
+
+import com.mongodb.client.result.InsertOneResult;
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
+import com.mongodb.reactivestreams.client.MongoCollection;
+import com.mongodb.reactivestreams.client.MongoDatabase;
+import de.flapdoodle.embed.mongo.MongodExecutable;
+import de.flapdoodle.embed.mongo.MongodProcess;
+import de.flapdoodle.embed.mongo.MongodStarter;
+import de.flapdoodle.embed.mongo.config.MongodConfig;
+import de.flapdoodle.embed.mongo.config.Net;
+import de.flapdoodle.embed.mongo.distribution.Version;
+import de.flapdoodle.embed.process.runtime.Network;
+import org.bson.BsonArray;
+import org.bson.BsonBinary;
+import org.bson.BsonBinarySubType;
+import org.bson.BsonBoolean;
+import org.bson.BsonDateTime;
+import org.bson.BsonDbPointer;
+import org.bson.BsonDocument;
+import org.bson.BsonDouble;
+import org.bson.BsonInt32;
+import org.bson.BsonInt64;
+import org.bson.BsonJavaScript;
+import org.bson.BsonJavaScriptWithScope;
+import org.bson.BsonNull;
+import org.bson.BsonObjectId;
+import org.bson.BsonRegularExpression;
+import org.bson.BsonString;
+import org.bson.BsonSymbol;
+import org.bson.BsonTimestamp;
+import org.bson.BsonUndefined;
+import org.bson.BsonValue;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.Arrays;
+import java.util.Date;
+
+@RestController
+public class MongoPluginController {
+    private int port;
+    private MongodExecutable mongodExecutable;
+    private MongodProcess mongod;
+
+    @PostConstruct
+    private void start() throws Exception {
+        MongodStarter starter = MongodStarter.getDefaultInstance();
+
+        this.port = Network.getFreeServerPort();
+        MongodConfig mongodConfig = MongodConfig.builder()
+                .version(Version.Main.PRODUCTION)
+                .net(new Net(port, Network.localhostIsIPv6()))
+                .build();
+
+        this.mongodExecutable = starter.prepare(mongodConfig);
+        this.mongod = mongodExecutable.start();
+    }
+
+    @PreDestroy
+    private void shutdown() {
+        if (mongod != null) {
+            mongod.stop();
+        }
+        if (mongodExecutable != null) {
+            mongodExecutable.stop();
+        }
+    }
+
+    @GetMapping(value = "/mongo/save")
+    public String save() throws Exception {
+        String uri = "mongodb://localhost:" + port;
+        MongoClient mongo = MongoClients.create(uri);
+        MongoDatabase db = mongo.getDatabase("test");
+
+        MongoCollection<Document> collection = db.getCollection("test");
+        Document doc = new Document("name", "MongoDB")
+                .append("type", "database")
+                .append("count", 1)
+                .append("info", new Document("x", 203).append("y", 102));
+
+        collection.insertOne(doc).subscribe(new SubscriberHelpers.OperationSubscriber<>());
+
+        return "OK";
+    }
+
+    private Document createComplexDocument() {
+        //insert Data
+        BsonValue a = new BsonString("stest");
+        BsonValue b = new BsonDouble(111);
+        BsonValue c = new BsonBoolean(true);
+
+        Document document = new Document()
+                .append("int32", new BsonInt32(12))
+                .append("int64", new BsonInt64(77L))
+                .append("bo\"olean", new BsonBoolean(true))
+                .append("date", new BsonDateTime(new Date().getTime()))
+                .append("double", new BsonDouble(12.3))
+                .append("string", new BsonString("pinpoint"))
+                .append("objectId", new BsonObjectId(new ObjectId()))
+                .append("code", new BsonJavaScript("int i = 10;"))
+                .append("codeWithScope", new BsonJavaScriptWithScope("int x = y", new BsonDocument("y", new BsonInt32(1))))
+                .append("regex", new BsonRegularExpression("^test.*regex.*xyz$", "big"))
+                .append("symbol", new BsonSymbol("wow"))
+                .append("timestamp", new BsonTimestamp(0x12345678, 5))
+                .append("undefined", new BsonUndefined())
+                .append("binary1", new BsonBinary(new byte[]{(byte) 0xe0, 0x4f, (byte) 0xd0, 0x20}))
+                .append("oldBinary", new BsonBinary(BsonBinarySubType.OLD_BINARY, new byte[]{1, 1, 1, 1, 1}))
+                .append("arrayInt", new BsonArray(Arrays.asList(a, b, c, new BsonInt32(7))))
+                .append("document", new BsonDocument("a", new BsonInt32(77)))
+                .append("dbPointer", new BsonDbPointer("db.coll", new ObjectId()))
+                .append("null", new BsonNull());
+        return document;
+    }
+
+}
