9f133b98f522e043eeb31a03c01a34d10b5fc4c5
Yongseok Kang <feelform@gmail.com>
2022-04-04 19:44:11 +0900

    [#8711] Fix SLF4J: Class path contains multiple SLF4J bindings

--- batch/build.gradle.kts
+++ batch/build.gradle.kts
@@ -8,10 +8,12 @@ dependencies {
     api(project(":pinpoint-commons-server"))
     api(project(":pinpoint-commons"))
     api(project(":pinpoint-thrift"))
-    api(project(":pinpoint-web"))
+    api(project(":pinpoint-web")) // all transitive depdency off on pinpoint-web
     api(project(":pinpoint-rpc"))
     implementation("org.apache.thrift:libthrift:0.15.0")
-    implementation("org.springframework.boot:spring-boot-starter-web:${Versions.springBoot}")
+    implementation("org.springframework.boot:spring-boot-starter-web:${Versions.springBoot}") {
+        exclude(group = "org.springframework.boot", module = "spring-boot-starter-logging")
+    }
     implementation("org.springframework.boot:spring-boot-starter-log4j2:${Versions.springBoot}")
     implementation("org.springframework:spring-web:${Versions.spring}")
     implementation("org.springframework:spring-context-support:${Versions.spring}")
@@ -20,11 +22,18 @@ dependencies {
     implementation("org.springframework.batch:spring-batch-core:4.3.3")
     implementation("com.zaxxer:HikariCP:4.0.3")
     implementation("org.mybatis:mybatis:3.5.7")
-    implementation("org.mybatis:mybatis-spring:2.0.6")
+    implementation("org.mybatis:mybatis-spring:2.0.6") {
+        exclude(group = "org.springframework", module = "spring-core")
+        exclude(group = "org.springframework", module = "spring-tx")
+        exclude(group = "org.springframework", module = "spring-jdbc")
+        exclude(group = "org.springframework", module = "spring-context")
+    }
     implementation("mysql:mysql-connector-java:8.0.27")
     runtimeOnly("com.sun.activation:jakarta.activation:1.2.2")
     runtimeOnly("org.slf4j:slf4j-api:1.7.30")
-    runtimeOnly("org.apache.logging.log4j:log4j-jcl:${Versions.log4jJDK8}")
+    runtimeOnly("org.apache.logging.log4j:log4j-jcl:${Versions.log4jJDK8}") {
+        exclude(group = "commons-logging", module = "commons-logging")
+    }
     runtimeOnly("org.apache.logging.log4j:log4j-slf4j-impl:${Versions.log4jJDK8}")
     runtimeOnly("org.apache.logging.log4j:log4j-core:${Versions.log4jJDK8}")
     testImplementation("org.springframework:spring-test:${Versions.spring}")
