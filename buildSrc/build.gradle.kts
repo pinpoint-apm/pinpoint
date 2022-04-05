68ea6bf848cc04bfe94cf452e7dd075e2799d057
Yongseok Kang <feelform@gmail.com>
2022-04-05 16:26:21 +0900

    [#8711] Dependency Management Plugin for dependency management like maven

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     // Support convention plugins written in Kotlin. Convention plugins are build scripts in 'src/main' that automatically become available as plugins in the main build.
     `kotlin-dsl`
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
 }
 
 repositories {
