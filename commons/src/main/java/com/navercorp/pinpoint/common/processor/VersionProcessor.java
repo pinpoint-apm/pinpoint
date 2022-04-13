50350b48f0510840e37ad084e4cc1aeb60d29017
Yongseok Kang <feelform@gmail.com>
2022-04-14 00:45:12 +0900

    [#8711] generate Version.java incremental annotation by gradle annotation processor

--- commons/src/main/java/com/navercorp/pinpoint/common/processor/VersionProcessor.java
+++ commons/src/main/java/com/navercorp/pinpoint/common/processor/VersionProcessor.java
@@ -1,13 +1,28 @@
 package com.navercorp.pinpoint.common.processor;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Filer;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.element.TypeElement;
+import javax.tools.JavaFileObject;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.Set;
 
 public class VersionProcessor extends AbstractProcessor {
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        return false;
+        Filer filer = processingEnv.getFiler();
+        try {
+            JavaFileObject serviceRegistry = filer.createSourceFile("ServiceRegistry");
+            Writer writer = serviceRegistry.openWriter();
+            writer.write("public class ServiceRegistry {");
+            writer.write("}");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Version Processor");
+        }
+        return true;
     }
 }
