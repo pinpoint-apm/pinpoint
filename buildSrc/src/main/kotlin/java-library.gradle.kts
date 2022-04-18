6601386d6f12d6c4dee23286c82617ccfe59eedd
Yongseok Kang <feelform@gmail.com>
2022-04-18 18:55:25 +0900

    [#8711] maven publication for pinpoint

--- buildSrc/src/main/kotlin/java-library.gradle.kts
+++ buildSrc/src/main/kotlin/java-library.gradle.kts
@@ -31,8 +31,45 @@ group = "com.navercorp.pinpoint"
 version = "2.4.0-SNAPSHOT"
 
 publishing {
-    publications.create<MavenPublication>("maven") {
+    publications.create<MavenPublication>("pinpoint") {
         from(components["java"])
+        pom {
+            name.set("pinpoint")
+            description.set("Pinpoint APM, Application Performance Management tool for large-scale distributed systems")
+            url.set("https://github.com/pinpoint-apm/pinpoint")
+            licenses {
+                license {
+                    name.set("Apache License, Version 2.0")
+                    url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    distribution.set("repo")
+                }
+            }
+            inceptionYear.set("2012")
+
+            scm {
+                url.set("https://github.com/pinpoint-apm/pinpoint")
+                connection.set("scm:git:git://github.com/pinpoint-apm/pinpoint.git")
+                developerConnection.set("scm:git:ssh://git@github.com/pinpoint-apm/pinpoint.git")
+            }
+            developers {
+                developer {
+                    id.set("emeroad")
+                    name.set("WoonDuk Kang")
+                    email.set("emeroad@gamil.com")
+                    organization.set("NAVER Corp.")
+                    organizationUrl.set("http://www.naver.com")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "ossrh"
+
+            val releasesRepoUrl = "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
+            val snapshotsRepoUrl = "https://oss.sonatype.org/content/repositories/snapshots"
+            url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+        }
     }
 }
 
