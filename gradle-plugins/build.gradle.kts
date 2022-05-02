52e8d3be407d56eca431013e52cb617f6a459d34
Yongseok Kang <feelform@gmail.com>
2022-05-02 19:24:16 +0900

    [#8711] Sharing prebuild script plugins with build logic pinpoint

--- gradle-plugins/build.gradle.kts
+++ gradle-plugins/build.gradle.kts
@@ -15,7 +15,11 @@ dependencies {
 publishing {
     repositories {
         maven {
-            url = uri(layout.buildDirectory.dir("repo"))
+            name = "ossrh"
+
+            val releasesRepoUrl = "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
+            val snapshotsRepoUrl = "https://oss.sonatype.org/content/repositories/snapshots"
+            url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
         }
     }
 }
