3c51ad6692ca6e34dbc8fc0b3a85f253974a525b
emeroad <emeroad@gmail.com>
2022-05-10 10:21:58 +0900

    [#noissue] Cleanup

--- plugins-it/thread-it/src/test/java/com/navercorp/test/pinpoint/plugin/thread/pkg/two/MockRunnable.java
+++ plugins-it/thread-it/src/test/java/com/navercorp/test/pinpoint/plugin/thread/pkg/two/MockRunnable.java
@@ -1,15 +1,12 @@
 package com.navercorp.test.pinpoint.plugin.thread.pkg.two;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class MockRunnable implements Runnable {
-    private final Logger logger = LogManager.getLogger(this.getClass());
     public MockRunnable() {
     }
 
     @Override
     public void run() {
-        logger.info("runnable-----------------");
+        System.out.println("runnable-----------------");
     }
 };
