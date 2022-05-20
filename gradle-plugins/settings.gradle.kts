d2a546a3abb92a03de9989730b0e6f5997ef69ae
Yongseok Kang <feelform@gmail.com>
2022-05-20 23:36:28 +0900

    [#8711] sharing catalogs by version catalog plugin

--- gradle-plugins/settings.gradle.kts
+++ gradle-plugins/settings.gradle.kts
@@ -1,9 +1,1 @@
 rootProject.name="pinpoint-gradle-plugins"
-
-//dependencyResolutionManagement {
-//    versionCatalogs {
-//        create("libs") {
-//            from("com.navercorp.pinpoint:pinpoint-gradle-plugins:2.4.0-SNAPSHOT")
-//        }
-//    }
-//}
