b20fd2cc46b1700f2b421684ec95c3b2f9a198df
Yongseok Kang <feelform@gmail.com>
2022-06-08 19:51:30 +0900

    [#8711] version catalog plugin for sharing

--- hbase/hbase-schema-manager/build.gradle.kts
+++ hbase/hbase-schema-manager/build.gradle.kts
@@ -16,7 +16,7 @@ dependencies {
     implementation(libs.spring.boot.starter.log4j2)
     implementation(libs.spring.tx)
     implementation("org.apache.logging.log4j:log4j-1.2-api:2.14.1")
-    testImplementation("org.springframework.boot:spring-boot-starter-test:${Versions.springBoot}")
+    testImplementation(libs.spring.boot.starter.test)
 
     implementation(libs.hbase.shaded.client) {
         exclude(group = "org.slf4j", module = "slf4j-log4j12")
