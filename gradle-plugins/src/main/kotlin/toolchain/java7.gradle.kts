de75fd2a398a55f6bafa729f9ccea292232a8afe
Yongseok Kang <feelform@gmail.com>
2022-06-08 17:29:48 +0900

    [#8711] version catalog plugin for sharing

--- gradle-plugins/src/main/kotlin/toolchain/java7.gradle.kts
+++ gradle-plugins/src/main/kotlin/toolchain/java7.gradle.kts
@@ -1,7 +1,5 @@
 package com.navercorp.pinpoint.gradle.plugins.toolchain
 
-import gradle.kotlin.dsl.accessors._f4124090576aed771f557cac1ca7fd48.java
-
 plugins {
     id("com.navercorp.pinpoint.gradle.plugins.java-library")
     id("com.navercorp.pinpoint.gradle.plugins.dependency.java7")
