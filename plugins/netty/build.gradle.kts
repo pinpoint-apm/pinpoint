a71ecd6f220268d79c04c5b5384f306d802022bc
Yongseok Kang <feelform@gmail.com>
2022-06-09 11:52:56 +0900

    [#8711] update version by rebase upstream/master 2.5.0-snapshot

--- plugins/netty/build.gradle.kts
+++ plugins/netty/build.gradle.kts
@@ -10,7 +10,13 @@ plugins {
 
 dependencies {
     compileOnly(project(":pinpoint-bootstrap-core"))
-    compileOnly(libs.netty.all)
+    compileOnly(libs.netty.all) {
+        exclude(group = "io.netty", module = "netty-codec-haproxy")
+        exclude(group = "io.netty", module = "netty-codec-smtp")
+        exclude(group = "io.netty", module = "netty-codec-stomp")
+        exclude(group = "io.netty", module = "netty-codec-xml")
+        exclude(group = "io.netty", module = "netty-codec-socks")
+    }
 }
 
 description = "pinpoint-netty-plugin"
