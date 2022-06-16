7a4b5eaa8d92db349385c41c4946e69affe0d49b
jaehong-kim <jaehong.kim@navercorp.com>
2022-06-16 18:21:28 +0900

    [#8930] Update lettuce reactive

---
+++ agent-testweb/redis-lettuce-plugin-testweb/src/main/java/com/pinpoint/test/plugin/RedisRateLimiterConfig.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2022 NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pinpoint.test.plugin;
+
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisRateLimiterConfig {
+    @Bean
+    public RedisRateLimiter redisRateLimiter() {
+        return new RedisRateLimiter(10, 10);
+    }
+}
