fae7eaccaf7bd8e8d5c8c012a41d51f55d539fbb
emeroad <emeroad@gmail.com>
2022-06-22 16:12:05 +0900

    [#noissue] Prepare 2.4.1-SNAPSHOT

--- commons/src/main/java/com/navercorp/pinpoint/common/Version.java
+++ commons/src/main/java/com/navercorp/pinpoint/common/Version.java
@@ -1,4 +1,4 @@
 package com.navercorp.pinpoint.common;
 public final class Version {
-    public static final String VERSION = "2.4.0";
+    public static final String VERSION = "2.4.1-SNAPSHOT";
 }
