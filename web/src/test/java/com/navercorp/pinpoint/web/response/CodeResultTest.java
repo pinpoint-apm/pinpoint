916ccd0d5f0f156a645c000105281e2e8b521abf
ga-ram <galam.choi@navercorp.com>
2022-07-04 18:14:04 +0900

    [#noissue] Add tests

--- web/src/test/java/com/navercorp/pinpoint/web/response/CodeResultTest.java
+++ web/src/test/java/com/navercorp/pinpoint/web/response/CodeResultTest.java
@@ -1,9 +1,8 @@
-package com.navercorp.pinpoint.web.vo;
+package com.navercorp.pinpoint.web.response;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.navercorp.pinpoint.web.response.CodeResult;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Assertions;
@@ -18,10 +17,18 @@ public class CodeResultTest {
     @Test
     public void getMessage_NON_NULL() throws JsonProcessingException {
         CodeResult result = new CodeResult(1);
+        checkOutput(result);
+    }
 
-        String jsonString = mapper.writeValueAsString(result);
+    @Test
+    public void getMessage_static() throws JsonProcessingException{
+        CodeResult result = CodeResult.ok("test message");
+        checkOutput(result);
+    }
+
+    private void checkOutput(CodeResult codeResult) throws JsonProcessingException {
+        String jsonString = mapper.writeValueAsString(codeResult);
         JsonNode jsonNode = mapper.readTree(jsonString);
-
         Assertions.assertNotNull(jsonNode.get("code"));
         Assertions.assertNull(jsonNode.get("result"));
     }
---
+++ web/src/test/java/com/navercorp/pinpoint/web/response/WebhookResponseTest.java
@@ -0,0 +1,28 @@
+package com.navercorp.pinpoint.web.response;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.TextNode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class WebhookResponseTest {
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @Test
+    public void testCreatedMessage() throws JsonProcessingException {
+        WebhookResponse result = new WebhookResponse("SUCCESS", "12345");
+        checkOutput(result);
+    }
+
+    private void checkOutput(WebhookResponse codeResult) throws JsonProcessingException {
+        String jsonString = mapper.writeValueAsString(codeResult);
+        JsonNode jsonNode = mapper.readTree(jsonString);
+        Assertions.assertEquals(TextNode.valueOf("SUCCESS"), jsonNode.get("result"));
+        Assertions.assertNull(jsonNode.get("message"));
+        Assertions.assertEquals(TextNode.valueOf("12345"), jsonNode.get("webhookId"));
+    }
+
+
+}
