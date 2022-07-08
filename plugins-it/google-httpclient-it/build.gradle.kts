4e5277a136b64945444f59c90e2288d7f38f406e
Yongseok Kang <feelform@gmail.com>
2022-07-08 22:06:49 +0900

    [#8711] Fix :pinpoint-google-httpclient-plugin-it test build failure

--- plugins-it/google-httpclient-it/build.gradle.kts
+++ plugins-it/google-httpclient-it/build.gradle.kts
@@ -5,6 +5,10 @@ plugins {
 dependencies {
     api(project(":pinpoint-plugin-it-utils"))
     testImplementation("com.google.http-client:google-http-client:1.20.0")
+    testImplementation(project(":pinpoint-test")) {
+        exclude(group = "org.tinylog", module = "slf4j-tinylog")
+    }
+    testImplementation(libs.nanohttpd)
 }
 
 description = "pinpoint-google-httpclient-plugin-it"
