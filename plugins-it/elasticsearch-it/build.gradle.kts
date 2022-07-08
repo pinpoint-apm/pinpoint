98dd10296635fa7c82527755c06c19ac8f645566
Yongseok Kang <feelform@gmail.com>
2022-07-08 21:38:26 +0900

    [#8711] Fix :pinpoint-elasticsearch-plugin-it test build failure

--- plugins-it/elasticsearch-it/build.gradle.kts
+++ plugins-it/elasticsearch-it/build.gradle.kts
@@ -7,6 +7,9 @@ dependencies {
     testImplementation("org.elasticsearch.client:elasticsearch-rest-high-level-client:7.3.0")
     testImplementation("pl.allegro.tech:embedded-elasticsearch:2.10.0")
     testImplementation(project(":pinpoint-elasticsearch-plugin"))
+    testImplementation(project(":pinpoint-test")) {
+        exclude(group = "org.tinylog", module = "slf4j-tinylog")
+    }
 }
 
 description = "pinpoint-elasticsearch-plugin-it"
