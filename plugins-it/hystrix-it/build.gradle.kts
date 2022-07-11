47abb451da504aba71ad6e8d67663f05853eec3d
Yongseok Kang <feelform@gmail.com>
2022-07-11 16:44:47 +0900

    [#8711] Fix :pinpoint-hystrix-plugin-it test  compile build fails

--- plugins-it/hystrix-it/build.gradle.kts
+++ plugins-it/hystrix-it/build.gradle.kts
@@ -5,6 +5,11 @@ plugins {
 dependencies {
     api(project(":pinpoint-plugin-it-utils"))
     testImplementation("com.netflix.hystrix:hystrix-core:1.5.12")
+    testImplementation(project(":pinpoint-bootstrap-core"))
+    testImplementation(project(":pinpoint-test")) {
+        exclude(group = "org.tinylog", module = "slf4j-tinylog")
+    }
+    testImplementation(libs.log4j.api)
 }
 
 description = "pinpoint-hystrix-plugin-it"
