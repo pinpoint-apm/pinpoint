def42d6af2d9f8c5c49c35c15bcdfad629d5c861
Yongseok Kang <feelform@gmail.com>
2022-07-11 16:39:49 +0900

    [#8711] Fix :pinpoint-httpclient4-plugin-it test  compile build fails

--- plugins-it/httpclient4-it/build.gradle.kts
+++ plugins-it/httpclient4-it/build.gradle.kts
@@ -8,6 +8,10 @@ dependencies {
     testImplementation("org.apache.httpcomponents:httpasyncclient:4.1.4")
     testImplementation("org.apache.httpcomponents:httpcore-nio:4.4.14")
     testImplementation(libs.httpclient)
+    testImplementation(project(":pinpoint-test")) {
+        exclude(group = "org.tinylog", module = "slf4j-tinylog")
+    }
+    testImplementation(libs.nanohttpd)
 }
 
 description = "pinpoint-httpclient4-plugin-it"
--- plugins/httpclient4/build.gradle.kts
+++ plugins/httpclient4/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 dependencies {
     compileOnly(project(":pinpoint-bootstrap-core"))
     compileOnly(libs.httpcore)
-    compileOnly("org.apache.httpcomponents:httpasyncclient:4.1.4")
+    compileOnlyApi("org.apache.httpcomponents:httpasyncclient:4.1.4")
     compileOnly("org.apache.httpcomponents:httpcore-nio:4.4.14")
     compileOnly(libs.httpclient)
     compileOnly("commons-httpclient:commons-httpclient:3.1")
