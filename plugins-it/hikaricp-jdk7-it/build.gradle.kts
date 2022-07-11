ba286762cdb5a683fea2e239dd84f31c4c99f026
Yongseok Kang <feelform@gmail.com>
2022-07-11 15:32:58 +0900

    [#8711] Fix :pinpoint-hikaricp-plugin-jdk7-it test  compile build fails

--- plugins-it/hikaricp-jdk7-it/build.gradle.kts
+++ plugins-it/hikaricp-jdk7-it/build.gradle.kts
@@ -5,6 +5,10 @@ plugins {
 dependencies {
     api(project(":pinpoint-plugin-it-utils"))
     testImplementation("com.zaxxer:HikariCP-java6:2.3.13")
+    testImplementation(project(":pinpoint-bootstrap-core"))
+    testImplementation(project(":pinpoint-test")) {
+        exclude(group = "org.tinylog", module = "slf4j-tinylog")
+    }
 }
 
 description = "pinpoint-hikaricp-plugin-jdk7-it"
