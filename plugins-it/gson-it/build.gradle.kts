5e0d70bfc44472fe36574f4cf01b098fba97c43c
Yongseok Kang <feelform@gmail.com>
2022-07-11 12:07:25 +0900

    [#8711] Fix :pinpoint-gson-plugin-it test  compile build fails

--- plugins-it/gson-it/build.gradle.kts
+++ plugins-it/gson-it/build.gradle.kts
@@ -5,6 +5,10 @@ plugins {
 dependencies {
     api(project(":pinpoint-plugin-it-utils"))
     testImplementation("com.google.code.gson:gson:2.3.1")
+    testImplementation(project(":pinpoint-bootstrap-core"))
+    testImplementation(project(":pinpoint-test")) {
+        exclude(group = "org.tinylog", module = "slf4j-tinylog")
+    }
 }
 
 description = "pinpoint-gson-plugin-it"
