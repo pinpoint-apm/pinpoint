ea6792dc20de5f94a3890d6b1446411a4210e39e
Yongseok Kang <feelform@gmail.com>
2022-07-12 17:42:24 +0900

    [#8711] Fix pinpoint-mybatis-plugin-it test compile build fails

--- plugins-it/mybatis-it/build.gradle.kts
+++ plugins-it/mybatis-it/build.gradle.kts
@@ -7,6 +7,9 @@ dependencies {
     api(project(":pinpoint-plugin-it-utils"))
     testImplementation(libs.mybatis)
     testImplementation(libs.mybatis.spring)
+    testImplementation(project(":pinpoint-test")) {
+        exclude(group = "org.tinylog", module = "slf4j-tinylog")
+    }
 }
 
 description = "pinpoint-mybatis-plugin-it"
