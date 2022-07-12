b734448dd57a306166be6647024da8750d58035d
Yongseok Kang <feelform@gmail.com>
2022-07-12 16:52:32 +0900

    [#8711] Fix mariadb test compile build fails

--- plugins-it/mariadb-jdbc-it/build.gradle.kts
+++ plugins-it/mariadb-jdbc-it/build.gradle.kts
@@ -8,6 +8,10 @@ dependencies {
     testImplementation("org.testcontainers:testcontainers:1.16.2")
     testImplementation("org.testcontainers:mariadb:1.16.2")
     testImplementation(project(":pinpoint-plugin-it-jdbc-test"))
+    testImplementation(project(":pinpoint-test")) {
+        exclude(group = "org.tinylog", module = "slf4j-tinylog")
+    }
+    testImplementation(libs.log4j.api)
 }
 
 description = "pinpoint-mariadb-jdbc-driver-plugin-it"
