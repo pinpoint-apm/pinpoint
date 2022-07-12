290a04a8c0fb619c347e45a171449d2794f4b329
Yongseok Kang <feelform@gmail.com>
2022-07-12 16:05:20 +0900

    [#8711] Fix :pinpoint-kotlin-coroutines-plugin-it test compile build fails

--- plugins-it/kotlin-coroutines-it/build.gradle.kts
+++ plugins-it/kotlin-coroutines-it/build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    kotlin("jvm") version "1.7.10"
     id("com.navercorp.pinpoint.gradle.plugins.toolchain.java8")
 }
 
@@ -6,6 +7,9 @@ dependencies {
     api(project(":pinpoint-plugin-it-utils"))
     testImplementation(project(":pinpoint-kotlin-coroutines-plugin"))
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+    testImplementation(project(":pinpoint-test")) {
+        exclude(group = "org.tinylog", module = "slf4j-tinylog")
+    }
 }
 
 description = "pinpoint-kotlin-coroutines-plugin-it"
