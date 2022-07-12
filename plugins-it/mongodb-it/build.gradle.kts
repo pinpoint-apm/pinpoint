25ca3d4dd0db10f075c7aa20fcf49d456f6dd72e
Yongseok Kang <feelform@gmail.com>
2022-07-12 17:09:53 +0900

    [#8711] Fix pinpoint-mongodb-driver-plugin test compile build fails

--- plugins-it/mongodb-it/build.gradle.kts
+++ plugins-it/mongodb-it/build.gradle.kts
@@ -7,7 +7,10 @@ dependencies {
     testImplementation(project(":pinpoint-mongodb-driver-plugin"))
     testImplementation("org.mongodb:bson:3.7.0")
     testImplementation("org.mongodb:mongodb-driver:3.9.0")
-    testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo:1.50.5")
+    testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo:3.4.6")
+    testImplementation(project(":pinpoint-test")) {
+        exclude(group = "org.tinylog", module = "slf4j-tinylog")
+    }
 }
 
 description = "pinpoint-mongodb-plugin-it"
--- plugins/mongodb/build.gradle.kts
+++ plugins/mongodb/build.gradle.kts
@@ -7,8 +7,8 @@ dependencies {
     testImplementation(libs.log4j.slf4j.impl.jdk7)
     testImplementation(libs.log4j.core.jdk7)
     testImplementation(libs.jackson.databind)
-    compileOnly(project(":pinpoint-bootstrap-core"))
-    compileOnly("org.mongodb:mongodb-driver:3.9.0")
+    compileOnlyApi(project(":pinpoint-bootstrap-core"))
+    compileOnlyApi("org.mongodb:mongodb-driver:3.9.0")
 }
 
 description = "pinpoint-mongodb-driver-plugin"
