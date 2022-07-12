8ae7a55fce4408456a46c6e66a5e263fea10c7f5
Yongseok Kang <feelform@gmail.com>
2022-07-12 16:10:54 +0900

    [#8711] Fix :pinpoint-lambda-it test compile build fails

--- plugins-it/lambda-it/build.gradle.kts
+++ plugins-it/lambda-it/build.gradle.kts
@@ -5,6 +5,10 @@ plugins {
 dependencies {
     testImplementation(libs.spring.context)
     testImplementation(project(":pinpoint-plugin-it-utils"))
+    testImplementation(project(":pinpoint-bootstrap-core"))
+    testImplementation(project(":pinpoint-test")) {
+        exclude(group = "org.tinylog", module = "slf4j-tinylog")
+    }
 }
 
 description = "pinpoint-lambda-it"
