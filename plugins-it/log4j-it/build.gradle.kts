c8eb0f76f093e2c61923ad61204460874bd5bcbc
Yongseok Kang <feelform@gmail.com>
2022-07-12 16:12:21 +0900

    [#8711] Fix :pinpoint-log4j-plugin-it test compile build fails

--- plugins-it/log4j-it/build.gradle.kts
+++ plugins-it/log4j-it/build.gradle.kts
@@ -5,6 +5,9 @@ plugins {
 dependencies {
     api(project(":pinpoint-plugin-it-utils"))
     testImplementation(libs.log4j)
+    testImplementation(project(":pinpoint-test")) {
+        exclude(group = "org.tinylog", module = "slf4j-tinylog")
+    }
 }
 
 description = "pinpoint-log4j-plugin-it"
