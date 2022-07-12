42f26175ea7069abae12bd02f66561318f86e8e4
Yongseok Kang <feelform@gmail.com>
2022-07-12 16:39:03 +0900

    [#8711] Fix pinpoint-logback-plugin-it test compile build fails

--- plugins-it/logback-it/build.gradle.kts
+++ plugins-it/logback-it/build.gradle.kts
@@ -5,6 +5,10 @@ plugins {
 dependencies {
     api(project(":pinpoint-plugin-it-utils"))
     testImplementation("ch.qos.logback:logback-classic:1.2.3")
+    testImplementation(project(":pinpoint-test")) {
+        exclude(group = "org.tinylog", module = "slf4j-tinylog")
+    }
+    testImplementation(libs.log4j.api)
 }
 
 description = "pinpoint-logback-plugin-it"
