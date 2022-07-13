08b375f5d959ce7e723721f05af89a05be69d5fc
Yongseok Kang <feelform@gmail.com>
2022-07-13 14:02:48 +0900

    [#8711] Fix pinpoint-postgresql-jdbc-driver-plugin-it test compile build fails

--- plugins-it/postgresql-jdbc-it/build.gradle.kts
+++ plugins-it/postgresql-jdbc-it/build.gradle.kts
@@ -11,6 +11,10 @@ dependencies {
     testImplementation("org.postgresql:postgresql:9.4.1212.jre6")
     testImplementation(project(":pinpoint-postgresql-jdbc-driver-plugin"))
     testImplementation(project(":pinpoint-profiler-test"))
+    testImplementation(project(":pinpoint-test")) {
+        exclude(group = "org.tinylog", module = "slf4j-tinylog")
+    }
+    testImplementation(libs.log4j.api)
 }
 
 description = "pinpoint-postgresql-jdbc-driver-plugin-it"
