811c4ef89eb29c2c2e5404ed96a0f5d2f9367919
Yongseok Kang <feelform@gmail.com>
2022-07-13 19:51:02 +0900

    [#8711] Fix pinpoint-rxjava-plugin-it test compile build fails

--- plugins-it/rxjava-it/build.gradle.kts
+++ plugins-it/rxjava-it/build.gradle.kts
@@ -5,6 +5,10 @@ plugins {
 dependencies {
     api(project(":pinpoint-plugin-it-utils"))
     testImplementation("io.reactivex:rxjava:1.2.0")
+    testImplementation(project(":pinpoint-test")) {
+        exclude(group = "org.tinylog", module = "slf4j-tinylog")
+    }
+    testImplementation(libs.log4j.api)
 }
 
 description = "pinpoint-rxjava-plugin-it"
