05ab0309024e29501ad7048efd434288532bbcbb
Yongseok Kang <feelform@gmail.com>
2022-07-13 13:54:24 +0900

    [#8711] Fix pinpoint-oracle-jdbc-driver-plugin-it test compile build fails

--- plugins-it/oracledb-it/build.gradle.kts
+++ plugins-it/oracledb-it/build.gradle.kts
@@ -7,6 +7,10 @@ dependencies {
     api(project(":pinpoint-plugin-it-utils"))
     testImplementation(project(":pinpoint-oracle-jdbc-driver-plugin"))
     testImplementation("org.testcontainers:oracle-xe:1.16.2")
+    testImplementation(project(":pinpoint-test")) {
+        exclude(group = "org.tinylog", module = "slf4j-tinylog")
+    }
+    testImplementation(libs.log4j.api)
 }
 
 description = "pinpoint-oracle-jdbc-driver-plugin-it"
