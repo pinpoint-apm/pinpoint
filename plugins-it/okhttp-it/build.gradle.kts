fd914ac27282b1919b01280e9667b30b61232fa6
Yongseok Kang <feelform@gmail.com>
2022-07-13 11:55:44 +0900

    [#8711] Fix pinpoint-okhttp-plugin-it test compile build fails

--- plugins-it/okhttp-it/build.gradle.kts
+++ plugins-it/okhttp-it/build.gradle.kts
@@ -7,6 +7,10 @@ dependencies {
     testImplementation("com.squareup.okhttp:okhttp:2.5.0")
     testImplementation("com.squareup.okhttp3:okhttp:3.8.1")
     testImplementation(project(":pinpoint-okhttp-plugin"))
+    testImplementation(project(":pinpoint-test")) {
+        exclude(group = "org.tinylog", module = "slf4j-tinylog")
+    }
+    testImplementation(libs.nanohttpd)
 }
 
 description = "pinpoint-okhttp-plugin-it"
