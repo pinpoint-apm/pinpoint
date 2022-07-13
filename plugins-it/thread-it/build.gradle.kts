27cbc63c5b23feb6386c795b1cad7b3ec770bb11
Yongseok Kang <feelform@gmail.com>
2022-07-13 20:01:37 +0900

    [#8711] Fix pinpoint-thread-plugin-it test compile build fails

--- plugins-it/thread-it/build.gradle.kts
+++ plugins-it/thread-it/build.gradle.kts
@@ -4,6 +4,9 @@ plugins {
 
 dependencies {
     api(project(":pinpoint-plugin-it-utils"))
+    testImplementation(project(":pinpoint-test")) {
+        exclude(group = "org.tinylog", module = "slf4j-tinylog")
+    }
 }
 
 description = "pinpoint-thread-plugin-it"
