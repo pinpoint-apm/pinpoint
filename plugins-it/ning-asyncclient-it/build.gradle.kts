6958f0fab94eb0b198b5b6057db60c59d04124d3
Yongseok Kang <feelform@gmail.com>
2022-07-13 11:53:12 +0900

    [#8711] Fix pinpoint-ning-aysncclient-plugin-it test compile build fails

--- plugins-it/ning-asyncclient-it/build.gradle.kts
+++ plugins-it/ning-asyncclient-it/build.gradle.kts
@@ -5,6 +5,10 @@ plugins {
 dependencies {
     implementation("com.ning:async-http-client:1.8.3")
     api(project(":pinpoint-plugin-it-utils"))
+    testImplementation(project(":pinpoint-test")) {
+        exclude(group = "org.tinylog", module = "slf4j-tinylog")
+    }
+    testImplementation(libs.nanohttpd)
 }
 
 description = "pinpoint-ning-aysncclient-plugin-it"
