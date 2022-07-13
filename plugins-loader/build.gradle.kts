eade99eb8854911ab91d419e6a9fbb5b251da998
Yongseok Kang <feelform@gmail.com>
2022-07-13 13:58:30 +0900

    [#8711] Fix pinpoint-plugins-loader test compile build fails

--- plugins-loader/build.gradle.kts
+++ plugins-loader/build.gradle.kts
@@ -7,7 +7,7 @@ dependencies {
     implementation(libs.log4j.api.jdk7)
     testImplementation(libs.log4j.slf4j.impl.jdk7)
     testImplementation(libs.log4j.core.jdk7)
-    compileOnly(project(":pinpoint-commons"))
+    compileOnlyApi(project(":pinpoint-commons"))
     compileOnly(project(":pinpoint-bootstrap-core"))
 }
 
