9a2a2f79ca7472fe43317fc2b1ce3159fe43207c
Yongseok Kang <feelform@gmail.com>
2022-07-13 19:59:56 +0900

    [#8711] Fix pinpoint-spring-plugin test compile build fails

--- plugins-it/spring-it/build.gradle.kts
+++ plugins-it/spring-it/build.gradle.kts
@@ -8,6 +8,10 @@ dependencies {
     testImplementation(libs.spring4.context)
     testImplementation(libs.spring4.webmvc)
     testImplementation(libs.javax.servlet.api.v3)
+    testImplementation(project(":pinpoint-test")) {
+        exclude(group = "org.tinylog", module = "slf4j-tinylog")
+    }
+    testImplementation(libs.nanohttpd)
 }
 
 description = "pinpoint-spring-plugin-it"
--- plugins/spring/build.gradle.kts
+++ plugins/spring/build.gradle.kts
@@ -7,6 +7,8 @@ dependencies {
     compileOnly(project(":pinpoint-bootstrap-core"))
     compileOnly(libs.spring4.context)
     compileOnly(libs.spring4.web)
+    testCompileOnly(libs.spring4.beans)
+    testImplementation(libs.log4j.api.jdk7)
 }
 
 description = "pinpoint-spring-plugin"
