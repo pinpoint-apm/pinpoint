788697f50a316dbdb99eccaf63629f70d8802749
Yongseok Kang <feelform@gmail.com>
2022-07-13 13:56:34 +0900

    [#8711] Fix pinpoint-paho-mqtt-plugin-it test compile build fails

--- plugins-it/paho-mqtt-it/build.gradle.kts
+++ plugins-it/paho-mqtt-it/build.gradle.kts
@@ -7,6 +7,10 @@ dependencies {
     testImplementation("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.2.5")
     testImplementation("org.eclipse.paho:org.eclipse.paho.mqttv5.client:1.2.5")
     testImplementation("org.testcontainers:testcontainers:1.16.2")
+    testImplementation(project(":pinpoint-test")) {
+        exclude(group = "org.tinylog", module = "slf4j-tinylog")
+    }
+    testImplementation(libs.log4j.api)
 }
 
 description = "pinpoint-paho-mqtt-plugin-it"
