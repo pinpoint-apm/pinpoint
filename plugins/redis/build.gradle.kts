d0f9d5bfe5e1394b8eb3bceec77f6f873bfd2f59
Yongseok Kang <feelform@gmail.com>
2022-07-13 19:42:43 +0900

    [#8711] Fix pinpoint-redis-plugin test compile build fails

--- plugins/redis/build.gradle.kts
+++ plugins/redis/build.gradle.kts
@@ -6,7 +6,7 @@ dependencies {
     testImplementation(project(":pinpoint-profiler"))
     testImplementation(project(":pinpoint-profiler-test"))
     compileOnly(project(":pinpoint-bootstrap-core"))
-    compileOnly("redis.clients:jedis:2.4.2")
+    compileOnlyApi("redis.clients:jedis:2.4.2")
 }
 
 description = "pinpoint-redis-plugin"
