f8c2d2357945ff667310d63281763f739216bd59
BillionaireDY <doyoonkim1002@gmail.com>
2022-08-04 13:33:47 +0900

    FEAT: ApplicationSelector

--- web/src/main/web-frontend/apps/web/pages/_app.tsx
+++ web/src/main/web-frontend/apps/web/pages/_app.tsx
@@ -1,15 +1,17 @@
 import { ReactElement, ReactNode, useEffect } from 'react'
 import { NextPage } from 'next';
-import type { AppContext, AppProps } from 'next/app'
 import Script from 'next/script'
-import { IconContext } from 'react-icons';
+import type { AppContext, AppProps } from 'next/app'
 import App from 'next/app';
+import { IconContext } from 'react-icons';
+
 // import { SessionProvider } from "next-auth/react"
 
 import { GlobalStyle } from '@pinpoint-fe/common/components/Styled/GlobalStyle';
 import { RootThemeProvider } from '@pinpoint-fe/common/styles/theme';
 import themeLight from '@pinpoint-fe/common/styles/theme-light';
 import themeDark from '@pinpoint-fe/common/styles/theme-dark';
+import { SWRConfig } from 'swr';
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P,IP> & {
   getLayout?: (page: ReactElement) => ReactNode
@@ -29,9 +31,14 @@ const AppRoot =  ({ Component, pageProps: { session, pageProps} }: AppPropsWithL
       <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
         {/* <SessionProvider session={session}> */}
         <RootThemeProvider theme={themeLight}>
-          <div className="content">
-            {getLayout(<Component {...pageProps} />)}
-          </div>
+          <SWRConfig value={{
+            suspense: true,
+            fetcher: (...args: any) => fetch(args as any).then(res => res.json())
+          }}>
+            <div className="content">
+              {getLayout(<Component {...pageProps} />)}
+            </div>
+          </SWRConfig>
         </RootThemeProvider>
         {/* </SessionProvider> */}
       </IconContext.Provider>
---
+++ web/src/main/web-frontend/packages/common/components/Application/ListItemSkeleton.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+
+import { StyledItem } from './ApplicationList';
+
+export interface ListItemSkeletonProps {
+  count?: number;
+}
+
+const ListItemSkeleton = ({
+  count = 3
+}: ListItemSkeletonProps) => {
+  return (
+    <>
+    {[...Array(count)].map((_, i) => 
+      <StyledItem key={i}><Skeleton /></StyledItem>
+    )}
+    </>
+  );
+};
+
+export default ListItemSkeleton;
