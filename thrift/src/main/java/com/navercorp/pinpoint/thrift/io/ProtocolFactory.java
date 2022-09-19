776f17a2c74bc9b030de0bfdebc05445c69d1da9
youngjin.kim2 <youngjin.kim2@navercorp.com>
2022-09-19 16:33:59 +0900

    [#noissue] Increased thrift container max length

--- thrift/src/main/java/com/navercorp/pinpoint/thrift/io/ProtocolFactory.java
+++ thrift/src/main/java/com/navercorp/pinpoint/thrift/io/ProtocolFactory.java
@@ -5,7 +5,7 @@ import org.apache.thrift.protocol.TProtocolFactory;
 
 public final class ProtocolFactory {
 
-    private static final TProtocolFactory FACTORY = new TCompactProtocol.Factory(1024 * 60, 128);
+    private static final TProtocolFactory FACTORY = new TCompactProtocol.Factory(1024 * 60, 4096);
 
     public static TProtocolFactory getFactory() {
         return FACTORY;
