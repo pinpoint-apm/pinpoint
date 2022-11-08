23a8e55c278b4c8a60b60cd2cdbc397f41268154
ga-ram <galam.choi@navercorp.com>
2022-11-08 18:08:12 +0900

    [#9393] Return full range of timestamps and fill empty values to zero

--- metric-module/metric/src/main/java/com/navercorp/pinpoint/metric/web/controller/UriStatController.java
+++ metric-module/metric/src/main/java/com/navercorp/pinpoint/metric/web/controller/UriStatController.java
@@ -80,6 +80,6 @@ public class UriStatController {
             uriStats = uriStatService.getCollectedUriStatAgent(builder.build());
         }
 
-        return new UriStatView(uri, uriStats);
+        return new UriStatView(uri, timeWindow, uriStats);
     }
 }
--- metric-module/metric/src/main/java/com/navercorp/pinpoint/metric/web/model/UriStatGroup.java
+++ metric-module/metric/src/main/java/com/navercorp/pinpoint/metric/web/model/UriStatGroup.java
@@ -19,6 +19,7 @@ package com.navercorp.pinpoint.metric.web.model;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.navercorp.pinpoint.metric.common.model.StringPrecondition;
 import com.navercorp.pinpoint.metric.common.model.UriStat;
+import com.navercorp.pinpoint.metric.web.util.TimeWindow;
 import com.navercorp.pinpoint.metric.web.view.TimeSeriesValueView;
 import com.navercorp.pinpoint.metric.web.view.TimeseriesValueGroupView;
 import org.apache.commons.lang3.StringUtils;
@@ -27,6 +28,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class UriStatGroup implements TimeseriesValueGroupView {
@@ -41,9 +43,9 @@ public class UriStatGroup implements TimeseriesValueGroupView {
         this.values = Collections.emptyList();
     }
 
-    public UriStatGroup(String uri, List<UriStat> uriStats) {
+    public UriStatGroup(String uri, int dataSize, TimeWindow timeWindow, List<UriStat> uriStats) {
         this.uri = uri;
-        this.values = UriStatValue.createValueList(uriStats);
+        this.values = UriStatValue.createValueList(dataSize, timeWindow, uriStats);
     }
 
     @Override
@@ -61,22 +63,31 @@ public class UriStatGroup implements TimeseriesValueGroupView {
         private final String fieldName;
         private final List<Integer> values;
 
-        public static List<TimeSeriesValueView> createValueList(List<UriStat> uriStats) {
+        public static List<TimeSeriesValueView> createValueList(int dataSize, TimeWindow timeWindow,  List<UriStat> uriStats) {
             Objects.requireNonNull(uriStats);
+
             List<TimeSeriesValueView> values = new ArrayList<>();
 
             final int bucketSize = uriStats.get(0).getTotalHistogram().length;
-            for (int i = 0 ; i < bucketSize; i++) {
-                final int histogramIndex = i;
-                values.add(new UriStatValue(FIELD_PREFIX + histogramIndex,
-                        uriStats.stream().map(e -> e.getTotalHistogram()[histogramIndex]).collect(Collectors.toList())));
+
+            for (int histogramIndex = 0 ; histogramIndex < bucketSize; histogramIndex++) {
+                int[] filledData = new int[dataSize];
+
+                for (UriStat uriStat : uriStats) {
+                    int filledIndex = timeWindow.getWindowIndex(uriStat.getTimestamp());
+                    if (filledData[filledIndex] != 0) {
+                        throw new RuntimeException("Uri stat timestamp mismatch.");
+                    }
+                    filledData[filledIndex] = uriStat.getTotalHistogram()[histogramIndex];
+                }
+                values.add(new UriStatValue(FIELD_PREFIX + histogramIndex, filledData));
             }
             return values;
         }
 
-        public UriStatValue(String fieldName, List<Integer> values) {
+        public UriStatValue(String fieldName, int[] uriStats) {
             this.fieldName = StringPrecondition.requireHasLength(fieldName, "fieldName");
-            this.values = Objects.requireNonNull(values);
+            this.values = Arrays.stream(uriStats).boxed().collect(Collectors.toList());
         }
 
         @Override
