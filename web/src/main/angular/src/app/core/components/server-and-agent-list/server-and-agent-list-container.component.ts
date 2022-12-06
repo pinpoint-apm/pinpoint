631b5caf317c60be7200c3ea974f8860092a6c0c
QooQooDass <bindong.kim@navercorp.com>
2022-12-06 18:42:44 +0900

    [#noissue] Remove invalid filter logic

--- web/src/main/angular/src/app/core/components/server-and-agent-list/server-and-agent-list-container.component.ts
+++ web/src/main/angular/src/app/core/components/server-and-agent-list/server-and-agent-list-container.component.ts
@@ -65,9 +65,10 @@ export class ServerAndAgentListContainerComponent implements OnInit, OnDestroy {
                 tap((urlService: NewUrlStateNotificationService) => {
                     this.agentId = urlService.getPathValue(UrlPathId.AGENT_ID);
                 }),
-                filter((urlService: NewUrlStateNotificationService) => {
-                    return urlService.isValueChanged(UrlPathId.APPLICATION) || urlService.isValueChanged(UrlPathId.PERIOD) || urlService.isValueChanged(UrlPathId.END_TIME);
-                }),
+                // TODO: Check valid filter for url
+                // filter((urlService: NewUrlStateNotificationService) => {
+                //     return urlService.isValueChanged(UrlPathId.APPLICATION) || urlService.isValueChanged(UrlPathId.PERIOD) || urlService.isValueChanged(UrlPathId.END_TIME);
+                // }),
                 map((urlService: NewUrlStateNotificationService) => {
                     if (urlService.isRealTimeMode()) {
                         const to = urlService.getUrlServerTimeData();
