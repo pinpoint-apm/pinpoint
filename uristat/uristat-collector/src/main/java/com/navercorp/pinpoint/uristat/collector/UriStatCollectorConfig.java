eb3009f75cdf328e58544fcf53f0545453c13a77
Kim YoungJin <smilup2244@gmail.com>
2023-01-11 15:23:24 +0900

    [#9603] Changed path of property source for uri collector into profile-based

--- uristat/uristat-collector/src/main/java/com/navercorp/pinpoint/uristat/collector/UriStatCollectorConfig.java
+++ uristat/uristat-collector/src/main/java/com/navercorp/pinpoint/uristat/collector/UriStatCollectorConfig.java
@@ -8,7 +8,10 @@ import org.springframework.context.annotation.PropertySource;
 
 @Profile("metric")
 @ComponentScan({"com.navercorp.pinpoint.uristat.collector.service", "com.navercorp.pinpoint.uristat.collector.dao"})
-@PropertySource({"classpath:kafka-topic.properties", "classpath:kafka-producer-factory.properties"})
+@PropertySource({UriStatCollectorConfig.KAFKA_TOPIC_PROPERTIES, UriStatCollectorConfig.KAFKA_PRODUCER_FACTORY_PROPERTIES})
 @ImportResource({"classpath*:**/applicationContext-collector-metric-namespace.xml", "classpath:applicationContext-collector-pinot-kafka.xml"})
 public class UriStatCollectorConfig {
+    public static final String KAFKA_TOPIC_PROPERTIES = "classpath:profiles/${pinpoint.profiles.active}/kafka-topic.properties";
+
+    public static final String KAFKA_PRODUCER_FACTORY_PROPERTIES = "classpath:profiles/${pinpoint.profiles.active}/kafka-producer-factory.properties";
 }
