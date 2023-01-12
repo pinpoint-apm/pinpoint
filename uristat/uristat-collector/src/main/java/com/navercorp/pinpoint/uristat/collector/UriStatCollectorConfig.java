dad8e43de929b1c0ede53d339c39703e29ee5ec9
Seoyoung Park <se0y0ung.park@navercorp.com>
2023-01-12 17:00:33 +0900

    [#9603] add configuration and change filename

--- uristat/uristat-collector/src/main/java/com/navercorp/pinpoint/uristat/collector/UriStatCollectorConfig.java
+++ uristat/uristat-collector/src/main/java/com/navercorp/pinpoint/uristat/collector/UriStatCollectorConfig.java
@@ -2,14 +2,16 @@ package com.navercorp.pinpoint.uristat.collector;
 
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 
 @Profile("metric")
+@Configuration
+@ImportResource({"classpath*:**/applicationContext-collector-metric-namespace.xml", "classpath:applicationContext-collector-uristat-pinot-kafka.xml"})
 @ComponentScan({"com.navercorp.pinpoint.uristat.collector.service", "com.navercorp.pinpoint.uristat.collector.dao"})
 @PropertySource({UriStatCollectorConfig.KAFKA_TOPIC_PROPERTIES, UriStatCollectorConfig.KAFKA_PRODUCER_FACTORY_PROPERTIES})
-@ImportResource({"classpath*:**/applicationContext-collector-metric-namespace.xml", "classpath:applicationContext-collector-pinot-kafka.xml"})
 public class UriStatCollectorConfig {
     public static final String KAFKA_TOPIC_PROPERTIES = "classpath:profiles/${pinpoint.profiles.active}/kafka-topic.properties";
 
--- uristat/uristat-web/src/main/java/com/navercorp/pinpoint/uristat/web/UriStatWebConfig.java
+++ uristat/uristat-web/src/main/java/com/navercorp/pinpoint/uristat/web/UriStatWebConfig.java
@@ -1,9 +1,11 @@
 package com.navercorp.pinpoint.uristat.web;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.Profile;
 
+@Configuration
 @ImportResource({"classpath:applicationContext-web-uristat.xml"})
 @Import(UriStatWebPropertySources.class)
 @Profile("metric")
