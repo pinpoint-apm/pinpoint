60d7e50e038a7064c5950b3a7f04789bfc9181ee
Jaehong Kim <jaehong.kim@navercorp.com>
2023-01-17 11:29:12 +0900

    [#8666] Fix redis lettuce connection problem

--- plugins/redis-lettuce/src/main/java/com/navercorp/pinpoint/plugin/redis/lettuce/interceptor/RedisCluserClientConnectStatefulInterceptor.java
+++ plugins/redis-lettuce/src/main/java/com/navercorp/pinpoint/plugin/redis/lettuce/interceptor/RedisCluserClientConnectStatefulInterceptor.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2018 NAVER Corp.
+ * Copyright 2023 NAVER Corp.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -21,16 +21,10 @@ import com.navercorp.pinpoint.bootstrap.context.TraceContext;
 import com.navercorp.pinpoint.bootstrap.interceptor.AroundInterceptor;
 import com.navercorp.pinpoint.bootstrap.logging.PLogger;
 import com.navercorp.pinpoint.bootstrap.logging.PLoggerFactory;
+import com.navercorp.pinpoint.common.util.ArrayArgumentUtils;
 import com.navercorp.pinpoint.plugin.redis.lettuce.EndPointAccessor;
-import io.lettuce.core.ConnectionFuture;
-import io.lettuce.core.cluster.StatefulRedisClusterConnectionImpl;
 
-import java.util.concurrent.CompletableFuture;
-
-/**
- * @author jaehong.kim
- */
-public class AttachEndPointInterceptor implements AroundInterceptor {
+public class RedisCluserClientConnectStatefulInterceptor implements AroundInterceptor {
 
     private final PLogger logger = PLoggerFactory.getLogger(this.getClass());
     private final boolean isDebug = logger.isDebugEnabled();
@@ -38,7 +32,7 @@ public class AttachEndPointInterceptor implements AroundInterceptor {
     private final TraceContext traceContext;
     private final MethodDescriptor methodDescriptor;
 
-    public AttachEndPointInterceptor(final TraceContext traceContext, final MethodDescriptor methodDescriptor) {
+    public RedisCluserClientConnectStatefulInterceptor(final TraceContext traceContext, final MethodDescriptor methodDescriptor) {
         this.traceContext = traceContext;
         this.methodDescriptor = methodDescriptor;
     }
@@ -60,12 +54,12 @@ public class AttachEndPointInterceptor implements AroundInterceptor {
             if (Boolean.FALSE == (target instanceof EndPointAccessor)) {
                 return;
             }
-            if (Boolean.FALSE == (result instanceof EndPointAccessor)) {
-                return;
+            final EndPointAccessor endPointAccessor = ArrayArgumentUtils.getArgument(args, 0, EndPointAccessor.class);
+            if (endPointAccessor != null) {
+                // Attach endPoint
+                final String endPoint = ((EndPointAccessor) target)._$PINPOINT$_getEndPoint();
+                endPointAccessor._$PINPOINT$_setEndPoint(endPoint);
             }
-            // Attach endPoint
-            final String endPoint = ((EndPointAccessor) target)._$PINPOINT$_getEndPoint();
-            ((EndPointAccessor) result)._$PINPOINT$_setEndPoint(endPoint);
         } catch (Throwable t) {
             if (logger.isWarnEnabled()) {
                 logger.warn("Failed to AFTER process. {}", t.getMessage(), t);
