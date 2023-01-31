d39aafc098368c6f58c72204875280815bc7e4cb
donghun-cho <donghun.cho@navercorp.com>
2023-01-31 17:55:45 +0900

    [#9599] spring-framework 6 support

--- plugins/spring/src/main/java/com/navercorp/pinpoint/plugin/spring/web/jakarta/interceptor/LookupHandlerMethodInterceptor.java
+++ plugins/spring/src/main/java/com/navercorp/pinpoint/plugin/spring/web/jakarta/interceptor/LookupHandlerMethodInterceptor.java
@@ -1,4 +1,4 @@
-package com.navercorp.pinpoint.plugin.spring.web.interceptor;
+package com.navercorp.pinpoint.plugin.spring.web.jakarta.interceptor;
 
 import com.navercorp.pinpoint.bootstrap.context.SpanRecorder;
 import com.navercorp.pinpoint.bootstrap.context.Trace;
@@ -10,16 +10,14 @@ import com.navercorp.pinpoint.common.util.ArrayArgumentUtils;
 import com.navercorp.pinpoint.common.util.StringUtils;
 import com.navercorp.pinpoint.plugin.spring.web.SpringWebMvcConstants;
 
-import javax.servlet.ServletRequest;
+import jakarta.servlet.ServletRequest;
 
-public class ProcessRequestInterceptor implements AroundInterceptor {
+public class LookupHandlerMethodInterceptor implements AroundInterceptor {
     private final PLogger logger = PLoggerFactory.getLogger(getClass());
     private final TraceContext traceContext;
-    private final Boolean uriStatUseUserInput;
 
-    public ProcessRequestInterceptor(final TraceContext traceContext, Boolean uriStatUseUserInput) {
+    public LookupHandlerMethodInterceptor(final TraceContext traceContext) {
         this.traceContext = traceContext;
-        this.uriStatUseUserInput = uriStatUseUserInput;
     }
 
     @Override
@@ -28,22 +26,18 @@ public class ProcessRequestInterceptor implements AroundInterceptor {
 
     @Override
     public void after(Object target, Object[] args, Object result, Throwable throwable) {
-        if (Boolean.FALSE == uriStatUseUserInput) {
-            return;
-        }
-
         final Trace trace = traceContext.currentRawTraceObject();
         if (trace == null) {
             return;
         }
 
         try {
-            final ServletRequest request = ArrayArgumentUtils.getArgument(args, 0, ServletRequest.class);
+            final ServletRequest request = ArrayArgumentUtils.getArgument(args, 1, ServletRequest.class);
             if (request != null) {
-                final String uri = ServletRequestAttributeUtils.extractAttribute(request, SpringWebMvcConstants.SPRING_MVC_URI_USER_INPUT_ATTRIBUTE_KEYS);
+                final String uri = ServletRequestAttributeUtils.extractAttribute(request, SpringWebMvcConstants.SPRING_MVC_DEFAULT_URI_ATTRIBUTE_KEYS);
                 if (StringUtils.hasLength(uri)) {
                     final SpanRecorder spanRecorder = trace.getSpanRecorder();
-                    spanRecorder.recordUriTemplate(uri, true);
+                    spanRecorder.recordUriTemplate(uri);
                 }
             }
         } catch (Throwable th) {
---
+++ plugins/spring/src/main/java/com/navercorp/pinpoint/plugin/spring/web/jakarta/interceptor/ProcessRequestInterceptor.java
@@ -0,0 +1,55 @@
+package com.navercorp.pinpoint.plugin.spring.web.jakarta.interceptor;
+
+import com.navercorp.pinpoint.bootstrap.context.SpanRecorder;
+import com.navercorp.pinpoint.bootstrap.context.Trace;
+import com.navercorp.pinpoint.bootstrap.context.TraceContext;
+import com.navercorp.pinpoint.bootstrap.interceptor.AroundInterceptor;
+import com.navercorp.pinpoint.bootstrap.logging.PLogger;
+import com.navercorp.pinpoint.bootstrap.logging.PLoggerFactory;
+import com.navercorp.pinpoint.common.util.ArrayArgumentUtils;
+import com.navercorp.pinpoint.common.util.StringUtils;
+import com.navercorp.pinpoint.plugin.spring.web.SpringWebMvcConstants;
+
+import jakarta.servlet.ServletRequest;
+
+public class ProcessRequestInterceptor implements AroundInterceptor {
+    private final PLogger logger = PLoggerFactory.getLogger(getClass());
+    private final TraceContext traceContext;
+    private final Boolean uriStatUseUserInput;
+
+    public ProcessRequestInterceptor(final TraceContext traceContext, Boolean uriStatUseUserInput) {
+        this.traceContext = traceContext;
+        this.uriStatUseUserInput = uriStatUseUserInput;
+    }
+
+    @Override
+    public void before(Object target, Object[] args) {
+    }
+
+    @Override
+    public void after(Object target, Object[] args, Object result, Throwable throwable) {
+        if (Boolean.FALSE == uriStatUseUserInput) {
+            return;
+        }
+
+        final Trace trace = traceContext.currentRawTraceObject();
+        if (trace == null) {
+            return;
+        }
+
+        try {
+            final ServletRequest request = ArrayArgumentUtils.getArgument(args, 0, ServletRequest.class);
+            if (request != null) {
+                final String uri = ServletRequestAttributeUtils.extractAttribute(request, SpringWebMvcConstants.SPRING_MVC_URI_USER_INPUT_ATTRIBUTE_KEYS);
+                if (StringUtils.hasLength(uri)) {
+                    final SpanRecorder spanRecorder = trace.getSpanRecorder();
+                    spanRecorder.recordUriTemplate(uri, true);
+                }
+            }
+        } catch (Throwable th) {
+            if (logger.isWarnEnabled()) {
+                logger.warn("AFTER error. Caused:{}", th.getMessage(), th);
+            }
+        }
+    }
+}
