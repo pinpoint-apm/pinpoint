a8c3302354ec896bfb0d0eb8e3e79262c7116d2b
minwoo-jung <jungmwiner@gmail.com>
2023-03-07 19:58:15 +0900

    [#9724] Move the null check logic to sql.

--- metric-module/metric/src/main/java/com/navercorp/pinpoint/metric/web/service/SystemMetricHostInfoServiceImpl.java
+++ metric-module/metric/src/main/java/com/navercorp/pinpoint/metric/web/service/SystemMetricHostInfoServiceImpl.java
@@ -52,7 +52,6 @@ public class SystemMetricHostInfoServiceImpl implements SystemMetricHostInfoServ
     @Override
     public List<String> getHostGroupNameList(String tenantId) {
         List<String> hostGroupNameList = systemMetricHostInfoDao.selectHostGroupNameList(tenantId);
-        hostGroupNameList.remove(null);
         hostGroupNameList.sort(Comparator.naturalOrder());
 
         return hostGroupNameList;
