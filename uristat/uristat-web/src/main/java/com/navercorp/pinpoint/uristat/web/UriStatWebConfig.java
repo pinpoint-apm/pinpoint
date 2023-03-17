c6547ff771b7209d3fac0796277390ae005bf3c0
emeroad <emeroad@gmail.com>
2023-03-17 16:50:51 +0900

    [#9765] Refactor MyBatis configuration

--- uristat/uristat-web/src/main/java/com/navercorp/pinpoint/uristat/web/UriStatWebConfig.java
+++ uristat/uristat-web/src/main/java/com/navercorp/pinpoint/uristat/web/UriStatWebConfig.java
@@ -2,14 +2,15 @@ package com.navercorp.pinpoint.uristat.web;
 
 
 import com.navercorp.pinpoint.pinot.config.PinotConfiguration;
+import com.navercorp.pinpoint.uristat.web.config.UriStatPinotDaoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@ImportResource({"classpath:applicationContext-web-uristat.xml"})
-@Import({UriStatWebPropertySources.class, PinotConfiguration.class})
+@ComponentScan(basePackages = "com.navercorp.pinpoint.uristat.web")
+@Import({UriStatWebPropertySources.class, UriStatPinotDaoConfiguration.class, PinotConfiguration.class})
 @Profile("uri")
 public class UriStatWebConfig {
 }
