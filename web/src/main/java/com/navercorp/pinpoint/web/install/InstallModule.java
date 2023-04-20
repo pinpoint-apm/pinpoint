bd2a24edb13d64a2cf8b29d15689d8e568fa0aaf
emeroad <emeroad@gmail.com>
2023-04-20 14:59:10 +0900

    [#noissue] Apply ConditionalOnProperty to InstallModule

--- web/src/main/java/com/navercorp/pinpoint/web/install/InstallModule.java
+++ web/src/main/java/com/navercorp/pinpoint/web/install/InstallModule.java
@@ -1,14 +1,16 @@
 package com.navercorp.pinpoint.web.install;
 
 import com.navercorp.pinpoint.web.install.dao.AgentDownloadInfoDao;
-import com.navercorp.pinpoint.web.install.dao.AgentDownloadInfoDaoFactoryBean;
+import com.navercorp.pinpoint.web.install.dao.GithubAgentDownloadInfoDao;
+import com.navercorp.pinpoint.web.install.dao.MemoryAgentDownloadInfoDao;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.Assert;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -27,14 +29,19 @@ public class InstallModule {
     }
 
     @Bean
-    public FactoryBean<AgentDownloadInfoDao> agentDownloadInfoDao(
+    @ConditionalOnProperty(value = "pinpoint.modules.web.install.type", havingValue = "url")
+    public AgentDownloadInfoDao urlAgentDownloadInfoDao(
             @Value("${web.installation.pinpointVersion:}") String version,
-            @Value("${web.installation.downloadUrl:}") String downloadUrl,
-            RestTemplate restTemplate) {
-        AgentDownloadInfoDaoFactoryBean factoryBean = new AgentDownloadInfoDaoFactoryBean();
-        factoryBean.setVersion(version);
-        factoryBean.setDownloadUrl(downloadUrl);
-        factoryBean.setRestTemplate(restTemplate);
-        return factoryBean;
+            @Value("${web.installation.downloadUrl:}") String downloadUrl) {
+        Assert.hasLength(version, "version");
+        Assert.hasLength(downloadUrl, "downloadUrl");
+        return new MemoryAgentDownloadInfoDao(version, downloadUrl);
+
+    }
+
+    @Bean
+    @ConditionalOnProperty(value = "pinpoint.modules.web.install.type", havingValue = "github", matchIfMissing = true)
+    public AgentDownloadInfoDao githubAgentDownloadInfoDao(RestTemplate restTemplate) {
+        return new GithubAgentDownloadInfoDao(restTemplate);
     }
 }
