7eb36419798214939f2644c8f890cc55f322e154
seoyoung-park <se0y0ung.park@navercorp.com>
2023-05-22 17:20:39 +0900

    [#9893] Align Log4j2 loggingsystem lookup orders
    https://logging.apache.org/log4j/2.x/manual/configuration.html#automatic-configuration

--- profiler-logging/src/main/java/com/navercorp/pinpoint/profiler/logging/Log4j2LoggingSystem.java
+++ profiler-logging/src/main/java/com/navercorp/pinpoint/profiler/logging/Log4j2LoggingSystem.java
@@ -23,7 +23,10 @@ public class Log4j2LoggingSystem implements LoggingSystem {
     public static final String FACTORY_PROPERTY_NAME = "log4j2.loggerContextFactory";
     public static final String NOLOOKUPS = "log4j2.formatMsgNoLookups";
 
-    private static final String[] LOOKUP = {"log4j2-test.xml", "log4j2-agent.xml", "log4j2.properties"};
+    private static final String[] LOOKUP = {
+            "log4j2-test.properties", "log4j2-test.xml",
+            "log4j2-agent.properties", "log4j2-agent.xml",
+    };
 
     private LoggerContext loggerContext;
     private final Path profilePath;
