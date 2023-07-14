0b860c22e756f21c8b616962fe57be3e194b51a9
donghun-cho <donghun.cho@navercorp.com>
2023-07-14 16:06:59 +0900

    [#noissue] remove unused code

--- web/src/main/java/com/navercorp/pinpoint/web/controller/ApdexScoreController.java
+++ web/src/main/java/com/navercorp/pinpoint/web/controller/ApdexScoreController.java
@@ -7,9 +7,7 @@ import com.navercorp.pinpoint.web.service.ApplicationFactory;
 import com.navercorp.pinpoint.web.util.TimeWindow;
 import com.navercorp.pinpoint.web.util.TimeWindowSampler;
 import com.navercorp.pinpoint.web.util.TimeWindowSlotCentricSampler;
-import com.navercorp.pinpoint.web.view.InspectorView;
 import com.navercorp.pinpoint.web.vo.Application;
-import com.navercorp.pinpoint.web.vo.stat.chart.InspectorData;
 import com.navercorp.pinpoint.web.vo.stat.chart.StatChart;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -115,67 +113,5 @@ public class ApdexScoreController {
         Application application = applicationFactory.createApplicationByTypeName(applicationId, serviceTypeName);
 
         return apdexScoreService.selectAgentChart(application, range, timeWindow, agentId);
-    }
-
-    @GetMapping(value = "/getApplicationStat/apdexScore/inspectorView")
-    public InspectorView getApplicationApdexScoreView(
-            @RequestParam("applicationName") @NotBlank String applicationName,
-            @RequestParam("serviceTypeCode") Short serviceTypeCode,
-            @RequestParam("from") @PositiveOrZero long from,
-            @RequestParam("to") @PositiveOrZero long to) {
-        final Range range = Range.between(from, to);
-        TimeWindow timeWindow = new TimeWindow(range, APDEX_SCORE_TIME_WINDOW_SAMPLER);
-
-        Application application = applicationFactory.createApplication(applicationName, serviceTypeCode);
-
-        InspectorData inspectorData = apdexScoreService.selectApplicationInspectorData(application, range, timeWindow);
-        return new InspectorView(inspectorData);
-    }
-
-    @GetMapping(value = "/getApplicationStat/apdexScore/inspectorView", params = "serviceTypeName")
-    public InspectorView getApplicationApdexScoreView(
-            @RequestParam("applicationName") @NotBlank String applicationName,
-            @RequestParam("serviceTypeName") @NotBlank String serviceTypeName,
-            @RequestParam("from") @PositiveOrZero long from,
-            @RequestParam("to") @PositiveOrZero long to) {
-        final Range range = Range.between(from, to);
-        TimeWindow timeWindow = new TimeWindow(range, APDEX_SCORE_TIME_WINDOW_SAMPLER);
-
-        Application application = applicationFactory.createApplicationByTypeName(applicationName, serviceTypeName);
-
-        InspectorData inspectorData = apdexScoreService.selectApplicationInspectorData(application, range, timeWindow);
-        return new InspectorView(inspectorData);
-    }
-
-    @GetMapping(value = "/getAgentStat/apdexScore/inspectorView")
-    public InspectorView getAgentApdexScoreView(
-            @RequestParam("applicationName") @NotBlank String applicationName,
-            @RequestParam("serviceTypeCode") Short serviceTypeCode,
-            @RequestParam("agentId") @NotBlank String agentId,
-            @RequestParam("from") @PositiveOrZero long from,
-            @RequestParam("to") @PositiveOrZero long to) {
-        final Range range = Range.between(from, to);
-        TimeWindow timeWindow = new TimeWindow(range, APDEX_SCORE_TIME_WINDOW_SAMPLER);
-
-        Application application = applicationFactory.createApplication(applicationName, serviceTypeCode);
-
-        InspectorData inspectorData = apdexScoreService.selectAgentInspectorData(application, range, timeWindow, agentId);
-        return new InspectorView(inspectorData);
-    }
-
-    @GetMapping(value = "/getAgentStat/apdexScore/inspectorView", params = "serviceTypeName")
-    public InspectorView getAgentApdexScoreView(
-            @RequestParam("applicationName") @NotBlank String applicationName,
-            @RequestParam("serviceTypeName") @NotBlank String serviceTypeName,
-            @RequestParam("agentId") @NotBlank String agentId,
-            @RequestParam("from") @PositiveOrZero long from,
-            @RequestParam("to") @PositiveOrZero long to) {
-        final Range range = Range.between(from, to);
-        TimeWindow timeWindow = new TimeWindow(range, APDEX_SCORE_TIME_WINDOW_SAMPLER);
-
-        Application application = applicationFactory.createApplicationByTypeName(applicationName, serviceTypeName);
-
-        InspectorData inspectorData = apdexScoreService.selectAgentInspectorData(application, range, timeWindow, agentId);
-        return new InspectorView(inspectorData);
     }
 }
--- web/src/main/java/com/navercorp/pinpoint/web/vo/stat/chart/application/ApplicationApdexScoreChart.java
+++ web/src/main/java/com/navercorp/pinpoint/web/vo/stat/chart/application/ApplicationApdexScoreChart.java
@@ -3,8 +3,6 @@ package com.navercorp.pinpoint.web.vo.stat.chart.application;
 import com.navercorp.pinpoint.web.util.TimeWindow;
 import com.navercorp.pinpoint.web.vo.chart.Point;
 import com.navercorp.pinpoint.web.vo.stat.chart.ChartGroupBuilder;
-import com.navercorp.pinpoint.web.vo.stat.chart.InspectorData;
-import com.navercorp.pinpoint.web.vo.stat.chart.InspectorDataBuilder;
 import com.navercorp.pinpoint.web.vo.stat.chart.StatChartGroup;
 
 import java.util.List;
@@ -19,23 +17,10 @@ public class ApplicationApdexScoreChart extends DefaultApplicationChart<DoubleAp
     }
 
     private static final ChartGroupBuilder<DoubleApplicationStatPoint, ApplicationStatPoint<Double>> BUILDER = newChartBuilder();
-    private static final InspectorDataBuilder<DoubleApplicationStatPoint, ApplicationStatPoint<Double>> INSPECTOR_VIEW_DATA_BUILDER = newViewBuilder();
 
     static ChartGroupBuilder<DoubleApplicationStatPoint, ApplicationStatPoint<Double>> newChartBuilder() {
         ChartGroupBuilder<DoubleApplicationStatPoint, ApplicationStatPoint<Double>> builder = new ChartGroupBuilder<>(UNCOLLECTED_POINT);
         builder.addPointFunction(ApdexScoreChartType.APDEX_SCORE, ApplicationApdexScoreChart::newApdexScorePoint);
-        return builder;
-    }
-
-    static InspectorDataBuilder<DoubleApplicationStatPoint, ApplicationStatPoint<Double>> newViewBuilder() {
-        InspectorDataBuilder<DoubleApplicationStatPoint, ApplicationStatPoint<Double>> builder = new InspectorDataBuilder<>(UNCOLLECTED_POINT, "applicationApdexScore", "noUnit");
-        builder.addValueFunction("min", ApplicationStatPoint::getYValForMin);
-        builder.addValueFunction("minAgentId", ApplicationStatPoint::getAgentIdForMin);
-        builder.addValueFunction("max", ApplicationStatPoint::getYValForMax);
-        builder.addValueFunction("maxAgentId", ApplicationStatPoint::getAgentIdForMax);
-        builder.addValueFunction("avg", ApplicationStatPoint::getYValForAvg);
-        builder.addPointFunction(ApdexScoreChartType.APDEX_SCORE, ApplicationApdexScoreChart::newApdexScorePoint);
-
         return builder;
     }
 
@@ -45,9 +30,5 @@ public class ApplicationApdexScoreChart extends DefaultApplicationChart<DoubleAp
 
     private static ApplicationStatPoint<Double> newApdexScorePoint(DoubleApplicationStatPoint apdexScore) {
         return apdexScore;
-    }
-
-    public InspectorData getInspectorData(TimeWindow timeWindow, List<DoubleApplicationStatPoint> statList) {
-        return INSPECTOR_VIEW_DATA_BUILDER.build(timeWindow, statList);
     }
 }
