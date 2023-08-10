b7b86013602085714bfb7218816029b855d67f32
emeroad <emeroad@gmail.com>
2023-08-10 19:04:44 +0900

    [#noissue] Change xml config to java config in flink module

---
+++ flink/src/main/java/com/navercorp/pinpoint/flink/FlinkModule.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2023 NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.navercorp.pinpoint.flink;
+
+import com.navercorp.pinpoint.common.server.cluster.zookeeper.config.ClusterConfigurationFactory;
+import com.navercorp.pinpoint.flink.cache.FlinkCacheConfiguration;
+import com.navercorp.pinpoint.flink.dao.hbase.ApplicationDaoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ComponentScan(basePackages = {
+        "com.navercorp.pinpoint.common.server.util"
+})
+@ImportResource({
+        "classpath:applicationContext-flink.xml",
+
+        "classpath:applicationContext-flink-profile.xml",
+        "classpath:applicationContext-flink-extend.xml",
+        "classpath:applicationContext-flink-clean.xml",
+        "classpath:applicationContext-hbase.xml",
+})
+@Import({
+        FlinkCacheConfiguration.class,
+        ApplicationDaoConfiguration.class,
+
+        ClusterConfigurationFactory.class
+})
+public class FlinkModule {
+
+}
