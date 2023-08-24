70d8b4256c33732405f7ebcbcfe10facef3dcc85
emeroad <emeroad@gmail.com>
2023-08-24 19:39:44 +0900

    [#10246] Fix missing leave() of ScopedInterceptor when an exception occurs

--- bootstraps/bootstrap-core/src/main/java/com/navercorp/pinpoint/bootstrap/interceptor/scope/ScopedInterceptor5.java
+++ bootstraps/bootstrap-core/src/main/java/com/navercorp/pinpoint/bootstrap/interceptor/scope/ScopedInterceptor5.java
@@ -57,8 +57,11 @@ public class ScopedInterceptor5 implements AroundInterceptor5 {
         final InterceptorScopeInvocation transaction = scope.getCurrentInvocation();
         
         if (transaction.canLeave(policy)) {
-            this.interceptor.after(target, arg0, arg1, arg2, arg3, arg4, result, throwable);
-            transaction.leave(policy);
+            try {
+                this.interceptor.after(target, arg0, arg1, arg2, arg3, arg4, result, throwable);
+            } finally {
+                transaction.leave(policy);
+            }
         } else {
             if (debugEnabled) {
                 logger.debug("tryAfter() returns false: interceptorScopeTransaction: {}, executionPoint: {}. Skip interceptor {}", transaction, policy, interceptor.getClass());
