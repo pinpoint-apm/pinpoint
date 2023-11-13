da5d079f784131b65cf934c0b75026e35e30f545
emeroad <emeroad@gmail.com>
2023-11-13 16:10:32 +0900

    [#10504] Geometry filter query in the mongodb is not traced

--- plugins/mongodb/src/test/java/com/navercorp/pinpoint/plugin/mongo/WriteContextTest.java
+++ plugins/mongodb/src/test/java/com/navercorp/pinpoint/plugin/mongo/WriteContextTest.java
@@ -1,8 +1,10 @@
 package com.navercorp.pinpoint.plugin.mongo;
 
+import com.mongodb.client.model.Filters;
 import com.navercorp.pinpoint.common.util.BytesUtils;
 import org.bson.BsonBinary;
 import org.bson.BsonDocument;
+import org.bson.conversions.Bson;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +12,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Base64;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -34,5 +37,20 @@ public class WriteContextTest {
         byte[] bytes = "12345678".getBytes(StandardCharsets.UTF_8);
         byte[] sourceByes = Arrays.copyOf(Base64.getEncoder().encode(bytes), WriteContext.DEFAULT_ABBREVIATE_MAX_WIDTH);
         Assertions.assertEquals(BytesUtils.toString(sourceByes), nopadInput);
+    }
+
+
+    @Test
+    void parse_geometry() {
+        List<String> parameter = new ArrayList<>();
+        WriteContext context = new WriteContext(parameter, true, true);
+
+        BsonDocument bson = new BsonDocument();
+        BsonBinary binary = new BsonBinary("123456789".getBytes(StandardCharsets.UTF_8));
+        bson.append("bson", binary);
+
+        Bson geo = Filters.geoIntersects("geo", bson);
+        // touch GeometryOperatorFilter line
+        Assertions.assertThrows(ClassCastException.class, () -> context.parse(Collections.singletonList(geo)));
     }
 }
