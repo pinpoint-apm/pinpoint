b35ad47e6676f4cf808e30aa6fca2ef81d760dd5
wangj881 <wangj881@chinaunicom.cn>
2023-12-08 15:02:55 +0900

    add clickhouse plugin

---
+++ plugins/clickhouse-jdbc/src/main/java/com/navercorp/pinpoint/plugin/jdbc/clickhouse/ClickHouseConfig.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2014 NAVER Corp.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.navercorp.pinpoint.plugin.jdbc.clickhouse;
+
+import com.navercorp.pinpoint.bootstrap.config.ProfilerConfig;
+import com.navercorp.pinpoint.bootstrap.plugin.jdbc.JdbcConfig;
+
+/**
+ * @author Jongho Moon
+ */
+public class ClickHouseConfig extends JdbcConfig {
+    private final boolean profileSetAutoCommit;
+    private final boolean profileCommit;
+    private final boolean profileRollback;
+
+    public ClickHouseConfig(ProfilerConfig config) {
+        super(config.readBoolean("profiler.jdbc.mysql", false),
+                config.readBoolean("profiler.jdbc.mysql.tracesqlbindvalue", config.isTraceSqlBindValue()),
+                config.getMaxSqlBindValueSize());
+        this.profileSetAutoCommit = config.readBoolean("profiler.jdbc.mysql.setautocommit", false);
+        this.profileCommit = config.readBoolean("profiler.jdbc.mysql.commit", false);
+        this.profileRollback = config.readBoolean("profiler.jdbc.mysql.rollback", false);
+    }
+
+    public boolean isProfileSetAutoCommit() {
+        return profileSetAutoCommit;
+    }
+
+    public boolean isProfileCommit() {
+        return profileCommit;
+    }
+
+    public boolean isProfileRollback() {
+        return profileRollback;
+    }
+
+    @Override
+    public String toString() {
+        return "ClickHouseConfig [" + super.toString() + ", profileSetAutoCommit=" + profileSetAutoCommit + ", profileCommit=" + profileCommit + ", profileRollback=" + profileRollback + "]";
+    }
+}
---
+++ plugins/clickhouse-jdbc/src/main/java/com/navercorp/pinpoint/plugin/jdbc/clickhouse/interceptor/ClickHouseConnectionCreateInterceptor.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2014 NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.navercorp.pinpoint.plugin.jdbc.clickhouse.interceptor;
+
+import java.util.Arrays;
+
+import com.navercorp.pinpoint.bootstrap.context.DatabaseInfo;
+import com.navercorp.pinpoint.bootstrap.context.SpanEventRecorder;
+import com.navercorp.pinpoint.bootstrap.context.Trace;
+import com.navercorp.pinpoint.bootstrap.context.TraceContext;
+import com.navercorp.pinpoint.bootstrap.interceptor.AroundInterceptor;
+import com.navercorp.pinpoint.bootstrap.logging.PLogger;
+import com.navercorp.pinpoint.bootstrap.logging.PLoggerFactory;
+import com.navercorp.pinpoint.bootstrap.plugin.jdbc.DatabaseInfoAccessor;
+import com.navercorp.pinpoint.bootstrap.plugin.jdbc.DefaultDatabaseInfo;
+import com.navercorp.pinpoint.bootstrap.util.InterceptorUtils;
+import com.navercorp.pinpoint.plugin.jdbc.clickhouse.ClickHouseConstants;
+import ru.yandex.clickhouse.settings.ClickHouseProperties;
+
+/**
+ * @author emeroad
+ */
+// #1375 Workaround java level Deadlock
+// https://oss.navercorp.com/pinpoint/pinpoint-naver/issues/1375
+// @TargetConstructor({ "java.lang.String", "int", "java.util.Properties", "java.lang.String", "java.lang.String" })
+public class ClickHouseConnectionCreateInterceptor implements AroundInterceptor {
+
+    private final PLogger logger = PLoggerFactory.getLogger(this.getClass());
+    private final boolean isDebug = logger.isDebugEnabled();
+
+    private final TraceContext traceContext;
+
+    public ClickHouseConnectionCreateInterceptor(TraceContext traceContext) {
+        this.traceContext = traceContext;
+    }
+
+    @Override
+    public void after(Object target, Object[] args, Object result, Throwable throwable) {
+        if (isDebug) {
+            logger.afterInterceptor(target, args, result, throwable);
+        }
+        if (args == null ) {
+            return;
+        }
+
+        final String url = getString(args[0]);
+        DatabaseInfo databaseInfo = null;
+
+
+        if(args[1] instanceof ClickHouseProperties){
+            ClickHouseProperties clickHouseProperties=(ClickHouseProperties)args[1];
+            String databaseId=clickHouseProperties.getDatabase();
+
+            if (url != null && databaseId != null) {
+
+                String tmpURL=url.substring(url.lastIndexOf("/")+1);
+                // It's dangerous to use this url directly
+                databaseInfo = new DefaultDatabaseInfo(ClickHouseConstants.CLICK_HOUSE, ClickHouseConstants.CLICK_HOUSE_EXECUTE_QUERY, tmpURL, tmpURL, Arrays.asList(tmpURL), databaseId);
+                if (InterceptorUtils.isSuccess(throwable)) {
+                    // Set only if connection is success.
+                    if (target instanceof DatabaseInfoAccessor) {
+                        ((DatabaseInfoAccessor) target)._$PINPOINT$_setDatabaseInfo(databaseInfo);
+                    }
+                }
+            }
+        }else {
+            return;
+        }
+
+
+
+        final Trace trace = traceContext.currentTraceObject();
+        if (trace == null) {
+            return;
+        }
+
+        SpanEventRecorder recorder = trace.currentSpanEventRecorder();
+        // We must do this if current transaction is being recorded.
+        if (databaseInfo != null) {
+            recorder.recordServiceType(databaseInfo.getType());
+            recorder.recordEndPoint(databaseInfo.getMultipleHost());
+            recorder.recordDestinationId(databaseInfo.getDatabaseId());
+        }
+
+    }
+    
+    private DatabaseInfo createDatabaseInfo(String url, Integer port, String databaseId) {
+        if (url.indexOf(':') == -1) {
+            url += ":" + port;
+        }
+
+        DatabaseInfo databaseInfo = new DefaultDatabaseInfo(ClickHouseConstants.CLICK_HOUSE, ClickHouseConstants.CLICK_HOUSE_EXECUTE_QUERY, url, url, Arrays.asList(url), databaseId);
+        return databaseInfo;
+
+    }
+
+    private String getString(Object value) {
+        if (value instanceof String) {
+            return (String) value;
+        }
+        return null;
+    }
+
+    private Integer getInteger(Object value) {
+        if (value instanceof Integer) {
+            return (Integer) value;
+        }
+        return null;
+    }
+
+    @Override
+    public void before(Object target, Object[] args) {
+
+    }
+}
