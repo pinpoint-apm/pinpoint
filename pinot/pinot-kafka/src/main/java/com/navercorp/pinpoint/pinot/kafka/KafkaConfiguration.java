b6c26f8498e18a638f770dadbf45f2e58f25662c
emeroad <emeroad@gmail.com>
2024-02-01 18:22:50 +0900

    [#10531] Cleanup kafka configuration

--- pinot/pinot-kafka/src/main/java/com/navercorp/pinpoint/pinot/kafka/KafkaConfiguration.java
+++ pinot/pinot-kafka/src/main/java/com/navercorp/pinpoint/pinot/kafka/KafkaConfiguration.java
@@ -1,66 +1,60 @@
 package com.navercorp.pinpoint.pinot.kafka;
 
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.kafka.DefaultKafkaProducerFactoryCustomizer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.ProducerFactory;
 
-import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Consumer;
 
 @Configuration
-@PropertySource("classpath:/profiles/${pinpoint.profiles.active:release}/kafka-producer-factory.properties")
+@PropertySource(value = {
+        "classpath:/kafka-root.properties",
+        "classpath:/profiles/${pinpoint.profiles.active:release}/kafka-producer-factory.properties"
+})
+@EnableConfigurationProperties
 public class KafkaConfiguration {
 
     private final Logger logger = LogManager.getLogger(KafkaConfiguration.class);
 
-    public Map<String, Object> toConfig(KafkaProperties properties) {
-        Map<String, Object> config = new HashMap<>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getBootstrapServers());
-
-        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, properties.getKeySerializer());
-        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, properties.getValueSerializer());
-
-        config.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, properties.getPartitionerClass());
-        config.put(ProducerConfig.ACKS_CONFIG, properties.getAcks());
-        config.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, properties.getCompressionType());
-
-        return config;
+    public KafkaConfiguration() {
+        logger.info("Install {}", KafkaConfiguration.class);
     }
 
     @Bean
-    public ProducerFactory kafkaProducerFactory(KafkaProperties properties) {
-        logger.info("kafka {}:{}", ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getBootstrapServers());
-        logger.debug("kafka config:{}", properties);
+    public ProducerFactory<?, ?> kafkaProducerFactory(
+            ObjectProvider<DefaultKafkaProducerFactoryCustomizer> customizers) {
 
-        Map<String, Object> config = toConfig(properties);
-        return new DefaultKafkaProducerFactory<>(config);
-    }
+        KafkaProperties properties = kafkaProperties();
+        // ref : KafkaAutoConfiguration
+        Map<String, Object> producerProperties = properties.buildProducerProperties();
+        logger.info("ProducerProperties:{}", producerProperties);
 
-    @Bean
-    public KafkaProperties kafkaProperties(Environment env) {
-        KafkaProperties properties = new KafkaProperties();
+        DefaultKafkaProducerFactory<?, ?> factory = new DefaultKafkaProducerFactory<>(producerProperties);
 
-        bindProperties(env, "pinpoint.metric.kafka.bootstrap.servers", properties::setBootstrapServers);
-        bindProperties(env, "pinpoint.metric.kafka.key.serializer", properties::setKeySerializer);
-        bindProperties(env, "pinpoint.metric.kafka.value.serializer", properties::setValueSerializer);
-        bindProperties(env, "pinpoint.metric.kafka.acks", properties::setAcks);
-        bindProperties(env, "pinpoint.metric.kafka.compressionType", properties::setCompressionType);
-
-        return properties;
-    }
-
-    private void bindProperties(Environment env, String key, Consumer<String> consumer) {
-        String value = env.getProperty(key);
-        if (value != null) {
-            consumer.accept(value);
+        KafkaProperties.Producer producer = properties.getProducer();
+        String transactionIdPrefix = producer.getTransactionIdPrefix();
+        if (transactionIdPrefix != null) {
+            factory.setTransactionIdPrefix(transactionIdPrefix);
         }
+
+        customizers.orderedStream().forEach((customizer) -> customizer.customize(factory));
+        return factory;
     }
+
+    @Bean
+    @ConfigurationProperties(prefix = "pinpoint.metric.kafka")
+    public KafkaProperties kafkaProperties() {
+        return new KafkaProperties();
+    }
+
 
 }
