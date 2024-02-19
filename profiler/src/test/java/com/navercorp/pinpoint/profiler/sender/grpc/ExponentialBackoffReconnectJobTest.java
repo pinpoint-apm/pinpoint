583d7c4ec728d830d7e2178a66dd38e85aa7350b
emeroad <emeroad@gmail.com>
2024-02-19 19:27:19 +0900

    [#10701] Refactor SpanGrpcDataSender
    [#10701] Refactor SpanGrpcDataSender

--- profiler/src/test/java/com/navercorp/pinpoint/profiler/sender/grpc/ExponentialBackoffReconnectJobTest.java
+++ profiler/src/test/java/com/navercorp/pinpoint/profiler/sender/grpc/ExponentialBackoffReconnectJobTest.java
@@ -1,17 +1,19 @@
 package com.navercorp.pinpoint.profiler.sender.grpc;
 
+import io.github.resilience4j.core.IntervalFunction;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-
+@Disabled
 class ExponentialBackoffReconnectJobTest {
 
     private final Logger logger = LogManager.getLogger(this.getClass());
 
     @Test
-    void backoff() {
+    void exponentialBackoff() {
         ExponentialBackoffReconnectJob job = new ExponentialBackoffReconnectJob(
                 () -> logger.info("run()"), 1000, 3000);
 
@@ -20,6 +22,25 @@ class ExponentialBackoffReconnectJobTest {
         }
 
         Assertions.assertEquals(3000, job.nextInterval());
+    }
+
+
+    @Test
+    void random() {
+        IntervalFunction interval = IntervalFunction.ofRandomized(3000, 0.1);
+        for (int i = 0; i < 10; i++) {
+            logger.debug("{}", interval.apply(i +1 ));
+        }
+
+    }
+
+
+    @Test
+    void default_policy() {
+        IntervalFunction interval = IntervalFunction.of(3000);
+        for (int i = 0; i < 10; i++) {
+            logger.debug("{}", interval.apply(i +1 ));
+        }
 
     }
 }
