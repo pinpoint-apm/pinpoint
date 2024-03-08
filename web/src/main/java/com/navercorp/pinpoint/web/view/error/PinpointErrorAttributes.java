c4ee41a87a6d10688b012580a7bff8d940c7fda3
ga-ram <galam.choi@navercorp.com>
2024-03-08 19:05:18 +0900

    [#10739] Add hostname to PinpointErrorData for pinpoint web

--- web/src/main/java/com/navercorp/pinpoint/web/view/error/PinpointErrorAttributes.java
+++ web/src/main/java/com/navercorp/pinpoint/web/view/error/PinpointErrorAttributes.java
@@ -1,13 +1,24 @@
 package com.navercorp.pinpoint.web.view.error;
 
+import com.navercorp.pinpoint.common.util.OsType;
+import com.navercorp.pinpoint.common.util.OsUtils;
 import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.WebRequest;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Map;
 
 @Component
 public class PinpointErrorAttributes extends DefaultErrorAttributes {
+    public static final String ERROR_HOST_NAME = "UNKNOWN-HOST";
+    private final String hostname;
+
+    public PinpointErrorAttributes() {
+        this.hostname = getHostName();
+    }
 
     @Override
     public Map<String, Object> getErrorAttributes(WebRequest webRequest, ErrorAttributeOptions options) {
@@ -17,7 +28,34 @@ public class PinpointErrorAttributes extends DefaultErrorAttributes {
     }
 
     private void addCustomData(WebRequest webRequest, Map<String, Object> errorAttributes) {
-        PinpointErrorData pinpointErrorData = new PinpointErrorData(webRequest);
+        PinpointErrorData pinpointErrorData = new PinpointErrorData(this.hostname, webRequest);
         errorAttributes.put("data", pinpointErrorData);
+    }
+
+    private String getHostName() {
+        final String hostName = getHostNameFromEnv();
+        if (hostName != null) {
+            return hostName;
+        }
+
+        return getHostNameFromDns();
+    }
+
+    private String getHostNameFromEnv() {
+        final OsType type = OsUtils.getType();
+        if (OsType.WINDOW == type) {
+            return System.getenv("COMPUTERNAME");
+        } else {
+            return System.getenv("HOSTNAME");
+        }
+    }
+
+    private String getHostNameFromDns() {
+        try {
+            final InetAddress localHost = InetAddress.getLocalHost();
+            return localHost.getHostName();
+        } catch (UnknownHostException e) {
+            return ERROR_HOST_NAME;
+        }
     }
 }
