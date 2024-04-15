4de9e250b028e068342103edd2e66d62843007f1
emeroad <emeroad@gmail.com>
2024-04-15 20:17:30 +0900

    [#noissue] Add timeout to Elasticsearch IT

--- agent-module/plugins-it/elasticsearch-it/elasticsearch-6-it/src/test/java/com/navercorp/pinpoint/it/plugin/elasticsearch/ElasticsearchIT_7_0_x_IT.java
+++ agent-module/plugins-it/elasticsearch-it/elasticsearch-6-it/src/test/java/com/navercorp/pinpoint/it/plugin/elasticsearch/ElasticsearchIT_7_0_x_IT.java
@@ -26,10 +26,8 @@ import com.navercorp.pinpoint.test.plugin.PinpointAgent;
 import com.navercorp.pinpoint.test.plugin.PluginTest;
 import com.navercorp.pinpoint.test.plugin.shared.SharedDependency;
 import com.navercorp.pinpoint.test.plugin.shared.SharedTestLifeCycleClass;
-import org.apache.http.HttpHost;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.RequestOptions;
-import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.jupiter.api.AfterEach;
@@ -53,9 +51,11 @@ public class ElasticsearchIT_7_0_x_IT extends ElasticsearchITBase {
     @BeforeEach
     public void setup() {
         ES_PORT = Integer.parseInt(System.getProperty("PORT"));
-        restHighLevelClient = new RestHighLevelClient(
-                RestClient.builder(
-                        new HttpHost(getEsHost(), getEsPort(), "http")));
+
+        RestClientBuilderOption option = new RestClientBuilderOption(getEsHost(), getEsPort());
+        // deprecated
+        option.setMaxRetryTimeoutMillis(0);
+        restHighLevelClient = new RestHighLevelClient(option.build());
     }
 
     @AfterEach
