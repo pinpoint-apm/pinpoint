a05f58cef00b84ad9714a88df20090483cdfbb0a
emeroad <emeroad@gmail.com>
2024-05-22 14:49:28 +0900

    [#noissue] Refactor LinkHistogramView

---
+++ web/src/main/java/com/navercorp/pinpoint/web/view/LinkHistogramSummaryView.java
@@ -0,0 +1,33 @@
+package com.navercorp.pinpoint.web.view;
+
+import com.navercorp.pinpoint.web.applicationmap.histogram.ApplicationTimeHistogram;
+import com.navercorp.pinpoint.web.applicationmap.histogram.Histogram;
+import com.navercorp.pinpoint.web.applicationmap.histogram.TimeHistogramFormat;
+import com.navercorp.pinpoint.web.applicationmap.link.LinkHistogramSummary;
+import com.navercorp.pinpoint.web.applicationmap.link.LinkName;
+
+import java.util.List;
+import java.util.Objects;
+
+public class LinkHistogramSummaryView {
+    private final LinkHistogramSummary linkHistogramSummary;
+    private final TimeHistogramFormat format;
+
+    public LinkHistogramSummaryView(LinkHistogramSummary linkHistogramSummary, TimeHistogramFormat format) {
+        this.linkHistogramSummary = Objects.requireNonNull(linkHistogramSummary, "linkHistogramSummary");
+        this.format = Objects.requireNonNull(format, "format");
+    }
+
+    public LinkName getKey() {
+        return linkHistogramSummary.getLinkName();
+    }
+
+    public Histogram getHistogram() {
+        return linkHistogramSummary.getHistogram();
+    }
+
+    public List<TimeViewModel> getTimeSeriesHistogram() {
+        ApplicationTimeHistogram histogram = linkHistogramSummary.getLinkApplicationTimeHistogram();
+        return histogram.createViewModel(format);
+    }
+}
