661b389c71f2cc77d64c0b6c7ebe16b92f237f8d
emeroad <emeroad@gmail.com>
2024-05-27 17:32:18 +0900

    [#11050] Extract FilterView from FilteredServerMap

--- web/src/main/java/com/navercorp/pinpoint/web/applicationmap/SimpleApplicationMap.java
+++ web/src/main/java/com/navercorp/pinpoint/web/applicationmap/SimpleApplicationMap.java
@@ -1,7 +1,9 @@
 package com.navercorp.pinpoint.web.applicationmap;
 
+import com.navercorp.pinpoint.common.server.util.time.Range;
 import com.navercorp.pinpoint.web.applicationmap.link.Link;
 import com.navercorp.pinpoint.web.applicationmap.nodes.Node;
+import jakarta.annotation.Nullable;
 
 import java.util.Collection;
 import java.util.Objects;
@@ -10,9 +12,19 @@ public class SimpleApplicationMap implements ApplicationMap {
     private final Collection<Node> nodes;
     private final Collection<Link> links;
 
+    @Nullable
+    private final Range range;
+
     public SimpleApplicationMap(Collection<Node> nodes, Collection<Link> links) {
         this.nodes = Objects.requireNonNull(nodes, "nodes");
         this.links = Objects.requireNonNull(links, "links");
+        this.range = null;
+    }
+
+    public SimpleApplicationMap(Collection<Node> nodes, Collection<Link> links, Range range) {
+        this.nodes = Objects.requireNonNull(nodes, "nodes");
+        this.links = Objects.requireNonNull(links, "links");
+        this.range = Objects.requireNonNull(range, "range");
     }
 
     @Override
@@ -23,5 +35,10 @@ public class SimpleApplicationMap implements ApplicationMap {
     @Override
     public Collection<Link> getLinks() {
         return links;
+    }
+
+    @Override
+    public Range getRange() {
+        return range;
     }
 }
