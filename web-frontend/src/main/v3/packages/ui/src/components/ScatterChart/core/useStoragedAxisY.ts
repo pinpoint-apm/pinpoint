e389b05e37734f556fed7ac174733a35871b73c9
billionaireDY <doyoonkim1002@gmail.com>
2024-06-26 14:18:53 +0900

    [#noissue] fix: save scatter axis y

---
+++ web-frontend/src/main/v3/packages/ui/src/components/ScatterChart/core/useStoragedAxisY.ts
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useLocalStorage } from '@pinpoint-fe/hooks';
+import { APP_SETTING_KEYS } from '@pinpoint-fe/constants';
+
+type AxisRangeType = [number, number];
+
+export const useStoragedAxisY = (
+  initialValue = [0, 10000],
+): [AxisRangeType, React.Dispatch<React.SetStateAction<AxisRangeType>>] => {
+  const storagedAxisY = useLocalStorage<AxisRangeType>(
+    APP_SETTING_KEYS.SCATTER_Y_AXIS_MIN_MAX,
+    initialValue as AxisRangeType,
+  );
+
+  return storagedAxisY;
+};
