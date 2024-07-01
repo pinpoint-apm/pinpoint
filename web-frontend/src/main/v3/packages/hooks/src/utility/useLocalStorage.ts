bfdd20e528d390f8fb2d0edc9c267e330e35aa65
billionaireDY <doyoonkim1002@gmail.com>
2024-07-01 13:55:45 +0900

    [#noissue] feat: useExpiredLocalStorage

--- web-frontend/src/main/v3/packages/hooks/src/utility/useLocalStorage.ts
+++ web-frontend/src/main/v3/packages/hooks/src/utility/useLocalStorage.ts
@@ -3,11 +3,17 @@ import { useLocalStorage as useLocalStorageTS } from 'usehooks-ts';
 import { APP_SETTING_KEYS, EXPERIMENTAL_CONFIG_KEYS } from '@pinpoint-fe/constants';
 import { getCompatibleLocalStorageValue } from '@pinpoint-fe/utils';
 
-export const useLocalStorage = <T>(
-  key: APP_SETTING_KEYS | EXPERIMENTAL_CONFIG_KEYS,
+type LocalStorageKeyTypes = APP_SETTING_KEYS | EXPERIMENTAL_CONFIG_KEYS | string;
+
+const getV2Key = (key: string) => {
+  return `pp.${key}`;
+};
+
+export const useLocalStorage = <T, K extends LocalStorageKeyTypes = LocalStorageKeyTypes>(
+  key: K,
   initialValue: T,
 ) => {
-  const v2Key = `pp.${key}`;
+  const v2Key = getV2Key(key);
   const initValue = getCompatibleLocalStorageValue(key) ?? initialValue;
 
   const [data, setData] = useLocalStorageTS<T>(key, initValue);
@@ -17,6 +23,30 @@ export const useLocalStorage = <T>(
       window.localStorage.setItem(v2Key, JSON.stringify(data));
     }
   }, [data, v2Key]);
+
+  return [data, setData] as [T, React.Dispatch<React.SetStateAction<T>>];
+};
+
+export const useExpiredLocalStorage = <
+  T extends {
+    value?: unknown;
+    expire: number; // ms
+  },
+  K extends LocalStorageKeyTypes = LocalStorageKeyTypes,
+>(
+  key: K,
+  initialValue?: T,
+) => {
+  const [data, setData] = useLocalStorage(key, initialValue);
+
+  React.useEffect(() => {
+    const now = new Date().getTime();
+
+    if (data?.expire && data?.expire < now) {
+      window.localStorage.removeItem(key);
+      window.localStorage.removeItem(getV2Key(key));
+    }
+  }, [data]);
 
   return [data, setData] as [T, React.Dispatch<React.SetStateAction<T>>];
 };
