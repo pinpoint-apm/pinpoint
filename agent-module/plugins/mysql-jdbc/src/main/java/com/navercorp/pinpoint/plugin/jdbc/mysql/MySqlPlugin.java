49db63ccb1cf20984ac9c34af02e59f2ac730ad3
Sung-ho Kim <42758339+lancard@users.noreply.github.com>
2024-07-15 16:48:00 +0900

    add aws-mysql-wrapper driver

--- agent-module/plugins/mysql-jdbc/src/main/java/com/navercorp/pinpoint/plugin/jdbc/mysql/MySqlPlugin.java
+++ agent-module/plugins/mysql-jdbc/src/main/java/com/navercorp/pinpoint/plugin/jdbc/mysql/MySqlPlugin.java
@@ -198,6 +198,7 @@ public class MySqlPlugin implements ProfilerPlugin, TransformTemplateAware {
         transformTemplate.transform("com.mysql.cj.jdbc.NonRegisteringDriver", DriverTransform.class);
         // aws-mysql
         transformTemplate.transform("software.aws.rds.jdbc.mysql.Driver", DriverTransform.class);
+        transformTemplate.transform("software.amazon.jdbc.Driver", DriverTransform.class);        
     }
 
     public static class DriverTransform implements TransformCallback {
