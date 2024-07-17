5e4ee4b451fbad4efd0a4d710ba1135121968cf4
billionaireDY <doyoonkim1002@gmail.com>
2024-07-17 14:04:14 +0900

    [#noissue] fix: keyboard event propagation

--- web-frontend/src/main/v3/packages/datetime-picker/src/utils/useCaptureKeydown.ts
+++ web-frontend/src/main/v3/packages/datetime-picker/src/utils/useCaptureKeydown.ts
@@ -2,7 +2,6 @@ import * as React from 'react';
 
 export const useCaptureKeydown = (callback: (event: KeyboardEvent) => void) => {
   const handleKeydown = (e: KeyboardEvent) => {
-    e.stopPropagation();
     callback?.(e);
   };
 
