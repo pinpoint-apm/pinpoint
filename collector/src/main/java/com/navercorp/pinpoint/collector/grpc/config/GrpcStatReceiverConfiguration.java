951ce58d15d81e2184648b3abf8016250099d6ea
emeroad <emeroad@gmail.com>
2024-08-01 18:33:39 +0900

    [#11256] Fix a bug that incorrectly assigned bucket to interceptor

--- collector/src/main/java/com/navercorp/pinpoint/collector/grpc/config/GrpcStatReceiverConfiguration.java
+++ collector/src/main/java/com/navercorp/pinpoint/collector/grpc/config/GrpcStatReceiverConfiguration.java
@@ -63,7 +63,7 @@ public class GrpcStatReceiverConfiguration {
 
     @Deprecated
     @Configuration
-    @ConditionalOnProperty(name = "collector.receiver.grpc.stat.stream.flow-control.type", havingValue = "legacy", matchIfMissing = true)
+    @ConditionalOnProperty(name = "collector.receiver.grpc.stat.stream.flow-control.type", havingValue = "legacy")
     public static class LegacySpanInterceptorConfiguration {
 
         @Bean
@@ -92,7 +92,7 @@ public class GrpcStatReceiverConfiguration {
 
 
     @Configuration
-    @ConditionalOnProperty(name = "collector.receiver.grpc.stat.stream.flow-control.type", havingValue = "rate-limit")
+    @ConditionalOnProperty(name = "collector.receiver.grpc.stat.stream.flow-control.type", havingValue = "rate-limit", matchIfMissing = true)
     public static class RateLimitServerInterceptorConfiguration {
 
         @Bean
