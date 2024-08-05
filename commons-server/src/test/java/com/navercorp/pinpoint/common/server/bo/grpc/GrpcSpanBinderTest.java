da395b10829ad97b0d119910acfe6e057321e9bf
emeroad <emeroad@gmail.com>
2024-08-05 18:04:29 +0900

    [#11320] Add validation of parentApplicationName

---
+++ commons-server/src/test/java/com/navercorp/pinpoint/common/server/bo/grpc/GrpcSpanBinderTest.java
@@ -0,0 +1,36 @@
+package com.navercorp.pinpoint.common.server.bo.grpc;
+
+import com.navercorp.pinpoint.grpc.trace.PAcceptEvent;
+import com.navercorp.pinpoint.grpc.trace.PParentInfo;
+import com.navercorp.pinpoint.grpc.trace.PSpan;
+import com.navercorp.pinpoint.grpc.trace.PTransactionId;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class GrpcSpanBinderTest {
+
+    @Test
+    void bindSpanBo_InvalidParentApplicationName() {
+
+        BindAttribute bindAttribute = new BindAttribute("agentId", "applicationName", 1234, System.currentTimeMillis());
+        GrpcSpanBinder grpcSpanBinder = new GrpcSpanBinder();
+
+        PAcceptEvent event = PAcceptEvent.newBuilder()
+                .setParentInfo(PParentInfo.newBuilder()
+                        .setParentApplicationName("##invalidId")
+                        .build())
+                .build();
+        PTransactionId transactionId = PTransactionId.newBuilder()
+                .setAgentId("agentId")
+                .setAgentStartTime(1234)
+                .setSequence(1)
+                .build();
+
+        PSpan span = PSpan.newBuilder()
+                .setTransactionId(transactionId)
+                .setAcceptEvent(event)
+                .build();
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> grpcSpanBinder.newSpanBo(span, bindAttribute));
+    }
+}
