c822d23354b10308d5ab5593472018c26e111ed0
emeroad <emeroad@gmail.com>
2024-08-16 11:46:05 +0900

    [#11320] Root node has no parent information

--- commons-server/src/test/java/com/navercorp/pinpoint/common/server/bo/grpc/GrpcSpanBinderTest.java
+++ commons-server/src/test/java/com/navercorp/pinpoint/common/server/bo/grpc/GrpcSpanBinderTest.java
@@ -1,5 +1,6 @@
 package com.navercorp.pinpoint.common.server.bo.grpc;
 
+import com.navercorp.pinpoint.common.server.bo.SpanBo;
 import com.navercorp.pinpoint.grpc.trace.PAcceptEvent;
 import com.navercorp.pinpoint.grpc.trace.PParentInfo;
 import com.navercorp.pinpoint.grpc.trace.PSpan;
@@ -9,28 +10,63 @@ import org.junit.jupiter.api.Test;
 
 class GrpcSpanBinderTest {
 
-    @Test
-    void bindSpanBo_InvalidParentApplicationName() {
+    GrpcSpanBinder grpcSpanBinder = new GrpcSpanBinder();
 
-        BindAttribute bindAttribute = new BindAttribute("agentId-1", "appName-1", 1234, System.currentTimeMillis());
-        GrpcSpanBinder grpcSpanBinder = new GrpcSpanBinder();
+    BindAttribute bindAttribute = new BindAttribute("agentId-1", "appName-1", 1234, System.currentTimeMillis());
+
+    @Test
+    void bindSpanBo_parentApplicationName_invalid() {
 
         PAcceptEvent event = PAcceptEvent.newBuilder()
                 .setParentInfo(PParentInfo.newBuilder()
                         .setParentApplicationName("##invalidId")
                         .build())
                 .build();
+        PSpan span = newSpan(event);
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> grpcSpanBinder.newSpanBo(span, bindAttribute));
+    }
+
+    @Test
+    void bindSpanBo_parentApplicationName_valid() {
+
+        PAcceptEvent event = PAcceptEvent.newBuilder()
+                .setParentInfo(PParentInfo.newBuilder()
+                        .setParentApplicationName("validId")
+                        .setParentApplicationType(1000)
+                        .build())
+                .build();
+        PSpan span = newSpan(event);
+
+        SpanBo spanBo = grpcSpanBinder.newSpanBo(span, bindAttribute);
+        Assertions.assertEquals("validId", spanBo.getParentApplicationId());
+        Assertions.assertEquals(1000, spanBo.getParentApplicationServiceType());
+    }
+
+    private PSpan newSpan(PAcceptEvent acceptEvent) {
         PTransactionId transactionId = PTransactionId.newBuilder()
                 .setAgentId("agentId")
                 .setAgentStartTime(1234)
                 .setSequence(1)
                 .build();
 
-        PSpan span = PSpan.newBuilder()
-                .setTransactionId(transactionId)
-                .setAcceptEvent(event)
-                .build();
+        PSpan.Builder spanBuilder = PSpan.newBuilder();
 
-        Assertions.assertThrows(IllegalArgumentException.class, () -> grpcSpanBinder.newSpanBo(span, bindAttribute));
+        spanBuilder.setTransactionId(transactionId);
+        if (acceptEvent != null) {
+            spanBuilder.setAcceptEvent(acceptEvent);
+        }
+
+        return spanBuilder.build();
+    }
+
+    @Test
+    void bindSpanBo_root_node() {
+
+        PSpan span = newSpan(null);
+
+        SpanBo spanBo = grpcSpanBinder.newSpanBo(span, bindAttribute);
+        Assertions.assertNull(spanBo.getParentApplicationId());
+
     }
 }
