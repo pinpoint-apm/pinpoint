a5fcca06ffacc42509cb68cd0083b56a8f123b03
donghun-cho <donghun.cho@navercorp.com>
2024-08-19 20:14:55 +0900

    [#noissue] Add EnableScheduling annotation

--- collector/src/main/java/com/navercorp/pinpoint/collector/grpc/config/GrpcKeepAliveScheduler.java
+++ collector/src/main/java/com/navercorp/pinpoint/collector/grpc/config/GrpcKeepAliveScheduler.java
@@ -1,6 +1,9 @@
 package com.navercorp.pinpoint.collector.grpc.config;
 
 import com.navercorp.pinpoint.collector.receiver.grpc.service.KeepAliveService;
+import com.navercorp.pinpoint.common.profiler.logging.ThrottledLogger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -8,6 +11,9 @@ import java.util.Objects;
 
 @Component
 public class GrpcKeepAliveScheduler {
+    private final Logger logger = LogManager.getLogger(this.getClass());
+    private final ThrottledLogger throttledLogger = ThrottledLogger.getLogger(logger, 12);
+
     private final KeepAliveService keepAliveService;
 
     public GrpcKeepAliveScheduler(KeepAliveService keepAliveService) {
@@ -16,6 +22,9 @@ public class GrpcKeepAliveScheduler {
 
     @Scheduled(fixedRate = 300_000, scheduler = "grpcLifecycleScheduler")
     public void keepAliveServiceUpdate() {
+        if (throttledLogger.isInfoEnabled()) {
+            throttledLogger.info("keepAliveServiceUpdate started");
+        }
         this.keepAliveService.updateState();
     }
 }
