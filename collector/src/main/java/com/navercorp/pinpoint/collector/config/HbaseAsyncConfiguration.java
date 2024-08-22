c83f5f3390536879de4df37e9f94e9f289bb0b8d
emeroad <emeroad@gmail.com>
2024-08-22 14:52:30 +0900

    [#11379] Cleanup

--- collector/src/main/java/com/navercorp/pinpoint/collector/config/HbaseAsyncConfiguration.java
+++ collector/src/main/java/com/navercorp/pinpoint/collector/config/HbaseAsyncConfiguration.java
@@ -18,27 +18,19 @@
 package com.navercorp.pinpoint.collector.config;
 
 import com.navercorp.pinpoint.collector.manage.HBaseManager;
-import com.navercorp.pinpoint.common.hbase.async.HBaseTableMultiplexerFactory;
-import com.navercorp.pinpoint.common.hbase.async.HbasePutWriter;
-import com.navercorp.pinpoint.common.hbase.async.LoggingHbasePutWriter;
-import com.navercorp.pinpoint.common.hbase.config.HbaseMultiplexerProperties;
 import com.navercorp.pinpoint.common.hbase.counter.HBaseBatchPerformance;
 import com.navercorp.pinpoint.common.hbase.counter.HbaseBatchPerformanceCounter;
 import com.navercorp.pinpoint.common.server.config.CommonCacheManagerConfiguration;
-import org.apache.hadoop.hbase.client.Connection;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
 @Import({
         CommonCacheManagerConfiguration.class,
-        HbaseAsyncConfiguration.TableMultiplexerConfig.class
 })
 public class HbaseAsyncConfiguration {
 
@@ -47,45 +39,6 @@ public class HbaseAsyncConfiguration {
     @Bean
     public HbaseBatchPerformanceCounter batchPerformanceCounter() {
         return new HbaseBatchPerformanceCounter();
-    }
-
-    @Configuration
-    @ConditionalOnProperty(name = "hbase.client.put-writer", havingValue = "tableMultiplexer")
-    public static class TableMultiplexerConfig {
-        private final Logger logger = LogManager.getLogger(TableMultiplexerConfig.class);
-
-        @Bean
-        public HbaseMultiplexerProperties hbaseMultiplexerProperties() {
-            return new HbaseMultiplexerProperties();
-        }
-
-        @Primary
-        @Bean
-        public HbasePutWriter hbasePutWriter(@Qualifier("batchConnectionFactory") Connection connection,
-                                             HbaseBatchPerformanceCounter counter,
-                                             HbaseMultiplexerProperties properties) throws Exception {
-            HbasePutWriter writer = newPutWriter(connection, counter, properties);
-            logger.info("HbaseSpanPutWriter:{}", writer);
-            return writer;
-        }
-
-        @Bean
-        public HbasePutWriter spanPutWriter(@Qualifier("batchConnectionFactory") Connection connection,
-                                            HbaseBatchPerformanceCounter counter,
-                                            HbaseMultiplexerProperties properties) throws Exception {
-            HbasePutWriter writer = newPutWriter(connection, counter, properties);
-            logger.info("HbaseSpanPutWriter:{}", writer);
-            return writer;
-        }
-
-        private HbasePutWriter newPutWriter(Connection connection, HbaseBatchPerformanceCounter counter,
-                                            HbaseMultiplexerProperties properties) throws Exception {
-            HBaseTableMultiplexerFactory factory = new HBaseTableMultiplexerFactory(connection, counter);
-            factory.setHbaseMultiplexerProperties(properties);
-            HbasePutWriter writer = factory.getObject();
-            return new LoggingHbasePutWriter(writer);
-        }
-
     }
 
     @Bean
