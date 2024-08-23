ab474b205d3c1e1acef6c3e0627ee06abf987267
Jaehong-Kim <jaehong.kim@navercorp.com>
2024-08-23 10:32:45 +0900

    [#11390] Update reactor lift of spring-cloud-sleuth plugin

--- agent-module/plugins/spring-cloud-sleuth/src/main/java/com/navercorp/pinpoint/plugin/spring/cloud/sleuth/SpringCloudSleuthPlugin.java
+++ agent-module/plugins/spring-cloud-sleuth/src/main/java/com/navercorp/pinpoint/plugin/spring/cloud/sleuth/SpringCloudSleuthPlugin.java
@@ -32,10 +32,15 @@ import com.navercorp.pinpoint.bootstrap.logging.PluginLogger;
 import com.navercorp.pinpoint.bootstrap.plugin.ProfilerPlugin;
 import com.navercorp.pinpoint.bootstrap.plugin.ProfilerPluginSetupContext;
 import com.navercorp.pinpoint.bootstrap.plugin.reactor.CoreSubscriberConstructorInterceptor;
+import com.navercorp.pinpoint.bootstrap.plugin.reactor.FluxAndMonoOperatorConstructorInterceptor;
+import com.navercorp.pinpoint.bootstrap.plugin.reactor.FluxAndMonoOperatorSubscribeInterceptor;
 import com.navercorp.pinpoint.bootstrap.plugin.reactor.ReactorContextAccessor;
 import com.navercorp.pinpoint.common.util.ArrayUtils;
+import com.navercorp.pinpoint.plugin.spring.cloud.sleuth.interceptor.ParallelFluxSubscribeInterceptor;
 
 import java.security.ProtectionDomain;
+
+import static com.navercorp.pinpoint.common.util.VarArgs.va;
 
 public class SpringCloudSleuthPlugin implements ProfilerPlugin, MatchableTransformTemplateAware {
     private final PluginLogger logger = PluginLogManager.getLogger(this.getClass());
@@ -53,6 +58,8 @@ public class SpringCloudSleuthPlugin implements ProfilerPlugin, MatchableTransfo
 
         // for reactor
         addCoreSubscriber();
+
+
     }
 
     @Override
@@ -63,6 +70,18 @@ public class SpringCloudSleuthPlugin implements ProfilerPlugin, MatchableTransfo
     private void addCoreSubscriber() {
         final Matcher coreSubscriberMatcher = Matchers.newPackageBasedMatcher("org.springframework.cloud.sleuth.instrument.reactor", new InterfaceInternalNameMatcherOperand("reactor.core.CoreSubscriber", true));
         transformTemplate.transform(coreSubscriberMatcher, CoreSubscriberTransform.class);
+
+        // spring-cloud-sleuth 3.0.2, 3.0.3
+        // MonoOperator
+        transformTemplate.transform("org.springframework.cloud.sleuth.instrument.reactor.SleuthMonoLift", LiftTransform.class);
+        // FluxOperator
+        transformTemplate.transform("org.springframework.cloud.sleuth.instrument.reactor.SleuthFluxLift", LiftTransform.class);
+        // SleuthConnectableLift
+        transformTemplate.transform("org.springframework.cloud.sleuth.instrument.reactor.SleuthConnectableLift", LiftTransform.class);
+        // SleuthGroupedLift
+        transformTemplate.transform("org.springframework.cloud.sleuth.instrument.reactor.SleuthGroupedLift", LiftTransform.class);
+        // SleuthParallelLift
+        transformTemplate.transform("org.springframework.cloud.sleuth.instrument.reactor.SleuthParallelLift", LiftTransform.class);
     }
 
     public static class CoreSubscriberTransform implements TransformCallback {
@@ -80,6 +99,37 @@ public class SpringCloudSleuthPlugin implements ProfilerPlugin, MatchableTransfo
                 }
             }
 
+            return target.toBytecode();
+        }
+    }
+
+    public static class LiftTransform implements TransformCallback {
+        @Override
+        public byte[] doInTransform(Instrumentor instrumentor, ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws InstrumentException {
+            final InstrumentClass target = instrumentor.getInstrumentClass(loader, className, classfileBuffer);
+            // Async Object
+            target.addField(AsyncContextAccessor.class);
+            target.addField(ReactorContextAccessor.class);
+
+            for (InstrumentMethod constructorMethod : target.getDeclaredConstructors()) {
+                final String[] parameterTypes = constructorMethod.getParameterTypes();
+                if (ArrayUtils.hasLength(parameterTypes)) {
+                    constructorMethod.addInterceptor(FluxAndMonoOperatorConstructorInterceptor.class);
+                }
+            }
+
+            final InstrumentMethod subscribeMethod = target.getDeclaredMethod("subscribe", "reactor.core.CoreSubscriber");
+            if (subscribeMethod != null) {
+                subscribeMethod.addInterceptor(FluxAndMonoOperatorSubscribeInterceptor.class, va(SpringCloudSleuthConstants.SPRING_CLOUD_SLEUTH));
+            }
+            final InstrumentMethod subscribesMethod = target.getDeclaredMethod("subscribe", "reactor.core.CoreSubscriber[]");
+            if (subscribesMethod != null) {
+                subscribesMethod.addInterceptor(ParallelFluxSubscribeInterceptor.class, va(SpringCloudSleuthConstants.SPRING_CLOUD_SLEUTH));
+            }
+            final InstrumentMethod connectMethod = target.getDeclaredMethod("connect", "java.util.function.Consumer");
+            if (connectMethod != null) {
+                connectMethod.addInterceptor(FluxAndMonoOperatorSubscribeInterceptor.class, va(SpringCloudSleuthConstants.SPRING_CLOUD_SLEUTH));
+            }
             return target.toBytecode();
         }
     }
