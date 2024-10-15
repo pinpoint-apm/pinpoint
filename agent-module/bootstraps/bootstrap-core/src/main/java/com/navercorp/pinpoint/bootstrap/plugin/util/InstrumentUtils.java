eb07dd9da8874a97f775d5a4e2ae281aff45486a
Jaehong-Kim <jaehong.kim@navercorp.com>
2024-10-15 15:43:10 +0900

    [#11577] Update mariadb-plugin to support mariadb java client 3.x

--- agent-module/bootstraps/bootstrap-core/src/main/java/com/navercorp/pinpoint/bootstrap/plugin/util/InstrumentUtils.java
+++ agent-module/bootstraps/bootstrap-core/src/main/java/com/navercorp/pinpoint/bootstrap/plugin/util/InstrumentUtils.java
@@ -16,9 +16,14 @@
 
 package com.navercorp.pinpoint.bootstrap.plugin.util;
 
+import com.navercorp.pinpoint.bootstrap.context.MethodDescriptor;
 import com.navercorp.pinpoint.bootstrap.instrument.InstrumentClass;
+import com.navercorp.pinpoint.bootstrap.instrument.InstrumentException;
 import com.navercorp.pinpoint.bootstrap.instrument.InstrumentMethod;
 import com.navercorp.pinpoint.bootstrap.instrument.NotFoundInstrumentException;
+import com.navercorp.pinpoint.bootstrap.interceptor.Interceptor;
+import com.navercorp.pinpoint.bootstrap.interceptor.scope.ExecutionPolicy;
+import com.navercorp.pinpoint.bootstrap.interceptor.scope.InterceptorScope;
 
 import java.util.Arrays;
 
@@ -35,11 +40,120 @@ public final class InstrumentUtils {
         return method;
     }
 
+    public static InstrumentMethod findMethodOrIgnore(InstrumentClass clazz, String methodName, String... parameterTypes) throws NotFoundInstrumentException {
+        final InstrumentMethod method = clazz.getDeclaredMethod(methodName, parameterTypes);
+        if (method == null) {
+            return new NullInstrumentMethod();
+        }
+        return method;
+    }
+
     public static InstrumentMethod findConstructor(InstrumentClass clazz, String... parameterTypes) throws NotFoundInstrumentException {
         InstrumentMethod constructor = clazz.getConstructor(parameterTypes);
         if (constructor == null) {
             throw new NotFoundInstrumentException("Cannot find constructor with parameter types: " + Arrays.toString(parameterTypes));
         }
         return constructor;
+    }
+
+    public static InstrumentMethod findConstructorOrIgnore(InstrumentClass clazz, String... parameterTypes) throws NotFoundInstrumentException {
+        InstrumentMethod constructor = clazz.getConstructor(parameterTypes);
+        if (constructor == null) {
+            return new NullInstrumentMethod();
+        }
+        return constructor;
+    }
+
+    static class NullInstrumentMethod implements InstrumentMethod {
+
+        @Override
+        public String getName() {
+            return "";
+        }
+
+        @Override
+        public String[] getParameterTypes() {
+            return new String[0];
+        }
+
+        @Override
+        public String getReturnType() {
+            return "";
+        }
+
+        @Override
+        public int getModifiers() {
+            return 0;
+        }
+
+        @Override
+        public boolean isConstructor() {
+            return false;
+        }
+
+        @Override
+        public MethodDescriptor getDescriptor() {
+            throw new IllegalStateException("not implemented");
+        }
+
+        @Override
+        public int addInterceptor(Class<? extends Interceptor> interceptorClass) throws InstrumentException {
+            return 0;
+        }
+
+        @Override
+        public int addInterceptor(Class<? extends Interceptor> interceptorClass, Object[] constructorArgs) throws InstrumentException {
+            return 0;
+        }
+
+        @Override
+        public int addScopedInterceptor(Class<? extends Interceptor> interceptorClass, String scopeName) throws InstrumentException {
+            return 0;
+        }
+
+        @Override
+        public int addScopedInterceptor(Class<? extends Interceptor> interceptorClass, InterceptorScope interceptorScope) throws InstrumentException {
+            return 0;
+        }
+
+        @Override
+        public int addScopedInterceptor(Class<? extends Interceptor> interceptorClass, String scopeName, ExecutionPolicy executionPolicy) throws InstrumentException {
+            return 0;
+        }
+
+        @Override
+        public int addScopedInterceptor(Class<? extends Interceptor> interceptorClass, InterceptorScope interceptorScope, ExecutionPolicy executionPolicy) throws InstrumentException {
+            return 0;
+        }
+
+        @Override
+        public int addScopedInterceptor(Class<? extends Interceptor> interceptorClass, Object[] constructorArgs, String scopeName) throws InstrumentException {
+            return 0;
+        }
+
+        @Override
+        public int addScopedInterceptor(Class<? extends Interceptor> interceptorClass, Object[] constructorArgs, InterceptorScope interceptorScope) throws InstrumentException {
+            return 0;
+        }
+
+        @Override
+        public int addScopedInterceptor(Class<? extends Interceptor> interceptorClass, Object[] constructorArgs, String scopeName, ExecutionPolicy executionPolicy) throws InstrumentException {
+            return 0;
+        }
+
+        @Override
+        public int addScopedInterceptor(Class<? extends Interceptor> interceptorClass, Object[] constructorArgs, InterceptorScope interceptorScope, ExecutionPolicy executionPolicy) throws InstrumentException {
+            return 0;
+        }
+
+        @Override
+        public void addInterceptor(int interceptorId) throws InstrumentException {
+
+        }
+
+        @Override
+        public Class<? extends Interceptor> loadInterceptorClass(String interceptorClassName) throws InstrumentException {
+            throw new IllegalStateException("not implemented");
+        }
     }
 }
