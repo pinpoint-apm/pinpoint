f854a078fb9bc9c83a9e7c55c8674b2480afbe55
jihea-park <jihea.park@navercorp.com>
2024-10-16 11:28:49 +0900

    [#10776] OpenTelemetry > Apply blink style to save dashboard button

--- web-frontend/src/main/v3/packages/ui/tailwind.config.js
+++ web-frontend/src/main/v3/packages/ui/tailwind.config.js
@@ -92,10 +92,15 @@ export default {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: 0 },
         },
+        blink: {
+          '0%, 100%': { opacity: 1 },
+          '50%': { opacity: 0 },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        blink: 'blink 1s infinite',
       },
     },
   },
