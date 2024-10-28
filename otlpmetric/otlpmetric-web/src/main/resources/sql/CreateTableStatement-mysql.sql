c45534bf50128f8c300a47f16ea792398ac44e30
minwoo.jung <jungmwiner@gmail.com>
2024-10-28 16:39:05 +0900

    [#10776] edit schema for application_metric_definition table

--- otlpmetric/otlpmetric-web/src/main/resources/sql/CreateTableStatement-mysql.sql
+++ otlpmetric/otlpmetric-web/src/main/resources/sql/CreateTableStatement-mysql.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `application_metric_definition` (
-     `application_name` VARCHAR(60) NOT NULL,
+     `application_name` VARCHAR(255) NOT NULL,
      `metric_definition` text NOT NULL,
      `schema_version` int(10) NOT NULL,
      PRIMARY KEY (`application_name`)
