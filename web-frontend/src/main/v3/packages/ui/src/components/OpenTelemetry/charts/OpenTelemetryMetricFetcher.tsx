b6533db71f7cd830f35784b22fcc7edf82ff0fe1
jihea-park <jihea.park@navercorp.com>
2024-12-05 15:47:49 +0900

    [#7758] Apply lazy loading to OpenTelemetryMetricFetcher

--- web-frontend/src/main/v3/packages/ui/src/components/OpenTelemetry/charts/OpenTelemetryMetricFetcher.tsx
+++ web-frontend/src/main/v3/packages/ui/src/components/OpenTelemetry/charts/OpenTelemetryMetricFetcher.tsx
@@ -3,6 +3,7 @@ import { useOpenTelemetrySearchParameters, usePostOtlpMetricData } from '@pinpoi
 import React from 'react';
 import { assign } from 'lodash';
 import { ReChart } from '../../../components/ReChart';
+import { useInView } from 'react-intersection-observer';
 
 export interface OpenTelemetryMetricFetcherProps {
   metricDefinition: OtlpMetricDefUserDefined.Metric;
@@ -13,12 +14,15 @@ export interface OpenTelemetryMetricFetcherProps {
 export const OpenTelemetryMetricFetcher = ({
   metricDefinition,
   dashboardId,
-  inView,
 }: OpenTelemetryMetricFetcherProps) => {
   const { mutate, data } = usePostOtlpMetricData();
   const { dateRange, agentId } = useOpenTelemetrySearchParameters();
   const prevDateRange = React.useRef<{ from: Date; to: Date }>();
   const prevAgentId = React.useRef<string>();
+  const { ref, inView } = useInView({
+    initialInView: false,
+    threshold: 0.1,
+  });
 
   React.useEffect(() => {
     if (!inView) {
@@ -71,29 +75,31 @@ export const OpenTelemetryMetricFetcher = ({
   const { stack, stackDetails } = metricDefinition;
 
   return (
-    <ReChart
-      syncId={dashboardId}
-      chartData={{
-        title: '',
-        timestamp: data?.timestamp || [],
-        metricValueGroups: [
-          {
-            groupName: '',
-            chartType: data?.chartType || '',
-            unit: data?.unit || '',
-            metricValues: (data?.metricValues || [])?.map((mv) => {
-              return {
-                fieldName: mv?.legendName,
-                values: mv?.values,
-              };
-            }),
-          },
-        ],
-      }}
-      unit={data?.unit}
-      tooltipConfig={{
-        showTotal: stack && stackDetails?.showTotal,
-      }}
-    />
+    <div ref={ref} className="w-full h-full">
+      <ReChart
+        syncId={dashboardId}
+        chartData={{
+          title: '',
+          timestamp: data?.timestamp || [],
+          metricValueGroups: [
+            {
+              groupName: '',
+              chartType: data?.chartType || '',
+              unit: data?.unit || '',
+              metricValues: (data?.metricValues || [])?.map((mv) => {
+                return {
+                  fieldName: mv?.legendName,
+                  values: mv?.values,
+                };
+              }),
+            },
+          ],
+        }}
+        unit={data?.unit}
+        tooltipConfig={{
+          showTotal: stack && stackDetails?.showTotal,
+        }}
+      />
+    </div>
   );
 };
