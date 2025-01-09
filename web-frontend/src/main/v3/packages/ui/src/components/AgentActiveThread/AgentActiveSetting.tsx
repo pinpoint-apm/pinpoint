13147d4d8e0a1ada1bf4aacbfedb201b69a701ba
jihea-park <jihea.park@navercorp.com>
2025-01-09 20:21:48 +0900

    [#11962] Equalizer Bar Chart to replace real time chart

---
+++ web-frontend/src/main/v3/packages/ui/src/components/AgentActiveThread/AgentActiveSetting.tsx
@@ -0,0 +1,126 @@
+import React from 'react';
+import { useOnClickOutside } from 'usehooks-ts';
+import { cn } from '@pinpoint-fe/ui/lib';
+import {
+  Button,
+  Input,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  Checkbox,
+} from '@pinpoint-fe/ui/components/ui';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+
+export interface AgentActiveSettingProps {
+  className?: string;
+  onApply?: (value: AgentActiveSettingType) => void;
+  onClose?: () => void;
+  defaultValues?: AgentActiveSettingType;
+}
+
+export const DefaultValue = { yMax: 100, isSplit: true };
+
+const FormSchema = z.object({
+  yMax: z.coerce.number(),
+  isSplit: z.boolean(),
+});
+
+export type AgentActiveSettingType = z.infer<typeof FormSchema>;
+
+export const AgentActiveSetting = ({
+  className,
+  onApply,
+  onClose,
+  defaultValues = DefaultValue,
+}: AgentActiveSettingProps) => {
+  const containerRef = React.useRef(null);
+
+  const handleClickClose = () => {
+    onClose?.();
+  };
+
+  useOnClickOutside(containerRef, handleClickClose);
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      yMax: defaultValues?.yMax,
+      isSplit: defaultValues?.isSplit,
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    onApply?.(data);
+    handleClickClose();
+  }
+
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    const data = form.getValues();
+
+    if (e.key === 'Enter') {
+      onApply?.(data);
+    }
+  };
+
+  return (
+    <div
+      className={cn(
+        'rounded shadow bg-background p-4 w-60 flex gap-3 flex-col text-sm border',
+        className,
+      )}
+      ref={containerRef}
+    >
+      <div className="mb-3 font-semibold">Agent request chart Setting</div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+          <FormField
+            control={form.control}
+            name="yMax"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-xs text-muted-foreground">Max of Y axis</FormLabel>
+                <FormControl>
+                  <Input type="number" className="w-24 h-7" onKeyDown={handleKeyDown} {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="isSplit"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-xs text-muted-foreground">
+                  Split chart in 2 (&gt;=100)
+                </FormLabel>
+                <FormControl className="flex flex-row items-start space-x-3 space-y-0">
+                  <Checkbox
+                    checked={field.value}
+                    onCheckedChange={(checked) => {
+                      return field.onChange(checked);
+                    }}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex justify-end gap-1 mt-6">
+            <Button className="text-xs h-7" variant="outline" onClick={handleClickClose}>
+              Cancel
+            </Button>
+            <Button type="submit" className="text-xs h-7">
+              Apply
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
+};
--- web-frontend/src/main/v3/packages/ui/src/components/AgentActiveThread/AgentActiveThreadSkeleton.tsx
+++ web-frontend/src/main/v3/packages/ui/src/components/AgentActiveThread/AgentActiveThreadSkeleton.tsx
@@ -1,32 +1,28 @@
 import React from 'react';
 import ContentLoader from 'react-content-loader';
+import { DataTableSkeleton } from '../DataTable';
+import { ChartSkeleton } from '../Chart';
 
 export const AgentActiveThreadSkeleton = () => {
   const elementRef = React.useRef<HTMLDivElement>(null);
 
   return (
-    <div className="grid w-full grid-cols-[30rem_auto]" ref={elementRef}>
-      <div>
-        <ContentLoader speed={2} height="100%" width={470}>
+    <div
+      className="flex w-[-webkit-fill-available] h-[-webkit-fill-available] overflow-hidden"
+      ref={elementRef}
+    >
+      <div className="w-4/5 h-full">
+        <ContentLoader speed={2} width={470} hanging={40} className="h-fit" viewBox={`0 0 470 100`}>
           <rect x={24} y={24} rx="4" ry="4" width={220} height={32} />
-          <rect x={24} y={70} rx="4" ry="4" width={300} height={156} />
         </ContentLoader>
+        <ChartSkeleton />
       </div>
-      <div>
+      <div className="w-2/5">
         <ContentLoader speed={2} height={40} width={180}>
           <rect x={0} y={16} rx="4" ry="4" width={180} height={30} />
         </ContentLoader>
         <div className="flex flex-wrap gap-4 px-3 mt-3">
-          {Array(8)
-            .fill(0)
-            .map((_, i) => (
-              <div key={i} className="">
-                <ContentLoader speed={2} height={130} width={180}>
-                  <rect x={0} y={16} rx="4" ry="4" width={180} height={20} />
-                  <rect x={0} y={52} rx="4" ry="4" width={180} height={76} />
-                </ContentLoader>
-              </div>
-            ))}
+          <DataTableSkeleton hideRowBox />
         </div>
       </div>
     </div>
