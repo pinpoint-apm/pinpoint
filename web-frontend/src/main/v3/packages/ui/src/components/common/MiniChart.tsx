d6cbe0c352a645f381ba8593dd1d3c579127fd28
jihea-park <jihea.park@navercorp.com>
2025-01-23 18:12:54 +0900

    [#11982] Math.round to long maxValue

--- web-frontend/src/main/v3/packages/ui/src/components/common/MiniChart.tsx
+++ web-frontend/src/main/v3/packages/ui/src/components/common/MiniChart.tsx
@@ -1,37 +1,38 @@
 import { Chart } from '@pinpoint-fe/ui/constants';
 import { ComposedChart, ReferenceLine } from 'recharts';
 import { useRechart } from '../ReChart/useRechart';
+import { ChartContainer } from '../ui';
 
 export interface MiniChart {
   chart: Chart;
 }
 
 export const MiniChart = ({ chart }: MiniChart) => {
-  const { data, maxValue, renderChartChildComponents } = useRechart(chart);
+  const { data, chartConfig, maxValue, renderChartChildComponents } = useRechart(chart);
 
   return (
-    <ComposedChart
-      width={128}
-      height={40}
-      data={data}
-      margin={{
-        top: 6,
-        right: 30,
-        bottom: 6,
-        left: 2,
-      }}
-    >
-      {data?.length && maxValue > -1 && (
-        <ReferenceLine
-          y={maxValue}
-          stroke="black"
-          strokeDasharray="3 3"
-          position="middle"
-          label={{ position: 'right', value: maxValue }}
-          ifOverflow="extendDomain"
-        />
-      )}
-      {renderChartChildComponents()}
-    </ComposedChart>
+    <ChartContainer config={chartConfig} className="w-full h-[40px]">
+      <ComposedChart
+        data={data}
+        margin={{
+          top: 6,
+          right: 60,
+          bottom: 6,
+          left: 2,
+        }}
+      >
+        {data?.length && maxValue > -1 && (
+          <ReferenceLine
+            y={maxValue}
+            stroke="black"
+            strokeDasharray="3 3"
+            position="middle"
+            label={{ position: 'right', value: Math.round(maxValue * 100) / 100 }}
+            ifOverflow="extendDomain"
+          />
+        )}
+        {renderChartChildComponents()}
+      </ComposedChart>
+    </ChartContainer>
   );
 };
