0c063af28e9e065a1996011522f04adafed11e24
jihea.park <jihea.park@navercorp.com>
2025-02-03 13:57:21 +0900

    [#noissue] commit for test2

--- web-frontend/src/main/v3/packages/ui/src/components/OpenTelemetry/charts/OpenTelemetryMetricFetcher.tsx
+++ web-frontend/src/main/v3/packages/ui/src/components/OpenTelemetry/charts/OpenTelemetryMetricFetcher.tsx
@@ -43,7 +43,7 @@ export const OpenTelemetryMetricFetcher = ({
     mutate(
       assign(
         {
-          applicationName,
+          applicationName: metricName === 'cpu' ? 'error' : applicationName,
           metricGroupName,
           metricName,
           tagGroupList,
