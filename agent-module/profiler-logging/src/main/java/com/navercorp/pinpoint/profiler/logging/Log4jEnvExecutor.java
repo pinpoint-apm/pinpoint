b055f08061123ce321fdd9bb4f4b93d4beef0231
emeroad <emeroad@gmail.com>
2025-02-03 11:45:28 +0900

    [#11818] Improve log4j context initialization

--- agent-module/profiler-logging/src/main/java/com/navercorp/pinpoint/profiler/logging/Log4jEnvExecutor.java
+++ agent-module/profiler-logging/src/main/java/com/navercorp/pinpoint/profiler/logging/Log4jEnvExecutor.java
@@ -4,20 +4,29 @@ import java.util.function.Supplier;
 
 public class Log4jEnvExecutor {
 
+    /**
+     * log4j 2.24
+     * ref : {@value org.apache.logging.log4j.spi.Provider#PROVIDER_PROPERTY_NAME}
+     */
+    public static final String PROVIDER_PROPERTY_NAME = "log4j.provider";
     public static final String FACTORY_PROPERTY_NAME = "log4j2.loggerContextFactory";
 
     public <V> V call(Supplier<V> supplier) {
-        final String factory = prepare(FACTORY_PROPERTY_NAME, Log4j2ContextFactory.class.getName());
+        final String provider = prepare(PROVIDER_PROPERTY_NAME, Log4j2Provider.class.getName());
+        final String factory = prepare(FACTORY_PROPERTY_NAME, null);
         try {
             return supplier.get();
         } finally {
             rollback(FACTORY_PROPERTY_NAME, factory);
+            rollback(PROVIDER_PROPERTY_NAME, provider);
         }
     }
 
     private String prepare(String key, String value) {
         final String backup = System.getProperty(key);
-        System.setProperty(key, value);
+        if (value != null) {
+            System.setProperty(key, value);
+        }
         return backup;
     }
 
