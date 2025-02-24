dc0a0dab45cd9a809a53830492573ba606560e6e
Jaehong-Kim <jaehong.kim@navercorp.com>
2025-02-24 16:10:54 +0900

    [#noissue] rename 3.0.2-SNAPSHOT

--- commons/src/main/java/com/navercorp/pinpoint/common/Version.java
+++ commons/src/main/java/com/navercorp/pinpoint/common/Version.java
@@ -1,4 +1,4 @@
 package com.navercorp.pinpoint.common;
 public final class Version {
-    public static final String VERSION = "3.0.2-alpha2";
+    public static final String VERSION = "3.0.2-SNAPSHOT";
 }
