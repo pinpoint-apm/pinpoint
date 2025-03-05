420c7001a1fc5f57e987c7daf240136dee44af6c
jihea-park <jihea.park@navercorp.com>
2025-03-05 17:21:26 +0900

    [#noissue] Fix transactionDetail page error when accessing global search

--- web-frontend/src/main/v3/packages/ui/src/components/HOC/withInitialFetch.tsx
+++ web-frontend/src/main/v3/packages/ui/src/components/HOC/withInitialFetch.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { useExperimentals, useGetConfiguration } from '@pinpoint-fe/ui/src/hooks';
 import { useSetAtom } from 'jotai';
 import { configurationAtom } from '@pinpoint-fe/ui/src/atoms';
 import { APP_PATH, Configuration } from '@pinpoint-fe/ui/src/constants';
+import { searchParametersAtom } from '@pinpoint-fe/ui/src/atoms';
+import { getApplicationTypeAndName } from '@pinpoint-fe/ui/src/utils';
 
 export const withInitialFetch =
   <P extends object>(WrappedComponent: React.ComponentType<P>) =>
@@ -11,8 +13,23 @@ export const withInitialFetch =
     const navigate = useNavigate();
     const { data, error } = useGetConfiguration<Configuration>({ suspense: false });
     const setConfiguration = useSetAtom(configurationAtom);
+    const { pathname, search } = useLocation();
+    const application = getApplicationTypeAndName(pathname);
+    const searchParameters = Object.fromEntries(new URLSearchParams(search));
+    const setSearchParameters = useSetAtom(searchParametersAtom);
 
     useExperimentals(data);
+
+    React.useEffect(() => {
+      if (application && searchParameters) {
+        setSearchParameters({ application, searchParameters });
+      }
+    }, [
+      application?.applicationName,
+      application?.serviceType,
+      searchParameters?.to,
+      searchParameters?.from,
+    ]);
 
     React.useEffect(() => {
       setConfiguration(data);
--- web-frontend/src/main/v3/packages/ui/src/hooks/searchParameters/useSearchParameters.ts
+++ web-frontend/src/main/v3/packages/ui/src/hooks/searchParameters/useSearchParameters.ts
@@ -1,18 +1,11 @@
 import { useLocation } from 'react-router-dom';
 import { getApplicationTypeAndName } from '@pinpoint-fe/ui/src/utils';
-import { useAtomValue } from 'jotai';
-import { searchParametersAtom } from '@pinpoint-fe/ui/src/atoms';
 
 export const useSearchParameters = () => {
   const props = useLocation();
-  const atomSearchParameters = useAtomValue(searchParametersAtom);
 
   const searchParameters = Object.fromEntries(new URLSearchParams(props.search));
   const application = getApplicationTypeAndName(props.pathname);
 
-  return {
-    ...props,
-    searchParameters: application ? searchParameters : atomSearchParameters?.searchParameters,
-    application: application || atomSearchParameters?.application,
-  };
+  return { ...props, searchParameters, application };
 };
