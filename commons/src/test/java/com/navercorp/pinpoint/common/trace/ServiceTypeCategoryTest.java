c050e31ddc5be3aba6ff485dad8ad40b1cf4e13e
emeroad <emeroad@gmail.com>
2025-03-20 17:57:09 +0900

    [#noissue] Refactor ServiceTypeCategory

---
+++ commons/src/test/java/com/navercorp/pinpoint/common/trace/ServiceTypeCategoryTest.java
@@ -0,0 +1,27 @@
+package com.navercorp.pinpoint.common.trace;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ServiceTypeCategoryTest {
+
+    @Test
+    void findCategory() {
+        ServiceTypeCategory undefined = ServiceTypeCategory.findCategory(-1);
+        Assertions.assertEquals(ServiceTypeCategory.UNDEFINED_CATEGORY, undefined);
+
+        ServiceTypeCategory server = ServiceTypeCategory.findCategory(1000);
+        Assertions.assertEquals(ServiceTypeCategory.SERVER, server);
+
+        ServiceTypeCategory serverEnd = ServiceTypeCategory.findCategory(1999);
+        Assertions.assertEquals(ServiceTypeCategory.SERVER, serverEnd);
+
+        ServiceTypeCategory database = ServiceTypeCategory.findCategory(2000);
+        Assertions.assertEquals(ServiceTypeCategory.DATABASE, database);
+    }
+
+    @Test
+    void findCategory_error() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> ServiceTypeCategory.findCategory(-1000));
+    }
+}
