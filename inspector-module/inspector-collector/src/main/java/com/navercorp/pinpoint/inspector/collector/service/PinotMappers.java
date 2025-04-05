0300e68339730c4e84eaad9c574dd6ed7a074586
emeroad <emeroad@gmail.com>
2025-04-05 16:00:39 +0900

    [#noissue] Cleanup

--- inspector-module/inspector-collector/src/main/java/com/navercorp/pinpoint/inspector/collector/service/PinotMappers.java
+++ inspector-module/inspector-collector/src/main/java/com/navercorp/pinpoint/inspector/collector/service/PinotMappers.java
@@ -16,10 +16,19 @@ import com.navercorp.pinpoint.common.server.bo.stat.TransactionBo;
 import com.navercorp.pinpoint.inspector.collector.dao.pinot.PinotTypeMapper;
 import com.navercorp.pinpoint.inspector.collector.model.kafka.AgentStatModelConverter;
 import com.navercorp.pinpoint.inspector.collector.model.kafka.ApplicationStatModelConverter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.util.ReflectionUtils;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class PinotMappers {
+
+    private final Logger logger = LogManager.getLogger(this.getClass());
 
     private final AgentStatModelConverter agentMapper = new AgentStatModelConverter();
     private final ApplicationStatModelConverter appMapper = new ApplicationStatModelConverter();
@@ -28,71 +37,89 @@ public class PinotMappers {
     }
 
     public List<PinotTypeMapper<StatDataPoint>> getMapper() {
-        List<PinotTypeMapper<? extends StatDataPoint>> mappers = List.of(
-                this.getPinotCpuLoadDao(),
-                this.getPinotActiveTraceDao(),
-                this.getPinotJvmGcDao(),
-                this.getPinotJvmGcDetailedDao(),
-                this.getPinotTransactionDao(),
-                this.getPinotResponseTimeDao(),
 
-                this.getPinotDeadlockThreadCountDao(),
-                this.getPinotFileDescriptorDao(),
-                this.getPinotDirectBufferDao(),
+        List<Method> methods = findDeclaredMethods(this.getClass(), this::isTypeMapper);
 
-                this.getPinotTotalThreadCountDao(),
-                this.getPinotLoadedClassDao(),
-                this.getPinotDataSourceListDao()
-        );
-        return (List<PinotTypeMapper<StatDataPoint>>) (List<?>) mappers;
+        List<PinotTypeMapper<StatDataPoint>> mappers = new ArrayList<>();
+        for (Method method : methods) {
+            logger.info("Found PinotTypeMapper : {}", method.getName());
+            PinotTypeMapper<StatDataPoint> mapper = newMapper(method);
+            mappers.add(mapper);
+        }
+        return mappers;
     }
 
-    public PinotTypeMapper<CpuLoadBo> getPinotCpuLoadDao() {
+    private List<Method> findDeclaredMethods(Class<?> clazz, Predicate<Method> filter) {
+        final Method[] methods = clazz.getDeclaredMethods();
+        final List<Method> list = new ArrayList<>();
+        for (Method method : methods) {
+            if (filter.test(method)) {
+                list.add(method);
+            }
+        }
+        return list;
+    }
+
+    @SuppressWarnings("unchecked")
+    private PinotTypeMapper<StatDataPoint> newMapper(Method method) {
+        try {
+            return (PinotTypeMapper<StatDataPoint>) method.invoke(this);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            ReflectionUtils.handleReflectionException(e);
+        }
+        throw new IllegalStateException("Failed to invoke method: " + method.getName());
+    }
+
+    private boolean isTypeMapper(Method method) {
+        return method.getName().startsWith("get") &&
+                PinotTypeMapper.class.isAssignableFrom(method.getReturnType());
+    }
+
+    public PinotTypeMapper<CpuLoadBo> getCpuLoad() {
         return new PinotTypeMapper<>(AgentStatBo::getCpuLoadBos, agentMapper::convertCpuLoad);
     }
 
-
-    public PinotTypeMapper<? extends StatDataPoint> getPinotActiveTraceDao() {
+    public PinotTypeMapper<? extends StatDataPoint> getActiveTrace() {
         return new PinotTypeMapper<>(AgentStatBo::getActiveTraceBos, agentMapper::convertActiveTrace);
     }
 
-    public PinotTypeMapper<JvmGcBo> getPinotJvmGcDao() {
+    public PinotTypeMapper<JvmGcBo> getJvmGc() {
         return new PinotTypeMapper<>(AgentStatBo::getJvmGcBos, agentMapper::convertJvmGc);
     }
 
-    public PinotTypeMapper<JvmGcDetailedBo> getPinotJvmGcDetailedDao() {
+    public PinotTypeMapper<JvmGcDetailedBo> getJvmGcDetailed() {
         return new PinotTypeMapper<>(AgentStatBo::getJvmGcDetailedBos, agentMapper::convertJvmGCDetailed);
     }
 
-    public PinotTypeMapper<TransactionBo> getPinotTransactionDao() {
+    public PinotTypeMapper<TransactionBo> getTransaction() {
         return new PinotTypeMapper<>(AgentStatBo::getTransactionBos, agentMapper::convertTransaction);
     }
 
-    public PinotTypeMapper<ResponseTimeBo> getPinotResponseTimeDao() {
+    public PinotTypeMapper<ResponseTimeBo> getResponseTime() {
         return new PinotTypeMapper<>(AgentStatBo::getResponseTimeBos, agentMapper::convertResponseTime);
     }
 
-    public PinotTypeMapper<DeadlockThreadCountBo> getPinotDeadlockThreadCountDao() {
+    public PinotTypeMapper<DeadlockThreadCountBo> getDeadlockThreadCount() {
         return new PinotTypeMapper<>(AgentStatBo::getDeadlockThreadCountBos, agentMapper::convertDeadlockThreadCount);
     }
 
-    public PinotTypeMapper<FileDescriptorBo> getPinotFileDescriptorDao() {
+    public PinotTypeMapper<FileDescriptorBo> getFileDescriptor() {
         return new PinotTypeMapper<>(AgentStatBo::getFileDescriptorBos, agentMapper::convertFileDescriptor);
     }
 
-    public PinotTypeMapper<DirectBufferBo> getPinotDirectBufferDao() {
+    public PinotTypeMapper<DirectBufferBo> getDirectBuffer() {
         return new PinotTypeMapper<>(AgentStatBo::getDirectBufferBos, agentMapper::convertDirectBuffer);
     }
 
-    public PinotTypeMapper<TotalThreadCountBo> getPinotTotalThreadCountDao() {
+    public PinotTypeMapper<TotalThreadCountBo> getTotalThreadCount() {
         return new PinotTypeMapper<>(AgentStatBo::getTotalThreadCountBos, agentMapper::convertTotalThreadCount);
     }
 
-    public PinotTypeMapper<LoadedClassBo> getPinotLoadedClassDao() {
+    public PinotTypeMapper<LoadedClassBo> getLoadedClass() {
         return new PinotTypeMapper<>(AgentStatBo::getLoadedClassBos, agentMapper::convertLoadedClass);
     }
 
-    public PinotTypeMapper<DataSourceListBo> getPinotDataSourceListDao() {
+    public PinotTypeMapper<DataSourceListBo> getDataSourceList() {
         return new PinotTypeMapper<>(AgentStatBo::getDataSourceListBos, agentMapper::convertDataSource, appMapper::convertFromDataSource);
     }
 }
