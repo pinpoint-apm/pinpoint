848e73813bf819dc7aa7014c65582a4bb7b279f0
jihea.park <jihea.park@navercorp.com>
2025-04-16 08:59:50 +0900

    [#noissue] Revert useExpiredLocalStorage

--- web-frontend/src/main/v3/packages/ui/src/hooks/utility/useLocalStorage.ts
+++ web-frontend/src/main/v3/packages/ui/src/hooks/utility/useLocalStorage.ts
@@ -31,3 +31,26 @@ export const useLocalStorage = <T, K extends LocalStorageKeyTypes = LocalStorage
 
   return [data, setData] as [T, React.Dispatch<React.SetStateAction<T>>];
 };
+
+export const useExpiredLocalStorage = <
+  T extends {
+    value?: unknown;
+    expire: number; // ms
+  },
+  K extends LocalStorageKeyTypes = LocalStorageKeyTypes,
+>(
+  key: K,
+  initialValue?: T,
+) => {
+  const [data, setData] = useLocalStorage(key, initialValue);
+
+  React.useEffect(() => {
+    const now = new Date().getTime();
+
+    if (data?.expire && data?.expire < now) {
+      window.localStorage.removeItem(key);
+    }
+  }, [data]);
+
+  return [data, setData] as [T, React.Dispatch<React.SetStateAction<T>>];
+};
