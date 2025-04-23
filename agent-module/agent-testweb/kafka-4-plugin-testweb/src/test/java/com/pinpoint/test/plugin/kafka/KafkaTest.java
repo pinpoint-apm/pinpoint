49e065173f8451e4e85a9877ed6c523d22302219
Jaehong-Kim <jaehong.kim@navercorp.com>
2025-04-23 15:52:33 +0900

    [#12373] Update kafka plugin for compatibility with kafka 4.x

---
+++ agent-module/agent-testweb/kafka-4-plugin-testweb/src/test/java/com/pinpoint/test/plugin/kafka/KafkaTest.java
@@ -0,0 +1,27 @@
+package com.pinpoint.test.plugin.kafka;
+
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.AutoClose;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.DockerClientFactory;
+import org.testcontainers.kafka.KafkaContainer;
+
+public class KafkaTest {
+    @AutoClose
+    private static KafkaContainer container;
+
+    @BeforeAll
+    public static void beforeClass() {
+        Assumptions.assumeTrue(DockerClientFactory.instance().isDockerAvailable(), "Docker not enabled");
+
+        container = new KafkaContainer("apache/kafka:3.8.1");
+        container.start();
+    }
+
+    @Test
+    public void test() throws Exception {
+        String bootstrapServers = container.getBootstrapServers();
+        System.out.println("Kafka bootstrapServers:" + bootstrapServers);
+    }
+}
