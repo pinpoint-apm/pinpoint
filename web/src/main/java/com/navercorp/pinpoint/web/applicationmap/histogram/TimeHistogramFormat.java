aa05fa2703018efb5be88f3486753c7f11ce9152
emeroad <emeroad@gmail.com>
2025-04-25 14:31:22 +0900

    [#noissue] Refactor TransactionController

--- web/src/main/java/com/navercorp/pinpoint/web/applicationmap/histogram/TimeHistogramFormat.java
+++ web/src/main/java/com/navercorp/pinpoint/web/applicationmap/histogram/TimeHistogramFormat.java
@@ -16,12 +16,16 @@
 
 package com.navercorp.pinpoint.web.applicationmap.histogram;
 
+import java.util.EnumSet;
+import java.util.Set;
+
 public enum TimeHistogramFormat {
     @Deprecated
     V1, // key is slot("1s", "3s", "5s", "Slow", "Error"), value is {timestamp : count}
     V2, // key is timestamp, value is [1s, 3s, 5s, Slow, Error, Avg, Max, Sum, Tot] - LoadHistogram
-    V3;
+    V3; // key is slot("1s", "3s", "5s", "Slow", "Error"), value is {count}, timestamp is in root
 
+    private static final Set<TimeHistogramFormat> VERSIONS = EnumSet.allOf(TimeHistogramFormat.class);
 
     public static TimeHistogramFormat format(boolean useLoadHistogramFormat) {
         if (useLoadHistogramFormat) {
@@ -31,6 +35,10 @@ public enum TimeHistogramFormat {
     }
 
     public static TimeHistogramFormat format(int version) {
+        return format(version, null);
+    }
+
+    public static TimeHistogramFormat format(int version, TimeHistogramFormat defaultVersion) {
         if (version == 3) {
             return V3;
         } else if (version == 2) {
@@ -38,6 +46,19 @@ public enum TimeHistogramFormat {
         } else if (version == 1) {
             return V1;
         }
-        return V2;
+        return defaultVersion;
+    }
+
+    public static TimeHistogramFormat format(String version) {
+        return format(version, null);
+    }
+
+    public static TimeHistogramFormat format(String format, TimeHistogramFormat defaultVersion) {
+        for (TimeHistogramFormat version : VERSIONS) {
+            if (version.name().equalsIgnoreCase(format)) {
+                return version;
+            }
+        }
+        return defaultVersion;
     }
 }
