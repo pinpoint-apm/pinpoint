62658af38759479a824428f3a7de3fabb94b9874
donghun-cho <donghun.cho@navercorp.com>
2025-07-01 16:06:54 +0900

    [#noissue] Add missing /api prefix

--- web/src/main/java/com/navercorp/pinpoint/web/WebMvcConfig.java
+++ web/src/main/java/com/navercorp/pinpoint/web/WebMvcConfig.java
@@ -78,7 +78,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new AdminAuthInterceptor(password))
-                .addPathPatterns("/admin/**");
+                .addPathPatterns("/api/admin/**");
     }
 
     private ResourceHandlerConfigure newResourceHandlerConfigure() {
