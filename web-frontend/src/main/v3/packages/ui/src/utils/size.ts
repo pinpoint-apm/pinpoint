83ce012250d8d9591defec8d43f32466b78fcc64
jihea.park <jihea.park@navercorp.com>
2025-07-01 18:13:41 +0900

    [#noissue] utils/size.ts

---
+++ web-frontend/src/main/v3/packages/ui/src/utils/size.ts
@@ -0,0 +1,10 @@
+export function formatBytes(bytes: number, decimals = 2): string {
+  if (bytes === 0) return '0 Bytes';
+
+  const k = 1024;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  const size = bytes / Math.pow(k, i);
+  return `${parseFloat(size.toFixed(decimals))} ${sizes[i]}`;
+}
