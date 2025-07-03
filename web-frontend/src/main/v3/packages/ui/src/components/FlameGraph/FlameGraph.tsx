73eb6563e462410f1a6c61dbc66f9da494c70028
jihea-park <jihea.park@navercorp.com>
2025-07-03 19:40:02 +0900

    [#12649] Transaction List > Flame Graph async arrow

--- web-frontend/src/main/v3/packages/ui/src/components/FlameGraph/FlameGraph.tsx
+++ web-frontend/src/main/v3/packages/ui/src/components/FlameGraph/FlameGraph.tsx
@@ -8,18 +8,26 @@ import { FlameTimeline } from './FlameTimeline';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '../ui';
 import { GoZoomIn, GoZoomOut } from 'react-icons/go';
 import { Button } from '../ui';
+import { TraceViewerData } from '@pinpoint-fe/ui/src/constants';
+import { FlameArrow } from './FlameArrow';
 
 export interface FlameGraphProps<T>
   extends Pick<FlameNodeProps<T>, 'customNodeStyle' | 'customTextStyle' | 'onClickNode'> {
   data: FlameNodeType<T>[][];
   start?: number;
   end?: number;
+  nodeFlows?: {
+    prev: string[];
+    selected?: string;
+    next: string[];
+  };
 }
 
 export const FlameGraph = <T,>({
   data,
   start = 0,
   end = 0,
+  nodeFlows,
   onClickNode,
   customNodeStyle,
   customTextStyle,
@@ -27,8 +35,12 @@ export const FlameGraph = <T,>({
   const widthOffset = end - start || 1;
   const [config] = React.useState(flameGraphDefaultConfig);
   const [zoom, setZoom] = React.useState(1);
+  const [arrows, setArrows] = React.useState({
+    prev: [] as FlameNodeType<T>[],
+    selected: undefined as FlameNodeType<T> | undefined,
+    next: [] as FlameNodeType<T>[],
+  });
 
-  const prevDepth = React.useRef(0);
   const containerRef = React.useRef<HTMLDivElement>(null);
   const svgRef = React.useRef<SVGSVGElement>(null);
   const [width, setWidth] = React.useState(0);
@@ -95,6 +107,51 @@ export const FlameGraph = <T,>({
     }
   }, []);
 
+  React.useEffect(() => {
+    if (!nodeFlows?.selected || (nodeFlows?.prev?.length === 0 && nodeFlows?.next?.length === 0)) {
+      setArrows({
+        prev: [],
+        selected: undefined,
+        next: [],
+      });
+      return;
+    }
+  }, [nodeFlows]);
+
+  const styleArrow = React.useCallback(
+    (node: FlameNodeType<T>) => {
+      const detail = node?.detail as TraceViewerData.TraceEvent | undefined;
+
+      if (nodeFlows?.selected && nodeFlows?.selected === detail?.args?.id) {
+        return {
+          type: 'selected',
+          node,
+        };
+      }
+
+      if (detail?.args?.id && nodeFlows?.prev?.includes(detail?.args?.id)) {
+        return {
+          type: 'prev',
+          node,
+        };
+      }
+
+      if (detail?.args?.id && nodeFlows?.next?.includes(detail?.args?.id)) {
+        return {
+          type: 'next',
+          node,
+        };
+      }
+
+      for (const child of node.children || []) {
+        return styleArrow(child);
+      }
+
+      return;
+    },
+    [nodeFlows],
+  );
+
   const styleNode = (node: FlameNodeType<T>, depth = 0, xOffset = 0, yOffset = 0) => {
     const children = node.children || [];
     const { height, padding } = config;
@@ -110,6 +167,54 @@ export const FlameGraph = <T,>({
       currentXOffset += widthPerNode;
     });
   };
+
+  const dataForRender = React.useMemo(() => {
+    let pd = 0;
+    const prevNodes: FlameNodeType<T>[] = [];
+    const nextNodes: FlameNodeType<T>[] = [];
+    let selectedNode;
+
+    const returnData = data.map((group, i) => {
+      if (i === 0) pd = 0;
+      else {
+        const prevGroupMaxDepth = Math.max(...data[i - 1].map((node) => getMaxDepth(node)));
+        pd = prevGroupMaxDepth + pd + 1;
+      }
+
+      // node별 렌더링
+      return group.map((node) => {
+        const { height, padding, color } = config;
+        const newNode = cloneDeep(node);
+        const yOffset = pd * height.node + padding.group * i;
+
+        styleNode(newNode, 0, 0, yOffset);
+        const temp = styleArrow(newNode);
+
+        if (temp?.type === 'selected') {
+          selectedNode = temp.node;
+        } else if (temp?.type === 'prev') {
+          prevNodes.push(temp.node);
+        } else if (temp?.type === 'next') {
+          nextNodes.push(temp.node);
+        }
+
+        return {
+          ...newNode,
+          yOffset,
+          color,
+          padding,
+        };
+      });
+    });
+
+    setArrows({
+      prev: prevNodes,
+      selected: selectedNode,
+      next: nextNodes,
+    });
+
+    return returnData;
+  }, [data, config, widthRatio, start]);
 
   function getContainerHeight() {
     const { height, padding } = config;
@@ -179,28 +284,14 @@ export const FlameGraph = <T,>({
             <FlameAxis width={containerWidth} zoom={zoom} />
             {containerWidth &&
               // group별 렌더링
-              data.map((group, i) => {
-                if (i === 0) prevDepth.current = 0;
-                else {
-                  const prevGroupMaxDepth = Math.max(
-                    ...data[i - 1].map((node) => getMaxDepth(node)),
-                  );
-                  prevDepth.current = prevGroupMaxDepth + prevDepth.current + 1;
-                }
-
+              dataForRender.map((group, i) => {
                 // node별 렌더링
                 return group.map((node) => {
-                  const { height, padding, color } = config;
-                  const newNode = cloneDeep(node);
-                  const yOffset = prevDepth.current * height.node + padding.group * i;
-
-                  styleNode(newNode, 0, 0, yOffset);
-
                   return (
                     <React.Fragment key={node.id}>
                       <FlameNode
                         scrollLeft={scrollLeft}
-                        node={newNode}
+                        node={node}
                         svgRef={svgRef}
                         customNodeStyle={customNodeStyle}
                         customTextStyle={customTextStyle}
@@ -208,15 +299,37 @@ export const FlameGraph = <T,>({
                       />
                       <line
                         x1={0}
-                        y1={yOffset - padding.group / 2 + padding.top}
+                        y1={node?.yOffset - node?.padding?.group / 2 + node?.padding?.top}
                         x2={containerWidth}
-                        y2={yOffset - padding.group / 2 + padding.top}
-                        stroke={color.axis}
+                        y2={node?.yOffset - node?.padding.group / 2 + node?.padding.top}
+                        stroke={node?.color.axis}
                       />
                     </React.Fragment>
                   );
                 });
               })}
+            {arrows.next.map((nextNode, i) => {
+              return (
+                <FlameArrow
+                  key={i}
+                  x1={(arrows?.selected?.x || 0) + (arrows?.selected?.width || 0)}
+                  y1={(arrows?.selected?.y || 0) + (arrows?.selected?.height || 0) / 2}
+                  x2={nextNode?.x}
+                  y2={(nextNode?.y || 0) + (nextNode?.height || 0) / 2}
+                />
+              );
+            })}
+            {arrows.prev.map((prevNode, i) => {
+              return (
+                <FlameArrow
+                  key={i}
+                  x1={(prevNode?.x || 0) + (prevNode?.width || 0)}
+                  y1={(prevNode?.y || 0) + (prevNode?.height || 0) / 2}
+                  x2={arrows?.selected?.x}
+                  y2={(arrows?.selected?.y || 0) + (arrows?.selected?.height || 0) / 2}
+                />
+              );
+            })}
           </svg>
         </div>
       </div>
