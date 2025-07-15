5bb32c1e178af41a5ee88f31b3524bedf373152e
emeroad <emeroad@gmail.com>
2025-07-15 16:44:33 +0900

    [#noissue] Reduce memory usage of FuzzyRowKey

--- commons-server/src/main/java/com/navercorp/pinpoint/common/server/bo/serializer/agent/ApplicationNameRowKeyEncoder.java
+++ commons-server/src/main/java/com/navercorp/pinpoint/common/server/bo/serializer/agent/ApplicationNameRowKeyEncoder.java
@@ -1,6 +1,25 @@
+/*
+ * Copyright 2025 NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.navercorp.pinpoint.common.server.bo.serializer.agent;
 
 import com.navercorp.pinpoint.common.hbase.HbaseTableConstants;
+import com.navercorp.pinpoint.common.server.util.RowKeyUtils;
+import com.navercorp.pinpoint.common.util.BytesUtils;
+import com.navercorp.pinpoint.common.util.TimeUtils;
 
 public class ApplicationNameRowKeyEncoder extends IdRowKeyEncoder {
 
@@ -11,5 +30,11 @@ public class ApplicationNameRowKeyEncoder extends IdRowKeyEncoder {
     @Override
     public byte[] encodeRowKey(String applicationName, long timestamp) {
         return super.encodeRowKey(applicationName, timestamp);
+    }
+
+    public byte[] encodeFuzzyRowKey(String id, long timestamp, byte fuzzySlotKey) {
+        byte[] idKey = BytesUtils.toBytes(id);
+        long reverseTimestamp = TimeUtils.reverseTimeMillis(timestamp);
+        return RowKeyUtils.concatFixedByteAndLongFuzzySlot(idKey, max, reverseTimestamp, fuzzySlotKey);
     }
 }
