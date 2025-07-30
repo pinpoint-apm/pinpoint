158a1eb9ed4ccb74b8f54441f2acccf25757eee9
emeroad <emeroad@gmail.com>
2025-07-30 15:29:08 +0900

    [#12726] Bump grpc from 1.72.0 to 1.74.0

--- collector/src/test/java/com/navercorp/pinpoint/collector/receiver/grpc/AgentClientMock.java
+++ collector/src/test/java/com/navercorp/pinpoint/collector/receiver/grpc/AgentClientMock.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2019 NAVER Corp.
+ * Copyright 2025 NAVER Corp.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -251,12 +251,5 @@ public class AgentClientMock {
             return result;
         }
 
-        @Override
-        public void requestConnection() {
-            LoadBalancer.Subchannel subchannel = result.getSubchannel();
-            if (subchannel != null) {
-                subchannel.requestConnection();
-            }
-        }
     }
 }
