b59b17a720db14abe935f648552d36e9961dcc72
emeroad <emeroad@gmail.com>
2025-08-01 14:35:28 +0900

    [#noissue] Add qualifierToShort to CellUtils

--- commons-hbase/src/test/java/com/navercorp/pinpoint/common/hbase/util/CellUtilsTest.java
+++ commons-hbase/src/test/java/com/navercorp/pinpoint/common/hbase/util/CellUtilsTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.navercorp.pinpoint.common.hbase.util;
 
 import org.apache.hadoop.hbase.Cell;
@@ -24,6 +40,18 @@ public class CellUtilsTest {
         when(result.rawCells()).thenReturn(cells);
 
         Assertions.assertEquals(value, CellUtils.rowToString(result));
+    }
+
+    @Test
+    public void qualifierToShort() {
+        Cell cell = mock(Cell.class);
+        short value = 5;
+        byte[] bytes = Bytes.toBytes(value);
+        when(cell.getQualifierArray()).thenReturn(bytes);
+        when(cell.getQualifierOffset()).thenReturn(0);
+        when(cell.getQualifierLength()).thenReturn(bytes.length);
+
+        Assertions.assertEquals(value, CellUtils.qualifierToShort(cell));
     }
 
     @Test
