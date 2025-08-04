af5b807a2a83a65eb7167b878a313cadb2908c3b
jihea.park <jihea.park@navercorp.com>
2025-08-04 17:03:19 +0900

    [#12753] Improved servermap representation of single transactions

--- web-frontend/src/main/v3/packages/ui/src/components/ServerMap/ServerMapCore.tsx
+++ web-frontend/src/main/v3/packages/ui/src/components/ServerMap/ServerMapCore.tsx
@@ -96,6 +96,7 @@ export const ServerMapCore = ({
   React.useEffect(() => {
     const { nodeDataArray = [], linkDataArray = [] } = data?.applicationMapData || {};
     const nodeTypes = new Set(nodeDataArray.map((node) => node.serviceType));
+    const isFilteredMap = 'lastFetchedTimestamp' in (data || {});
 
     allServiceTypes.current = Array.from(nodeTypes);
 
@@ -106,6 +107,7 @@ export const ServerMapCore = ({
         type: node.serviceType,
         imgPath: getServerImagePath(node),
         transactionInfo: getTransactionInfo(node),
+        timeSeriesApdexInfo: isFilteredMap ? undefined : getTimeSeriesApdexInfo(node), // filtered map에서는 시간 시리즈 Apdex 정보를 사용하지 않는다.
         shouldNotMerge: () => {
           return (
             node.isWas ||
@@ -150,6 +152,73 @@ export const ServerMapCore = ({
         instanceCount: node.instanceCount,
       };
     }
+  };
+
+  const getTimeSeriesApdexInfo = (node: GetServerMap.NodeData | FilteredMap.NodeData) => {
+    const { isWas, isAuthorized, timeSeriesHistogram } = node;
+
+    if (!isAuthorized || !timeSeriesHistogram) {
+      return [];
+    }
+
+    const timestamp = data?.applicationMapData?.timestamp || [];
+    const maxSlots = 24; // 전체 원을 최대 24개의 slot으로 나눈다.
+    const dataLength = timestamp?.length;
+
+    const oneSecond =
+      timeSeriesHistogram?.find((time) => time.key === '1s' || time.key === '100ms')?.values || [];
+    const threeSecond =
+      timeSeriesHistogram?.find((time) => time.key === '3s' || time.key === '300ms')?.values || [];
+    const total = timeSeriesHistogram?.find((time) => time.key === 'Tot')?.values || [];
+
+    if (
+      oneSecond.length !== dataLength ||
+      threeSecond.length !== dataLength ||
+      total.length !== dataLength
+    ) {
+      return [];
+    }
+
+    if (dataLength <= maxSlots) {
+      return timestamp?.map((time, index) => {
+        if (total?.[index] === 0) {
+          return 1;
+        }
+
+        return (oneSecond?.[index] * 1 + threeSecond?.[index] * 0.5) / total?.[index];
+      });
+    }
+
+    const groupSize = Math.floor(dataLength / maxSlots); // 각 slot에 최소 몇 개의 데이터씩 들어가는지
+    let remainder = dataLength % maxSlots; // 나머지는 앞쪽 slot에 하나씩 더 넣기
+
+    let i = 0;
+    const result = [];
+
+    while (i < dataLength) {
+      const currentSlotSize = groupSize + (remainder > 0 ? 1 : 0);
+      remainder = Math.max(0, remainder - 1); // 나머지 하나 줄이기
+
+      const mergedTotal = total?.slice(i, i + currentSlotSize)?.reduce((t, acc) => {
+        return acc + t;
+      }, 0);
+
+      if (mergedTotal === 0) {
+        result.push(1);
+      } else {
+        const mergedOneSecond = oneSecond?.slice(i, i + currentSlotSize)?.reduce((t, acc) => {
+          return acc + t;
+        }, 0);
+        const mergedThreeSecond = threeSecond?.slice(i, i + currentSlotSize)?.reduce((t, acc) => {
+          return acc + t;
+        }, 0);
+
+        result.push((mergedOneSecond * 1 + mergedThreeSecond * 0.5) / mergedTotal);
+      }
+      i += currentSlotSize;
+    }
+
+    return result;
   };
 
   const handleClickBackground: ServerMapCoreProps['onClickBackground'] = ({
