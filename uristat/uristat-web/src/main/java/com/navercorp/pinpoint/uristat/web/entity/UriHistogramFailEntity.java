196a17fdb31862c70fdf88402f460e4d9c5c1e15
Seoyoung Park <se0y0ung.park@navercorp.com>
2025-08-13 10:48:27 +0900

    [#noissue] Revert "[#noissue] Simplify minimap in uristats"
    This reverts commit 22c257f0db407368e7494a5e238213f39880a784.

--- uristat/uristat-web/src/main/java/com/navercorp/pinpoint/uristat/web/entity/UriHistogramFailEntity.java
+++ uristat/uristat-web/src/main/java/com/navercorp/pinpoint/uristat/web/entity/UriHistogramFailEntity.java
@@ -9,23 +9,87 @@ import java.util.List;
  */
 public class UriHistogramFailEntity extends UriEntity {
     // fail
-    private double fail;
+    private double fail0;
+    private double fail1;
+    private double fail2;
+    private double fail3;
+    private double fail4;
+    private double fail5;
+    private double fail6;
+    private double fail7;
 
 
     public UriHistogramFailEntity() {
     }
 
-    public double getFail() {
-        return fail;
+    public double getFail0() {
+        return fail0;
     }
 
-    public void setFail(double fail) {
-        this.fail = fail;
+    public void setFail0(double fail0) {
+        this.fail0 = fail0;
+    }
+
+    public double getFail1() {
+        return fail1;
+    }
+
+    public void setFail1(double fail1) {
+        this.fail1 = fail1;
+    }
+
+    public double getFail2() {
+        return fail2;
+    }
+
+    public void setFail2(double fail2) {
+        this.fail2 = fail2;
+    }
+
+    public double getFail3() {
+        return fail3;
+    }
+
+    public void setFail3(double fail3) {
+        this.fail3 = fail3;
+    }
+
+    public double getFail4() {
+        return fail4;
+    }
+
+    public void setFail4(double fail4) {
+        this.fail4 = fail4;
+    }
+
+    public double getFail5() {
+        return fail5;
+    }
+
+    public void setFail5(double fail5) {
+        this.fail5 = fail5;
+    }
+
+    public double getFail6() {
+        return fail6;
+    }
+
+    public void setFail6(double fail6) {
+        this.fail6 = fail6;
+    }
+
+    public double getFail7() {
+        return fail7;
+    }
+
+    public void setFail7(double fail7) {
+        this.fail7 = fail7;
     }
 
     public List<Double> getFailureHistogram() {
         return Doubles.asList(
-                this.fail
+                this.fail0, this.fail1, this.fail2, this.fail3,
+                this.fail4, this.fail5, this.fail6, this.fail7
         );
     }
 }
