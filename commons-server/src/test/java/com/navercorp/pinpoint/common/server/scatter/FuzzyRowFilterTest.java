7c0f40801041f44f26b7b706047452f316666bea
emeroad <emeroad@gmail.com>
2025-08-14 11:54:13 +0900

    [#noissue] Cleanup

--- commons-server/src/test/java/com/navercorp/pinpoint/common/server/scatter/FuzzyRowFilterTest.java
+++ commons-server/src/test/java/com/navercorp/pinpoint/common/server/scatter/FuzzyRowFilterTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.navercorp.pinpoint.common.server.scatter;
 
 import org.apache.hadoop.hbase.KeyValue;
@@ -7,6 +23,7 @@ import org.apache.hadoop.hbase.util.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -28,18 +45,19 @@ public class FuzzyRowFilterTest {
 
         KeyValue keyValue = new KeyValue(new byte[]{0, 1}, 1L);
         Filter.ReturnCode returnCode = filter.filterCell(keyValue);
-        Assertions.assertEquals(returnCode, Filter.ReturnCode.SEEK_NEXT_USING_HINT);
+        Assertions.assertEquals(Filter.ReturnCode.SEEK_NEXT_USING_HINT, returnCode);
 
         KeyValue keyValue2 = new KeyValue(new byte[]{0, 5}, 1L);
         Filter.ReturnCode returnCode2 = filter.filterCell(keyValue2);
-        Assertions.assertEquals(returnCode2, Filter.ReturnCode.INCLUDE);
+        Assertions.assertEquals(Filter.ReturnCode.INCLUDE, returnCode2);
     }
 
     @Test
+    @Disabled
     public void test_reverseTimeStamp() {
         for (int i = 0; i < 560; i += 1) {
             short j = reverseTimestamp((short) i);
-            logger.debug(i + " hex:" + Integer.toHexString(i) + " rev:" + j + " rhex:" + Integer.toHexString(j));
+            logger.debug("{} hex:{} rev:{} rhex:{}", i, Integer.toHexString(i), j, Integer.toHexString(j));
         }
     }
 
