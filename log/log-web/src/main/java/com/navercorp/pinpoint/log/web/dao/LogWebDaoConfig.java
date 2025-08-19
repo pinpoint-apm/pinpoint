07c7809112813d71d6cd806a920a73cbd0dd50b7
donghun-cho <donghun.cho@navercorp.com>
2025-08-19 20:14:07 +0900

    [#noissue] Remove unnecessary ObjectMapper bean

--- log/log-web/src/main/java/com/navercorp/pinpoint/log/web/dao/LogWebDaoConfig.java
+++ log/log-web/src/main/java/com/navercorp/pinpoint/log/web/dao/LogWebDaoConfig.java
@@ -15,7 +15,6 @@
  */
 package com.navercorp.pinpoint.log.web.dao;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.navercorp.pinpoint.channel.ChannelProviderRepository;
 import com.navercorp.pinpoint.channel.redis.pubsub.RedisPubSubConfig;
 import com.navercorp.pinpoint.channel.service.FluxChannelServiceProtocol;
@@ -24,7 +23,6 @@ import com.navercorp.pinpoint.channel.service.client.FluxChannelServiceClient;
 import com.navercorp.pinpoint.log.LogServiceProtocolConfig;
 import com.navercorp.pinpoint.log.vo.FileKey;
 import com.navercorp.pinpoint.log.vo.LogPile;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -35,14 +33,8 @@ import reactor.core.scheduler.Schedulers;
  * @author youngjin.kim2
  */
 @Configuration
-@Import({ RedisPubSubConfig.class, LogServiceProtocolConfig.class })
+@Import({RedisPubSubConfig.class, LogServiceProtocolConfig.class})
 public class LogWebDaoConfig {
-
-    @Bean
-    @ConditionalOnMissingBean(ObjectMapper.class)
-    ObjectMapper objectMapper() {
-        return new ObjectMapper();
-    }
 
     @Bean
     FluxChannelServiceClient<FileKey, LogPile> liveTailClient(
