656b9aa6a7a5c8b807ab05019016aa7b5a3d6c6e
emeroad <emeroad@gmail.com>
2025-08-29 14:15:46 +0900

    [#12842] Bump spring-retry from 2.0.6 to 2.0.12

--- batch/src/main/java/com/navercorp/pinpoint/batch/alarm/dao/pinot/PinotAlarmDao.java
+++ batch/src/main/java/com/navercorp/pinpoint/batch/alarm/dao/pinot/PinotAlarmDao.java
@@ -1,15 +1,15 @@
 /*
- * Copyright 2024 NAVER Corp.
+ * Copyright 2025 NAVER Corp.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
@@ -38,7 +38,7 @@ import java.util.concurrent.CompletableFuture;
 /**
  * @author minwoo-jung
  */
-@Retryable(maxAttempts = 3, value = {Exception.class})
+@Retryable(maxAttempts = 3, retryFor = {Exception.class})
 public class PinotAlarmDao implements AlarmDao {
 
     private static final String NAMESPACE = PinotAlarmDao.class.getName() + ".";
