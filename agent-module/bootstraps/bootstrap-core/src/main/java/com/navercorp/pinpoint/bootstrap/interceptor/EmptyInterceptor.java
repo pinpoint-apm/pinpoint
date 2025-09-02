8762c6dedc915fa3e8582413e21b80b5a2adf719
emeroad <emeroad@gmail.com>
2025-09-02 17:41:29 +0900

    [#noissue] Remove unused defaultString method

--- agent-module/bootstraps/bootstrap-core/src/main/java/com/navercorp/pinpoint/bootstrap/interceptor/EmptyInterceptor.java
+++ agent-module/bootstraps/bootstrap-core/src/main/java/com/navercorp/pinpoint/bootstrap/interceptor/EmptyInterceptor.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2024 NAVER Corp.
+ * Copyright 2025 NAVER Corp.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -44,10 +44,6 @@ public class EmptyInterceptor implements StaticAroundInterceptor, AroundIntercep
 
     @Override
     public void after(Object target, Object[] args, Object result, Throwable throwable) {
-    }
-
-    public static String defaultString(final Object object) {
-        return String.valueOf(object);
     }
 
     @Override
--- agent-module/bootstraps/bootstrap-core/src/main/java/com/navercorp/pinpoint/bootstrap/interceptor/LoggingInterceptor.java
+++ agent-module/bootstraps/bootstrap-core/src/main/java/com/navercorp/pinpoint/bootstrap/interceptor/LoggingInterceptor.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2014 NAVER Corp.
+ * Copyright 2025 NAVER Corp.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -35,27 +35,24 @@ public class LoggingInterceptor implements StaticAroundInterceptor, AroundInterc
 
     @Override
     public void before(Object target, String className, String methodName, String parameterDescription, Object[] args) {
-        logger.accept("before " + defaultString(target) + " " + className + "." + methodName + parameterDescription + " args:" + Arrays.toString(args));
+        logger.accept("before " + target + " " + className + "." + methodName + parameterDescription + " args:" + Arrays.toString(args));
     }
 
     @Override
     public void after(Object target, String className, String methodName, String parameterDescription, Object[] args, Object result, Throwable throwable) {
-        logger.accept("after " + defaultString(target) + " " + className + "." + methodName + parameterDescription + " args:" + Arrays.toString(args) + " result:" + result + " Throwable:" + throwable);
+        logger.accept("after " + target + " " + className + "." + methodName + parameterDescription + " args:" + Arrays.toString(args) + " result:" + result + " Throwable:" + throwable);
     }
 
     @Override
     public void before(Object target, Object[] args) {
-        logger.accept("before " + defaultString(target) + " args:" + Arrays.toString(args));
+        logger.accept("before " + target + " args:" + Arrays.toString(args));
     }
 
     @Override
     public void after(Object target, Object[] args, Object result, Throwable throwable) {
-        logger.accept("after " + defaultString(target) + " args:" + Arrays.toString(args) + " result:" + result + " Throwable:" + throwable);
+        logger.accept("after " + target + " args:" + Arrays.toString(args) + " result:" + result + " Throwable:" + throwable);
     }
 
-    public static String defaultString(final Object object) {
-        return String.valueOf(object);
-    }
 
     @Override
     public void before(Object target, int apiId, Object[] args) {
