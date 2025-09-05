38c25423b50c5d4c66a6d8ba71b2afd6d9ca98e0
ga-ram <galam.choi@navercorp.com>
2025-09-05 12:05:44 +0900

    [#12868] Use property `pinpoint.modules.*.systemmetric.enabled` instead of profile `metric`

--- metric-module/metric/src/main/java/com/navercorp/pinpoint/metric/collector/MetricCollectorApp.java
+++ metric-module/metric/src/main/java/com/navercorp/pinpoint/metric/collector/MetricCollectorApp.java
@@ -1,45 +1,34 @@
+/*
+ * Copyright 2025 NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.navercorp.pinpoint.metric.collector;
 
-import com.navercorp.pinpoint.metric.collector.cache.MetricCacheConfiguration;
-import com.navercorp.pinpoint.metric.collector.config.MetricCollectorProperties;
-import com.navercorp.pinpoint.metric.collector.config.MetricKafkaConfiguration;
-import com.navercorp.pinpoint.metric.common.config.MetricCollectorPinotDaoConfiguration;
-import com.navercorp.pinpoint.pinot.config.PinotConfiguration;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
-import org.springframework.boot.autoconfigure.transaction.TransactionAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Profile;
 
 @SpringBootConfiguration
-@EnableAutoConfiguration(exclude = {
-        DataSourceAutoConfiguration.class,
-        DataSourceTransactionManagerAutoConfiguration.class,
-        TransactionAutoConfiguration.class,
-        KafkaAutoConfiguration.class
-})
-@ComponentScan(basePackages = {
-        "com.navercorp.pinpoint.metric.collector.dao",
-        "com.navercorp.pinpoint.metric.collector.service",
-        "com.navercorp.pinpoint.metric.common.model",
-        "com.navercorp.pinpoint.metric.common",
-        "com.navercorp.pinpoint.common.server.util",
-        "com.navercorp.pinpoint.metric.collector.controller"
-})
 @Import({
-        MetricAppPropertySources.class,
-        MetricCacheConfiguration.class,
-        PinotConfiguration.class,
-        MetricCollectorPinotDaoConfiguration.class,
-        MetricKafkaConfiguration.class,
-        MetricCollectorProperties.class,
         MetricCollectorConfig.class
 })
-@Profile("metric")
+@EnableAutoConfiguration(exclude = {
+        DataSourceAutoConfiguration.class
+})
 public class MetricCollectorApp {
 
 }
