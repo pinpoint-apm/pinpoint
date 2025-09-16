03f18ff7440de8a4dfa445c8a4ab24915ed07d9b
emeroad <emeroad@gmail.com>
2025-09-16 15:02:51 +0900

    [#noissue] Remove duplicate mapLinkRowKeyDistributor

--- web/src/main/java/com/navercorp/pinpoint/web/applicationmap/config/MapMapperConfiguration.java
+++ web/src/main/java/com/navercorp/pinpoint/web/applicationmap/config/MapMapperConfiguration.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.navercorp.pinpoint.web.applicationmap.config;
 
 
@@ -35,7 +51,7 @@ public class MapMapperConfiguration {
 
     @Bean
     public RowMapperFactory<LinkDataMap> mapOutLinkMapper(ApplicationFactory applicationFactory,
-                                                          @Qualifier("mapOutLinkRowKeyDistributor")
+                                                          @Qualifier("mapLinkRowKeyDistributor")
                                                           RowKeyDistributorByHashPrefix rowKeyDistributor) {
         return (windowFunction) -> new OutLinkMapper(applicationFactory, rowKeyDistributor, LinkFilter::skip, windowFunction);
     }
@@ -43,7 +59,7 @@ public class MapMapperConfiguration {
     @Bean
     public RowMapperFactory<LinkDataMap> mapInLinkMapper(ServiceTypeRegistryService registry,
                                                          ApplicationFactory applicationFactory,
-                                                         @Qualifier("mapInLinkRowKeyDistributor")
+                                                         @Qualifier("mapLinkRowKeyDistributor")
                                                          RowKeyDistributorByHashPrefix rowKeyDistributor) {
         return (windowFunction) -> new InLinkMapper(registry, applicationFactory, rowKeyDistributor, LinkFilter::skip, windowFunction);
     }
