57575a6e95a8f2383bf7cb4dac6ddc2b1d7ac275
emeroad <emeroad@gmail.com>
2025-09-30 10:54:28 +0900

    [#noissue] Refactor EventPublisher

--- commons-server/src/main/java/com/navercorp/pinpoint/common/server/event/SpanChunkInsertEvent.java
+++ commons-server/src/main/java/com/navercorp/pinpoint/common/server/event/SpanChunkInsertEvent.java
@@ -1,23 +1,38 @@
-package com.navercorp.pinpoint.common.server.event;
+/*
+ * Copyright 2025 NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import com.navercorp.pinpoint.common.server.bo.SpanChunkBo;
+package com.navercorp.pinpoint.common.server.event;
 
 import java.util.Objects;
 
 public class SpanChunkInsertEvent {
-    private final SpanChunkBo spanChunkBo;
+    private final ContextData contextData;
     private final boolean success;
 
-    public SpanChunkInsertEvent(SpanChunkBo spanChunkBo, boolean success) {
-        this.spanChunkBo = Objects.requireNonNull(spanChunkBo, "spanChunkBo");
+    public SpanChunkInsertEvent(ContextData contextData, boolean success) {
+        this.contextData = Objects.requireNonNull(contextData, "contextData");
         this.success = success;
     }
 
-    public SpanChunkBo getSpanChunkBo() {
-        return spanChunkBo;
+    public ContextData getContextData() {
+        return contextData;
     }
 
     public boolean isSuccess() {
         return success;
     }
+
 }
