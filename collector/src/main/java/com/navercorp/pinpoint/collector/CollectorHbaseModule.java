1e820217a01461c360a676d94ca04f352101d483
emeroad <emeroad@gmail.com>
2025-10-02 15:37:48 +0900

    [#noissue] Remove unnecessary `collector.scatter.serverside-scan` property

--- collector/src/main/java/com/navercorp/pinpoint/collector/CollectorHbaseModule.java
+++ collector/src/main/java/com/navercorp/pinpoint/collector/CollectorHbaseModule.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -35,7 +35,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -68,7 +67,6 @@ public class CollectorHbaseModule {
     private final Logger logger = LogManager.getLogger(CollectorHbaseModule.class);
 
     @Bean
-    @ConditionalOnProperty(name = "collector.scatter.serverside-scan", havingValue = "v2", matchIfMissing = true)
     public RowKeyEncoder<SpanBo> applicationIndexRowKeyEncoder(ApplicationNameRowKeyEncoder rowKeyEncoder,
                                                                  @Qualifier("applicationTraceIndexDistributor")
                                                                  RowKeyDistributor rowKeyDistributor) {
