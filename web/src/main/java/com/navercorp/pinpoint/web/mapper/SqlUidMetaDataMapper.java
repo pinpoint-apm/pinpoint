755b890b8415b9065bb3d8a7be4549891591e6cc
emeroad <emeroad@gmail.com>
2025-10-13 14:14:59 +0900

    [#noissue] Refactor SqlUidMetaDataMapper

--- web/src/main/java/com/navercorp/pinpoint/web/mapper/SqlUidMetaDataMapper.java
+++ web/src/main/java/com/navercorp/pinpoint/web/mapper/SqlUidMetaDataMapper.java
@@ -18,13 +18,14 @@ package com.navercorp.pinpoint.web.mapper;
 
 import com.navercorp.pinpoint.common.hbase.HbaseTables;
 import com.navercorp.pinpoint.common.hbase.RowMapper;
+import com.navercorp.pinpoint.common.hbase.util.CellUtils;
 import com.navercorp.pinpoint.common.server.bo.SqlUidMetaDataBo;
 import com.navercorp.pinpoint.common.server.bo.serializer.RowKeyDecoder;
 import com.navercorp.pinpoint.common.server.bo.serializer.metadata.uid.UidMetaDataRowKey;
 import com.navercorp.pinpoint.common.server.bo.serializer.metadata.uid.UidMetadataDecoder;
 import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.CellUtil;
 import org.apache.hadoop.hbase.client.Result;
-import org.apache.hadoop.hbase.util.Bytes;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -33,7 +34,7 @@ import java.util.List;
 
 @Component
 public class SqlUidMetaDataMapper implements RowMapper<List<SqlUidMetaDataBo>> {
-    private final static String SQL_UID_METADATA_CF_SQL_QUALI_SQLSTATEMENT = Bytes.toString(HbaseTables.SQL_UID_METADATA_SQL.QUALIFIER_SQLSTATEMENT);
+    private final static byte[] SQL_UID_METADATA_CF_SQL_QUALI_SQLSTATEMENT = HbaseTables.SQL_UID_METADATA_SQL.QUALIFIER_SQLSTATEMENT;
 
     private final RowKeyDecoder<UidMetaDataRowKey> decoder = new UidMetadataDecoder();
 
@@ -52,14 +53,11 @@ public class SqlUidMetaDataMapper implements RowMapper<List<SqlUidMetaDataBo>> {
         List<SqlUidMetaDataBo> sqlUidMetaDataList = new ArrayList<>();
 
         for (Cell cell : result.rawCells()) {
-            String sql = Bytes.toString(cell.getQualifierArray(), cell.getQualifierOffset(), cell.getQualifierLength());
-
-            if (SQL_UID_METADATA_CF_SQL_QUALI_SQLSTATEMENT.equals(sql)) {
-                sql = Bytes.toString(cell.getValueArray(), cell.getValueOffset(), cell.getValueLength());
+            if (CellUtil.matchingQualifier(cell, SQL_UID_METADATA_CF_SQL_QUALI_SQLSTATEMENT)) {
+                String sqlStatement = CellUtils.valueToString(cell);
+                SqlUidMetaDataBo sqlUidMetaDataBo = new SqlUidMetaDataBo(key.getAgentId(), key.getAgentStartTime(), key.getUid(), sqlStatement);
+                sqlUidMetaDataList.add(sqlUidMetaDataBo);
             }
-
-            SqlUidMetaDataBo sqlUidMetaDataBo = new SqlUidMetaDataBo(key.getAgentId(), key.getAgentStartTime(), key.getUid(), sql);
-            sqlUidMetaDataList.add(sqlUidMetaDataBo);
         }
         return sqlUidMetaDataList;
     }
