4f9085eac4a4fe60af1ade21a8cc3e4d446a830d
emeroad <emeroad@gmail.com>
2025-10-14 17:15:00 +0900

    [#12948] Refactor jitter-based scheduling

--- collector/src/main/java/com/navercorp/pinpoint/collector/config/SchedulerConfiguration.java
+++ collector/src/main/java/com/navercorp/pinpoint/collector/config/SchedulerConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 NAVER Corp.
+ * Copyright 2025 NAVER Corp.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,17 +12,20 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 package com.navercorp.pinpoint.collector.config;
 
 import com.navercorp.pinpoint.collector.dao.CachedStatisticsDao;
 import com.navercorp.pinpoint.collector.scheduler.AvgMaxLinkScheduler;
+import com.navercorp.pinpoint.collector.scheduler.JitterStartTimeDistributor;
+import com.navercorp.pinpoint.collector.scheduler.StartTimeDistributor;
 import com.navercorp.pinpoint.collector.scheduler.StatisticsLinkScheduler;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Scope;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
@@ -34,6 +37,15 @@ import java.util.List;
         StatisticsLinkScheduler.class
 })
 public class SchedulerConfiguration {
+
+    @Bean
+    @Scope("prototype")
+    public StartTimeDistributor startTimeDistributor(@Value("${collector.map-link.avg.flush-period:5000}") long flushPeriod,
+                                                     List<CachedStatisticsDao> statisticsDaoList,
+                                                     @Value("${collector.starttime-distributor.spread:0.4}") double spread) {
+        long nextDelay = flushPeriod / (statisticsDaoList.size() + 1);
+        return new JitterStartTimeDistributor(nextDelay, spread);
+    }
 
     @Bean
     public TaskScheduler statisticsLinkScheduler(List<CachedStatisticsDao> statisticsDaoList) {
