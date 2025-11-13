2b615fc68bf1b0b5ce4f390e69ef9d324fa7e66d
emeroad <emeroad@gmail.com>
2025-11-13 19:05:18 +0900

    [#noissue] Remove unused jcl-over-slf4j

--- inspector-module/inspector-collector/src/test/java/com/navercorp/pinpoint/inspector/collector/dao/pinot/DefaultAgentStatDaoTest.java
+++ inspector-module/inspector-collector/src/test/java/com/navercorp/pinpoint/inspector/collector/dao/pinot/DefaultAgentStatDaoTest.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2024 NAVER Corp.
+ * Copyright 2025 NAVER Corp.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -38,7 +38,8 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
  */
 class DefaultAgentStatDaoTest {
 
-    private final Logger logger = LogManager.getLogger(DefaultAgentStatDaoTest.class.getName());
+    private final Logger logger = LogManager.getLogger(getClass());
+
     @Test
     public void kafkaPatitionTest() {
         int numPartitions = 64;
