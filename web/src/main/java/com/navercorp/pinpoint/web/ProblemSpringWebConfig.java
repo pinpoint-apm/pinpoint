ebcd2506f8dca922818dacfca86a3cefe3dca510
emeroad <emeroad@gmail.com>
2025-11-13 17:59:32 +0900

    [#noissue] Enhance CustomExceptionHandler with improved logging and hostname handling

--- web/src/main/java/com/navercorp/pinpoint/web/ProblemSpringWebConfig.java
+++ web/src/main/java/com/navercorp/pinpoint/web/ProblemSpringWebConfig.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,20 +15,40 @@
  */
 package com.navercorp.pinpoint.web;
 
+import com.navercorp.pinpoint.common.util.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * @author intr3p1d
  */
 @Configuration
 public class ProblemSpringWebConfig {
+    private final Logger logger = LogManager.getLogger(this.getClass());
 
     @Bean
     @Primary
-    public ResponseEntityExceptionHandler pinpointExceptionHandling() {
-        return new CustomExceptionHandler();
+    public ResponseEntityExceptionHandler pinpointExceptionHandling(@Value("${pinpoint.problemdetails.hostname:}") String hostnameAlias) {
+        if (StringUtils.isEmpty(hostnameAlias)) {
+            hostnameAlias = getHostName();
+        }
+        logger.info("pinpoint.problemdetails.hostname:{}", hostnameAlias);
+        return new CustomExceptionHandler(hostnameAlias);
+    }
+
+    static private String getHostName() {
+        try {
+            return InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            return "unknown";
+        }
     }
 }
