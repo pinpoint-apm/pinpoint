be4f9c5f8cedd1fec2726deb7b6676e417dc6005
jihea.park <jihea.park@navercorp.com>
2025-11-14 14:49:32 +0900

    [#noissue] Fix a bug where dragging a heatmap would lead to the wrong node.

--- web-frontend/src/main/v3/packages/ui/src/components/Heatmap/core/HeatmapChartCore.tsx
+++ web-frontend/src/main/v3/packages/ui/src/components/Heatmap/core/HeatmapChartCore.tsx
@@ -105,7 +105,7 @@ const HeatmapChartCore = ({
   ) => {
     window.open(
       `${BASE_PATH}${getTransactionListPath(
-        application,
+        nodeData,
         isRealtime ? getFormattedDateRange(dateRange) : searchParameters,
       )}&${getTranscationListQueryString({
         ...data,
