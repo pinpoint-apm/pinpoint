97978f86c42c62f130d2b29a970f55d0893e6352
emeroad <emeroad@gmail.com>
2025-12-03 11:31:49 +0900

    [#noissue] Remove unused exclude filters from WebHbaseModule

--- web/src/main/java/com/navercorp/pinpoint/web/WebHbaseModule.java
+++ web/src/main/java/com/navercorp/pinpoint/web/WebHbaseModule.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -30,7 +30,6 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 
@@ -48,12 +47,6 @@ import org.springframework.context.annotation.PropertySource;
 @ComponentScan(
         basePackages = {
                 "com.navercorp.pinpoint.web.dao.hbase"
-        },
-        excludeFilters = {
-                @ComponentScan.Filter(
-                        type = FilterType.ASPECTJ,
-                        pattern = "com.navercorp.pinpoint.web.dao.hbase.config.*"
-                )
         }
 )
 @PropertySource(name = "WebHbaseModule", value = {
