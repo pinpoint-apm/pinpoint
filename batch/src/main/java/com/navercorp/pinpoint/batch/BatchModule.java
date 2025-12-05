63bd2991a7c9e9d2d761a1f16d544c637f30ccd7
minwoo.jung <minwoo.jung@navercorp.com>
2025-12-06 01:53:47 +0900

    [#13175] Refactor TraceConfiguration for improved structure and component scanning

--- batch/src/main/java/com/navercorp/pinpoint/batch/BatchModule.java
+++ batch/src/main/java/com/navercorp/pinpoint/batch/BatchModule.java
@@ -34,6 +34,7 @@ import com.navercorp.pinpoint.web.WebHbaseModule;
 import com.navercorp.pinpoint.web.WebServiceConfig;
 import com.navercorp.pinpoint.web.component.config.ComponentConfiguration;
 import com.navercorp.pinpoint.web.hyperlink.HyperLinkConfiguration;
+import com.navercorp.pinpoint.web.trace.TraceConfiguration;
 import com.navercorp.pinpoint.web.uid.WebUidConfiguration;
 import com.navercorp.pinpoint.web.webhook.WebhookModule;
 import org.springframework.beans.factory.annotation.Value;
@@ -67,6 +68,7 @@ import java.util.List;
         AlarmJobModule.class,
 
         WebServiceConfig.CommonConfig.class,
+        TraceConfiguration.TraceServiceConfiguration.class,
         WebUidConfiguration.class,
 
         WebhookModule.class,
