e64885460f76e0f39ce5a01d1dea4a2660883d32
Ohjun Kwon <dev@kojandy.com>
2025-12-11 14:27:40 +0900

    [#11286] Add error mark exclude configuration

--- agent-module/profiler/src/main/java/com/navercorp/pinpoint/profiler/context/config/ErrorRecorderConfig.java
+++ agent-module/profiler/src/main/java/com/navercorp/pinpoint/profiler/context/config/ErrorRecorderConfig.java
@@ -9,11 +9,18 @@ public class ErrorRecorderConfig {
     @Value("${profiler.error.mark}")
     private String errorMarkString;
 
+    @Value("${profiler.error.mark.exclude}")
+    private String errorMarkExcludeString;
+
     public boolean isEnable() {
         return enable;
     }
 
     public String getErrorMarkString() {
         return errorMarkString;
+    }
+
+    public String getErrorMarkExcludeString() {
+        return errorMarkExcludeString;
     }
 }
