1bb8bca0b46de3bd996c1cdbd4fb5ef791222067
emeroad <emeroad@gmail.com>
2025-12-17 17:53:41 +0900

    [#noissue] Refactor DistributedScanner to use LocalScanner

--- commons-hbase/src/main/java/com/navercorp/pinpoint/common/hbase/wd/DistributedScanner.java
+++ commons-hbase/src/main/java/com/navercorp/pinpoint/common/hbase/wd/DistributedScanner.java
@@ -24,10 +24,6 @@ import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.client.metrics.ScanMetrics;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Objects;
 
 /**
@@ -38,33 +34,39 @@ import java.util.Objects;
  */
 public class DistributedScanner implements ResultScanner {
     private final int saltKeySize;
-    private final ResultScanner[] scanners;
-    private final List<Result>[] nextOfScanners;
+
+    private final LocalScanner[] localScanners;
     private Result next = null;
 
-    @SuppressWarnings("unchecked")
     public DistributedScanner(int saltKeySize, ResultScanner[] scanners) {
+        Objects.requireNonNull(scanners, "scanners");
+
         this.saltKeySize = saltKeySize;
-        this.scanners = Objects.requireNonNull(scanners, "scanners");
-        this.nextOfScanners = new List[scanners.length];
-        for (int i = 0; i < this.nextOfScanners.length; i++) {
-            this.nextOfScanners[i] = new ArrayList<>();
-        }
+        this.localScanners = wrapLocalScanners(scanners);
     }
 
-    private boolean hasNext(int nbRows) throws IOException {
+    private LocalScanner[] wrapLocalScanners(ResultScanner[] scanners) {
+        final LocalScanner[] localScanners = new LocalScanner[scanners.length];
+        for (int i = 0; i < scanners.length; i++) {
+            ResultScanner scanner = scanners[i];
+            localScanners[i] = new LocalScannerImpl(scanner);
+        }
+        return localScanners;
+    }
+
+    private boolean hasNext() throws IOException {
         if (next != null) {
             return true;
         }
 
-        next = nextInternal(nbRows);
+        next = nextInternal();
 
         return next != null;
     }
 
     @Override
     public Result next() throws IOException {
-        if (hasNext(1)) {
+        if (hasNext()) {
             Result toReturn = next;
             next = null;
             return toReturn;
@@ -74,25 +76,13 @@ public class DistributedScanner implements ResultScanner {
     }
 
     @Override
-    public Result[] next(int nbRows) throws IOException {
-        // Identical to HTable.ClientScanner implementation
-        // Collect values to be returned here
-        ArrayList<Result> resultSets = new ArrayList<>(nbRows);
-        for(int i = 0; i < nbRows; i++) {
-            Result next = next();
-            if (next != null) {
-                resultSets.add(next);
-            } else {
-                break;
-            }
-        }
-        return resultSets.toArray(new Result[0]);
-    }
-
-    @Override
     public void close() {
-        for (ResultScanner scanner : scanners) {
-            scanner.close();
+        for (LocalScanner scanner : localScanners) {
+            try {
+                scanner.close();
+            } catch (IOException ignore) {
+                // ignore
+            }
         }
     }
 
@@ -117,84 +107,31 @@ public class DistributedScanner implements ResultScanner {
         return new DistributedScanner(saltKeySize, rss);
     }
 
-    private Result nextInternal(int nbRows) throws IOException {
+    private Result nextInternal() throws IOException {
         Result result = null;
-        int indexOfScannerToUse = -1;
-        for (int i = 0; i < nextOfScanners.length; i++) {
-            if (nextOfScanners[i] == null) {
+        LocalScanner fetchedScanner = null;
+        for (LocalScanner localScanner : localScanners) {
+            if (localScanner.isExhausted()) {
                 // result scanner is exhausted, don't advance it any more
                 continue;
             }
 
-            if (nextOfScanners[i].isEmpty()) {
-                // advancing result scanner
-                Result[] results = scanners[i].next(nbRows);
-                if (results.length == 0) {
-                    // marking result scanner as exhausted
-                    nextOfScanners[i] = null;
-                    continue;
-                }
-                nextOfScanners[i].addAll(Arrays.asList(results));
+            Result localResult = localScanner.next();
+            if (localResult == null) {
+                continue;
             }
 
             // if result is null or next record has original key less than the candidate to be returned
-            if (result == null || CellUtils.compareFirstRow(nextOfScanners[i].get(0), result, saltKeySize) < 0) {
-                result = nextOfScanners[i].get(0);
-                indexOfScannerToUse = i;
+            if (result == null || CellUtils.compareFirstRow(localResult, result, saltKeySize) < 0) {
+                result = localResult;
+                fetchedScanner = localScanner;
             }
         }
 
-        if (indexOfScannerToUse >= 0) {
-            nextOfScanners[indexOfScannerToUse].remove(0);
+        if (fetchedScanner != null) {
+            fetchedScanner.consume();
         }
-
         return result;
-    }
-
-    @Override
-    public Iterator<Result> iterator() {
-        // Identical to HTable.ClientScanner implementation
-        return new Iterator<>() {
-            // The next RowResult, possibly pre-read
-            Result next = null;
-
-            // return true if there is another item pending, false if there isn't.
-            // this method is where the actual advancing takes place, but you need
-            // to call next() to consume it. hasNext() will only advance if there
-            // isn't a pending next().
-            public boolean hasNext() {
-                if (next == null) {
-                    try {
-                        next = DistributedScanner.this.next();
-                        return next != null;
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-                return true;
-            }
-
-            // get the pending next item and advance the iterator. returns null if
-            // there is no next item.
-            public Result next() {
-                // since hasNext() does the real advancing, we call this to determine
-                // if there is a next before proceeding.
-                if (!hasNext()) {
-                    return null;
-                }
-
-                // if we get to here, then hasNext() has given us an item to return.
-                // we want to return the item and then null out the next pointer, so
-                // we use a temporary variable.
-                Result temp = next;
-                next = null;
-                return temp;
-            }
-
-            public void remove() {
-                throw new UnsupportedOperationException();
-            }
-        };
     }
 }
 
