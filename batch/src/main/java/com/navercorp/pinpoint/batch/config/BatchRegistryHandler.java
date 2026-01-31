258fb12bcd2f5f2b2dc2267a9d0d9cf30fec69f7
minwoo.jung <minwoo.jung@navercorp.com>
2026-01-31 17:40:13 +0900

    [#13287] Improved MyBatis TypeHandler configuration

--- batch/src/main/java/com/navercorp/pinpoint/batch/config/BatchRegistryHandler.java
+++ batch/src/main/java/com/navercorp/pinpoint/batch/config/BatchRegistryHandler.java
@@ -16,6 +16,7 @@
 
 package com.navercorp.pinpoint.batch.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.navercorp.pinpoint.batch.alarm.dao.model.BatchQueryParameter;
 import com.navercorp.pinpoint.batch.alarm.vo.AgentFieldUsage;
 import com.navercorp.pinpoint.batch.alarm.vo.AgentUsage;
@@ -25,13 +26,29 @@ import com.navercorp.pinpoint.common.model.TagInformation;
 import com.navercorp.pinpoint.metric.common.model.Tag;
 import com.navercorp.pinpoint.metric.common.mybatis.typehandler.TagTypeHandler;
 import com.navercorp.pinpoint.mybatis.MyBatisRegistryHandler;
+import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.type.TypeAliasRegistry;
 import org.apache.ibatis.type.TypeHandlerRegistry;
+import org.apache.ibatis.type.TypeReference;
+
+import java.util.List;
+import java.util.Objects;
 
 /**
  * @author minwoo-jung
  */
 public class BatchRegistryHandler implements MyBatisRegistryHandler {
+
+    private final ObjectMapper mapper;
+
+    public BatchRegistryHandler(ObjectMapper mapper) {
+        this.mapper = Objects.requireNonNull(mapper, "mapper");
+    }
+
+    public void registerHandlers(Configuration config) {
+        registerTypeAlias(config.getTypeAliasRegistry());
+        registerTypeHandler(config.getTypeHandlerRegistry());
+    }
 
     @Override
     public void registerTypeAlias(TypeAliasRegistry typeAliasRegistry) {
@@ -47,5 +64,8 @@ public class BatchRegistryHandler implements MyBatisRegistryHandler {
     @Override
     public void registerTypeHandler(TypeHandlerRegistry typeHandlerRegistry) {
         typeHandlerRegistry.register(Tag.class, TagTypeHandler.class);
+
+        TypeReference<List<Tag>> javaTypeReference = new TypeReference<>() {};
+        typeHandlerRegistry.register(javaTypeReference, new MultiValueTagTypeHandler(mapper));
     }
 }
