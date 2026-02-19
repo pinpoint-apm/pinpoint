1957b5033c49574c31a016cc9cdd8ca8f55e930a
minwoo.jung <minwoo.jung@navercorp.com>
2026-02-19 16:52:33 +0900

    [#13287] Remove unused XML configurations from UriStatAlarmConfiguration

--- uristat/uristat-batch/src/main/java/com/navercorp/pinpoint/batch/UriStatAlarmConfiguration.java
+++ uristat/uristat-batch/src/main/java/com/navercorp/pinpoint/batch/UriStatAlarmConfiguration.java
@@ -2,9 +2,7 @@ package com.navercorp.pinpoint.batch;
 
 import com.navercorp.pinpoint.batch.alarm.config.UriStatBatchDaoConfiguration;
 import com.navercorp.pinpoint.batch.config.PinotBatchDaoConfig;
-import com.navercorp.pinpoint.batch.config.PinotBatchDaoXmlConfig;
 import com.navercorp.pinpoint.batch.config.UriAlarmJobConfig;
-import com.navercorp.pinpoint.batch.config.UriAlarmJobXmlConfig;
 import com.navercorp.pinpoint.pinot.config.PinotConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -13,9 +11,6 @@ import org.springframework.context.annotation.Import;
 @Import({
         UriStatBatchDaoConfiguration.class,
         PinotConfiguration.class,
-
-        PinotBatchDaoXmlConfig.class,
-        UriAlarmJobXmlConfig.class,
 
         PinotBatchDaoConfig.class,
         UriAlarmJobConfig.class
