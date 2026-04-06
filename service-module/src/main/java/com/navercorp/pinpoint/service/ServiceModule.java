c61a6c0ed004ab8d980a5547067793abd90ff535
minwoo.jung <minwoo.jung@navercorp.com>
2026-04-06 18:08:24 +0900

    [#13534] Deprecate ServiceModule and remove associated configuration and bean definitions

--- service-module/src/main/java/com/navercorp/pinpoint/service/ServiceModule.java
+++ service-module/src/main/java/com/navercorp/pinpoint/service/ServiceModule.java
@@ -1,40 +1,11 @@
 package com.navercorp.pinpoint.service;
 
-import com.navercorp.pinpoint.common.server.uid.ServiceUid;
-import com.navercorp.pinpoint.common.server.util.IdGenerator;
-import com.navercorp.pinpoint.common.server.util.RandomServiceUidGenerator;
-import com.navercorp.pinpoint.service.component.StaticServiceRegistry;
-import com.navercorp.pinpoint.service.config.mysql.ServiceMysqlDaoConfiguration;
-import com.navercorp.pinpoint.service.dao.EmptyServiceDao;
-import com.navercorp.pinpoint.service.dao.ServiceDao;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
-
 @Deprecated
-@Import({
-        ServiceMysqlDaoConfiguration.class,
-})
-@ComponentScan(basePackages = {
-        "com.navercorp.pinpoint.service.dao",
-        "com.navercorp.pinpoint.service.service",
-})
 public class ServiceModule {
 
-    @Bean
-    public StaticServiceRegistry staticServiceRegistry() {
-        return new StaticServiceRegistry();
-    }
-
-    @Bean
-    public IdGenerator<ServiceUid> serviceUidGenerator() {
-        return new RandomServiceUidGenerator();
-    }
-
-    @Bean
-    @ConditionalOnMissingBean(ServiceDao.class)
-    public ServiceDao serviceDao() {
-        return new EmptyServiceDao();
-    }
 }
+
+
+
+
+
