2af1da3ffd00176b20178d260633c5911d2b06ea
Seoyoung Park <se0y0ung.park@navercorp.com>
2026-04-20 19:17:36 +0900

    [#noissue] Revert "[#noissue] Add @Validated to controllers to prevent empty applicationName from reaching HBase scan"
    This reverts commit e70f213e77473521a9ad2450dd21126cf3b4f86e.

--- web/src/main/java/com/navercorp/pinpoint/web/authorization/controller/ResponseTimeController.java
+++ web/src/main/java/com/navercorp/pinpoint/web/authorization/controller/ResponseTimeController.java
@@ -47,7 +47,6 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -64,7 +63,6 @@ import java.util.Objects;
 
 @RestController
 @RequestMapping("/api")
-@Validated
 public class ResponseTimeController {
     private final Logger logger = LogManager.getLogger(this.getClass());
 
