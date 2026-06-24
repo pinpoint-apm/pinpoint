8a162b963c93011e13df4a64aa000de4fcb857dc
emeroad <emeroad@gmail.com>
2026-06-24 16:39:38 +0900

    [#13903] Cache ColumnNameV3 instances by slot code

--- commons-server/src/main/java/com/navercorp/pinpoint/common/server/applicationmap/statistics/v3/ColumnNameV3.java
+++ commons-server/src/main/java/com/navercorp/pinpoint/common/server/applicationmap/statistics/v3/ColumnNameV3.java
@@ -18,7 +18,6 @@ package com.navercorp.pinpoint.common.server.applicationmap.statistics.v3;
 
 import com.navercorp.pinpoint.common.server.applicationmap.statistics.ColumnName;
 import com.navercorp.pinpoint.common.trace.SlotCode;
-import org.apache.hadoop.hbase.Cell;
 
 
 /**
@@ -26,10 +25,60 @@ import org.apache.hadoop.hbase.Cell;
  */
 public class ColumnNameV3 implements ColumnName {
 
+    private static final ColumnNameV3 F_NORMAL = new ColumnNameV3(SlotCode.F_NORMAL.code());
+    private static final ColumnNameV3 F_FAST = new ColumnNameV3(SlotCode.F_FAST.code());
+    private static final ColumnNameV3 F_SLOW = new ColumnNameV3(SlotCode.F_SLOW.code());
+    private static final ColumnNameV3 F_VERY_SLOW = new ColumnNameV3(SlotCode.F_VERY_SLOW.code());
+
+    private static final ColumnNameV3 F_FAST_ERROR = new ColumnNameV3(SlotCode.F_FAST_ERROR.code());
+    private static final ColumnNameV3 F_NORMAL_ERROR = new ColumnNameV3(SlotCode.F_NORMAL_ERROR.code());
+    private static final ColumnNameV3 F_SLOW_ERROR = new ColumnNameV3(SlotCode.F_SLOW_ERROR.code());
+    private static final ColumnNameV3 F_VERY_SLOW_ERROR = new ColumnNameV3(SlotCode.F_VERY_SLOW_ERROR.code());
+
+    private static final ColumnNameV3 N_FAST = new ColumnNameV3(SlotCode.N_FAST.code());
+    private static final ColumnNameV3 N_NORMAL = new ColumnNameV3(SlotCode.N_NORMAL.code());
+    private static final ColumnNameV3 N_SLOW = new ColumnNameV3(SlotCode.N_SLOW.code());
+    private static final ColumnNameV3 N_VERY_SLOW = new ColumnNameV3(SlotCode.N_VERY_SLOW.code());
+
+    private static final ColumnNameV3 N_FAST_ERROR = new ColumnNameV3(SlotCode.N_FAST_ERROR.code());
+    private static final ColumnNameV3 N_NORMAL_ERROR = new ColumnNameV3(SlotCode.N_NORMAL_ERROR.code());
+    private static final ColumnNameV3 N_SLOW_ERROR = new ColumnNameV3(SlotCode.N_SLOW_ERROR.code());
+    private static final ColumnNameV3 N_VERY_SLOW_ERROR = new ColumnNameV3(SlotCode.N_VERY_SLOW_ERROR.code());
+
+    private static final ColumnNameV3 ERROR = new ColumnNameV3(SlotCode.ERROR.code());
+    private static final ColumnNameV3 SUM_STAT = new ColumnNameV3(SlotCode.SUM_STAT.code());
+    private static final ColumnNameV3 MAX_STAT = new ColumnNameV3(SlotCode.MAX_STAT.code());
+    private static final ColumnNameV3 PING = new ColumnNameV3(SlotCode.PING.code());
+
     private final byte slotCode;
 
     public static ColumnName histogram(SlotCode slotCode) {
-        return new ColumnNameV3(slotCode.code());
+        return valueOf(slotCode);
+    }
+
+    private static ColumnNameV3 valueOf(SlotCode slotCode) {
+        return switch (slotCode) {
+            case F_NORMAL -> F_NORMAL;
+            case F_FAST -> F_FAST;
+            case F_SLOW -> F_SLOW;
+            case F_VERY_SLOW -> F_VERY_SLOW;
+            case F_FAST_ERROR -> F_FAST_ERROR;
+            case F_NORMAL_ERROR -> F_NORMAL_ERROR;
+            case F_SLOW_ERROR -> F_SLOW_ERROR;
+            case F_VERY_SLOW_ERROR -> F_VERY_SLOW_ERROR;
+            case N_FAST -> N_FAST;
+            case N_NORMAL -> N_NORMAL;
+            case N_SLOW -> N_SLOW;
+            case N_VERY_SLOW -> N_VERY_SLOW;
+            case N_FAST_ERROR -> N_FAST_ERROR;
+            case N_NORMAL_ERROR -> N_NORMAL_ERROR;
+            case N_SLOW_ERROR -> N_SLOW_ERROR;
+            case N_VERY_SLOW_ERROR -> N_VERY_SLOW_ERROR;
+            case ERROR -> ERROR;
+            case SUM_STAT -> SUM_STAT;
+            case MAX_STAT -> MAX_STAT;
+            case PING -> PING;
+        };
     }
 
     public ColumnNameV3(byte slotCode) {
@@ -46,12 +95,6 @@ public class ColumnNameV3 implements ColumnName {
 
     public static byte[] makeColumnName(byte slotCode) {
         return new byte[] { slotCode };
-    }
-
-    public static ColumnNameV3 makeColumnName(Cell cell) {
-        byte[] qualifierArray = cell.getQualifierArray();
-        byte slotCode = qualifierArray[cell.getQualifierOffset()];
-        return new ColumnNameV3(slotCode);
     }
 
     @Override
