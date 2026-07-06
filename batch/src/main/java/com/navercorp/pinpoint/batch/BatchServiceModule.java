22f52c8255de47cd99abe1e542060080daee8ed8
minwoo.jung <minwoo.jung@navercorp.com>
2026-07-06 13:59:35 +0900

    [#13924] Add BatchServiceModule for batch module service bean configuration

---
+++ batch/src/main/java/com/navercorp/pinpoint/batch/BatchServiceModule.java
@@ -0,0 +1,41 @@
+package com.navercorp.pinpoint.batch;
+
+import com.navercorp.pinpoint.common.server.uid.ServiceUid;
+import com.navercorp.pinpoint.common.server.util.IdGenerator;
+import com.navercorp.pinpoint.common.server.util.RandomServiceUidGenerator;
+import com.navercorp.pinpoint.service.config.ServiceMysqlConfiguration;
+import com.navercorp.pinpoint.service.service.ServiceRegistryService;
+import com.navercorp.pinpoint.web.applicationmap.servicemap.ServiceResolver;
+import com.navercorp.pinpoint.web.service.ServiceModelResolver;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+
+@Import({
+        ServiceMysqlConfiguration.class,
+})
+@ComponentScan(
+        basePackages = {
+                "com.navercorp.pinpoint.service.service",
+        }
+)
+public class BatchServiceModule {
+
+    @Bean
+    @ConditionalOnMissingBean(ServiceResolver.class)
+    public ServiceResolver serviceResolver() {
+        return ServiceResolver.emptyResolver();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(name = "serviceUidGenerator")
+    public IdGenerator<ServiceUid> serviceUidGenerator() {
+        return new RandomServiceUidGenerator();
+    }
+
+    @Bean
+    public ServiceModelResolver serviceModelResolver(ServiceRegistryService serviceRegistryService) {
+        return new ServiceModelResolver(serviceRegistryService);
+    }
+}
