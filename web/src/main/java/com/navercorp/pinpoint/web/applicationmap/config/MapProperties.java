b254b7b2781cf9603e2929794923e067f8f01dbe
minwoo.jung <jungmwiner@gmail.com>
2026-09-01 17:25:41 +0900

    [#14224] Decouple service map activation from experimental.enableServiceMap flag

--- web/src/main/java/com/navercorp/pinpoint/web/applicationmap/config/MapProperties.java
+++ web/src/main/java/com/navercorp/pinpoint/web/applicationmap/config/MapProperties.java
@@ -3,7 +3,7 @@ package com.navercorp.pinpoint.web.applicationmap.config;
 import org.springframework.beans.factory.annotation.Value;
 
 public class MapProperties {
-    @Value("${experimental.enableServiceMap.value:false}")
+    @Value("${pinpoint.modules.web.servicemap.enabled:false}")
     private boolean enableServiceMap;
 
     public boolean isEnableServiceMap() {
