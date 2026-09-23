de79f54853813e2dc740f9bd08257beb4c7d9873
emeroad <emeroad@gmail.com>
2026-09-23 16:09:31 +0900

    [#noissue] Merge the pre-release version patterns into one case-insensitive alternation

--- agent-module/plugins-test-module/plugins-test/src/main/java/com/navercorp/pinpoint/test/plugin/maven/DependencyVersionFilter.java
+++ agent-module/plugins-test-module/plugins-test/src/main/java/com/navercorp/pinpoint/test/plugin/maven/DependencyVersionFilter.java
@@ -24,24 +24,14 @@ public class DependencyVersionFilter implements Predicate<String> {
     public static boolean FILTERED = true;
     public static boolean NOT_FILTERED = false;
 
-    private static final Pattern RC_PATTERN = Pattern.compile(".*[Rr][Cc]-?\\.?\\d*$");
-    private static final Pattern M_PATTERN = Pattern.compile(".*[Mm]-?\\.?\\d*$");
-    private static final Pattern ALPHA_PATTERN = Pattern.compile(".*[Aa][Ll][Pp][Hh][Aa]-?\\.?\\d*$");
-    private static final Pattern BETA_PATTERN = Pattern.compile(".*[Bb][Ee][Tt][Aa]-?\\.?\\d*$");
-    private static final Pattern PATCH_PATTERN = Pattern.compile(".*[Pp][Aa][Tt][Cc][Hh]-?\\.?\\d*$");
-    private static final Pattern TEST_PATTERN = Pattern.compile(".*[Tt][Ee][Ss][Tt]-?\\.?\\d*$");
-    private static final Pattern MILESTONE_PATTERN = Pattern.compile(".*[Mm][Ii][Ll][Ee][Ss][Tt][Oo][Nn][Ee]-?\\.?\\d*$");
-    private static final Pattern PRE_PATTERN = Pattern.compile(".*[Pp][Rr][Ee]-?\\.?\\d*$");
-
-    private static final Pattern[] PATTERNS = new Pattern[]{RC_PATTERN, M_PATTERN, ALPHA_PATTERN, BETA_PATTERN, PATCH_PATTERN, TEST_PATTERN, MILESTONE_PATTERN, PRE_PATTERN};
-
+    // one alternation instead of one Pattern per qualifier: test() then allocates a single Matcher per version
+    private static final Pattern PRE_RELEASE_PATTERN = Pattern.compile(
+            ".*(?:rc|m|alpha|beta|patch|test|milestone|pre)-?\\.?\\d*$", Pattern.CASE_INSENSITIVE);
 
     @Override
     public boolean test(String value) {
-        for (Pattern pattern : PATTERNS) {
-            if (pattern.matcher(value).matches()) {
-                return FILTERED;
-            }
+        if (PRE_RELEASE_PATTERN.matcher(value).matches()) {
+            return FILTERED;
         }
         return NOT_FILTERED;
     }
